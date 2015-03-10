44a6e9ef78089b02f97ae4eadb16b7403d7f75cb
Henri Sara <hesara@vaadin.com>
2015-03-10 15:20:50 +0200

    Merge AbstractSpringUIProvider to SpringUIProvider
    Change-Id: Ib48648961f1c22d3ce606b452d68d6d3c10111b3

--- vaadin-spring/src/main/java/com/vaadin/spring/server/SpringUIProvider.java
+++ vaadin-spring/src/main/java/com/vaadin/spring/server/SpringUIProvider.java
@@ -15,12 +15,21 @@
  */
 package com.vaadin.spring.server;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.vaadin.server.UIClassSelectionEvent;
+import com.vaadin.server.UICreateEvent;
+import com.vaadin.server.UIProvider;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.spring.internal.Conventions;
-import com.vaadin.spring.server.internal.AbstractSpringUIProvider;
+import com.vaadin.spring.internal.UIID;
 import com.vaadin.ui.UI;
+import com.vaadin.util.CurrentInstance;
 
 /**
  * Vaadin {@link com.vaadin.server.UIProvider} that looks up UI classes from the
@@ -28,16 +37,30 @@ import com.vaadin.ui.UI;
  * {@link com.vaadin.spring.annotation.SpringUI}.
  *
  * @author Petter Holmström (petter@vaadin.com)
+ * @author Henri Sara (hesara@vaadin.com)
  */
-public class SpringUIProvider extends AbstractSpringUIProvider {
+public class SpringUIProvider extends UIProvider {
 
     private static final long serialVersionUID = 6954428459733726004L;
 
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final WebApplicationContext webApplicationContext;
+    private final Map<String, Class<? extends UI>> pathToUIMap = new ConcurrentHashMap<String, Class<? extends UI>>();
+    private final Map<String, Class<? extends UI>> wildcardPathToUIMap = new ConcurrentHashMap<String, Class<? extends UI>>();
+
     public SpringUIProvider(WebApplicationContext webApplicationContext) {
-        super(webApplicationContext);
+        if (webApplicationContext == null) {
+            throw new IllegalStateException(
+                    "Spring WebApplicationContext not initialized for UI provider. Use e.g. ContextLoaderListener to initialize it.");
+        }
+        this.webApplicationContext = webApplicationContext;
+        detectUIs();
+        if (pathToUIMap.isEmpty()) {
+            logger.warn("Found no Vaadin UIs in the application context");
+        }
     }
 
-    @Override
     @SuppressWarnings("unchecked")
     protected void detectUIs() {
         logger.info("Checking the application context for Vaadin UIs");
@@ -89,6 +112,73 @@ public class SpringUIProvider extends AbstractSpringUIProvider {
      */
     protected String deriveMappingForUI(Class<?> beanType, SpringUI annotation) {
         return Conventions.deriveMappingForUI(beanType, annotation);
+    }
+
+    @Override
+    public Class<? extends UI> getUIClass(
+            UIClassSelectionEvent uiClassSelectionEvent) {
+        final String path = extractUIPathFromPathInfo(uiClassSelectionEvent
+                .getRequest().getPathInfo());
+        if (pathToUIMap.containsKey(path)) {
+            return pathToUIMap.get(path);
+        }
+
+        for (Map.Entry<String, Class<? extends UI>> entry : wildcardPathToUIMap
+                .entrySet()) {
+            if (path.startsWith(entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    private String extractUIPathFromPathInfo(String pathInfo) {
+        if (pathInfo != null && pathInfo.length() > 1) {
+            String path = pathInfo;
+            final int indexOfBang = path.indexOf('!');
+            if (indexOfBang > -1) {
+                path = path.substring(0, indexOfBang);
+            }
+
+            if (path.endsWith("/")) {
+                path = path.substring(0, path.length() - 1);
+            }
+            return path;
+        }
+        return "";
+    }
+
+    protected WebApplicationContext getWebApplicationContext() {
+        return webApplicationContext;
+    }
+
+    protected void mapPathToUI(String path, Class<? extends UI> uiClass) {
+        if (path.endsWith("/*")) {
+            wildcardPathToUIMap.put(path.substring(0, path.length() - 2),
+                    uiClass);
+        } else {
+            pathToUIMap.put(path, uiClass);
+        }
+    }
+
+    protected Class<? extends UI> getUIByPath(String path) {
+        return pathToUIMap.get(path);
+    }
+
+    @Override
+    public UI createInstance(UICreateEvent event) {
+        final Class<UIID> key = UIID.class;
+        final UIID identifier = new UIID(event);
+        CurrentInstance.set(key, identifier);
+        try {
+            logger.debug(
+                    "Creating a new UI bean of class [{}] with identifier [{}]",
+                    event.getUIClass().getCanonicalName(), identifier);
+            return webApplicationContext.getBean(event.getUIClass());
+        } finally {
+            CurrentInstance.set(key, null);
+        }
     }
 
 }
