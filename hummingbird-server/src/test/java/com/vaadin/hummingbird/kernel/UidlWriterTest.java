abab3a731c0394d8adf7ff4207b8ed0bc9089eaf
Leif Åstrand <leif@vaadin.com>
2015-08-25 10:10:47 +0300

    Introduce UidlWriter tests, test that boolean attribute is JSON boolean

---
+++ hummingbird-server/src/test/java/com/vaadin/hummingbird/kernel/UidlWriterTest.java
@@ -0,0 +1,70 @@
+package com.vaadin.hummingbird.kernel;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.communication.UidlWriter;
+import com.vaadin.ui.UI;
+
+import elemental.json.Json;
+import elemental.json.JsonArray;
+import elemental.json.JsonObject;
+import elemental.json.JsonType;
+import elemental.json.JsonValue;
+
+public class UidlWriterTest {
+    private static class UidlWriterTestUI extends UI {
+        @Override
+        protected void init(VaadinRequest request) {
+            // Do nothing
+        }
+    }
+
+    private Element element;
+    private UI ui;
+
+    @Before
+    public void setup() {
+        ui = new UI() {
+            @Override
+            protected void init(VaadinRequest request) {
+                // Do nothing
+            }
+        };
+        ui.getRootNode().commit();
+
+        element = ui.getElement();
+    }
+
+    private JsonObject encode() {
+        JsonObject response = Json.createObject();
+        UidlWriter.encodeChanges(ui, response);
+        return response;
+    }
+
+    private JsonArray encodeElementChanges() {
+        return encode().getArray("elementChanges");
+    }
+
+    @Test
+    public void testBooleanAttribute_booleanChangeValue() {
+        element.setAttribute("foo", true);
+
+        JsonArray changes = encodeElementChanges();
+
+        Assert.assertEquals(1, changes.length());
+
+        JsonObject change = changes.getObject(0);
+
+        Assert.assertEquals("put", change.getString("type"));
+        Assert.assertEquals("foo", change.getString("key"));
+
+        JsonValue value = change.get("value");
+
+        Assert.assertNotNull(value);
+        Assert.assertEquals(JsonType.BOOLEAN, value.getType());
+        Assert.assertTrue(value.asBoolean());
+    }
+}
