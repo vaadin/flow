26ffc555d2e8377f01f1c292134259fa28baa04b
Artur Signell <artur@vaadin.com>
2015-10-27 17:34:22 +0200

    Fix value change listener for text field

--- hummingbird-server/src/main/java/com/vaadin/ui/TextField.java
+++ hummingbird-server/src/main/java/com/vaadin/ui/TextField.java
@@ -123,4 +123,16 @@ public class TextField extends AbstractTextField {
         getElement().setAttribute("value", getInternalValue());
     }
 
+    @Override
+    public void addValueChangeListener(
+            com.vaadin.data.Property.ValueChangeListener listener) {
+        if (!hasListeners(ValueChangeEvent.class)) {
+            getElement().addEventData("change", "element.value");
+            getElement().addEventListener("change", e -> {
+                setValue(e.getString("element.value"));
+            });
+        }
+        super.addValueChangeListener(listener);
+
+    }
 }
