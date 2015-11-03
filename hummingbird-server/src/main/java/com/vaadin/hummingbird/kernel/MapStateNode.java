ac6e39a080cbbf311be470e853d32cd34db5f11d
Artur Signell <artur@vaadin.com>
2015-11-03 10:36:58 +0200

    Ease debugging of statenodes

--- hummingbird-server/src/main/java/com/vaadin/hummingbird/kernel/MapStateNode.java
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/kernel/MapStateNode.java
@@ -37,4 +37,9 @@ public class MapStateNode extends StateNode {
         return Object.class;
     }
 
+    @Override
+    public String toString() {
+        return "MapStateNode [values=" + values + "]";
+    }
+
 }
