6d69db443b164bbd199687cb8745b60d0c035a43
Leif Åstrand <leif@vaadin.com>
2015-12-03 18:19:19 +0200

    Simplify pruning by not removing empty lists

--- hummingbird-server/src/main/java/com/vaadin/server/communication/TransactionLogPruner.java
+++ hummingbird-server/src/main/java/com/vaadin/server/communication/TransactionLogPruner.java
@@ -1,6 +1,5 @@
 package com.vaadin.server.communication;
 
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,18 +15,13 @@ public class TransactionLogPruner {
             LinkedHashMap<StateNode, List<NodeChange>> changes, UI ui) {
         Map<Integer, Set<NodeChange>> ignoreChanges = ui.getIgnoreChanges();
 
-        Iterator<StateNode> iterator = changes.keySet().iterator();
-        while (iterator.hasNext()) {
-            StateNode node = iterator.next();
-            List<NodeChange> nodeChanges = changes.get(node);
+        changes.forEach((node, nodeChanges) -> {
             Set<NodeChange> nodeIgnores = ignoreChanges.get(node.getId());
             if (nodeIgnores != null) {
                 nodeChanges.removeAll(nodeIgnores);
-                if (nodeChanges.isEmpty()) {
-                    iterator.remove();
-                }
             }
-        }
+        });
+
         ignoreChanges.clear();
 
         return changes;
