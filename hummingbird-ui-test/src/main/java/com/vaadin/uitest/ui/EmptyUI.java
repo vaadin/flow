809cf0efa3ce073bb6067509c0fa29efc0ede187
Artur Signell <artur@vaadin.com>
2016-01-20 16:02:24 +0200

    Replace Ivy+Ant with Maven
    * Use Java 8
    * Use GWT 2.8.0-beta1
    * Prefix all modules with hummingbird-
    
    Closes #93

---
+++ hummingbird-ui-test/src/main/java/com/vaadin/uitest/ui/EmptyUI.java
@@ -0,0 +1,13 @@
+package com.vaadin.uitest.ui;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+public class EmptyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+    }
+
+}
