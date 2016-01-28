d2ca074854683a6078c45417a29f731493a1017c
Artur Signell <artur@vaadin.com>
2016-01-28 15:04:42 +0200

    Remove old framework scripts

--- scripts/generateFontAwesomeEnum.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -s https://raw.githubusercontent.com/FortAwesome/Font-Awesome/v4.5.0/scss/_variables.scss|grep fa-var-|sed "s/.*fa-var-//"|sed "s/-/_/g"|tr a-z A-Z|sed 's/: "\\/(0X/'|sed 's#";#), //#'|sed "s/^\\([0-9]\\)/_\\1/"
-echo ";"
