63971fc0b01f0fd355ad2ead127090a197142476
Leif Åstrand <leif@vaadin.com>
2016-02-02 12:28:05 +0200

    Add reactive base functionality

---
+++ hummingbird-client/src/main/java/com/vaadin/client/hummingbird/reactive/ReactiveEventRouter.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.client.hummingbird.reactive;
+
+import com.vaadin.client.hummingbird.collection.JsCollections;
+import com.vaadin.client.hummingbird.collection.JsSet;
+
+import elemental.events.EventRemover;
+
+/**
+ * Event router providing integration with reactive features in {@link Reactive}
+ * and {@link Computation}. Listeners can be added both for a specific event
+ * type as well as for generic reactive change events. All events are fired to
+ * both types of listeners, as well as to event collectors registered using
+ * {@link Reactive#addEventCollector(ReactiveChangeListener)}.
+ *
+ * @since
+ * @author Vaadin Ltd
+ * @param <L>
+ *            the listener type of this router
+ * @param <E>
+ *            the reactive event type of this router
+ */
+public abstract class ReactiveEventRouter<L, E extends ReactiveChangeEvent> {
+    private final JsSet<L> listeners = JsCollections.set();
+
+    private final ReactiveValue reactiveValue;
+
+    /**
+     * Creates a new event router for a reactive value.
+     *
+     * @param reactiveValue
+     *            the reactive value, not <code>null</code>
+     */
+    public ReactiveEventRouter(ReactiveValue reactiveValue) {
+        assert reactiveValue != null;
+
+        this.reactiveValue = reactiveValue;
+    }
+
+    /**
+     * Adds a listener to this event router.
+     *
+     * @param listener
+     *            the listener to add
+     * @return an event remover that can be used for removing the added listener
+     */
+    public EventRemover addListener(L listener) {
+        listeners.add(listener);
+        EventRemover remover = () -> listeners.delete(listener);
+
+        Computation computation = Reactive.getCurrentComputation();
+        if (computation != null) {
+            computation.onNextInvalidate(e -> remover.remove());
+        }
+
+        return remover;
+    }
+
+    /**
+     * Adds a generic reactive change listener to this router.
+     *
+     * @param listener
+     *            the change listener
+     * @return an event remover that can be used for removing the added listener
+     */
+    public EventRemover addReactiveListener(ReactiveChangeListener listener) {
+        return addListener(wrap(listener));
+    }
+
+    /**
+     * Fires an event to all listeners added to this router using
+     * {@link #addListener(Object)} or
+     * {@link #addReactiveListener(ReactiveChangeListener)} as well as all
+     * global event collectors added using
+     * {@link Reactive#addEventCollector(ReactiveChangeListener)}.
+     *
+     * @param event
+     *            the event to fire
+     */
+    public void fireEvent(E event) {
+        assert event.getSource() == reactiveValue;
+
+        JsSet<L> copy = JsCollections.set(listeners);
+
+        copy.forEach(listener -> dispatchEvent(listener, event));
+
+        Reactive.notifyEventCollectors(event);
+    }
+
+    /**
+     * Registers access to the data for which this event router fires event.
+     * This registers the event source of this event router to be set as a
+     * dependency of the current computation if there is one.
+     */
+    public void registerRead() {
+        Computation computation = Reactive.getCurrentComputation();
+        if (computation != null) {
+            computation.addDependency(reactiveValue);
+        }
+    }
+
+    /**
+     * Gets the reactive value for which this router fires event.
+     *
+     * @return the reactive value
+     */
+    public ReactiveValue getReactiveValue() {
+        return reactiveValue;
+    }
+
+    /**
+     * Callback for wrapping a generic reactive change listener to an instance
+     * of the listener type natively supported by this event router.
+     *
+     * @param listener
+     *            the reactive change listener
+     * @return an event listener wrapping the provided listener
+     */
+    protected abstract L wrap(ReactiveChangeListener listener);
+
+    /**
+     * Callback for dispatching an event to a listener.
+     *
+     * @param listener
+     *            the listener that should receive the event
+     * @param event
+     *            the event to dispatch
+     */
+    protected abstract void dispatchEvent(L listener, E event);
+}
