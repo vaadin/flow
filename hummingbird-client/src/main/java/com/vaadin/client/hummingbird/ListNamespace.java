5cd51c2863a0cc8f2416a617115ca1628f126bbc
Leif Åstrand <leif@vaadin.com>
2016-02-02 15:40:34 +0200

    Add event support to the client-side state tree

--- hummingbird-client/src/main/java/com/vaadin/client/hummingbird/ListNamespace.java
+++ hummingbird-client/src/main/java/com/vaadin/client/hummingbird/ListNamespace.java
@@ -17,26 +17,54 @@ package com.vaadin.client.hummingbird;
 
 import com.vaadin.client.hummingbird.collection.JsArray;
 import com.vaadin.client.hummingbird.collection.JsCollections;
+import com.vaadin.client.hummingbird.reactive.Computation;
+import com.vaadin.client.hummingbird.reactive.ReactiveChangeListener;
+import com.vaadin.client.hummingbird.reactive.ReactiveEventRouter;
+import com.vaadin.client.hummingbird.reactive.ReactiveValue;
 
+import elemental.events.EventRemover;
 import elemental.json.Json;
 import elemental.json.JsonArray;
 import elemental.json.JsonValue;
 
 /**
  * A state node namespace that structures data as a list.
+ * <p>
+ * The list works as a reactive value with regards to its structure. A
+ * {@link Computation} will get a dependency on this list for any read operation
+ * that depends on the list structure, such as querying the length, iterating
+ * the list or finding the index of an item. Accessing an item by index does not
+ * create a dependency. The <code>Computation</code> is invalidated when items
+ * are added, removed, reordered or replaced. It is not invalidated when the
+ * contents of an item is updated since all items are expected to be either
+ * immutable or reactive values of their own.
  *
  * @since
  * @author Vaadin Ltd
  */
-public class ListNamespace extends AbstractNamespace {
+public class ListNamespace extends AbstractNamespace implements ReactiveValue {
 
     private final JsArray<Object> values = JsCollections.array();
+
+    private final ReactiveEventRouter<ListSpliceListener, ListSpliceEvent> eventRouter = new ReactiveEventRouter<ListSpliceListener, ListSpliceEvent>(
+            this) {
+        @Override
+        protected ListSpliceListener wrap(ReactiveChangeListener listener) {
+            return listener::onChange;
+        }
+
+        @Override
+        protected void dispatchEvent(ListSpliceListener listener,
+                ListSpliceEvent event) {
+            listener.onSplice(event);
+        }
+    };
 
     /**
      * Creates a new list namespace.
      *
      * @param id
-     *            the if of the namespace
+     *            the id of the namespace
      * @param node
      *            the node of the namespace
      */
@@ -50,6 +78,7 @@ public class ListNamespace extends AbstractNamespace {
      * @return the number of items
      */
     public int length() {
+        eventRouter.registerRead();
         return values.length();
     }
 
@@ -85,7 +114,9 @@ public class ListNamespace extends AbstractNamespace {
      *            the number of items to remove
      */
     public void splice(int index, int remove) {
-        values.splice(index, remove);
+        JsArray<Object> removed = values.splice(index, remove);
+        eventRouter.fireEvent(
+                new ListSpliceEvent(this, index, removed, new Object[0]));
     }
 
     /**
@@ -100,7 +131,8 @@ public class ListNamespace extends AbstractNamespace {
      */
     @SafeVarargs
     public final <T> void splice(int index, int remove, T... add) {
-        values.splice(index, remove, add);
+        JsArray<Object> removed = values.splice(index, remove, add);
+        eventRouter.fireEvent(new ListSpliceEvent(this, index, removed, add));
     }
 
     @Override
@@ -115,5 +147,22 @@ public class ListNamespace extends AbstractNamespace {
         }
 
         return json;
+    }
+
+    /**
+     * Adds a listener that will be notified when the list structure changes.
+     *
+     * @param listener
+     *            the list change listener
+     * @return an event remover that can be used for removing the added listener
+     */
+    public EventRemover addSpliceListener(ListSpliceListener listener) {
+        return eventRouter.addListener(listener);
+    }
+
+    @Override
+    public EventRemover addReactiveChangeListener(
+            ReactiveChangeListener listener) {
+        return eventRouter.addReactiveListener(listener);
     }
 }
