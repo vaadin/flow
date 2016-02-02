bd39eb271412b5341bcb2b2ebc9a0db13d0ffa09
Leif Åstrand <leif@vaadin.com>
2016-02-02 15:46:06 +0200

    Make namespace ids shared with the client

---
+++ hummingbird-server/src/main/java/com/vaadin/shared/Namespaces.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.shared;
+
+import com.vaadin.hummingbird.namespace.ElementChildrenNamespace;
+import com.vaadin.hummingbird.namespace.ElementDataNamespace;
+import com.vaadin.hummingbird.namespace.ElementPropertiesNamespace;
+
+/**
+ * Registry of namespace id numbers shared between server and client.
+ *
+ * @since
+ * @author Vaadin Ltd
+ */
+public class Namespaces {
+    /**
+     * Id for {@link ElementDataNamespace}.
+     */
+    public static final int ELEMENT_DATA = 0;
+
+    /**
+     * Id for {@link ElementPropertiesNamespace}.
+     */
+    public static final int ELEMENT_PROPERTIES = 1;
+
+    /**
+     * Id for {@link ElementChildrenNamespace}.
+     */
+    public static final int ELEMENT_CHILDREN = 2;
+
+    private Namespaces() {
+        // Only static
+    }
+}
