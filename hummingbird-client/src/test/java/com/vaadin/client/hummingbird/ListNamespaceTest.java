3f246de891b3f401c1966040413bc9f796bb8426
Leif Åstrand <leif@vaadin.com>
2016-02-02 15:40:34 +0200

    Add event support to the client-side state tree

---
+++ hummingbird-client/src/test/java/com/vaadin/client/hummingbird/ListNamespaceTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.client.hummingbird;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.client.hummingbird.collection.JsArray;
+import com.vaadin.client.hummingbird.reactive.CountingComputation;
+import com.vaadin.client.hummingbird.reactive.Reactive;
+
+import elemental.events.EventRemover;
+
+public class ListNamespaceTest {
+    private ListNamespace namespace = new StateTree().getRootNode()
+            .getListNamespace(0);
+
+    @Test
+    public void testInitialEmpty() {
+        Assert.assertEquals(0, namespace.length());
+    }
+
+    @Test
+    public void testSplice() {
+        namespace.splice(0, 0, "1", "2", "3");
+
+        Assert.assertEquals(3, namespace.length());
+        Assert.assertEquals("1", namespace.get(0));
+        Assert.assertEquals("2", namespace.get(1));
+        Assert.assertEquals("3", namespace.get(2));
+
+        namespace.splice(0, 2);
+
+        Assert.assertEquals(1, namespace.length());
+        Assert.assertEquals("3", namespace.get(0));
+    }
+
+    @Test
+    public void testSpliceEvents() {
+        AtomicReference<ListSpliceEvent> lastEvent = new AtomicReference<>();
+
+        EventRemover remover = namespace
+                .addSpliceListener(new ListSpliceListener() {
+                    @Override
+                    public void onSplice(ListSpliceEvent event) {
+                        Assert.assertNull("Got unexpected event",
+                                lastEvent.get());
+                        lastEvent.set(event);
+                    }
+                });
+
+        namespace.splice(0, 0, "1", "2", "3");
+
+        ListSpliceEvent addEvent = lastEvent.get();
+        Assert.assertSame(namespace, addEvent.getSource());
+        Assert.assertEquals(0, addEvent.getIndex());
+        Assert.assertEquals(0, addEvent.getRemove().length());
+        Assert.assertEquals(Arrays.asList("1", "2", "3"),
+                Arrays.asList(addEvent.getAdd()));
+
+        lastEvent.set(null);
+
+        namespace.splice(1, 2);
+        ListSpliceEvent removeEvent = lastEvent.get();
+        Assert.assertEquals(1, removeEvent.getIndex());
+        Assert.assertEquals(0, removeEvent.getAdd().length);
+
+        JsArray<Object> removed = removeEvent.getRemove();
+        Assert.assertEquals(2, removed.length());
+        Assert.assertEquals("2", removed.get(0));
+        Assert.assertEquals("3", removed.get(1));
+
+        remover.remove();
+
+        namespace.splice(0, 0, "1", "2", "3");
+        Assert.assertSame("No new event should have been fired", removeEvent,
+                lastEvent.get());
+    }
+
+    @Test
+    public void testReactive() {
+        CountingComputation computation = new CountingComputation(
+                () -> namespace.length());
+
+        Reactive.flush();
+
+        namespace.splice(0, 0, "1");
+
+        Assert.assertEquals(1, computation.getCount());
+
+        Reactive.flush();
+        Assert.assertEquals(2, computation.getCount());
+
+        namespace.get(0);
+
+        Reactive.flush();
+        Assert.assertEquals("Get should not trigger recompute", 2,
+                computation.getCount());
+    }
+
+}
--- hummingbird-client/src/test/java/com/vaadin/client/hummingbird/MapPropertyTest.java
+++ hummingbird-client/src/test/java/com/vaadin/client/hummingbird/MapPropertyTest.java
@@ -15,8 +15,15 @@
  */
 package com.vaadin.client.hummingbird;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.vaadin.client.hummingbird.reactive.CountingComputation;
+import com.vaadin.client.hummingbird.reactive.Reactive;
+
+import elemental.events.EventRemover;
 
 public class MapPropertyTest {
     private MapProperty property = new MapProperty("foo",
@@ -29,5 +36,55 @@ public class MapPropertyTest {
         property.setValue("bar");
 
         Assert.assertEquals("bar", property.getValue());
+    }
+
+    @Test
+    public void testChangeListener() {
+        AtomicReference<MapPropertyChangeEvent> lastEvent = new AtomicReference<>();
+
+        EventRemover remover = property
+                .addChangeListener(new MapPropertyChangeListener() {
+                    @Override
+                    public void onPropertyChange(MapPropertyChangeEvent event) {
+                        Assert.assertNull("Got unexpected event",
+                                lastEvent.get());
+                        lastEvent.set(event);
+                    }
+                });
+
+        property.setValue("foo");
+
+        MapPropertyChangeEvent event = lastEvent.get();
+        Assert.assertSame(property, event.getSource());
+        Assert.assertNull(event.getOldValue());
+        Assert.assertEquals("foo", event.getNewValue());
+
+        property.setValue("foo");
+        Assert.assertSame("No new event should have fired", event,
+                lastEvent.get());
+
+        remover.remove();
+
+        property.setValue("bar");
+
+        Assert.assertSame("No new event should have fired", event,
+                lastEvent.get());
+    }
+
+    @Test
+    public void testReactive() {
+        CountingComputation computation = new CountingComputation(
+                () -> property.getValue());
+
+        Reactive.flush();
+
+        property.setValue("bar");
+        property.setValue("baz");
+
+        Assert.assertEquals(1, computation.getCount());
+
+        Reactive.flush();
+
+        Assert.assertEquals(2, computation.getCount());
     }
 }
