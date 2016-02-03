ca9dfc9e5fb0f2a5ade43aee521e28238afaaa95
Artur Signell <artur@vaadin.com>
2016-02-03 13:14:01 +0200

    Run PhantomJS smoke test during build

---
+++ hummingbird-ui-test/src/test/java/com/vaadin/hummingbird/uitest/TestBenchHelpers.java
@@ -0,0 +1,104 @@
+package com.vaadin.hummingbird.uitest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.vaadin.testbench.TestBenchTestCase;
+
+public class TestBenchHelpers extends TestBenchTestCase {
+    /**
+     * Waits up to 10s for the given condition to become true. Use e.g. as
+     * {@link #waitUntil(ExpectedConditions.textToBePresentInElement(by, text))}
+     *
+     * @param condition
+     *            the condition to wait for to become true
+     */
+    protected <T> void waitUntil(ExpectedCondition<T> condition) {
+        waitUntil(condition, 10);
+    }
+
+    /**
+     * Waits the given number of seconds for the given condition to become true.
+     * Use e.g. as
+     * {@link #waitUntil(ExpectedConditions.textToBePresentInElement(by, text))}
+     *
+     * @param condition
+     *            the condition to wait for to become true
+     */
+    protected <T> void waitUntil(ExpectedCondition<T> condition,
+            long timeoutInSeconds) {
+        new WebDriverWait(driver, timeoutInSeconds).until(condition);
+    }
+
+    /**
+     * Waits up to 10s for the given condition to become false. Use e.g. as
+     * {@link #waitUntilNot(ExpectedConditions.textToBePresentInElement(by,
+     * text))}
+     *
+     * @param condition
+     *            the condition to wait for to become false
+     */
+    protected <T> void waitUntilNot(ExpectedCondition<T> condition) {
+        waitUntilNot(condition, 10);
+    }
+
+    /**
+     * Waits the given number of seconds for the given condition to become
+     * false. Use e.g. as
+     * {@link #waitUntilNot(ExpectedConditions.textToBePresentInElement(by,
+     * text))}
+     *
+     * @param condition
+     *            the condition to wait for to become false
+     */
+    protected <T> void waitUntilNot(ExpectedCondition<T> condition,
+            long timeoutInSeconds) {
+        waitUntil(ExpectedConditions.not(condition), timeoutInSeconds);
+    }
+
+    protected void waitForElementPresent(final By by) {
+        waitUntil(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    protected void waitForElementNotPresent(final By by) {
+        waitUntil(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver input) {
+                return input.findElements(by).isEmpty();
+            }
+        });
+    }
+
+    protected void waitForElementVisible(final By by) {
+        waitUntil(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    /**
+     * Checks if the given element has the given class name.
+     *
+     * Matches only full class names, i.e. has ("foo") does not match
+     * class="foobar"
+     *
+     * @param element
+     * @param className
+     * @return
+     */
+    protected boolean hasCssClass(WebElement element, String className) {
+        String classes = element.getAttribute("class");
+        if (classes == null || classes.isEmpty()) {
+            return (className == null || className.isEmpty());
+        }
+
+        for (String cls : classes.split(" ")) {
+            if (className.equals(cls)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
