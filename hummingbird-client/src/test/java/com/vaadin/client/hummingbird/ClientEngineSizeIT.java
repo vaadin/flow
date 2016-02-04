f8ecc2378368eaa34825added31566780060a0d2
Artur Signell <artur@vaadin.com>
2016-02-04 16:30:39 +0200

    Report size of client engine to TeamCity

---
+++ hummingbird-client/src/test/java/com/vaadin/client/hummingbird/ClientEngineSizeIT.java
@@ -0,0 +1,56 @@
+package com.vaadin.client.hummingbird;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class ClientEngineSizeIT {
+
+    @Test
+    public void testClientEngineSize() throws Exception {
+        File compiledModuleFolder = new File(
+                "target/classes/VAADIN/client/com.vaadin.ClientEngine");
+        if (!compiledModuleFolder.exists()) {
+            throw new IOException(
+                    "Folder with compiled client engine does not exist: "
+                            + compiledModuleFolder.getAbsolutePath());
+        }
+
+        boolean cacheJsReported = false;
+        boolean cacheJsGzReported = false;
+
+        for (File f : compiledModuleFolder.listFiles()) {
+            if (f.getName().endsWith(".cache.js")) {
+                if (cacheJsReported) {
+                    throw new IOException(
+                            "Multiple uncompresed cache.js files found!");
+                }
+                printTeamcityStats("clientEngine", f.length());
+                cacheJsReported = true;
+            } else if (f.getName().endsWith(".cache.js.gz")) {
+                if (cacheJsGzReported) {
+                    throw new IOException(
+                            "Multiple compressed cache.js.gz files found!");
+                }
+                printTeamcityStats("clientEngineGzipped", f.length());
+                cacheJsGzReported = true;
+            }
+        }
+        if (!cacheJsReported) {
+            throw new IOException("Uncompressed cache.js file not found!");
+        }
+        if (!cacheJsGzReported) {
+            throw new IOException("Compressed cache.js.gz file not found!");
+        }
+    }
+
+    private void printTeamcityStats(String key, long value) {
+        // ##teamcity[buildStatisticValue key=&#39;&lt;valueTypeKey&gt;&#39;
+        // value=&#39;&lt;value&gt;&#39;]
+        System.out.println("##teamcity[buildStatisticValue key='" + key
+                + "' value='" + value + "']");
+
+    }
+
+}
