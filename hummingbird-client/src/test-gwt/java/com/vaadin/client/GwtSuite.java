9fa26566b6674386f35b063d1bcd4f5518368c3e
Leif Åstrand <leif@vaadin.com>
2016-02-18 12:04:06 +0200

    Don't try to convert JsArray to Java array
    GWT can use a Java array as a JsArray, but the other way around gives
    problems in some situations since a Java array is expected to have type
    information embedded.

--- hummingbird-client/src/test-gwt/java/com/vaadin/client/GwtSuite.java
+++ hummingbird-client/src/test-gwt/java/com/vaadin/client/GwtSuite.java
@@ -1,6 +1,7 @@
 package com.vaadin.client;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
+import com.vaadin.client.hummingbird.GwtTreeChangeProcessorTest;
 import com.vaadin.client.hummingbird.collection.GwtBasicElementBinderTest;
 import com.vaadin.client.hummingbird.collection.GwtJsArrayTest;
 import com.vaadin.client.hummingbird.collection.GwtJsMapTest;
@@ -23,6 +24,7 @@ public class GwtSuite extends GWTTestSuite {
         suite.addTestSuite(GwtJsWeakMapTest.class);
         suite.addTestSuite(GwtJsSetTest.class);
         suite.addTestSuite(GwtBasicElementBinderTest.class);
+        suite.addTestSuite(GwtTreeChangeProcessorTest.class);
         return suite;
     }
 }
