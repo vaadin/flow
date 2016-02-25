7db2c47469684ea1d9f19468865514be1d95af4d
Artur Signell <artur@vaadin.com>
2016-02-25 10:54:33 +0200

    Make merge script project independent
    * Detect GitHub repository from "origin" remote URL
    * Detect target branch from the pull request

--- scripts/mergePr.sh
+++ scripts/mergePr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PR=$1
+GITHUB_REPOSITORY=$(git config --get remote.origin.url|grep github.com:|sed "s/.*github.com://"|grep .git|sed "s/.git.*//")
 
 if [ "$PR" = "" ]
 then
@@ -14,19 +15,27 @@ then
 	exit 2
 fi
 
+if [ "$GITHUB_REPOSITORY" = "" ]
+then
+	echo "Unable to determine GitHub repository from the origin remote URL. Ensure you have cloned the project from GitHub"
+	exit 3
+fi
+
+curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PR > PR_DATA
+target=$(cat PR_DATA|jq .base.ref|cut -d\" -f 2)
+head=$(cat PR_DATA|jq .head.sha|cut -d\" -f 2)
 
 BRANCH=pr-$PR
 git branch -D $BRANCH
-git fetch && git checkout origin/master && git checkout -b $BRANCH
+git fetch && git checkout origin/$target && git checkout -b $BRANCH
 if [ "$?" != "0" ]
 then
 	echo "Failed to create branch $BRANCH"
 	exit 5
 fi
 
-head=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/vaadin/hummingbird/pulls/$PR|jq .head.sha|cut -d\" -f 2)
-commits=$(git log origin/master..$head --reverse --oneline|cut -d' ' -f 1)
-prtitle=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/vaadin/hummingbird/pulls/$PR|jq .title|sed "s/^.//"|sed "s/.$//")
+commits=$(git log origin/$target..$head --reverse --oneline|cut -d' ' -f 1)
+prtitle=$(cat PR_DATA|jq .title|sed "s/^.//"|sed "s/.$//")
 
 echo "$prtitle" > COMMIT_MSG
 echo >> COMMIT_MSG
@@ -49,7 +58,7 @@ do
 		if [ "$?" != "0" ]
 		then
 			echo "Cherry-pick of $commit failed, aborting"
-			exit 3
+			exit 4
 		fi
 	else
 		echo "Skipping merge commit $(git show -s --oneline $commit)"
@@ -64,6 +73,6 @@ git commit --author "$AUTHOR"  -F COMMIT_MSG  --edit
 echo
 echo "If everything is ok, do"
 echo
-echo "git push origin $BRANCH:master"
+echo "git push origin $BRANCH:$target"
 
-rm -f COMMIT_MSG
+rm -f COMMIT_MSG PR_DATA
