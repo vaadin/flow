ae0397629a1fd1547e3f78bf8d47540625a23d53
Leif Åstrand <leif@vaadin.com>
2016-03-04 14:14:01 +0200

    Add base href to simplify dependency handling with e.g. pushState
    Closes #272

---
+++ hummingbird-ui-test/src/test/java/com/vaadin/hummingbird/uitest/ui/BaseHrefIT.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.uitest.ui;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import com.vaadin.hummingbird.testutil.PhantomJSTest;
+
+public class BaseHrefIT extends PhantomJSTest {
+
+    @Test
+    public void testBaseHref() throws URISyntaxException {
+        URI baseUri = new URI(getTestURL(getUIClass()));
+        String uiUrl = baseUri.toString();
+        String expectedUrl = baseUri.resolve("./link").toString();
+
+        getDriver().get(uiUrl);
+        Assert.assertEquals(expectedUrl, getLinkHref());
+
+        getDriver().get(uiUrl + "/foo/bar/baz");
+        Assert.assertEquals(expectedUrl, getLinkHref());
+    }
+
+    private String getLinkHref() {
+        return findElement(By.tagName("a")).getAttribute("href");
+    }
+
+}
