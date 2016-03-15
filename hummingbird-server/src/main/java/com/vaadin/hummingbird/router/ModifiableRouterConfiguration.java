13bff1b2745d43a207a3de738a6198a8875a8477
Leif Åstrand <leif@vaadin.com>
2016-03-15 15:11:30 +0200

    Implement setRoute

--- hummingbird-server/src/main/java/com/vaadin/hummingbird/router/ModifiableRouterConfiguration.java
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/router/ModifiableRouterConfiguration.java
@@ -16,6 +16,9 @@
 package com.vaadin.hummingbird.router;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * A {@link Router} configuration object that may be in a modifiable state.
@@ -31,6 +34,17 @@ public class ModifiableRouterConfiguration
         implements Serializable, RouterConfiguration {
     private final boolean modifiable;
 
+    /**
+     * The root of the tree structure configured through
+     * {@link #setRoute(String, NavigationHandler)}.
+     * <p>
+     * We should never give out references to this instance since it doesn't
+     * have the any immutability checks.
+     */
+    private final RouteTreeNode routeTreeRoot;
+
+    private final HashMap<Class<? extends View>, Class<? extends HasChildView>> parentViewTypes;
+
     private Resolver resolver;
 
     /**
@@ -38,6 +52,8 @@ public class ModifiableRouterConfiguration
      */
     public ModifiableRouterConfiguration() {
         resolver = e -> null;
+        routeTreeRoot = new RouteTreeNode();
+        parentViewTypes = new HashMap<>();
         modifiable = false;
     }
 
@@ -56,12 +72,19 @@ public class ModifiableRouterConfiguration
         assert original != null;
 
         resolver = original.resolver;
+
+        routeTreeRoot = new RouteTreeNode(original.routeTreeRoot);
+
+        parentViewTypes = new HashMap<>(original.parentViewTypes);
+
         this.modifiable = modifiable;
     }
 
     /**
      * Sets the resolver to use for resolving what to show for a given
-     * navigation event.
+     * navigation event. If the resolver doesn't provide a result, the routes
+     * configured using the various <code>setRoute</code> methods will be
+     * considered instead.
      *
      * @param resolver
      *            the resolver, not <code>null</code>
@@ -95,4 +118,316 @@ public class ModifiableRouterConfiguration
         return modifiable;
     }
 
+    /**
+     * Resolves a route based on what has been configured using the various
+     * <code>setRoute</code> methods.
+     *
+     * @param event
+     *            the event for which to resolve a route
+     * @return a navigation handler or handling the route, or <code>null</code>
+     *         if no configured route matched the location
+     */
+    public NavigationHandler resolveRoute(NavigationEvent event) {
+        assert event != null;
+
+        // Start the recursion
+        return resolveRoute(routeTreeRoot, event.getLocation(), event);
+    }
+
+    private static NavigationHandler resolveRoute(RouteTreeNode node,
+            Location location, NavigationEvent event) {
+        String segment = location.getFirstSegment();
+        Location subLocation = location.getSubLocation();
+
+        NavigationHandler handler = null;
+
+        if (subLocation.hasSegments()) {
+            // Try to use a child node if there are more path segments
+            RouteTreeNode childNode = node.resolveChild(segment);
+
+            if (childNode != null) {
+                handler = resolveRoute(childNode, subLocation, event);
+            }
+        } else {
+            // Find an actual handler if this is the last path segment
+            handler = node.resolveRoute(segment);
+        }
+
+        if (handler == null) {
+            // Use a wildcard handler if we haven't found anything else
+            handler = node.getWildcardHandler();
+        }
+
+        return handler;
+    }
+
+    /**
+     * Set the view type and immediate parent view type to use for the given
+     * path. The parent view type will be (recursively) nested in further parent
+     * view types as set by {@link #setParentView(Class, Class)}.
+     * <p>
+     * See {@link #setRoute(String, NavigationHandler)} for a description of the
+     * supported path formats.
+     *
+     * @param path
+     *            the path for which the view type should be used, not
+     *            <code>null</code>
+     * @param viewType
+     *            the view type to use for the path, not <code>null</code>
+     */
+    public void setRoute(String path, Class<? extends View> viewType,
+            Class<? extends HasChildView> parentViewType) {
+        assert path != null;
+        assert viewType != null;
+
+        setRoute(path, new NavigationHandler() {
+            @Override
+            public void handle(NavigationEvent event) {
+                List<Class<? extends HasChildView>> parentViews = findParentViews(
+                        parentViewType);
+                parentViews.add(0, parentViewType);
+                new ViewRenderer(viewType, parentViews).handle(event);
+            }
+        });
+    }
+
+    /**
+     * Set the view type to use for the given path. The view type will be
+     * (recursively) nested in parent view types as set by
+     * {@link #setRoute(String, Class)}.
+     * <p>
+     * See {@link #setRoute(String, NavigationHandler)} for a description of the
+     * supported path formats.
+     *
+     * @param path
+     *            the path for which the view type should be used, not
+     *            <code>null</code>
+     * @param viewType
+     *            the view type to use for the path, not <code>null</code>
+     */
+    public void setRoute(String path, Class<? extends View> viewType) {
+        assert path != null;
+        assert viewType != null;
+
+        setRoute(path, new NavigationHandler() {
+            @Override
+            public void handle(NavigationEvent event) {
+                List<Class<? extends HasChildView>> parentViews = findParentViews(
+                        viewType);
+                new ViewRenderer(viewType, parentViews).handle(event);
+            }
+        });
+    }
+
+    /**
+     * Sets the parent view type that should be used together with the view
+     * types configured using {@link #setRoute(String, Class)} or
+     * {@link #setRoute(String, Class, Class)} .
+     *
+     * @param viewType
+     *            the type of the view for which to set a parent, not
+     *            <code>null</code>
+     * @param parentView
+     *            the parent view type to use, or <code>null</code> to remove a
+     *            mapping
+     */
+    public void setParentView(Class<? extends View> viewType,
+            Class<? extends HasChildView> parentView) {
+        assert viewType != null;
+
+        if (parentView == null) {
+            parentViewTypes.remove(viewType);
+        } else if (!parentViewTypes.containsKey(viewType)) {
+            List<Class<? extends HasChildView>> parentViews = findParentViews(
+                    parentView);
+            if (parentViews.contains(viewType)) {
+                throw new IllegalStateException(
+                        "Setting " + parentView + " as a parent of " + viewType
+                                + " would create a loop");
+            }
+
+            parentViewTypes.put(viewType, parentView);
+        } else {
+            throw new IllegalStateException(
+                    "There is alerady a parent view configured for "
+                            + viewType);
+        }
+    }
+
+    /**
+     * Gets the parent type configured for the given view type using
+     * {@link #setParentView(Class, Class)}.
+     *
+     * @param viewType
+     *            the view type for which to find a parent, not
+     *            <code>null</code>
+     * @return the parent view type, or <code>null</code> if no parent view has
+     *         been set
+     */
+    public Class<? extends HasChildView> getParentView(
+            Class<? extends View> viewType) {
+        assert viewType != null;
+
+        return parentViewTypes.get(viewType);
+    }
+
+    private List<Class<? extends HasChildView>> findParentViews(
+            Class<? extends View> viewType) {
+        ArrayList<Class<? extends HasChildView>> parentViews = new ArrayList<>();
+        while (true) {
+            Class<? extends HasChildView> parentType = parentViewTypes
+                    .get(viewType);
+            if (parentType == null) {
+                break;
+            }
+            parentViews.add(parentType);
+            viewType = parentType;
+        }
+        return parentViews;
+    }
+
+    /**
+     * Sets the navigation handler to use for the given path.
+     * <p>
+     * The path is made up of segments separated by <code>/</code>. A segment
+     * name enclosed in <code>{</code> and <code>}</code> is interpreted as a
+     * placeholder segment. If no exact match is found when resolving a URL but
+     * a placeholder is defined, then that placeholder will be used and the
+     * actual path segment name will be available in the navigation event. The
+     * last segment in a path can also be a wildcard, defined as <code>*</code>.
+     * A wildcard route accepts any path segment names for the wildcard part,
+     * but it will only be used if no route can be found using exact names or
+     * placeholders.
+     *
+     * @param path
+     *            the path to use for the route
+     * @param navigationHandler
+     *            the navigation handler to use for the route
+     */
+    public void setRoute(String path, NavigationHandler navigationHandler) {
+        assert path != null;
+        assert navigationHandler != null;
+        assert !path.startsWith("/");
+        assert !path.contains("://");
+
+        // Start the recursion
+        setRoute(new Location(path), routeTreeRoot, navigationHandler);
+    }
+
+    private void setRoute(Location location, RouteTreeNode node,
+            NavigationHandler navigationHandler) {
+        throwIfImmutable();
+
+        String segment = location.getFirstSegment();
+        Location subLocation = location.getSubLocation();
+
+        if (isWildcardSegment(segment)) {
+            if (subLocation.hasSegments()) {
+                throw new IllegalArgumentException(
+                        "Wildcard should be last segment");
+            }
+            node.setWildcardHandler(navigationHandler);
+            return;
+        }
+
+        if (isPlaceholderSegment(segment)) {
+            // A future patch that makes the placeholder values available in the
+            // event should record the used name here.
+            segment = "{}";
+        }
+
+        if (subLocation.hasSegments()) {
+            // Configure the rest of the location in a child node
+            RouteTreeNode childNode = node.getOrCreateChild(segment);
+
+            setRoute(subLocation, childNode, navigationHandler);
+        } else {
+            // Record the navigation handler for this final part of the location
+            node.setRoute(segment, navigationHandler);
+        }
+    }
+
+    /**
+     * Helper for checking whether a segment is a wildcard segment. Also checks
+     * for illegal use of the wildcard segment identifier if the segment is not
+     * a wildcard.
+     *
+     * @param segment
+     *            the segment to check, not <code>null</code>
+     * @return <code>true</code> if the segment is a wildcard segment,
+     *         <code>false</code> otherwise.
+     */
+    private static boolean isWildcardSegment(String segment) {
+        assert segment != null;
+
+        boolean isWildcard = "*".equals(segment);
+        if (!isWildcard && segment.contains("*")) {
+            throw new IllegalArgumentException("* is only valid as \"/*\"");
+
+        }
+        return isWildcard;
+    }
+
+    /**
+     * Helper for checking whether a segment is a placeholder segment. Also
+     * checks for illegal use of the placeholder segment identifiers if the
+     * segment is not a placeholder.
+     *
+     * @param segment
+     *            the segment to check, not <code>null</code>
+     * @return <code>true</code> if the segment is a placeholder segment,
+     *         <code>false</code> otherwise.
+     */
+    private static boolean isPlaceholderSegment(String segment) {
+        boolean isPlaceholder = segment.startsWith("{")
+                && segment.endsWith("}");
+        if (!isPlaceholder
+                && (segment.contains("{") || segment.contains("}"))) {
+            throw new IllegalArgumentException(
+                    "{ and } are only allowed in the start and end of a segment");
+        }
+        return isPlaceholder;
+    }
+
+    /**
+     * Removes the route described by the given path. See
+     * {@link #setRoute(String, NavigationHandler)} for a description of the
+     * supported path formats.
+     *
+     * @param path
+     *            the path of the route to remove, not <code>null</code>
+     */
+    public void removeRoute(String path) {
+        assert path != null;
+
+        // Start the recursion
+        removeRoute(new Location(path), routeTreeRoot);
+    }
+
+    private void removeRoute(Location location, RouteTreeNode node) {
+        throwIfImmutable();
+
+        String segment = location.getFirstSegment();
+        Location subLocation = location.getSubLocation();
+
+        if (isWildcardSegment(segment)) {
+            node.setWildcardHandler(null);
+            return;
+        }
+
+        if (isPlaceholderSegment(segment)) {
+            segment = "{}";
+        }
+
+        if (!subLocation.hasSegments()) {
+            node.setRoute(segment, null);
+        } else if (node.hasChild(segment)) {
+            RouteTreeNode childNode = node.getOrCreateChild(segment);
+
+            removeRoute(subLocation, childNode);
+            if (childNode.isEmpty()) {
+                node.removeChild(segment);
+            }
+        }
+    }
 }
---
+++ hummingbird-server/src/test/java/com/vaadin/hummingbird/router/ModifiableRouterConfigurationTest.java
@@ -0,0 +1,290 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.router;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.hummingbird.router.ViewRendererTest.AnotherParentView;
+import com.vaadin.hummingbird.router.ViewRendererTest.ParentView;
+import com.vaadin.hummingbird.router.ViewRendererTest.TestView;
+
+public class ModifiableRouterConfigurationTest {
+    @Test
+    public void testExactRoute() {
+        assertMatches("", "");
+        assertNotMatches("a", "");
+
+        assertMatches("foo", "foo");
+        assertMatches("foo/", "foo/");
+
+        assertNotMatches("foo/", "foo");
+        assertNotMatches("foo", "foo/");
+
+        assertNotMatches("foo/bar", "foo/");
+        assertNotMatches("foo/", "foo/bar");
+
+        assertNotMatches("foo/bar", "foo/baz");
+        assertNotMatches("foo/bar", "baz/bar");
+
+        assertMatches("foo/bar/baz", "foo/bar/baz");
+    }
+
+    @Test
+    public void testPlaceholderRoute() {
+        assertMatches("foo", "{name}");
+        assertMatches("foo/", "{name}/");
+
+        assertNotMatches("foo", "{name}/");
+        assertNotMatches("foo/", "{name}");
+
+        assertMatches("foo/bar/baz", "{name}/bar/baz");
+        assertMatches("foo/bar/baz", "foo/{name}/baz");
+        assertMatches("foo/bar/baz", "foo/bar/{name}");
+
+        assertNotMatches("foo/bar/baz", "{name}/asdf/baz");
+        assertNotMatches("foo/bar/baz", "{name}/bar/asdf");
+        assertNotMatches("foo/bar/baz", "asdf/{name}/baz");
+        assertNotMatches("foo/bar/baz", "foo/{name}/asdf");
+        assertNotMatches("foo/bar/baz", "asdf/bar/{name}");
+        assertNotMatches("foo/bar/baz", "foo/asdf/{name}");
+
+        assertMatches("foo/bar/baz", "{name1}/{name2}/{name3}");
+    }
+
+    @Test
+    public void testWildcardRoute() {
+        assertMatches("", "*");
+        assertMatches("foo", "*");
+        assertMatches("foo/", "*");
+        assertMatches("foo/bar", "*");
+
+        assertMatches("foo/", "foo/*");
+        assertMatches("foo/bar", "foo/*");
+        assertMatches("foo/bar/", "foo/*");
+
+        assertNotMatches("foo", "foo/*");
+        assertNotMatches("bar/foo", "foo/*");
+    }
+
+    @Test
+    public void testRoutePriorityOrder() {
+        assertRoutePriorityOrder("foo", "{name}", "*");
+
+        assertRoutePriorityOrder("foo/bar", "foo/{name}", "{name}/bar",
+                "{name}/{name2}");
+
+        assertRoutePriorityOrder("foo/bar", "foo/*", "*");
+    }
+
+    @Test
+    public void testRoutesCopied() {
+        ModifiableRouterConfiguration original = createConfiguration();
+        original.setRoute("foo/bar", createNoopHandler());
+
+        ModifiableRouterConfiguration copy = new ModifiableRouterConfiguration(
+                original, false);
+
+        original.removeRoute("foo/bar");
+
+        Assert.assertNotNull("Updating the original should not affect the copy",
+                copy.resolveRoute(createEvent("foo/bar")));
+    }
+
+    @Test
+    public void testRemoveRoutes() {
+        ModifiableRouterConfiguration configuration = createConfiguration();
+
+        NavigationHandler navigationHandler = createNoopHandler();
+
+        configuration.setRoute("foo", navigationHandler);
+        configuration.setRoute("{name}", navigationHandler);
+        configuration.setRoute("*", navigationHandler);
+
+        configuration.removeRoute("foo");
+        Assert.assertNotNull(configuration.resolveRoute(createEvent("foo")));
+        configuration.removeRoute("{otherName}");
+        Assert.assertNotNull(configuration.resolveRoute(createEvent("foo")));
+        configuration.removeRoute("*");
+
+        // Should resolve to null only after removing all the routes
+        Assert.assertNull(configuration.resolveRoute(createEvent("foo")));
+    }
+
+    private static NavigationHandler createNoopHandler() {
+        return e -> {
+        };
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testSetExistingRouteThrows() {
+        ModifiableRouterConfiguration configuration = createConfiguration();
+        configuration.setRoute("foo", createNoopHandler());
+        configuration.setRoute("foo", createNoopHandler());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testSetExistingPlaceholderThrows() {
+        ModifiableRouterConfiguration configuration = createConfiguration();
+        configuration.setRoute("{name}", createNoopHandler());
+        configuration.setRoute("{anotherName}", createNoopHandler());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testSetExistingWildcardThrows() {
+        ModifiableRouterConfiguration configuration = createConfiguration();
+        configuration.setRoute("foo/*", createNoopHandler());
+        configuration.setRoute("foo/*", createNoopHandler());
+    }
+
+    @Test
+    public void testParentViewsWithoutParent() {
+        RouterUI ui = new RouterUI();
+
+        Router router = new Router();
+
+        router.reconfigure(conf -> {
+            conf.setRoute("route", TestView.class);
+            conf.setParentView(TestView.class, ParentView.class);
+            conf.setParentView(ParentView.class, AnotherParentView.class);
+        });
+
+        router.navigate(ui, new Location("route"));
+
+        Assert.assertEquals(Arrays.asList(TestView.class, ParentView.class,
+                AnotherParentView.class), getViewChainTypes(ui));
+    }
+
+    @Test
+    public void testParentViewsWithParent() {
+        RouterUI ui = new RouterUI();
+
+        Router router = new Router();
+
+        router.reconfigure(conf -> {
+            conf.setRoute("route", TestView.class, ParentView.class);
+            conf.setParentView(ParentView.class, AnotherParentView.class);
+        });
+
+        router.navigate(ui, new Location("route"));
+
+        Assert.assertEquals(Arrays.asList(TestView.class, ParentView.class,
+                AnotherParentView.class), getViewChainTypes(ui));
+    }
+
+    private List<Class<? extends View>> getViewChainTypes(RouterUI ui) {
+        return ui.getViewChain().stream().map(v -> v.getClass())
+                .collect(Collectors.toList());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testReplaceParentViewThrows() {
+        ModifiableRouterConfiguration configuration = createConfiguration();
+
+        configuration.setParentView(TestView.class, ParentView.class);
+        configuration.setParentView(TestView.class, ParentView.class);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testParentViewLoopDetection() {
+        ModifiableRouterConfiguration configuration = createConfiguration();
+
+        configuration.setParentView(AnotherParentView.class, ParentView.class);
+        configuration.setParentView(ParentView.class, AnotherParentView.class);
+    }
+
+    private static void assertRoutePriorityOrder(String... routes) {
+        // First param also serves as the base location
+        String location = routes[0];
+        for (int i = 0; i < routes.length - 1; i++) {
+            String stronger = routes[i];
+            String weaker = routes[i + 1];
+
+            assertRoutePriority(location, stronger, weaker);
+        }
+    }
+
+    private static void assertRoutePriority(String location,
+            String strongerRoute, String weakerRoute) {
+        // First verify that the test makes sense
+        assertMatches(location, strongerRoute);
+        assertMatches(location, weakerRoute);
+
+        NavigationHandler weakerHandler = createNoopHandler();
+        NavigationHandler strongerHandler = createNoopHandler();
+
+        ModifiableRouterConfiguration configuration = createConfiguration();
+
+        configuration.setRoute(strongerRoute, strongerHandler);
+        configuration.setRoute(weakerRoute, weakerHandler);
+
+        NavigationHandler handler = configuration
+                .resolveRoute(createEvent(location));
+        Assert.assertSame(handler, strongerHandler);
+
+        // Do the same again with setRoute run in the opposite order
+        configuration = createConfiguration();
+
+        configuration.setRoute(weakerRoute, weakerHandler);
+        configuration.setRoute(strongerRoute, strongerHandler);
+
+        handler = configuration.resolveRoute(createEvent(location));
+        Assert.assertSame(handler, strongerHandler);
+    }
+
+    private static void assertMatches(String location, String route) {
+        Assert.assertTrue(
+                "The route " + route + " should match the location " + location,
+                routeMatches(location, route));
+    }
+
+    private static void assertNotMatches(String location, String route) {
+        Assert.assertFalse("The route " + route
+                + " shouldn't match the location " + location,
+                routeMatches(location, route));
+    }
+
+    private static boolean routeMatches(String location, String route) {
+        ModifiableRouterConfiguration configuration = createConfiguration();
+        configuration.setRoute(route, createNoopHandler());
+
+        NavigationHandler resolveRoute = configuration
+                .resolveRoute(createEvent(location));
+        boolean condition = resolveRoute != null;
+        return condition;
+    }
+
+    private static ModifiableRouterConfiguration createConfiguration() {
+        // Create a modifiable copy of a an empty configuration
+        return new ModifiableRouterConfiguration(
+                new ModifiableRouterConfiguration(), true);
+    }
+
+    private static NavigationEvent createEvent(String location) {
+        Assert.assertFalse(location.contains(".."));
+        Assert.assertFalse(location.contains("*"));
+        Assert.assertFalse(location.contains("{"));
+        Assert.assertFalse(location.contains("}"));
+
+        return new NavigationEvent(new Router(), new Location(location),
+                new RouterUI());
+    }
+
+}
