4dc08f3c03b174673e04670a9a06ac1ad0404638
Pekka Hyvönen <pekka@vaadin.com>
2016-03-16 12:07:03 +0200

    Added a module with tutorial code
    For now, needs to be synchronized by hand when modified.

---
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/EventListener.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.humminbird.tutorial;
+
+import com.vaadin.hummingbird.dom.Element;
+import com.vaadin.ui.UI;
+
+import elemental.json.JsonObject;
+
+/**
+ * Tutorial code related to tutorial-event-listener.asciidoc.
+ */
+public abstract class EventListener extends UI {
+
+    void tutorialCode() {
+        Element helloButton = new Element("button").setTextContent("Say hello");
+        helloButton.addEventListener("click", e -> {
+            Element response = new Element("div").setTextContent("Hello!");
+            getElement().appendChild(response);
+        });
+        getElement().appendChild(helloButton);
+
+        helloButton.addEventListener("click", e -> {
+            JsonObject eventData = e.getEventData();
+            boolean shiftKey = eventData.getBoolean("event.shiftKey");
+            double width = eventData.getNumber("element.offsetWidth");
+
+            String text = "Shift " + (shiftKey ? "down" : "up");
+            text += " on button whose width is " + width + "px";
+
+            Element response = new Element("div");
+            response.setTextContent(text);
+            getElement().appendChild(response);
+        }, "event.shiftKey", "element.offsetWidth");
+    }
+}
---
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/PropertiesAttributes.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.humminbird.tutorial;
+
+import com.vaadin.hummingbird.dom.Element;
+
+/**
+ * Tutorial code related to tutorial-properties-attributes.asciidoc.
+ */
+public class PropertiesAttributes {
+
+    void tutorialCode() {
+        Element nameField = new Element("input");
+        nameField.setAttribute("id", "nameField");
+        nameField.setAttribute("placeholder", "John Doe");
+        nameField.setAttribute("autofocus", "");
+
+        // "John Doe"
+        nameField.getAttribute("placeholder");
+
+        // true
+        nameField.hasAttribute("autofocus");
+
+        nameField.removeAttribute("autofocus");
+
+        // ["id", "placeholder"]
+        nameField.getAttributeNames().toArray();
+
+        Element element = new Element("input");
+        element.setProperty("value", "Hello");
+
+        element.getClassList().add("error");
+        element.getClassList().add("critical");
+        element.getClassList().remove("primary");
+
+        element.getProperty("className"); // will return "error critical".
+
+        element.getStyle().set("color", "red");
+        // Note the camelCase, not dash - separated
+        element.getStyle().set("fontWeight", "bold");
+
+        // Note the camelCase, not dash - separated
+        element.getStyle().remove("backgroundColor");
+
+        element.getStyle().has("cursor");
+
+        element = new Element("div");
+        element.setTextContent("Hello world");  // <div>Hello world</div>
+
+        element.appendChild(new Element("span")); // <div>Hello world<span></span></div>
+
+        element.setTextContent("Replacement text"); // <div>Replacement text</div>
+
+        element.setTextContent("Welcome back ");
+
+        Element nameSpan = new Element("b").setTextContent("Rudolph Reindeer");
+        element.appendChild(nameSpan);
+
+        element.getTextContent(); // will return "Welcome back Rudolph Reindeer"
+    }
+}
