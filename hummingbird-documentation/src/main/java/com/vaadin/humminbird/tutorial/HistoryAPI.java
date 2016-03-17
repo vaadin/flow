bc35a020734693218b48d8243bebc982d4d7ddd6
Pekka Hyvönen <pekka@vaadin.com>
2016-03-17 13:45:43 +0200

    Tutorial for History API

---
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/HistoryAPI.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.humminbird.tutorial;
+
+import com.vaadin.ui.History;
+import com.vaadin.ui.History.HistoryStateChangeEvent;
+import com.vaadin.ui.UI;
+
+import elemental.json.Json;
+import elemental.json.JsonValue;
+
+/**
+ * Tutorial code related to tutorial-history-api.asciidoc.
+ */
+public class HistoryAPI {
+
+    void tutorialCode() {
+        History history = UI.getCurrent().getPage().getHistory();
+
+        history.back(); // navigates back to previous page
+
+        history.forward(); // navigates forward to the next page
+
+        history.go(-2); // navigates back two pages
+        history.go(1); // equal to forward()
+        history.go(0); // will reload the current page
+
+        history.setHistoryStateChangeHandler(this::onHistoryStateChange);
+
+        // adds a new history entry for location "home", no state
+        history.pushState(null, "home");
+
+        // replaces the current entry with location "document" and a state object
+        JsonValue state = Json.create("preview-mode");
+        history.replaceState(state, "document");
+    }
+
+    private void onHistoryStateChange(HistoryStateChangeEvent event) {
+        // site base url is www.abc.com/
+        // user navigates back from abc.com/dashboard to abc.com/home
+        event.getLocation(); // returns "home"
+
+        // returns a optional json state object set for "home" page previously
+        event.getState();
+    }
+
+}
