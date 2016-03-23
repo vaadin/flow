a105c1ff4ff5b1434c94a5498d62a2e02b190d4e
Denis Anisimov <denis@vaadin.com>
2016-03-23 10:22:32 +0200

    Review based fixes.

--- hummingbird-server/src/main/java/com/vaadin/server/communication/StreamResourceRequestHandler.java
+++ hummingbird-server/src/main/java/com/vaadin/server/communication/StreamResourceRequestHandler.java
@@ -116,15 +116,21 @@ public class StreamResourceRequestHandler implements RequestHandler {
 =======
         InputStream stream = null;
 
-        StringBuilder pathInfo = new StringBuilder(request.getPathInfo());
-        if (pathInfo.charAt(0) == '/') {
-            pathInfo.delete(0, 1);
+        if (request.getPathInfo() == null) {
+            return false;
         }
+        StringBuilder pathInfo = new StringBuilder(request.getPathInfo());
+        // remove leading '/'
+        pathInfo.delete(0, 1);
+
         boolean requiresLock;
         session.lock();
         try {
             VaadinServletRequest servletRequest = (VaadinServletRequest) request;
-            pathInfo.append('?').append(servletRequest.getQueryString());
+            String queryString = servletRequest.getQueryString();
+            if (queryString != null && !queryString.isEmpty()) {
+                pathInfo.append('?').append(servletRequest.getQueryString());
+            }
             StreamResource resource = session.getResource(pathInfo.toString());
             if (resource == null) {
                 return false;
