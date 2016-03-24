3027809a81db91510817ef5aec6f56bd35688bcf
Denis Anisimov <denis@vaadin.com>
2016-03-24 14:21:21 +0200

    Corrections.

--- hummingbird-server/src/main/java/com/vaadin/hummingbird/dom/Element.java
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/dom/Element.java
@@ -1251,7 +1251,7 @@ public class Element implements Serializable {
      * 
      * @param child
      *            the child node
-     * @return index of the {@code child}
+     * @return index of the {@code child} or -1 if it's not a child
      */
     public int indexOfChild(Element child) {
         if (!equals(child.getParent())) {
