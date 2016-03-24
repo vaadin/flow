01280d07e0a1e0ff2a3cea241505f378c450eea3
Denis Anisimov <denis@vaadin.com>
2016-03-24 16:56:23 +0200

    Added forgotten files.

---
+++ hummingbird-server/src/main/java/com/vaadin/server/FutureAccess.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.server;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.vaadin.util.CurrentInstance;
+
+/**
+ * Encapsulates a {@link Command} submitted using
+ * {@link VaadinSession#access(Command)}. This class is used internally by
+ * the framework and is not intended to be directly used by application
+ * developers.
+ *
+ * @since 7.1
+ * @author Vaadin Ltd
+ */
+public class FutureAccess extends FutureTask<Void> {
+    /**
+     * Snapshot of all non-inheritable current instances at the time this
+     * object was created.
+     */
+    private final Map<Class<?>, CurrentInstance> instances = CurrentInstance
+            .getInstances(true);
+    private final VaadinSession session;
+    private Command command;
+
+    /**
+     * Creates an instance for the given command.
+     *
+     * @param session
+     *            the session to which the task belongs
+     * @param command
+     *            the command to run when this task is purged from the queue
+     */
+    public FutureAccess(VaadinSession session, Command command) {
+        super(() -> command.execute(), null);
+        this.session = session;
+        this.command = command;
+    }
+
+    @Override
+    public Void get() throws InterruptedException, ExecutionException {
+        /*
+         * Help the developer avoid programming patterns that cause
+         * deadlocks unless implemented very carefully. get(long, TimeUnit)
+         * does not have the same detection since a sensible timeout should
+         * avoid completely locking up the application.
+         *
+         * Even though no deadlock could occur after the command has been
+         * run, the check is always done as the deterministic behavior makes
+         * it easier to detect potential problems.
+         */
+        VaadinService.verifyNoOtherSessionLocked(session);
+        return super.get();
+    }
+
+    /**
+     * Gets the current instance values that should be used when running
+     * this task.
+     *
+     * @see CurrentInstance#restoreInstances(Map)
+     *
+     * @return a map of current instances.
+     */
+    public Map<Class<?>, CurrentInstance> getCurrentInstances() {
+        return instances;
+    }
+
+    /**
+     * Handles exceptions thrown during the execution of this task.
+     *
+     * @since 7.1.8
+     * @param exception
+     *            the thrown exception.
+     */
+    public void handleError(Exception exception) {
+        try {
+            if (command instanceof ErrorHandlingCommand) {
+                ErrorHandlingCommand errorHandlingCommand = (ErrorHandlingCommand) command;
+
+                errorHandlingCommand.handleError(exception);
+            } else {
+                ErrorEvent errorEvent = new ErrorEvent(exception);
+
+                ErrorHandler errorHandler = ErrorEvent
+                        .findErrorHandler(session);
+
+                if (errorHandler == null) {
+                    errorHandler = new DefaultErrorHandler();
+                }
+
+                errorHandler.error(errorEvent);
+            }
+        } catch (Exception e) {
+            Logger.getLogger(FutureAccess.class.getName()).log(Level.SEVERE,
+                    e.getMessage(), e);
+        }
+    }
+}
