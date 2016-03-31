c3649d6b622078fc059f01d7dd10a89446ed9480
Leif Åstrand <leif@vaadin.com>
2016-03-31 14:56:22 +0300

    Implement basic template AST nodes and builders

---
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/template/TemplateBinding.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.template;
+
+import java.io.Serializable;
+
+import com.vaadin.hummingbird.StateNode;
+
+import elemental.json.JsonValue;
+
+/**
+ * Representation of a static or dynamic value derived from a template. A
+ * binding can be used e.g. as the value of an element attribute or as the text
+ * in a text node.
+ *
+ * @since
+ * @author Vaadin Ltd
+ */
+public interface TemplateBinding extends Serializable {
+    /**
+     * Produces a value for the given state node.
+     *
+     * @param node
+     *            the state node for which to produce a value, not
+     *            <code>null</code>
+     * @return the binding value
+     */
+    Object getValue(StateNode node);
+
+    /**
+     * Produces a string value for the given state node.
+     *
+     * @param node
+     *            the state node for which to produce a value, not
+     *            <code>null</code>
+     * @param defaultValue
+     *            the default value to use if the produced value is
+     *            <code>null</code>
+     * @return the binding value
+     */
+    default String getValue(StateNode node, String defaultValue) {
+        Object value = getValue(node);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return value.toString();
+        }
+    }
+
+    /**
+     * Encodes this binding as JSON.
+     *
+     * @return the encoded JSONvalue
+     */
+    JsonValue toJson();
+}
---
+++ hummingbird-server/src/test/java/com/vaadin/hummingbird/template/TemplateBuilderTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.template;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TemplateBuilderTest {
+    @Test
+    public void testBasicTemplate() {
+        // <div foo="bar">baz</div>
+        ElementTemplateBuilder builder = new ElementTemplateBuilder("div")
+                .addAttribute("foo", new StaticBinding("bar"))
+                .addChild(new TextTemplateBuilder(new StaticBinding("baz")));
+
+        ElementTemplateNode node = builder.build();
+
+        Assert.assertNull(node.getParent());
+        Assert.assertEquals("div", node.getTag());
+
+        Assert.assertArrayEquals(new String[] { "foo" },
+                node.getAttributeNames().toArray());
+        Assert.assertEquals("bar",
+                node.getAttributeBinding("foo").get().getValue(null));
+
+        Assert.assertEquals(1, node.getChildCount());
+
+        TextTemplateNode child = (TextTemplateNode) node.getChild(0);
+        Assert.assertSame(node, child.getParent());
+        Assert.assertEquals("baz", child.getTextBinding().getValue(null));
+        Assert.assertEquals(0, child.getChildCount());
+    }
+}
