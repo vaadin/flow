31ce65087a9da1289e4cf892aae523cfbe31cd9c
Artur Signell <artur@vaadin.com>
2016-03-31 22:14:01 +0300

    Html component

---
+++ hummingbird-server/src/test/java/com/vaadin/ui/HTMLTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.ui;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.hummingbird.dom.Element;
+
+public class HTMLTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullHtml() {
+        new Html((String) null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullStream() {
+        new Html((InputStream) null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void emptyHtml() {
+        new Html("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void twoRoots() {
+        new Html("<b></b><div></div>");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void text() {
+        new Html("hello");
+    }
+
+    @Test
+    public void simpleHtml() {
+        Html html = new Html("<span>hello</span>");
+        Assert.assertEquals("span", html.getElement().getTag());
+        Assert.assertEquals("hello", html.getInnerHtml());
+    }
+
+    @Test
+    public void rootAttributes() {
+        Html html = new Html("<span foo='bar'>hello</span>");
+        Assert.assertEquals("span", html.getElement().getTag());
+        Assert.assertEquals(1, html.getElement().getAttributeNames().count());
+        Assert.assertEquals("bar", html.getElement().getAttribute("foo"));
+        Assert.assertEquals("hello", html.getInnerHtml());
+    }
+
+    @Test
+    public void rootSpecialAttributes() {
+        Html html = new Html(
+                "<span class='foo' style='color: red'>hello</span>");
+        Element element = html.getElement();
+        Assert.assertEquals("span", element.getTag());
+
+        // Styles are ignored until Element.setAttribute("style") works
+        Assert.assertEquals(1, element.getAttributeNames().count());
+        Assert.assertEquals("foo", element.getAttribute("class"));
+        Assert.assertEquals("hello", html.getInnerHtml());
+    }
+
+    @Test
+    public void fromStream() {
+        new Html(new ByteArrayInputStream(
+                "<div><span>contents</span></div>".getBytes()));
+    }
+
+    @Test
+    public void brokenHtml() {
+        Html html = new Html("<b></div>");
+        Assert.assertEquals("b", html.getElement().getTag());
+        Assert.assertEquals("", html.getInnerHtml());
+    }
+
+}
