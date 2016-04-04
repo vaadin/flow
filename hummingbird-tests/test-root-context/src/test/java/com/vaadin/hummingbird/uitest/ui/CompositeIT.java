46a8d24fd77faa42561c6bc6232d369971af21a4
Artur Signell <artur@vaadin.com>
2016-04-04 08:49:38 +0300

    Add a Composite component
    A composite can be used for creating new components by combining existing
    components using their Java API. A component or a component tree with a
    single root is wrapped inside a Composite so that the API of the root
    component does not become public API of the composite.
    
    Fixes #382

---
+++ hummingbird-tests/test-root-context/src/test/java/com/vaadin/hummingbird/uitest/ui/CompositeIT.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.uitest.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.hummingbird.testutil.PhantomJSTest;
+
+public class CompositeIT extends PhantomJSTest {
+
+    @Test
+    public void changeOnClient() {
+        open();
+        WebElement name = findElement(By.id(CompositeNestedView.NAME_ID));
+        WebElement input = findElement(
+                By.id(CompositeNestedView.NAME_FIELD_ID));
+        Assert.assertEquals("Name on server:", name.getText());
+        input.sendKeys("123");
+        findElement(By.xpath("//body")).click(); // Make change event happen
+        Assert.assertEquals("Name on server: 123", name.getText());
+
+        WebElement serverValueInput = findElement(
+                By.id(CompositeView.SERVER_INPUT_ID));
+        WebElement serverValueButton = findElement(
+                By.id(CompositeView.SERVER_INPUT_BUTTON_ID));
+
+        serverValueInput.sendKeys("server value");
+        serverValueButton.click();
+
+        Assert.assertEquals("Name on server: server value", name.getText());
+
+    }
+}
