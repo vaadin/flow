e183caa69200a4ac67361a5fbc8d098d5e501841
Leif Åstrand <leif@vaadin.com>
2016-04-05 10:50:25 +0300

    Use templates on the client

--- hummingbird-client/src/main/java/com/vaadin/client/Registry.java
+++ hummingbird-client/src/main/java/com/vaadin/client/Registry.java
@@ -29,6 +29,7 @@ import com.vaadin.client.hummingbird.ExecuteJavaScriptProcessor;
 import com.vaadin.client.hummingbird.StateTree;
 import com.vaadin.client.hummingbird.collection.JsCollections;
 import com.vaadin.client.hummingbird.collection.JsMap;
+import com.vaadin.client.hummingbird.template.Templates;
 
 /**
  * A registry of singleton instances, such as {@link ServerRpcQueue}, which can
@@ -251,5 +252,14 @@ public class Registry {
      */
     public ServerConnector getServerConnector() {
         return get(ServerConnector.class);
+    }
+
+    /**
+     * Gets the {@link Templates} singleton.
+     * 
+     * @return the {@link Templates} singleton
+     */
+    public Templates getTemplates() {
+        return get(Templates.class);
     }
 }
