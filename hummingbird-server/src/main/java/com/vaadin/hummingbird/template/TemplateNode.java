d966a14244e2234cfb1b1df02dfc3daf9f4773bd
Leif Åstrand <leif@vaadin.com>
2016-04-05 15:07:28 +0300

    Implement basic template AST nodes and builders

---
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/template/TemplateNode.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.template;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A node in the AST parsed from a template file. A template node is immutable.
+ *
+ * @since
+ * @author Vaadin Ltd
+ */
+public abstract class TemplateNode implements Serializable {
+    private static final AtomicInteger nextId = new AtomicInteger();
+
+    private final int id = nextId.incrementAndGet();
+
+    // Not final because of the chicken and egg problem
+    private TemplateNode parent;
+
+    /**
+     * Gets the parent node of this node, or <code>null</code> if this is the
+     * root node.
+     *
+     * @return the parent node, or <code>null</code>
+     */
+    public TemplateNode getParent() {
+        return parent;
+    }
+
+    /**
+     * Gets the globally unique id of this template node.
+     *
+     * @return the template node id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Gets the number of child nodes.
+     *
+     * @return the number of child nodes
+     */
+    public abstract int getChildCount();
+
+    /**
+     * Gets the child node at the given index.
+     *
+     * @param index
+     *            the child index
+     * @return the child at the given index
+     */
+    public abstract TemplateNode getChild(int index);
+
+    /**
+     * Sets the parent of this node. The parent should be set before the node is
+     * used. The parent can only be set once.
+     *
+     * @param parent
+     *            the parent node to set, not <code>null</code>
+     */
+    void setParent(TemplateNode parent) {
+        assert this.parent == null : "Parent can only be set once";
+
+        assert parent != null;
+        this.parent = parent;
+    }
+}
---
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/template/TextTemplateBuilder.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.template;
+
+/**
+ * Builder for text template nodes.
+ *
+ * @since
+ * @author Vaadin Ltd
+ */
+public class TextTemplateBuilder implements TemplateBuilder {
+
+    private TemplateBinding binding;
+
+    /**
+     * Creates a new text template builder.
+     *
+     * @param binding
+     *            the text content binding
+     */
+    public TextTemplateBuilder(TemplateBinding binding) {
+        assert binding != null;
+
+        this.binding = binding;
+    }
+
+    /**
+     * Gets the text content binding.
+     *
+     * @return the text content binding, not <code>null</code>
+     */
+    public TemplateBinding getBinding() {
+        return binding;
+    }
+
+    @Override
+    public TemplateNode build() {
+        return new TextTemplateNode(binding);
+    }
+}
