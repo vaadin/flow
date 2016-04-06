780e19d944c3d01b41d96b43df456c73aa69e02e
Artur Signell <artur@vaadin.com>
2016-04-06 14:43:39 +0300

    Escape script tags in initial UIDL
    Fixes #415

---
+++ hummingbird-tests/test-root-context/src/main/java/com/vaadin/hummingbird/uitest/ui/ScriptInjectView.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.uitest.ui;
+
+import com.vaadin.hummingbird.dom.Element;
+import com.vaadin.hummingbird.dom.ElementFactory;
+
+public class ScriptInjectView extends AbstractDivView {
+
+    public ScriptInjectView() {
+        createInput("</script foo>");
+        createInput("</Script>");
+        createInput("</SCRIPT >");
+        createInput("</SCRIPT>");
+        createInput("< / SCRIPT>");
+        createInput("</ SCRIPT>");
+        createInput("< / SCRIPT >");
+        createInput("</SCRIpT>");
+    }
+
+    private void createInput(String endTag) {
+        String string = endTag + "<script>alert('foo');>" + endTag;
+        Element input = ElementFactory.createInput();
+        input.setAttribute("value", string);
+        getElement().appendChild(input);
+    }
+}
