f1e83b0bece3756d8ca87b7dff4be832780a52a0
Pekka Hyvönen <pekka@vaadin.com>
2016-04-09 08:17:41 +0300

    Adds Attach Detach Listener API for element
    Makes it possible to react to an element being attached / detached
    for e.g. creating or releasing resources.
    
    Fixes #386

--- hummingbird-server/src/main/java/com/vaadin/hummingbird/dom/Element.java
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/dom/Element.java
@@ -1398,4 +1398,48 @@ public class Element implements Serializable {
             throw new IllegalArgumentException("Event type must not be null");
         }
     }
+
+    /**
+     * Adds an attach listener for this element. It is invoked when the element
+     * is attached to a UI.
+     * <p>
+     * When a hierarchy of elements is being attached, the event is fired
+     * child-first.
+     *
+     * @param attachListener
+     *            the attach listener to add
+     * @return an event registration handle for removing the listener
+     */
+    public EventRegistrationHandle addAttachListener(
+            ElementAttachListener attachListener) {
+        if (attachListener == null) {
+            throw new IllegalArgumentException(
+                    "ElementAttachListener cannot be null");
+        }
+
+        return getNode().addAttachListener(
+                () -> attachListener.onAttach(new ElementAttachEvent(this)));
+    }
+
+    /**
+     * Adds a detach listener for this element. It is invoked when the element
+     * is detached from a UI.
+     * <p>
+     * When a hierarchy of elements is being detached, the event is fired
+     * child-first.
+     *
+     * @param detachListener
+     *            the detach listener to add
+     * @return an event registration handle for removing the listener
+     */
+    public EventRegistrationHandle addDetachListener(
+            ElementDetachListener detachListener) {
+        if (detachListener == null) {
+            throw new IllegalArgumentException(
+                    "ElementDetachListener cannot be null");
+        }
+
+        return getNode().addDetachListener(
+                () -> detachListener.onDetach(new ElementDetachEvent(this)));
+    }
 }
