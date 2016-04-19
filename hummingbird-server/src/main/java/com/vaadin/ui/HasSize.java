2c13efa6e6c2079dc163c8bc12c97aae82688f6a
Artur Signell <artur@vaadin.com>
2016-04-19 11:38:29 +0300

    Add HasSize which defines setWidth/setHeight for components
    HtmlComponents now implement HasSize
    
    Fixes #493

---
+++ hummingbird-server/src/main/java/com/vaadin/ui/HasSize.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.ui;
+
+import com.vaadin.hummingbird.dom.Element;
+
+/**
+ * Any component implementing this interface supports setting the size of the
+ * component using {@link #setWidth(String)} and {@link #setHeight(String)}. The
+ * sizes are set on the element as inline styles, i.e. using
+ * {@link Element#getStyle()}.
+ *
+ * @author Vaadin Ltd
+ * @since
+ */
+public interface HasSize extends HasElement {
+
+    String PROPERTY_WIDTH = "width";
+    String PROPERTY_HEIGHT = "height";
+
+    /**
+     * Sets the width of the component.
+     * <p>
+     * The width should be in a format understood by the browser, e.g. "100px"
+     * or "2.5em".
+     *
+     * @param width
+     *            the width to set
+     */
+    default void setWidth(String width) {
+        if (width == null) {
+            getElement().getStyle().remove(PROPERTY_WIDTH);
+        } else {
+            getElement().getStyle().set(PROPERTY_WIDTH, width);
+        }
+    }
+
+    /**
+     * Gets the width defined for the component.
+     * <p>
+     * Note that this does not return the actual size of the component but the
+     * width which has been set using {@link #setWidth(String)}.
+     *
+     * @return the width which has been set for the component
+     */
+    default String getWidth() {
+        return getElement().getStyle().get(PROPERTY_WIDTH);
+    }
+
+    /**
+     * Sets the height of the component.
+     * <p>
+     * The height should be in a format understood by the browser, e.g. "100px"
+     * or "2.5em".
+     *
+     * @param height
+     *            the height to set
+     */
+    default void setHeight(String height) {
+        if (height == null) {
+            getElement().getStyle().remove(PROPERTY_HEIGHT);
+        } else {
+            getElement().getStyle().set(PROPERTY_HEIGHT, height);
+        }
+    }
+
+    /**
+     * Gets the height defined for the component.
+     * <p>
+     * Note that this does not return the actual size of the component but the
+     * height which has been set using {@link #setHeight(String)}.
+     *
+     * @return the height which has been set for the component
+     */
+    default String getHeight() {
+        return getElement().getStyle().get(PROPERTY_HEIGHT);
+    }
+}
