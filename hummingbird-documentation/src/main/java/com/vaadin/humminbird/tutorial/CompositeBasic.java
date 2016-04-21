c7e1b78f5aad1737989cd45f12a21b9a3533432d
Artur Signell <artur@vaadin.com>
2016-04-21 11:15:57 +0300

    Add TextField Composite tutorial
    Fixes #439

---
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/CompositeBasic.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.humminbird.tutorial;
+
+import com.vaadin.humminbird.tutorial.annotations.CodeFor;
+import com.vaadin.hummingbird.html.Div;
+import com.vaadin.hummingbird.html.Input;
+import com.vaadin.hummingbird.html.Label;
+import com.vaadin.ui.Composite;
+
+@CodeFor("tutorial-component-composite.asciidoc")
+public class CompositeBasic {
+    public class TextField extends Composite<Div> {
+
+        private Label label;
+        private Input input;
+
+        public TextField(String labelText, String value) {
+            label = new Label();
+            label.setText(labelText);
+            input = new Input();
+            input.setValue(value);
+
+            getContent().add(label, input);
+        }
+
+        public String getValue() {
+            return input.getValue();
+        }
+
+        public void setValue(String value) {
+            input.setValue(value);
+        }
+
+        public void setLabel(String labelText) {
+            label.setText(labelText);
+        }
+
+        public String getLabel() {
+            return label.getText();
+        }
+    }
+
+}
