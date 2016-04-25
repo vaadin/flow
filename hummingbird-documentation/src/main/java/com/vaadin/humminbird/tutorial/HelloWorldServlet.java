241e8cca46ff3bb363e5a16da99b57ab02fbaf46
Artur <artur@vaadin.com>
2016-04-25 10:53:28 +0300

    Update hello world tutorial to use a view (#569)

--- hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/HelloWorldServlet.java
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/HelloWorldServlet.java
@@ -18,11 +18,20 @@ package com.vaadin.humminbird.tutorial;
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.humminbird.tutorial.HelloWorldServlet.HelloWorldConfiguration;
 import com.vaadin.humminbird.tutorial.annotations.CodeFor;
+import com.vaadin.hummingbird.router.RouterConfiguration;
+import com.vaadin.hummingbird.router.RouterConfigurator;
 import com.vaadin.server.VaadinServlet;
 
 @CodeFor("tutorial-hello-world.asciidoc")
 @WebServlet(urlPatterns = "/*", name = "HelloWorldServlet")
-@VaadinServletConfiguration(ui = HelloWorldUI.class, productionMode = false)
+@VaadinServletConfiguration(routerConfigurator = HelloWorldConfiguration.class, productionMode = false)
 public class HelloWorldServlet extends VaadinServlet {
+    public static class HelloWorldConfiguration implements RouterConfigurator {
+        @Override
+        public void configure(RouterConfiguration configuration) {
+            configuration.setRoute("", HelloWorld.class);
+        }
+    }
 }
