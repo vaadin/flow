b38a2826268a409eec06fae7db315cfd48577647
Denis Anisimov <denis@vaadin.com>
2016-04-26 16:50:08 +0300

    Implement binding class for text node.
    Fixes #594.

---
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/template/TextValueBinding.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.template;
+
+import com.vaadin.hummingbird.JsonCodec;
+import com.vaadin.hummingbird.StateNode;
+
+import elemental.json.Json;
+import elemental.json.JsonObject;
+import elemental.json.JsonValue;
+
+/**
+ * A template binding that produces dynamic text value for a text node.
+ *
+ * @since
+ * @author Vaadin Ltd
+ */
+public class TextValueBinding implements TemplateBinding {
+
+    /**
+     * Type identifier used for text bindings in JSON messages.
+     */
+    public static final String TYPE = "text";
+
+    private final String key;
+
+    public TextValueBinding(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String getValue(StateNode node) {
+        return node.getFeature(featureType)
+    }
+
+    @Override
+    public JsonValue toJson() {
+        JsonObject json = Json.createObject();
+
+        json.put("type", TYPE);
+        json.put("key", JsonCodec.encodeWithoutTypeInfo(key));
+
+        return json;
+    }
+
+}
