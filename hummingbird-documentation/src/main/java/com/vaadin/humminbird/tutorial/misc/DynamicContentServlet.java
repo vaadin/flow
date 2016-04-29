5f8139d2a2c71a4e4fdc4f842945b1e6543970dd
denis-anisimov <denis@vaadin.com>
2016-04-29 16:03:38 +0300

    Provide a tutorial for using components in templates. (#646)
    Fixes #584.

--- hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/misc/DynamicContentServlet.java
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/misc/DynamicContentServlet.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.humminbird.tutorial;
+package com.vaadin.humminbird.tutorial.misc;
 
 import java.io.IOException;
 
