b606e99601618b1a50947fb415e5fba974409e71
Pekka Hyvönen <pekka@vaadin.com>
2016-05-09 10:19:20 +0300

    Tutorial for ngFor in Template (#694)
    Closes #656

---
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/template/EmployeesTable.java
@@ -0,0 +1,43 @@
+package com.vaadin.humminbird.tutorial.template;
+
+import com.vaadin.humminbird.tutorial.annotations.CodeFor;
+import com.vaadin.hummingbird.StateNode;
+import com.vaadin.hummingbird.nodefeature.ModelList;
+import com.vaadin.hummingbird.nodefeature.ModelMap;
+import com.vaadin.ui.Template;
+
+@CodeFor("tutorial-template-for.asciidoc")
+public class EmployeesTable extends Template {
+
+    public void addEmployee(String name, String title, String email) {
+        // create a new item representing an employee in inside the model
+        StateNode employeeItem = new StateNode(ModelMap.class);
+
+        // populate the values for the employee item
+        ModelMap employeeModel = employeeItem.getFeature(ModelMap.class);
+        employeeModel.setValue("name", name);
+        employeeModel.setValue("title", title);
+        employeeModel.setValue("email", email);
+
+        // add the new item to the list of employees
+        getEmployeesList().add(employeeItem);
+    }
+
+    // helper for fetching the employees list from the model
+    private ModelList getEmployeesList() {
+        StateNode employees = (StateNode) getModel().getValue("employees");
+
+        // create the employees list if necessary
+        if (employees == null) {
+            employees = new StateNode(ModelList.class);
+            getModel().setValue("employees", employees);
+        }
+
+        return employees.getFeature(ModelList.class);
+    }
+
+    // helper for accessing the model of the template
+    private ModelMap getModel() {
+        return getElement().getNode().getFeature(ModelMap.class);
+    }
+}
