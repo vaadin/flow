b85fed293414e9e8e3ed1ccbc5896e2620afa005
Pekka Hyvönen <pekka@vaadin.com>
2016-05-13 10:31:59 +0300

    Add Component dependencies with @Javascript and @StyleSheet
    Fixes #442

---
+++ hummingbird-server/src/main/java/com/vaadin/annotations/JavaScript.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.UI;
+
+/**
+ * Annotation for defining JavaScript dependencies on a {@link Component} class.
+ * For adding multiple JavaScript files for a single component, you can use this
+ * annotation multiple times.
+ * <p>
+ * It is guaranteed that the JavaScript files are loaded on the client side
+ * before the component is used for the first time in a {@link UI}. Each
+ * dependency is sent only once.
+ * <p>
+ * NOTE: while this annotation is not inherited using the
+ * {@link Inherited @Inherited} annotation, the annotations of the possible
+ * parent components or implemented interfaces are read when sending the
+ * dependencies to the browser.
+ *
+ * @author Vaadin Ltd
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Repeatable(JavaScripts.class)
+public @interface JavaScript {
+
+    /**
+     * JavaScript file URLs to load before using the annotated {@link Component}
+     * in the browser.
+     * <p>
+     * Relative URLs are interpreted as relative to the service (servlet) path.
+     * You can prefix the URL with {@literal context://} to make it relative to
+     * the context path or use an absolute URL to refer to files outside the
+     * service (servlet) path.
+     *
+     * @return an JavaScript file URL, not <code>null</code>
+     */
+    String value();
+}
--- hummingbird-server/src/main/java/com/vaadin/server/communication/UidlWriter.java
+++ hummingbird-server/src/main/java/com/vaadin/server/communication/UidlWriter.java
@@ -17,15 +17,23 @@
 package com.vaadin.server.communication;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
+import com.vaadin.annotations.AnnotationReader;
+import com.vaadin.annotations.JavaScript;
+import com.vaadin.annotations.StyleSheet;
 import com.vaadin.hummingbird.JsonCodec;
 import com.vaadin.hummingbird.StateTree;
 import com.vaadin.hummingbird.change.MapPutChange;
+import com.vaadin.hummingbird.change.NodeAttachChange;
+import com.vaadin.hummingbird.change.NodeChange;
+import com.vaadin.hummingbird.nodefeature.ComponentMapping;
 import com.vaadin.hummingbird.nodefeature.TemplateMap;
 import com.vaadin.hummingbird.shared.NodeFeatures;
 import com.vaadin.hummingbird.template.TemplateNode;
@@ -35,6 +43,9 @@ import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.shared.ApplicationConstants;
 import com.vaadin.shared.JsonConstants;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Dependency;
+import com.vaadin.ui.Dependency.Type;
 import com.vaadin.ui.DependencyList;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.UIInternals;
@@ -54,6 +65,8 @@ import elemental.json.JsonValue;
  * @since 7.1
  */
 public class UidlWriter implements Serializable {
+
+    private final HashSet<String> sentComponentDependencies = new HashSet<>();
 
     /**
      * Creates a JSON object containing all pending changes to the given UI.
@@ -100,10 +113,10 @@ public class UidlWriter implements Serializable {
 
         JsonArray stateChanges = Json.createArray();
         JsonObject templates = Json.createObject();
-
-        encodeChanges(ui, stateChanges, templates);
-
         DependencyList dependencyList = ui.getInternals().getDependencyList();
+
+        encodeChanges(ui, stateChanges, templates, dependencyList);
+
         JsonArray pendingDeps = dependencyList.getPendingSendToClient();
         if (pendingDeps.length() != 0) {
             response.put(DependencyList.DEPENDENCY_KEY, pendingDeps);
@@ -160,7 +173,7 @@ public class UidlWriter implements Serializable {
      *            a JSON object to put new template nodes into
      */
     private void encodeChanges(UI ui, JsonArray stateChanges,
-            JsonObject templates) {
+            JsonObject templates, DependencyList dependencyList) {
         StateTree stateTree = ui.getInternals().getStateTree();
 
         Consumer<TemplateNode> templateEncoder = new Consumer<TemplateNode>() {
@@ -178,22 +191,67 @@ public class UidlWriter implements Serializable {
                 }
             }
         };
-
         stateTree.collectChanges(change -> {
             // Ensure new templates are sent to the client
-            if (change instanceof MapPutChange) {
-                MapPutChange put = (MapPutChange) change;
-                if (put.getFeature() == TemplateMap.class
-                        && put.getKey().equals(NodeFeatures.ROOT_TEMPLATE_ID)) {
-                    Integer id = (Integer) put.getValue();
-                    TemplateNode templateNode = TemplateNode.get(id.intValue());
-                    templateEncoder.accept(templateNode);
-                }
-            }
+            runIfNewTemplateChange(change, templateEncoder);
+
+            // send components' @StyleSheets and @JavaScript dependencies
+            runIfComponentAttachChange(change,
+                    c -> readComponentDependencies(c, dependencyList));
 
             // Encode the actual change
             stateChanges.set(stateChanges.length(), change.toJson());
         });
+    }
+
+    private static void runIfNewTemplateChange(NodeChange change,
+            Consumer<TemplateNode> consumer) {
+        if (change instanceof MapPutChange) {
+            MapPutChange put = (MapPutChange) change;
+            if (put.getFeature() == TemplateMap.class
+                    && put.getKey().equals(NodeFeatures.ROOT_TEMPLATE_ID)) {
+                Integer id = (Integer) put.getValue();
+                TemplateNode templateNode = TemplateNode.get(id.intValue());
+
+                consumer.accept(templateNode);
+            }
+        }
+    }
+
+    private static void runIfComponentAttachChange(NodeChange change,
+            Consumer<Component> consumer) {
+        if (change instanceof NodeAttachChange
+                && change.getNode().hasFeature(ComponentMapping.class)) {
+            Optional<Component> component = change.getNode()
+                    .getFeature(ComponentMapping.class).getComponent();
+            component.ifPresent(consumer);
+        }
+    }
+
+    private void readComponentDependencies(Component component,
+            DependencyList dependencyList) {
+        List<JavaScript> javaScripts = AnnotationReader
+                .getJavaScriptAnnotations(component.getClass());
+        javaScripts.forEach(js -> writeDependencies(Type.JAVASCRIPT, js.value(),
+                dependencyList));
+
+        List<StyleSheet> styleSheets = AnnotationReader
+                .getStyleSheetAnnotations(component.getClass());
+        styleSheets.forEach(sS -> writeDependencies(Type.STYLESHEET, sS.value(),
+                dependencyList));
+    }
+
+    private void writeDependencies(Type dependencyType,
+            String dependencyUrlToWrite,
+            DependencyList dependencyListToWriteTo) {
+        assert dependencyUrlToWrite != null : "Dependency URL cannot be null, Type:"
+                + dependencyType;
+        // don't send urls twice
+        if (!sentComponentDependencies.contains(dependencyUrlToWrite)) {
+            dependencyListToWriteTo
+                    .add(new Dependency(dependencyType, dependencyUrlToWrite));
+            sentComponentDependencies.add(dependencyUrlToWrite);
+        }
     }
 
     /**
