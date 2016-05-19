8e5045e6dc1c106e3bf1caf4507e1b60a562e3c9
Artur Signell <artur@vaadin.com>
2016-05-19 14:58:07 +0300

    Support setting styles using setAttribute("style")

---
+++ hummingbird-server/src/main/java/com/vaadin/hummingbird/dom/StyleUtil.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.dom;
+
+import com.vaadin.shared.util.SharedUtil;
+
+public class StyleUtil {
+
+    private static String[] vendorPrefixes = new String[] { "webkit", "moz",
+            "ms", "o" };
+
+    /**
+     * Converts the given attribute style (dash-separated) into a property style
+     * (camelCase) which is accepted by {@link #set(String, String)}.
+     *
+     * @param attributeStyle
+     *            the attribute style
+     * @return the property style
+     */
+    static String styleAttributeToProperty(String attributeStyle) {
+        String propertyStyle;
+        if (attributeStyle.startsWith("-")) {
+            // Vendor prefix -webkit-border-after is webkitBorderAfter
+            propertyStyle = attributeStyle.substring(1);
+        } else {
+            propertyStyle = attributeStyle;
+        }
+
+        return SharedUtil.dashSeparatedToCamelCase(propertyStyle);
+    }
+
+    /**
+     * Converts the given property style (camelCase) into a attribute style
+     * (dash-separated).
+     *
+     * @param propertyStyle
+     *            the property style
+     * @return the attribute style
+     */
+    public static String stylePropertyToAttribute(String propertyStyle) {
+        String attributeStyle = SharedUtil
+                .camelCaseToDashSeparated(propertyStyle);
+        int dashIndex = attributeStyle.indexOf("-");
+        if (dashIndex != -1) {
+            // webkit-border-after -> -webkit-border-after
+            String possibleVendorPrefix = propertyStyle.substring(0, dashIndex);
+            for (String vendorPrefix : vendorPrefixes) {
+                if (vendorPrefix.equals(possibleVendorPrefix)) {
+                    return "-" + attributeStyle;
+                }
+            }
+        }
+
+        return attributeStyle;
+    }
+
+}
