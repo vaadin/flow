b86bfd52f6ef213a97bc6e7061b4e578e0864184
Artur Signell <artur@vaadin.com>
2016-05-24 13:52:29 +0300

    Support script tags in templates
    Fixes #831

---
+++ hummingbird-server/src/test/java/com/vaadin/hummingbird/template/ScriptParsingTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.template;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.hummingbird.StateNode;
+import com.vaadin.hummingbird.template.parser.TemplateParser;
+
+public class ScriptParsingTest {
+
+    private TemplateNode parse(String html) {
+        return TemplateParser.parse(html);
+    }
+
+    @Test
+    public void inlineScript() {
+        TemplateNode templateNode = parse("<script>\n" //
+                + "window.alert('hello');\n" //
+                + "window.alert('world');\n" //
+                + "</script>" //
+        ); //
+        Assert.assertEquals(ElementTemplateNode.class, templateNode.getClass());
+        ElementTemplateNode elementTemplate = (ElementTemplateNode) templateNode;
+        Assert.assertEquals("script", elementTemplate.getTag());
+
+        TextTemplateNode textNode = ((TextTemplateNode) elementTemplate
+                .getChild(0));
+        String nodeContents = (String) textNode.getTextBinding()
+                .getValue(new StateNode());
+        Assert.assertEquals(
+                "\n" + "window.alert('hello');\n" + "window.alert('world');\n",
+                nodeContents);
+    }
+
+    @Test
+    public void emptyInlineScript() {
+        TemplateNode templateNode = parse("<script></script>");
+        Assert.assertEquals(ElementTemplateNode.class, templateNode.getClass());
+        ElementTemplateNode elementTemplate = (ElementTemplateNode) templateNode;
+        Assert.assertEquals("script", elementTemplate.getTag());
+
+    }
+
+    @Test
+    public void scriptWithAttributes() {
+        TemplateNode templateNode = parse(
+                "<script type='text/javascript' src='file://foobar'/>");
+        Assert.assertEquals(ElementTemplateNode.class, templateNode.getClass());
+        ElementTemplateNode elementTemplate = (ElementTemplateNode) templateNode;
+        Assert.assertEquals("script", elementTemplate.getTag());
+        Assert.assertEquals("text/javascript", elementTemplate
+                .getAttributeBinding("type").get().getValue(new StateNode()));
+        Assert.assertEquals("file://foobar", elementTemplate
+                .getAttributeBinding("src").get().getValue(new StateNode()));
+
+    }
+}
