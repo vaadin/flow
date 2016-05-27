64342182a238151fb42d1f1000133f68e8822cd6
Artur Signell <artur@vaadin.com>
2016-05-27 13:37:36 +0300

    Tutorial for property descriptors
    Fixes #822

---
+++ hummingbird-documentation/src/main/java/com/vaadin/humminbird/tutorial/component/ComponentPropertyDescriptor.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.humminbird.tutorial.component;
+
+import java.util.Optional;
+
+import com.vaadin.annotations.Tag;
+import com.vaadin.humminbird.tutorial.annotations.CodeFor;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.PropertyDescriptor;
+import com.vaadin.ui.PropertyDescriptors;
+
+@CodeFor("tutorial-component-property-descriptor.asciidoc")
+public class ComponentPropertyDescriptor {
+    @Tag("input")
+    public class TextField extends Component {
+        public String getValue() {
+            return getElement().getProperty("value");
+        }
+
+        public void setValue(String value) {
+            if (value == null) {
+                // Make it easier to use the component when getValue() never
+                // returns null
+                throw new IllegalArgumentException("The value cannot be null");
+            } else if ("".equals(value)) {
+                // Reset the component to the default state so we do not have
+                // two ways of describing the same state (no "value" property
+                // and a "value" property set to "")
+                getElement().removeProperty("value");
+            } else {
+                getElement().setProperty("value", value);
+            }
+        }
+    }
+
+    // @formatter:off
+    private static PropertyDescriptor<String, String> VALUE = PropertyDescriptors.propertyWithDefault("value", "");
+    private static PropertyDescriptor<String, Optional<String>> PLACEHOLDER = PropertyDescriptors.optionalAttributeWithDefault("placeholder", "");
+    // @formatter:on
+
+    class A {
+        @Tag("input")
+        public class TextField extends Component {
+
+            public String getValue() {
+                return get(VALUE);
+            }
+
+            public void setValue(String value) {
+                set(VALUE, value);
+            }
+        }
+    }
+
+    class B {
+        @Tag("input")
+        public class TextField extends Component {
+
+            public Optional<String> getPlaceholder() {
+                return get(PLACEHOLDER);
+            }
+
+            public void setPlaceholder(String placeholder) {
+                set(PLACEHOLDER, placeholder);
+            }
+        }
+
+    }
+}
