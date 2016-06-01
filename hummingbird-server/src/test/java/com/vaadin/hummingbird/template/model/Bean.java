71a106de4ee892513bf31b89885e4ea6a4503e0c
Pekka Hyvönen <pekka@vaadin.com>
2016-06-01 12:16:25 +0300

    Import Bean to Template Model (#886)
    Fixes #877

---
+++ hummingbird-server/src/test/java/com/vaadin/hummingbird/template/model/Bean.java
@@ -0,0 +1,67 @@
+package com.vaadin.hummingbird.template.model;
+
+public class Bean {
+    private boolean booleanValue;
+    private Boolean booleanObject;
+    private int intValue;
+    private Integer intObject;
+    private double doubleValue;
+    private Double doubleObject;
+    private String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public boolean isBooleanValue() {
+        return booleanValue;
+    }
+
+    public void setBooleanValue(boolean booleanValue) {
+        this.booleanValue = booleanValue;
+    }
+
+    public Boolean getBooleanObject() {
+        return booleanObject;
+    }
+
+    public void setBooleanObject(Boolean booleanObject) {
+        this.booleanObject = booleanObject;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public Integer getIntObject() {
+        return intObject;
+    }
+
+    public void setIntObject(Integer intObject) {
+        this.intObject = intObject;
+    }
+
+    public double getDoubleValue() {
+        return doubleValue;
+    }
+
+    public void setDoubleValue(double doubleValue) {
+        this.doubleValue = doubleValue;
+    }
+
+    public Double getDoubleObject() {
+        return doubleObject;
+    }
+
+    public void setDoubleObject(Double doubleObject) {
+        this.doubleObject = doubleObject;
+    }
+}
