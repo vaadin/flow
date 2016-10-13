0a2812e2e84c710d4de4e42fc21168193737b59c
Henri Sara <henri.sara@gmail.com>
2016-10-13 10:34:07 +0300

    Update copyright years
    Closesvaadin/spring#100

--- vaadin-spring/src/main/java/com/vaadin/spring/server/SpringVaadinServletRequest.java
+++ vaadin-spring/src/main/java/com/vaadin/spring/server/SpringVaadinServletRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 The original authors
+ * Copyright 2015-2016 The original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
