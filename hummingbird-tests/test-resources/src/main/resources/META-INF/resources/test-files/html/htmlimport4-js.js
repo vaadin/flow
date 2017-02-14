b8c304c674d23875ac9fe340928fe5074f774c13
Kirill Bulatov <mail4score@gmail.com>
2017-02-14 11:29:13 +0200

    Fixed html import test.

--- hummingbird-tests/test-resources/src/main/resources/META-INF/resources/test-files/html/htmlimport4-js.js
+++ hummingbird-tests/test-resources/src/main/resources/META-INF/resources/test-files/html/htmlimport4-js.js
@@ -1,2 +1,8 @@
-// This should always be loaded after htmlimport4.html
+window.logMessage = function(msg) {
+    var d = document.createElement("div");
+    d.innerText = msg;
+    d.classList.add("message");
+    document.body.appendChild(d);
+};
+
 logMessage("HTML import 4 companion JS loaded");
