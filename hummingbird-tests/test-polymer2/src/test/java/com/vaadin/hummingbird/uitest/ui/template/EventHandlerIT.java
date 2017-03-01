39e02049b1d65343a1db13cf4b430b04a4402ba1
Denis Anisimov <denis@vaadin.com>
2017-03-01 17:49:48 +0300

    Add Polymer 2 test module and configure travis to use Chrome for it.
    Fixes #1276

--- hummingbird-tests/test-polymer2/src/test/java/com/vaadin/hummingbird/uitest/ui/template/EventHandlerIT.java
+++ hummingbird-tests/test-polymer2/src/test/java/com/vaadin/hummingbird/uitest/ui/template/EventHandlerIT.java
@@ -16,20 +16,18 @@
 package com.vaadin.hummingbird.uitest.ui.template;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import com.vaadin.hummingbird.testutil.PhantomJSTest;
+import com.vaadin.hummingbird.testutil.SingleBrowserTest;
 import com.vaadin.testbench.By;
 
 /**
  * @author Vaadin Ltd
  *
  */
-public class EventHandlerIT extends PhantomJSTest {
+public class EventHandlerIT extends SingleBrowserTest {
 
     @Test
-    @Ignore("Phantom JS doesn't support Polymer")
     public void handleEventOnServer() {
         open();
 
