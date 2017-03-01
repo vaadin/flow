1b1bc9fe2f29f46373f014ce50bac0fae0acf352
Mikael Grankvist <mikael.grankvist@vaadin.com>
2017-03-01 09:36:08 +0200

    Remove created by.

--- hummingbird-tests/test-expense-manager-imperative/src/main/java/com/vaadin/hummingbird/demo/expensemanager/ExpenseManagerUI.java
+++ hummingbird-tests/test-expense-manager-imperative/src/main/java/com/vaadin/hummingbird/demo/expensemanager/ExpenseManagerUI.java
@@ -3,9 +3,6 @@ package com.vaadin.hummingbird.demo.expensemanager;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
-/**
- * Created by Mikael on 24/02/17.
- */
 public class ExpenseManagerUI extends UI {
     @Override
     protected void init(VaadinRequest request) {
