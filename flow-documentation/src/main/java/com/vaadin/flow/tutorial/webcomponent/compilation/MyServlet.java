da371ac59819588e7b274f1c4f7476d71f4710cf
Gilberto Torrezan <gilberto-torrezan@users.noreply.github.com>
2017-04-27 10:35:07 +0300

    Update ES5 compilation tutorial (#1526)
    * Update ES5 compilation tutorial
    
    Fix configuration error and add examples of configuration using servlet init params.
    
    * Fix broken link.
    
    * Add a small explanation about the web.xml section.
    
    * Merge branch 'master' into gilberto/es5-docs-fixes
    
    * Add java files for the compilation tutorial.

---
+++ flow-documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/compilation/MyServlet.java
@@ -0,0 +1,16 @@
+package com.vaadin.flow.tutorial.webcomponent.compilation;
+
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+import com.vaadin.server.VaadinServlet;
+
+@CodeFor("tutorial-webcomponents-es5.asciidoc")
+@WebServlet(urlPatterns = "/*", name = "myservlet", asyncSupported = true, initParams = {
+        @WebInitParam(name = "frontend.url.es6", value = "http://mydomain.com/es6/"),
+        @WebInitParam(name = "frontend.url.es5", value = "http://mydomain.com/es5/") })
+@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+public class MyServlet extends VaadinServlet {
+}
