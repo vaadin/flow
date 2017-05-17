8c4babbcebc96b5e35124a058f8c75e959ac5404
Denis <denis@vaadin.com>
2017-05-17 14:43:23 +0300

    Provide a Sonar Qube validation option to run tests (on Travis) script (#1612)

--- .travis.validation.sh
+++ .travis.validation.sh
@@ -8,8 +8,14 @@ while [ "$1" != "" ]; do
          -javadoc )      shift
                          javadoc=1
                          ;;
-         -full )         shift
+         -all-tests )    shift
                          full=1
+                         ;;
+         -sonar )        shift
+                         sonar=1
+                         ;;
+         -sonaronly )    shift
+                         sonaronly=1
                          ;;
     esac
     shift
@@ -24,8 +30,19 @@ else
    javadoc_opts=" -Dmaven.javadoc.skip=false javadoc:javadoc"
 fi
 
+if [ "$sonaronly" = "1" ]; then
+mvn -B -e -V -Dmaven.test.skip=true -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.host.url=$SONAR_HOST -Dsonar.login=$SONAR_LOGIN org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar
+exit
+fi
+
+sonar_opts=""
+
+if [ "$sonar" = "1" ]; then
+sonar_opts="-Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.host.url=$SONAR_HOST -Dsonar.login=$SONAR_LOGIN org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar"
+fi
+
 if [ "$full" = "1" ]; then
-mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE  -Pall-tests verify $javadoc_opts
+mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE  -Pall-tests verify $javadoc_opts $sonar_opts
 else
-mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -P validation verify $javadoc_opts
+mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -P validation verify $javadoc_opts $sonar_opts
 fi
