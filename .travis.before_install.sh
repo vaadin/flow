bf21ae037734b4e48d4b3c56d0e1207c86c08584
Kirill Bulatov <mail4score@gmail.com>
2017-05-22 15:58:36 +0300

    Trying to use real Chrome.

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-# Disable all Travis default repositories 
+# Disable all Travis default repositories
 sed -i "s/activeByDefault>true</activeByDefault>false</g"  ~/.m2/settings.xml
+export CHROME_BIN=/usr/bin/google-chrome
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+# The Following line fixes random Chrome startup failures (https://github.com/SeleniumHQ/docker-selenium/issues/87)
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
