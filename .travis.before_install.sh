f520b28c01628691284e3c089a660ba5484e98be
Kirill Bulatov <mail4score@gmail.com>
2017-05-23 17:39:53 +0300

    zz

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -2,10 +2,3 @@
 
 # Disable all Travis default repositories
 sed -i "s/activeByDefault>true</activeByDefault>false</g"  ~/.m2/settings.xml
-
-# Make Chrome IT tests more stable
-export CHROME_BIN=/usr/bin/google-chrome
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
-# The Following line fixes random Chrome startup failures (https://github.com/SeleniumHQ/docker-selenium/issues/87)
-export DBUS_SESSION_BUS_ADDRESS=/dev/null
