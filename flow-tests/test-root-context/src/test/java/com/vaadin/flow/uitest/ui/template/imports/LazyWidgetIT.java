9785892007089a69853a1e439e112578f2169d19
Kirill Bulatov <mail4score@gmail.com>
2017-05-31 15:00:38 +0300

    Added a test case that pinpoints the problem.

---
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/template/imports/LazyWidgetIT.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.uitest.ui.template.imports;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.testbench.By;
+
+/**
+ * @author Vaadin Ltd.
+ */
+public class LazyWidgetIT extends ChromeBrowserTest {
+
+    @Test
+    public void test() {
+        open();
+        WebElement template = findElements(By.id("template")).get(0);
+        String input = "InputMaster";
+        Assert.assertFalse(
+                "No greeting should be present before we press the button",
+                getInShadowRoot(template, By.id("greeting")).isPresent());
+
+        getInShadowRoot(template, By.id("input")).get().sendKeys(input);
+        getInShadowRoot(template, By.id("button")).get().click();
+
+        Assert.assertEquals("Greeting is different from expected",
+                String.format(LazyWidgetView.GREETINGS_TEMPLATE, input),
+                getInShadowRoot(template, By.id("greeting")).get().getText());
+    }
+}
