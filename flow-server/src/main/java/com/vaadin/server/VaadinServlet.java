bc66b3061ba80ef381aaacfdf09ad3dc329e96ae
Leif Åstrand <leif@vaadin.com>
2017-06-02 15:44:18 +0300

    Use the webcomponents polyfill from bower_components
    Without this fix, there's a potential for a version mismatch since some
    parts of Polymer indirectly loads polyfill files from the same location
    where Polymer was loaded.

--- flow-server/src/main/java/com/vaadin/server/VaadinServlet.java
+++ flow-server/src/main/java/com/vaadin/server/VaadinServlet.java
@@ -19,9 +19,14 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Enumeration;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -214,7 +219,29 @@ public class VaadinServlet extends HttpServlet {
 
     protected DeploymentConfiguration createDeploymentConfiguration(
             Properties initParameters) {
-        return new DefaultDeploymentConfiguration(getClass(), initParameters);
+        return new DefaultDeploymentConfiguration(getClass(), initParameters,
+                this::scanForResources);
+    }
+
+    protected void scanForResources(String basePath,
+            Predicate<String> consumer) {
+        Deque<String> queue = new ArrayDeque<>();
+        queue.add(basePath);
+
+        ServletContext context = getServletContext();
+        int prefixLength = basePath.length();
+
+        while (!queue.isEmpty()) {
+            String path = queue.removeLast();
+            boolean visit = consumer.test(path.substring(prefixLength));
+
+            if (visit && path.endsWith("/")) {
+                Set<String> resourcePaths = context.getResourcePaths(path);
+                if (resourcePaths != null) {
+                    queue.addAll(resourcePaths);
+                }
+            }
+        }
     }
 
     protected VaadinServletService createServletService(
@@ -536,6 +563,10 @@ public class VaadinServlet extends HttpServlet {
      */
     protected StaticFileServer createStaticFileServer() {
         return new StaticFileServer();
+    }
+
+    private static Logger getLogger() {
+        return Logger.getLogger(VaadinServlet.class.getName());
     }
 
 }
