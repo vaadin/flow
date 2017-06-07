e4e03ef17ad80848bf1f6ff46a854f5f68b900d0
caalador <mikael.grankvist@gmail.com>
2017-06-07 22:33:13 +0300

    Update travis.script for targeted builds (#1740)
    * Test new travis.script for target build
    
    This test should only run the components module tests and skip all other tests. Everything should still be compiled.
    
    * Check all commits against master when not running tests.
    If we don't run tests then don't dowload bower dependencies nor start the jetty server.
    
    * Skip the travis install step.
    Skip the install step of travis as we will do the same actions in script.
    
    * Only consider commits to this branch.
    We now ignore any merge commits and only check changed files for current branch.
    
    * Make the script travis compatible.
    
    * Only run and test changed modules.
    Only build and test modules with changes and those that depend on the changed modules
    
    * Modules add their own -pl
    
    * Ignore expense manager on travis.
    
    *  sub-module needs to be marked module/submodule
    
    * exclude flow from expense manager
    
    Exclusion is added as else the import tree
    is broken.
    
    * Run everything for tests.
    Due to travis failing for expense manager
    we run the whole flow when building the
    test module.
    
    * Only activate expense manager for gatling test
    * Build flow-tests for tests
    * Have test module as last.
    * Don't remove merged pull requests on non pull-request
    Only remove merged pull requests when building a pull-request.

--- .travis.script.sh
+++ .travis.script.sh
@@ -5,20 +5,86 @@
 # TRAVIS_SECURE_ENV_VARS == true if encrypted variables, e.g. SONAR_HOST is available
 # TRAVIS_REPO_SLUG == the repository, e.g. vaadin/vaadin
 
+# Get all changes to branch (no-merges)
+actualCommits=`git log --no-merges --pretty=oneline master^..HEAD`
+
+# If running a pull request drop merged pull requests that have '(#' in the comment)
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]
+then
+  actualCommits=`echo "$actualCommits" | grep -v "(#"`
+fi
+
+#Collect commit hashes
+actualCommits=`echo "$actualCommits" | awk '{print $1}' | tr '\n' ' '`
+
+# Get changed files with full path for branch commits.
+change=`diff <(git show --name-only $actualCommits) <(git show --summary $actualCommits)`
+
+# Collect changed modules to build and build also modules that depend on the
+# selected modules (see the flag '-amd')
+
+modules=
+if [[ $change == *"flow-push/"* ]]
+then
+  modules="$modules -pl flow-push"
+else
+  ## All modules except for production mode depend on push.
+  if [[ $change == *"flow-server/"* ]]
+  then
+    modules="$modules -pl flow-server"
+  else
+    if [[ $change == *"flow-html-components/"* ]]
+    then
+      modules="$modules -pl flow-html-components"
+    else
+      if [[ $change == *"flow-documentation/"* ]]
+      then
+        modules="$modules -pl flow-documentation"
+      fi
+    fi
+
+    if [[ $change == *"flow-components/"* ]]
+    then
+      echo "Setting components flag to true"
+      modules="$modules -pl flow-components"
+    fi
+
+    if [[ $change == *"flow-client/"* ]]
+    then
+      modules="$modules -pl flow-client"
+    else
+      if [[ $change == *"flow-test-util/"* ]]
+      then
+        modules="$modules -pl flow-test-util"
+      else
+        if [[ $change == *"flow-tests/"* ]]
+        then
+          modules="$modules -pl flow-tests"
+        fi
+      fi
+    fi
+  fi
+fi
+
+if [[ $change == *"flow-server-production-mode/"* ]]
+then
+  modules="$modules -pl flow-server-production-mode"
+fi
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
 then
     # Pull request for master with secure vars (SONAR_GITHUB_OAUTH, SONAR_HOST) available
 
     # Trigger Sonar analysis
     echo "Running Sonar"
-    mvn -B -e -V -P validation -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar javadoc:javadoc
+    mvn -B -e -V -P validation -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar javadoc:javadoc $modules -amd
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]
 then
     # master build
-    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Pall-tests -Dgatling.skip=true clean org.jacoco:jacoco-maven-plugin:prepare-agent install
+    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Pall-tests -Dgatling.skip=true clean org.jacoco:jacoco-maven-plugin:prepare-agent install $modules -amd
     # Sonar should be run after the project is built so that findbugs can analyze compiled sources
-    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dgatling.skip=true -Dsonar.verbose=true -Dsonar.analysis.mode=publish -Dsonar.host.url=$SONAR_HOST -Dsonar.login=$SONAR_LOGIN sonar:sonar
+    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dgatling.skip=true -Dsonar.verbose=true -Dsonar.analysis.mode=publish -Dsonar.host.url=$SONAR_HOST -Dsonar.login=$SONAR_LOGIN sonar:sonar $modules -amd
 else
     # Something else than a "safe" pull request
-    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Pall-tests verify javadoc:javadoc
+    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Pall-tests verify javadoc:javadoc $modules -amd
 fi
