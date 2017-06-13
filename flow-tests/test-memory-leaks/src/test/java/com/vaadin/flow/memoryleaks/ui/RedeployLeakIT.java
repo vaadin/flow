aaf98808baaa72861b3f326e8fca4a3e323b8273
Kirill Bulatov <mail4score@gmail.com>
2017-06-13 12:31:35 +0300

    Code review fixes.

--- flow-tests/test-memory-leaks/src/test/java/com/vaadin/flow/memoryleaks/ui/RedeployLeakIT.java
+++ flow-tests/test-memory-leaks/src/test/java/com/vaadin/flow/memoryleaks/ui/RedeployLeakIT.java
@@ -18,6 +18,7 @@ package com.vaadin.flow.memoryleaks.ui;
 
 import java.io.File;
 import java.lang.ref.WeakReference;
+import java.util.List;
 
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
@@ -39,6 +40,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.vaadin.flow.testutil.AbstractTestBenchTest;
 import com.vaadin.testbench.annotations.RunOnHub;
+import com.vaadin.testbench.parallel.Browser;
 
 /**
  * Test that verifies no leaks happen during redeployment of a war. Uses Jetty
@@ -48,12 +50,29 @@ import com.vaadin.testbench.annotations.RunOnHub;
  * uses files from the target folder.
  */
 @RunOnHub
-public class RedeployLeakIT {
+public class RedeployLeakIT extends AbstractTestBenchTest {
 
     private static final String testClass = "com.vaadin.server.VaadinServlet";
     private Server server;
     private WebAppContext context;
     private WeakReference<Class<?>> testReference;
+
+    @Override
+    public void checkIfServerAvailable() {
+        // do nothing, since it's a special test that starts server manually
+    }
+
+    @Override
+    protected String getTestPath() {
+        // We open test pages manually, no need to specify other paths.
+        return null;
+    }
+
+    @Override
+    protected List<DesiredCapabilities> getHubBrowsersToTest() {
+        // Because other browsers are not supported
+        return getBrowserCapabilities(Browser.CHROME);
+    }
 
     @Test
     public void deployUndeployCheck() throws Exception {
@@ -70,7 +89,7 @@ public class RedeployLeakIT {
         setup(7778);
         RemoteWebDriver driver = new RemoteWebDriver(DesiredCapabilities.chrome());
         try {
-            driver.get("http://"+ AbstractTestBenchTest.getCurrentHostAddress() + ":7778/");
+            driver.get("http://"+ getCurrentHostAddress() + ":7778/");
             Assert.assertNotNull(driver.findElement(By.id("hello")));
         } finally {
             driver.close();
