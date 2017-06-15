8d8307b2e79ba99c13c681fb332ea0d53036317e
Kirill Bulatov <mail4score@gmail.com>
2017-06-15 16:36:40 +0300

    Now scroll position on page is updated when we've loaded and opened the new page.

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/scroll/LongToOpenView.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.uitest.ui.scroll;
+
+import com.vaadin.flow.html.Button;
+import com.vaadin.flow.html.Div;
+import com.vaadin.flow.uitest.ui.AbstractDivView;
+
+/**
+ * @author Vaadin Ltd.
+ */
+public class LongToOpenView extends AbstractDivView {
+    public LongToOpenView() {
+        Div div = new Div();
+        div.setText("I am the long to open view");
+
+        Button back = new Button("Back", event -> getPage().getHistory().back());
+
+        add(div, back);
+    }
+
+    @Override
+    protected void onShow() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
