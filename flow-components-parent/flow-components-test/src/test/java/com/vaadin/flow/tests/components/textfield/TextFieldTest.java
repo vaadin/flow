713cc3f80f42ee5106d8eff4b341693e4bedc72d
Aleksi Hietanen <aleksi@vaadin.com>
2017-07-31 09:32:58 +0300

    Add integration test module for flow-components

---
+++ flow-components-parent/flow-components-test/src/test/java/com/vaadin/flow/tests/components/textfield/TextFieldTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.tests.components.textfield;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.tests.components.AbstractComponentIT;
+import com.vaadin.testbench.By;
+import com.vaadin.ui.VaadinTextField;
+
+/**
+ * Integration tests for {@link VaadinTextField}.
+ */
+public class TextFieldTest extends AbstractComponentIT {
+
+    @Before
+    public void init() {
+        open();
+    }
+
+    @Test
+    public void pass() {
+        isElementPresent(By.tagName("vaadin-text-field"));
+    }
+}
