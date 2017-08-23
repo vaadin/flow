66fe0bd7b596e64fa08ccfb609b70170a58fc711
Pekka Hyvönen <pekka@vaadin.com>
2017-08-23 13:00:22 +0300

    Update vaadin-checkbox and vaadin-date-picker versions (#2219)
    * Bump vaadin-date-picker and vaadin-check-box versions
    
    Also updated vaadin-text-field, but it was already generated with new
    version
    
    * Regenerate components to fix importing order
    
    It seems that there was a previous patch, which generated the json and updates to match the new component versions, but did not include the updated the bower.json

--- flow-components-parent/flow-generated-components/src/main/java/com/vaadin/generated/paper/menu/button/GeneratedPaperMenuButton.java
+++ flow-components-parent/flow-generated-components/src/main/java/com/vaadin/generated/paper/menu/button/GeneratedPaperMenuButton.java
@@ -15,21 +15,19 @@
  */
 package com.vaadin.generated.paper.menu.button;
 
-import javax.annotation.Generated;
-
-import com.vaadin.annotations.DomEvent;
-import com.vaadin.annotations.HtmlImport;
-import com.vaadin.annotations.Synchronize;
-import com.vaadin.annotations.Tag;
-import com.vaadin.components.NotSupported;
-import com.vaadin.flow.event.ComponentEventListener;
-import com.vaadin.shared.Registration;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.ComponentEvent;
 import com.vaadin.ui.ComponentSupplier;
 import com.vaadin.ui.HasStyle;
-
+import javax.annotation.Generated;
+import com.vaadin.annotations.Tag;
+import com.vaadin.annotations.HtmlImport;
 import elemental.json.JsonObject;
+import com.vaadin.annotations.Synchronize;
+import com.vaadin.components.NotSupported;
+import com.vaadin.annotations.DomEvent;
+import com.vaadin.ui.ComponentEvent;
+import com.vaadin.flow.event.ComponentEventListener;
+import com.vaadin.shared.Registration;
 
 /**
  * <p>
