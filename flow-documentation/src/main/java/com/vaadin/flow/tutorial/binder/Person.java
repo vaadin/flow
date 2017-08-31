4a5230519633b336b295309c50148538dd28c385
Denis <denis@vaadin.com>
2017-08-31 13:25:54 +0300

    Binder documentation Java samples (#2284)

---
+++ flow-documentation/src/main/java/com/vaadin/flow/tutorial/binder/Person.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.tutorial.binder;
+
+import java.io.Serializable;
+
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+
+@CodeFor("tutorial-flow-components-binder.asciidoc")
+public class Person implements Serializable {
+    private String name;
+    private String lastName;
+    private String email;
+    private String title;
+
+    private int yearOfBirth;
+
+    private Gender gender;
+
+    public enum Gender {
+        MALE, FEMALE;
+    }
+
+    public Person(String string, int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+}
