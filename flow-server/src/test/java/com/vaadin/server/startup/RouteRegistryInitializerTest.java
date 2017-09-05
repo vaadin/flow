750919ff852568bab2a5c6ddc596d519696663dd
Aleksi Hietanen <aleksi@vaadin.com>
2017-09-05 09:24:32 +0300

    Create RouteRegistryInitializer and RouteRegistry
    Closes #2285

---
+++ flow-server/src/test/java/com/vaadin/server/startup/RouteRegistryInitializerTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.server.startup;
+
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.servlet.ServletException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.annotations.Route;
+import com.vaadin.flow.router.Location;
+import com.vaadin.server.InvalidRouteConfigurationException;
+import com.vaadin.ui.Component;
+
+/**
+ * Unit tests for RouteRegistryInitializer and RouteRegistry.
+ */
+public class RouteRegistryInitializerTest {
+
+    private RouteRegistryInitializer routeRegistryInitializer;
+
+    @Before
+    public void init() {
+        routeRegistryInitializer = new RouteRegistryInitializer();
+    }
+
+    @Test
+    public void onStartUp() throws ServletException {
+        routeRegistryInitializer.onStartup(
+                Stream.of(NavigationTarget.class, NavigationTargetFoo.class,
+                        NavigationTargetBar.class).collect(Collectors.toSet()),
+                null);
+
+        Assert.assertEquals("Route '' registered to NavigationTarget.class",
+                NavigationTarget.class, RouteRegistry.getInstance()
+                        .getNavigationTarget(new Location("")).get());
+        Assert.assertEquals(
+                "Route 'foo' registered to NavigationTargetFoo.class",
+                NavigationTargetFoo.class, RouteRegistry.getInstance()
+                        .getNavigationTarget(new Location("foo")).get());
+        Assert.assertEquals(
+                "Route 'bar' registered to NavigationTargetBar.class",
+                NavigationTargetBar.class, RouteRegistry.getInstance()
+                        .getNavigationTarget(new Location("bar")).get());
+    }
+
+    @Test(expected = ServletException.class)
+    public void onStartUp_duplicate_routes_throws() throws ServletException {
+        routeRegistryInitializer.onStartup(
+                Stream.of(NavigationTargetFoo.class, NavigationTargetFoo2.class)
+                        .collect(Collectors.toSet()),
+                null);
+    }
+
+    @Test(expected = InvalidRouteConfigurationException.class)
+    public void routeRegistry_routes_can_only_be_set_once()
+            throws InvalidRouteConfigurationException {
+        RouteRegistry.getInstance().setNavigationTargets(new HashSet<>());
+        RouteRegistry.getInstance().setNavigationTargets(new HashSet<>());
+    }
+
+    @Route("")
+    private static class NavigationTarget extends Component {
+    }
+
+    @Route("foo")
+    private static class NavigationTargetFoo extends Component {
+    }
+
+    @Route("foo2")
+    private static class NavigationTargetFoo2 extends Component {
+    }
+
+    @Route("bar")
+    private static class NavigationTargetBar extends Component {
+    }
+}
