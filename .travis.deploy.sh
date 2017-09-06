b548e9f304d28a8a1865f1f9e9829a0dc024297f
Kirill Bulatov <mail4score@gmail.com>
2017-09-06 14:18:21 +0300

    Demo jar deployment fixed.

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-REPOSITORY_ID="hummingbird-snapshot"
-DEPLOYMENT_REPO="$REPOSITORY_ID::default::https://repo.vaadin.com/nexus/content/repositories/flow-snapshots/"
+DEPLOYMENT_REPO="hummingbird-snapshot::default::https://repo.vaadin.com/nexus/content/repositories/flow-snapshots/"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
-    mvn -B -e -V -DskipTests -DskipITs -Djetty.skip -Dgatling.skip -repositoryId=$REPOSITORY_ID -DaltDeploymentRepository=$DEPLOYMENT_REPO deploy --settings settings.xml
+    mvn -B -e -V -DskipTests -DskipITs -Djetty.skip -Dgatling.skip -DaltDeploymentRepository=$DEPLOYMENT_REPO deploy --settings settings.xml
 
     # Copy demo-flow-components to the server
     scp -o "StrictHostKeyChecking no" -P 5177 ./flow-components-parent/demo-flow-components/target/demo-flow-components*.war dev@virtuallypreinstalled.com:tomcat/webapps/
