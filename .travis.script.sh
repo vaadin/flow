18fc3df56ccccc71ddd11776cd494887475a4fec
Kirill Bulatov <mail4score@gmail.com>
2017-09-07 10:25:13 +0300

    Removed only partial modules built.

--- .travis.script.sh
+++ .travis.script.sh
@@ -7,72 +7,6 @@
 # SKIP_SONAR == skip sonar checks
 # USE_SELENOID == use Docker and Selenoid to run IT tests in personal hub
 
-# Get all changes to branch (no-merges)
-actualCommits=`git log --no-merges --pretty=oneline master^..HEAD`
-
-# If running a pull request drop merged pull requests that have '(#' in the comment)
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]
-then
-  actualCommits=`echo "$actualCommits" | grep -v "(#"`
-fi
-
-#Collect commit hashes
-actualCommits=`echo "$actualCommits" | awk '{print $1}' | tr '\n' ' '`
-
-# Get changed files with full path for branch commits.
-change=`diff <(git show --name-only $actualCommits) <(git show --summary $actualCommits)`
-
-# Collect changed modules to build and build also modules that depend on the
-# selected modules (see the flag '-amd')
-modules=
-if [[ $change == *"flow-push/"* ]]
-then
-  modules="$modules -pl flow-push"
-else
-  ## All modules except for production mode depend on push.
-  if [[ $change == *"flow-server/"* ]]
-  then
-    modules="$modules -pl flow-server"
-  else
-    if [[ $change == *"flow-html-components/"* ]]
-    then
-      modules="$modules -pl flow-html-components"
-    else
-      if [[ $change == *"flow-documentation/"* ]]
-      then
-        modules="$modules -pl flow-documentation"
-      fi
-    fi
-
-    if [[ $change == *"flow-components-parent/"* ]]
-    then
-      ## only trigger the analyzer & generator for validation builds on PRs that touched component generation
-      echo "Setting components flag to true"
-      modules="$modules -pl flow-components-parent -P generator"
-    fi
-
-    if [[ $change == *"flow-client/"* ]]
-    then
-      modules="$modules -pl flow-client"
-    fi
-  fi
-fi
-
-if [[ $change == *"flow-test-util/"* ]]
-then
-  modules="$modules -pl flow-test-util"
-else
-  if [[ $change == *"flow-tests/"* ]]
-  then
-    modules="$modules -pl flow-tests"
-  fi
-fi
-
-if [[ $change == *"flow-server-production-mode/"* ]]
-then
-  modules="$modules -pl flow-server-production-mode"
-fi
-
 function getDockerParamsIfNeeded {
     if [ "$USE_SELENOID" == "true" ]
     then
@@ -116,11 +50,8 @@ function runSonar {
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
 then
-    # Pull request for master with secure vars (SONAR_GITHUB_OAUTH, SONAR_HOST) available
-
-    # Trigger Sonar analysis
-    # Verify build and build javadoc
-    echo "Running clean verify $modules -amd"
+    # Pull request with secure vars (SONAR_GITHUB_OAUTH, SONAR_HOST) available
+    echo "Running clean verify"
     mvn -B -e -V \
         -Pvalidation \
         -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
@@ -128,8 +59,8 @@ then
         $(getDockerParamsIfNeeded) \
         clean \
         license:download-licenses \
-        org.jacoco:jacoco-maven-plugin:prepare-agent verify javadoc:javadoc $modules -amd
-    # Get the status for the previous maven command and if not exception then run sonar.
+        org.jacoco:jacoco-maven-plugin:prepare-agent verify javadoc:javadoc
+
     STATUS=$?
     if [ $STATUS -eq 0 ]
     then
@@ -140,7 +71,7 @@ then
     fi
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]
 then
-    # master build
+    # Master build with secure vars (SONAR_GITHUB_OAUTH, SONAR_HOST) available
     # Production mode commented out due to https://github.com/vaadin/flow/issues/2165
     # -Dvaadin.productionMode=true \
     mvn -B -e -V \
@@ -148,9 +79,8 @@ then
         -Dmaven.javadoc.skip=false \
         -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
         $(getDockerParamsIfNeeded) \
-        clean org.jacoco:jacoco-maven-plugin:prepare-agent license:download-licenses install
+        clean org.jacoco:jacoco-maven-plugin:prepare-agent license:download-licenses verify javadoc:javadoc
 
-    # Get the status for the previous maven command and if not exception then run sonar.
     STATUS=$?
     if [ $STATUS -eq 0 ]
     then
@@ -160,12 +90,10 @@ then
         exit 1
     fi
 else
-    # Something else than a "safe" pull request
+    # Something else than a "safe" pull request, without any secure vars available, no sonar possible
     mvn -B -e -V \
         -Pall-tests \
         -Dmaven.javadoc.skip=false \
         $(getDockerParamsIfNeeded) \
-        -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
-        license:download-licenses \
-        verify javadoc:javadoc $modules -amd
+        clean license:download-licenses verify javadoc:javadoc
 fi
