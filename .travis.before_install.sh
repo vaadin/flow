19b903c0768db364e0c722601fa9c1a4a1325f9b
Kirill Bulatov <mail4score@gmail.com>
2017-09-08 23:50:36 +0300

    Add a possibility to fix Docker Selenoid image version.

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# USE_SELENOID == use Docker and Selenoid to run IT tests in personal hub
+# SELENOID_VERSION == specify a particular Docker Selenoid image version
+
 # Disable all Travis default repositories
 sed -i "s/activeByDefault>true</activeByDefault>false</g"  ~/.m2/settings.xml
 
@@ -12,7 +15,13 @@ then
     # Parse browser config using jq and download all docker images
     cat ./browsers.json | jq -r '..|.image?|strings' | xargs -I{} docker pull {}
 
+    # Determine Docker Selenoid image version, full version list: https://hub.docker.com/r/aerokube/selenoid/tags/
+    selenoidVersion=${SELENOID_VERSION}
+    if [ -z ${selenoidVersion} ]; then
+        selenoidVersion="latest-release"
+    fi
+
     # Run Selenoid (https://github.com/aerokube/selenoid)
     docker run -d -p 4444:4444 -v `pwd`:/etc/selenoid:ro \
-          -v /var/run/docker.sock:/var/run/docker.sock aerokube/selenoid:1.3.6 -limit 10
+          -v /var/run/docker.sock:/var/run/docker.sock aerokube/selenoid:${selenoidVersion}
 fi
