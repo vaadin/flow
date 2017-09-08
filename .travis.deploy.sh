baa74e913d18d567fca8c532a7c5bc19ff07a37e
Kirill Bulatov <mail4score@gmail.com>
2017-09-08 22:53:05 +0300

    vaadin.com jar deployment repo url can be customized.

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-DEPLOYMENT_REPO="hummingbird-snapshot::default::https://repo.vaadin.com/nexus/content/repositories/flow-snapshots/"
-
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
+    DEPLOYMENT_REPO="hummingbird-snapshot::default::https://repo.vaadin.com/nexus/content/repositories/flow-snapshots/"
     mvn -B -e -V -DskipTests -DskipITs -Djetty.skip -Dgatling.skip -DaltDeploymentRepository=$DEPLOYMENT_REPO deploy --settings settings.xml
 
     # Copy demo-flow-components to the server
