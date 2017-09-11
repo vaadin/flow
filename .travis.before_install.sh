722fb56fbf8afc9f4a858df4b5242a7a17b4d68f
Kirill Bulatov <mail4score@gmail.com>
2017-09-11 11:09:15 +0300

    Make it possible to specify Chrome default url.

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -2,6 +2,7 @@
 
 # USE_SELENOID == use Docker and Selenoid to run IT tests in personal hub
 # SELENOID_VERSION == specify a particular Docker Selenoid image version
+# CHROME_DEFAULT_URL == specify a default url that will be used to download Chrome when local distribution cannot be found
 
 # Disable all Travis default repositories
 sed -i "s/activeByDefault>true</activeByDefault>false</g"  ~/.m2/settings.xml
@@ -29,10 +30,16 @@ else
     chrome_package_name=google-chrome-stable_current_amd64.deb
     mkdir -p ${custom_chrome_dir}
     scp -o "StrictHostKeyChecking no" -P 5177 dev@virtuallypreinstalled.com:/home/dev/custom_chrome/${chrome_package_name} ${custom_chrome_dir}
+
     if [ ! -f ${custom_chrome_dir}/${chrome_package_name} ]; then
         echo "Was not able to download custom Chrome binary from the remote server, using the official distribution"
-        wget -O ${custom_chrome_dir} http://dl.google.com/dl/linux/direct/google-chrome-stable_current_amd64.deb
+        chrome_default_url=${CHROME_DEFAULT_URL}
+        if [ -z ${selenoidVersion} ]; then
+            chrome_default_url="http://dl.google.com/dl/linux/direct/google-chrome-stable_current_amd64.deb"
+        fi
+        wget -O ${custom_chrome_dir} ${chrome_default_url}
     fi
+
     sudo dpkg -i --force-all ${custom_chrome_dir}/${chrome_package_name}
     rm -rfv ${custom_chrome_dir}
 fi
