31deed10e5c2f5b25690e4fbf6858d0ca36f1579
Gilberto Torrezan <gilberto@vaadin.com>
2017-09-12 11:11:37 +0300

    Update renderers to properly use RouterLayout

--- flow-server/src/main/java/com/vaadin/flow/router/RouteTargetRenderer.java
+++ flow-server/src/main/java/com/vaadin/flow/router/RouteTargetRenderer.java
@@ -16,12 +16,16 @@
 package com.vaadin.flow.router;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import com.vaadin.annotations.Route;
 import com.vaadin.annotations.Title;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.HasElement;
 import com.vaadin.ui.UI;
 import com.vaadin.util.ReflectTools;
 
@@ -45,6 +49,25 @@ public abstract class RouteTargetRenderer implements NavigationHandler {
             NavigationEvent event);
 
     /**
+     * Gets the router layout types to show for the given route target type,
+     * starting from the parent layout immediately wrapping the route target
+     * type.
+     *
+     * @see #getRouteTargetType(NavigationEvent)
+     *
+     * @param event
+     *            the event which triggered showing of the component and its
+     *            parents
+     * @param routeTargetType
+     *            component type to show
+     *
+     * @return a list of parent {@link RouterLayout} types, not
+     *         <code>null</code>
+     */
+    public abstract List<Class<? extends RouterLayout>> getRouterLayoutTypes(
+            NavigationEvent event, Class<? extends Component> routeTargetType);
+
+    /**
      * Gets the component instance to use for the given type and the
      * corresponding navigation event.
      * <p>
@@ -60,9 +83,16 @@ public abstract class RouteTargetRenderer implements NavigationHandler {
      *            the navigation event that uses the route target
      * @return an instance of the route target component
      */
-    protected <T extends Component> T getRouteTarget(Class<T> routeTargetType,
+    @SuppressWarnings("unchecked")
+    protected <T extends HasElement> T getRouteTarget(Class<T> routeTargetType,
             NavigationEvent event) {
-        return ReflectTools.createInstance(routeTargetType);
+        Optional<HasElement> currentInstance = event.getUI().getInternals()
+                .getActiveRouterTargetsChain().stream()
+                .filter(component -> component.getClass()
+                        .equals(routeTargetType))
+                .findAny();
+        return (T) currentInstance
+                .orElseGet(() -> ReflectTools.createInstance(routeTargetType));
     }
 
     @Override
@@ -70,23 +100,57 @@ public abstract class RouteTargetRenderer implements NavigationHandler {
         UI ui = event.getUI();
 
         Class<? extends Component> routeTargetType = getRouteTargetType(event);
+        List<Class<? extends RouterLayout>> routeLayoutTypes = getRouterLayoutTypes(
+                event, routeTargetType);
 
         assert routeTargetType != null;
+        assert routeLayoutTypes != null;
+
+        checkForDuplicates(routeTargetType, routeLayoutTypes);
 
         Component componentInstance = getRouteTarget(routeTargetType, event);
 
-        List<Component> routeTargetChain = new ArrayList<>();
-        routeTargetChain.add(componentInstance);
+        List<HasElement> chain = new ArrayList<>();
+        chain.add(componentInstance);
 
-        NewLocationChangeEvent locationChangeEvent = createEvent(event,
-                routeTargetChain);
+        for (Class<? extends RouterLayout> parentType : routeLayoutTypes) {
+            chain.add(getRouteTarget(parentType, event));
+        }
+
+        NewLocationChangeEvent locationChangeEvent = createEvent(event, chain);
+
+        @SuppressWarnings("unchecked")
+        List<RouterLayout> routerLayouts = (List<RouterLayout>) (List<?>) chain
+                .subList(1, chain.size());
 
         ui.getInternals().showRouteTarget(event.getLocation(),
-                componentInstance, Collections.emptyList());
+                componentInstance, routerLayouts);
 
-        updatePageTitle(event, routeTargetType);
+        updatePageTitle(event, routeTargetType, routeLayoutTypes);
 
         return locationChangeEvent.getStatusCode();
+    }
+
+    /**
+     * Checks that the same component type is not used in multiple parts of a
+     * route chain.
+     *
+     * @param routeTargetType
+     *            the actual component in the route chain
+     * @param routeLayoutTypes
+     *            the parent types in the route chain
+     */
+    protected static void checkForDuplicates(
+            Class<? extends Component> routeTargetType,
+            Collection<Class<? extends RouterLayout>> routeLayoutTypes) {
+        Set<Class<?>> duplicateCheck = new HashSet<>();
+        duplicateCheck.add(routeTargetType);
+        for (Class<?> parentType : routeLayoutTypes) {
+            if (!duplicateCheck.add(parentType)) {
+                throw new IllegalArgumentException(
+                        parentType + " is used in multiple locations");
+            }
+        }
     }
 
     /**
@@ -100,9 +164,18 @@ public abstract class RouteTargetRenderer implements NavigationHandler {
      *            the type of the route target
      */
     protected void updatePageTitle(NavigationEvent navigationEvent,
-            Class<? extends Component> routeTargetType) {
+            Class<? extends Component> routeTargetType,
+            List<Class<? extends RouterLayout>> routeLayoutTypes) {
 
         Title annotation = routeTargetType.getAnnotation(Title.class);
+        if (annotation == null) {
+            for (Class<?> clazz : routeLayoutTypes) {
+                annotation = clazz.getAnnotation(Title.class);
+                if (annotation != null) {
+                    break;
+                }
+            }
+        }
         if (annotation == null || annotation.value() == null) {
             navigationEvent.getUI().getPage().setTitle("");
         } else {
@@ -111,7 +184,7 @@ public abstract class RouteTargetRenderer implements NavigationHandler {
     }
 
     private NewLocationChangeEvent createEvent(NavigationEvent event,
-            List<Component> routeTargetChain) {
+            List<HasElement> routeTargetChain) {
         return new NewLocationChangeEvent(event.getSource(), event.getUI(),
                 event.getTrigger(), event.getLocation());
     }
