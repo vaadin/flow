2ac58096448606f60794f518883f15130553d38e
Pekka Maanpaa <pekkamaa@vaadin.com>
2017-09-19 12:03:30 +0300

    Add new component class Icon and VaadinIcons enum
    Icon is a component based on the <iron-icon> webcomponent that imports the vaadin-icons collection. VaadinIcons is an enumeration of all the different icons in the vaadin-icons collection

---
+++ flow-components-parent/flow-components/src/main/java/com/vaadin/ui/Icon.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.ui;
+
+import com.vaadin.annotations.HtmlImport;
+import com.vaadin.annotations.Tag;
+import com.vaadin.flow.dom.ElementConstants;
+
+/**
+ * Component for displaying an icon from the
+ * <a href="https://vaadin.com/icons">Vaadin Icons</a> collection.
+ * 
+ * @author Vaadin Ltd
+ */
+@Tag("iron-icon")
+@HtmlImport("frontend://bower_components/vaadin-icons/vaadin-icons.html")
+public class Icon extends Component implements HasStyle {
+
+    private static final String ICON_PROPERTY_NAME = "icon";
+    private static final String ICON_COLLECTION_NAME = "vaadin";
+
+    /**
+     * Creates an Icon component that displays a Vaadin logo.
+     */
+    public Icon() {
+        this(VaadinIcons.VAADIN_H);
+    }
+
+    /**
+     * Creates an Icon component that displays the given icon from
+     * {@link VaadinIcons}.
+     * 
+     * @param icon
+     *            the icon to display
+     */
+    public Icon(VaadinIcons icon) {
+        // iron-icon's icon-property uses the format "collection:name",
+        // eg. icon="vaadin:arrow-down"
+        getElement().setProperty(ICON_PROPERTY_NAME, ICON_COLLECTION_NAME + ":"
+                + icon.name().toLowerCase().replace('_', '-'));
+    }
+
+    /**
+     * Sets the width and the height of the icon.
+     * <p>
+     * The size should be in a format understood by the browser, e.g. "100px" or
+     * "2.5em".
+     *
+     * @param size
+     *            the size to set
+     */
+    public void setSize(String size) {
+        if (size == null) {
+            getStyle().remove(ElementConstants.STYLE_WIDTH);
+            getStyle().remove(ElementConstants.STYLE_HEIGHT);
+        } else {
+            getStyle().set(ElementConstants.STYLE_WIDTH, size);
+            getStyle().set(ElementConstants.STYLE_HEIGHT, size);
+        }
+    }
+
+    /**
+     * Sets the fill color of the icon.
+     * <p>
+     * The color should be in a format understood by the browser, e.g. "orange",
+     * "#FF9E2C" or "rgb(255, 158, 44)".
+     * 
+     * @param color
+     *            the fill color to set
+     */
+    public void setColor(String color) {
+        if (color == null) {
+            getStyle().remove(ElementConstants.STYLE_COLOR);
+        } else {
+            getStyle().set(ElementConstants.STYLE_COLOR, color);
+        }
+    }
+
+    /**
+     * Gets the fill color of this icon as a String.
+     * 
+     * @return the fill color of the icon, or <code>null</code> if the color has
+     *         not been set
+     */
+    public String getColor() {
+        return getStyle().get(ElementConstants.STYLE_COLOR);
+    }
+}
