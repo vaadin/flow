1c5fb5c82744ef57d4a5e92d2587fe542c4911a1
Kirill Bulatov <kirill@vaadin.com>
2017-10-09 17:22:13 +0300

    Test.

--- flow-test-util/src/main/java/com/vaadin/flow/testutil/TestBenchHelpers.java
+++ flow-test-util/src/main/java/com/vaadin/flow/testutil/TestBenchHelpers.java
@@ -92,7 +92,7 @@ public class TestBenchHelpers extends ParallelTest {
         waitUntil(ExpectedConditions.not(condition), timeoutInSeconds);
     }
 
-    protected void waitForElementPresent(final By by) {
+    protected void waitForElementPresent(By by) {
         waitUntil(ExpectedConditions.presenceOfElementLocated(by));
     }
 
