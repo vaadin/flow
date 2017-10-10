b796bd41438dc4af29a93b5d29bd1e4eeec4f613
Gilberto Torrezan <gilberto@vaadin.com>
2017-10-10 16:12:46 +0300

    Fix javadoc

--- flow-server/src/main/java/com/vaadin/ui/renderers/TemplateRenderer.java
+++ flow-server/src/main/java/com/vaadin/ui/renderers/TemplateRenderer.java
@@ -35,102 +35,105 @@ import com.vaadin.util.JsonSerializer;
  *            the type of the input object used inside the template
  * 
  * @see ValueProvider
- * @see https://www.polymer-project.org/2.0/docs/devguide/templates
+ * @see <a href=
+ *      "https://www.polymer-project.org/2.0/docs/devguide/templates">https://www.polymer-project.org/2.0/docs/devguide/templates</a>
  */
 public class TemplateRenderer<SOURCE> implements Serializable {
 
-	private String template;
-	private Map<String, ValueProvider<SOURCE, ?>> valueProviders;
+    private String template;
+    private Map<String, ValueProvider<SOURCE, ?>> valueProviders;
 
-	/**
-	 * Creates a new TemplateRenderer based on the provided template. The
-	 * template accepts anything that is allowed inside a {@code <template>}
-	 * element, and works with Polymer data binding syntax.
-	 * <p>
-	 * Examples:
-	 * 
-	 * <pre>
-	 * {@code
-	 * // Prints the index of the item inside a repeating list
-	 * TemplateRenderer.of("[[index]]");
-	 * 
-	 *  // Prints the property of an item
-	 * TemplateRenderer.of("<div>Property: [[item.property]]</div>");
-	 * }
-	 * </pre>
-	 * 
-	 * @param template
-	 *            the template used to render items, not <code>null</code>
-	 * @return an initialized TemplateRenderer
-	 * @see TemplateRenderer#withProperty(String, ValueProvider)
-	 */
-	public static <T> TemplateRenderer<T> of(String template) {
-		Objects.requireNonNull(template);
-		TemplateRenderer<T> renderer = new TemplateRenderer<>();
-		renderer.template = template;
-		return renderer;
-	}
+    /**
+     * Creates a new TemplateRenderer based on the provided template. The
+     * template accepts anything that is allowed inside a {@code <template>}
+     * element, and works with Polymer data binding syntax.
+     * <p>
+     * Examples:
+     * 
+     * <pre>
+     * {@code
+     * // Prints the index of the item inside a repeating list
+     * TemplateRenderer.of("[[index]]");
+     * 
+     *  // Prints the property of an item
+     * TemplateRenderer.of("<div>Property: [[item.property]]</div>");
+     * }
+     * </pre>
+     * 
+     * @param <SOURCE>
+     *            the type of the input object used inside the template
+     * @param template
+     *            the template used to render items, not <code>null</code>
+     * @return an initialized TemplateRenderer
+     * @see TemplateRenderer#withProperty(String, ValueProvider)
+     */
+    public static <SOURCE> TemplateRenderer<SOURCE> of(String template) {
+        Objects.requireNonNull(template);
+        TemplateRenderer<SOURCE> renderer = new TemplateRenderer<>();
+        renderer.template = template;
+        return renderer;
+    }
 
-	/**
-	 * Sets a property to be used inside the template. Each property is
-	 * referenced inside the template by using the {@code [[item.property]]}
-	 * syntax.
-	 * <p>
-	 * Examples:
-	 * 
-	 * <pre>
-	 * {@code
-	 * // Regular property
-	 * TemplateRenderer.<Person> of("<div>Name: [[item.name]]</div>")
-	 * 			.withProperty("name", Person::getName);
-	 * 
-	 * // Property that uses a bean. Note that in this case the entire "Adress" object will be sent to the template.
-	 * // Note that even properties of the bean which are not used in the template are sent to the client, so use
-	 * // this feature with caution.
-	 * TemplateRenderer.<Person> of("<span>Street: [[item.address.street]]</span>")
-	 * 			.withProperty("address", Person::getAddress); 
-	 * 
-	 * // In this case only the street field inside the Address object is sent
-	 * TemplateRenderer.<Person> of("<span>Street: [[item.street]]</span>")
-	 * 			.withProperty("street", person -> person.getAddress().getStreet());
-	 * }
-	 * </pre>
-	 * 
-	 * Any types supported by the {@link JsonSerializer} are valid types for the
-	 * TemplateRenderer.
-	 * 
-	 * @param property
-	 *            the name of the property used inside the template
-	 * @param provider
-	 *            a {@link ValueProvider} that provides the actual
-	 * @return this instance for method chaining
-	 */
-	public TemplateRenderer<SOURCE> withProperty(String property,
-			ValueProvider<SOURCE, ?> provider) {
-		valueProviders.put(property, provider);
-		return this;
-	}
+    /**
+     * Sets a property to be used inside the template. Each property is
+     * referenced inside the template by using the {@code [[item.property]]}
+     * syntax.
+     * <p>
+     * Examples:
+     * 
+     * <pre>
+     * {@code
+     * // Regular property
+     * TemplateRenderer.<Person> of("<div>Name: [[item.name]]</div>")
+     * 			.withProperty("name", Person::getName);
+     * 
+     * // Property that uses a bean. Note that in this case the entire "Adress" object will be sent to the template.
+     * // Note that even properties of the bean which are not used in the template are sent to the client, so use
+     * // this feature with caution.
+     * TemplateRenderer.<Person> of("<span>Street: [[item.address.street]]</span>")
+     * 			.withProperty("address", Person::getAddress); 
+     * 
+     * // In this case only the street field inside the Address object is sent
+     * TemplateRenderer.<Person> of("<span>Street: [[item.street]]</span>")
+     * 			.withProperty("street", person -> person.getAddress().getStreet());
+     * }
+     * </pre>
+     * 
+     * Any types supported by the {@link JsonSerializer} are valid types for the
+     * TemplateRenderer.
+     * 
+     * @param property
+     *            the name of the property used inside the template
+     * @param provider
+     *            a {@link ValueProvider} that provides the actual
+     * @return this instance for method chaining
+     */
+    public TemplateRenderer<SOURCE> withProperty(String property,
+            ValueProvider<SOURCE, ?> provider) {
+        valueProviders.put(property, provider);
+        return this;
+    }
 
-	protected TemplateRenderer() {
-		valueProviders = new HashMap<>();
-	}
+    protected TemplateRenderer() {
+        valueProviders = new HashMap<>();
+    }
 
-	/**
-	 * Gets the template set for this renderer.
-	 * 
-	 * @return the template, never <code>null</code>
-	 */
-	public String getTemplate() {
-		return template;
-	}
+    /**
+     * Gets the template set for this renderer.
+     * 
+     * @return the template, never <code>null</code>
+     */
+    public String getTemplate() {
+        return template;
+    }
 
-	/**
-	 * Gets the property mapped to {@link ValueProvider}s in this renderer. The
-	 * returned map is immutable.
-	 * 
-	 * @return the mapped properties, never <code>null</code>
-	 */
-	public Map<String, ValueProvider<SOURCE, ?>> getValueProviders() {
-		return Collections.unmodifiableMap(valueProviders);
-	}
+    /**
+     * Gets the property mapped to {@link ValueProvider}s in this renderer. The
+     * returned map is immutable.
+     * 
+     * @return the mapped properties, never <code>null</code>
+     */
+    public Map<String, ValueProvider<SOURCE, ?>> getValueProviders() {
+        return Collections.unmodifiableMap(valueProviders);
+    }
 }
