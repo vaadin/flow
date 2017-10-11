071f8b372b167347907d452e5d80e4e35854de7b
Mikael Grankvist <mikael.grankvist@vaadin.com>
2017-10-11 15:36:10 +0300

    Add import that was removed in merge.

--- flow-server/src/main/java/com/vaadin/router/event/BeforeNavigationEvent.java
+++ flow-server/src/main/java/com/vaadin/router/event/BeforeNavigationEvent.java
@@ -19,6 +19,7 @@ import java.util.Collections;
 import java.util.EventObject;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.vaadin.router.ErrorParameter;
