a03909897d552280dc53a4ed5280d1ffa667d6d7
ZheSun88 <zhe@vaadin.com>
2017-10-11 10:41:08 +0300

    Reimplement uncompleted methods
    implement BeforeNavigation
    implement hasUrlParameter
    implement hasErrorParameter

--- flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/servlet/ErrorView.java
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/servlet/ErrorView.java
@@ -1,21 +1,32 @@
 package com.vaadin.flow.uitest.servlet;
 
 import com.vaadin.flow.dom.ElementFactory;
+import com.vaadin.router.ErrorParameter;
+import com.vaadin.router.HasErrorParameter;
+import com.vaadin.router.Route;
+import com.vaadin.router.event.BeforeNavigationEvent;
+import com.vaadin.router.event.BeforeNavigationListener;
 import com.vaadin.ui.html.Div;
-import com.vaadin.flow.router.LocationChangeEvent;
-import com.vaadin.flow.router.View;
 
-public class ErrorView extends Div implements View {
+@Route(value = "ErrorView")
+public class ErrorView extends Div implements BeforeNavigationListener,
+        HasErrorParameter<ClassNotFoundException> {
 
     @Override
-    public void onLocationChange(LocationChangeEvent locationChangeEvent) {
-        getElement()
-                .appendChild(
-                        ElementFactory.createDiv(
-                                "This is the error view. Next element contains the error path "),
-                        ElementFactory
-                                .createDiv(locationChangeEvent.getLocation()
-                                        .getPath())
-                                .setAttribute("id", "error-path"));
+    public void beforeNavigation(BeforeNavigationEvent event) {
+        getElement().appendChild(ElementFactory.createDiv(
+                "This is the error view. Next element contains the error path "),
+                ElementFactory.createDiv(event.getLocation().getPath())
+                        .setAttribute("id", "error-path"));
+    }
+
+    @Override
+    public int setErrorParameter(BeforeNavigationEvent event,
+            ErrorParameter<ClassNotFoundException> parameter) {
+        getElement().appendChild(ElementFactory.createDiv(
+                "This is the error view. Next element contains the error path "),
+                ElementFactory.createDiv(event.getLocation().getPath())
+                        .setAttribute("id", "error-path"));
+        return 0;
     }
 }
