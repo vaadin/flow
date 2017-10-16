3d83e7aceac55c15ddbee3a77ccb5d142dcd70a8
Gilberto Torrezan <gilberto@vaadin.com>
2017-10-16 16:56:56 +0300

    Add a new constructor for Grid allowing to set the pageSize

---
+++ flow-components-parent/flow-components-test/src/test/java/com/vaadin/flow/tests/components/grid/GridPageSizeViewIT.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.tests.components.grid;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.tests.components.AbstractComponentIT;
+
+public class GridPageSizeViewIT extends AbstractComponentIT {
+
+    @Test
+    public void gridWithPageSize10() {
+        open();
+
+        waitForElementPresent(By.id("query-info"));
+        WebElement info = findElement(By.id("query-info"));
+
+        // the webcomponent use 3 * the page size as the query limit
+        waitUntil(driver -> info.getText().endsWith("Query limit: 30"));
+    }
+
+}
