5be773ca0ef10ea070cca1b43e478d7ca8d2b4f3
Mikael Grankvist <mikael.grankvist@vaadin.com>
2017-10-17 09:43:21 +0300

    Target a view as we can only have one root view.

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/RouterSessionExpirationIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/RouterSessionExpirationIT.java
@@ -30,7 +30,7 @@ public class RouterSessionExpirationIT extends ChromeBrowserTest {
 
     @Test
     public void navigationAfterSessionExpired() {
-        open();
+        open("NormalView");
 
         navigateToAnotherView();
         String sessionId = getSessionId();
@@ -44,7 +44,7 @@ public class RouterSessionExpirationIT extends ChromeBrowserTest {
 
     @Test
     public void navigationAfterInternalError() {
-        open();
+        open("NormalView");
 
         navigateToAnotherView();
         String sessionId = getSessionId();
