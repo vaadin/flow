590ff554293e210035bce44cb127bf236d2429ba
ZheSun88 <zhe@vaadin.com>
2017-10-18 14:49:00 +0300

    Remove the ordered-layout folder
    add layout dependency to pom;

--- flow-components-parent/demo-flow-components/src/main/webapp/frontend/bower_components/vaadin-ordered-layout/test/common.js
+++
@@ -1,5 +0,0 @@
-window.positionEquals = (node, top, right, bottom, left) => {
-  const rect = node.getBoundingClientRect();
-  return rect.top === top && rect.bottom === bottom &&
-          rect.left === left && rect.right === right;
-};
