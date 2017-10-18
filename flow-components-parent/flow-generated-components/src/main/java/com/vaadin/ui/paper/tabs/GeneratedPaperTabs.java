d712c47d6d3285c4698579c53e8ba6a4186e6318
Kirill Bulatov <mail4score@gmail.com>
2017-10-18 15:15:39 +0300

    Update webcomponents to correspond to vaadin-bom versions. (#2712)

--- flow-components-parent/flow-generated-components/src/main/java/com/vaadin/ui/paper/tabs/GeneratedPaperTabs.java
+++ flow-components-parent/flow-generated-components/src/main/java/com/vaadin/ui/paper/tabs/GeneratedPaperTabs.java
@@ -1149,7 +1149,7 @@ public class GeneratedPaperTabs<R extends GeneratedPaperTabs<R>>
 	 * @param value
 	 *            the value to select.
 	 */
-	public void select(double value) {
+	public void select(java.lang.String value) {
 		getElement().callFunction("select", value);
 	}
 
@@ -1166,7 +1166,7 @@ public class GeneratedPaperTabs<R extends GeneratedPaperTabs<R>>
 	 * @param value
 	 *            the value to select.
 	 */
-	public void select(java.lang.String value) {
+	public void select(double value) {
 		getElement().callFunction("select", value);
 	}
 
