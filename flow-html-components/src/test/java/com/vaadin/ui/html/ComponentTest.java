8d4f52193b2751ca312a9364429587141a8fea28
Giovanni Lovato <heruan@aldu.net>
2017-10-19 19:33:57 +0200

    Add missing base HTML elements (#2719)

--- flow-html-components/src/test/java/com/vaadin/ui/html/ComponentTest.java
+++ flow-html-components/src/test/java/com/vaadin/ui/html/ComponentTest.java
@@ -90,8 +90,14 @@ public abstract class ComponentTest {
 
     protected <U> void addProperty(String propertyName, Class<U> propertyType,
             U defaultValue, boolean isOptional, boolean removeDefault) {
+        addProperty(propertyName, propertyType, defaultValue, null, isOptional,
+                removeDefault);;
+    }
+
+    protected <U> void addProperty(String propertyName, Class<U> propertyType,
+            U defaultValue, U otherValue, boolean isOptional, boolean removeDefault) {
         properties.add(new ComponentProperty(getComponent().getClass(),
-                propertyName, propertyType, defaultValue, isOptional,
+                propertyName, propertyType, defaultValue, otherValue, isOptional,
                 removeDefault));
     }
 
