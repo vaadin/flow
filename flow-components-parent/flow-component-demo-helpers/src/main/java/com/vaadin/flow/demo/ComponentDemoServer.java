3f0032ef6e1f4e2b4a4a1f288bb6791d3db40c08
Pekka Maanpaa <pekkamaa@vaadin.com>
2017-10-20 17:20:21 +0300

    Add a new module flow-component-demo-helpers
    It's a child-module of flow-components-parent. It contains all the common stuff for creating and running component-demos, like the demo-server and source-code parsing logic. DemoView should be extended for creating component-demos.

---
+++ flow-components-parent/flow-component-demo-helpers/src/main/java/com/vaadin/flow/demo/ComponentDemoServer.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo;
+
+import java.io.File;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.Configuration.ClassList;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+
+import com.vaadin.server.VaadinServlet;
+
+/**
+ * Server class for running component demos and executing integration tests.
+ * <p>
+ * The {@link #startServer()} method in this class opens a web server to
+ * http://localhost:9998/ to serve all your demo views annotated with
+ * <code>@Route</code> for development and integration testing. This class can
+ * be extended for additional configuration.
+ * 
+ * @author Vaadin Ltd
+ */
+public class ComponentDemoServer {
+
+    private static final String WEB_APP_PATH = "webapp";
+
+    private static final String TEST_CLASSES_PATH = ".*/target/test-classes/";
+
+    private static final String JAR_PATTERN = ".*\\.jar$";
+
+    /**
+     * Starts a web server to the port defined by {@link #getPort()}. It serves
+     * the test UIs annotated with <code>@Route</code>.
+     * 
+     * @return the server object
+     */
+    public Server startServer() throws Exception {
+
+        Server server = new Server();
+
+        final ServerConnector connector = new ServerConnector(server);
+        connector.setPort(getPort());
+        server.setConnectors(new Connector[] { connector });
+
+        WebAppContext context = new WebAppContext();
+
+        VaadinServlet vaadinServlet = createServlet();
+        ServletHolder servletHolder = new ServletHolder(vaadinServlet);
+        context.addServlet(servletHolder, "/");
+
+        File file = new File(WEB_APP_PATH);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        context.setWar(file.getPath());
+
+        context.setContextPath("/");
+
+        ClassList classlist = ClassList.setServerDefault(server);
+        classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+                "org.eclipse.jetty.annotations.AnnotationConfiguration");
+
+        // Enable annotation scanning for uitest classes.
+        // Enable scanning for resources inside jar-files.
+        context.setAttribute(WebInfConfiguration.CONTAINER_JAR_PATTERN,
+                TEST_CLASSES_PATH + "|" + JAR_PATTERN);
+
+        configure(context, server);
+        server.setHandler(context);
+        server.start();
+        return server;
+    }
+
+    /**
+     * Creates and returns the {@link VaadinServlet} to be served by this
+     * server.
+     * 
+     * @return the servlet to be served by this server
+     */
+    protected VaadinServlet createServlet() {
+        return new DemoServlet();
+    }
+
+    /**
+     * Gets the port number to which this server will be connected.
+     * 
+     * @return the port number to which this server will be connected.
+     */
+    protected int getPort() {
+        return 9998;
+    }
+
+    /**
+     * Hook for additional configuration to perform before starting the server.
+     *
+     * @param context
+     *            the context
+     * @param server
+     *            the server
+     */
+    protected void configure(WebAppContext context, Server server) {
+
+    }
+
+}
---
+++ flow-components-parent/flow-component-demo-helpers/src/main/java/com/vaadin/flow/demo/DemoView.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.common.HasComponents;
+import com.vaadin.ui.common.JavaScript;
+import com.vaadin.ui.common.StyleSheet;
+import com.vaadin.ui.event.AttachEvent;
+import com.vaadin.ui.html.Div;
+import com.vaadin.ui.html.H3;
+
+/**
+ * Base class for all the Views that demo some component.
+ */
+@StyleSheet("src/css/demo.css")
+@JavaScript("src/script/prism.js")
+public abstract class DemoView extends Div implements HasComponents {
+
+    private Map<String, List<SourceCodeExample>> sourceCodeExamples = new HashMap<>();
+
+    protected DemoView() {
+        try {
+            getElement().setAttribute("class", "demo-view");
+
+            populateSources();
+            initView();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        UI.getCurrent().getPage().executeJavaScript("Prism.highlightAll();");
+    }
+
+    /**
+     * Method run where the actual view builds its contents.
+     */
+    protected abstract void initView();
+
+    /**
+     * When called the view should populate the given SourceContainer with
+     * sample source code to be shown.
+     */
+    public void populateSources() {
+        SourceContentResolver.getSourceCodeExamplesForClass(getClass())
+                .forEach(this::putSourceCode);
+    }
+
+    private void putSourceCode(SourceCodeExample example) {
+        String heading = example.getHeading();
+        List<SourceCodeExample> list = sourceCodeExamples
+                .computeIfAbsent(heading, key -> new ArrayList<>());
+        list.add(example);
+    }
+
+    /**
+     * Create and add a new component card to the view. It automatically adds
+     * any source code examples with the same heading to the bottom of the card.
+     *
+     * @param heading
+     *            the header text of the card, that is added to the layout. If
+     *            <code>null</code> or empty, the header is not added
+     *
+     * @param components
+     *            components to add on creation. If <code>null</code> or empty,
+     *            the card is created without the components inside
+     * @return created component container card.
+     */
+    public Card addCard(String heading, Component... components) {
+        if (heading != null && !heading.isEmpty()) {
+            add(new H3(heading));
+        }
+
+        Card card = new Card();
+        if (components != null && components.length > 0) {
+            card.add(components);
+        }
+
+        List<SourceCodeExample> list = sourceCodeExamples.get(heading);
+        if (list != null) {
+            list.stream().map(this::createSourceContent).forEach(card::add);
+        }
+
+        add(card);
+
+        return card;
+    }
+
+    private SourceContent createSourceContent(
+            SourceCodeExample sourceCodeExample) {
+        SourceContent content = new SourceContent();
+        String sourceString = sourceCodeExample.getSourceCode();
+        switch (sourceCodeExample.getSourceType()) {
+        case CSS:
+            content.addCss(sourceString);
+            break;
+        case JAVA:
+            content.addCode(sourceString);
+            break;
+        case UNDEFINED:
+        default:
+            content.addCode(sourceString);
+            break;
+        }
+        return content;
+    }
+}
