beffcceeaf674fb6bcc7f7998d24b82ed5000261
Aleksi Hietanen <aleksi@vaadin.com>
2017-10-24 16:21:01 +0300

    Ignore Grid.Column

--- flow-components-parent/flow-components-test/src/test/java/com/vaadin/flow/tests/components/MixinTest.java
+++ flow-components-parent/flow-components-test/src/test/java/com/vaadin/flow/tests/components/MixinTest.java
@@ -30,6 +30,7 @@ import com.vaadin.ui.common.HasSize;
 import com.vaadin.ui.common.HasStyle;
 import com.vaadin.ui.dialog.Dialog;
 import com.vaadin.ui.formlayout.FormLayout.FormItem;
+import com.vaadin.ui.grid.Grid;
 import com.vaadin.ui.grid.GridSelectionColumn;
 import com.vaadin.ui.icon.Icon;
 
@@ -49,7 +50,8 @@ public class MixinTest {
                         "com.vaadin.ui.polymertemplate"));
         CLASS_WHITELIST
                 .addAll(Arrays.asList(Icon.class, FormItem.class,
-                        GridSelectionColumn.class, Dialog.class));
+                        GridSelectionColumn.class, Dialog.class,
+                        Grid.Column.class));
         REQUIRED_INTERFACES.addAll(
                 Arrays.asList(HasSize.class, HasStyle.class));
     }
