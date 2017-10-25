1e29377c2f68a59c6cd7d7361b5a8dda98ecd1a3
Denis Anisimov <denis@vaadin.com>
2017-10-25 09:28:12 +0300

    Allow Spring add-on co-exist with other Spring services with overlapping path
    Fix for #2660

---
+++ flow-spring-addon/src/main/java/com/vaadin/spring/RootMappedCondition.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.spring;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * @author Vaadin Ltd
+ *
+ */
+public class RootMappedCondition implements Condition {
+
+    public static final String URL_MAPPING_PROPERTY = "vaadin.urlMapping";
+
+    @Override
+    public boolean matches(ConditionContext context,
+            AnnotatedTypeMetadata metadata) {
+        return isRootMapping(
+                context.getEnvironment().getProperty(URL_MAPPING_PROPERTY));
+    }
+
+    public static boolean isRootMapping(String mapping) {
+        if (mapping == null) {
+            return true;
+        }
+        return mapping.trim().replaceAll("(/\\**)?$", "").isEmpty();
+    }
+}
--- flow-tests/test-spring-war/src/main/java/com/vaadin/flow/spring/TestServletInitializer.java
+++ flow-tests/test-spring-war/src/main/java/com/vaadin/flow/spring/TestServletInitializer.java
@@ -17,8 +17,14 @@ package com.vaadin.flow.spring;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@Configuration
+@EnableWebSecurity
 public class TestServletInitializer extends SpringBootServletInitializer {
 
 }
