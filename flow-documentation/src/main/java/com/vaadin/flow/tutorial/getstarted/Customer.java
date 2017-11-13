18be6823f1fcc20db3bee832dda2cab75e2afcd8
Henri Muurimaa <henri@vaadin.com>
2017-11-13 14:19:43 +0200

    Add example code checker files

---
+++ flow-documentation/src/main/java/com/vaadin/flow/tutorial/getstarted/Customer.java
@@ -0,0 +1,122 @@
+package com.vaadin.flow.tutorial.getstarted;
+import java.io.Serializable;
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+
+/**
+ * A entity object, like in any other Java application. In a typical real world
+ * application this could for example be a JPA entity.
+ */
+@SuppressWarnings("serial")
+@CodeFor("get-started.asciidoc")
+public class Customer implements Serializable, Cloneable {
+
+	private Long id;
+
+	private String firstName = "";
+
+	private String lastName = "";
+
+	private CustomerStatus status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Get the value of status
+	 *
+	 * @return the value of status
+	 */
+	public CustomerStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * Set the value of status
+	 *
+	 * @param status
+	 *            new value of status
+	 */
+	public void setStatus(CustomerStatus status) {
+		this.status = status;
+	}
+
+	/**
+	 * Get the value of lastName
+	 *
+	 * @return the value of lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * Set the value of lastName
+	 *
+	 * @param lastName
+	 *            new value of lastName
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * Get the value of firstName
+	 *
+	 * @return the value of firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * Set the value of firstName
+	 *
+	 * @param firstName
+	 *            new value of firstName
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public boolean isPersisted() {
+		return id != null;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (this.id == null) {
+			return false;
+		}
+
+		if (obj instanceof Customer && obj.getClass().equals(getClass())) {
+			return this.id.equals(((Customer) obj).id);
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 43 * hash + (id == null ? 0 : id.hashCode());
+		return hash;
+	}
+
+	@Override
+	public Customer clone() throws CloneNotSupportedException {
+		return (Customer) super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return firstName + " " + lastName;
+	}
+}
