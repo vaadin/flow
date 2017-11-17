223b70be6f90bacc842c5a55ec0f15748ca0c4ce
Péter Török <31210544+torok-peter@users.noreply.github.com>
2017-11-17 10:40:30 +0200

    Change sendMessage access modifier to protected (#2965)
    Port change from Fw 8.1.0.rc2
    Original PR:
    https://github.com/vaadin/framework/pull/9622

--- flow-server/src/main/java/com/vaadin/server/communication/AtmospherePushConnection.java
+++ flow-server/src/main/java/com/vaadin/server/communication/AtmospherePushConnection.java
@@ -205,7 +205,7 @@ public class AtmospherePushConnection implements PushConnection {
      * @param message
      *            The message to send
      */
-    void sendMessage(String message) {
+    protected void sendMessage(String message) {
         assert (isConnected());
         // "Broadcast" the changes to the single client only
         outgoingMessage = getResource().getBroadcaster().broadcast(message,
