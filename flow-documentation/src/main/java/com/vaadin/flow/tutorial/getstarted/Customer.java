1c32d9d356faee8ff3f090f3fb2c9aa987617f63
Ahmed Ashour <asashour@yahoo.com>
2017-11-21 10:57:56 +0100

    Use Use spaces instead of tab.

--- flow-documentation/src/main/java/com/vaadin/flow/tutorial/getstarted/Customer.java
+++ flow-documentation/src/main/java/com/vaadin/flow/tutorial/getstarted/Customer.java
@@ -10,113 +10,113 @@ import com.vaadin.flow.tutorial.annotations.CodeFor;
 @CodeFor("get-started.asciidoc")
 public class Customer implements Serializable, Cloneable {
 
-	private Long id;
+    private Long id;
 
-	private String firstName = "";
+    private String firstName = "";
 
-	private String lastName = "";
+    private String lastName = "";
 
-	private CustomerStatus status;
+    private CustomerStatus status;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	/**
-	 * Get the value of status
-	 *
-	 * @return the value of status
-	 */
-	public CustomerStatus getStatus() {
-		return status;
-	}
+    /**
+     * Get the value of status
+     *
+     * @return the value of status
+     */
+    public CustomerStatus getStatus() {
+        return status;
+    }
 
-	/**
-	 * Set the value of status
-	 *
-	 * @param status
-	 *            new value of status
-	 */
-	public void setStatus(CustomerStatus status) {
-		this.status = status;
-	}
+    /**
+     * Set the value of status
+     *
+     * @param status
+     *            new value of status
+     */
+    public void setStatus(CustomerStatus status) {
+        this.status = status;
+    }
 
-	/**
-	 * Get the value of lastName
-	 *
-	 * @return the value of lastName
-	 */
-	public String getLastName() {
-		return lastName;
-	}
+    /**
+     * Get the value of lastName
+     *
+     * @return the value of lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
 
-	/**
-	 * Set the value of lastName
-	 *
-	 * @param lastName
-	 *            new value of lastName
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    /**
+     * Set the value of lastName
+     *
+     * @param lastName
+     *            new value of lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	/**
-	 * Get the value of firstName
-	 *
-	 * @return the value of firstName
-	 */
-	public String getFirstName() {
-		return firstName;
-	}
+    /**
+     * Get the value of firstName
+     *
+     * @return the value of firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
 
-	/**
-	 * Set the value of firstName
-	 *
-	 * @param firstName
-	 *            new value of firstName
-	 */
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    /**
+     * Set the value of firstName
+     *
+     * @param firstName
+     *            new value of firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public boolean isPersisted() {
-		return id != null;
-	}
+    public boolean isPersisted() {
+        return id != null;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (this.id == null) {
-			return false;
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (this.id == null) {
+            return false;
+        }
 
-		if (obj instanceof Customer && obj.getClass().equals(getClass())) {
-			return this.id.equals(((Customer) obj).id);
-		}
+        if (obj instanceof Customer && obj.getClass().equals(getClass())) {
+            return this.id.equals(((Customer) obj).id);
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 5;
-		hash = 43 * hash + (id == null ? 0 : id.hashCode());
-		return hash;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + (id == null ? 0 : id.hashCode());
+        return hash;
+    }
 
-	@Override
-	public Customer clone() throws CloneNotSupportedException {
-		return (Customer) super.clone();
-	}
+    @Override
+    public Customer clone() throws CloneNotSupportedException {
+        return (Customer) super.clone();
+    }
 
-	@Override
-	public String toString() {
-		return firstName + " " + lastName;
-	}
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
 }
