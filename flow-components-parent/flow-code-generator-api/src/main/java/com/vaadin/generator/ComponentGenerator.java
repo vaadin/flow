41aa4ed4bbbb7fcb2b90c53e57991307128b8aa5
Ahmed Ashour <asashour@yahoo.com>
2017-11-21 18:32:11 +0100

    Make the ComponentGenerator add `@SuppressWarnings`
    For both "rawtypes" and "unchecked".

--- flow-components-parent/flow-code-generator-api/src/main/java/com/vaadin/generator/ComponentGenerator.java
+++ flow-components-parent/flow-code-generator-api/src/main/java/com/vaadin/generator/ComponentGenerator.java
@@ -15,8 +15,6 @@
  */
 package com.vaadin.generator;
 
-import javax.annotation.Generated;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,9 +32,8 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.annotation.Generated;
+
 import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jboss.forge.roaster.Roaster;
@@ -46,6 +43,9 @@ import org.jboss.forge.roaster.model.source.JavaDocSource;
 import org.jboss.forge.roaster.model.source.MethodSource;
 import org.jboss.forge.roaster.model.source.ParameterSource;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.generator.exception.ComponentGenerationException;
 import com.vaadin.generator.metadata.ComponentBasicType;
 import com.vaadin.generator.metadata.ComponentEventData;
@@ -1221,6 +1221,9 @@ public class ComponentGenerator {
                 "Adds a listener for {@code %s} events fired by the webcomponent.",
                 event.getName()))
                 .addTagValue(JAVADOC_PARAM, "listener the listener");
+
+        method.addAnnotation(SuppressWarnings.class)
+                .setStringArrayValue(new String[] { "rawtypes", "unchecked" });
 
         method.setBody(String.format(
                 "return addListener(%s.class, (ComponentEventListener) listener);",
