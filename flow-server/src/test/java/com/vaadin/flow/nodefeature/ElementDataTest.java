4a06e8bf53fa4ce72edc5f45a5e50c786ad3f9d2
Denis Anisimov <denis@vaadin.com>
2017-11-29 13:21:26 +0300

    Junit test for ElementData node feature.

--- flow-server/src/test/java/com/vaadin/flow/nodefeature/ElementDataTest.java
+++ flow-server/src/test/java/com/vaadin/flow/nodefeature/ElementDataTest.java
@@ -16,18 +16,95 @@
 
 package com.vaadin.flow.nodefeature;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.vaadin.flow.StateNode;
+import com.vaadin.flow.change.MapPutChange;
+import com.vaadin.flow.change.NodeChange;
+
+import elemental.json.Json;
+import elemental.json.JsonObject;
+
 public class ElementDataTest extends AbstractNodeFeatureTest<ElementData> {
-    private final ElementData elementData = createFeature();
+    private final ElementData elementData = new StateNode(
+            Collections.singletonList(ElementData.class))
+                    .getFeature(ElementData.class);
 
     @Test
-    public void testSetGetTag() {
+    public void setGetTag() {
         Assert.assertNull("Tag should initially be null", elementData.getTag());
 
         elementData.setTag("myTag");
 
         Assert.assertEquals("myTag", elementData.getTag());
+    }
+
+    @Test
+    public void setGetPayload() {
+        Assert.assertNull("Tag should initially be null",
+                elementData.getPayload());
+
+        JsonObject object = Json.createObject();
+        elementData.setPyload(object);
+
+        Assert.assertEquals(object, elementData.getPayload());
+    }
+
+    @Test
+    public void collectChanges_setTagOnly_onlyOneChange() {
+        elementData.setTag("foo");
+        List<NodeChange> changes = new ArrayList<>();
+        elementData.collectChanges(changes::add);
+
+        Assert.assertEquals(1, changes.size());
+        Assert.assertTrue(changes.get(0) instanceof MapPutChange);
+        MapPutChange change = (MapPutChange) changes.get(0);
+        Assert.assertEquals(NodeProperties.TAG, change.getKey());
+        Assert.assertEquals(elementData.getNode(), change.getNode());
+        Assert.assertEquals("foo", change.getValue());
+    }
+
+    @Test
+    public void collectChanges_setPayloadOnly_onlyOneChange() {
+        JsonObject object = Json.createObject();
+        elementData.setPyload(object);
+        List<NodeChange> changes = new ArrayList<>();
+        elementData.collectChanges(changes::add);
+
+        Assert.assertEquals(1, changes.size());
+        Assert.assertTrue(changes.get(0) instanceof MapPutChange);
+        MapPutChange change = (MapPutChange) changes.get(0);
+        Assert.assertEquals(NodeProperties.PAYLOAD, change.getKey());
+        Assert.assertEquals(elementData.getNode(), change.getNode());
+        Assert.assertEquals(object, change.getValue());
+    }
+
+    @Test
+    public void collectChanges_setBothTagAndPayload_twoChanges() {
+        JsonObject object = Json.createObject();
+        elementData.setPyload(object);
+        elementData.setTag("foo");
+
+        List<NodeChange> changes = new ArrayList<>();
+        elementData.collectChanges(changes::add);
+
+        Assert.assertEquals(2, changes.size());
+        Assert.assertTrue(changes.get(0) instanceof MapPutChange);
+        Assert.assertTrue(changes.get(1) instanceof MapPutChange);
+
+        MapPutChange change = (MapPutChange) changes.get(0);
+        Assert.assertEquals(NodeProperties.TAG, change.getKey());
+        Assert.assertEquals(elementData.getNode(), change.getNode());
+        Assert.assertEquals("foo", change.getValue());
+
+        change = (MapPutChange) changes.get(1);
+        Assert.assertEquals(NodeProperties.PAYLOAD, change.getKey());
+        Assert.assertEquals(elementData.getNode(), change.getNode());
+        Assert.assertEquals(object, change.getValue());
     }
 }
