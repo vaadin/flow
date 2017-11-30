e2dbed48f259f15c7ca35ceba092236b3396ac30
Artur <artur@vaadin.com>
2017-11-30 10:27:39 +0200

    Publish productionMode information for JS (#3083)

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/ExportedJSFunctionView.java
@@ -0,0 +1,34 @@
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+import com.vaadin.router.PageTitle;
+import com.vaadin.router.Route;
+import com.vaadin.ui.html.Div;
+
+@Route(value = "com.vaadin.flow.uitest.ui.ExportedJSFunctionView", layout = ViewTestLayout.class)
+@PageTitle("Exported JS function view")
+public class ExportedJSFunctionView extends AbstractDivView {
+
+    @Override
+    protected void onShow() {
+        Div version = new Div();
+        version.setId("version");
+        add(version);
+        String client = "window.vaadin.clients[Object.keys(window.vaadin.clients)]";
+        String versionJs = "var msg = '';" + "var versionInfoMethod = " + client
+                + ".getVersionInfo;" + "if (versionInfoMethod) {"
+                + "  msg += 'version: '+versionInfoMethod().flow;" + "} else {" //
+                + "  msg += 'versionInfoMethod not published';" //
+                + "}" //
+                + "$0.innerHTML = msg;";
+        getPage().executeJavaScript(versionJs, version);
+
+        Div productionMode = new Div();
+        productionMode.setId("productionMode");
+        add(productionMode);
+        String productionModeJs = "var productionMode = " + client+".productionMode;"
+                + "$0.innerText = 'Production mode: '+productionMode;";
+        getPage().executeJavaScript(productionModeJs, productionMode);
+    }
+
+}
