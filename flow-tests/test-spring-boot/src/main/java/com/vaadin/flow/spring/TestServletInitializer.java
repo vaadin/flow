67a1ef72abdd1b1a2ce0530f120db4bb5bb93b2a
caalador <mikael.grankvist@gmail.com>
2017-12-01 13:40:02 +0200

    Update Spring-boot to 2.0.0.M7 (#3084)
    * Update spring boot to 2.0.0.M6
    
    * Remove local test module
    
    * Update to M7
    
    * Merge branch 'master' into issues/3006_update_spring_boot
    
    * Enable Auth server
    
    * Update OAuth, fix selenium dependencies.
    
    * Update TestBench, force compatible selenium versions
    
    * Update Spring to 5.0.2.RELEASE, fix dependency versions.

--- flow-tests/test-spring-boot/src/main/java/com/vaadin/flow/spring/TestServletInitializer.java
+++ flow-tests/test-spring-boot/src/main/java/com/vaadin/flow/spring/TestServletInitializer.java
@@ -19,16 +19,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 @Configuration
 @EnableWebSecurity
-public class TestServletInitializer {
+public class TestServletInitializer
+        implements AuthorizationServerConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(TestServletInitializer.class, args);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security)
+            throws Exception {
+        // doesn't need any impl
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)
+            throws Exception {
+        clients.inMemory().build();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+            throws Exception {
+        // doesn't need any impl
     }
 
 }
