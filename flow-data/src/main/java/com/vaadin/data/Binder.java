2fa137fa3aa14d3110a725eca7fe1cf5d17fc501
Sun Zhe <31067185+ZheSun88@users.noreply.github.com>
2017-12-13 16:12:05 +0200

    Improve nested property support for Binder (#3140)
    * Improve nested property support for Binder
    
    Port Change from FW 8.2.0.alpha3 to Flow
    
    Original PR: vaadin/framework#9925
    
    * Extract AbstractBeanPropertyDefinition and PropertyFilterDefinition
    
    this comes from the FW vaadin/framework#10347
    
    * correct Unit test
    
    * adding new line to the end of the files
    
    * correct javadoc

--- flow-data/src/main/java/com/vaadin/data/Binder.java
+++ flow-data/src/main/java/com/vaadin/data/Binder.java
@@ -963,8 +963,10 @@ public class Binder<BEAN> implements Serializable {
         }
 
         private FIELDVALUE convertDataToFieldType(BEAN bean) {
-            return converterValidatorChain.convertToPresentation(
-                    getter.apply(bean), createValueContext());
+            TARGET target = getter.apply(bean);
+            ValueContext valueContext = createValueContext();
+            return converterValidatorChain.convertToPresentation(target,
+                    valueContext);
         }
 
         /**
@@ -1219,6 +1221,21 @@ public class Binder<BEAN> implements Serializable {
         } else {
             binding.validate();
         }
+    }
+
+    /**
+     * Creates a new binder that uses reflection based on the provided bean type
+     * to resolve bean properties.
+     *
+     * @param beanType
+     *            the bean type to use, not {@code null}
+     * @param scanNestedDefinitions
+     *            if {@code true}, scan for nested property definitions as well
+     * @since
+     */
+    public Binder(Class<BEAN> beanType, boolean scanNestedDefinitions) {
+        this(BeanPropertySet.get(beanType, scanNestedDefinitions,
+                PropertyFilterDefinition.getDefaultFilter()));
     }
 
     /**
