0613162ed8602f53d62daa5f0356ae202e53b9ea
ZheSun88 <zhe@vaadin.com>
2017-12-19 08:35:01 +0200

    generate notification component

---
+++ flow-components-parent/flow-generated-components/src/main/java/com/vaadin/flow/component/notification/GeneratedVaadinNotificationCard.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.notification;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.common.HasStyle;
+import com.vaadin.ui.common.ComponentSupplier;
+import javax.annotation.Generated;
+import com.vaadin.ui.Tag;
+import com.vaadin.ui.common.HtmlImport;
+import com.vaadin.ui.common.HasComponents;
+
+/**
+ * <p>
+ * Description copied from corresponding location in WebComponent:
+ * </p>
+ * <p>
+ * The container element for the notification
+ * </p>
+ * <h3>Styling</h3>
+ * <p>
+ * <a href=
+ * "https://cdn.vaadin.com/vaadin-valo-theme/0.3.1/demo/customization.html"
+ * >Generic styling/theming documentation</a>
+ * </p>
+ */
+@Generated({ "Generator: com.vaadin.generator.ComponentGenerator#1.0-SNAPSHOT",
+        "WebComponent: Vaadin.VaadinNotificationCard#UNKNOWN",
+        "Flow#1.0-SNAPSHOT" })
+@Tag("vaadin-notification-card")
+@HtmlImport("frontend://bower_components/vaadin-notification/vaadin-notification.html")
+public class GeneratedVaadinNotificationCard<R extends GeneratedVaadinNotificationCard<R>>
+        extends Component
+        implements HasStyle, ComponentSupplier<R>, HasComponents {
+
+    /**
+     * Adds the given components as children of this component.
+     * 
+     * @param components
+     *            the components to add
+     * @see HasComponents#add(Component...)
+     */
+    public GeneratedVaadinNotificationCard(Component... components) {
+        add(components);
+    }
+
+    /**
+     * Default constructor.
+     */
+    public GeneratedVaadinNotificationCard() {
+    }
+}
