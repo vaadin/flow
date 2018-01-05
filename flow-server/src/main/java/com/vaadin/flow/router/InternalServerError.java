6ab039bfb39becdc03db2db7890399f4012b8606
Denis Anisimov <denis@vaadin.com>
2018-01-05 10:08:13 +0300

    Log an exception into the web page and warn about log binding absence

--- flow-server/src/main/java/com/vaadin/flow/router/InternalServerError.java
+++ flow-server/src/main/java/com/vaadin/flow/router/InternalServerError.java
@@ -15,13 +15,22 @@
  */
 package com.vaadin.flow.router;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.helpers.NOPLoggerFactory;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.dom.ElementFactory;
+import com.vaadin.flow.server.VaadinService;
 
 /**
  * This is a basic default error view shown on exceptions during navigation.
@@ -41,21 +50,74 @@ public class InternalServerError extends Component
                     event.getLocation().getPath(),
                     parameter.getCustomMessage());
         } else {
-            exceptionText = String.format("There was an exception while trying to navigate to '%s'",
+            exceptionText = String.format(
+                    "There was an exception while trying to navigate to '%s'",
                     event.getLocation().getPath());
         }
 
         Exception exception = parameter.getException();
         if (exception != null) {
-            getLogger().error("There was an exception while trying to navigate to '{}'",
-                event.getLocation().getPath(), exception);
+            reportException(exception, event.getLocation().getPath(),
+                    exceptionText);
+        } else {
+            getElement().setText(exceptionText);
         }
-
-        getElement().setText(exceptionText);
         return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
     }
 
     private static Logger getLogger() {
         return LoggerFactory.getLogger(InternalServerError.class.getName());
+    }
+
+    private void reportException(Exception exception, String path,
+            String exceptionText) {
+        getElement().appendChild(Element.createText(exceptionText));
+
+        boolean productionMode = VaadinService.getCurrent()
+                .getDeploymentConfiguration().isProductionMode();
+        if (!productionMode) {
+            checkLogBinding();
+            printStacktrace(exception);
+        }
+
+        getLogger().error(
+                "There was an exception while trying to navigate to '{}'", path,
+                exception);
+    }
+
+    private void printStacktrace(Exception exception) {
+        StringWriter writer = new StringWriter();
+        try {
+            exception.printStackTrace(new PrintWriter(writer));
+            getElement().appendChild(
+                    ElementFactory.createPreformatted(writer.toString()));
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                // StringWriter doesn't throw an exception
+                assert false;
+            }
+        }
+    }
+
+    private void checkLogBinding() {
+        ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+        boolean hasLogBinding = loggerFactory != null
+                && !NOPLoggerFactory.class.equals(loggerFactory.getClass());
+        if (!hasLogBinding) {
+            Element logInfo = ElementFactory
+                    .createDiv("Your application doesn't have SLF4J binding. "
+                            + "As a result the logger doesn't do any real logging. "
+                            + "Add some binding as a dependency to your project. "
+                            + "See details ");
+            logInfo.getStyle().set("marginTop", "10px");
+            logInfo.getStyle().set("marginBottom", "10px");
+            logInfo.getStyle().set("fontWeight", "bold");
+            logInfo.getStyle().set("color", "#6495ED");
+            logInfo.appendChild(ElementFactory.createAnchor(
+                    "https://www.slf4j.org/manual.html#swapping", "here"));
+            getElement().appendChild(logInfo);
+        }
     }
 }
