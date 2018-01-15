30b7de9d4d847281ae13c3113cc1061355367338
Kirill Bulatov <kirill@vaadin.com>
2018-01-15 16:24:33 +0200

    Sonar fixes.

--- flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/common/FrontendToolsManager.java
+++ flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/common/FrontendToolsManager.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.flow.plugin.common;
 
 import java.io.File;
@@ -275,7 +290,7 @@ public class FrontendToolsManager {
     private File getFragmentFile(File es6SourceDirectory, String fragmentFilePath) {
         File fragmentFile = new File(es6SourceDirectory, fragmentFilePath);
         if (!fragmentFile.isFile()) {
-            throw new RuntimeException(String.format(
+            throw new IllegalArgumentException(String.format(
                     "The fragment file path '%s' was resolved to '%s', which either does not exist or not a file.",
                     fragmentFilePath, fragmentFile));
         }
