361869cf9bc359798ebbf04bd9e90a208f5cc59d
Denis Anisimov <denis@vaadin.com>
2018-01-17 10:18:09 +0300

    Add IT test

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/template/MutationSeveralSyncedPropsView.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui.template;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+
+@Tag("multiple-props-mutation")
+@HtmlImport("frontend://com/vaadin/flow/uitest/ui/template/MultiplePropsMutation.html")
+@Route(value = "com.vaadin.flow.uitest.ui.template.MutationSeveralSyncedPropsView", layout = ViewTestLayout.class)
+public class MutationSeveralSyncedPropsView
+        extends PolymerTemplate<TemplateModel> {
+
+    public MutationSeveralSyncedPropsView() {
+        getElement().synchronizeProperty("name", "name-changed");
+        getElement().synchronizeProperty("message", "message-changed");
+
+        getElement().setProperty("name", "foo");
+        getElement().setProperty("message", "msg");
+
+        setId("template");
+
+        NativeButton button = new NativeButton(
+                "Update two synchronized properties simultaneously", event -> {
+                    getElement().setProperty("name", "bar");
+                    getElement().setProperty("message", "baz");
+                });
+        button.setId("update");
+        getElement().appendChild(button.getElement());
+    }
+}
