f3cd5df5cd24e7da0860f53ee95947ed649d05f9
Artur Signell <artur@vaadin.com>
2018-01-26 23:00:56 +0200

    Merge com.vaadin.data.provider with com.vaadin.flow.data.provider

--- flow-data/src/test/java/com/vaadin/flow/data/provider/ReplaceListDataProvider.java
+++ flow-data/src/test/java/com/vaadin/flow/data/provider/ReplaceListDataProvider.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.data.provider;
+package com.vaadin.flow.data.provider;
 
 import java.util.List;
 import java.util.stream.Stream;
