010a459810f9a46db328805846b734fc510a7443
Artur Signell <artur@vaadin.com>
2018-01-30 09:29:39 +0200

    Move documentation to a separate repository

--- flow-documentation/src/main/java/com/vaadin/flow/tutorial/getstarted/CustomerStatus.java
+++
@@ -1,7 +0,0 @@
-package com.vaadin.flow.tutorial.getstarted;
-import com.vaadin.flow.tutorial.annotations.CodeFor;
-
-@CodeFor("introduction/tutorial-get-started.asciidoc")
-public enum CustomerStatus {
-    ImportedLead, NotContacted, Contacted, Customer, ClosedLost
-}
