da43c8de29388d46c79b78eea6a2d6d360793c3e
Sun Zhe <31067185+ZheSun88@users.noreply.github.com>
2018-02-05 17:19:50 +0200

    Generate vaadin-form-layout 2.0.0-alpha6 (#3493)
    * generate vaadin-form-layout 2.0.0-alpha6
    
    update webjars.

--- flow-components-parent/flow-generated-components/src/main/java/com/vaadin/flow/component/formlayout/GeneratedVaadinFormLayout.java
+++ flow-components-parent/flow-generated-components/src/main/java/com/vaadin/flow/component/formlayout/GeneratedVaadinFormLayout.java
@@ -127,15 +127,15 @@ import elemental.json.JsonObject;
  * </tr>
  * </thead> <tbody>
  * <tr>
- * <td>{@code --vaadin-form-layout-column-gap}</td>
- * <td>Length of the gap between columns</td>
+ * <td>{@code --vaadin-form-layout-column-spacing}</td>
+ * <td>Length of the spacing between columns</td>
  * <td>{@code 2em}</td>
  * </tr>
  * </tbody>
  * </table>
  */
 @Generated({ "Generator: com.vaadin.generator.ComponentGenerator#1.0-SNAPSHOT",
-        "WebComponent: Vaadin.FormLayoutElement#2.0.0-alpha5",
+        "WebComponent: Vaadin.FormLayoutElement#2.0.0-alpha6",
         "Flow#1.0-SNAPSHOT" })
 @Tag("vaadin-form-layout")
 @HtmlImport("frontend://bower_components/vaadin-form-layout/src/vaadin-form-layout.html")
