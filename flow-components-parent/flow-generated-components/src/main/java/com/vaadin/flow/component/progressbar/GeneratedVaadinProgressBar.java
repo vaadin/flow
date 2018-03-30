ccddbe7e2000b80e40bd8bc9f37c2c51d6a4faeb
Denis Anisimov <denis@vaadin.com>
2018-03-30 13:24:49 +0300

    Update progress bar web component version

--- flow-components-parent/flow-generated-components/src/main/java/com/vaadin/flow/component/progressbar/GeneratedVaadinProgressBar.java
+++ flow-components-parent/flow-generated-components/src/main/java/com/vaadin/flow/component/progressbar/GeneratedVaadinProgressBar.java
@@ -96,7 +96,7 @@ import com.vaadin.flow.component.dependency.HtmlImport;
  * </table>
  */
 @Generated({ "Generator: com.vaadin.generator.ComponentGenerator#1.0-SNAPSHOT",
-        "WebComponent: Vaadin.ProgressBarElement#1.0.0-beta1",
+        "WebComponent: Vaadin.ProgressBarElement#1.0.0-beta2",
         "Flow#1.0-SNAPSHOT" })
 @Tag("vaadin-progress-bar")
 @HtmlImport("frontend://bower_components/vaadin-progress-bar/src/vaadin-progress-bar.html")
