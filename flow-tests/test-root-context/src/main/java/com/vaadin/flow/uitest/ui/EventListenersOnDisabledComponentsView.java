d79f96b963c6b6309797fb599804daee35db4a37
Gilberto Torrezan <gilberto@vaadin.com>
2018-04-11 09:59:29 +0300

    Recursively checks for parents in order to verify whether a node is enabled or visible

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/EventListenersOnDisabledComponentsView.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+
+@Route(value = "com.vaadin.flow.uitest.ui.EventListenersOnDisabledComponentsView", layout = ViewTestLayout.class)
+public class EventListenersOnDisabledComponentsView extends AbstractDivView {
+
+    @Override
+    protected void onShow() {
+        Div message = new Div();
+        message.setId("message");
+
+        NativeButton button = new NativeButton("Click me", event -> message
+                .setText("ERROR! This shouldn't be happening!"));
+        button.setId("button");
+
+        Div container = new Div(message, button);
+        container.setEnabled(false);
+
+        add(container);
+    }
+
+}
