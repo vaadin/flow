41763f0e1dc4dcdcc6113696c610828e0d469063
Artur Signell <artur@vaadin.com>
2018-04-13 15:48:28 +0300

    Remove VaadinServlet resource methods

--- flow-server/src/test/java/com/vaadin/flow/server/VaadinServletTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/VaadinServletTest.java
@@ -15,56 +15,13 @@
  */
 package com.vaadin.flow.server;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-
-import com.vaadin.flow.function.DeploymentConfiguration;
-import com.vaadin.flow.internal.CurrentInstance;
-import com.vaadin.flow.theme.AbstractTheme;
 
 import net.jcip.annotations.NotThreadSafe;
 
 @NotThreadSafe
 public class VaadinServletTest {
-
-    private VaadinSession session;
-    private ServletContext context;
-    private VaadinServletService service;
-    private ServiceContextUriResolver vaadinUriResolver;
-
-    private CustomVaadinServlet servlet = new CustomVaadinServlet();
-
-    @Before
-    public void setUp() throws ServletException {
-        service = Mockito.mock(VaadinServletService.class);
-        session = Mockito.mock(VaadinSession.class);
-        WebBrowser mockedEs6Browser = Mockito.mock(WebBrowser.class);
-        Mockito.when(session.getBrowser()).thenReturn(mockedEs6Browser);
-        Mockito.when(mockedEs6Browser.isEs6Supported()).thenReturn(true);
-        DeploymentConfiguration configuration = Mockito
-                .mock(DeploymentConfiguration.class);
-        Mockito.when(configuration.getEs6FrontendPrefix())
-                .thenReturn(Constants.FRONTEND_URL_DEV_DEFAULT);
-        Mockito.when(session.getConfiguration()).thenReturn(configuration);
-        Mockito.when(service.getDeploymentConfiguration())
-                .thenReturn(configuration);
-        context = Mockito.mock(ServletContext.class);
-
-        servlet.init(Mockito.mock(ServletConfig.class));
-        VaadinSession.setCurrent(session);
-    }
-
-    @After
-    public void tearDown() {
-        CurrentInstance.clearAll();
-    }
 
     @Test
     public void testGetLastPathParameter() {
@@ -104,64 +61,4 @@ public class VaadinServletTest {
                 .getLastPathParameter("http://myhost.com/a;hello/;b=1,c=2/"));
     }
 
-    @Test
-    public void resolveServletContextResource() {
-        assertResolvedUrl(
-                "/frontend/bower_components/vaadin-button/vaadin-button.html",
-                "frontend://bower_components/vaadin-button/vaadin-button.html");
-        assertResolvedUrl("/foo/bar", "/foo/bar");
-        assertResolvedUrl(
-                "/frontend/bower_components/vaadin-button/vaadin-button.html",
-                "context://frontend/bower_components/vaadin-button/vaadin-button.html");
-    }
-
-    @Test
-    public void urlTranslation() {
-        Assert.assertEquals("/theme/foo",
-                servlet.getUrlTranslation(new MyTheme(), "/src/foo"));
-        Assert.assertEquals("/other/foo",
-                servlet.getUrlTranslation(new MyTheme(), "/other/foo"));
-    }
-
-    private void assertResolvedUrl(String expected, String untranslated) {
-        Assert.assertEquals(expected, servlet.resolveResource(untranslated));
-
-    }
-
-    private final class CustomVaadinServlet extends VaadinServlet {
-
-        @Override
-        public ServletContext getServletContext() {
-            return context;
-        }
-
-        @Override
-        protected VaadinServletService createServletService() {
-            return service;
-        }
-
-        @Override
-        public VaadinServletService getService() {
-            return service;
-        }
-
-        @Override
-        boolean isInServletContext(String resolvedUrl) {
-            // For testing URL translations, assume all resources can be found
-            return true;
-        }
-    }
-
-    private static class MyTheme implements AbstractTheme {
-
-        @Override
-        public String getBaseUrl() {
-            return "/src/";
-        }
-
-        @Override
-        public String getThemeUrl() {
-            return "/theme/";
-        }
-    }
 }
