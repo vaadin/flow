6fd9410bf941e9ac034551e6f748d894feb569bc
Leif Åstrand <leif@vaadin.com>
2018-04-18 12:06:44 +0300

    Implement filtering for DOM events
    This is an intermediate step for #468.

---
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/DomEventFilterIT.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
+public class DomEventFilterIT extends ChromeBrowserTest {
+
+    @Test
+    public void filtering() {
+        open();
+
+        WebElement findElement = findElement(By.id("input"));
+
+        findElement.sendKeys("asdf");
+
+        Assert.assertEquals(0, getMessages().size());
+
+        findElement.sendKeys("foo  bar");
+
+        Assert.assertEquals(2, getMessages().size());
+    }
+
+    private List<WebElement> getMessages() {
+        WebElement messagesHolder = findElement(By.id("messages"));
+        List<WebElement> messages = messagesHolder
+                .findElements(By.cssSelector("div"));
+        return messages;
+    }
+}
