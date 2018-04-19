8cae9cb792336e651d0bb5d3d442a9732b37d6ee
Denis Anisimov <denis@vaadin.com>
2018-04-19 11:36:04 +0300

    Update javadocs for Theme
    Fixes #3889

--- flow-server/src/main/java/com/vaadin/flow/theme/Theme.java
+++ flow-server/src/main/java/com/vaadin/flow/theme/Theme.java
@@ -22,9 +22,58 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLayout;
+
 /**
  * Defines that there is a theme to use and defines the theme handler
  * implementation.
+ * <p>
+ * The theme allows to define a way to translate base component url to the
+ * themed component url (@see {@link AbstractTheme}), which specifies components
+ * styles.
+ * <p>
+ * By default {@code com.vaadin.flow.theme.lumo.Lumo} theme is used if it's in
+ * the classpath. You may disable theming with {@link NoTheme} annotation.
+ * <p>
+ * {@link Theme} annotation should be added to your root navigation level,
+ * {@link RouterLayout} or to the top level @{@link Route}.
+ *
+ * <p>
+ * Here are examples:
+ *
+ * <ul>
+ * <li>On the navigation root
+ *
+ * <pre>
+ * <code>
+ * &#64;Route(value = "")
+ * &#64;Theme(Lumo.class)
+ * public class Main extends Div {
+ * }
+ * </code>
+ * </pre>
+ *
+ * <li>on the top level router layout
+ *
+ * <pre>
+ * <code>
+ * &#64;Theme(MyTheme.class)
+ * public class MainLayout extends Div implements RouterLayout {
+ * }
+ *
+ * &#64;Route(value = "editor", layout = MainLayout.class)
+ * public class Editor extends Div {
+ * }
+ * </code>
+ * </pre>
+ *
+ * </ul>
+ *
+ * @see AbstractTheme
+ * @see NoTheme
+ * @see RouterLayout
+ * @see Route
  *
  * @author Vaadin Ltd
  */
@@ -36,14 +85,14 @@ public @interface Theme {
 
     /**
      * The theme translation handler.
-     * 
+     *
      * @return theme handler
      */
     Class<? extends AbstractTheme> value();
 
     /**
      * The theme variant, if any.
-     * 
+     *
      * @return the theme variant
      */
     String variant() default "";
