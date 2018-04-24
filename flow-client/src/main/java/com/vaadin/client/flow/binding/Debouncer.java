1d0aae86087bb62b231154f676e5aaa9664f26be
Leif Åstrand <legioth@gmail.com>
2018-04-24 10:21:02 +0300

    Add debounce and throttle support for DOM events (#3933)
    * Add debounce and throttle support for DOM events
    
    Internally, the data and filter expressions structure is supplemented to
    support an array of debounce settings for events matching a given filter
    in addition to the true and false that determine whether the expression
    should be used as a filter.
    
    Fixes #467

---
+++ flow-client/src/main/java/com/vaadin/client/flow/binding/Debouncer.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.client.flow.binding;
+
+import java.util.function.Consumer;
+
+import com.vaadin.client.flow.collection.JsCollections;
+import com.vaadin.client.flow.collection.JsMap;
+import com.vaadin.client.flow.collection.JsSet;
+import com.vaadin.client.flow.collection.JsWeakMap;
+import com.vaadin.flow.shared.JsonConstants;
+
+import elemental.dom.Node;
+import elemental.util.Timer;
+
+/**
+ * Manages debouncing of events. Use {@link #getOrCreate(Node, String, double)}
+ * to either create a new instance or get an existing instance that currently
+ * tracks a sequence of similar events.
+ *
+ * @author Vaadin Ltd
+ */
+public class Debouncer {
+
+    private static final JsWeakMap<Node, JsMap<String, JsMap<Double, Debouncer>>> debouncers = JsCollections
+            .weakMap();
+
+    private final double timeout;
+    private final Node element;
+    private final String identifier;
+
+    private Timer idleTimer;
+    private Timer intermediateTimer;
+
+    private boolean fireTrailing = false;
+    private Consumer<String> lastCommand;
+
+    private Debouncer(Node element, String identifier, double timeout) {
+        this.element = element;
+        this.identifier = identifier;
+        this.timeout = timeout;
+    }
+
+    /**
+     * Informs this debouncer that an event has occurred.
+     *
+     * @param phases
+     *            a set of strings identifying the phases for which the
+     *            triggered event should be considered.
+     * @param command
+     *            a consumer that will may be asynchronously invoked with a
+     *            phase code if an associated phase is triggered
+     * @return <code>true</code> if the event should be processed as-is without
+     *         delaying
+     */
+    public boolean trigger(JsSet<String> phases, Consumer<String> command) {
+        lastCommand = command;
+
+        boolean triggerImmediately = false;
+        if (idleTimer == null) {
+            triggerImmediately = phases.has(JsonConstants.EVENT_PHASE_LEADING);
+
+            idleTimer = new Timer() {
+                @Override
+                public void run() {
+                    if (intermediateTimer != null) {
+                        intermediateTimer.cancel();
+                    }
+
+                    if (fireTrailing) {
+                        lastCommand.accept(JsonConstants.EVENT_PHASE_TRAILING);
+                    }
+
+                    unregister();
+                }
+            };
+        }
+
+        idleTimer.cancel();
+        idleTimer.schedule((int) timeout);
+
+        if (intermediateTimer == null
+                && phases.has(JsonConstants.EVENT_PHASE_INTERMEDIATE)) {
+            intermediateTimer = new Timer() {
+                @Override
+                public void run() {
+                    lastCommand.accept(JsonConstants.EVENT_PHASE_INTERMEDIATE);
+                }
+            };
+            intermediateTimer.scheduleRepeating((int) timeout);
+        }
+
+        fireTrailing |= phases.has(JsonConstants.EVENT_PHASE_TRAILING);
+
+        return triggerImmediately;
+    }
+
+    private void unregister() {
+        JsMap<String, JsMap<Double, Debouncer>> elementMap = debouncers
+                .get(element);
+        if (elementMap == null) {
+            return;
+        }
+
+        JsMap<Double, Debouncer> identifierMap = elementMap.get(identifier);
+        if (identifierMap == null) {
+            return;
+        }
+
+        identifierMap.delete(Double.valueOf(timeout));
+
+        if (identifierMap.isEmpty()) {
+            elementMap.delete(identifier);
+
+            if (elementMap.isEmpty()) {
+                debouncers.delete(element);
+            }
+        }
+    }
+
+    /**
+     * Gets an existing debouncer or creates a new one associated with the given
+     * DOM node, identifier and debounce timeout.
+     *
+     * @param element
+     *            the DOM node to which this debouncer is bound
+     * @param identifier
+     *            a unique identifier string in the scope of the provided
+     *            element
+     * @param debounce
+     *            the debounce timeout
+     * @return a debouncer instance
+     */
+    public static Debouncer getOrCreate(Node element, String identifier,
+            double debounce) {
+        JsMap<String, JsMap<Double, Debouncer>> elementMap = debouncers
+                .get(element);
+        if (elementMap == null) {
+            elementMap = JsCollections.map();
+            debouncers.set(element, elementMap);
+        }
+
+        JsMap<Double, Debouncer> identifierMap = elementMap.get(identifier);
+        if (identifierMap == null) {
+            identifierMap = JsCollections.map();
+            elementMap.set(identifier, identifierMap);
+        }
+
+        Debouncer debouncer = identifierMap.get(Double.valueOf(debounce));
+        if (debouncer == null) {
+            debouncer = new Debouncer(element, identifier, debounce);
+            identifierMap.set(Double.valueOf(debounce), debouncer);
+        }
+
+        return debouncer;
+    }
+}
---
+++ flow-server/src/main/java/com/vaadin/flow/dom/DebouncePhase.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.dom;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.vaadin.flow.shared.JsonConstants;
+
+/**
+ * Defines phases for an event that is debounced. For most cases, high level
+ * methods such as {@link DomListenerRegistration#debounce(int)} or
+ * {@link DomListenerRegistration#throttle(int)} should be used instead.
+ *
+ * @see DomListenerRegistration#debounce(int, DebouncePhase, DebouncePhase...)
+ * @see DomEvent#getPhase()
+ *
+ * @author Vaadin Ltd
+ */
+public enum DebouncePhase {
+    /**
+     * Debounce phase that happens immediately when the event is first
+     * triggered. Another leading event will not be sent to the server until the
+     * debounce timeout period has passed without any new events being fired.
+     * This is useful for cases such as button click where double submitting
+     * should be avoided
+     */
+    LEADING(JsonConstants.EVENT_PHASE_LEADING),
+
+    /**
+     * Debounce phase for events that are periodically sent to the server while
+     * events are being fired in rapid succession. This is useful for cases such
+     * as text input when you don't want to receive an event for each individual
+     * keystroke, but still want periodic updates. This is sometimes useful in
+     * combination with {@link #LEADING} so that the first event is sent
+     * immediately. Can also be combined with {@link #TRAILING} to get a
+     * separate event when the input has stopped.
+     */
+    INTERMEDIATE(JsonConstants.EVENT_PHASE_INTERMEDIATE),
+
+    /**
+     * Debounce phase that is sent to the server once there have been at least
+     * one debounce timeout period since the last event of the same type. This
+     * is useful for cases such as text input when you are only want to react to
+     * the text when the user pauses typing.
+     */
+    TRAILING(JsonConstants.EVENT_PHASE_TRAILING);
+
+    private static final Map<String, DebouncePhase> INSTANCES = Stream
+            .of(DebouncePhase.values()).collect(Collectors
+                    .toMap(DebouncePhase::getIdentifier, Function.identity()));
+
+    private String identifier;
+
+    DebouncePhase(String identifier) {
+        this.identifier = identifier;
+    }
+
+    /**
+     * Gets the string that is used to identify this phase.
+     *
+     * @see #forIdentifier(String)
+     *
+     * @return the identifier string
+     */
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    /**
+     * Gets the phase that corresponds to the given identifier character.
+     *
+     * @see #getIdentifier()
+     * @param identifier
+     *            the identifier character to look for
+     * @return the debounce phase corresponding to the provided identifier
+     *         character
+     * @throws IllegalArgumentException
+     *             if there is no corresponding character
+     */
+    public static DebouncePhase forIdentifier(String identifier) {
+        DebouncePhase phase = INSTANCES.get(identifier);
+        if (phase == null) {
+            throw new IllegalArgumentException(
+                    "Unsupported debounce phase identifier: " + identifier);
+        } else {
+            return phase;
+        }
+    }
+}
