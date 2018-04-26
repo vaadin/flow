86b8753255ff21a392a049377d8078915b18d1e9
Leif Åstrand <leif@vaadin.com>
2018-04-26 10:36:30 +0300

    Clarify acceptNullValues parameter description

--- flow-server/src/main/java/com/vaadin/flow/component/AbstractSinglePropertyField.java
+++ flow-server/src/main/java/com/vaadin/flow/component/AbstractSinglePropertyField.java
@@ -137,7 +137,10 @@ public class AbstractSinglePropertyField<C extends AbstractField<C, T>, T>
      * @param defaultValue
      *            the default value to use if the property isn't defined
      * @param acceptNullValues
-     *            whether <code>null</code> is accepted as a model value
+     *            if <code>true</code>, an exception will be thrown if the model
+     *            value is set to <code>null</code>; if <code>false</code> the
+     *            property will be removed when the model value is set to
+     *            <code>null</code>
      */
     @SuppressWarnings("unchecked")
     public AbstractSinglePropertyField(String propertyName, T defaultValue,
