85a9a194e922bfba0b742219baec28fd0bfb30fa
Kirill Bulatov <mail4score@gmail.com>
2018-05-08 08:01:11 +0300

    Remove unused constants. (#4083)
    * Remove unused constants.

--- flow-server/src/main/java/com/vaadin/flow/server/Constants.java
+++ flow-server/src/main/java/com/vaadin/flow/server/Constants.java
@@ -41,8 +41,6 @@ public final class Constants implements Serializable {
     public static final String SERVLET_PARAMETER_SYNC_ID_CHECK = "syncIdCheck";
     public static final String SERVLET_PARAMETER_SEND_URLS_AS_PARAMETERS = "sendUrlsAsParameters";
     public static final String SERVLET_PARAMETER_PUSH_SUSPEND_TIMEOUT_LONGPOLLING = "pushLongPollingSuspendTimeout";
-    public static final String SERVLET_PARAMETER_ROUTER_CONFIGURATOR = "routerConfigurator";
-    public static final String SERVLET_PARAMETER_POLYFILL_BASE = "polyfillBase";
 
     /**
      * Configuration name for loading the ES5 adapters.
