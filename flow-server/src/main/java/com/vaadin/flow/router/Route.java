0193b6630577e96a3baf69b2e0b3b09e7ff57d49
Matti Tahvonen <matti@vaadin.com>
2018-05-09 20:59:09 +0300

    removed unused import

--- flow-server/src/main/java/com/vaadin/flow/router/Route.java
+++ flow-server/src/main/java/com/vaadin/flow/router/Route.java
@@ -22,7 +22,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 
 /**
