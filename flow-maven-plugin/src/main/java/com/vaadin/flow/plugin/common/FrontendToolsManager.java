e51488ccbf847518f9f1e87658cea28f3c0d5e70
Denis <denis@vaadin.com>
2018-05-21 15:26:02 +0300

    Provide an option to specify network concurrency arg for yarn (#4135)
    * Provide an option to specify network concurrency arg for yarn
    
    Fixes #4122

--- flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/common/FrontendToolsManager.java
+++ flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/common/FrontendToolsManager.java
@@ -28,6 +28,11 @@ import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
 import com.github.eirslett.maven.plugins.frontend.lib.InstallationException;
 import com.github.eirslett.maven.plugins.frontend.lib.NodeInstaller;
@@ -35,10 +40,6 @@ import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
 import com.github.eirslett.maven.plugins.frontend.lib.TaskRunnerException;
 import com.github.eirslett.maven.plugins.frontend.lib.YarnInstaller;
 import com.google.common.collect.ImmutableMap;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Entity to operate frontend tools to transpile files.
@@ -46,15 +47,20 @@ import org.slf4j.LoggerFactory;
  * Transpilation is based on <a href="https://github.com/gulpjs/gulp">gulp</a>
  * and <a href="https://github.com/Polymer/polymer-build">polymer-build</a>.
  * <p>
- * Polymer tools are used since they are the only ones capable of bundling html files along with css and js ones and providing helper method for processing web components.
+ * Polymer tools are used since they are the only ones capable of bundling html
+ * files along with css and js ones and providing helper method for processing
+ * web components.
  * <p>
- * Apart from gulp and polymer-build, <a href="https://github.com/yarnpkg/yarn">yarn</a> and <a href="https://github.com/nodejs/node">node</a>
- * are used to get and launch the tools required.
+ * Apart from gulp and polymer-build,
+ * <a href="https://github.com/yarnpkg/yarn">yarn</a> and
+ * <a href="https://github.com/nodejs/node">node</a> are used to get and launch
+ * the tools required.
  *
  * @author Vaadin Ltd.
  */
 public class FrontendToolsManager {
-    private static final Logger LOGGER = LoggerFactory.getLogger(FrontendToolsManager.class);
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(FrontendToolsManager.class);
 
     private final FrontendPluginFactory factory;
     private final File workingDirectory;
@@ -64,39 +70,65 @@ public class FrontendToolsManager {
 
     /**
      * Prepares the manager.
-     * @param workingDirectory          the directory to install and process files in, not {@code null}
-     * @param es5OutputDirectoryName    the name for the directory to put transpiled ES5 files into, not {@code null}
-     * @param es6OutputDirectoryName    the name for the directory to put minified ES6 files into, not {@code null}
-     * @param frontendDataProvider      the source of the files required by the {@link FrontendToolsManager}, not {@code null}
+     *
+     * @param workingDirectory
+     *            the directory to install and process files in, not
+     *            {@code null}
+     * @param es5OutputDirectoryName
+     *            the name for the directory to put transpiled ES5 files into,
+     *            not {@code null}
+     * @param es6OutputDirectoryName
+     *            the name for the directory to put minified ES6 files into, not
+     *            {@code null}
+     * @param frontendDataProvider
+     *            the source of the files required by the
+     *            {@link FrontendToolsManager}, not {@code null}
      */
     public FrontendToolsManager(File workingDirectory,
-                                String es5OutputDirectoryName, String es6OutputDirectoryName, FrontendDataProvider frontendDataProvider) {
-        FlowPluginFileUtils.forceMkdir(Objects.requireNonNull(workingDirectory));
-        this.factory = new FrontendPluginFactory(workingDirectory, workingDirectory);
+            String es5OutputDirectoryName, String es6OutputDirectoryName,
+            FrontendDataProvider frontendDataProvider) {
+        FlowPluginFileUtils
+                .forceMkdir(Objects.requireNonNull(workingDirectory));
+        this.factory = new FrontendPluginFactory(workingDirectory,
+                workingDirectory);
         this.workingDirectory = workingDirectory;
-        this.es5OutputDirectoryName = Objects.requireNonNull(es5OutputDirectoryName);
-        this.es6OutputDirectoryName = Objects.requireNonNull(es6OutputDirectoryName);
-        this.frontendDataProvider = Objects.requireNonNull(frontendDataProvider);
+        this.es5OutputDirectoryName = Objects
+                .requireNonNull(es5OutputDirectoryName);
+        this.es6OutputDirectoryName = Objects
+                .requireNonNull(es6OutputDirectoryName);
+        this.frontendDataProvider = Objects
+                .requireNonNull(frontendDataProvider);
     }
 
     /**
-     * Installs tools required for transpilation.
-     * Tools installed are:
+     * Installs tools required for transpilation. Tools installed are:
      * <ul>
      * <li><a href="https://github.com/nodejs/node">node</a></li>
      * <li><a href="https://github.com/yarnpkg/yarn">yarn</a></li>
      * <li><a href="https://github.com/gulpjs/gulp">gulp</a></li>
      * </ul>
-     * Additionally, {@literal gulpfile.js} file that is used for installation and transpilation via {@literal gulp}.
+     * Additionally, {@literal gulpfile.js} file that is used for installation
+     * and transpilation via {@literal gulp}.
      *
-     * @param proxyConfig proxy config to use when downloading frontend tools, not {@code null}
-     * @param nodeVersion node version, not {@code null}
-     * @param yarnVersion yarn version, not {@code null}
-     * @throws IllegalStateException if dependency installation fails
-     * @throws UncheckedIOException  if supplementary file creation fails
+     * @param proxyConfig
+     *            proxy config to use when downloading frontend tools, not
+     *            {@code null}
+     * @param nodeVersion
+     *            node version, not {@code null}
+     * @param yarnVersion
+     *            yarn version, not {@code null}
+     * @param networkConcurrency
+     *            maximum number of concurrent network requests
+     *
+     * @throws IllegalStateException
+     *             if dependency installation fails
+     * @throws UncheckedIOException
+     *             if supplementary file creation fails
      */
-    public void installFrontendTools(ProxyConfig proxyConfig, String nodeVersion, String yarnVersion) {
-        LOGGER.info("Installing required frontend tools to '{}'", workingDirectory);
+    public void installFrontendTools(ProxyConfig proxyConfig,
+            String nodeVersion, String yarnVersion, int networkConcurrency) {
+        LOGGER.info("Installing required frontend tools to '{}'",
+                workingDirectory);
         Objects.requireNonNull(proxyConfig);
         Objects.requireNonNull(nodeVersion);
         Objects.requireNonNull(yarnVersion);
@@ -104,99 +136,147 @@ public class FrontendToolsManager {
         createFileFromTemplateResource("package.json", Collections.emptyMap());
         createFileFromTemplateResource("yarn.lock", Collections.emptyMap());
         try {
-            factory.getNodeInstaller(proxyConfig)
-                    .setNodeVersion(nodeVersion)
-                    .setNodeDownloadRoot(NodeInstaller.DEFAULT_NODEJS_DOWNLOAD_ROOT)
+            factory.getNodeInstaller(proxyConfig).setNodeVersion(nodeVersion)
+                    .setNodeDownloadRoot(
+                            NodeInstaller.DEFAULT_NODEJS_DOWNLOAD_ROOT)
                     .install();
-            factory.getYarnInstaller(proxyConfig)
-                    .setYarnVersion(yarnVersion)
-                    .setYarnDownloadRoot(YarnInstaller.DEFAULT_YARN_DOWNLOAD_ROOT)
+            factory.getYarnInstaller(proxyConfig).setYarnVersion(yarnVersion)
+                    .setYarnDownloadRoot(
+                            YarnInstaller.DEFAULT_YARN_DOWNLOAD_ROOT)
                     .install();
-            factory.getYarnRunner(proxyConfig, null)
-                    .execute("install", Collections.emptyMap());
+            StringBuilder args = new StringBuilder("install");
+            if (networkConcurrency >= 0) {
+                args.append(" --network-concurrency ");
+                args.append(networkConcurrency);
+            }
+            factory.getYarnRunner(proxyConfig, null).execute(args.toString(),
+                    Collections.emptyMap());
         } catch (InstallationException | TaskRunnerException e) {
-            throw new IllegalStateException("Failed to install required frontend dependencies", e);
+            throw new IllegalStateException(
+                    "Failed to install required frontend dependencies", e);
         }
     }
 
-    private void createFileFromTemplateResource(String templateResourceName, Map<String, String> replacements) {
-        try (InputStream resource = getClass().getClassLoader().getResourceAsStream(templateResourceName)) {
+    private void createFileFromTemplateResource(String templateResourceName,
+            Map<String, String> replacements) {
+        try (InputStream resource = getClass().getClassLoader()
+                .getResourceAsStream(templateResourceName)) {
             if (resource == null) {
-                throw new IllegalStateException(String.format("File '%s' is not found in application resources", templateResourceName));
+                throw new IllegalStateException(String.format(
+                        "File '%s' is not found in application resources",
+                        templateResourceName));
             }
 
-            String resourceContents = IOUtils.toString(resource, StandardCharsets.UTF_8);
-            for (Map.Entry<String, String> replacement : replacements.entrySet()) {
-                resourceContents = resourceContents.replaceFirst(Pattern.quote(replacement.getKey()), replacement.getValue().replace("\\", "/"));
+            String resourceContents = IOUtils.toString(resource,
+                    StandardCharsets.UTF_8);
+            for (Map.Entry<String, String> replacement : replacements
+                    .entrySet()) {
+                resourceContents = resourceContents.replaceFirst(
+                        Pattern.quote(replacement.getKey()),
+                        replacement.getValue().replace("\\", "/"));
             }
 
-            FileUtils.write(new File(workingDirectory, templateResourceName), resourceContents, StandardCharsets.UTF_8);
+            FileUtils.write(new File(workingDirectory, templateResourceName),
+                    resourceContents, StandardCharsets.UTF_8);
         } catch (IOException e) {
-            throw new UncheckedIOException(String.format("Failed to create a file in '%s'", workingDirectory), e);
+            throw new UncheckedIOException(String.format(
+                    "Failed to create a file in '%s'", workingDirectory), e);
         }
     }
 
     /**
-     * Transpiles files in the directory provided, copying them to the working directory.
-     * Creates a new output directory, in working directory, with two separate directories for ES5 (transpiled) and ES6 (optimized) files.
+     * Transpiles files in the directory provided, copying them to the working
+     * directory. Creates a new output directory, in working directory, with two
+     * separate directories for ES5 (transpiled) and ES6 (optimized) files.
      * Additionally, creates supplementary files, used by transpilation process.
      *
-     * @param es6SourceDirectory   the directory to get ES6 files from, not {@code null}
-     * @param outputDirectory      the directory to put transpiled files into, created if absent
-     * @param skipEs5              whether to skip the transpilation step or not
+     * @param es6SourceDirectory
+     *            the directory to get ES6 files from, not {@code null}
+     * @param outputDirectory
+     *            the directory to put transpiled files into, created if absent
+     * @param skipEs5
+     *            whether to skip the transpilation step or not
      * @return generated output directory path
-     * @throws IllegalStateException    if transpilation fails
-     * @throws IllegalArgumentException if es6SourceDirectory is not a directory or does not exist
-     * @throws UncheckedIOException     if output directory creation fails or other {@link IOException} occurs
+     * @throws IllegalStateException
+     *             if transpilation fails
+     * @throws IllegalArgumentException
+     *             if es6SourceDirectory is not a directory or does not exist
+     * @throws UncheckedIOException
+     *             if output directory creation fails or other
+     *             {@link IOException} occurs
      */
-    public Map<String, File> transpileFiles(File es6SourceDirectory, File outputDirectory, boolean skipEs5) {
+    public Map<String, File> transpileFiles(File es6SourceDirectory,
+            File outputDirectory, boolean skipEs5) {
         LOGGER.info("Processing frontend files from '{}'", es6SourceDirectory);
         try {
             FileUtils.forceMkdir(Objects.requireNonNull(outputDirectory));
         } catch (IOException e) {
-            throw new UncheckedIOException(String.format("Failed to create output directory '%s'", outputDirectory), e);
+            throw new UncheckedIOException(
+                    String.format("Failed to create output directory '%s'",
+                            outputDirectory),
+                    e);
         }
 
         if (!Objects.requireNonNull(es6SourceDirectory).isDirectory()) {
-            throw new IllegalArgumentException(String.format("es6SourceDirectory '%s' is not a directory or does not exist", es6SourceDirectory));
+            throw new IllegalArgumentException(String.format(
+                    "es6SourceDirectory '%s' is not a directory or does not exist",
+                    es6SourceDirectory));
         }
 
         ImmutableMap.Builder<String, String> gulpFileParameters = new ImmutableMap.Builder<String, String>()
-                .put("{es6_source_directory}", es6SourceDirectory.getAbsolutePath())
+                .put("{es6_source_directory}",
+                        es6SourceDirectory.getAbsolutePath())
                 .put("{target_directory}", outputDirectory.getAbsolutePath())
                 .put("{es5_configuration_name}", es5OutputDirectoryName)
                 .put("{es6_configuration_name}", es6OutputDirectoryName)
-                .put("{bundle}", Boolean.toString(frontendDataProvider.shouldBundle()))
-                .put("{minify}", Boolean.toString(frontendDataProvider.shouldMinify()))
-                .put("{hash}", Boolean.toString(frontendDataProvider.shouldHash()))
-                .put("{shell_file}", frontendDataProvider.createShellFile(workingDirectory))
-                .put("{fragment_files}", combineFilePathsIntoString(frontendDataProvider.createFragmentFiles(workingDirectory)));
-        createFileFromTemplateResource("gulpfile.js", gulpFileParameters.build());
+                .put("{bundle}",
+                        Boolean.toString(frontendDataProvider.shouldBundle()))
+                .put("{minify}",
+                        Boolean.toString(frontendDataProvider.shouldMinify()))
+                .put("{hash}",
+                        Boolean.toString(frontendDataProvider.shouldHash()))
+                .put("{shell_file}",
+                        frontendDataProvider.createShellFile(workingDirectory))
+                .put("{fragment_files}",
+                        combineFilePathsIntoString(frontendDataProvider
+                                .createFragmentFiles(workingDirectory)));
+        createFileFromTemplateResource("gulpfile.js",
+                gulpFileParameters.build());
 
         Map<String, File> transpilationResults = new HashMap<>();
         try {
-            factory.getGulpRunner().execute("build_es6", Collections.emptyMap());
-            addTranspilationResult(transpilationResults, outputDirectory, es6OutputDirectoryName);
+            factory.getGulpRunner().execute("build_es6",
+                    Collections.emptyMap());
+            addTranspilationResult(transpilationResults, outputDirectory,
+                    es6OutputDirectoryName);
 
             if (!skipEs5) {
-                factory.getGulpRunner().execute("build_es5", Collections.emptyMap());
-                addTranspilationResult(transpilationResults, outputDirectory, es5OutputDirectoryName);
+                factory.getGulpRunner().execute("build_es5",
+                        Collections.emptyMap());
+                addTranspilationResult(transpilationResults, outputDirectory,
+                        es5OutputDirectoryName);
             }
         } catch (TaskRunnerException e) {
-            throw new IllegalStateException("Transpilation with gulp has failed", e);
+            throw new IllegalStateException(
+                    "Transpilation with gulp has failed", e);
         }
 
         return transpilationResults;
     }
 
     private String combineFilePathsIntoString(Set<String> fragmentFiles) {
-        return fragmentFiles.stream().map(fileName -> "'" + fileName + "'").collect(Collectors.joining(", "));
+        return fragmentFiles.stream().map(fileName -> "'" + fileName + "'")
+                .collect(Collectors.joining(", "));
     }
 
-    private void addTranspilationResult(Map<String, File> transpilationResults, File outputDirectory, String configurationName) {
-        File configurationOutput = FileUtils.getFile(outputDirectory, configurationName);
+    private void addTranspilationResult(Map<String, File> transpilationResults,
+            File outputDirectory, String configurationName) {
+        File configurationOutput = FileUtils.getFile(outputDirectory,
+                configurationName);
         if (!configurationOutput.isDirectory()) {
-            throw new IllegalStateException(String.format("Unable to find transpilation result directory at '%s'", configurationOutput));
+            throw new IllegalStateException(String.format(
+                    "Unable to find transpilation result directory at '%s'",
+                    configurationOutput));
         }
         transpilationResults.put(configurationName, configurationOutput);
     }
