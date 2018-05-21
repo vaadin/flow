bf5dbe08f1dc3f76497f8162ab426146a1b211e6
Denis Anisimov <denis@vaadin.com>
2018-05-21 20:46:07 +0300

    Make two way data binding work with beans in list
    Fixes #4091

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/template/collections/TwoWayListBindingView.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui.template.collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.polymertemplate.EventHandler;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.templatemodel.AllowClientUpdates;
+import com.vaadin.flow.templatemodel.ClientUpdateMode;
+import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+
+@Route(value = "com.vaadin.flow.uitest.ui.template.collections.TwoWayListBindingView", layout = ViewTestLayout.class)
+@Tag("two-way-list-binding")
+@HtmlImport("frontend://com/vaadin/flow/uitest/ui/template/collections/TwoWayListBinding.html")
+public class TwoWayListBindingView
+        extends PolymerTemplate<TwoWayListBindingView.TwoWayBindingModel>
+        implements HasComponents {
+
+    public interface TwoWayBindingModel extends TemplateModel {
+
+        void setMessages(List<Message> messages);
+
+        @AllowClientUpdates(ClientUpdateMode.ALLOW)
+        List<Message> getMessages();
+
+        void setEnable(boolean enable);
+    }
+
+    public TwoWayListBindingView() {
+        getModel().setMessages(
+                Arrays.asList(new Message("foo"), new Message("bar")));
+
+        NativeButton button = new NativeButton("Show listing",
+                event -> getModel().setEnable(true));
+        button.setId("enable");
+        add(button);
+    }
+
+    @EventHandler
+    private void valueUpdated() {
+        Div div = new Div();
+        div.setClassName("messages");
+        div.setText(getModel().getMessages().toString());
+        add(div);
+    }
+}
---
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/template/collections/TwoWayListBindingIT.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui.template.collections;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
+public class TwoWayListBindingIT extends ChromeBrowserTest {
+
+    @Test
+    public void itemsInList_twoWayDataBinding_updatesAreSentToServer() {
+        open();
+
+        findElement(By.id("enable")).click();
+
+        List<WebElement> fields = findInShadowRoot(
+                findElement(By.tagName("two-way-list-binding")),
+                By.cssSelector("input"));
+
+        // self check
+        Assert.assertEquals(2, fields.size());
+
+        fields.get(0).clear();
+        fields.get(0).sendKeys("baz");
+        fields.get(0).sendKeys(Keys.TAB);
+
+        Assert.assertEquals("[baz, bar]", getLastInfoMessage());
+
+        fields.get(1).sendKeys("foo");
+        fields.get(1).sendKeys(Keys.TAB);
+
+        Assert.assertEquals("[baz, barfoo]", getLastInfoMessage());
+    }
+
+    private String getLastInfoMessage() {
+        List<WebElement> messages = findElements(By.className("messages"));
+        // self check
+        Assert.assertTrue(messages.size() > 0);
+        return messages.get(messages.size() - 1).getText();
+    }
+}
