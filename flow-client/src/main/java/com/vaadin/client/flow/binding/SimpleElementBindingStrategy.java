f55023608e057cfaedb892d0616334f80568f998
Denis Anisimov <denis@vaadin.com>
2018-05-28 14:34:04 +0300

    Use another sentence int the comments.

--- flow-client/src/main/java/com/vaadin/client/flow/binding/SimpleElementBindingStrategy.java
+++ flow-client/src/main/java/com/vaadin/client/flow/binding/SimpleElementBindingStrategy.java
@@ -283,9 +283,9 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
     /*-{
         this.@SimpleElementBindingStrategy::bindInitialModelProperties(*)(node, element);
         var self = this;
-
+    
         var originalPropertiesChanged = element._propertiesChanged;
-
+    
         if (originalPropertiesChanged) {
             element._propertiesChanged = function (currentProps, changedProps, oldProps) {
                 $entry(function () {
@@ -294,24 +294,28 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                 originalPropertiesChanged.apply(this, arguments);
             };
         }
-    
+
         var tree = node.@com.vaadin.client.flow.StateNode::getTree()();
-    
+
         var originalReady = element.ready;
         element.ready = function (){
             originalReady.apply(this, arguments);
             @com.vaadin.client.PolymerUtils::fireReadyEvent(*)(element);
-    
+
             // The  _propertiesChanged method which is replaced above for the element
             // doesn't do anything for items in dom-repeat.
             // Instead it's called with some meaningful info for the <code>dom-repeat</code> element.
-            // So here <code>_propertiesChanged</code> is changed for the <code>dom-repeat</code> prototype
+            // So here the <code>_propertiesChanged</code> method is replaced
+            // for the <code>dom-repeat</code> prototype
             // which changes this method for any dom-repeat instance.
             var replaceDomRepeatPropertyChange = function(){
                 var domRepeat = element.root.querySelector('dom-repeat');
-
+    
                 if ( domRepeat ){
-                 // dom-repeat may be already in the DOM but may be not yet.
+                 // If the <code>dom-repeat</code> element is in the DOM then
+                 // this method should not be executed anymore. The logic below will replace
+                 // the <code>_propertiesChanged</code> method in its prototype so that our
+                 // method will work for any dom-repeat instance.
                  element.removeEventListener('dom-change', replaceDomRepeatPropertyChange);
                 }
                 else {
@@ -320,12 +324,12 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                 // if dom-repeat is found => replace _propertiesChanged method in the prototype and mark it as replaced.
                 if ( !domRepeat.constructor.prototype.$propChangedModified){
                     domRepeat.constructor.prototype.$propChangedModified = true;
-    
+
                     var changed = domRepeat.constructor.prototype._propertiesChanged;
-    
+
                     domRepeat.constructor.prototype._propertiesChanged = function(currentProps, changedProps, oldProps){
                         changed.apply(this, arguments);
-
+    
                         var props = Object.getOwnPropertyNames(changedProps);
                         var items = "items.";
                         for(i=0; i<props.length; i++){
@@ -339,12 +343,12 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                                 if ( index >0){
                                     // this is the index of the changed item
                                     var arrayIndex = prop.substr(0,index);
-                                    // this is the property name of the cahnged item
+                                    // this is the property name of the changed item
                                     var propertyName = prop.substr(index+1);
-    
+
                                     var nodeId = currentProps.items[arrayIndex].nodeId;
                                     var value = currentProps.items[arrayIndex][propertyName];
-    
+
                                     // this is an attempt to find the template element
                                     // which is not available as a context in the protype method
                                     var host = this.__dataHost;
@@ -355,7 +359,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                                     while( !host.localName || host.__dataHost ){
                                         host = host.__dataHost;
                                     }
-
+    
                                     $entry(function () {
                                         @SimpleElementBindingStrategy::handleListItemPropertyChange(*)(nodeId, host, propertyName, value, tree);
                                     })();
@@ -365,7 +369,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                     };
                 }
             };
-    
+
             // dom-repeat doesn't have to be in DOM even if template has it
             //  such situation happens if there is dom-if e.g. which evaluates to <code>false</code> initially.
             // in this case dom-repeat is not yet in the DOM tree until dom-if becomes <code>true</code>
@@ -380,7 +384,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                 element.addEventListener('dom-change',replaceDomRepeatPropertyChange);
             }
         }
-    
+
     }-*/;
 
     private static void handleListItemPropertyChange(double nodeId,
