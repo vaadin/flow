4f3920dcf4dad91765038b6becc116e0f230db91
caalador <mikael.grankvist@gmail.com>
2018-06-05 15:02:11 +0300

    The spring servlet can now handle spring type resources. vaadin/spring#317
    Serve spring-boot resources if spring boot ResourceProperties is configured.

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/SpringVaadinServletService.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/SpringVaadinServletService.java
@@ -15,11 +15,15 @@
  */
 package com.vaadin.flow.spring;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 
 import com.vaadin.flow.di.Instantiator;
 import com.vaadin.flow.function.DeploymentConfiguration;
@@ -29,17 +33,18 @@ import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinServlet;
 import com.vaadin.flow.server.VaadinServletService;
 import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.server.WebBrowser;
 import com.vaadin.flow.shared.Registration;
+import com.vaadin.flow.theme.AbstractTheme;
 
 /**
  * Spring application context aware Vaadin servlet service implementation.
  *
  * @author Vaadin Ltd
- *
  */
 public class SpringVaadinServletService extends VaadinServletService {
 
-    private final ApplicationContext context;
+    private final transient ApplicationContext context;
 
     private final Registration serviceDestroyRegistration;
 
@@ -48,11 +53,11 @@ public class SpringVaadinServletService extends VaadinServletService {
      * configuration with provided application {@code context}.
      *
      * @param servlet
-     *            the servlet which receives requests
+     *         the servlet which receives requests
      * @param deploymentConfiguration
-     *            the configuration to use
+     *         the configuration to use
      * @param context
-     *            the Spring application context
+     *         the Spring application context
      */
     public SpringVaadinServletService(VaadinServlet servlet,
             DeploymentConfiguration deploymentConfiguration,
@@ -105,5 +110,102 @@ public class SpringVaadinServletService extends VaadinServletService {
     private void serviceDestroyed(Registration registration) {
         registration.remove();
         serviceDestroyRegistration.remove();
+    }
+
+    @Override
+    public URL getStaticResource(String path) {
+        URL resource = super.getStaticResource(path);
+        if (resource == null) {
+            resource = getResourceURL(path);
+        }
+        return resource;
+    }
+
+    @Override
+    public URL getResource(String path, WebBrowser browser,
+            AbstractTheme theme) {
+        URL resource = super.getResource(path, browser, theme);
+        if (resource == null) {
+            resource = getResourceURL(
+                    getThemeResolvedPath(path, browser, theme));
+        }
+        return resource;
+    }
+
+    private URL getResourceURL(String path) {
+        if (!isSpringBootConfigured()) {
+            return null;
+        }
+        for (String prefix : context.getBean(
+                org.springframework.boot.autoconfigure.web.ResourceProperties.class)
+                .getStaticLocations()) {
+            Resource resource = context.getResource(getFullPath(path, prefix));
+            if (resource != null) {
+                try {
+                    return resource.getURL();
+                } catch (IOException e) {
+                    // NO-OP file was not found.
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getFullPath(String path, String prefix) {
+        if (prefix.endsWith("/") && path.startsWith("/")) {
+            return prefix + path.substring(1);
+        }
+        return prefix + path;
+    }
+
+    private boolean isSpringBootConfigured() {
+        String resourcePropertiesFQN = "org.springframework.boot.autoconfigure.web.ResourceProperties";
+        if (isClassnameAvailable(resourcePropertiesFQN)) {
+            return context.getBeanNamesForType(
+                    org.springframework.boot.autoconfigure.web.ResourceProperties.class).length != 0;
+        }
+        return false;
+    }
+
+    private static boolean isClassnameAvailable(String clazzName) {
+        try {
+            Class.forName(clazzName, false,
+                    SpringVaadinServletService.class.getClassLoader());
+        } catch (LinkageError | ClassNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String path, WebBrowser browser,
+            AbstractTheme theme) {
+        InputStream resourceAsStream = super
+                .getResourceAsStream(path, browser, theme);
+        if (resourceAsStream == null) {
+            URL resourceURL = getResourceURL(
+                    getThemeResolvedPath(path, browser, theme));
+            if (resourceURL != null) {
+                try {
+                    resourceAsStream = resourceURL.openStream();
+                } catch (IOException e) {
+                    // NO-OP return null stream
+                }
+            }
+        }
+        return resourceAsStream;
+    }
+
+    private String getThemeResolvedPath(String url, WebBrowser browser,
+            AbstractTheme theme) {
+        String resourceUrl = resolveResource(url, browser);
+        if (theme != null) {
+            String themeUrl = theme.translateUrl(resourceUrl);
+            if (!resourceUrl.equals(themeUrl)
+                    && getResourceURL(themeUrl) != null) {
+                return themeUrl;
+            }
+        }
+        return resourceUrl;
     }
 }
