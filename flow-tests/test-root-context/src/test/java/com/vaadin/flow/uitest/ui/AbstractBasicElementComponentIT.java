5206b93ff4ac044136e23cde6726495e500212b4
Pekka Maanpaa <pekkamaa@vaadin.com>
2018-06-08 15:53:29 +0300

    Use same code in subclass

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/AbstractBasicElementComponentIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/AbstractBasicElementComponentIT.java
@@ -17,7 +17,10 @@ public abstract class AbstractBasicElementComponentIT
     @Test
     public void ensureDomUpdatesAndEventsDoSomething() {
         open();
+        assertDomUpdatesAndEventsDoSomething();
+    }
 
+    protected void assertDomUpdatesAndEventsDoSomething() {
         Assert.assertEquals(0, getThankYouCount());
         $(InputTextElement.class).first().setValue("abc");
 
