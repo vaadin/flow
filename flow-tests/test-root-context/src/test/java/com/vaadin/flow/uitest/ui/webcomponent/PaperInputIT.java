a4eccf6c033894c606844095fa6e3e2b09a9a3fc
Pekka Maanpää <pekkamaa@vaadin.com>
2018-06-21 14:40:28 +0300

    Fix a few tests for IE11 (#4317)
    * Fix PaperInputIT by moving to the end of the field before typing
    
    * Fix AttachExistingDomElementByIdIT by using tab instead of enter to commit the value change of an input field
    
    * WebComponentsIT: Skip browser log API which doesn't exist for IE11
    
    * Move ExceptionStackTraceIT to test-dev-mode because stack trace is not shown in production mode
    
    * Use TB query instead of getInShadowRoot

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/webcomponent/PaperInputIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/webcomponent/PaperInputIT.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.flow.testutil.ChromeBrowserTest;
@@ -31,12 +32,13 @@ public class PaperInputIT extends ChromeBrowserTest {
 
         WebElement input = findElement(By.tagName("paper-input"));
         String originalValue = input.getAttribute("value");
-        input.sendKeys("bar");
+        input.sendKeys(Keys.END + "bar");
 
         List<WebElement> updateValueElements = findElements(
                 By.className("update-value"));
         WebElement lastUpdateValue = updateValueElements
                 .get(updateValueElements.size() - 1);
-        org.junit.Assert.assertEquals(originalValue + "bar", lastUpdateValue.getText());
+        org.junit.Assert.assertEquals(originalValue + "bar",
+                lastUpdateValue.getText());
     }
 }
