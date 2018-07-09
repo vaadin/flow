75f3d346c61616783fdc1d1f06f207cbb9af2c0e
Denis Anisimov <denis@vaadin.com>
2018-07-09 10:10:36 +0300

    Make an access to the client-side resources from server module
    Fixes #4371

---
+++ flow-server/src/main/java/com/vaadin/flow/client/ClientResources.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.client;
+
+import java.io.InputStream;
+
+/**
+ * Service which allows to get reosurces from the client side module.
+ * <p>
+ * {@link DefaultClientResources} is used if we are in plain Java environment
+ * where resources may be gotten via the {@code Class::getResourceAsStream()}
+ * (it doesn't always work out of the box since the resources are in the
+ * different bundle).
+ * <p>
+ * In OSGi Environment a special service is registered which allows to get the
+ * resources.
+ *
+ * @author Vaadin Ltd
+ *
+ */
+public interface ClientResources {
+
+    InputStream getResource(String path);
+}
---
+++ flow-server/src/main/java/com/vaadin/flow/client/ClientResourcesUtils.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.client;
+
+import java.io.InputStream;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utilities to load client access resources from the client side module.
+ *
+ * @author Vaadin Ltd
+ *
+ */
+public class ClientResourcesUtils {
+
+    private static final ClientResources SERVICE = loadService();
+
+    public static InputStream getResource(String path) {
+        return SERVICE.getResource(path);
+    }
+
+    private static ClientResources loadService() {
+        try {
+            Class.forName("org.osgi.framework.FrameworkUtil");
+            Bundle bundle = FrameworkUtil.getBundle(ClientResources.class);
+            BundleContext context = bundle.getBundleContext();
+
+            ServiceReference<ClientResources> reference = context
+                    .getServiceReference(ClientResources.class);
+            if (reference == null) {
+                return getDefaultService(null);
+            }
+            LoggerFactory.getLogger(ClientResourcesUtils.class).trace(
+                    "OSGi environment is detected. Load client resources using OSGi service");
+            return context.getService(reference);
+        } catch (ClassNotFoundException exception) {
+            return getDefaultService(exception);
+        }
+    }
+
+    private static ClientResources getDefaultService(
+            ClassNotFoundException exception) {
+        LoggerFactory.getLogger(ClientResourcesUtils.class)
+                .trace("Using standard Java way to access "
+                        + "to the client resources", exception);
+        return new DefaultClientResources();
+    }
+}
