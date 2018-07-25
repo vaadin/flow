8e7694891807b05cbc1b6fe91e8f8bc5629c0603
Denis Anisimov <denis@vaadin.com>
2018-07-25 17:27:28 +0300

    Add comment about workaround to wait HTTP server inside OSGi container

--- flow-tests/servlet-containers/felix-jetty/src/test/java/com/vaadin/flow/wait/WaitHttpIT.java
+++ flow-tests/servlet-containers/felix-jetty/src/test/java/com/vaadin/flow/wait/WaitHttpIT.java
@@ -37,6 +37,16 @@ public class WaitHttpIT extends ChromeBrowserTest {
     @Test
     public void waitForHttp()
             throws MalformedURLException, InterruptedException {
+        // This is not really a test.
+        // It allows to wait when HTTP container inside OSGi becomes ready.
+        // Without this workaround IT tests starts immediately because there is
+        // no maven server plugin which runs the server and wait when it becomes
+        // ready and then switch to the next maven phase.
+        // With the current configuration the server start is done async in the
+        // separate JVM and no one waits for its readiness.
+        // As a result IT tests starts immediately and this workaround is used
+        // to wait when HTTP server starts to handle HTTP requests.
+        // It's executed before any other IT test.
         waitViewUrl(60);
     }
 
