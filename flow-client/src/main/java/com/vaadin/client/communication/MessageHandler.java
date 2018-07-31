253c11850586844a26bf76b012a7b42a806ad3ca
Denis Anisimov <denis@vaadin.com>
2018-07-31 15:37:48 +0300

    Execute JS in the very end of the message processing event loop
    Fixes #4444

--- flow-client/src/main/java/com/vaadin/client/communication/MessageHandler.java
+++ flow-client/src/main/java/com/vaadin/client/communication/MessageHandler.java
@@ -378,10 +378,12 @@ public class MessageHandler {
 
             if (json.hasKey(JsonConstants.UIDL_KEY_EXECUTE)) {
                 // Invoke JS only after all tree changes have been
-                // propagated
-                Reactive.addPostFlushListener(
-                        () -> registry.getExecuteJavaScriptProcessor().execute(
-                                json.getArray(JsonConstants.UIDL_KEY_EXECUTE)));
+                // propagated and in the very end of the browser event loop
+                Reactive.addPostFlushListener(() -> Scheduler.get()
+                        .scheduleFinally(() -> registry
+                                .getExecuteJavaScriptProcessor()
+                                .execute(json.getArray(
+                                        JsonConstants.UIDL_KEY_EXECUTE))));
             }
 
             Console.log("handleUIDLMessage: "
