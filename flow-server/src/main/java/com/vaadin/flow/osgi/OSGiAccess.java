215b346bd5de2ca6f91011b1fcb8ad9b8f4bca51
Denis Anisimov <denis@vaadin.com>
2018-10-16 12:02:26 +0300

    Scan for servlet container initializers and for classes that they are eligible to handle

---
+++ flow-server/src/main/java/com/vaadin/flow/osgi/OSGiAccess.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.osgi;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.annotation.HandlesTypes;
+
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.flow.internal.AnnotationReader;
+import com.vaadin.flow.internal.ReflectTools;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+
+/**
+ * @author Vaadin Ltd
+ *
+ */
+public final class OSGiAccess {
+    private final static OSGiAccess INSTANCE = new OSGiAccess();
+
+    private final ServletContext context = createOSGiServletContext();
+
+    private final AtomicReference<Collection<Class<? extends ServletContainerInitializer>>> initializerClasses = new AtomicReference<>();
+
+    private final Map<Long, Collection<Class<?>>> cachedClasses = new ConcurrentHashMap<>();
+
+    private OSGiAccess() {
+    }
+
+    public static abstract class OSGiServletContext implements ServletContext {
+
+        private final Map<String, Object> attributes = new HashMap<>();
+
+        @Override
+        public void setAttribute(String name, Object object) {
+            attributes.put(name, object);
+        }
+
+        @Override
+        public Object getAttribute(String name) {
+            return attributes.get(name);
+        }
+
+        @Override
+        public void log(String msg) {
+            // This method is used by Atmosphere initiailizer
+            LoggerFactory.getLogger(OSGiAccess.class).warn(msg);
+        }
+
+        @Override
+        public Map<String, ? extends ServletRegistration> getServletRegistrations() {
+            // This method is used by Atmosphere initiailizer
+            return Collections.emptyMap();
+        }
+
+    }
+
+    public static OSGiAccess getInstance() {
+        return INSTANCE;
+    }
+
+    public ServletContext getOsgiServletContext() {
+        return context;
+    }
+
+    public void setServletContainerInitializers(
+            Collection<Class<? extends ServletContainerInitializer>> contextInitializers) {
+        assert contextInitializers != null;
+        initializerClasses.set(new ArrayList<>(contextInitializers));
+    }
+
+    public boolean hasInitializers() {
+        return initializerClasses.get() != null;
+    }
+
+    public void addScannedClasses(
+            Map<Long, Collection<Class<?>>> extenderClasses) {
+        cachedClasses.putAll(extenderClasses);
+        resetContextInitializers();
+    }
+
+    public void removeScannedClasses(Long bundleId) {
+        cachedClasses.remove(bundleId);
+        resetContextInitializers();
+    }
+
+    private void resetContextInitializers() {
+        initializerClasses.get().stream().map(ReflectTools::createInstance)
+                .forEach(this::handleTypes);
+    }
+
+    private void handleTypes(ServletContainerInitializer initializer) {
+        Optional<HandlesTypes> handleTypes = AnnotationReader
+                .getAnnotationFor(initializer.getClass(), HandlesTypes.class);
+        try {
+            initializer.onStartup(filterClasses(handleTypes.orElse(null)),
+                    getOsgiServletContext());
+        } catch (ServletException e) {
+            throw new RuntimeException(
+                    "Couldn't run servlet context initializer "
+                            + initializer.getClass(),
+                    e);
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private Set<Class<?>> filterClasses(HandlesTypes typesAnnotation) {
+        Set<Class<?>> result = new HashSet<>();
+        if (typesAnnotation == null) {
+            cachedClasses.forEach((bundle, classes) -> result.addAll(classes));
+        } else {
+            Class<?>[] requestedAnnotationTypes = typesAnnotation.value();
+            assert validateTypes(
+                    requestedAnnotationTypes) == null : validateTypes(
+                            requestedAnnotationTypes);
+            Predicate<Class<?>> hasType = clazz -> Stream
+                    .of(requestedAnnotationTypes)
+                    .anyMatch(annotation -> AnnotationReader
+                            .getAnnotationFor(clazz, (Class) annotation)
+                            .isPresent());
+            cachedClasses.forEach((bundle, classes) -> result.addAll(classes
+                    .stream().filter(hasType).collect(Collectors.toList())));
+        }
+        return result;
+    }
+
+    private String validateTypes(Class<?>[] types) {
+        Predicate<Class<?>> isAnnotation = Class::isAnnotation;
+        Optional<Class<?>> notAnnotationType = Stream.of(types)
+                .filter(isAnnotation.negate()).findFirst();
+        if (notAnnotationType.isPresent()) {
+            return String.format("Unexpected type '%s' in '%s' annotation. "
+                    + "The current implementation doesn't supoport it. "
+                    + "Implementation design has to be changed to support this type.",
+                    notAnnotationType.get(),
+                    HandlesTypes.class.getSimpleName());
+        }
+        return null;
+    }
+
+    private ServletContext createOSGiServletContext() {
+        Builder<OSGiServletContext> builder = new ByteBuddy()
+                .subclass(OSGiServletContext.class);
+
+        Class<? extends OSGiServletContext> osgiServletContextClass = builder
+                .make().load(OSGiServletContext.class.getClassLoader(),
+                        ClassLoadingStrategy.Default.WRAPPER)
+                .getLoaded();
+
+        return ReflectTools.createProxyInstance(osgiServletContextClass,
+                ServletContext.class);
+    }
+}
