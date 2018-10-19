7a2740b718346e7461afd693a3d01015f642f8f7
Elmot <elmot@vaadin.com>
2018-10-19 16:02:41 +0300

    Use another context

--- flow-tests/test-frontend-production-custom-context/src/main/webapp/frontend/es6.js
+++ flow-tests/test-frontend-production-custom-context/src/main/webapp/frontend/es6.js
@@ -1,4 +1,4 @@
-let anEs6Variable = "Piece of ES6 works";
+let anEs6Variable = "Piece of ES6 works from bundled frontend resources";
 let newChild = document.createElement("div");
 newChild.id = "es6-div";
 newChild.innerText = anEs6Variable;
