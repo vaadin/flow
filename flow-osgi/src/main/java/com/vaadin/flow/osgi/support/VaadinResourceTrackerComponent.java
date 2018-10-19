8f290a2d71cbbbe4cd0b7811bc84bde4cd0365f6
Denis Anisimov <denis@vaadin.com>
2018-10-19 08:29:07 +0300

    Flow OSGi support bundle

---
+++ flow-osgi/src/main/java/com/vaadin/flow/osgi/support/VaadinResourceTrackerComponent.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.osgi.support;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
+/**
+ * @author Vaadin Ltd
+ *
+ */
+@Component(immediate = true)
+public class VaadinResourceTrackerComponent {
+
+    private HttpService httpService;
+
+    @Reference
+    void setHttpService(HttpService service) {
+        this.httpService = service;
+    }
+
+    void unsetHttpService(HttpService service) {
+        this.httpService = null;
+    }
+
+    @Activate
+    protected void activate() throws NamespaceException {
+        System.out.println("SSSSSSSSSSSS");
+    }
+
+    @Deactivate
+    protected void deactivate() {
+
+    }
+}
