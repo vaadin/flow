eafa81e6288094f7f46528cdae3f02be97106620
Denis Anisimov <denis@vaadin.com>
2018-10-19 11:10:21 +0300

    Rename packages to avoid cross bundle conflicts

--- flow-client/src/main/java/com/vaadin/flow/client/osgi/Activator.java
+++ flow-client/src/main/java/com/vaadin/flow/client/osgi/Activator.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.flow.osgi;
+package com.vaadin.flow.client.osgi;
 
 import java.util.Hashtable;
 
