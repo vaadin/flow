fb848cc479bcf71c1a9f788b5734418467937d8c
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-01-29 14:04:12 +0200

    Fix JavaDoc.

--- flow-server/src/main/java/com/vaadin/flow/server/webcomponent/PropertyReadException.java
+++ flow-server/src/main/java/com/vaadin/flow/server/webcomponent/PropertyReadException.java
@@ -28,7 +28,7 @@ public class PropertyReadException extends RuntimeException {
      *         by the {@link #getMessage()} method).
      * @param cause
      *         the cause (which is saved for later retrieval by the
-     *         {@link #getCause()} method).  (A <@code null> value is
+     *         {@link #getCause()} method).  (A {@code null} value is
      *         permitted, and indicates that the cause is nonexistent or
      *         unknown.)
      */
