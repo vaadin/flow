74154a9f542956b9734914ba77c824cd6ade31ea
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-01-31 07:23:43 +0200

    remove un-used import

--- flow-server/src/main/java/com/vaadin/flow/router/RouteConfiguration.java
+++ flow-server/src/main/java/com/vaadin/flow/router/RouteConfiguration.java
@@ -22,7 +22,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
