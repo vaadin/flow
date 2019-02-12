0ce6ef9cdabce07a4ae8514f42592aaacc2cbb41
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-02-12 09:16:31 +0200

    Npm template parser and webpack statistics bundle parser.

---
+++ flow-server/src/main/java/com/vaadin/flow/component/polymertemplate/BundleParser.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.polymertemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.google.common.base.Predicates;
+import com.google.javascript.jscomp.NodeUtil;
+import com.google.javascript.jscomp.SourceFile;
+import com.google.javascript.jscomp.parsing.Config;
+import com.google.javascript.jscomp.parsing.ParserRunner;
+import com.google.javascript.rhino.ErrorReporter;
+import com.google.javascript.rhino.Node;
+import com.google.javascript.rhino.SimpleErrorReporter;
+import com.google.javascript.rhino.StaticSourceFile;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import elemental.json.Json;
+import elemental.json.JsonArray;
+import elemental.json.JsonObject;
+
+public final class BundleParser {
+
+    private static Config config = ParserRunner
+            .createConfig(Config.LanguageMode.ECMASCRIPT6, null,
+                    Config.StrictMode.STRICT);
+
+    private BundleParser() {
+    }
+
+    public static Optional<Element> getIdElement(String id, String fileName,
+            String statistics) {
+        Element template = parseTemplateElement(fileName, statistics);
+        return Optional.ofNullable(template.getElementById(id));
+    }
+
+    public static Element parseTemplateElement(String fileName,
+            String statistics) {
+        JsonObject moduleJson = getObjectWithId(fileName,
+                Json.parse(statistics));
+
+        ErrorReporter errorReporter = new SimpleErrorReporter();
+
+        // parse a source file into an ast.
+        SourceFile sourceFile = new SourceFile(fileName,
+                StaticSourceFile.SourceKind.STRONG);
+
+        ParserRunner.ParseResult parseResult = ParserRunner
+                .parse(sourceFile, moduleJson.getString("source"), config,
+                        errorReporter);
+
+        // run the visitor on the ast to extract the needed values.
+        DependencyVisitor visitor = new DependencyVisitor();
+        NodeUtil.visitPreOrder(parseResult.ast, visitor,
+                Predicates.alwaysTrue());
+
+        Document templateDocument = Jsoup
+                .parse(visitor.getterContent.get("template"));
+
+        Element template = templateDocument.createElement("template");
+
+        templateDocument.body().getAllElements().stream()
+                .filter(node -> !node.equals(templateDocument.body()))
+                .forEach(template::appendChild);
+
+        return template;
+    }
+
+    private static JsonObject getObjectWithId(String fileName,
+            JsonObject parse) {
+        JsonObject like = null;
+        JsonArray chunks = parse.getArray("chunks");
+        for (int i = 0; i < chunks.length(); i++) {
+            JsonObject obj = chunks.getObject(i);
+            JsonArray modules = obj.getArray("modules");
+            for (int j = 0; j < modules.length(); j++) {
+                JsonObject object = modules.getObject(j);
+                if (object.hasKey("id") && (object.getString("id")
+                        .equals(fileName) || object.getString("id").equals("."+fileName))) {
+                    like = object;
+                    break;
+                }
+            }
+            if (like != null)
+                break;
+        }
+        return like;
+    }
+
+    private static class DependencyVisitor implements NodeUtil.Visitor {
+
+        public List<String> imports = new ArrayList<>();
+        public List<String> getters = new ArrayList<>();
+        public Map<String, String> getterContent = new HashMap<>();
+
+        @Override
+        public void visit(Node node) {
+            switch (node.getToken()) {
+            case IMPORT:
+                addImport(node);
+                break;
+            case GETTER_DEF:
+                addGetter(node);
+                break;
+            }
+        }
+
+        private void addGetter(Node node) {
+            getters.add(node.getString());
+            if (node.getString().equals("template")) {
+                String content = getTextNode(node).getRawString();
+                getterContent.put(node.getString(), content);
+            }
+        }
+
+        private void addImport(Node node) {
+            if (node.hasChildren()) {
+                Node child = getTextNode(node);
+                imports.add(child.getString());
+            }
+        }
+
+        private Node getTextNode(Node node) {
+            Node child = node.getFirstChild();
+            while (child.getFirstChild() != null || child.getNext() != null) {
+                if (child.getNext() == null) {
+                    child = child.getFirstChild();
+                } else {
+                    child = child.getNext();
+                }
+            }
+            return child;
+        }
+    }
+
+}
---
+++ flow-server/src/main/java/com/vaadin/flow/component/polymertemplate/NpmTemplateParser.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.polymertemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.internal.AnnotationReader;
+import com.vaadin.flow.server.DependencyFilter;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.WebBrowser;
+import com.vaadin.flow.server.startup.FakeBrowser;
+import com.vaadin.flow.shared.ui.Dependency;
+
+public class NpmTemplateParser implements TemplateParser {
+
+    private static final TemplateParser INSTANCE = new NpmTemplateParser();
+
+    private NpmTemplateParser() {
+        // Doesn't allow external instantiation
+    }
+
+    public static TemplateParser getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public TemplateData getTemplateContent(
+            Class<? extends PolymerTemplate<?>> clazz, String tag,
+            VaadinService service) {
+
+        //        boolean logEnabled = LOG_CACHE.get(clazz).compareAndSet(false, true);
+        WebBrowser browser = FakeBrowser.getEs6();
+
+        List<Dependency> dependencies = AnnotationReader
+                .getAnnotationsFor(clazz, JavaScript.class).stream()
+                .map(htmlImport -> new Dependency(Dependency.Type.JAVASCRIPT,
+                        htmlImport.value(), htmlImport.loadMode()))
+                .collect(Collectors.toList());
+
+        DependencyFilter.FilterContext filterContext = new DependencyFilter.FilterContext(
+                service, browser);
+        for (DependencyFilter filter : service.getDependencyFilters()) {
+            dependencies = filter
+                    .filter(new ArrayList<>(dependencies), filterContext);
+        }
+
+        for (Dependency dependency : dependencies) {
+            if (dependency.getType() != Dependency.Type.JAVASCRIPT) {
+                continue;
+            }
+            String url = dependency.getUrl();
+            String statisticsFilePath = service.getDeploymentConfiguration()
+                    .getStringProperty("statistics.file.path",
+                            "META-INF/resources/stats.json");
+            try (InputStream content = getClass().getClassLoader()
+                    .getResourceAsStream(statisticsFilePath)) {
+                if (content == null) {
+                    throw new IllegalStateException(String.format(
+                            "Can't find resource '%s' "
+                                    + "via the servlet context", url));
+                }
+                Element templateElement = BundleParser
+                        .parseTemplateElement(url, streamToString(content));
+                Element parent = new Element(tag);
+                parent.attr("id", tag);
+                templateElement.appendTo(parent);
+
+                if (templateElement != null) {
+                    getLogger()
+                            .debug("Found a template file containing template definition for the tag '{}' by the path '{}'",
+                                    tag, url);
+                }
+
+                if (templateElement != null) {
+                    return new TemplateData(url, templateElement);
+
+                }
+            } catch (IOException exception) {
+                // ignore exception on close()
+                getLogger().warn("Couldn't close template input stream",
+                        exception);
+            }
+        }
+        throw new IllegalStateException(String.format("Couldn't find the "
+                        + "definition of the element with tag '%s' "
+                        + "in any template file declared using @'%s' annotations. "
+                        + "Check the availability of the template files in your WAR "
+                        + "file or provide alternative implementation of the "
+                        + "method getTemplateContent() which should return an element "
+                        + "representing the content of the template file", tag,
+                HtmlImport.class.getSimpleName()));
+    }
+
+    private String streamToString(InputStream inputStream) throws IOException {
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(inputStream,
+                        StandardCharsets.UTF_8.name()))) {
+            return br.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+
+    private Logger getLogger() {
+        return LoggerFactory.getLogger(NpmTemplateParser.class.getName());
+    }
+}
