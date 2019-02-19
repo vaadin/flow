406a43d61ec3360df4ece3be523ebb993ead1093
Denis <denis@vaadin.com>
2019-02-19 10:43:30 +0300

    Deprecate getBodyAttributes and replace it with getHtmlAttributes (#5019) Fixes #4765

--- flow-server/src/main/java/com/vaadin/flow/server/BootstrapUtils.java
+++ flow-server/src/main/java/com/vaadin/flow/server/BootstrapUtils.java
@@ -70,7 +70,7 @@ class BootstrapUtils {
     static class ThemeSettings {
         private List<JsonObject> headContents;
         private JsonObject headInjectedContent;
-        private Map<String, String> bodyAttributes;
+        private Map<String, String> htmlAttributes;
 
         public List<JsonObject> getHeadContents() {
             return headContents;
@@ -88,12 +88,12 @@ class BootstrapUtils {
             this.headInjectedContent = headInjectedContent;
         }
 
-        public Map<String, String> getBodyAttributes() {
-            return bodyAttributes;
+        public Map<String, String> getHtmlAttributes() {
+            return htmlAttributes;
         }
 
-        public void setBodyAttributes(Map<String, String> bodyAttributes) {
-            this.bodyAttributes = bodyAttributes;
+        public void setHtmlAttributes(Map<String, String> htmlAttributes) {
+            this.htmlAttributes = htmlAttributes;
         }
     }
 
@@ -336,8 +336,8 @@ class BootstrapUtils {
 
         settings.setHeadInjectedContent(createHeaderInlineScript(theme));
 
-        settings.setBodyAttributes(
-                theme.getBodyAttributes(themeDefinition.getVariant()));
+        settings.setHtmlAttributes(
+                theme.getHtmlAttributes(themeDefinition.getVariant()));
 
         return settings;
     }
