f75fc2e0eaa3e6b7f4488fe3ec00d6cbaae185f9
Denis <denis@vaadin.com>
2019-02-19 18:36:53 +0300

    Catch Throwable during maps sync handling (#5077)

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/ErrorHandlingIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/ErrorHandlingIT.java
@@ -1,16 +1,10 @@
 package com.vaadin.flow.uitest.ui;
 
-import java.util.List;
-
-import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 
-import com.vaadin.flow.component.html.testbench.DivElement;
 import com.vaadin.flow.component.html.testbench.NativeButtonElement;
-import com.vaadin.flow.testutil.ChromeBrowserTest;
 
-public class ErrorHandlingIT extends ChromeBrowserTest {
+public class ErrorHandlingIT extends AbstractErrorIT {
 
     @Test
     public void exceptionInClickHandlerDoesNotCauseInternalError() {
@@ -19,18 +13,8 @@ public class ErrorHandlingIT extends ChromeBrowserTest {
 
         assertNoSystemErrors();
 
-        List<DivElement> errors = $(DivElement.class)
-                .attributeContains("class", "error").all();
-        Assert.assertEquals(1, errors.size());
-        Assert.assertEquals(
-                "An error occurred: java.lang.IllegalStateException: Intentional fail in click handler",
-                errors.get(0).getText());
-    }
-
-    private void assertNoSystemErrors() {
-        Assert.assertEquals(0,
-                findElements(By.className("v-system-error")).size());
-
+        assertErrorReported(
+                "An error occurred: java.lang.IllegalStateException: Intentional fail in click handler");
     }
 
 }
