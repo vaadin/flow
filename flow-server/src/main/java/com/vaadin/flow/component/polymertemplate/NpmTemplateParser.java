66fc5766e1aa8089d2d14424c88789b02fdb1c99
Manolo Carrasco <manolo@apache.org>
2019-02-20 08:21:50 +0100

    stats file is available also in devmode

--- flow-server/src/main/java/com/vaadin/flow/component/polymertemplate/NpmTemplateParser.java
+++ flow-server/src/main/java/com/vaadin/flow/component/polymertemplate/NpmTemplateParser.java
@@ -21,10 +21,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.jsoup.nodes.Element;
@@ -60,12 +59,9 @@ public class NpmTemplateParser implements TemplateParser {
 
     private static final TemplateParser INSTANCE = new NpmTemplateParser();
 
-    private String hash;
-    private JsonObject statisticsJson;
+    private HashMap<String, String> cache = new HashMap<>();
     private ReentrantLock lock = new ReentrantLock();
-
-    private static final Pattern HASH_PATTERN = Pattern
-            .compile("\"hash\":\\s\"(.*)\",");
+    private JsonObject jsonStats;
 
     private NpmTemplateParser() {
         // Doesn't allow external instantiation
@@ -76,120 +72,96 @@ public class NpmTemplateParser implements TemplateParser {
     }
 
     @Override
-    public TemplateData getTemplateContent(
-            Class<? extends PolymerTemplate<?>> clazz, String tag,
+    public TemplateData getTemplateContent(Class<? extends PolymerTemplate<?>> clazz, String tag,
             VaadinService service) {
 
         WebBrowser browser = FakeBrowser.getEs6();
 
-        List<Dependency> dependencies = AnnotationReader
-                .getAnnotationsFor(clazz, JsModule.class).stream()
-                .map(htmlImport -> new Dependency(Dependency.Type.JS_MODULE,
-                        htmlImport.value(), htmlImport.loadMode()))
+        List<Dependency> dependencies = AnnotationReader.getAnnotationsFor(clazz, JsModule.class).stream()
+                .map(htmlImport -> new Dependency(Dependency.Type.JS_MODULE, htmlImport.value(), htmlImport.loadMode()))
                 .collect(Collectors.toList());
 
-        DependencyFilter.FilterContext filterContext = new DependencyFilter.FilterContext(
-                service, browser);
+        DependencyFilter.FilterContext filterContext = new DependencyFilter.FilterContext(service, browser);
         for (DependencyFilter filter : service.getDependencyFilters()) {
-            dependencies = filter
-                    .filter(new ArrayList<>(dependencies), filterContext);
+            dependencies = filter.filter(new ArrayList<>(dependencies), filterContext);
         }
-
-        boolean productionMode = service.getDeploymentConfiguration()
-                .isProductionMode();
 
         for (Dependency dependency : dependencies) {
             if (dependency.getType() != Dependency.Type.JS_MODULE) {
                 continue;
             }
             String url = dependency.getUrl();
-            try {
-                // Try first the local file
-                InputStream content = getClass().getClassLoader().getResourceAsStream(url);
-                // Use stats if local file not found
+
+            // Use source file
+            InputStream content = getClass().getClassLoader().getResourceAsStream(url);
+            String source = content != null ? streamToString(content) : null;
+            if (source != null) {
+                getLogger().debug("Found sources for the tag '{}' in '{}'", tag, url);
+            } else {
+                // Use stats file
+                String stats = service.getDeploymentConfiguration().getStringProperty(Constants.STATISTICS_JSON,
+                        "META-INF/resources/stats.json");
+
+                content = getClass().getClassLoader().getResourceAsStream(stats);
                 if (content == null) {
-                    String stats = service.getDeploymentConfiguration()
-                            .getStringProperty(Constants.STATISTICS_JSON,
-                                    "META-INF/resources/stats.json");
-                    content = getClass().getClassLoader().getResourceAsStream(stats);
+                    throw new IllegalStateException(
+                            String.format("Can't find resource '%s' or '%s'" + "via the ClassLoader", url, stats));
                 }
 
-                if (content == null) {
-                    throw new IllegalStateException(String.format(
-                            "Can't find resource '%s' "
-                                    + "via the ClassLoader", url));
+                updateCache(url, streamToString(content));
+                source = cache.get(url);
+
+                if (source == null) {
+                    throw new IllegalStateException(
+                            String.format("Can't find sources for '%s' resource in the '%s' file.", url, stats));
                 }
 
-                Element templateElement = getTemplateElement(productionMode,
-                        url, streamToString(content));
-
-                // Wrap template with an element with id, to look like a P2 template
-                Element parent = new Element(tag);
-                parent.attr("id", tag);
-                templateElement.appendTo(parent);
-
-                getLogger()
-                        .debug("Found a template file containing template definition for the tag '{}' by the path '{}'",
-                                tag, url);
-
-                return new TemplateData(url, templateElement);
-
-            } catch (IOException exception) {
-                // ignore exception on close()
-                getLogger().warn("Couldn't close template input stream",
-                        exception);
+                getLogger().debug("Found sources for the tag '{}' in '{}'", tag, stats);
             }
+            Element templateElement = BundleParser.parseTemplateElement(url, source);
+
+            // Wrap template with an element with id, to look like a P2
+            // template
+            Element parent = new Element(tag);
+            parent.attr("id", tag);
+            templateElement.appendTo(parent);
+
+            return new TemplateData(url, templateElement);
         }
-        throw new IllegalStateException(String.format("Couldn't find the "
-                        + "definition of the element with tag '%s' "
-                        + "in any template file declared using @'%s' annotations. "
-                        + "Check the availability of the template files in your WAR "
-                        + "file or provide alternative implementation of the "
-                        + "method getTemplateContent() which should return an element "
-                        + "representing the content of the template file", tag,
-                JsModule.class.getSimpleName()));
+        throw new IllegalStateException(String.format("Couldn't find the " + "definition of the element with tag '%s' "
+                + "in any template file declared using '@%s' annotations. "
+                + "Check the availability of the template files in your WAR "
+                + "file or provide alternative implementation of the "
+                + "method getTemplateContent() which should return an element "
+                + "representing the content of the template file", tag, JsModule.class.getSimpleName()));
     }
 
-    private Element getTemplateElement(boolean productionMode, String url, String fileContents) {
-        Element templateElement;
-        if (productionMode) {
-            setStatisticsJson(url, fileContents);
-            templateElement = BundleParser
-                    .parseTemplateElement(url, statisticsJson);
-        } else {
-            templateElement = BundleParser
-                    .parseTemplateElement(url, fileContents);
-        }
-        return templateElement;
-    }
-
-    private void setStatisticsJson(String url, String fileContents) {
-        Matcher matcher = HASH_PATTERN.matcher(fileContents);
-        if (matcher.find()) {
-            String contentHash = matcher.group(1);
-            lock.lock();
+    private void updateCache(String url, String fileContents) {
+        if (jsonStats == null || !jsonStats.getString("hash").equals(BundleParser.getHashFromStatistics(fileContents))) {
+            cache.clear();
             try {
-                if (!contentHash.equals(this.hash)) {
-                    this.hash = contentHash;
-                    statisticsJson = BundleParser
-                            .getStatisticsJson(url, fileContents);
-                }
+                lock.lock();
+                jsonStats = BundleParser.parseJsonStatistics(fileContents);
             } finally {
                 lock.unlock();
             }
-        } else {
-            statisticsJson = BundleParser
-                    .getStatisticsJson(url, fileContents);
+        }
+        if (!cache.containsKey(url)) {
+            cache.put(url, BundleParser.getSourceFromStatistics(url, jsonStats));
         }
     }
 
-    private String streamToString(InputStream inputStream) throws IOException {
+    private String streamToString(InputStream inputStream) {
+        String ret = "";
         try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(inputStream,
-                        StandardCharsets.UTF_8.name()))) {
-            return br.lines()
-                    .collect(Collectors.joining(System.lineSeparator()));
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8.name()))) {
+
+            ret = br.lines().collect(Collectors.joining(System.lineSeparator()));
+        } catch (IOException exception) {
+            // ignore exception on close()
+            getLogger().warn("Couldn't close template input stream", exception);
         }
+        return ret;
     }
 
     private Logger getLogger() {
