d032e4d71ba7c04de4ab14913c62995ca132a4ac
Denis Anisimov <denis@vaadin.com>
2019-02-22 10:12:12 +0300

    Rewrite UI for push test and make an IT test for it.

--- flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/UpdateDivView.java
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/UpdateDivView.java
@@ -18,57 +18,56 @@ package com.vaadin.flow.uitest.ui;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.DetachEvent;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.dom.Element;
-import com.vaadin.flow.dom.ElementFactory;
 import com.vaadin.flow.router.Route;
 
 @Push
 @Route("com.vaadin.flow.uitest.ui.UpdateDivView")
-public class UpdateDivView extends AbstractDivView {
-    private int msgId = 1;
-    private Element div = ElementFactory.createDiv();
+public class UpdateDivView extends Div {
+    private AtomicInteger count = new AtomicInteger();
 
-    private ScheduledExecutorService ses;
+    private final ScheduledExecutorService service = Executors
+            .newScheduledThreadPool(1);
+
+    private static final int DELAY = 100;
+
+    private static final int MAX_UPDATE = 50;
+
+    public UpdateDivView() {
+        setId("push-update");
+    }
 
     @Override
     protected void onAttach(AttachEvent attachEvent) {
-        super.onAttach(attachEvent);
-
-        Element bodyElement = getElement();
-        bodyElement.appendChild(div);
         updateDiv();
 
-        ses = Executors.newScheduledThreadPool(1);
-        long delay = 2;
-        ses.scheduleAtFixedRate(() -> {
+        scheduleUpdate(attachEvent.getUI());
+    }
 
-            attachEvent.getUI().access(() -> {
-                updateDiv();
-            });
-
-            if (msgId > 1000) {
-                throw new RuntimeException("Done");
+    private void scheduleUpdate(final UI ui) {
+        service.schedule(() -> {
+            ui.access(this::updateDiv);
+            if (count.getAndIncrement() < MAX_UPDATE) {
+                scheduleUpdate(ui);
+            } else {
+                service.shutdown();
             }
-        }, delay, delay, TimeUnit.MILLISECONDS);
+        }, DELAY, TimeUnit.MILLISECONDS);
     }
 
     @Override
     protected void onDetach(DetachEvent detachEvent) {
-        super.onDetach(detachEvent);
-
-        if (ses != null) {
-            ses.shutdownNow();
-            ses = null;
-        }
+        service.shutdownNow();
     }
 
     private void updateDiv() {
-        div.setText("Hello world at " + System.currentTimeMillis() + " ("
-                + msgId++ + ")");
+        setText(String.valueOf(count.get()));
     }
 
 }
