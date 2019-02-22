22429f4831307298cfadbc6686ba381836dfe209
Klaudeta <klaudeta@vaadin.com>
2019-02-22 11:10:16 +0200

    remove unused import

--- flow-data/src/main/java/com/vaadin/flow/data/provider/hierarchy/TreeDataProvider.java
+++ flow-data/src/main/java/com/vaadin/flow/data/provider/hierarchy/TreeDataProvider.java
@@ -16,7 +16,6 @@
 package com.vaadin.flow.data.provider.hierarchy;
 
 import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
