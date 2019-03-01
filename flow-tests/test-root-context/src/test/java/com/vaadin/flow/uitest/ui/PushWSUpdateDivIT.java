d6c9a0e67bd0f2cdeb9a2697f1ee3bfd75b248c2
Denis <denis@vaadin.com>
2019-03-01 10:49:42 +0300

    Register push static web resources (#5112) (#5151)

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/PushWSUpdateDivIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/PushWSUpdateDivIT.java
@@ -15,23 +15,14 @@
  */
 package com.vaadin.flow.uitest.ui;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.openqa.selenium.By;
 
 import com.vaadin.flow.testcategory.IgnoreOSGi;
-import com.vaadin.flow.testutil.ChromeBrowserTest;
 
+/**
+ * The tests are in the superclass.
+ */
 @Category(IgnoreOSGi.class)
-public class PushSettingsIT extends ChromeBrowserTest {
-    @Test
-    public void pushSetting() {
-        open();
-
-        String modeText = findElement(By.id("pushMode")).getText();
-
-        Assert.assertEquals("Push mode: AUTOMATIC", modeText);
-    }
+public class PushWSUpdateDivIT extends AbstractUpdateDivIT {
 
 }
