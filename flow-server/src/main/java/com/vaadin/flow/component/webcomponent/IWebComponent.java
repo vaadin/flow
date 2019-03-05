aba8e38ee0a9d29535d8262f82b94da00419710a
Joni Uitto <joni@vaadin.com>
2019-03-05 13:48:49 +0200

    Brought over the new WebComponent interfaces. WebComponent is currently named IWebComponent to avoid naming collisions with the existing classes - this will be fixed once the functionality has been transferred over to the new components.

---
+++ flow-server/src/main/java/com/vaadin/flow/component/webcomponent/IWebComponent.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.webcomponent;
+
+import com.vaadin.flow.component.Component;
+
+import elemental.json.JsonValue;
+
+/**
+ * WebComponent to be configured by {@link InstanceConfigurator}
+ */
+public interface IWebComponent<C extends Component> {
+    void fireEvent(String eventName);
+
+    void fireEvent(String eventName, JsonValue objectData);
+
+    void fireEvent(String eventName, JsonValue objectData,
+                   EventOptions options);
+
+    <P> void setProperty(
+            PropertyConfiguration<C, P> propertyConfiguration, P value);
+
+    <P> P getProperty(PropertyConfiguration<C, P> propertyConfiguration);
+}
