b4228f950650427a449c6ab725d2d0002b1bda6a
Denis <denis@vaadin.com>
2019-03-07 12:12:49 +0300

    Catch Throwable during maps sync handling (#5077) (#5196)

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/ExceptionDuringMapSyncView.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.component.html.Input;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+
+@Route(value = "com.vaadin.flow.uitest.ui.ExceptionDuringMapSyncView", layout = ViewTestLayout.class)
+public class ExceptionDuringMapSyncView extends AbstractErrorHandlerView {
+
+    public ExceptionDuringMapSyncView() {
+        Input input = new Input();
+        input.addValueChangeListener(event -> {
+            throw new RuntimeException(
+                    "Intentional exception in property sync handler");
+        });
+        add(input);
+    }
+
+}
