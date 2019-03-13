8710d057dda9c3faf8f853fbd09412bd0ba9e803
Joni Uitto <joni@vaadin.com>
2019-03-13 09:13:19 +0200

    Server-push IT fixed

---
+++ flow-tests/test-web-components/src/main/java/com/vaadin/flow/webcomponent/PushTestExporter.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.webcomponent;
+
+import com.vaadin.flow.component.WebComponentExporter;
+import com.vaadin.flow.component.webcomponent.WebComponentDefinition;
+
+public class PushTestExporter implements WebComponentExporter<UpdateServerSideWebComponent2> {
+    @Override
+    public String tag() {
+        return "sswc-push";
+    }
+
+    @Override
+    public void define(WebComponentDefinition<UpdateServerSideWebComponent2> definition) {
+        // UpdateServerSideWebComponent does not have any properties
+    }
+}
