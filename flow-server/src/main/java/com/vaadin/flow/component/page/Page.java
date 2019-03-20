cb6e7d5b10aff22ad88ea9d7502e665325931671
Sahal Saad <caalshift@gmail.com>
2019-03-20 13:45:02 +0800

    Implement open and setLocation method to Page. (#4869) (#5166)

--- flow-server/src/main/java/com/vaadin/flow/component/page/Page.java
+++ flow-server/src/main/java/com/vaadin/flow/component/page/Page.java
@@ -15,14 +15,6 @@
  */
 package com.vaadin.flow.component.page;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
-
 import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
@@ -37,6 +29,15 @@ import com.vaadin.flow.shared.Registration;
 import com.vaadin.flow.shared.ui.Dependency;
 import com.vaadin.flow.shared.ui.Dependency.Type;
 import com.vaadin.flow.shared.ui.LoadMode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 /**
  * Represents the web page open in the browser, containing the UI it is
@@ -387,6 +388,80 @@ public class Page implements Serializable {
             executeJavaScript(LazyJsLoader.WINDOW_LISTENER_JS, resizeReceiver);
         }
         return resizeReceiver.addListener(resizeListener);
+    }
+
+    /**
+     * Opens the given url in a new tab.
+     *
+     * @param url
+     *            the URL to open.
+     */
+    public void open(String url) {
+        open(url, "_blank");
+    }
+
+    /**
+     * Opens the given URL in a window with the given name.
+     * <p>
+     * The supplied {@code windowName} is used as the target name in a
+     * window.open call in the client. This means that special values such as
+     * "_blank", "_self", "_top", "_parent" have special meaning. An empty or
+     * <code>null</code> window name is also a special case.
+     * </p>
+     * <p>
+     * "", null and "_self" as {@code windowName} all causes the URL to be
+     * opened in the current window, replacing any old contents. For
+     * downloadable content you should avoid "_self" as "_self" causes the
+     * client to skip rendering of any other changes as it considers them
+     * irrelevant (the page will be replaced by the response from the URL). This
+     * can speed up the opening of a URL, but it might also put the client side
+     * into an inconsistent state if the window content is not completely
+     * replaced e.g., if the URL is downloaded instead of displayed in the
+     * browser.
+     * </p>
+     * <p>
+     * "_blank" as {@code windowName} causes the URL to always be opened in a
+     * new window or tab (depends on the browser and browser settings).
+     * </p>
+     * <p>
+     * "_top" and "_parent" as {@code windowName} works as specified by the HTML
+     * standard.
+     * </p>
+     * <p>
+     * Any other {@code windowName} will open the URL in a window with that
+     * name, either by opening a new window/tab in the browser or by replacing
+     * the contents of an existing window with that name.
+     * </p>
+     *
+     * @param url
+     *            the URL to open.
+     * @param windowName
+     *            the name of the window.
+     */
+    private void open(String url, String windowName) {
+        executeJavaScript("window.open($0, $1)", url, windowName);
+    }
+
+    /**
+     * Navigates this page to the given URI. The contents of this page in the
+     * browser is replaced with whatever is returned for the given URI.
+     *
+     * @param uri
+     *            the URI to show
+     */
+    public void setLocation(String uri) {
+        open(uri, "_self");
+    }
+
+    /**
+     * Navigates this page to the given URI. The contents of this page in the
+     * browser is replaced with whatever is returned for the given URI.
+     *
+     * @param uri
+     *            the URI to show
+     */
+    public void setLocation(URI uri) {
+        setLocation(uri.toString());
     }
 
     private static class LazyJsLoader implements Serializable {
