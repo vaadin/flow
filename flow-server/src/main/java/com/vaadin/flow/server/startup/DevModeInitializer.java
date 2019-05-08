809c16cb8db2b5db397bdad317d1468d3aad363c
Leif Åstrand <leif@vaadin.com>
2019-05-08 14:31:20 +0300

    Log a warning for unvisited dependency annotations
    Fixes #5624

--- flow-server/src/main/java/com/vaadin/flow/server/startup/DevModeInitializer.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/DevModeInitializer.java
@@ -15,20 +15,27 @@
  */
 package com.vaadin.flow.server.startup;
 
+import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_IMPORTS;
+import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_NPM;
+import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_WEBPACK_RUNNING_PORT;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import javax.servlet.annotation.HandlesTypes;
-import java.io.File;
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.component.WebComponentExporter;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.router.Route;
@@ -39,27 +46,72 @@ import com.vaadin.flow.server.frontend.FrontendUtils;
 import com.vaadin.flow.server.frontend.NodeTasks;
 import com.vaadin.flow.server.startup.ServletDeployer.StubServletConfig;
 
-import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_IMPORTS;
-import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_NPM;
-import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_WEBPACK_RUNNING_PORT;
-
 /**
  * Servlet initializer starting node updaters as well as the webpack-dev-mode
  * server.
  */
 @HandlesTypes({ Route.class, NpmPackage.class, WebComponentExporter.class})
-public class DevModeInitializer implements ServletContainerInitializer, Serializable {
+public class DevModeInitializer
+        implements ServletContainerInitializer, Serializable {
+
+    /**
+     * The classes that were visited when determining which frontend resources
+     * are actually used.
+     */
+    public static class VisitedClasses implements Serializable {
+        private Set<String> visitedClassNames;
+
+        /**
+         * Creates a new instance based on a set of class names.
+         *
+         * @param visitedClassNames
+         *            the set of visited class names, not <code>null</code>
+         */
+        public VisitedClasses(Set<String> visitedClassNames) {
+            assert visitedClassNames != null;
+            this.visitedClassNames = visitedClassNames;
+        }
+
+        /**
+         * Checks whether all dependency annotations of the provided class have
+         * been visited.
+         *
+         * @param dependencyClass
+         *            the class to check
+         * @return <code>true</code> if all dependencies of the class have been
+         *         visited, <code>false</code> otherwise
+         */
+        public boolean allDependenciesVisited(Class<?> dependencyClass) {
+            if (visitedClassNames.contains(dependencyClass.getName())) {
+                return true;
+            }
+
+            /*
+             * Not being visited is only a problem if the class has own
+             * dependency annotations or the parent class has problems.
+             */
+            if (dependencyClass
+                    .getDeclaredAnnotationsByType(JsModule.class).length != 0) {
+                return false;
+            }
+
+            return allDependenciesVisited(dependencyClass.getSuperclass());
+        }
+    }
 
     @Override
-    public void onStartup(Set<Class<?>> classes, ServletContext context) throws ServletException {
-        Collection<? extends ServletRegistration> registrations = context.getServletRegistrations().values();
+    public void onStartup(Set<Class<?>> classes, ServletContext context)
+            throws ServletException {
+        Collection<? extends ServletRegistration> registrations = context
+                .getServletRegistrations().values();
 
         if (registrations.isEmpty()) {
             return;
         }
 
-        DeploymentConfiguration config = StubServletConfig.createDeploymentConfiguration(context,
-                registrations.iterator().next(), VaadinServlet.class);
+        DeploymentConfiguration config = StubServletConfig
+                .createDeploymentConfiguration(context,
+                        registrations.iterator().next(), VaadinServlet.class);
 
         if (config.isProductionMode() || config.isBowerMode()) {
             return;
@@ -68,21 +120,31 @@ public class DevModeInitializer implements ServletContainerInitializer, Serializ
         // Our working dir is not in a proper dev project
         // (need to check package.json and webpack.config.js in the future)
         if (!new File(FrontendUtils.getBaseDir(), "src").isDirectory()
-                // We don't run in dev project, but user might have webpack-dev-server
+                // We don't run in dev project, but user might have
+                // webpack-dev-server
                 // running in the background
-                && config.getStringProperty(SERVLET_PARAMETER_DEVMODE_WEBPACK_RUNNING_PORT, null) == null) {
+                && config.getStringProperty(
+                        SERVLET_PARAMETER_DEVMODE_WEBPACK_RUNNING_PORT,
+                        null) == null) {
             return;
         }
 
         try {
+            Set<String> visitedClassNames = new HashSet<>();
+
             new NodeTasks.Builder(new DefaultClassFinder(classes))
                     .enablePackagesUpdate(!config.getBooleanProperty(
                             SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_NPM, false))
                     .enableImportsUpdate(!config.getBooleanProperty(
-                            SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_IMPORTS, false))
+                            SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_IMPORTS,
+                            false))
                     .runNpmInstall(true)
                     .withEmbeddableWebComponents(true)
+                    .collectVisitedClasses(visitedClassNames)
                     .build().execute();
+
+            context.setAttribute(VisitedClasses.class.getName(),
+                    new VisitedClasses(visitedClassNames));
 
             DevModeHandler.start(config);
         } catch (Exception e) {
@@ -95,4 +157,4 @@ public class DevModeInitializer implements ServletContainerInitializer, Serializ
     private Logger log() {
         return LoggerFactory.getLogger(getClass());
     }
- }
+}
