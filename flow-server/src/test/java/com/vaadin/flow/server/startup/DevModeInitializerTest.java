bca6de708af94633662004c2b26d3fb8c706599b
Manolo Carrasco <manolo@apache.org>
2019-05-14 17:18:22 +0200

    Fix checks for DevModeInitializer to point to correct folders

--- flow-server/src/test/java/com/vaadin/flow/server/startup/DevModeInitializerTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/startup/DevModeInitializerTest.java
@@ -2,6 +2,7 @@ package com.vaadin.flow.server.startup;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+
 import java.io.File;
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -12,9 +13,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
-import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.server.DevModeHandler;
-import com.vaadin.flow.server.startup.DevModeInitializer.VisitedClasses;
 import net.jcip.annotations.NotThreadSafe;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -24,9 +22,14 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.Mockito;
 
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.server.DevModeHandler;
+import com.vaadin.flow.server.startup.DevModeInitializer.VisitedClasses;
+
 import static com.vaadin.flow.server.Constants.PACKAGE_JSON;
 import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_IMPORTS;
 import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_NPM;
+import static com.vaadin.flow.server.frontend.FrontendUtils.DEFAULT_GENERATED_DIR;
 import static com.vaadin.flow.server.frontend.FrontendUtils.WEBPACK_CONFIG;
 import static com.vaadin.flow.server.frontend.FrontendUtils.getBaseDir;
 import static com.vaadin.flow.server.frontend.NodeUpdateTestUtil.createStubNode;
@@ -42,6 +45,10 @@ public class DevModeInitializerTest {
     private ServletContext servletContext;
     private DevModeInitializer devModeInitializer;
     private Set<Class<?>> classes;
+
+    private File mainPackageFile = new File(getBaseDir(), PACKAGE_JSON);
+    private File webpackFile = new File(getBaseDir(), WEBPACK_CONFIG);
+    private File appPackageFile = new File(getBaseDir(), DEFAULT_GENERATED_DIR + PACKAGE_JSON);
 
     @JsModule("foo")
     public static class Visited {
@@ -77,14 +84,12 @@ public class DevModeInitializerTest {
     public void setup() throws Exception {
 
         System.setProperty("user.dir", temporaryFolder.getRoot().getPath());
-        new File(getBaseDir(), "src").mkdir();
 
         createStubNode(false, true);
         createStubWebpackServer("Compiled", 1500);
 
         servletContext = Mockito.mock(ServletContext.class);
-        ServletRegistration registration = Mockito
-                .mock(ServletRegistration.class);
+        ServletRegistration registration = Mockito.mock(ServletRegistration.class);
         classes = new HashSet<>();
 
         Map registry = new HashMap();
@@ -93,6 +98,12 @@ public class DevModeInitializerTest {
                 .thenReturn(registry);
         Mockito.when(servletContext.getInitParameterNames())
                 .thenReturn(Collections.emptyEnumeration());
+
+
+        mainPackageFile = new File(getBaseDir(), PACKAGE_JSON);
+        appPackageFile = new File(getBaseDir(), DEFAULT_GENERATED_DIR + PACKAGE_JSON);
+        webpackFile = new File(getBaseDir(), WEBPACK_CONFIG);
+        appPackageFile.getParentFile().mkdirs();
 
         devModeInitializer = new DevModeInitializer();
     }
@@ -103,8 +114,10 @@ public class DevModeInitializerTest {
                 "vaadin." + SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_NPM);
         System.clearProperty(
                 "vaadin." + SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_IMPORTS);
-        new File(getBaseDir(), PACKAGE_JSON).delete();
-        new File(getBaseDir(), WEBPACK_CONFIG).delete();
+
+        webpackFile.delete();
+        mainPackageFile.delete();
+        appPackageFile.delete();
 
         // Reset unique instance in DevModeHandler
         Field atomicHandler = DevModeHandler.class
@@ -121,8 +134,8 @@ public class DevModeInitializerTest {
                 "vaadin." + SERVLET_PARAMETER_DEVMODE_SKIP_UPDATE_IMPORTS,
                 "true");
         devModeInitializer.onStartup(classes, servletContext);
-        assertFalse(new File(getBaseDir(), PACKAGE_JSON).canRead());
-        assertFalse(new File(getBaseDir(), WEBPACK_CONFIG).canRead());
+        assertFalse(mainPackageFile.canRead());
+        assertFalse(webpackFile.canRead());
         assertNull(DevModeHandler.getDevModeHandler());
     }
 
@@ -133,10 +146,35 @@ public class DevModeInitializerTest {
     }
 
     @Test
-    public void should_Run_Updaters_when_NodeConfFiles() throws Exception {
-        FileUtils.write(new File(getBaseDir(), PACKAGE_JSON), "{}", "UTF-8");
-        new File(getBaseDir(), WEBPACK_CONFIG).createNewFile();
+    public void should_Not_Run_Updaters_when_NoMainPackageFile() throws Exception {
+        appPackageFile.createNewFile();
+        webpackFile.createNewFile();
+        devModeInitializer.onStartup(classes, servletContext);
+        assertNull(DevModeHandler.getDevModeHandler());
+    }
 
+    @Test
+    public void should_Not_Run_Updaters_when_NoAppPackageFile() throws Exception {
+        mainPackageFile.createNewFile();
+        webpackFile.createNewFile();
+        devModeInitializer.onStartup(classes, servletContext);
+        assertNull(DevModeHandler.getDevModeHandler());
+    }
+
+    @Test
+    public void should_Not_Run_Updaters_when_NoWebpackFile() throws Exception {
+        mainPackageFile.createNewFile();
+        appPackageFile.createNewFile();
+        devModeInitializer.onStartup(classes, servletContext);
+        assertNull(DevModeHandler.getDevModeHandler());
+    }
+
+
+    @Test
+    public void should_Run_Updaters_when_NodeAllConfFiles() throws Exception {
+        FileUtils.write(mainPackageFile, "{}", "UTF-8");
+        FileUtils.write(appPackageFile, "{}", "UTF-8");
+        webpackFile.createNewFile();
         devModeInitializer.onStartup(classes, servletContext);
         assertNotNull(DevModeHandler.getDevModeHandler());
     }
