d4c3bb88c61445c991d1d1b1705accb3c3ebda1b
miki <miki@vaadin.com>
2019-05-20 11:13:24 +0300

    5565: mocking eq(class) instead of raw class in mocked method

--- flow-server/src/test/java/com/vaadin/flow/server/communication/WebComponentProviderTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/communication/WebComponentProviderTest.java
@@ -54,6 +54,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 
 @NotThreadSafe
@@ -79,7 +80,7 @@ public class WebComponentProviderTest {
         Mockito.when(request.getService()).thenReturn(service);
         Mockito.when(session.getService()).thenReturn(service);
         Mockito.when(service.getAttribute(WebComponentConfigurationRegistry.class)).thenReturn(new WebComponentConfigurationRegistry(){});
-        Mockito.when(service.getAttribute(WebComponentConfigurationRegistry.class, anyObject())).thenReturn(new WebComponentConfigurationRegistry(){});
+        Mockito.when(service.getAttribute(eq(WebComponentConfigurationRegistry.class), anyObject())).thenReturn(new WebComponentConfigurationRegistry(){});
         VaadinService.setCurrent(service);
         Mockito.when(service.getInstantiator())
                 .thenReturn(new MockInstantiator());
