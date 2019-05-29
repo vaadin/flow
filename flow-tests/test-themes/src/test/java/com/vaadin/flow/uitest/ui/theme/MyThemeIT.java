7600204d6205b16f360b257a83b1a655ada8fb0d
Denis Anisimov <denis@vaadin.com>
2019-05-29 09:11:24 +0300

    Avoid running bower mode only tests and npm only tests together

--- flow-tests/test-themes/src/test/java/com/vaadin/flow/uitest/ui/theme/MyThemeIT.java
+++ flow-tests/test-themes/src/test/java/com/vaadin/flow/uitest/ui/theme/MyThemeIT.java
@@ -17,14 +17,11 @@ package com.vaadin.flow.uitest.ui.theme;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import com.vaadin.flow.testcategory.IgnoreNPM;
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 
-@Category(IgnoreNPM.class)
 public class MyThemeIT extends ChromeBrowserTest {
 
     @Test
