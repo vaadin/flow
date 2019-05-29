4c64f7ab0545b32858718dbf749ab56118b31bbf
Artur Signell <artur@vaadin.com>
2019-05-29 16:16:04 +0300

    Avoid using Polymer for generated web components
    Fixes #5800

--- flow-server/src/main/java/com/vaadin/flow/server/webcomponent/WebComponentGenerator.java
+++ flow-server/src/main/java/com/vaadin/flow/server/webcomponent/WebComponentGenerator.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import org.apache.commons.io.IOUtils;
 
@@ -31,8 +30,6 @@ import com.vaadin.flow.component.WebComponentExporter;
 import com.vaadin.flow.component.webcomponent.WebComponentConfiguration;
 import com.vaadin.flow.shared.util.SharedUtil;
 
-import elemental.json.Json;
-import elemental.json.JsonObject;
 import elemental.json.JsonValue;
 
 /**
@@ -44,11 +41,18 @@ import elemental.json.JsonValue;
  * @author Vaadin Ltd.
  */
 public class WebComponentGenerator {
+    private static final String TOKEN_DEFAULT_VALUE = "_DefaultValue_";
+    private static final String TOKEN_JS_TYPE = "_JSType_";
+    private static final String TOKEN_ATTRIBUTE_NAME = "_AttributeName_";
+    private static final String TOKEN_CHANGE_EVENT_NAME = "_ChangeEventName_";
+    private static final String TOKEN_PROPERTY_NAME = "_PropertyName_";
     private static final String HTML_TEMPLATE = "webcomponent-template.html";
     private static final String JS_TEMPLATE = "webcomponent-template.js";
     private static final String SCRIPT_TEMPLATE = "webcomponent-script-template.js";
-
-    private static final String INDENTATION = "    ";
+    private static final String CODE_PROPERTY_DEFAULT = "webcomponent-property-default.js";
+    private static final String CODE_PROPERTY_VALUES = "webcomponent-property-values.js";
+    private static final String CODE_ATTRIBUTE_CHANGE = "webcomponent-attribute-change.js";
+    private static final String CODE_PROPERTY_METHODS = "webcomponent-property-methods.js";
 
     private WebComponentGenerator() {
     }
@@ -89,7 +93,7 @@ public class WebComponentGenerator {
      */
     public static String generateModule(
             Class<? extends WebComponentExporter<? extends Component>> exporterClass,
-            String frontendURI, boolean compatibilityMode) {
+                    String frontendURI, boolean compatibilityMode) {
         Objects.requireNonNull(exporterClass);
         Objects.requireNonNull(frontendURI);
 
@@ -152,79 +156,119 @@ public class WebComponentGenerator {
         replacements.put("TagCamel", SharedUtil
                 .capitalize(SharedUtil.dashSeparatedToCamelCase(tag)));
 
+        replacements.put("AttributeChange", getAttributeChange(
+                getStringResource(CODE_ATTRIBUTE_CHANGE), propertyDataSet));
         replacements.put("PropertyMethods", getPropertyMethods(
-                propertyDataSet.stream().map(PropertyData::getName)));
-
-        replacements.put("Properties", getPropertyDefinitions(propertyDataSet));
+                getStringResource(CODE_PROPERTY_METHODS), propertyDataSet));
+        replacements.put("PropertyDefaults", getPropertyDefaults(
+                getStringResource(CODE_PROPERTY_DEFAULT), propertyDataSet));
+        replacements.put("PropertyValues", getPropertyValues(
+                getStringResource(CODE_PROPERTY_VALUES), propertyDataSet));
 
         replacements.put("frontend_resources", frontendURI);
 
         replacements.put("ui_import",
                 generateUiImport
-                        ? "<link rel='import' href='web-component-ui.html'>"
+                ? "<link rel='import' href='web-component-ui.html'>"
                         : "");
 
         return replacements;
     }
 
-    private static String getPropertyDefinitions(
+    private static String getPropertyMethods(String codePropertyMethods,
             Set<PropertyData<?>> properties) {
-        JsonObject props = Json.createObject();
-
+        StringBuilder setters = new StringBuilder();
         for (PropertyData<?> property : properties) {
-            JsonObject prop = createPropertyDefinition(property);
-            props.put(property.getName(), prop);
+            setters.append(
+                    createPropertySetterGetter(codePropertyMethods, property));
         }
-        return props.toJson();
+        return setters.toString();
     }
 
-    private static JsonObject createPropertyDefinition(
+    private static String getPropertyDefaults(String codePropertyDefault,
+            Set<PropertyData<?>> properties) {
+        StringBuilder setters = new StringBuilder();
+        for (PropertyData<?> property : properties) {
+            setters.append(
+                    createPropertyDefault(codePropertyDefault, property));
+        }
+        return setters.toString();
+    }
+
+    private static String getPropertyValues(String codePropertyValues,
+            Set<PropertyData<?>> properties) {
+        if (properties.isEmpty()) {
+            return "{}";
+        }
+        StringBuilder sync = new StringBuilder();
+        sync.append("{");
+        for (PropertyData<?> property : properties) {
+            sync.append(codePropertyValues.replace(TOKEN_PROPERTY_NAME,
+                    property.getName()));
+            sync.append(",");
+        }
+        sync.delete(sync.length() - 1, sync.length());
+        sync.append("}");
+        return sync.toString();
+    }
+
+    private static String getAttributeChange(String codeAttributeChange,
+            Set<PropertyData<?>> properties) {
+        StringBuilder sync = new StringBuilder();
+        for (PropertyData<?> property : properties) {
+            sync.append(codeAttributeChange
+                    .replace(TOKEN_ATTRIBUTE_NAME, property.getAttributeName())
+                    .replace(TOKEN_PROPERTY_NAME, property.getName())
+                    .replace(TOKEN_JS_TYPE, property.getJSType()));
+        }
+        return sync.toString();
+    }
+
+    private static String createPropertyDefault(String code,
             PropertyData<?> property) {
-        JsonObject prop = Json.createObject();
+        // Note about the JS code:
+        // If a property has been set before the element is upgraded, it needs
+        // to be deleted so the getter/setters are used
 
-        prop.put("type", property.getType().getSimpleName());
+        return code.replace(TOKEN_PROPERTY_NAME, property.getName())
+                .replace(TOKEN_DEFAULT_VALUE, getDefaultJsValue(property));
+    }
 
-        if (property.getDefaultValue() != null) {
-            String propertyValue = "value";
-            if (property.getType() == Boolean.class) {
-                prop.put(propertyValue, (Boolean) property.getDefaultValue());
-            } else if (property.getType() == Double.class) {
-                prop.put(propertyValue, (Double) property.getDefaultValue());
-            } else if (property.getType() == Integer.class) {
-                prop.put(propertyValue, (Integer) property.getDefaultValue());
-            } else if (property.getType() == String.class) {
-                prop.put(propertyValue, (String) property.getDefaultValue());
-            } else if (JsonValue.class.isAssignableFrom(property.getType())) {
-                prop.put(propertyValue, (JsonValue) property.getDefaultValue());
-            } else {
-                throw new UnsupportedPropertyTypeException(String.format(
-                        "%s is not a currently supported type for a Property."
-                                + " Please use %s instead.",
-                        property.getType().getSimpleName(),
-                        JsonValue.class.getSimpleName()));
-            }
+    private static String getDefaultJsValue(PropertyData<?> property) {
+        String value;
+        if (property.getDefaultValue() == null) {
+            value = "undefined";
+        } else if (property.getType() == Boolean.class) {
+            value = String.valueOf((property.getDefaultValue()));
+        } else if (property.getType() == Double.class) {
+            value = String.valueOf((property.getDefaultValue()));
+        } else if (property.getType() == Integer.class) {
+            value = String.valueOf((property.getDefaultValue()));
+        } else if (property.getType() == String.class) {
+            value = "'" + ((String) property.getDefaultValue()).replaceAll("'",
+                    "\\'") + "'";
+        } else if (JsonValue.class.isAssignableFrom(property.getType())) {
+            value = ((JsonValue) property.getDefaultValue()).toJson();
+        } else {
+            throw new UnsupportedPropertyTypeException(String.format(
+                    "%s is not a currently supported type for a Property."
+                            + " Please use %s instead.",
+                            property.getType().getSimpleName(),
+                            JsonValue.class.getSimpleName()));
         }
-        prop.put("observer", getSyncMethod(property.getName()));
-        prop.put("notify", true);
-        prop.put("reflectToAttribute", false);
+        if (value == null) {
+            value = "null";
+        }
 
-        return prop;
+        return value;
     }
 
-    private static String getSyncMethod(String property) {
-        return "_sync_" + SharedUtil.dashSeparatedToCamelCase(property);
+    private static String createPropertySetterGetter(String codePropertyMethods,
+            PropertyData<?> property) {
+        return codePropertyMethods
+                .replace(TOKEN_PROPERTY_NAME, property.getName())
+                .replace(TOKEN_CHANGE_EVENT_NAME,
+                        property.getAttributeName() + "-changed");
     }
 
-    private static String getPropertyMethods(Stream<String> properties) {
-        StringBuilder methods = new StringBuilder();
-        properties.forEach(property -> {
-            methods.append(INDENTATION);
-            methods.append(getSyncMethod(property));
-            methods.append("(newValue, oldValue) { ");
-            methods.append("this._sync('").append(property)
-                    .append("', newValue);");
-            methods.append("}\n");
-        });
-        return methods.toString();
-    }
 }
