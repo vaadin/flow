09c0e815576835fa189273ac678a253dbf6f3b72
Denis Anisimov <denis@vaadin.com>
2019-06-04 14:41:49 +0300

    Fix code style issues in stop dev mode context listener.

--- flow-server/src/main/java/com/vaadin/flow/server/startup/StopDevMode.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/StopDevMode.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.vaadin.flow.server.startup;
 
 import javax.servlet.ServletContextEvent;
@@ -8,6 +24,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import com.vaadin.flow.server.DevModeHandler;
 
+/**
+ * Stops {@link DevModeHandler} if there is no anymore initialized servlet
+ * context instances.
+ * 
+ * @author Vaadin Ltd
+ *
+ */
 class StopDevMode implements ServletContextListener, Serializable {
 
     private static final AtomicInteger SERVLET_CONTEXTS = new AtomicInteger();
