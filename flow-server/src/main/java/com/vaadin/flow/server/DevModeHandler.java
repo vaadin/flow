14d93596fd30eae031fc699de12636eb1e96387d
Joni Uitto <joni@vaadin.com>
2019-06-06 13:03:00 +0300

    Formatting changes after rebase

--- flow-server/src/main/java/com/vaadin/flow/server/DevModeHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/DevModeHandler.java
@@ -15,7 +15,6 @@
  */
 package com.vaadin.flow.server;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -48,7 +47,6 @@ import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_WEBPACK
 import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_WEBPACK_RUNNING_PORT;
 import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_WEBPACK_SUCCESS_PATTERN;
 import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_DEVMODE_WEBPACK_TIMEOUT;
-import static com.vaadin.flow.server.Constants.VAADIN_PREFIX;
 import static com.vaadin.flow.server.Constants.VAADIN_MAPPING;
 import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendUtils.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendUtils.java
@@ -29,11 +29,12 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.server.Constants;
 import com.vaadin.flow.server.VaadinService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.vaadin.flow.server.Constants.SERVLET_PARAMETER_STATISTICS_JSON;
 import static com.vaadin.flow.server.Constants.STATISTICS_JSON_DEFAULT;
@@ -41,8 +42,8 @@ import static com.vaadin.flow.server.Constants.VAADIN_SERVLET_RESOURCES;
 import static com.vaadin.flow.server.DevModeHandler.DevModePort;
 
 /**
- * A class for static methods and definitions that might be
- * used in different locations.
+ * A class for static methods and definitions that might be used in different
+ * locations.
  */
 public class FrontendUtils {
 
@@ -75,12 +76,13 @@ public class FrontendUtils {
      *
      * By default it is <code>/frontend</code> in the project folder.
      */
-    public static final String DEFAULT_FRONTEND_DIR = DEFAULT_NODE_DIR + FRONTEND;
+    public static final String DEFAULT_FRONTEND_DIR = DEFAULT_NODE_DIR
+            + FRONTEND;
 
     /**
      * The name of the webpack configuration file.
      */
-    public static final String WEBPACK_CONFIG ="webpack.config.js";
+    public static final String WEBPACK_CONFIG = "webpack.config.js";
 
     /**
      * The NPM package name that will be used for the javascript files present
@@ -101,9 +103,9 @@ public class FrontendUtils {
     public static final String DEFAULT_GENERATED_DIR = TARGET + FRONTEND;
 
     /**
-     * Name of the file that contains application imports, javascript, theme and style annotations.
-     * It is also the entry-point for webpack.
-     * It is always generated in the {@link FrontendUtils#DEFAULT_GENERATED_DIR} folder.
+     * Name of the file that contains application imports, javascript, theme and
+     * style annotations. It is also the entry-point for webpack. It is always
+     * generated in the {@link FrontendUtils#DEFAULT_GENERATED_DIR} folder.
      */
     public static final String IMPORTS_NAME = "generated-flow-imports.js";
 
@@ -114,14 +116,16 @@ public class FrontendUtils {
     public static final String PARAM_GENERATED_DIR = "vaadin.frontend.generated.folder";
 
     /**
-     * A parameter for overriding the
-     * {@link FrontendUtils#DEFAULT_FRONTEND_DIR} folder.
+     * A parameter for overriding the {@link FrontendUtils#DEFAULT_FRONTEND_DIR}
+     * folder.
      */
     public static final String PARAM_FRONTEND_DIR = "vaadin.frontend.frontend.folder";
 
     /**
-     * A special prefix used by webpack to map imports placed in the {@link FrontendUtils#DEFAULT_FRONTEND_DIR}.
-     * e.g. <code>import 'Frontend/foo.js';</code> references the file<code>frontend/foo.js</code>.
+     * A special prefix used by webpack to map imports placed in the
+     * {@link FrontendUtils#DEFAULT_FRONTEND_DIR}. e.g.
+     * <code>import 'Frontend/foo.js';</code> references the
+     * file<code>frontend/foo.js</code>.
      */
     public static final String WEBPACK_PREFIX_ALIAS = "Frontend/";
 
@@ -131,14 +135,13 @@ public class FrontendUtils {
     public static final String TOKEN_FILE = "build/flow-build-info.json";
 
     /**
-     * A parameter informing about the location of the {@link FrontendUtils#TOKEN_FILE}.
+     * A parameter informing about the location of the
+     * {@link FrontendUtils#TOKEN_FILE}.
      */
     public static final String PARAM_TOKEN_FILE = "vaadin.frontend.token.file";
 
-    private static final String NOT_FOUND =
-            "%n%n======================================================================================================"
-            + "%nFailed to determine '%s' tool."
-            + "%nPlease install it either:"
+    private static final String NOT_FOUND = "%n%n======================================================================================================"
+            + "%nFailed to determine '%s' tool." + "%nPlease install it either:"
             + "%n  - by following the https://nodejs.org/en/download/ guide to install it globally"
             + "%n  - or by running the frontend-maven-plugin goal to install it in this project:"
             + "%n  $ mvn com.github.eirslett:frontend-maven-plugin:1.7.6:install-node-and-npm -DnodeVersion=\"v11.6.0\" "
@@ -183,7 +186,8 @@ public class FrontendUtils {
      * @return folder location
      */
     public static String getBaseDir() {
-        return System.getProperty(PROJECT_BASEDIR, System.getProperty("user.dir", "."));
+        return System.getProperty(PROJECT_BASEDIR,
+                System.getProperty("user.dir", "."));
     }
 
     /**
@@ -193,7 +197,8 @@ public class FrontendUtils {
      */
     public static String getNodeExecutable() {
         String command = isWindows() ? "node.exe" : "node";
-        String defaultNode = FrontendUtils.isWindows() ? "node/node.exe" : "node/node";
+        String defaultNode = FrontendUtils.isWindows() ? "node/node.exe"
+                : "node/node";
         return getExecutable(command, defaultNode).getAbsolutePath();
     }
 
@@ -204,10 +209,13 @@ public class FrontendUtils {
      *         to have npm running
      */
     public static List<String> getNpmExecutable() {
-        // If `node` is not found in PATH, `node/node_modules/npm/bin/npm` will not work
-        // because it's a shell or windows script that looks for node and will fail.
+        // If `node` is not found in PATH, `node/node_modules/npm/bin/npm` will
+        // not work
+        // because it's a shell or windows script that looks for node and will
+        // fail.
         // Thus we look for the `mpn-cli` node script instead
-        File file = new File(getBaseDir(), "node/node_modules/npm/bin/npm-cli.js");
+        File file = new File(getBaseDir(),
+                "node/node_modules/npm/bin/npm-cli.js");
         if (file.canRead()) {
             // We return a two element list with node binary and npm-cli script
             return Arrays.asList(getNodeExecutable(), file.getAbsolutePath());
@@ -220,11 +228,13 @@ public class FrontendUtils {
     private static File getExecutable(String cmd, String defaultLocation) {
         File file = null;
         try {
-            file = defaultLocation == null ? frontendToolsLocator.tryLocateTool(cmd).orElse(null)
+            file = defaultLocation == null
+                    ? frontendToolsLocator.tryLocateTool(cmd).orElse(null)
                     : Optional.of(new File(getBaseDir(), defaultLocation))
                             .filter(frontendToolsLocator::verifyTool)
-                            .orElseGet(() -> frontendToolsLocator.tryLocateTool(cmd).orElse(null));
-        } catch (Exception e) { //NOSONAR
+                            .orElseGet(() -> frontendToolsLocator
+                                    .tryLocateTool(cmd).orElse(null));
+        } catch (Exception e) { // NOSONAR
             // There are IOException coming from process fork
         }
         if (file == null) {
@@ -242,13 +252,15 @@ public class FrontendUtils {
      */
     public static String streamToString(InputStream inputStream) {
         String ret = "";
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(inputStream, StandardCharsets.UTF_8.name()))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                inputStream, StandardCharsets.UTF_8.name()))) {
 
-            ret = br.lines().collect(Collectors.joining(System.lineSeparator()));
+            ret = br.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
         } catch (IOException exception) {
             // ignore exception on close()
-            LoggerFactory.getLogger(FrontendUtils.class).warn("Couldn't close template input stream", exception);
+            LoggerFactory.getLogger(FrontendUtils.class)
+                    .warn("Couldn't close template input stream", exception);
         }
         return ret;
     }
@@ -296,7 +308,8 @@ public class FrontendUtils {
      * @throws IOException
      *             on error reading stats file.
      */
-    public static String getStatsContent(VaadinService service) throws IOException {
+    public static String getStatsContent(VaadinService service)
+            throws IOException {
         DeploymentConfiguration config = service.getDeploymentConfiguration();
         String stats = config
                 .getStringProperty(SERVLET_PARAMETER_STATISTICS_JSON,
@@ -305,15 +318,18 @@ public class FrontendUtils {
                 .replaceFirst("^/", "");
 
         // Try stats as a resource from the class path
-        InputStream content = service.getClassLoader().getResourceAsStream(stats);
+        InputStream content = service.getClassLoader()
+                .getResourceAsStream(stats);
         if (content != null) {
-            getLogger().debug("Found stats file as a resource file '{}'.", stats);
+            getLogger().debug("Found stats file as a resource file '{}'.",
+                    stats);
         } else {
             URL statsUrl = null;
             if (config.isProductionMode()) {
                 statsUrl = service.getStaticResource("/" + stats);
             } else {
-                statsUrl = getStatsFromWebpack(service, config, stats, statsUrl);
+                statsUrl = getStatsFromWebpack(service, config, stats,
+                        statsUrl);
             }
             if (statsUrl != null) {
                 getLogger().debug("Found stats file at url '{}'", statsUrl);
@@ -323,22 +339,27 @@ public class FrontendUtils {
         return content != null ? streamToString(content) : null;
     }
 
-    private static URL getStatsFromWebpack(VaadinService service, DeploymentConfiguration config, String stats,
-            URL statsUrl) throws MalformedURLException {
+    private static URL getStatsFromWebpack(VaadinService service,
+            DeploymentConfiguration config, String stats, URL statsUrl)
+            throws MalformedURLException {
         DevModePort port = service.getContext().getAttribute(DevModePort.class);
         if (port != null) {
-            statsUrl = new URL("http://localhost:" + port.getValue() + "/" + stats);
+            statsUrl = new URL(
+                    "http://localhost:" + port.getValue() + "/" + stats);
         }
         if (statsUrl == null) {
             statsUrl = service.getStaticResource("/" + stats);
             if (statsUrl == null) {
                 getLogger().warn(
                         "Cannot get the stats file through webpack-dev-server. "
-                        + "The webpack port is unavailable via VaadinContext.");
+                                + "The webpack port is unavailable via VaadinContext.");
             } else {
-                getLogger().debug("Cannot get the stats file through webpack-dev-server, "
-                        + "however it was found in the web contenxt, which means that the application was build previously. "
-                        + "To disable this message run the application in PRODUCTION mode.");
+                getLogger().debug(
+                        "Cannot get the stats file through webpack-dev-server, "
+                                + "however it was found in the web context, " +
+                                "which means that the application was build " +
+                                "previously. To disable this message run the " +
+                                "application in PRODUCTION mode.");
             }
         }
         return statsUrl;
