00573a94e1492f7c1b8bce4c022f9af8339bd4c9
Denis Anisimov <denis@vaadin.com>
2019-06-13 18:23:37 +0300

    Fix upgrade element code in P2.

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/template/UpgradeElementIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/template/UpgradeElementIT.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 import com.vaadin.testbench.TestBenchElement;
@@ -34,8 +35,8 @@ public class UpgradeElementIT extends ChromeBrowserTest {
 
         TestBenchElement template = $(TestBenchElement.class).id("template");
         WebElement input = template.$(TestBenchElement.class).id("input");
-        input.sendKeys("foo");
-        input.sendKeys(Keys.ENTER);
+        new Actions(getDriver()).click(input).sendKeys("foo")
+                .sendKeys(Keys.ENTER).build().perform();
         WebElement result = findElement(By.id("text-update"));
         Assert.assertEquals("foo", result.getText());
     }
