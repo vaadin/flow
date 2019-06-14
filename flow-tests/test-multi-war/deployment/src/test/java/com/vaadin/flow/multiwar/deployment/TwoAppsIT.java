23255918288434344c871d9aa5345555862dac3c
Artur Signell <artur@vaadin.com>
2019-06-14 14:46:58 +0300

    Test for multiple wars deployed to the same server

---
+++ flow-tests/test-multi-war/deployment/src/test/java/com/vaadin/flow/multiwar/deployment/TwoAppsIT.java
@@ -0,0 +1,42 @@
+package com.vaadin.flow.multiwar.deployment;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
+public class TwoAppsIT extends ChromeBrowserTest {
+    @Override
+    protected String getTestPath() {
+        return "/both.html";
+    }
+
+    @Test
+    public void testWar1Works() {
+        testWarWorks("war1");
+    }
+
+    @Test
+    public void testWar2Works() {
+        testWarWorks("war2");
+    }
+
+    private void testWarWorks(String warId) {
+        getDriver()
+        .get(getTestURL(getRootURL(), "/test-" + warId,
+                        new String[] {}));
+        WebElement helloText = findElement(By.id("hello"));
+        Assert.assertEquals(
+                "Hello from com.vaadin.flow.multiwar." + warId + ".MainView",
+                helloText.getText());
+        helloText.click();
+        Assert.assertEquals(
+                "Hello Hello from com.vaadin.flow.multiwar." + warId
+                        + ".MainView",
+                helloText.getText());
+
+    }
+
+}
