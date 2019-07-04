a8fde61126919ff3c6804ad5d2bf9a24718a0b0e
Manolo Carrasco <manolo@apache.org>
2019-07-04 07:42:13 +0200

    Add test module for spring

---
+++ flow-tests/test-spring-misc/src/main/java/com/vaadin/flow/misc/ui/TestComponent.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.misc.ui;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.templatemodel.TemplateModel;
+
+/**
+ * A Web Component that extends vaadin-element-mixin
+ * so as we enable statistics.
+ */
+@Tag("test-component")
+@JsModule("./src/test-component.js")
+@HtmlImport("frontend://src/test-component.html")
+@NpmPackage(value = "@vaadin/vaadin-element-mixin", version = "2.1.3")
+public class TestComponent extends PolymerTemplate<TemplateModel> {
+
+    @Id
+    NativeButton button;
+    @Id
+    Div content;
+
+    public TestComponent() {
+        button.addClickListener(e -> {
+            content.setText("Hello World");
+        });
+    }
+}
