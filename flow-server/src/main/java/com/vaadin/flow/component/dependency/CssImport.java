b74226773ec85c8bfb79218f0c40a731c02b2496
Manolo Carrasco <manolo@apache.org>
2019-07-05 14:51:21 +0200

    Add a new annotation for CSS imports

---
+++ flow-server/src/main/java/com/vaadin/flow/component/dependency/CssImport.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.dependency;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * An annotation for instructing flow to import a CSS file into the application
+ * bundle.
+ * <p>
+ * Depending on the attributes provided, the CSS content will be appended in
+ * different ways:
+ * 
+ * <ul>
+ * <li>When specifying only the 'value', it will be appended to the
+ * 'document.head' inside a '&lt;custom-style>' block.
+ * 
+ * <pre>
+ *  &lt;custom-style>
+ *    &lt;style>
+ *      CSS-CONTENT
+ *    &lt;/style>
+ *  &lt;/custom-style>
+ * </pre>
+ * 
+ * <li>When specifying 'value' and 'include', it is appended inside a
+ * '&lt;custom-style>' block, but 'include' value is used for including a
+ * defined module.
+ * 
+ * <pre>
+ *  &lt;custom-style>
+ *    &lt;style include="INCLUDE-VALUE">
+ *      CSS-CONTENT
+ *    &lt;/style>
+ *  &lt;/custom-style>
+ * </pre>
+ * 
+ * <li>When 'value' and 'id' are given, a new 'dom-module' with the provided
+ * 'id' is created
+ * 
+ * <pre>
+ * &lt;dom-module id="ID-VALUE">
+ *   &lt;template>
+ *     &lt;style>
+ *       CSS-CONTENT
+ *     &lt;/style>
+ *   &lt;/template>
+ * &lt;/dom-module>
+ * </pre>
+ * 
+ * <li>When 'value' and 'themeFor' are given, a new 'dom-module' for customizing
+ * a themable element is created created
+ * 
+ * <pre>
+ * &lt;dom-module id="RANDOM-ID" theme-for="THEME-FOR-VALUE">
+ *   &lt;template>
+ *     &lt;style>
+ *       CSS-CONTENT
+ *     &lt;/style>
+ *   &lt;/template>
+ * &lt;/dom-module>
+ * </pre>
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Repeatable(CssImport.Container.class)
+public @interface CssImport {
+
+    /**
+     * Location of the file with the CSS content.
+     * 
+     * @return the value.
+     */
+    String value();
+    
+    /**
+     * The 'id' of a module to include in the generated 'custom-style'.
+     * 
+     * @return the include value.
+     */
+    String include() default "";
+    
+    /**
+     * The 'id' of the new 'dom-module' created.
+     * 
+     * @return the id.
+     */
+    String id() default "";
+    
+    /**
+     * The tag name of the themable element that the generated 'dom-module' will
+     * target.
+     * 
+     * @return the themable element.
+     */
+    String themeFor() default "";
+
+    /**
+     * Internal annotation to enable use of multiple {@link CssImport}
+     * annotations.
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Documented
+    @interface Container {
+
+        /**
+         * Internally used to enable use of multiple {@link CssImport}
+         * annotations.
+         *
+         * @return an array of the JavaScript annotations
+         */
+        CssImport[] value();
+    }
+}
