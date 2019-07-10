14740794945c00262f09776bb365bb52cdc5e406
Joni Uitto <joni@vaadin.com>
2019-07-10 16:57:35 +0300

    Fixed windows test failures in NodeUpdateImportsTest

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateImports.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateImports.java
@@ -176,7 +176,7 @@ public class TaskUpdateImports extends NodeUpdater {
     }
 
     private void addLines(Collection<String> lines, String content) {
-        lines.addAll(Arrays.asList(content.split("\n")));
+        lines.addAll(Arrays.asList(content.replace("\r", "").split("\n")));
     }
 
     private Collection<String> getCssLines() {
