865a6ae7d042a001396e317937358775183560a0
Johannes Eriksson <joheriks@vaadin.com>
2019-07-16 15:18:35 +0300

    Remove unused import

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateImports.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateImports.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
