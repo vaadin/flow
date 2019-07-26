001556aeb3e6a745f4e1f593488f43c6436c6a10
Mehdi Javan <32511762+mehdi-vaadin@users.noreply.github.com>
2019-07-26 15:21:08 +0300

    Add IT for whitelisted-packages vaadin/spring#469

---
+++ flow-tests/vaadin-spring-tests/test-spring-white-list/src/main/java/com/vaadin/flow/spring/test/whitelist/TestServletInitializer.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.spring.test.whitelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+@EnableAuthorizationServer
+@Configuration
+@EnableWebSecurity
+public class TestServletInitializer implements AuthorizationServerConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestServletInitializer.class, args);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security)
+            throws Exception {
+        // doesn't need any impl
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)
+            throws Exception {
+        clients.inMemory().build();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+            throws Exception {
+        // doesn't need any impl
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        return multipartResolver;
+    }
+}
