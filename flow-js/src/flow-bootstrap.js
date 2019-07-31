f7505d6e0dc27ccb6c7c4014d49b542e24f04862
Manolo Carrasco <manolo@apache.org>
2019-07-31 09:26:45 +0200

    CCDM: Adding a npm package for flow JS client API

---
+++ flow-js/src/flow-bootstrap.js
@@ -0,0 +1,4 @@
+
+// Intentionally empty for now.
+// Will have the content of flow BootstrapHandler.js
+(function() {})();
---
+++ flow-js/src/flow-client.js
@@ -0,0 +1,7 @@
+// Intentionally empty for now.
+// Will have the content of the gwt compiled module
+function client(){
+}
+client();
+
+({})();
