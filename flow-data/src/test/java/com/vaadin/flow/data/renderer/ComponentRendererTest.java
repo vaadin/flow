3fc964fc40bde898b77b36e56b71faf0893fe062
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2019-08-02 12:20:24 +0300

    Add 2 test cases to simulates the bug #710 of vaadin-grid-flow

--- flow-data/src/test/java/com/vaadin/flow/data/renderer/ComponentRendererTest.java
+++ flow-data/src/test/java/com/vaadin/flow/data/renderer/ComponentRendererTest.java
@@ -15,15 +15,118 @@
  */
 package com.vaadin.flow.data.renderer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import com.vaadin.flow.data.binder.testcomponents.TestLabel;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.internal.PendingJavaScriptInvocation;
+import com.vaadin.flow.component.internal.UIInternals;
+import com.vaadin.flow.data.binder.testcomponents.TestLabel;
+import com.vaadin.flow.data.provider.ComponentDataGenerator;
+import com.vaadin.flow.data.provider.KeyMapper;
+import com.vaadin.flow.dom.Element;
 
 public class ComponentRendererTest {
+
+
+    private static class TestUIInternals extends UIInternals {
+
+        private List<PendingJavaScriptInvocation> invocations = new ArrayList<>();
+
+        public TestUIInternals(UI ui) {
+            super(ui);
+        }
+
+        @Override
+        public void addJavaScriptInvocation(
+                PendingJavaScriptInvocation invocation) {
+            invocations.add(invocation);
+        }
+
+    }
+
+    private static class TestUI extends UI {
+
+        private UIInternals internals;
+
+        @Override
+        public UIInternals getInternals() {
+            if (internals == null) {
+                internals = new TestUIInternals(this);
+            }
+            return internals;
+        }
+    }
+
+    @Test
+    public void templateRenderered_() {
+        UI ui = new TestUI();
+        TestUIInternals internals = (TestUIInternals) ui.getInternals();
+        
+               
+        ComponentRenderer<TestLabel,String> renderer = new ComponentRenderer<>(e -> (new TestLabel()));
+        
+        Element containerParent = new Element("div");
+        Element container = new Element("div");
+
+        KeyMapper<String> keyMapper = new KeyMapper<>();
+
+        ComponentDataGenerator<String> rendering = (ComponentDataGenerator<String>) renderer.render(container, keyMapper);
+
+        // simulate a call from the grid to refresh data - template is not setup
+        containerParent.getNode()
+                .runWhenAttached(ui2 -> ui2.getInternals().getStateTree()
+                        .beforeClientResponse(containerParent.getNode(),
+                                context -> {
+                                    Assert.assertNotNull("NodeIdPropertyName should not be null", rendering.getNodeIdPropertyName() );
+                        }));
+
+        // attach the parent (ex: grid) before the child (ex: column)
+        attachElement(ui, containerParent);
+        attachElement(ui, container);
+
+        internals.getStateTree().runExecutionsBeforeClientResponse();
+
+    }
+
+    @Test
+    public void templateRenderered_CR2() {
+        UI ui = new TestUI();
+        TestUIInternals internals = (TestUIInternals) ui.getInternals();
+        
+               
+        ComponentRenderer<TestLabel,String> renderer = new ComponentRenderer<>(e -> (new TestLabel()));
+        
+        Element containerParent = new Element("div");
+        Element container = new Element("div");
+        KeyMapper<String> keyMapper = new KeyMapper<>();
+
+        ComponentDataGenerator<String> rendering = (ComponentDataGenerator<String>) renderer.render(container, keyMapper);
+
+        containerParent.getNode()
+                .runWhenAttached(ui2 -> ui2.getInternals().getStateTree()
+                        .beforeClientResponse(containerParent.getNode(),
+                                context -> {
+                                	// if nodeid is null then the component won't be rendered correctly
+                                    Assert.assertNotNull("NodeIdPropertyName should not be null", rendering.getNodeIdPropertyName() );
+                        }));
+        // attach the child (ex: container) before the parent (ex: grid)
+        attachElement(ui, container);
+        attachElement(ui, containerParent);
+        
+
+        internals.getStateTree().runExecutionsBeforeClientResponse();
+
+    }
+
+    private void attachElement(UI ui, Element contentTemplate) {
+        ui.getElement().appendChild(contentTemplate);
+    }
 
     @Test
     public void componentFunction_invokedOnCreate() {
