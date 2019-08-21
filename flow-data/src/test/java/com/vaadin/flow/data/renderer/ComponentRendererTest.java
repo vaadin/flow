c473c5d7d780a1f1d17348f866f0b4d9d370c2a8
Jean-Christophe Gueriaud <51313578+jcgueriaud1@users.noreply.github.com>
2019-08-21 07:42:57 +0300

    Setup the template of the renderer earlier (#6144)
    Run setupTemplate earlier
    
    Add 2 test cases that simulate the bug #710 of vaadin-grid-flow
    
    # Conflicts:
    #	flow-data/src/test/java/com/vaadin/flow/data/renderer/ComponentRendererTest.java

--- flow-data/src/test/java/com/vaadin/flow/data/renderer/ComponentRendererTest.java
+++ flow-data/src/test/java/com/vaadin/flow/data/renderer/ComponentRendererTest.java
@@ -15,15 +15,135 @@
  */
 package com.vaadin.flow.data.renderer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.internal.PendingJavaScriptInvocation;
+import com.vaadin.flow.component.internal.UIInternals;
+import com.vaadin.flow.data.binder.testcomponents.TestLabel;
+import com.vaadin.flow.data.provider.ComponentDataGenerator;
+import com.vaadin.flow.data.provider.KeyMapper;
+import com.vaadin.flow.dom.Element;
+
+import elemental.json.Json;
+import elemental.json.JsonObject;
 
 public class ComponentRendererTest {
+
+    private static class TestUIInternals extends UIInternals {
+
+        private List<PendingJavaScriptInvocation> invocations = new ArrayList<>();
+
+        public TestUIInternals(UI ui) {
+            super(ui);
+        }
+
+        @Override
+        public void addJavaScriptInvocation(
+                PendingJavaScriptInvocation invocation) {
+            invocations.add(invocation);
+        }
+
+    }
+
+    private static class TestUI extends UI {
+
+        private UIInternals internals;
+
+        @Override
+        public UIInternals getInternals() {
+            if (internals == null) {
+                internals = new TestUIInternals(this);
+            }
+            return internals;
+        }
+    }
+
+    @Test
+    public void templateRenderered_parentAttachedBeforeChild() {
+        UI ui = new TestUI();
+        TestUIInternals internals = (TestUIInternals) ui.getInternals();
+
+        ComponentRenderer<TestLabel, String> renderer = new ComponentRenderer<>(
+                e -> (new TestLabel()));
+
+        Element containerParent = new Element("div");
+        Element container = new Element("div");
+
+        KeyMapper<String> keyMapper = new KeyMapper<>();
+
+        ComponentDataGenerator<String> rendering = (ComponentDataGenerator<String>) renderer
+                .render(container, keyMapper);
+
+        // simulate a call from the grid to refresh data - template is not setup
+        containerParent.getNode().runWhenAttached(
+                ui2 -> ui2.getInternals().getStateTree()
+                        .beforeClientResponse(containerParent.getNode(),
+                                context -> {
+                                    Assert.assertNotNull(
+                                            "NodeIdPropertyName should not be null",
+                                            rendering.getNodeIdPropertyName());
+                                    JsonObject value = Json.createObject();
+                                    rendering.generateData("item", value);
+                                    Assert.assertEquals(
+                                            "generateData should add one element in the jsonobject",
+                                            1, value.keys().length);
+                                }));
+
+        // attach the parent (ex: grid) before the child (ex: column)
+        attachElement(ui, containerParent);
+        attachElement(ui, container);
+
+        internals.getStateTree().runExecutionsBeforeClientResponse();
+
+    }
+
+    @Test
+    public void templateRenderered_childAttachedBeforeParent() {
+        UI ui = new TestUI();
+        TestUIInternals internals = (TestUIInternals) ui.getInternals();
+
+        ComponentRenderer<TestLabel, String> renderer = new ComponentRenderer<>(
+                e -> (new TestLabel()));
+
+        Element containerParent = new Element("div");
+        Element container = new Element("div");
+        KeyMapper<String> keyMapper = new KeyMapper<>();
+
+        ComponentDataGenerator<String> rendering = (ComponentDataGenerator<String>) renderer
+                .render(container, keyMapper);
+
+        containerParent.getNode().runWhenAttached(
+                ui2 -> ui2.getInternals().getStateTree()
+                        .beforeClientResponse(containerParent.getNode(),
+                                context -> {
+                                    // if nodeid is null then the component won't be rendered correctly
+                                    Assert.assertNotNull(
+                                            "NodeIdPropertyName should not be null",
+                                            rendering.getNodeIdPropertyName());
+                                    JsonObject value = Json.createObject();
+                                    rendering.generateData("item", value);
+                                    Assert.assertEquals(
+                                            "generateData should add one element in the jsonobject",
+                                            1, value.keys().length);
+                                }));
+        // attach the child (ex: container) before the parent (ex: grid)
+        attachElement(ui, container);
+        attachElement(ui, containerParent);
+
+        internals.getStateTree().runExecutionsBeforeClientResponse();
+
+    }
+
+    private void attachElement(UI ui, Element contentTemplate) {
+        ui.getElement().appendChild(contentTemplate);
+    }
 
     @Test
     public void componentFunction_invokedOnCreate() {
@@ -73,14 +193,14 @@ public class ComponentRendererTest {
                     Assert.assertEquals("New item", item);
                     return new Div();
                 }, (component, item) -> {
-                    updateInvocations.incrementAndGet();
-                    Assert.assertEquals("Updated item", item);
-                    return component;
-                });
+            updateInvocations.incrementAndGet();
+            Assert.assertEquals("Updated item", item);
+            return component;
+        });
 
-        Div div = renderer.createComponent("New item");
-        Component updatedComponent = renderer.updateComponent(div,
-                "Updated item");
+        TestLabel div = renderer.createComponent("New item");
+        Component updatedComponent = renderer
+                .updateComponent(div, "Updated item");
 
         Assert.assertEquals(
                 "The component creation function should have been invoked once",
