7d18bf43b7dc7afc93597c8fcc7113bf545fa6cb
Joni Uitto <joni@vaadin.com>
2019-09-04 15:53:06 +0300

    Fix ITs for no-protocol cases

--- flow-tests/test-npm-only-features/test-npm-general/src/test/java/com/vaadin/flow/testnpmonlyfeatures/general/ExternalJSModuleIT.java
+++ flow-tests/test-npm-only-features/test-npm-general/src/test/java/com/vaadin/flow/testnpmonlyfeatures/general/ExternalJSModuleIT.java
@@ -18,14 +18,21 @@ package com.vaadin.flow.testnpmonlyfeatures.general;
 
 import java.util.List;
 
-import com.vaadin.flow.testutil.ChromeBrowserTest;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
 public class ExternalJSModuleIT extends ChromeBrowserTest {
+    // prefix with "http:" since Selenium drives seem to expand url fragments
+    // to full length:
+    // https://github.com/seleniumhq/selenium-google-code-issue-archive/issues/1824
+    private final static String EXPECTED_SRC_FOR_NO_PROTOCOL =
+            "http:" + ComponentWithExternalJsModule.SOME_RANDOM_EXTERNAL_JS_URL_WITHOUT_PROTOCOL;
+
     @Before
     public void init() {
         open();
@@ -45,7 +52,7 @@ public class ExternalJSModuleIT extends ChromeBrowserTest {
         Assert.assertTrue(
                 "External JS without protocol annotated with @JsModule annotation should be added as a script tag with module type to the page!",
                 scriptTags.stream().anyMatch(
-                        scriptTag -> ComponentWithExternalJsModule.SOME_RANDOM_EXTERNAL_JS_URL_WITHOUT_PROTOCOL
+                        scriptTag -> EXPECTED_SRC_FOR_NO_PROTOCOL
                                 .equals(scriptTag.getAttribute("src"))
                                 && "module".equals(
                                         scriptTag.getAttribute("type"))));
@@ -67,7 +74,7 @@ public class ExternalJSModuleIT extends ChromeBrowserTest {
         Assert.assertTrue(
                 "When a component is added to the page, external JS without protocol annotated with @JsModule annotation in the component should be added as a script tag with module type to the page!",
                 scriptTags.stream().anyMatch(
-                        scriptTag -> ComponentWithExternalJsModule.SOME_RANDOM_EXTERNAL_JS_URL_WITHOUT_PROTOCOL
+                        scriptTag -> EXPECTED_SRC_FOR_NO_PROTOCOL
                                 .equals(scriptTag.getAttribute("src"))
                                 && "module".equals(
                                         scriptTag.getAttribute("type"))));
