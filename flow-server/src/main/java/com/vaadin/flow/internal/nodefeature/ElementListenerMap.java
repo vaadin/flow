4645860d6dc1abc366027b4e3752fe86c7256d23
Joni Uitto <joni@vaadin.com>
2019-09-10 12:11:26 +0300

    Make sure, that settings set by a event listener are removed ... when the listener is removed

--- flow-server/src/main/java/com/vaadin/flow/internal/nodefeature/ElementListenerMap.java
+++ flow-server/src/main/java/com/vaadin/flow/internal/nodefeature/ElementListenerMap.java
@@ -130,7 +130,14 @@ public class ElementListenerMap extends NodeMap {
             if (unregisterHandlers != null) {
                 unregisterHandlers.forEach(SerializableRunnable::run);
             }
+
             listenerMap.removeListener(type, this);
+
+            // update settings after removal. If we have listeners of the
+            // same type registered, we want to remove settings set by this
+            // particular listener from the overall set
+            // fixes #5090
+            listenerMap.updateEventSettings(type);
         }
 
         @Override
@@ -365,15 +372,17 @@ public class ElementListenerMap extends NodeMap {
     }
 
     private void updateEventSettings(String eventType) {
-        Map<String, ExpressionSettings> eventSettings = collectEventExpressions(
-                eventType);
-        JsonObject eventSettingsJson = JsonUtils.createObject(eventSettings,
-                ExpressionSettings::toJson);
+        if (listeners != null && listeners.containsKey(eventType)) {
+            Map<String, ExpressionSettings> eventSettings = collectEventExpressions(
+                    eventType);
+            JsonObject eventSettingsJson = JsonUtils.createObject(eventSettings,
+                    ExpressionSettings::toJson);
 
-        ConstantPoolKey constantPoolKey = new ConstantPoolKey(
-                eventSettingsJson);
+            ConstantPoolKey constantPoolKey = new ConstantPoolKey(
+                    eventSettingsJson);
 
-        put(eventType, constantPoolKey);
+            put(eventType, constantPoolKey);
+        }
     }
 
     private void removeListener(String eventType,
