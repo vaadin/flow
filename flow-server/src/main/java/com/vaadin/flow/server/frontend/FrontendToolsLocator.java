da5d96b1386a45b159efaa5360fab0fcdae376d8
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-09-20 15:17:29 +0300

    Clean imports

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendToolsLocator.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendToolsLocator.java
@@ -15,10 +15,8 @@
  */
 package com.vaadin.flow.server.frontend;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
