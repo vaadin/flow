be068aff4d6bf8f1f2aed50ef473c23ba388e2fb
Joni Uitto <joni@vaadin.com>
2019-09-24 15:16:23 +0300

    Remove unused import

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/scanner/EndPointData.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/scanner/EndPointData.java
@@ -22,7 +22,6 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.vaadin.flow.router.Route;
 
