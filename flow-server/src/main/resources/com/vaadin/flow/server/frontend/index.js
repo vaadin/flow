b4ba83c0f1215bd566e51fc8d864f5055486a9a8
Tien Nguyen <nqtien.dev@gmail.com>
2019-09-26 16:52:34 +0300

    Import Flow.js explicitly

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/index.js
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/index.js
@@ -1,4 +1,4 @@
-import { Flow } from '@vaadin/flow-frontend/Flow';
+import { Flow } from '@vaadin/flow-frontend/Flow.js';
 
 const flow = new Flow({
   imports: () => import('[to-be-generated-by-flow]')
