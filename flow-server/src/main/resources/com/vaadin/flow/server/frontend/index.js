f65d18a3c476cbb38da145da2603e4f7e608d743
Tien Nguyen <nqtien.dev@gmail.com>
2019-09-27 11:54:47 +0300

    Update index.js content

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/index.js
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/index.js
@@ -1,7 +1,10 @@
+// import Flow module to enable traditional Vaadin server-side navigation
+// If you prefer to use TypeScript, you can import `Flow.ts` instead
 import { Flow } from '@vaadin/flow-frontend/Flow.js';
 
 const flow = new Flow({
   imports: () => import('[to-be-generated-by-flow]')
 });
-// Let the flow-server control the application
+// Let the flow-server control the application and use traditional server-side routing
 flow.start();
+// If you prefer to use client-side routing, please check out the 'Client-side Routing' page in `https://vaadin.com/docs`
