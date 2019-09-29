fb7665a3aa44ba5535d8d80bf7f16dc9980f02df
Johannes Eriksson <joheriks@vaadin.com>
2019-09-29 22:26:13 +0300

    Test against performance regressions when starting up a project with large endpoint dependencies

---
+++ flow-tests/test-npm-only-features/test-npm-performance-regression/src/main/java/com/vaadin/starter/skeleton/StartupPerformance.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.starter.skeleton;
+
+import javax.persistence.EntityManager;
+import javax.transaction.TransactionManager;
+
+import akka.actor.Actor;
+import akka.actor.ActorSelection;
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.MetricSet;
+import com.google.common.collect.Multimap;
+import com.google.inject.Injector;
+import org.hibernate.Hibernate;
+import org.hibernate.cfg.JPAIndexHolder;
+import org.hibernate.dialect.H2Dialect;
+import org.hibernate.dialect.MySQL5Dialect;
+
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.html.Div;
+
+@Route("")
+public class StartupPerformance extends Div {
+    // Reference classes in various libraries. If bytecode scanning is in use,
+    // visiting these classes is expected to be quite slow.
+    Hibernate hibernate;
+    EntityManager entityManager;
+    TransactionManager trsManager;
+    JPAIndexHolder jpa;
+    H2Dialect h2Dialect;
+    MySQL5Dialect mysqlDialect;
+    Multimap<String,String> mmap;
+    Injector injector;
+    MetricRegistry registry;
+    MetricSet metricSet;
+    ConsoleReporter reporter;
+    Gauge gauge;
+    Actor actor;
+    ActorSelection actorSelection;
+}
---
+++ flow-tests/test-npm-only-features/test-npm-performance-regression/src/test/java/com/vaadin/starter/skeleton/StartupPerformanceIT.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.starter.skeleton;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StartupPerformanceIT {
+    @Test
+    public void devModeInitializerToWebPackUpIsBelow2000ms() {
+        int startupTime = measureLogEntryTimeDistance(
+                "com.vaadin.flow.server.startup.DevModeInitializer - Starting dev-mode updaters in",
+                "dev-webpack.*Time: [0-9]+ms"
+        );
+        final int thresholdMs = 2000;
+        Assert.assertTrue(String.format("startupTime expected <= %d but was %d", thresholdMs, startupTime),
+                startupTime <= thresholdMs);
+    }
+
+    private int measureLogEntryTimeDistance(String startFragment, String endFragment) {
+        Pattern startPattern = createPattern(startFragment);
+        Pattern endPattern = createPattern(endFragment);
+        AtomicInteger startTime = new AtomicInteger();
+        AtomicInteger endTime = new AtomicInteger();
+        try {
+            Files.lines(getLogPath())
+                    .forEach(line -> {
+                        Matcher matcherFirst = startPattern.matcher(line);
+                        if (matcherFirst.matches() && startTime.get() == 0) {
+                            startTime.set(Integer.parseInt(matcherFirst.group(1)));
+                        }
+                        Matcher matcherEnd = endPattern.matcher(line);
+                        if (matcherEnd.matches()) {
+                            endTime.set(Integer.parseInt(matcherEnd.group(1)));
+                        }
+                    });
+            if (startTime.get() == 0) {
+                throw new RuntimeException("No match: " + startFragment);
+            }
+            if (endTime.get() == 0) {
+                throw new RuntimeException("No match: " + endFragment);
+            }
+            return endTime.get() - startTime.get();
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    private Pattern createPattern(String fragment) {
+        return Pattern.compile(String.format("([0-9]+).*%s.*", fragment));
+    }
+
+    private Path getLogPath() throws IOException {
+        Properties prop = new Properties();
+        prop.load(getClass().getClassLoader().getResourceAsStream("simplelogger.properties"));
+        File logFile = new File(prop.getProperty("org.slf4j.simpleLogger.logFile"));
+        return logFile.toPath();
+    }
+}
