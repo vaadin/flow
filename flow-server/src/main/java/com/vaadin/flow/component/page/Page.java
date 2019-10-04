b10780b9afd6410aba1e01958eeb4c2a72d832b2
Denis Anisimov <denis@vaadin.com>
2019-10-04 12:47:01 +0300

    One more freaking merge

--- flow-server/src/main/java/com/vaadin/flow/component/page/Page.java
+++ flow-server/src/main/java/com/vaadin/flow/component/page/Page.java
@@ -360,9 +360,9 @@ public class Page implements Serializable {
      * Adds a dynamic import using a JavaScript {@code expression} which is
      * supposed to return a JavaScript {@code Promise}.
      * <p>
-     * No any change will be applied on the client side until resulting Promise
-     * of the {@code expression} is completed. It behaves like other
-     * dependencies ({@link #addJavaScript(String)},
+     * No change will be applied on the client side until resulting
+     * {@code Promise} of the {@code expression} is completed. It behaves like
+     * other dependencies ({@link #addJavaScript(String)},
      * {@link #addJsModule(String)}, etc.)
      *
      *
