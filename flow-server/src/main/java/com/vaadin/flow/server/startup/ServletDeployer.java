7252f259728c59336bffb8927f8ed74427029841
Denis <denis@vaadin.com>
2019-10-10 16:16:22 +0300

    Use compatibility mode for Frontend servlet. (#6660)
    * Use compatibility mode for Frontend servlet.
    
    Fixes #6651
    
    (cherry picked from commit d1f3ee050025a2f6c9cc62dc95766e61e8a13294)

--- flow-server/src/main/java/com/vaadin/flow/server/startup/ServletDeployer.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/ServletDeployer.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -34,6 +35,7 @@ import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.Constants;
 import com.vaadin.flow.server.DeploymentConfigurationFactory;
 import com.vaadin.flow.server.FrontendVaadinServlet;
 import com.vaadin.flow.server.VaadinServlet;
@@ -183,7 +185,10 @@ public class ServletDeployer implements ServletContextListener {
                         context) == VaadinServletCreation.SERVLET_EXISTS
                 && hasDevelopmentMode && isCompatibilityMode) {
             createServletIfNotExists(context, "frontendFilesServlet",
-                    FrontendVaadinServlet.class, "/frontend/*");
+                    FrontendVaadinServlet.class, "/frontend/*",
+                    Collections.singletonMap(
+                            Constants.SERVLET_PARAMETER_COMPATIBILITY_MODE,
+                            Boolean.TRUE.toString()));
         }
     }
 
@@ -234,6 +239,14 @@ public class ServletDeployer implements ServletContextListener {
     private VaadinServletCreation createServletIfNotExists(
             ServletContext context, String name,
             Class<? extends Servlet> servletClass, String path) {
+        return createServletIfNotExists(context, name, servletClass, path,
+                null);
+    }
+
+    private VaadinServletCreation createServletIfNotExists(
+            ServletContext context, String name,
+            Class<? extends Servlet> servletClass, String path,
+            Map<String, String> initParams) {
         ServletRegistration existingServlet = findServletByPathPart(context,
                 path);
         if (existingServlet != null) {
@@ -246,6 +259,9 @@ public class ServletDeployer implements ServletContextListener {
 
         ServletRegistration.Dynamic registration = context.addServlet(name,
                 servletClass);
+        if (initParams != null) {
+            registration.setInitParameters(initParams);
+        }
         if (registration == null) {
             // Not expected to ever happen
             getLogger().info("{} there is already a servlet with the name {}",
