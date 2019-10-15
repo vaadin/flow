7953e6538ac94101bfc302c86ba021f44a75a509
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-10-15 09:06:26 +0300

    Return method to make portlet feature +0.1.0

--- flow-server/src/main/java/com/vaadin/flow/server/startup/ApplicationRouteRegistry.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/ApplicationRouteRegistry.java
@@ -203,6 +203,24 @@ public class ApplicationRouteRegistry extends AbstractRouteRegistry {
      *         <code>null</code>
      * @return a registry instance for the given servlet context, not
      * <code>null</code>
+     * @deprecated this is deprecated in favor of {@code getInstance(VaadinContext)}
+     * and will be removed in a future release
+     */
+    @Deprecated
+    public static ApplicationRouteRegistry getInstance(ServletContext context) {
+        return getInstance(new VaadinServletContext(context));
+    }
+
+    /**
+     * Gets the route registry for the given Vaadin context. If the Vaadin
+     * context has no route registry, a new instance is created and assigned to
+     * the context.
+     *
+     * @param context
+     *         the vaadin context for which to get a route registry, not
+     *         <code>null</code>
+     * @return a registry instance for the given servlet context, not
+     * <code>null</code>
      */
     public static ApplicationRouteRegistry getInstance(VaadinContext context) {
         assert context != null;
