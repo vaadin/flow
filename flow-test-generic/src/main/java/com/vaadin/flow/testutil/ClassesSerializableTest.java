83fe542e86f968b0cfc78c3d845f6ed6e6bcf59a
Tien Nguyen <nqtien.dev@gmail.com>
2019-10-16 12:40:16 +0300

    Fix serializable tests

--- flow-test-generic/src/main/java/com/vaadin/flow/testutil/ClassesSerializableTest.java
+++ flow-test-generic/src/main/java/com/vaadin/flow/testutil/ClassesSerializableTest.java
@@ -170,8 +170,7 @@ public abstract class ClassesSerializableTest {
                 "com\\.vaadin\\.flow\\.server\\.communication\\.WebComponentBootstrapHandler(\\$.*)?",
                 "com\\.vaadin\\.flow\\.server\\.frontend\\.scanner\\..*",
                 "com\\.vaadin\\.flow\\.server\\.frontend\\.JarContentsManager",
-                "com\\.vaadin\\.flow\\.server\\.connect\\.generator\\..*",
-                "com\\.vaadin\\.flow\\.server\\.connect\\.aut\\..*",
+                "com\\.vaadin\\.flow\\.server\\.connect\\..*",
 
                 // Various test classes
                 ".*\\.test(s)?\\..*", ".*Test.*",
