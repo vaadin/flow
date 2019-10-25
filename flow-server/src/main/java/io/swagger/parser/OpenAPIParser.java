1a0f940ba2160cc1e05b22eb4bfe58ed797c7cec
Manolo Carrasco <manolo@apache.org>
2019-10-25 10:20:36 +0200

    CCDM: fix sonar

--- flow-server/src/main/java/io/swagger/parser/OpenAPIParser.java
+++ flow-server/src/main/java/io/swagger/parser/OpenAPIParser.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.swagger.parser;
 
 import java.util.Collections;
@@ -13,12 +28,15 @@ import io.swagger.v3.parser.core.models.SwaggerParseResult;
  * A copy of the original OpenAPIParser to avoid problems with flow. Using the
  * original one makes Flow CompositeTest and CompositeTextNodeTest tests fail.
  * 
+ * NOTE: This is a copy of the original file which is under the Apache
+ * License 2.0 as described at https://swagger.io/license/
+ *
  * @author manolo
  * @since 3.0
  *
  */
 public class OpenAPIParser {
-    public SwaggerParseResult readLocation(String url, List<AuthorizationValue> auth, ParseOptions options) {
+    public SwaggerParseResult readLocation(String url, List<AuthorizationValue> auth, ParseOptions options) { // NOSONAR
         SwaggerParseResult output = null;
         for(SwaggerParserExtension extension : getExtensions()) {
             output = extension.readLocation(url, auth, options);
@@ -29,7 +47,7 @@ public class OpenAPIParser {
         return output;
     }
 
-    public SwaggerParseResult readContents(String swaggerAsString, List<AuthorizationValue> auth, ParseOptions options) {
+    public SwaggerParseResult readContents(String swaggerAsString, List<AuthorizationValue> auth, ParseOptions options) { // NOSONAR
         SwaggerParseResult output = null;
 
         for(SwaggerParserExtension extension : getExtensions()) {
