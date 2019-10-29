d76bf1a06620a3815229fff3debf40b6d3acf83b
Leif Åstrand <leif@vaadin.com>
2019-10-29 11:36:44 +0200

    Support @ClientCallable return values (#6597)
    Methods in $server will return a promise that is completed with the
    result of the client-side invocation.
    
    When such a method is invoked, an incremental promise id is generated
    and a promise is created. The resolve and reject callbacks of the
    promise are stored using the promise id as the key. The promise id is
    passed to the server with the invocation.
    
    After running the target method on the server, the promise id and the
    invocation result is passed back to the client using executeJs. The
    executed function finds the appropriate promise callback based on the
    promise id and invokes it to complete the cycle.
    
    Fixes #4050

--- flow-server/src/main/java/com/vaadin/flow/internal/nodefeature/ClientCallableHandlers.java
+++ flow-server/src/main/java/com/vaadin/flow/internal/nodefeature/ClientCallableHandlers.java
@@ -16,10 +16,13 @@
 package com.vaadin.flow.internal.nodefeature;
 
 import java.lang.reflect.Method;
+import java.util.Locale;
 
 import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.dom.DisabledUpdateMode;
+import com.vaadin.flow.internal.JsonCodec;
+import com.vaadin.flow.internal.ReflectTools;
 import com.vaadin.flow.internal.StateNode;
 
 /**
@@ -51,6 +54,25 @@ public class ClientCallableHandlers extends AbstractServerHandlers<Component> {
     protected void ensureSupportedParameterTypes(Method method) {
         // decoder may be able to convert any value to any type so no need to
         // limit supported types
+    }
+
+    @Override
+    protected void ensureSupportedReturnType(Method method) {
+        Class<?> returnType = method.getReturnType();
+        if (returnType.isPrimitive()) {
+            returnType = ReflectTools.convertPrimitiveType(returnType);
+        }
+
+        if (!void.class.equals(returnType)
+                && !JsonCodec.canEncodeWithTypeInfo(returnType)) {
+            String msg = String.format(Locale.ENGLISH,
+                    "Only return types that can be used as Element.executeJs parameters are supported. "
+                            + "Component '%s' has method '%s' annotated with '%s' whose return type is \"%s\"",
+                    method.getDeclaringClass().getName(), method.getName(),
+                    getHandlerAnnotation().getName(),
+                    method.getReturnType().getSimpleName());
+            throw new IllegalStateException(msg);
+        }
     }
 
     @Override
