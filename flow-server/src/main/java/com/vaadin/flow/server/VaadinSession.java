e5ad311d8032951b01523ca5cb5b36d8115a5290
Mehdi Javan <32511762+mehdi-vaadin@users.noreply.github.com>
2019-11-22 17:28:09 +0200

    Prevent VaadinSession.setErrorHandler from accepting null (#7011)
    (cherry picked from commit 194265f609fd8bdc299e99afcc186d539ff390d9)

--- flow-server/src/main/java/com/vaadin/flow/server/VaadinSession.java
+++ flow-server/src/main/java/com/vaadin/flow/server/VaadinSession.java
@@ -27,6 +27,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.Set;
 import java.util.UUID;
@@ -375,9 +376,11 @@ public class VaadinSession implements HttpSessionBindingListener, Serializable {
      * Sets the session error handler.
      *
      * @param errorHandler
-     *            the new error handler
+     *            the new error handler, not <code>null</code>
      */
     public void setErrorHandler(ErrorHandler errorHandler) {
+        Objects.requireNonNull(errorHandler, "errorHandler can not be null!");
+
         checkHasLock();
         this.errorHandler = errorHandler;
     }
