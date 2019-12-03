2bc10787ec52b15ede8f217f8095e9c9ccdc009c
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-12-03 14:21:09 +0200

    Clean imports.

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunNpmInstall.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunNpmInstall.java
@@ -18,10 +18,7 @@ package com.vaadin.flow.server.frontend;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import com.vaadin.flow.server.ExecutionFailedException;
 
