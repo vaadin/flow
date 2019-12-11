b5fe5f67209e0b77bb53a9c16483350cf47a2f3d
Manolo Carrasco <manolo@apache.org>
2019-12-11 10:42:47 +0200

    CCDM: add div#outlet in index.html and set css sizes to outlet

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeTasks.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeTasks.java
@@ -524,11 +524,11 @@ public class NodeTasks implements FallibleCommand {
         TaskGenerateIndexHtml taskGenerateIndexHtml = new TaskGenerateIndexHtml(
                 builder.frontendDirectory, outputDirectory);
         commands.add(taskGenerateIndexHtml);
-        TaskGenerateIndexTs taskGenerateIndexJs = new TaskGenerateIndexTs(
+        TaskGenerateIndexTs taskGenerateIndexTs = new TaskGenerateIndexTs(
                 builder.frontendDirectory,
                 new File(builder.generatedFolder, IMPORTS_NAME),
                 outputDirectory);
-        commands.add(taskGenerateIndexJs);
+        commands.add(taskGenerateIndexTs);
 
         TaskGenerateTsConfig taskGenerateTsConfig = new TaskGenerateTsConfig(
                 builder.frontendDirectory, builder.npmFolder, outputDirectory);
