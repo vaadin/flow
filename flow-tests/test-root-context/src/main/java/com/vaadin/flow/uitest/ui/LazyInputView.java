ede716db48416c50d64541be3359383de4de8499
Denis Anisimov <denis@vaadin.com>
2019-12-12 16:30:13 +0300

    Only sending to server should be postponed
    - Update value in StateTree on the client side immediately
    - Defer execution only sending a new value to the server
    
    Fixes #7128

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/LazyInputView.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Input;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+
+@Route(value = "com.vaadin.flow.uitest.ui.LazyInputView", layout = ViewTestLayout.class)
+public class LazyInputView extends Div {
+
+    public LazyInputView() {
+        Input input = new Input();
+        input.setValueChangeMode(ValueChangeMode.LAZY);
+        input.setValueChangeTimeout(500);
+
+        input.addValueChangeListener(event -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        add(input);
+    }
+}
