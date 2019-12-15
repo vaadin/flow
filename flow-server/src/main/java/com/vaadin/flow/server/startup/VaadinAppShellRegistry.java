8a86b859b495e7be091b22aa9c5161bf9c0c2a25
Thanh Dat PHAN <ptdat.thanhdatphan@gmail.com>
2019-12-15 23:16:26 +0200

    reconfig auto format

--- flow-server/src/main/java/com/vaadin/flow/server/startup/VaadinAppShellRegistry.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/VaadinAppShellRegistry.java
@@ -23,7 +23,6 @@ import java.util.List;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import com.vaadin.flow.component.page.Meta;
 import com.vaadin.flow.component.page.VaadinAppShell;
@@ -48,13 +47,14 @@ public class VaadinAppShellRegistry implements Serializable {
             + "%nThe following annotations must be either removed or moved to the '%s' class:%n  %s%n";
 
     private static final String ERROR_LINE = "  - %s from %s";
-    private static final String ERROR_MULTIPLE_SHELL = "%nUnable to find a single class extending `VaadinAppShell` from the following candidates:%n  %s%n  %s%n";
+    private static final String ERROR_MULTIPLE_SHELL =
+            "%nUnable to find a single class extending `VaadinAppShell` from the following candidates:%n  %s%n  %s%n";
 
     private Class<? extends VaadinAppShell> shell;
 
     /**
-     * A wrapper class for storing the {@link VaadinAppShellRegistry} instance in
-     * the servlet context.
+     * A wrapper class for storing the {@link VaadinAppShellRegistry} instance
+     * in the servlet context.
      */
     public static class VaadinAppShellRegistryWrapper implements Serializable {
         private final VaadinAppShellRegistry registry;
@@ -73,15 +73,18 @@ public class VaadinAppShellRegistry implements Serializable {
      * Returns the instance of the registry, or create a new one if it does not
      * exist yet.
      *
-     * @param context servlet context
+     * @param context
+     *            servlet context
      * @return the registry instance
      */
     @SuppressWarnings("unchecked")
     public static VaadinAppShellRegistry getInstance(VaadinContext context) {
         synchronized (context) { // NOSONAR
-            VaadinAppShellRegistryWrapper attribute = context.getAttribute(VaadinAppShellRegistryWrapper.class);
+            VaadinAppShellRegistryWrapper attribute = context
+                    .getAttribute(VaadinAppShellRegistryWrapper.class);
             if (attribute == null) {
-                attribute = new VaadinAppShellRegistryWrapper(new VaadinAppShellRegistry());
+                attribute = new VaadinAppShellRegistryWrapper(
+                        new VaadinAppShellRegistry());
                 context.setAttribute(attribute);
             }
             return attribute.registry;
@@ -100,12 +103,15 @@ public class VaadinAppShellRegistry implements Serializable {
      * Sets the {@link VaadinAppShell} class in the application. Pass a null to
      * reset the previous one when reusing the instance.
      *
-     * @param shell the class extending VaadinAppShell class.
+     * @param shell
+     *            the class extending VaadinAppShell class.
      */
-    public void setShell(Class<? extends VaadinAppShell> shell) {
+    public void setShell(
+            Class<? extends VaadinAppShell> shell) {
         if (this.shell != null && shell != null) {
             throw new InvalidApplicationConfigurationException(
-                    String.format(VaadinAppShellRegistry.ERROR_MULTIPLE_SHELL, this.shell.getName(), shell.getName()));
+                    String.format(VaadinAppShellRegistry.ERROR_MULTIPLE_SHELL,
+                            this.shell.getName(), shell.getName()));
         }
         this.shell = shell;
     }
@@ -122,14 +128,17 @@ public class VaadinAppShellRegistry implements Serializable {
     /**
      * Checks whether the class is extending {@link VaadinAppShell}.
      *
-     * @param clz the class to check.
+     * @param clz
+     *            the class to check.
      * @return true if the class extends {@link VaadinAppShell}.
      */
     public boolean isShell(Class<?> clz) {
         assert clz != null;
         try {
             // Use the same class-loader for the checking
-            return clz.getClassLoader().loadClass(VaadinAppShell.class.getName()).isAssignableFrom(clz);
+            return clz.getClassLoader()
+                    .loadClass(VaadinAppShell.class.getName())
+                    .isAssignableFrom(clz);
         } catch (ClassNotFoundException e) {
             throw new IllegalArgumentException(e);
         }
@@ -139,15 +148,19 @@ public class VaadinAppShellRegistry implements Serializable {
      * Checks whether a class have annotations that should only be in
      * {@link VaadinAppShell} classes.
      *
-     * @param clz the class to check.
-     * @return a string with the error lines if the class has offending annotations
+     * @param clz
+     *            the class to check.
+     * @return a string with the error lines if the class has offending
+     *         annotations
      */
     public String validateClass(Class<?> clz) {
         String error = null;
         @SuppressWarnings({ "unchecked", "rawtypes" })
-        String annotations = AbstractAnnotationValidator.getClassAnnotations(clz, (List) getValidAnnotations());
+        String annotations = AbstractAnnotationValidator
+                .getClassAnnotations(clz, (List) getValidAnnotations());
         if (!annotations.isEmpty()) {
-            error = String.format(VaadinAppShellRegistry.ERROR_LINE, annotations, clz.getName());
+            error = String.format(VaadinAppShellRegistry.ERROR_LINE,
+                    annotations, clz.getName());
         }
         return error;
     }
