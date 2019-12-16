657442b7bac8fafd9c6c4e860f1548d85b32e139
Thanh Dat PHAN <ptdat.thanhdatphan@gmail.com>
2019-12-16 21:05:34 +0200

    add test case should use dynamic content of bodysize annotation

--- flow-tests/test-ccdm/src/main/java/com/vaadin/flow/ccdmtest/AppShell.java
+++ flow-tests/test-ccdm/src/main/java/com/vaadin/flow/ccdmtest/AppShell.java
@@ -1,10 +1,12 @@
 package com.vaadin.flow.ccdmtest;
 
+import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.component.page.Meta;
 import com.vaadin.flow.component.page.VaadinAppShell;
 import com.vaadin.flow.server.PWA;
 
 @Meta(name = "foo", content = "bar")
 @PWA(name = "My App", shortName = "app")
+@BodySize(height = "50vh", width = "50vw")
 public class AppShell extends VaadinAppShell {
 }
