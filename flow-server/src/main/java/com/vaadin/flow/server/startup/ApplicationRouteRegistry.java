20d0fcf272c02c6d684d1eab1bdbc0690452b559
caalador <mikael.grankvist@gmail.com>
2019-12-20 13:50:55 +0200

    Don't add abstract error targets (#7215)
    Filter out any abstract classes from
    error navigation targets as they can't
    be instantiated anyway.
    
    Fixes vaadin/spring#530
    
    (cherry picked from commit df2d460d6bcc376ac9b07c90507c09e3cf1d6143)

--- flow-server/src/main/java/com/vaadin/flow/server/startup/ApplicationRouteRegistry.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/ApplicationRouteRegistry.java
@@ -16,6 +16,8 @@
 package com.vaadin.flow.server.startup;
 
 import javax.servlet.ServletContext;
+import java.io.Serializable;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -257,6 +259,7 @@ public class ApplicationRouteRegistry extends AbstractRouteRegistry {
         errorNavigationTargets.stream()
                 .filter(target -> !defaultErrorHandlers.contains(target))
                 .filter(this::allErrorFiltersMatch)
+                .filter(handler -> !Modifier.isAbstract(handler.getModifiers()))
                 .forEach(target -> addErrorTarget(target, exceptionTargetsMap));
 
         initErrorTargets(exceptionTargetsMap);
