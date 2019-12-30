642794cbe6ebe1532ff5acdb1b5865617cc47df4
Denis Anisimov <denis@vaadin.com>
2019-12-30 11:11:33 +0300

    More frontend protocol removals

--- flow-tests/test-mixed/src/main/java/com/vaadin/flow/mixedtest/ui/IdTestView.java
+++ flow-tests/test-mixed/src/main/java/com/vaadin/flow/mixedtest/ui/IdTestView.java
@@ -18,7 +18,6 @@ package com.vaadin.flow.mixedtest.ui;
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.NativeButton;
@@ -31,7 +30,6 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 @Route(value = "route-path")
 @Tag("my-component")
 @JsModule("./my-component.js")
-@HtmlImport("my-component.html")
 public class IdTestView extends PolymerTemplate<TemplateModel> {
 
     @WebServlet("/servlet-path/*")
