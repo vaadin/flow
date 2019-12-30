ce0301e80cf8b9700e107151dcef682f9e42e89c
Denis Anisimov <denis@vaadin.com>
2019-12-30 10:44:07 +0300

    Covert lumo ITs to npm mode

--- flow-tests/test-lumo-theme/src/main/java/com/vaadin/flow/uitest/ui/lumo/ExplicitLumoTemplateView.java
+++ flow-tests/test-lumo-theme/src/main/java/com/vaadin/flow/uitest/ui/lumo/ExplicitLumoTemplateView.java
@@ -16,15 +16,14 @@
 package com.vaadin.flow.uitest.ui.lumo;
 
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 
 @Tag("explicit-lumo-themed-template")
-@HtmlImport("frontend://bower_components/themed-template/src/LumoThemedTemplate.html")
+@JsModule("./src/LumoThemedTemplate.html")
 @Route(value = "com.vaadin.flow.uitest.ui.lumo.ExplicitLumoTemplateView")
 @Theme(Lumo.class)
 public class ExplicitLumoTemplateView extends PolymerTemplate<TemplateModel> {
