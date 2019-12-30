f87d5bb6651d1879d2953c0137c79f2971db4018
Denis Anisimov <denis@vaadin.com>
2019-12-30 11:11:33 +0300

    More frontend protocol removals

--- flow-tests/test-dev-mode/src/main/java/com/vaadin/flow/uitest/ui/template/AttachExistingElementByIdView.java
+++ flow-tests/test-dev-mode/src/main/java/com/vaadin/flow/uitest/ui/template/AttachExistingElementByIdView.java
@@ -16,37 +16,23 @@
 package com.vaadin.flow.uitest.ui.template;
 
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.uitest.ui.AbstractDivView;
-import com.vaadin.flow.uitest.ui.template.AttachExistingElementByIdView.AttachExistingElementByIdTemplate;
 
 @Route("com.vaadin.flow.uitest.ui.template.AttachExistingElementByIdView")
 public class AttachExistingElementByIdView extends AbstractDivView {
 
-    @HtmlImport("context://frontend/com/vaadin/flow/uitest/ui/template/ContextAttachExistingElementById.html")
-    @Tag("context-existing-element")
-    public static class ContextAttachExistingElementByIdTemplate
+    // @HtmlImport("frontend://com/vaadin/flow/uitest/ui/template/AttachExistingElementById.html")
+    @Tag("existing-element")
+    public static class AttachExistingElementByIdTemplate
             extends AbstractAttachExistingElementByIdTemplate {
 
-        ContextAttachExistingElementByIdTemplate() {
-            super("context-path");
-        }
-    }
-
-    @HtmlImport("components/AttachExistingElementById.html")
-    @Tag("frontend-existing-element")
-    public static class FrontendAttachExistingElementByIdTemplate
-            extends AbstractAttachExistingElementByIdTemplate {
-
-        FrontendAttachExistingElementByIdTemplate() {
-            super("frontend-path");
+        AttachExistingElementByIdTemplate() {
+            super("simple-path");
         }
     }
 
     public AttachExistingElementByIdView() {
-        add(new AttachExistingElementByIdTemplate(),
-                new ContextAttachExistingElementByIdTemplate(),
-                new FrontendAttachExistingElementByIdTemplate());
+        add(new AttachExistingElementByIdTemplate());
     }
 }
