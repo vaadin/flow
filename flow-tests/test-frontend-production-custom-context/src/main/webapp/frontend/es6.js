b85beda01d78926f58e63977e6c8cde817adceba
Denis Anisimov <denis@vaadin.com>
2019-12-30 09:43:45 +0300

    Remove html imports related ITs

--- flow-tests/test-frontend-production-custom-context/src/main/webapp/frontend/es6.js
+++
@@ -1,5 +0,0 @@
-let anEs6Variable = "Piece of ES6 works from bundled frontend resources";
-let newChild = document.createElement("div");
-newChild.id = "es6-div";
-newChild.innerText = anEs6Variable;
-document.body.appendChild(newChild);
