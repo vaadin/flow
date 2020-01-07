27d0f55a515ec4ac6f9504153b65256f0225c404
Denis Anisimov <denis@vaadin.com>
2020-01-07 10:24:55 +0300

    Correct and update tests for browser logging and dependency filter.

--- flow-tests/test-dev-mode/src/main/java/com/vaadin/flow/uitest/ui/dependencies/DependencyFilterView.java
+++ flow-tests/test-dev-mode/src/main/java/com/vaadin/flow/uitest/ui/dependencies/DependencyFilterView.java
@@ -2,6 +2,7 @@ package com.vaadin.flow.uitest.ui.dependencies;
 
 import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
 @Route("com.vaadin.flow.uitest.ui.dependencies.DependencyFilterView")
@@ -10,6 +11,9 @@ import com.vaadin.flow.router.Route;
 public class DependencyFilterView extends DependenciesLoadingBaseView {
 
     public DependencyFilterView() {
-        setId("filtered-css");
+        Div filtered = new Div();
+        filtered.setText("filtered");
+        filtered.setId("filtered-css");
+        add(filtered);
     }
 }
