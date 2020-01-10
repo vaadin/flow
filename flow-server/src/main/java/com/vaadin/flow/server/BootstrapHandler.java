0e845e0b267dc691da802c513c4b329a96ca9b09
haijian <haijian@vaadin.com>
2020-01-10 16:35:18 +0200

    fix sonarcube issues

--- flow-server/src/main/java/com/vaadin/flow/server/BootstrapHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/BootstrapHandler.java
@@ -38,7 +38,6 @@ import java.util.ServiceLoader;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.jsoup.Jsoup;
@@ -61,8 +60,6 @@ import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.internal.AnnotationReader;
 import com.vaadin.flow.internal.ReflectTools;
-import com.vaadin.flow.internal.UsageStatistics;
-import com.vaadin.flow.internal.UsageStatistics.UsageEntry;
 import com.vaadin.flow.internal.UsageStatisticsExporter;
 import com.vaadin.flow.server.communication.AtmospherePushConnection;
 import com.vaadin.flow.server.communication.PushConnectionFactory;
