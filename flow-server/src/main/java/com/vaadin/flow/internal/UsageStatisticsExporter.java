22b78c0eaf31da32846e154662fea3fab9098267
haijian <haijian@vaadin.com>
2020-01-10 14:55:54 +0200

    make UsageStatisticsExporter.java serializable

--- flow-server/src/main/java/com/vaadin/flow/internal/UsageStatisticsExporter.java
+++ flow-server/src/main/java/com/vaadin/flow/internal/UsageStatisticsExporter.java
@@ -1,5 +1,6 @@
 package com.vaadin.flow.internal;
 
+import java.io.Serializable;
 import java.util.stream.Collectors;
 
 import elemental.json.Json;
@@ -12,9 +13,7 @@ import org.jsoup.nodes.Document;
  * @author Vaadin Ltd
  * @since 3.0
  */
-public class UsageStatisticsExporter {
-
-    private static final String SCRIPT_TAG = "script";
+public class UsageStatisticsExporter implements Serializable {
 
     /**
      * Export {@link UsageStatistics} entries to a document. It appends a
@@ -30,13 +29,12 @@ public class UsageStatisticsExporter {
         if (!entries.isEmpty()) {
             // Registers the entries in a way that is picked up as a Vaadin
             // WebComponent by the usage stats gatherer
-            StringBuilder builder = new StringBuilder();
-            builder.append("window.Vaadin = window.Vaadin || {};\n")
-                    .append("window.Vaadin.registrations = window.Vaadin.registrations || [];\n")
-                    .append("window.Vaadin.registrations.push(")
-                    .append(entries)
-                    .append(");");
-            document.body().appendElement(SCRIPT_TAG).text(builder.toString());
+            String builder = "window.Vaadin = window.Vaadin || {};\n" +
+                    "window.Vaadin.registrations = window.Vaadin.registrations || [];\n" +
+                    "window.Vaadin.registrations.push(" +
+                    entries +
+                    ");";
+            document.body().appendElement("script").text(builder);
         }
     }
 
