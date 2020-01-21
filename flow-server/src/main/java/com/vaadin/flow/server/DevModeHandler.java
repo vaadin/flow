f5485f9a7433bfd449e2f50b06144aeca858f49a
Pekka Hyvönen <pekka@vaadin.com>
2020-01-21 15:50:56 +0200

    Clarify that webpack may take a moment
    Fixes vaadin/90min-dx-tasks#59

--- flow-server/src/main/java/com/vaadin/flow/server/DevModeHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/DevModeHandler.java
@@ -190,7 +190,7 @@ public final class DevModeHandler {
             logStream(webpackProcess.getInputStream(), succeed, failure);
 
             getLogger()
-                    .info("Waiting for webpack compilation before proceeding.");
+                    .info("Running webpack to compile frontend resources. This may take a moment, please stand by...");
             synchronized (this) {
                 this.wait(Integer.parseInt(config.getStringProperty( // NOSONAR
                         SERVLET_PARAMETER_DEVMODE_WEBPACK_TIMEOUT,
