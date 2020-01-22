119e82fdaf6dd4b124d5c9217d6effac3700a7e8
Bogdan <bogdan@vaadin.com>
2020-01-22 14:35:36 +0200

    Working on test with eligible parameters disabled.

--- flow-server/src/test/java/com/vaadin/flow/router/internal/RouteSegmentTest.java
+++ flow-server/src/test/java/com/vaadin/flow/router/internal/RouteSegmentTest.java
@@ -17,10 +17,12 @@
 
 package com.vaadin.flow.router.internal;
 
-import java.util.Optional;
+import java.util.Collections;
+import java.util.Map;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RouteSegmentTest {
@@ -37,21 +39,84 @@ public class RouteSegmentTest {
     public static class Branch extends Component {
     }
 
+    @Tag(Tag.DIV)
+    public static class BranchEdit extends Component {
+    }
+
     @Test
     public void test() {
+
         RouteSegment root = RouteSegment.createRoot();
         root.addPath("", Root.class);
         root.addPath("trunk", Trunk.class);
-        root.addPath("trunk/branch", Branch.class);
+        root.addPath("trunk/branch/:id<int>", Branch.class);
+        root.addPath("trunk/:[name]/:[type]/branch/:[id<int>]/edit",
+                BranchEdit.class);
 
-        Optional<RouteSegment.RouteResult> route = root.findRoute("");
-        System.out.println("route: " + route);
+        RouteSegment.RouteSearchResult result;
+        String path;
 
-        route = root.findRoute("trunk");
-        System.out.println("route: " + route);
+        path = "";
+        result = root.getRoute(path);
+        assertResult(result, path, Root.class, Collections.emptyMap());
 
-        route = root.findRoute("trunk/branch");
-        System.out.println("route: " + route);
+        path = "trunk";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/branch";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/branch/12";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/branch/";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/branch/view";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/branch/edit";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/red/branch/12/edit";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/branch/12/edit";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+        path = "trunk/red/birch/branch/12/edit";
+        result = root.getRoute(path);
+        System.out.println("result: " + result);
+
+    }
+
+    private void assertResult(RouteSegment.RouteSearchResult result,
+                              String path, Class<? extends Component> target,
+                              Map<String, String> urlParameters) {
+
+        System.out.println("result: " + result);
+
+        Assert.assertEquals("Invalid path", path, result.getPath());
+
+        Assert.assertEquals("Weird target", target == null,
+                result.getTarget() == null);
+
+        if (target != null) {
+            Assert.assertTrue("Invalid target",
+                    result.getTarget().containsTarget(target));
+        }
+
+        Assert.assertEquals("Invalid path", urlParameters,
+                result.getUrlParameters());
+
     }
 
 }
