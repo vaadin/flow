9762c0b544fb51c7c8e09d0528df0cacad9117da
Manolo Carrasco <manolo@apache.org>
2020-01-30 13:48:01 +0100

    Windows path needs to be normalized before comparing with theme url.
    This fixes the UpdateThemedImportsTest that fails in windows

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/AbstractUpdateImports.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/AbstractUpdateImports.java
@@ -511,6 +511,7 @@ abstract class AbstractUpdateImports implements Runnable {
                 // don't do anything if such file doesn't exist at all
                 continue;
             }
+            resolvedPath  = normalizePath(resolvedPath);
             if (resolvedPath.contains(theme.getBaseUrl())) {
                 String translatedPath = theme.translateUrl(resolvedPath);
                 if (!visitedImports.contains(translatedPath)
@@ -569,10 +570,13 @@ abstract class AbstractUpdateImports implements Runnable {
         return resolvedPath;
     }
 
-    private String normalizeImportPath(String path) {
-        String importPath = toValidBrowserImport(path);
-        File file = new File(importPath);
+    private String normalizePath(String path) {
+        File file = new File(path);
         return file.toPath().normalize().toString().replace("\\", "/");
+    }
+
+    private String normalizeImportPath(String path) {
+        return toValidBrowserImport(normalizePath(path));
     }
 
     private static String generatedResourcePathIntoRelativePath(String path) {
