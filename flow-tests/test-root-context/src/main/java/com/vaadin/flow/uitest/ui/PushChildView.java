e987872557994bfdd2970f0cc5eb9abe448c192d
Denis Anisimov <denis@vaadin.com>
2020-02-03 10:10:41 +0300

    Test UI classes.

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/PushChildView.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "com.vaadin.flow.uitest.ui.PushChildView", layout = PushLayout.class)
+public class PushChildView extends Div {
+    public PushChildView() {
+        setId("push-childs");
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        setText("Push mode: "
+                + attachEvent.getUI().getPushConfiguration().getPushMode());
+    }
+
+}
