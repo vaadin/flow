bcf82733e6d8ce02c5aeb1f791c198d0196088bd
Denis Anisimov <denis@vaadin.com>
2020-02-11 10:24:54 +0300

    * Handle navigation for HasErrorParameter in the same way as for other views. * Add logic to prevent an infinite loop due changes in HasErrorParameter handling logic
    (cherry picked from commit 56caabd59a15e0753a146786ae52e74de44de78b)

--- flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/routing/NPETargetView.java
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/routing/NPETargetView.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.flow.uitest.ui.temp;
+package com.vaadin.flow.uitest.ui.routing;
 
 import com.vaadin.flow.uitest.ui.AbstractDivView;
 import com.vaadin.flow.router.Route;
