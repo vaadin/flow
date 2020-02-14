3000a4cdea2831ca829600f6c709b2aae5d2cba7
Mehdi Javan <32511762+mehdi-vaadin@users.noreply.github.com>
2020-02-14 18:16:48 +0200

    Remove usage of com.google.common.base.Supplier (#7597)
    * Remove usage of com.google.common.base.Supplier

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/FrontendUtilsTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/FrontendUtilsTest.java
@@ -26,10 +26,10 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.google.common.base.Supplier;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
