3cf8aeed8c4ebdf142c6f879f4777640449661bf
caalador <mikael.grankvist@gmail.com>
2020-02-18 10:07:00 +0200

    Node and npm download and installation (#7571)
    Add features fron eirslett plugin to
    download and install node+npm
    to the system to a target directory.
    
    Fixes #7562
    
    # Conflicts:
    #	flow-server/src/test/java/com/vaadin/flow/server/frontend/FrontendUtilsTest.java

---
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/installer/FileDownloader.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2020 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.server.frontend.installer;
+
+import java.io.File;
+import java.net.URI;
+
+/**
+ * Handle file download from given url to target destination.
+ * <p>
+ * Derived from eirslett/frontend-maven-plugin
+ *
+ * @since
+ */
+interface FileDownloader {
+
+    /**
+     * Download to destination from url using username and password.
+     *
+     * @param downloadTarget
+     *         uri string from where to download
+     * @param destination
+     *         file target directory
+     * @param userName
+     *         user name, {@code null} accepted
+     * @param password
+     *         password, {@code null} accepted
+     * @throws DownloadException
+     *         exception thrown when download fails
+     */
+    void download(URI downloadTarget, File destination, String userName,
+            String password) throws DownloadException;
+}
+
