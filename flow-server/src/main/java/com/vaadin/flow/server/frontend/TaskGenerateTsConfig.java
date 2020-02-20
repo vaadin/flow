c5e9c95f9fd19bf114b752f215438c02786003e2
Manuel Carrasco Moñino <manolo@vaadin.com>
2020-02-20 10:42:17 +0200

    CCDM: do not override tsconfig when index.ts is generated (#7621)
    * CCDM: do not override tsconfig if it exists but there is no custom index.ts file
    
    Fixes #7532
    
    * simplify tsconfig generation condition
    
    * fix tests

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskGenerateTsConfig.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskGenerateTsConfig.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
 
-import static com.vaadin.flow.server.frontend.FrontendUtils.INDEX_TS;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
@@ -32,25 +31,16 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class TaskGenerateTsConfig extends AbstractTaskClientGenerator {
 
     private static final String TSCONFIG_JSON = "tsconfig.json";
-    private final File frontendFolder;
     private final File npmFolder;
-    private final File outputDirectory;
 
     /**
      * Create a task to generate <code>tsconfig.json</code> file.
-     * 
-     * @param frontendFolder
-     *            frontend folder is to check if <code>index.ts</code> exists or
-     *            not.
+     *
      * @param npmFolder
      *            project folder where the file will be generated.
-     * @param outputDirectory
-     *            the output directory of the generated index.ts file
      */
-    TaskGenerateTsConfig(File frontendFolder, File npmFolder, File outputDirectory) {
-        this.frontendFolder = frontendFolder;
+    TaskGenerateTsConfig(File npmFolder) {
         this.npmFolder = npmFolder;
-        this.outputDirectory = outputDirectory;
     }
 
     @Override
@@ -66,9 +56,6 @@ public class TaskGenerateTsConfig extends AbstractTaskClientGenerator {
 
     @Override
     protected boolean shouldGenerate() {
-        File tsConfigFile = new File(npmFolder, TSCONFIG_JSON);
-        return !tsConfigFile.exists()
-                && new File(frontendFolder, INDEX_TS).exists()
-                || new File(outputDirectory, INDEX_TS).exists();
+        return !new File(npmFolder, TSCONFIG_JSON).exists();
     }
 }
