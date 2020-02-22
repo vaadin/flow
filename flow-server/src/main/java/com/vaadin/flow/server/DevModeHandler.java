7223e99ff00294f7cde0e784463c2b703d71ac01
Denis Anisimov <denis@vaadin.com>
2020-02-22 19:06:55 +0300

    Don't throw an exception on getCause but just return it

--- flow-server/src/main/java/com/vaadin/flow/server/DevModeHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/DevModeHandler.java
@@ -76,7 +76,7 @@ import static java.net.HttpURLConnection.HTTP_OK;
 public final class DevModeHandler implements RequestHandler {
 
     /**
-     * 
+     *
      */
     private static final String COULDN_T_START_DEV_SERVER = "Couldn't start dev server because ";
 
@@ -214,7 +214,7 @@ public final class DevModeHandler implements RequestHandler {
         } else if (exception instanceof RuntimeException) {
             return (RuntimeException) exception;
         } else {
-            throw new IllegalStateException(exception);
+            return new IllegalStateException(exception);
         }
     }
 
@@ -606,9 +606,8 @@ public final class DevModeHandler implements RequestHandler {
         File webpackConfig = new File(npmFolder, FrontendUtils.WEBPACK_CONFIG);
         if (!npmFolder.exists()) {
             getLogger().warn("No project folder '{}' exists", npmFolder);
-            throw new ExecutionFailedException(
-                    COULDN_T_START_DEV_SERVER
-                            + "the target execution folder doesn't exist.");
+            throw new ExecutionFailedException(COULDN_T_START_DEV_SERVER
+                    + "the target execution folder doesn't exist.");
         }
         if (!webpack.exists()) {
             getLogger().warn("'{}' doesn't exist. Did you run `npm install`?",
