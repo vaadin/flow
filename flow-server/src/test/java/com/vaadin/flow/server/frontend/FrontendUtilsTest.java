583976549412e63628d0e87e9ed95c24aba40577
Manuel Carrasco Moñino <manolo@apache.org>
2020-03-05 08:36:41 +0100

    Fix exported webcomponents in client-side (#7614)
    Fixes #7550

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/FrontendUtilsTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/FrontendUtilsTest.java
@@ -265,7 +265,8 @@ public class FrontendUtilsTest {
         String statsAssetsByChunkName = FrontendUtils
                 .getStatsAssetsByChunkName(service);
 
-        Assert.assertEquals("{" + "\"index\": \"build/index-1111.cache.js\"}",
+        Assert.assertEquals(
+                "{\"bundle\": \"build/vaadin-bundle-1111.cache.js\",\"export\": \"build/vaadin-export-2222.cache.js\"}",
                 statsAssetsByChunkName);
     }
 
@@ -277,7 +278,8 @@ public class FrontendUtilsTest {
         String statsAssetsByChunkName = FrontendUtils
                 .getStatsAssetsByChunkName(service);
 
-        Assert.assertEquals("{" + "\"index\": \"build/index-1111.cache.js\"}",
+        Assert.assertEquals(
+                "{\"bundle\": \"build/vaadin-bundle-1111.cache.js\"}",
                 statsAssetsByChunkName);
     }
 
