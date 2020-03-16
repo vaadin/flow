952f9b0f6f634a9887dd90906a3bb7ae8ade4770
Jonathan Hult <Jonathan@JonathanHult.com>
2020-03-16 11:27:11 -0400

    Fixed tests so that compiler actually verifies subtype

--- flow-data/src/test/java/com/vaadin/flow/data/provider/HasDataProviderTest.java
+++ flow-data/src/test/java/com/vaadin/flow/data/provider/HasDataProviderTest.java
@@ -3,31 +3,36 @@ package com.vaadin.flow.data.provider;
 import com.vaadin.flow.data.binder.HasDataProvider;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 /**
  * Test to ensure we allow using a generic wildcard for {@link HasDataProvider#setDataProvider(DataProvider)}.
  */
 public class HasDataProviderTest {
 
+    private static class Animal {}
+    private static class Dog extends Animal {}
+
     @Test
     public void setDataProvider_generic_wildcard() {
-        ListDataProvider<ArrayList<String>> arrayListListDataProvider = new ListDataProvider<>(Collections.emptyList());
-        HasDataProviderImpl<List<String>> hasDataProviderImpl = new HasDataProviderImpl<>();
-        hasDataProviderImpl.setDataProvider(arrayListListDataProvider);
+        final HasDataProviderImpl<Animal> hasDataProviderImpl = new HasDataProviderImpl<>();
+
+        final WildcardDataProvider<Animal> animalProvider = new WildcardDataProvider<>(Collections.emptyList());
+        hasDataProviderImpl.setDataProvider(animalProvider);
+
+        final WildcardDataProvider<Dog> dogProvider = new WildcardDataProvider<>(Collections.emptyList());
+        hasDataProviderImpl.setDataProvider(dogProvider);
     }
 
     private static class HasDataProviderImpl<T> implements HasDataProvider<T> {
 
         @Override
-        public void setDataProvider(DataProvider<? extends T, ?> dataProvider) {
+        public void setDataProvider(final DataProvider<? extends T, ?> dataProvider) {
         }
 
         @Override
-        public void setItems(Collection<T> items) {
+        public void setItems(final Collection<T> items) {
         }
     }
 }
--- flow-data/src/test/java/com/vaadin/flow/data/provider/HasFilterableDataProviderTest.java
+++ flow-data/src/test/java/com/vaadin/flow/data/provider/HasFilterableDataProviderTest.java
@@ -2,10 +2,8 @@ package com.vaadin.flow.data.provider;
 
 import com.vaadin.flow.data.binder.HasFilterableDataProvider;
 import com.vaadin.flow.function.SerializableFunction;
-import com.vaadin.flow.function.SerializablePredicate;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -14,25 +12,32 @@ import java.util.Collections;
  */
 public class HasFilterableDataProviderTest {
 
+    private static class Animal {}
+    private static class Dog extends Animal{}
+
     @Test
     public void setDataProvider_generic_wildcard() {
-        ListDataProvider<ArrayList<String>> arrayListListDataProvider = new ListDataProvider<>(Collections.emptyList());
-        HasFilterableDataProviderImpl<ArrayList<String>, SerializablePredicate<ArrayList<String>>> hasDataProviderImpl = new HasFilterableDataProviderImpl<>();
-        hasDataProviderImpl.setDataProvider(arrayListListDataProvider);
+        final HasFilterableDataProviderImpl<Animal, Void> hasDataProviderImpl = new HasFilterableDataProviderImpl<>();
+
+        final WildcardDataProvider<Animal> animalProvider = new WildcardDataProvider<>(Collections.emptyList());
+        hasDataProviderImpl.setDataProvider(animalProvider);
+
+        final WildcardDataProvider<Dog> dogProvider = new WildcardDataProvider<>(Collections.emptyList());
+        hasDataProviderImpl.setDataProvider(dogProvider);
     }
 
     private static class HasFilterableDataProviderImpl<T, F> implements HasFilterableDataProvider<T, F> {
 
         @Override
-        public void setDataProvider(DataProvider<? extends T, F> dataProvider) {
+        public void setDataProvider(final DataProvider<? extends T, F> dataProvider) {
         }
 
         @Override
-        public <C> void setDataProvider(DataProvider<? extends T, C> dataProvider, SerializableFunction<F, C> filterConverter) {
+        public <C> void setDataProvider(final DataProvider<? extends T, C> dataProvider, SerializableFunction<F, C> filterConverter) {
         }
 
         @Override
-        public void setItems(Collection<T> items) {
+        public void setItems(final Collection<T> items) {
         }
     }
 }
