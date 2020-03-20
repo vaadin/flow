52153b14e1e941d1beacb609639aad20c946b59c
Denis Anisimov <denis@vaadin.com>
2020-03-20 15:58:17 +0300

    Use some dir as a home to install pnpm

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/TaskRunPnpmInstallTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/TaskRunPnpmInstallTest.java
@@ -44,9 +44,11 @@ public class TaskRunPnpmInstallTest extends TaskRunNpmInstallTest {
     @Before
     public void setUp() throws IOException {
         super.setUp();
+
+        File home = temporaryFolder.newFolder();
         // ensure there is a valid pnpm installed in the system
-        new FrontendTools(getNodeUpdater().npmFolder.getAbsolutePath(), null)
-                .ensurePnpm();
+        new FrontendTools(getNodeUpdater().npmFolder.getAbsolutePath(),
+                () -> home.getAbsolutePath()).ensurePnpm();
         // create an empty package.json so as pnpm can be run without error
         FileUtils.write(new File(getNodeUpdater().npmFolder, PACKAGE_JSON),
                 "{}", StandardCharsets.UTF_8);
