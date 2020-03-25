6fbabeb5470689ec7a4829ddb4db3328c39c5813
Denis Anisimov <denis@vaadin.com>
2020-03-25 13:33:04 +0300

    Disable failing test because of #7878.

--- flow-tests/test-root-ui-context/src/test/java/com/vaadin/flow/uitest/ui/push/ReconnectLongPollingIT.java
+++ flow-tests/test-root-ui-context/src/test/java/com/vaadin/flow/uitest/ui/push/ReconnectLongPollingIT.java
@@ -1,7 +1,11 @@
 package com.vaadin.flow.uitest.ui.push;
 
+import org.junit.Ignore;
+
 import com.vaadin.flow.component.UI;
 
+@Ignore("see https://github.com/vaadin/flow/issues/7878."
+        + " The test is failing in 3.0 branch because there a bug in this branch.")
 public class ReconnectLongPollingIT extends ReconnectTest {
 
     @Override
