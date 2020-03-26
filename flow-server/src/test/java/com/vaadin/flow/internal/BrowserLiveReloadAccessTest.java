7e371afa45bb0a78edf87b4fa2ed53af568116f7
Mehdi Javan <mehdi@vaadin.com>
2020-03-26 11:46:08 +0200

    Add tests

--- flow-server/src/test/java/com/vaadin/flow/internal/BrowserLiveReloadAccessTest.java
+++ flow-server/src/test/java/com/vaadin/flow/internal/BrowserLiveReloadAccessTest.java
@@ -39,6 +39,18 @@ public class BrowserLiveReloadAccessTest {
     }
 
     @Test
+    public void getLiveReload_liveReloadDisabled_nullIsReturned() {
+        VaadinService service = Mockito.mock(VaadinService.class);
+        DeploymentConfiguration config = Mockito
+                .mock(DeploymentConfiguration.class);
+        Mockito.when(service.getDeploymentConfiguration()).thenReturn(config);
+        Mockito.when(config.isProductionMode()).thenReturn(false);
+        Mockito.when(config.isLiveReloadEnabled()).thenReturn(false);
+
+        Assert.assertNull(access.getLiveReload(service));
+    }
+
+    @Test
     public void getLiveReload_devMode_contextHasNoReloadInstance_instanceIsCreated() {
         VaadinService service = Mockito.mock(VaadinService.class);
         DeploymentConfiguration config = Mockito
