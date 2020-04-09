0e0d0e091eb31f123abec0ec45238f1292f4191c
Manolo Carrasco <manolo@vaadin.com>
2020-04-09 08:57:44 +0200

    validators are classes implementing Validator

--- flow-client/src/main/resources/META-INF/resources/frontend/FormValidator.ts
+++ flow-client/src/main/resources/META-INF/resources/frontend/FormValidator.ts
@@ -1,15 +1,58 @@
 import * as validator from 'validator';
 
+export type ValidationCallback<T> = (value: T) => boolean | Promise<boolean>;
+
+export interface Validator<T> {
+  validate: ValidationCallback<T>,
+  message: string,
+  value?: any
+}
+
 // JSR380 equivalent (https://beanvalidation.org/2.0/spec/#builtinconstraints)
-export const email = (value: string) => validator.isEmail(value);
-export const isNull = (value: any) => value == null;
-export const notNull = (value: any) => !isNull(value);
-export const notEmpty = (value: any) => notNull(value) && !validator.isEmpty(value);
-export const notBlank= (value: any) => notEmpty(value);
-export const assertTrue = (value: any) => validator.isBoolean(String(value)) && String(value) === 'true';
-export const assertFalse = (value: any) => !assertTrue(value);
-export const min = (value: any, limit: number) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {min: limit})
-export const max = (value: any, limit: number) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {max: limit})
+export class Email implements Validator<string> {
+  validate = (value: string) => validator.isEmail(value);
+  message = 'invalid';
+}
+export class Null implements Validator<any> {
+  validate = (value: any) => value == null;
+  message = 'invalid';
+}
+export class NotNull implements Validator<any> {
+  validate = (value: any) => !new Null().validate(value);
+  message = 'invalid';
+}
+export class NotEmpty implements Validator<any> {
+  validate = (value: any) => new NotNull().validate(value) && !validator.isEmpty(value);
+  message = 'invalid';
+}
+export class NotBlank implements Validator<any> {
+  validate = (value: any) => new NotEmpty().validate(value);
+  message = 'invalid';
+}
+export class AssertTrue implements Validator<any> {
+  validate = (value: any) => validator.isBoolean(String(value)) && String(value) === 'true';
+  message = 'invalid';
+}
+export class AssertFalse implements Validator<any> {
+  validate = (value: any) => !new AssertTrue().validate(value);
+  message = 'invalid';
+}
+export class Min implements Validator<any> {
+  validate = (value: any) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {min: this.value})
+  message = 'invalid';
+  value: number;
+  constructor(value: number) {
+    this.value = value;
+  }
+}
+export class Max implements Validator<any> {
+  validate = (value: any) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {max: this.value})
+  message = 'invalid';
+  value: number;
+  constructor(value: number) {
+    this.value = value;
+  }
+}
 
 // export const decimalMin
 // export const decimalMax
