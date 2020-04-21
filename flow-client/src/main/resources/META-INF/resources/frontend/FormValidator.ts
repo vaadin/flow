1e69070108f3b7ea6550b1b81181b3d49bd35fdb
Manolo Carrasco <manolo@vaadin.com>
2020-04-21 09:37:53 +0200

    fix lint errors in ts

--- flow-client/src/main/resources/META-INF/resources/frontend/FormValidator.ts
+++ flow-client/src/main/resources/META-INF/resources/frontend/FormValidator.ts
@@ -1,3 +1,5 @@
+/* tslint:disable:max-classes-per-file */
+
 import * as validator from 'validator';
 
 export type ValidationCallback<T> = (value: T) => boolean | Promise<boolean>;
@@ -10,128 +12,128 @@ export interface Validator<T> {
 
 // JSR380 equivalent (https://beanvalidation.org/2.0/spec/#builtinconstraints)
 export class Email implements Validator<string> {
-  validate = (value: string) => validator.isEmail(value);
   message = 'invalid';
+  validate = (value: string) => validator.isEmail(value);
 }
 export class Null implements Validator<any> {
-  validate = (value: any) => value == null;
   message = 'invalid';
+  validate = (value: any) => value == null;
 }
 export class NotNull implements Validator<any> {
-  validate = (value: any) => !new Null().validate(value);
   message = 'invalid';
+  validate = (value: any) => !new Null().validate(value);
 }
 export class NotEmpty implements Validator<any> {
-  validate = (value: any) => new NotNull().validate(value) && !validator.isEmpty(value);
   message = 'invalid';
+  validate = (value: any) => new NotNull().validate(value) && !validator.isEmpty(value);
 }
 export class NotBlank implements Validator<any> {
-  validate = (value: any) => new NotEmpty().validate(value);
   message = 'invalid';
+  validate = (value: any) => new NotEmpty().validate(value);
 }
 export class AssertTrue implements Validator<any> {
-  validate = (value: any) => validator.isBoolean(String(value)) && String(value) === 'true';
   message = 'invalid';
+  validate = (value: any) => validator.isBoolean(String(value)) && String(value) === 'true';
 }
 export class AssertFalse implements Validator<any> {
-  validate = (value: any) => !new AssertTrue().validate(value);
   message = 'invalid';
+  validate = (value: any) => !new AssertTrue().validate(value);
 }
 export class Min implements Validator<any> {
-  validate = (value: any) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {min: this.value})
   message = 'invalid';
   value: number;
   constructor(value: number) {
     this.value = value;
   }
+  validate = (value: any) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {min: this.value})
 }
 export class Max implements Validator<any> {
-  validate = (value: any) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {max: this.value})
   message = 'invalid';
   value: number;
   constructor(value: number) {
     this.value = value;
   }
+  validate = (value: any) => validator.isNumeric(String(value)) && validator.isFloat(String(value), {max: this.value})
 }
 
 export class DecimalMin implements Validator<any> {
-  validate = (value: any) => validator.isDecimal(`${value}`, {decimal_digits: `${this.value},`, force_decimal: true});
   message = 'invalid';
   value: number;
   constructor(value: number) {
     this.value = value;
   }
+  validate = (value: any) => validator.isDecimal(`${value}`, {decimal_digits: `${this.value},`, force_decimal: true});
 }
 export class DecimalMax implements Validator<any> {
-  validate = (value: any) => validator.isDecimal(`${value}`, {decimal_digits: `0,${this.value}`});
   message = 'invalid';
   value: number;
   constructor(value: number) {
     this.value = value;
   }
+  validate = (value: any) => validator.isDecimal(`${value}`, {decimal_digits: `0,${this.value}`});
 }
 
 export class Negative implements Validator<any> {
-  validate = (value: any) => validator.toFloat(`${value}`) < 0;
   message = 'invalid';
+  validate = (value: any) => validator.toFloat(`${value}`) < 0;
 }
 export class NegativeOrCero implements Validator<any> {
-  validate = (value: any) => validator.toFloat(`${value}`) <= 0;
   message = 'invalid';
+  validate = (value: any) => validator.toFloat(`${value}`) <= 0;
 }
 
 export class Positive implements Validator<any> {
-  validate = (value: any) => validator.toFloat(`${value}`) > 0;
   message = 'invalid';
+  validate = (value: any) => validator.toFloat(`${value}`) > 0;
 }
 
 export class PositiveOrCero implements Validator<any> {
-  validate = (value: any) => validator.toFloat(`${value}`) >= 0;
   message = 'invalid';
+  validate = (value: any) => validator.toFloat(`${value}`) >= 0;
 }
 
 export class Size implements Validator<string> {
-  validate = (value: string) => validator.isLength(value, this.value);
   message = 'invalid';
   value: any;
   constructor(min: number, max: number) {
     this.value = {min, max};
   }
+  validate = (value: string) => validator.isLength(value, this.value);
 }
 
 export class Digits implements Validator<string> {
-  validate = (value: any) =>
-      String(validator.toFloat(`${value}`)).replace(/(.*)\.\d+/, "$1").length === this.value.integer
-      && new DecimalMax(this.value.decimal).validate(value);
   message = 'invalid';
   value: any;
   constructor(integer: number, decimal: number) {
     this.value = {integer, decimal};
   }
+  validate = (value: any) =>
+      String(validator.toFloat(`${value}`)).replace(/(.*)\.\d+/, "$1").length === this.value.integer
+      && new DecimalMax(this.value.decimal).validate(value);
 }
 
 export class Past implements Validator<any> {
-  validate = (value: any) => validator.isBefore(value);
   message = 'invalid';
+  validate = (value: any) => validator.isBefore(value);
 }
 
 export class PastOrPresent implements Validator<any> {
-  validate = (value: any) => validator.isBefore(value);
   message = 'invalid';
+  validate = (value: any) => validator.isBefore(value);
 }
 
 export class Future implements Validator<any> {
-  validate = (value: any) => validator.isAfter(value);
   message = 'invalid';
+  validate = (value: any) => validator.isAfter(value);
 }
 
 export class Pattern implements Validator<string> {
-  validate = (value: any) => validator.matches(value, this.value);
   message = 'invalid';
   value: any;
   constructor(pattern: RegExp) {
     this.value = pattern;
   }
+  validate = (value: any) => validator.matches(value, this.value);
 }
 
 // export class PastOrPresent implements Validator<any>
