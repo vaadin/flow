a6ad0b2acbb5bbddfcea09736cdb07c806c27fcf
João Ferreira <joao@vaadin.com>
2020-05-12 01:04:37 +0300

    Change test to assert.deepEqual

--- flow-client/src/test/frontend/VaadinDevmodeGizmoTests.js
+++ flow-client/src/test/frontend/VaadinDevmodeGizmoTests.js
@@ -38,6 +38,6 @@ describe('VaadinDevmodeGizmo', () => {
     };
     assert.equal(gizmo.status, 'error');
     assert.equal(gizmo.messages.length, 1);
-    assert.equal(gizmo.messages[0], message);
+    assert.deepEqual(gizmo.messages[0], message);
   });
 });
