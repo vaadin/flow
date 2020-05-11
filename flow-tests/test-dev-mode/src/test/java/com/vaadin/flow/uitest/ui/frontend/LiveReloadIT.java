9c7fe7d4056dc78843d3395cde94c54f5f9a2e34
Johannes Eriksson <joheriks@vaadin.com>
2020-05-11 12:19:18 +0300

    Force live-reload ITs to run in sequence using `@NotThreadSafe` rather than locks (#8279)

--- flow-tests/test-dev-mode/src/test/java/com/vaadin/flow/uitest/ui/frontend/LiveReloadIT.java
+++ flow-tests/test-dev-mode/src/test/java/com/vaadin/flow/uitest/ui/frontend/LiveReloadIT.java
@@ -16,14 +16,10 @@
 
 package com.vaadin.flow.uitest.ui.frontend;
 
-import javax.annotation.concurrent.NotThreadSafe;
+import net.jcip.annotations.NotThreadSafe;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -31,25 +27,8 @@ import org.openqa.selenium.WebElement;
 
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 
-// These tests are not parallelizable, nor should they be run at the same time
-// as other tests in the same module, due to live-reload affecting the whole
-// application
 @NotThreadSafe
 public class LiveReloadIT extends ChromeBrowserTest {
-
-    private static final Lock lock = new ReentrantLock();
-
-    @Before
-    @Override
-    public void setup() throws Exception {
-        lock.lock();
-        super.setup();
-    }
-
-    @After
-    public void tearDown() {
-        lock.unlock();
-    }
 
     @Test
     public void overlayShouldRender() {
