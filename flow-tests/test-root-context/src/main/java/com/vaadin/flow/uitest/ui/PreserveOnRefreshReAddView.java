dcf5a313939722f65ae19b5fd1b86b20b1dd1c06
Denis Anisimov <denis@vaadin.com>
2020-05-18 11:02:39 +0300

    Add IT test

--- flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/PreserveOnRefreshReAddView.java
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/PreserveOnRefreshReAddView.java
@@ -30,6 +30,7 @@ public class PreserveOnRefreshReAddView extends Div {
         Text text2 = new Text("Another Text");
 
         Div container = new Div();
+        container.setId("container");
 
         NativeButton setText = new NativeButton("Set text", e -> {
             container.removeAll();
