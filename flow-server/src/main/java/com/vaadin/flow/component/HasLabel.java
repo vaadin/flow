48a57431fd016893c3278e64580fe1f66c4b2d28
Ovidiu Popa <opopa1991@gmail.com>
2020-05-26 18:12:12 +0300

    Create new mixin interface HasLabel(#3241)
    Add new HasLabel interface to be used by components that supports label
    definitions

---
+++ flow-server/src/main/java/com/vaadin/flow/component/HasLabel.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2000-2020 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component;
+
+/**
+ * A component that supports label definition. 
+ * <p>
+ * The default implementations set the label of the component to the given text for
+ * {@link #getElement()}. Override all methods in this interface if the text
+ * should be added to some other element.
+ *
+ *
+ * @author Vaadin Ltd
+ * @since 3.0
+ */
+public interface HasLabel extends HasElement{
+
+	 static final String LABEL_PROPERTY_NAME = "label";
+
+	/**
+     * Set the label of the component to the given text.
+     * <p>
+     * Any HTML is automatically escaped to prevent injection attacks.
+     *
+     * @param label
+     *            the label text to set
+     */
+    default public void setLabel(String label) {
+        getElement().setProperty(LABEL_PROPERTY_NAME, label == null ? "" : label);
+    }
+
+    /**
+     * Gets the label of the component.
+     *
+     * @return the label of the component or <code>""</code> if no label has
+     *         been set
+     */
+    default String getLabel() {
+        return getElement().getProperty(LABEL_PROPERTY_NAME, "");
+    }
+}
