2eee4163dd73645cd51a5e7fbb6b588eb37f19e6
Denis <denis@vaadin.com>
2020-05-27 14:22:11 +0300

    Don't override initial value of hidden HTML Element attribute. (#8419)
    Fixes #8100

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/template/HiddenTemplateIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/template/HiddenTemplateIT.java
@@ -31,7 +31,8 @@ public class HiddenTemplateIT extends ChromeBrowserTest {
         open();
 
         TestBenchElement template = $(TestBenchElement.class).id("template");
-        WebElement child = template.$(TestBenchElement.class).id("child");
+        WebElement child = template.$(TestBenchElement.class)
+                .id("hidden-child");
         Assert.assertNotNull(child.getAttribute("hidden"));
 
         WebElement visibility = template.$(TestBenchElement.class)
@@ -48,9 +49,6 @@ public class HiddenTemplateIT extends ChromeBrowserTest {
         open();
 
         TestBenchElement template = $(TestBenchElement.class).id("template");
-
-        WebElement hidden = template.$(TestBenchElement.class).id("hidden");
-        hidden.click();
 
         WebElement child = template.$(TestBenchElement.class).id("child");
         Assert.assertNull(child.getAttribute("hidden"));
