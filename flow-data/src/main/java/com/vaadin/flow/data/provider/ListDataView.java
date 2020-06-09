9c7a435e1ef57fa8bd86ceb24f65f1994aca54ca
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2020-06-09 15:03:27 +0300

    UpdateItem javadoc fix
    Co-authored-by: caalador <mikael.grankvist@gmail.com>

--- flow-data/src/main/java/com/vaadin/flow/data/provider/ListDataView.java
+++ flow-data/src/main/java/com/vaadin/flow/data/provider/ListDataView.java
@@ -120,7 +120,7 @@ public interface ListDataView<T, V extends ListDataView<T, ?>>
      * Finds an items equal to {@code item} in the non-filtered data set
      * and replaces it with {@code item}.
      * <p>
-     * Equality between items relies on equality of its identifiers
+     * Equality between the items is determined by the identifiers
      * provided by {@link DataProvider#getId(Object)}.
      *
      * @param item
