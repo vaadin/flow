c57888229981b5a4c44b7651b0686b602fa71222
Denis Anisimov <denis@vaadin.com>
2020-06-26 07:50:24 +0300

    Use once initialized field instead of local variable.

--- flow-server/src/test/java/com/vaadin/flow/server/WebBrowserTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/WebBrowserTest.java
@@ -20,52 +20,40 @@ import org.junit.Test;
 
 public class WebBrowserTest {
 
+    private WebBrowser browser = new WebBrowser();
+
     @Test
     public void isLinux_noDetails_returnsFalse() {
-        WebBrowser browser = new WebBrowser();
-
         Assert.assertFalse(browser.isLinux());
     }
 
     @Test
     public void isMacOSX_noDetails_returnsFalse() {
-        WebBrowser browser = new WebBrowser();
-
         Assert.assertFalse(browser.isMacOSX());
     }
 
     @Test
     public void isWindows_noDetails_returnsFalse() {
-        WebBrowser browser = new WebBrowser();
-
         Assert.assertFalse(browser.isWindows());
     }
 
     @Test
     public void isWindowsPhone_noDetails_returnsFalse() {
-        WebBrowser browser = new WebBrowser();
-
         Assert.assertFalse(browser.isWindowsPhone());
     }
 
     @Test
     public void isAndroid_noDetails_returnsFalse() {
-        WebBrowser browser = new WebBrowser();
-
         Assert.assertFalse(browser.isAndroid());
     }
 
     @Test
     public void isIPhone_noDetails_returnsFalse() {
-        WebBrowser browser = new WebBrowser();
-
         Assert.assertFalse(browser.isIPhone());
     }
 
     @Test
     public void isChromeOS_noDetails_returnsFalse() {
-        WebBrowser browser = new WebBrowser();
-
         Assert.assertFalse(browser.isChromeOS());
     }
 }
