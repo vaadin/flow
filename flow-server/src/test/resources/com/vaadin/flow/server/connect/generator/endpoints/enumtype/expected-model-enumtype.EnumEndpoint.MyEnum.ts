d9bc8379f9f6ef265069b39bd8976f0d1d99d69a
Kari Söderholm <haprog@vaadin.com>
2020-06-30 13:05:35 +0300

    Fix tests

--- flow-server/src/test/resources/com/vaadin/flow/server/connect/generator/endpoints/enumtype/expected-model-enumtype.EnumEndpoint.MyEnum.ts
+++ flow-server/src/test/resources/com/vaadin/flow/server/connect/generator/endpoints/enumtype/expected-model-enumtype.EnumEndpoint.MyEnum.ts
@@ -2,7 +2,8 @@
  * This module is generated from com.vaadin.flow.server.connect.generator.endpoints.enumtype.EnumEndpoint.MyEnum.
  * All changes to this file are overridden. Please consider to make changes in the corresponding Java file if necessary.
  */
-export const enum MyEnum {
+const enum MyEnum {
   ENUM1 = 'ENUM1',
   ENUM2 = 'ENUM2',
 }
+export default MyEnum;
