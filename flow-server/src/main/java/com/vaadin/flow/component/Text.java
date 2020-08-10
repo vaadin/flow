ec3db1c693a06176c3398eac1c6a4128b4522f88
Mikhail Shabarov <mikhail@vaadin.com>
2020-08-10 17:04:45 +0300

    Extend javadocs for Text and clarify not-supported API (#8799)

--- flow-server/src/main/java/com/vaadin/flow/component/Text.java
+++ flow-server/src/main/java/com/vaadin/flow/component/Text.java
@@ -19,6 +19,15 @@ import com.vaadin.flow.dom.Element;
 
 /**
  * A component which encapsulates the given text in a text node.
+ * <p>
+ * Text node doesn't support setting any attribute or property so you may not
+ * use Element API (and {@link Text} doesn't provide any such contract) for
+ * setting attribute/property. It implies that you may not style this component
+ * as well. Any attempt to set attribute/property value throws an exception. The
+ * only available API for a {@link Text} component is set a text.
+ * <p>
+ * If you need a text component which can be styled then check {@code Span}
+ * class (from {@code flow-html-components}) module.
  *
  * @author Vaadin Ltd
  * @since 1.0
@@ -57,6 +66,41 @@ public class Text extends Component implements HasText {
     @Override
     public String getText() {
         return getElement().getText();
+    }
+
+    @Override
+    protected <T> void set(PropertyDescriptor<T, ?> descriptor, T value) {
+        throw new UnsupportedOperationException("Cannot set '"
+                + descriptor.getPropertyName() + "' property to the "
+                + getClass().getSimpleName() + " component because it doesn't "
+                + "represent an HTML Element but a text Node on the client side.");
+    }
+
+    /**
+     * The method is not supported for the {@link Text} class.
+     * <p>
+     * Always throws an {@link UnsupportedOperationException}.
+     *
+     * @throws UnsupportedOperationException
+     */
+    @Override
+    public void setId(String id) {
+        super.setId(id);
+    }
+
+    /**
+     * The method is not supported for the {@link Text} class.
+     * <p>
+     * Always throws an {@link UnsupportedOperationException}.
+     *
+     * @throws UnsupportedOperationException
+     */
+    @Override
+    public void setVisible(boolean visible) {
+        throw new UnsupportedOperationException("Cannot change "
+                + getClass().getSimpleName()
+                + " component visibility because it doesn't "
+                + "represent an HTML Element but a text Node on the client side.");
     }
 
 }
