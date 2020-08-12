80e31943fce1831d4c85eef6879c9c4a67247770
Denis <denis@vaadin.com>
2020-08-12 14:46:22 +0300

    Add transmission semantic for URI attribute values (#8826)
    * Add transmission semantic for URI attribute values
    
    Fixes #8813
    
    * Add some comments, correct javadocs
    
    * Fix GWT tests
    
    * Use one magic method instead of another
    
    * Check the type of JsonValue before read it

--- flow-client/src/main/java/com/vaadin/client/flow/binding/SimpleElementBindingStrategy.java
+++ flow-client/src/main/java/com/vaadin/client/flow/binding/SimpleElementBindingStrategy.java
@@ -24,6 +24,7 @@ import jsinterop.annotations.JsFunction;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.Scheduler;
 
+import com.vaadin.client.ApplicationConfiguration;
 import com.vaadin.client.Command;
 import com.vaadin.client.Console;
 import com.vaadin.client.ElementUtil;
@@ -290,9 +291,9 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
     private native void hookUpPolymerElement(StateNode node, Element element)
     /*-{
         var self = this;
-    
+
         var originalPropertiesChanged = element._propertiesChanged;
-    
+
         if (originalPropertiesChanged) {
             element._propertiesChanged = function (currentProps, changedProps, oldProps) {
                 $entry(function () {
@@ -301,16 +302,16 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                 originalPropertiesChanged.apply(this, arguments);
             };
         }
-    
-    
+
+
         var tree = node.@com.vaadin.client.flow.StateNode::getTree()();
-    
+
         var originalReady = element.ready;
-    
+
         element.ready = function (){
             originalReady.apply(this, arguments);
             @com.vaadin.client.PolymerUtils::fireReadyEvent(*)(element);
-    
+
             // The  _propertiesChanged method which is replaced above for the element
             // doesn't do anything for items in dom-repeat.
             // Instead it's called with some meaningful info for the <code>dom-repeat</code> element.
@@ -319,7 +320,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
             // which changes this method for any dom-repeat instance.
             var replaceDomRepeatPropertyChange = function(){
                 var domRepeat = element.root.querySelector('dom-repeat');
-    
+
                 if ( domRepeat ){
                  // If the <code>dom-repeat</code> element is in the DOM then
                  // this method should not be executed anymore. The logic below will replace
@@ -333,12 +334,12 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                 // if dom-repeat is found => replace _propertiesChanged method in the prototype and mark it as replaced.
                 if ( !domRepeat.constructor.prototype.$propChangedModified){
                     domRepeat.constructor.prototype.$propChangedModified = true;
-    
+
                     var changed = domRepeat.constructor.prototype._propertiesChanged;
-    
+
                     domRepeat.constructor.prototype._propertiesChanged = function(currentProps, changedProps, oldProps){
                         changed.apply(this, arguments);
-    
+
                         var props = Object.getOwnPropertyNames(changedProps);
                         var items = "items.";
                         var i;
@@ -359,7 +360,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                                     if( currentPropsItem && currentPropsItem.nodeId ){
                                         var nodeId = currentPropsItem.nodeId;
                                         var value = currentPropsItem[propertyName];
-    
+
                                         // this is an attempt to find the template element
                                         // which is not available as a context in the protype method
                                         var host = this.__dataHost;
@@ -370,7 +371,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                                         while( !host.localName || host.__dataHost ){
                                             host = host.__dataHost;
                                         }
-    
+
                                         $entry(function () {
                                             @SimpleElementBindingStrategy::handleListItemPropertyChange(*)(nodeId, host, propertyName, value, tree);
                                         })();
@@ -381,7 +382,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                     };
                 }
             };
-    
+
             // dom-repeat doesn't have to be in DOM even if template has it
             //  such situation happens if there is dom-if e.g. which evaluates to <code>false</code> initially.
             // in this case dom-repeat is not yet in the DOM tree until dom-if becomes <code>true</code>
@@ -396,7 +397,7 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
                 element.addEventListener('dom-change',replaceDomRepeatPropertyChange);
             }
         }
-    
+
     }-*/;
 
     private static void handleListItemPropertyChange(double nodeId,
@@ -630,7 +631,10 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
     private void updateVisibility(Element element, NodeMap visibilityData,
             Boolean visibility) {
         storeInitialHiddenAttribute(element, visibilityData);
-        WidgetUtil.updateAttribute(element, HIDDEN_ATTRIBUTE, visibility);
+        updateAttributeValue(
+                visibilityData.getNode().getTree().getRegistry()
+                        .getApplicationConfiguration(),
+                element, HIDDEN_ATTRIBUTE, visibility);
     }
 
     private void restoreInitialHiddenAttribute(Element element,
@@ -638,8 +642,10 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
         MapProperty initialVisibility = storeInitialHiddenAttribute(element,
                 visibilityData);
         if (initialVisibility.hasValue()) {
-            WidgetUtil.updateAttribute(element, HIDDEN_ATTRIBUTE,
-                    initialVisibility.getValue());
+            updateAttributeValue(
+                    visibilityData.getNode().getTree().getRegistry()
+                            .getApplicationConfiguration(),
+                    element, HIDDEN_ATTRIBUTE, initialVisibility.getValue());
         }
     }
 
@@ -734,7 +740,10 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
 
     private void updateAttribute(MapProperty mapProperty, Element element) {
         String name = mapProperty.getName();
-        WidgetUtil.updateAttribute(element, name, mapProperty.getValue());
+        updateAttributeValue(
+                mapProperty.getMap().getNode().getTree().getRegistry()
+                        .getApplicationConfiguration(),
+                element, name, mapProperty.getValue());
     }
 
     private EventRemover bindChildren(BindingContext context) {
@@ -1364,6 +1373,35 @@ public class SimpleElementBindingStrategy implements BindingStrategy<Element> {
         assert context.htmlNode instanceof Element : "Cannot bind client delegate methods to a Node";
         return ServerEventHandlerBinder.bindServerEventHandlerNames(
                 (Element) context.htmlNode, context.node);
+    }
+
+    private static void updateAttributeValue(
+            ApplicationConfiguration configuration, Element element,
+            String attribute, Object value) {
+        if (value == null || value instanceof String) {
+            WidgetUtil.updateAttribute(element, attribute, (String) value);
+        } else {
+            JsonValue jsonValue = WidgetUtil.crazyJsoCast(value);
+            if (JsonType.OBJECT.equals(jsonValue.getType())) {
+                JsonObject object = (JsonObject) jsonValue;
+                assert object.hasKey(
+                        NodeProperties.URI_ATTRIBUTE) : "Implementation error: JsonObject is recieved as an attribute value for '"
+                                + attribute + "' but it has no "
+                                + NodeProperties.URI_ATTRIBUTE + " key";
+                String uri = object.getString(NodeProperties.URI_ATTRIBUTE);
+                if (configuration.isWebComponentMode()) {
+                    String baseUri = configuration.getServiceUrl();
+                    baseUri = baseUri.endsWith("/") ? baseUri : baseUri + "/";
+                    WidgetUtil.updateAttribute(element, attribute,
+                            baseUri + uri);
+                } else {
+                    WidgetUtil.updateAttribute(element, attribute, uri);
+                }
+            } else {
+                WidgetUtil.updateAttribute(element, attribute,
+                        value.toString());
+            }
+        }
     }
 
     private static EventExpression getOrCreateExpression(
