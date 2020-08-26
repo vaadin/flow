1e3bc10ed22d4771288c143c9e5529a6ee2b573b
Kari Söderholm <haprog@vaadin.com>
2020-08-26 09:55:47 +0300

    Add missing @deprecated Javadoc tag

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendUtils.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendUtils.java
@@ -128,18 +128,20 @@ public class FrontendUtils {
     /**
      * Default folder for copying front-end resources present in the classpath
      * jars.
-     * @see #DEFAULT_FLOW_RESOURCES_FOLDER
      */
-    @Deprecated
-    public static final String DEAULT_FLOW_RESOURCES_FOLDER = TARGET
+    public static final String DEFAULT_FLOW_RESOURCES_FOLDER = TARGET
             + "flow-frontend";
 
     /**
      * Default folder for copying front-end resources present in the classpath
      * jars.
+     * @deprecated This is deprecated due to a typo.
+     *             Use DEFAULT_FLOW_RESOURCES_FOLDER instead.
+     * @see #DEFAULT_FLOW_RESOURCES_FOLDER
      */
-    public static final String DEFAULT_FLOW_RESOURCES_FOLDER = TARGET
-            + "flow-frontend";
+    @Deprecated
+    public static final String DEAULT_FLOW_RESOURCES_FOLDER =
+            DEFAULT_FLOW_RESOURCES_FOLDER;
 
     /**
      * Default folder name for flow generated stuff relative to the
