8e2d8f59f356a708957260a8e7eff0991c2155b1
Denis Anisimov <denis@vaadin.com>
2020-09-07 12:51:53 +0300

    Add IT test for template attribute reading

---
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/template/TemplateAttributeIT.java
@@ -0,0 +1,19 @@
+package com.vaadin.flow.uitest.ui.template;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.testbench.TestBenchElement;
+
+public class TemplateAttributeIT extends ChromeBrowserTest {
+
+    @Test
+    public void readTemplateAttribiute() {
+        open();
+
+        TestBenchElement template = $(TestBenchElement.class).id("template");
+        TestBenchElement info = template.$(TestBenchElement.class).id("info");
+        Assert.assertEquals("foo", info.getText());
+    }
+}
