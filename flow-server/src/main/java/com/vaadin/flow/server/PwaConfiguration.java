b87a830c37f854b75e7077ef7b98b609b489d20d
Anton Platonov <platosha@gmail.com>
2020-09-08 15:04:40 +0300

    Remove unused import

--- flow-server/src/main/java/com/vaadin/flow/server/PwaConfiguration.java
+++ flow-server/src/main/java/com/vaadin/flow/server/PwaConfiguration.java
@@ -20,8 +20,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import javax.servlet.ServletContext;
-
 /**
  * Holds the configuration from the {@link PWA} annotation.
  *
