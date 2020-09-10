a177b6462afb2fbf3b65ff61124ba087f5fec166
Johannes Eriksson <joheriks@vaadin.com>
2020-09-10 19:47:51 +0300

    Show webpack error on live reload of frontend resources (#8990)
    Identify recompile using webpack console output instead of using a separate comm channel.
    This ensures that webpack errors in `DevModeHandler` are synced when the live reload occurs.

--- flow-tests/test-live-reload/src/main/java/com/vaadin/flow/uitest/ui/LiveReloadView.java
+++ flow-tests/test-live-reload/src/main/java/com/vaadin/flow/uitest/ui/LiveReloadView.java
@@ -16,12 +16,13 @@
 
 package com.vaadin.flow.uitest.ui;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
 
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -38,31 +39,40 @@ import com.vaadin.flow.uitest.servlet.ViewTestLayout;
 @Route(value = "com.vaadin.flow.uitest.ui.LiveReloadView", layout = ViewTestLayout.class)
 @CssImport("./styles.css")
 public class LiveReloadView extends Div {
-
     public static final String INSTANCE_IDENTIFIER = "instance-identifier";
     public static final String PAGE_RELOADING = "page-reloading";
 
     public static final String JAVA_LIVE_RELOAD_TRIGGER_BUTTON = "java-live-reload-trigger-button";
+
     public static final String WEBPACK_LIVE_RELOAD_TRIGGER_BUTTON = "webpack-live-reload-trigger-button";
+    public static final String WEBPACK_LIVE_RELOAD_BREAK_BUTTON = "webpack-live-reload-break-button";
 
     private static final Random random = new Random();
     Integer instanceIdentifier = random.nextInt();
-
 
     public LiveReloadView() {
         Span label = new Span(Integer.toString(instanceIdentifier));
         label.setId(INSTANCE_IDENTIFIER);
         add(label);
 
-        NativeButton javaReloadButton = new NativeButton("Trigger Java live reload");
+        NativeButton javaReloadButton = new NativeButton(
+                "Trigger Java live reload");
         javaReloadButton.addClickListener(this::handleClickJavaLiveReload);
         javaReloadButton.setId(JAVA_LIVE_RELOAD_TRIGGER_BUTTON);
         add(javaReloadButton);
 
-        NativeButton webpackReloadButton = new NativeButton("Trigger Webpack live reload");
-        webpackReloadButton.addClickListener(this::handleClickWebpackLiveReload);
+        NativeButton webpackReloadButton = new NativeButton(
+                "Trigger Webpack live reload");
+        webpackReloadButton
+                .addClickListener(this::handleClickWebpackLiveReload);
         webpackReloadButton.setId(WEBPACK_LIVE_RELOAD_TRIGGER_BUTTON);
         add(webpackReloadButton);
+
+        NativeButton webpackBreakButton = new NativeButton(
+                "Insert an error in a frontend file");
+        webpackBreakButton.addClickListener(this::handleClickBreakWebpack);
+        webpackBreakButton.setId(WEBPACK_LIVE_RELOAD_BREAK_BUTTON);
+        add(webpackBreakButton);
     }
 
     // Java triggered live reload is faked as we do not have Trava JDK in test
@@ -75,33 +85,35 @@ public class LiveReloadView extends Div {
         browserLiveReload.reload();
     }
 
+    private void handleClickBreakWebpack(
+            ClickEvent<NativeButton> nativeButtonClickEvent) {
+        writeToFrontendCSSFile(VaadinService.getCurrent(), "{");
+    }
+
     // Touch a frontend file to trigger webpack reload
     private void handleClickWebpackLiveReload(ClickEvent event) {
         addPageReloadingSpan();
-        final String projectFrontendDir = FrontendUtils.getProjectFrontendDir(
-                VaadinService.getCurrent().getDeploymentConfiguration());
-        File styleFile = new File(projectFrontendDir,"styles.css");
-        try {
-            BufferedWriter out = null;
-            try {
-                FileWriter fstream = new FileWriter(styleFile, true);
-                out = new BufferedWriter(fstream);
-                out.write(String.format("\nbody { background-color: rgb(%d,%d,%d); }",
-                        random.nextInt(256), random.nextInt(256), random.nextInt(256)));
-            } finally {
-                if (out != null) {
-                    out.close();
-                }
-            }
-        }
-        catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+        String css = String.format(
+                "\nbody { background-color: rgb(%d,%d,%d); }",
+                random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        writeToFrontendCSSFile(VaadinService.getCurrent(), css);
     }
 
     private void addPageReloadingSpan() {
         Span reloading = new Span("Reload triggered...");
         reloading.setId(PAGE_RELOADING);
         add(reloading);
+    }
+
+    public static void writeToFrontendCSSFile(VaadinService vaadinService,
+                                              String text) {
+        final String projectFrontendDir = FrontendUtils.getProjectFrontendDir(
+                vaadinService.getDeploymentConfiguration());
+        File styleFile = new File(projectFrontendDir, "styles.css");
+        try {
+            FileUtils.write(styleFile, text, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
