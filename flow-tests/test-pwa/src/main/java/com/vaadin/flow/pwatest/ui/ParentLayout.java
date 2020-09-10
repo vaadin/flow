2ad0eb44aacc99660b71e992430f4624b1ff203a
Anton Platonov <platosha@gmail.com>
2020-09-10 22:53:15 +0300

    fix PwaTestIT fixture app as PWA annotation must be on AppShell class

--- flow-tests/test-pwa/src/main/java/com/vaadin/flow/pwatest/ui/ParentLayout.java
+++ flow-tests/test-pwa/src/main/java/com/vaadin/flow/pwatest/ui/ParentLayout.java
@@ -16,6 +16,7 @@
 package com.vaadin.flow.pwatest.ui;
 
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.PWA;
 
@@ -23,7 +24,8 @@ import com.vaadin.flow.server.PWA;
         shortName = ParentLayout.PWA_SHORT_NAME,
         themeColor = ParentLayout.THEME_COLOR,
         backgroundColor = ParentLayout.BG_COLOR)
-public class ParentLayout extends Div implements RouterLayout {
+public class ParentLayout extends Div
+        implements RouterLayout, AppShellConfigurator {
     static final String THEME_COLOR = "#1f1f1f";
     static final String BG_COLOR = "#ffffff";
     static final String PWA_NAME = "PWA test name";
