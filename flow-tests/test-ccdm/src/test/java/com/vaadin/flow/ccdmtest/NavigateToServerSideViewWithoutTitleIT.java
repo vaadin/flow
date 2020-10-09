bb559847c8e355b7ca44dd3eeb7a589d195f2ad0
haijian <haijian@vaadin.com>
2020-10-09 14:42:36 +0300

    fix typo

--- flow-tests/test-ccdm/src/test/java/com/vaadin/flow/ccdmtest/NavigateToServerSideViewWithoutTitleIT.java
+++ flow-tests/test-ccdm/src/test/java/com/vaadin/flow/ccdmtest/NavigateToServerSideViewWithoutTitleIT.java
@@ -39,7 +39,7 @@ public class NavigateToServerSideViewWithoutTitleIT extends CCDMTest {
 
         // update document.title on the client-side
         findElement(By.id("updatePageTitle")).click();
-        Assert.assertEquals("Client-side should have updated the shell title", 
+        Assert.assertEquals("Client-side should have updated the app shell title", 
             "client-side-updated-title", getDriver().getTitle());
         
         findAnchor("view-with-server-view-button").click();
