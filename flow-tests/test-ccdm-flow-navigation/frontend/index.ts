083c74794aa0268014fbfe946703480135b1ebd9
Kari Söderholm <haprog@vaadin.com>
2020-10-29 10:04:05 +0200

    test: add test for offline start aka navigation fallback
    Closes #9215

--- flow-tests/test-ccdm-flow-navigation/frontend/index.ts
+++ flow-tests/test-ccdm-flow-navigation/frontend/index.ts
@@ -8,21 +8,34 @@ const {serverSideRoutes} = new Flow({
 const routes = [
     {
         path: '',
-        component: 'about-view',
-        action: async () => {
-            await import ('./views/about/about-view');
-        }
-    },
-
-    {
-        path: 'about',
-        component: 'about-view',
-        action: async () => {
-            await import ('./views/about/about-view');
-        }
-    },
-    // for server-side, the next magic line sends all unmatched routes:
-    ...serverSideRoutes // IMPORTANT: this must be the last entry in the array
+        component: 'main-view',
+        action: async () => { await import ('./views/main/main-view'); },
+        children: [
+            {
+                path: '',
+                component: 'about-view',
+                action: async () => {
+                    await import ('./views/about/about-view');
+                }
+            },
+            {
+                path: 'about',
+                component: 'about-view',
+                action: async () => {
+                    await import ('./views/about/about-view');
+                }
+            },
+            {
+                path: 'another',
+                component: 'another-view',
+                action: async () => {
+                    await import ('./views/another/another-view');
+                }
+            },
+            // for server-side, the next magic line sends all unmatched routes:
+            ...serverSideRoutes // IMPORTANT: this must be the last entry in the array
+        ]
+    }
 ];
 
 export const router = new Router(document.querySelector('#outlet'));
