31577503f03923a5a043e1aba4eed9863088b8b8
Stefan Bischof <stbischof@bipolis.org>
2020-11-09 23:39:03 +0100

    OSGiResourceProvider as a Service

--- flow-server/src/main/java/com/vaadin/flow/server/osgi/OSGiResourceProvider.java
+++ flow-server/src/main/java/com/vaadin/flow/server/osgi/OSGiResourceProvider.java
@@ -23,86 +23,87 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Objects;
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.FrameworkUtil;
-
 import com.vaadin.flow.di.ResourceProvider;
 import com.vaadin.flow.server.VaadinServletService;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * OSGi capable implementation of {@link ResourceProvider}.
- * 
+ *
  * @author Vaadin Ltd
  * @since
  *
  */
+@Component
 public class OSGiResourceProvider implements ResourceProvider {
 
-    @Override
-    public URL getApplicationResource(Class<?> clazz, String path) {
-        return FrameworkUtil.getBundle(Objects.requireNonNull(clazz))
-                .getResource(path);
-    }
+	@Override
+	public URL getApplicationResource(Class<?> clazz, String path) {
+		return FrameworkUtil.getBundle(Objects.requireNonNull(clazz))
+				.getResource(path);
+	}
 
-    @Override
-    public URL getApplicationResource(Object context, String path) {
-        Class<?> appClass = getApplicationClass(context);
-        if (appClass == null) {
-            return null;
-        }
-        return getApplicationResource(appClass, path);
-    }
+	@Override
+	public URL getApplicationResource(Object context, String path) {
+		Class<?> appClass = getApplicationClass(context);
+		if (appClass == null) {
+			return null;
+		}
+		return getApplicationResource(appClass, path);
+	}
 
-    @Override
-    public List<URL> getApplicationResources(Object context, String path)
-            throws IOException {
-        Class<?> appClass = getApplicationClass(context);
-        if (appClass == null) {
-            return Collections.emptyList();
-        }
-        return getApplicationResources(appClass, path);
+	@Override
+	public List<URL> getApplicationResources(Object context, String path)
+			throws IOException {
+		Class<?> appClass = getApplicationClass(context);
+		if (appClass == null) {
+			return Collections.emptyList();
+		}
+		return getApplicationResources(appClass, path);
 
-    }
+	}
 
-    @Override
-    public List<URL> getApplicationResources(Class<?> clazz, String path)
-            throws IOException {
-        Bundle bundle = FrameworkUtil.getBundle(Objects.requireNonNull(clazz));
-        Enumeration<URL> resources = bundle.getResources(path);
-        if (resources == null) {
-            return Collections.emptyList();
-        }
-        return Collections.list(resources);
-    }
+	@Override
+	public List<URL> getApplicationResources(Class<?> clazz, String path)
+			throws IOException {
+		Bundle bundle = FrameworkUtil.getBundle(Objects.requireNonNull(clazz));
+		Enumeration<URL> resources = bundle.getResources(path);
+		if (resources == null) {
+			return Collections.emptyList();
+		}
+		return Collections.list(resources);
+	}
 
-    @Override
-    public URL getClientResource(String path) {
-        Bundle[] bundles = FrameworkUtil.getBundle(OSGiResourceProvider.class)
-                .getBundleContext().getBundles();
-        for (Bundle bundle : bundles) {
-            if ("com.vaadin.flow.client".equals(bundle.getSymbolicName())) {
-                return bundle.getResource(path);
-            }
-        }
-        return null;
-    }
+	@Override
+	public URL getClientResource(String path) {
+		Bundle[] bundles = FrameworkUtil.getBundle(OSGiResourceProvider.class)
+				.getBundleContext().getBundles();
+		for (Bundle bundle : bundles) {
+			if ("com.vaadin.flow.client".equals(bundle.getSymbolicName())) {
+				return bundle.getResource(path);
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public InputStream getClientResourceAsStream(String path)
-            throws IOException {
-        // No any caching !: flow-client may be reinstalled at any moment
-        return getClientResource(path).openStream();
-    }
+	@Override
+	public InputStream getClientResourceAsStream(String path)
+			throws IOException {
+		// No any caching !: flow-client may be reinstalled at any moment
+		return getClientResource(path).openStream();
+	}
 
-    private Class<?> getApplicationClass(Object context) {
-        Class<?> clazz = Objects.requireNonNull(context).getClass();
-        if (context instanceof VaadinServletService) {
-            clazz = ((VaadinServletService) context).getServlet().getClass();
-        }
-        if (!"com.vaadin.flow.server"
-                .equals(FrameworkUtil.getBundle(clazz).getSymbolicName())) {
-            return clazz;
-        }
-        return null;
-    }
+	private Class<?> getApplicationClass(Object context) {
+		Class<?> clazz = Objects.requireNonNull(context).getClass();
+		if (context instanceof VaadinServletService) {
+			clazz = ((VaadinServletService) context).getServlet().getClass();
+		}
+		if (!"com.vaadin.flow.server"
+				.equals(FrameworkUtil.getBundle(clazz).getSymbolicName())) {
+			return clazz;
+		}
+		return null;
+	}
 }
