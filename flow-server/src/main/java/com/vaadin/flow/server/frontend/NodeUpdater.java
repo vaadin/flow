c4d8b399b47de3babd69152b1a2f12595e418fae
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2020-11-11 20:45:51 +0200

    feat: tree-shakeable ES imports for TS form validators (#9374)
    - Use tree-shakeable ES imports for TS form validators based on the latest version of validators lib.
    - Add missing `return` in the `submit()` method
    
    (cherry picked from commit 6608604b94af4f11013463dfdcf9521681ddb4e0)

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -306,8 +306,8 @@ public abstract class NodeUpdater implements FallibleCommand {
         defaults.put("raw-loader", "4.0.0");
         defaults.put("lit-element", "2.3.1");
         defaults.put("lit-html", "1.2.1");
-        defaults.put("@types/validator", "10.11.3");
-        defaults.put("validator", "12.0.0");
+        defaults.put("@types/validator", "13.1.0");
+        defaults.put("validator", "13.1.17");
 
         // Forcing chokidar version for now until new babel version is available
         // check out https://github.com/babel/babel/issues/11488
