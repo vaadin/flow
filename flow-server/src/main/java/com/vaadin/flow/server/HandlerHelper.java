354efc12bde65266a69b6309534b6ed6257dc762
Mikhail Shabarov <mikhail@vaadin.com>
2020-11-18 20:35:50 +0200

    chore: update header

--- flow-server/src/main/java/com/vaadin/flow/server/HandlerHelper.java
+++ flow-server/src/main/java/com/vaadin/flow/server/HandlerHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2018 Vaadin Ltd.
+ * Copyright 2000-2020 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package com.vaadin.flow.server;
 
 import java.io.Serializable;
