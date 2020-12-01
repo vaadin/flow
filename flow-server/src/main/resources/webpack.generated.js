490533abc27b0867e66e930da09ba9bcbda09bc9
tanbui <trungtanbui@gmail.com>
2020-12-01 10:09:41 +0200

    Allow webpack css loader to import from node_modules

--- flow-server/src/main/resources/webpack.generated.js
+++ flow-server/src/main/resources/webpack.generated.js
@@ -170,6 +170,12 @@ module.exports = {
         use: [
           {
             loader: 'css-loader',
+            import: (url, media, resourcePath) => {
+              // resourcePath - path to css file
+              // If the import happens from within a node_modules file, we must resolve and inline.
+              // Otherwise resources will not be found at runtime as node_modules is not deployed
+              return resourcePath.includes('/node_modules/');
+            },
             options: {
               url: (url, resourcePath) => {
                 // Only translate files from node_modules
