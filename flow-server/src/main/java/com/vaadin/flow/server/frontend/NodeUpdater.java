2c4f25c0f9a0d8ad57bb058d683adbfaad1803ed
tanbui <trungtanbui@gmail.com>
2020-12-01 12:08:28 +0200

    Add file-loader to NodeUpdater

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -303,6 +303,7 @@ public abstract class NodeUpdater implements FallibleCommand {
         defaults.put("compression-webpack-plugin", "4.0.1");
         defaults.put("webpack-merge", "4.2.2");
         defaults.put("css-loader", "4.2.1");
+        defaults.put("file-loader", "6.1.0");
         defaults.put("extract-loader", "5.1.0");
         defaults.put("lit-css-loader", "0.0.4");
         defaults.put("loader-utils", "1.4.0");
