a635dc68edca91cb74af6dec4f4150e756fbf579
caalador <mikael.grankvist@gmail.com>
2020-12-04 09:07:48 +0200

    feat: Resource handling to not need plugin copying (#9558)
    Fixed the url handling so that theme resources
    get prepended with theme/[themeName] while
    having the correct absolute path. With this we can handle
    these url resources with the css-loader which in turn
    leads to file-loader gettign the files for copying.
    
    External url are still not touched in any way.
    
    part of #9410 and #9533

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -305,6 +305,7 @@ public abstract class NodeUpdater implements FallibleCommand {
         defaults.put("css-loader", "4.2.1");
         defaults.put("extract-loader", "5.1.0");
         defaults.put("lit-css-loader", "0.0.4");
+        defaults.put("file-loader", "6.1.0");
         defaults.put("loader-utils", "1.4.0");
         defaults.put("lit-element", "2.3.1");
         defaults.put("lit-html", "1.2.1");
--- flow-server/src/main/resources/plugins/theme-loader/theme-loader.js
+++ flow-server/src/main/resources/plugins/theme-loader/theme-loader.js
@@ -2,7 +2,7 @@ const loaderUtils = require("loader-utils");
 const fs = require('fs');
 const path = require('path');
 
-// Collect groups [url(] [ |'|"]optional './', file part and end of url
+// Collect groups [url(] [ |'|"]optional './|../', file part and end of url
 const urlMatcher = /(url\()(\'|\")?(\.\/|\.\.\/)(\S*)(\2\))/g;
 
 /**
@@ -21,11 +21,11 @@ module.exports = function (source, map) {
   let themeFolder = handledResourceFolder;
   // Recurse up until we find the theme folder or don't have 'theme' on the path.
   while (themeFolder.indexOf("theme") > 1
-      && path.basename(path.resolve(themeFolder, "..")) !== "theme") {
+  && path.basename(path.resolve(themeFolder, "..")) !== "theme") {
     themeFolder = path.resolve(themeFolder, "..");
   }
   // If we have found no theme folder return without doing anything.
-  if(path.basename(path.resolve(themeFolder, "..")) !== "theme") {
+  if (path.basename(path.resolve(themeFolder, "..")) !== "theme") {
     this.callback(null, source, map);
     return;
   }
@@ -35,14 +35,15 @@ module.exports = function (source, map) {
   source = source.replace(urlMatcher, function (match, url, quoteMark, replace, fileUrl, endString) {
     let absolutePath = path.resolve(handledResourceFolder, replace, fileUrl);
     if (fs.existsSync(absolutePath) && absolutePath.startsWith(themeFolder)) {
-      logger.debug("Updating url for file '", replace, fileUrl, "' to use 'VAADIN/static'");
+      const frontendThemeFolder = "theme/" + path.basename(themeFolder);
+      logger.debug("Updating url for file", "'" + replace + fileUrl + "'", "to use", "'" + frontendThemeFolder + "/" + fileUrl + "'");
       const pathResolved = absolutePath.substring(themeFolder.length).replace(/\\/g, '/');
 
-      // keep the url the same except replace the ./ to VAADIN/static
+      // keep the url the same except replace the ./ or ../ to theme/[themeFolder]
       if (quoteMark) {
-        return url + quoteMark + 'VAADIN/static' + pathResolved + endString;
+        return url + quoteMark + frontendThemeFolder + pathResolved + endString;
       }
-      return url + 'VAADIN/static' + pathResolved + endString;
+      return url + frontendThemeFolder + pathResolved + endString;
     } else if (options.devMode) {
       logger.info("No rewrite for '", match, "' as the file was not found.");
     }
