71a5d5f34a2441380ad6c7974c1225409628d6db
Johannes Eriksson <joheriks@vaadin.com>
2020-12-14 14:02:43 +0200

    test: misc-IT for gz compression

---
+++ flow-tests/test-misc/src/test/java/com/vaadin/flow/misc/ui/CompressedResourceIT.java
@@ -0,0 +1,32 @@
+package com.vaadin.flow.misc.ui;
+
+import com.vaadin.flow.testutil.AbstractParallelTestBenchTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
+
+public class CompressedResourceIT extends AbstractParallelTestBenchTest {
+
+    @Test
+    public void compressedResourcesAreServedCompressed() throws IOException {
+        URL textFileUrl = new URL(getRootURL() + "/textfile.txt");
+        HttpURLConnection connection = (HttpURLConnection) textFileUrl.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept-Encoding", "gzip");
+
+        int status = connection.getResponseCode();
+        Assert.assertEquals(200, status);
+        Assert.assertEquals("gzip", connection.getHeaderField("Content-Encoding"));
+
+        byte[] bytes = new byte[1024];
+        int count = new GZIPInputStream(connection.getInputStream())
+                .read(bytes, 0, bytes.length);
+        String content = new String(bytes, 0, count, StandardCharsets.UTF_8);
+        Assert.assertEquals("Text file contents.\n", content);
+    }
+}
