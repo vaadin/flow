4eac0f69aed9407b8eb53bc233c640c73feed6e6
tanbui <trungtanbui@gmail.com>
2020-12-16 19:41:58 +0200

    Move reusable-theme to be packaged in test-resources module

---
+++ flow-tests/test-resources/src/main/java/com.vaadin.apptheme.reusabletheme/Dependency.java
@@ -0,0 +1,8 @@
+package org.vaadin.apptheme.reusabletheme;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+@NpmPackage(value = "@fortawesome/fontawesome-free", version = "5.15.1")
+public class Dependency {
+
+}
