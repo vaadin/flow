2feb0a3cdffa6037d257a8d763d5ec2a512c82b1
taefi <taefi.soroosh@gmail.com>
2020-12-17 00:41:25 +0200

    organize imports

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateThemeImport.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateThemeImport.java
@@ -18,7 +18,6 @@ package com.vaadin.flow.server.frontend;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.LoggerFactory;
