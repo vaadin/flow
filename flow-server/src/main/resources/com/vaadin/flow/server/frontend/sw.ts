e611bfa4696908b19532337ec07fe3f66221d6be
Anton Platonov <anton@vaadin.com>
2020-12-17 04:33:06 +0200

    fix(offline): Rewrite <base href> in service worker using sw scope (#9652)
    * fix(offline): Rewrite <base href> in service worker using sw scope
    
    Fixes #8878
    
    * test: use non-root contextPath in test-ccdm-flow-navigation IT
    
    Co-authored-by: Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/sw.ts
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/sw.ts
@@ -1,45 +1,72 @@
 /// <reference lib="webworker" />
 
 importScripts('sw-runtime-resources-precache.js');
-import {skipWaiting, clientsClaim} from 'workbox-core';
+import {
+  skipWaiting,
+  clientsClaim,
+  RouteHandlerCallbackOptions
+} from 'workbox-core';
 import {matchPrecache, precacheAndRoute} from 'workbox-precaching';
 import {NavigationRoute, registerRoute} from 'workbox-routing';
 import {PrecacheEntry} from 'workbox-precaching/_types';
 import {NetworkOnly} from 'workbox-strategies';
 
+declare var self: ServiceWorkerGlobalScope & {
+  __WB_MANIFEST: Array<PrecacheEntry>,
+  additionalManifestEntries?: Array<PrecacheEntry>
+};
+
 skipWaiting();
 clientsClaim();
 
-const appShellPath = '/';
-// @ts-ignore: OFFLINE_PATH_ENABLED and OFFLINE_PATH are defined by webpack.generated.js
+declare var OFFLINE_PATH_ENABLED: boolean; // defined by webpack.generated.js
+declare var OFFLINE_PATH: string; // defined by webpack.generated.js
+
+/**
+ * Replaces <base href> in pre-cached response HTML with the service worker’s
+ * scope URL.
+ *
+ * @param response HTML response to modify
+ * @returns modified response
+ */
+const rewriteBaseHref = async (response: Response) => {
+  const html = await response.text();
+  return new Response(
+    html.replace(
+      /<base\s+href=[^>]*>/,
+      `<base href="${self.registration.scope}">`
+    ),
+    response
+  );
+};
+
+const appShellPath = '.';
 const offlinePath = OFFLINE_PATH_ENABLED ? '/' + OFFLINE_PATH : appShellPath;
 const networkOnly = new NetworkOnly();
-const navigationFallback = new NavigationRoute(async (params: any) => {
+const navigationFallback = new NavigationRoute(async (context: RouteHandlerCallbackOptions) => {
   // Use offlinePath fallback if offline was detected
   if (!self.navigator.onLine) {
     const offlinePathPrecachedResponse = await matchPrecache(offlinePath);
     if (offlinePathPrecachedResponse) {
-      return offlinePathPrecachedResponse;
+      return rewriteBaseHref(offlinePathPrecachedResponse);
     }
   }
 
   // Sometimes navigator.onLine is not reliable, use fallback to offlinePath
   // also in case of network failure
   try {
-    return await networkOnly.handle(params);
+    return await networkOnly.handle(context);
   } catch (error) {
-    return (await matchPrecache(offlinePath)) || error;
+    const precachedResponse = await matchPrecache(offlinePath);
+    return precachedResponse ? await rewriteBaseHref(precachedResponse) : error;
   }
 });
 
 registerRoute(navigationFallback);
 
-// @ts-ignore: __WB_MANIFEST is injected by the InjectManifest plugin
-let manifestEntries: Array<PrecacheEntry> = self.__WB_MANIFEST;
-// @ts-ignore: additionalManifestEntries is defined in sw-runtime-resources-precache.js
-const additionalManifestEntries: Array<PrecacheEntry> = self.additionalManifestEntries;
-if (additionalManifestEntries && additionalManifestEntries.length) {
-  manifestEntries = [...manifestEntries, ...additionalManifestEntries];
+let manifestEntries = self.__WB_MANIFEST;
+if (self.additionalManifestEntries && self.additionalManifestEntries.length) {
+  manifestEntries = [...manifestEntries, ...self.additionalManifestEntries];
 }
 
 precacheAndRoute(manifestEntries);
