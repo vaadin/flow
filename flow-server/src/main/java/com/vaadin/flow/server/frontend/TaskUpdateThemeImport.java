f38a4cc224ce61ae047d786deb64655ac42177bb
Soroosh Taefi <taefi.soroosh@gmail.com>
2020-12-22 16:28:29 +0200

    fix: verify theme folder existence based on app-theme name in @Theme (#9676)
    * verify theme folder existence based on app-theme name used in @Theme

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateThemeImport.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskUpdateThemeImport.java
@@ -15,17 +15,20 @@
  */
 package com.vaadin.flow.server.frontend;
 
+import static com.vaadin.flow.server.Constants.APPLICATION_THEME_ROOT;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.server.ExecutionFailedException;
 import com.vaadin.flow.theme.ThemeDefinition;
-
-import static com.vaadin.flow.server.Constants.APPLICATION_THEME_ROOT;
 
 /**
  * Task for generating the theme-generated.js file for importing application
@@ -34,18 +37,26 @@ import static com.vaadin.flow.server.Constants.APPLICATION_THEME_ROOT;
  * @since
  */
 public class TaskUpdateThemeImport implements FallibleCommand {
+    
+    public static final String APPLICATION_META_INF_RESOURCES = "src/main/resources/META-INF/resources";
+    public static final String APPLICATION_STATIC_RESOURCES = "src/main/resources/static";
 
-    private File themeImportFile;
-    private ThemeDefinition theme;
+    private final File themeImportFile;
+    private final ThemeDefinition theme;
+    private final File frontendDirectory;
+    private final File npmFolder;
 
-    TaskUpdateThemeImport(File npmFolder, ThemeDefinition theme) {
+    TaskUpdateThemeImport(File npmFolder, ThemeDefinition theme,
+            File frontendDirectory) {
         File nodeModules = new File(npmFolder, FrontendUtils.NODE_MODULES);
         File flowFrontend = new File(nodeModules,
-            FrontendUtils.FLOW_NPM_PACKAGE_NAME);
+                FrontendUtils.FLOW_NPM_PACKAGE_NAME);
         this.themeImportFile = new File(
                 new File(flowFrontend, APPLICATION_THEME_ROOT),
-            "theme-generated.js");
+                "theme-generated.js");
         this.theme = theme;
+        this.frontendDirectory = frontendDirectory;
+        this.npmFolder = npmFolder;
     }
 
     @Override
@@ -53,6 +64,9 @@ public class TaskUpdateThemeImport implements FallibleCommand {
         if (theme == null || theme.getName().isEmpty()) {
             return;
         }
+
+        verifyThemeDirectoryExistence();
+
         if (!themeImportFile.getParentFile().mkdirs()) {
             LoggerFactory.getLogger(getClass()).debug(
                 "Didn't create folders as they probably already exist. "
@@ -69,5 +83,78 @@ public class TaskUpdateThemeImport implements FallibleCommand {
             throw new ExecutionFailedException(
                 "Unable to write theme import file", e);
         }
+    }
+
+    private void verifyThemeDirectoryExistence()
+            throws ExecutionFailedException {
+
+        String themeName = theme.getName();
+        String themePath = String.join("/", APPLICATION_THEME_ROOT, themeName);
+
+        List<String> appThemePossiblePaths = getAppThemePossiblePaths(
+                themePath);
+        List<File> existingAppThemeDirectories = appThemePossiblePaths.stream()
+                .map(path -> new File(npmFolder, path))
+                .filter(File::exists)
+                .collect(Collectors.toList());
+
+        if (existingAppThemeDirectories.isEmpty()) {
+            String errorMessage = "Discovered @Theme annotation with theme "
+                    + "name '%s', but could not find the theme directory "
+                    + "in the project or available as a jar dependency. "
+                    + "Check if you forgot to create the folder under '%s' "
+                    + "or have mistyped the theme or folder name for '%s'.";
+            throw new ExecutionFailedException(
+                    String.format(errorMessage, themeName,
+                            new File(frontendDirectory, APPLICATION_THEME_ROOT)
+                                    .getPath(), themeName));
+        }
+        if (existingAppThemeDirectories.size() >= 2) {
+
+            boolean themeFoundInJar = existingAppThemeDirectories
+                    .stream().map(File::getPath)
+                    .anyMatch(path -> path
+                            .contains(FrontendUtils.FLOW_NPM_PACKAGE_NAME));
+
+            if (themeFoundInJar) {
+                String errorMessage = "Theme '%s' should not exist inside a "
+                        + "jar and in the project at the same time.%n"
+                        + "Extending another theme is possible by adding "
+                        + "{ \"parent\": \"your-parent-theme\" } entry to the "
+                        + "'theme.json' file inside your theme folder.";
+                throw new ExecutionFailedException(
+                        String.format(errorMessage, themeName));
+            } else {
+                String errorMessage = "Discovered Theme folder for theme '%s' "
+                        + "in more than one place in the project. Please "
+                        + "make sure there is only one theme folder with name "
+                        + "'%s' exists in the your project. "
+                        + "The recommended place to put the theme folder "
+                        + "inside the project is '%s'";
+                throw new ExecutionFailedException(String.format(errorMessage,
+                        themeName, themeName,
+                        new File(frontendDirectory, APPLICATION_THEME_ROOT)
+                                .getPath()));
+            }
+        }
+    }
+
+    private List<String> getAppThemePossiblePaths(String themePath) {
+        String frontendTheme = String.join("/",
+                npmFolder.toPath()
+                .relativize(frontendDirectory.toPath()).toString(), themePath);
+
+        String themePathInMetaInfResources = String.join("/",
+                APPLICATION_META_INF_RESOURCES, themePath);
+
+        String themePathInStaticResources = String.join("/",
+                APPLICATION_STATIC_RESOURCES, themePath);
+
+        String themePathInClassPathResources = String.join("",
+                FrontendUtils.NODE_MODULES, FrontendUtils.FLOW_NPM_PACKAGE_NAME,
+                themePath);
+
+        return Arrays.asList(frontendTheme, themePathInMetaInfResources,
+                themePathInStaticResources, themePathInClassPathResources);
     }
 }
