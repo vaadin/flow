09236ec34623817fcee9822b6bb15e06649e433b
tanbui <trungtanbui@gmail.com>
2021-01-08 16:58:57 +0200

    Replace TS import for WebComponentGenerator

--- flow-server/src/main/java/com/vaadin/flow/server/webcomponent/WebComponentGenerator.java
+++ flow-server/src/main/java/com/vaadin/flow/server/webcomponent/WebComponentGenerator.java
@@ -160,7 +160,7 @@ public class WebComponentGenerator {
 
         if (themeName != null && !themeName.isEmpty()) {
             replacements.put("ThemeImport",
-                "import {applyTheme} from 'themes/theme-generated.js';\n\n");
+                "import {applyTheme} from '@vaadin/flow-frontend/themes/theme-generated';\n\n");
             replacements.put("ApplyTheme", "applyTheme(shadow);\n    ");
         } else {
             replacements.put("ThemeImport", "");
