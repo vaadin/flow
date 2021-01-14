4f4ef2ca57851a5e77f6ff90ce958af1c48ba4b8
Artur <artur@vaadin.com>
2021-01-14 11:32:18 +0200

    chore: Use Chokidar 3.5 to avoid deprecated fsevents 2.1 (#9781)
    (cherry picked from commit 641ff63cac4863cf2ea7ad1846c794e4b9123e2d)

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -262,7 +262,7 @@ public abstract class NodeUpdater implements FallibleCommand {
 
         // Forcing chokidar version for now until new babel version is available
         // check out https://github.com/babel/babel/issues/11488
-        defaults.put("chokidar", "^3.4.0");
+        defaults.put("chokidar", "^3.5.0");
 
         return defaults;
     }
