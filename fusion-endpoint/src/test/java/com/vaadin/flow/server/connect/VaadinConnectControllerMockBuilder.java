eebed4679868c58e07c34691ea512c8789b5dd7f
haijian <haijian@vaadin.com>
2021-01-21 15:47:39 +0200

    fix more tests

---
+++ fusion-endpoint/src/test/java/com/vaadin/flow/server/connect/VaadinConnectControllerMockBuilder.java
@@ -0,0 +1,28 @@
+package com.vaadin.flow.server.connect;
+
+import static org.mockito.Mockito.mock;
+
+import com.vaadin.flow.server.connect.auth.VaadinConnectAccessChecker;
+
+import org.mockito.Mockito;
+import org.springframework.context.ApplicationContext;
+
+public class VaadinConnectControllerMockBuilder {
+    private ApplicationContext applicationContext;
+
+    public VaadinConnectControllerMockBuilder withApplicationContext(ApplicationContext applicationContext){
+       this.applicationContext = applicationContext;
+       return this;
+    }
+
+    public VaadinConnectController build(){
+        VaadinConnectController controller = Mockito.spy(
+            new VaadinConnectController(null,
+                      mock(EndpointNameChecker.class),
+                      mock(ExplicitNullableTypeChecker.class),
+                      applicationContext)
+        );
+        Mockito.doReturn(mock(VaadinConnectAccessChecker.class)).when(controller).getAccessChecker(Mockito.any(), Mockito.any());
+        return controller;
+    }
+}
