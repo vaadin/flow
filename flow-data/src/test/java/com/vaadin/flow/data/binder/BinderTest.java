f3beb671a0aa4256433786ebb30bc7a54bfdd68b
miguel <miguel@vaadin.com>
2021-02-01 12:06:54 +0100

    fix: make BinderTest#withConverter_writeBackValue locale aware
    BinderTest#withConverter_writeBackValue was failing depending on the locale. Now it's locale aware and does not fail when run on any (e.g. spanish) locale

--- flow-data/src/test/java/com/vaadin/flow/data/binder/BinderTest.java
+++ flow-data/src/test/java/com/vaadin/flow/data/binder/BinderTest.java
@@ -580,7 +580,9 @@ public class BinderTest extends BinderTestBase<Binder<Person>, Person> {
         binder.setBean(item);
         rentField.setValue("10");
 
-        assertEquals("€ 10.00", rentField.getValue());
+        //€ 10.00
+        assertEquals("€ " + new DecimalFormat("0.00").format(10)
+                , rentField.getValue());
     }    
 
     @Test
