6252806937ded47173797d4e3c7627bd6e67bf84
Artur Signell <artur@vaadin.com>
2021-02-01 17:30:26 +0200

    Upgrade lit-html + LitElement

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -307,8 +307,8 @@ public abstract class NodeUpdater implements FallibleCommand {
         defaults.put("lit-css-loader", "0.0.4");
         defaults.put("file-loader", "6.1.0");
         defaults.put("loader-utils", "1.4.0");
-        defaults.put("lit-element", "2.3.1");
-        defaults.put("lit-html", "1.2.1");
+        defaults.put("lit-element", "2.4.0");
+        defaults.put("lit-html", "1.3.0");
         final String WORKBOX_VERSION = "5.1.4";
         defaults.put("workbox-webpack-plugin", WORKBOX_VERSION);
         defaults.put("workbox-core", WORKBOX_VERSION);
