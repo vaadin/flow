c2c114559ba2a1089c6ab063888b032f8d2c6844
Artur <artur@vaadin.com>
2021-02-04 09:29:54 +0200

    fix: Upgrade loader deps for webpack build (#9935)
    Updates file-loader 6.1.0->6.2.0 and loader-utils 1.4.0->2.0.0

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -304,8 +304,8 @@ public abstract class NodeUpdater implements FallibleCommand {
         defaults.put("css-loader", "4.2.1");
         defaults.put("extract-loader", "5.1.0");
         defaults.put("lit-css-loader", "0.0.4");
-        defaults.put("file-loader", "6.1.0");
-        defaults.put("loader-utils", "1.4.0");
+        defaults.put("file-loader", "6.2.0");
+        defaults.put("loader-utils", "2.0.0");
         defaults.put("lit-element", "2.3.1");
         defaults.put("lit-html", "1.2.1");
         final String WORKBOX_VERSION = "5.1.4";
