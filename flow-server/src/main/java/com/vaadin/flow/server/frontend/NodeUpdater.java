f58bbcd32107f7f701305cec684cbd6eed9bbb10
Artur <artur@vaadin.com>
2021-02-14 17:36:12 +0200

    Update stylesheet polyfill
    Fixes #9869

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -318,7 +318,7 @@ public abstract class NodeUpdater implements FallibleCommand {
         defaults.put("validator", "13.1.17");
         // Constructable style sheets is only implemented for chrome,
         // polyfill needed for FireFox et.al. at the moment
-        defaults.put("construct-style-sheets-polyfill", "2.4.6");
+        defaults.put("construct-style-sheets-polyfill", "2.4.16");
 
         // Forcing chokidar version for now until new babel version is available
         // check out https://github.com/babel/babel/issues/11488
