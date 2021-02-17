70641dd61eb3295f5c66991111b4f4fa103207dc
Kari Söderholm <haprog@vaadin.com>
2021-02-17 11:48:16 +0200

    feat: global CSS loader (#10036)
    * feat: global CSS loader
    
    Closes #9970
    
    * docs: add deprecation message description

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/types.d.ts
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/types.d.ts
@@ -3,8 +3,27 @@
 // This is needed for TypeScript compiler to declare and export as a TypeScript module.
 // It is recommended to commit this file to the VCS.
 // You might want to change the configurations to fit your preferences
+declare module '*.shadow.css' {
+    import { CSSResult } from 'lit-element';
+    const content: CSSResult;
+    export default content;
+}
+
 declare module '*.css' {
-    import {CSSResult} from "lit-element";
+    import { CSSResult } from 'lit-element';
+    /**
+     * @deprecated For CSS files that should be imported as a `CssResult`
+     *   (for usage in a `static get styles()` method for example),
+     *   you should use a file name suffix of `*.shadow.css` to keep your import
+     *   forwards compatible.
+     *   
+     *   In a future version we will change the default CSS loader to inject the
+     *   CSS as a stylesheet into document scope instead of exporting it as
+     *   a `CssResult` and the old behaviour of exporting `CssResult` will be
+     *   available via a separate loader that only applies to `*.shadow.css` files.
+     *   
+     *   See https://github.com/vaadin/flow/issues/9970 for more info.
+     */
     const content: CSSResult;
     export default content;
 }
