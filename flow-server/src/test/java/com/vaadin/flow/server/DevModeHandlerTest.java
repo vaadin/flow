4244a32c5fbe4e75dee8007276dde19d12e697fc
Mikael Grankvist <mikael.grankvist@vaadin.com>
2021-02-19 15:06:29 +0200

    chore: Fix initalizing to devServers
    Don't start third devServer in middle.
    
    Fixes #10018

--- flow-server/src/test/java/com/vaadin/flow/server/DevModeHandlerTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/DevModeHandlerTest.java
@@ -591,14 +591,6 @@ public class DevModeHandlerTest {
         // remove the "singleton" instance to be able to start another one
         removeDevModeHandlerInstance();
 
-        // since the timeout is quite big the server port still should be
-        // available and the second instance should try to reuse it
-
-        DevModeHandler.start(0, createDevModeLookup(), npmFolder,
-                CompletableFuture.completedFuture(null));
-
-        // make checks only if webpack has not yet completed
-
         DevModeHandler anotherHandler = DevModeHandler.start(0,
                 createDevModeLookup(), npmFolder,
                 CompletableFuture.completedFuture(null));
