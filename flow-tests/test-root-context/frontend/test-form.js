e487d3e06aaa74241e7816f62a17a311a1bd80d6
Denis Anisimov <denis@vaadin.com>
2021-03-01 09:51:00 +0300

    chore: use Polymer template instead of LitTempalte in ITs.

---
+++ flow-tests/test-root-context/frontend/test-form.js
@@ -0,0 +1,15 @@
+import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+
+class TestForm extends PolymerElement {
+  static get is() {
+    return 'test-form'
+  }
+
+  static get template() {
+    return html`
+         <div id="div">Template text</div> 
+    `;
+  }
+}
+customElements.define(TestForm.is, TestForm);
