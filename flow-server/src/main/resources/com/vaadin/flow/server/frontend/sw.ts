5dc6c52530c8d147aac7d2cb05e3b4c345ec0acf
haijian <haijian@vaadin.com>
2021-03-04 09:04:49 +0200

    fix: offline path should be relative to current path not root

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/sw.ts
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/sw.ts
@@ -41,7 +41,7 @@ const rewriteBaseHref = async (response: Response) => {
 };
 
 const appShellPath = '.';
-const offlinePath = OFFLINE_PATH_ENABLED ? '/' + OFFLINE_PATH : appShellPath;
+const offlinePath = OFFLINE_PATH_ENABLED ? OFFLINE_PATH : appShellPath;
 const networkOnly = new NetworkOnly();
 let connectionLost = false;
 const navigationFallback = new NavigationRoute(async (context: RouteHandlerCallbackOptions) => {
