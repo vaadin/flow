3028f0b571c9fd6a5c1693652c8fa110272a2aee
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2021-03-18 15:19:22 +0200

    test: Disable LogoutIT flaky test (#10336)

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/LogoutIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/LogoutIT.java
@@ -16,6 +16,7 @@
 package com.vaadin.flow.uitest.ui;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
@@ -24,6 +25,7 @@ import com.vaadin.flow.testutil.ChromeBrowserTest;
 
 public class LogoutIT extends ChromeBrowserTest {
 
+    @Ignore("Flaky test, see https://github.com/vaadin/flow/issues/9705")
     @Test
     public void setLocation_noErrorMessages() {
         open();
