e76104e66372fb2cfc6b2612b85eb98343f241ac
Denis <denis@vaadin.com>
2021-03-22 18:04:17 +0300

    fix: don't run servlet deployer logic if there is no Lookup (#10358)

--- flow-server/src/test/java/com/vaadin/flow/server/startup/ServletDeployerTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/startup/ServletDeployerTest.java
@@ -23,6 +23,7 @@ import org.easymock.CaptureType;
 import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.WebComponentExporter;
@@ -292,6 +293,18 @@ public class ServletDeployerTest {
 
         assertMappingsCount(1, 1);
         assertMappingIsRegistered(ServletDeployer.class.getName(), "/*");
+    }
+
+    @Test
+    public void contextInitialized_noLookup_noAction() {
+        ServletContextEvent event = Mockito.mock(ServletContextEvent.class);
+        ServletContext context = Mockito.mock(ServletContext.class);
+        Mockito.when(event.getServletContext()).thenReturn(context);
+
+        deployer.contextInitialized(event);
+
+        Mockito.verify(context).getAttribute(Lookup.class.getName());
+        Mockito.verifyNoMoreInteractions(context);
     }
 
     private void assertMappingsCount(int numServlets, int numMappings) {
