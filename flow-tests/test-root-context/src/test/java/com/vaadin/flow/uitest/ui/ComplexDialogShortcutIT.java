e70d9d098eedf5bc9f1ce509e7d258f7aafb3f26
Pekka Hyvönen <pekka@vaadin.com>
2021-03-24 12:57:33 +0200

    chore: refactor flaky test (#10359)
    Refactors the code a bit to make it easier to understand why are dialog
    shortcut ITs flaky in the CI. Only for master branch now.
    
    Related to #10302, #10284

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/ComplexDialogShortcutIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/ComplexDialogShortcutIT.java
@@ -1,24 +1,25 @@
 package com.vaadin.flow.uitest.ui;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-@Ignore("Disabled due flaky behavior, see #10284")
+import static com.vaadin.flow.uitest.ui.DialogShortcutView.REUSABLE_DIALOG_ID;
+
 public class ComplexDialogShortcutIT extends DialogShortcutIT {
 
     @Test
     public void dialogWithShortcutListenOnAndPreserveOnRefresh_refreshWhenDialogOpened_shortcutScopingWorks() {
-        openNewDialog();
+        final int firstDialogIndex = openNewDialog();
 
-        listenToShortcutOnDialog(0);
-        pressShortcutKey(getFirstDialogInput());
+        listenToShortcutOnDialog(firstDialogIndex);
+        pressShortcutKey(getDialogInput(firstDialogIndex));
         // focus on dialog -> only dialog shortcut occurs
-        validateLatestShortcutEvent(0, DialogShortcutView.DIALOG_ID + 0);
+        validateLatestShortcutEvent(0,
+                DialogShortcutView.DIALOG_ID + firstDialogIndex);
 
         init(); // need to reset elements too
 
-        pressShortcutKey(getFirstDialogInput());
+        pressShortcutKey(getDialogInput(firstDialogIndex));
         // focus on dialog -> only dialog shortcut occurs
         validateLatestShortcutEvent(1, DialogShortcutView.DIALOG_ID + 0);
     }
@@ -26,13 +27,14 @@ public class ComplexDialogShortcutIT extends DialogShortcutIT {
     @Override
     protected void openReusedDialog() {
         super.openReusedDialog();
-        waitForTransport(0);
+        waitForTransport(REUSABLE_DIALOG_ID);
     }
 
     @Override
-    protected void openNewDialog() {
-        super.openNewDialog();
-        waitForTransport(dialogCounter);
+    protected int openNewDialog() {
+        final int dialogId = super.openNewDialog();
+        waitForTransport(dialogId);
+        return dialogId;
     }
 
     private void waitForTransport(int expectedDialogCounter) {
