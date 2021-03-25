5c309c31a40217b3e5dd24166fcb071c04b88434
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2021-03-25 09:27:00 +0200

    chore: Increase startup time threshold

--- flow-tests/test-npm-only-features/test-npm-performance-regression/src/test/java/com/vaadin/flow/testnpmonlyfeatures/performanceregression/StartupPerformanceIT.java
+++ flow-tests/test-npm-only-features/test-npm-performance-regression/src/test/java/com/vaadin/flow/testnpmonlyfeatures/performanceregression/StartupPerformanceIT.java
@@ -55,7 +55,7 @@ public class StartupPerformanceIT extends ChromeBrowserTest {
         final int thresholdMs = Boolean.getBoolean(
                 System.getProperty("vaadin.useDeprecatedV14Bootstrapping"))
                         ? 5500
-                        : 11500;
+                        : 12000;
 
 
         Assert.assertTrue(
