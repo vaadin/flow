5e1614487f3f696062172dd2f9f7d053bd3a5f1e
Artur Signell <artur@vaadin.com>
2021-03-29 10:30:34 +0300

    Remove unused import

--- flow-server/src/main/java/com/vaadin/flow/server/BootstrapUtils.java
+++ flow-server/src/main/java/com/vaadin/flow/server/BootstrapUtils.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
