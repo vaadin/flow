a07351cf2e98322c4da8540ce61d8f81291f172f
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2021-03-30 14:59:15 +0300

    chore: disable flaky theme component test (#10490)
    Disables the flaky test in ApplicationThemeComponentIT of test-embedding-reusable-theme module.
    
    Temporary solution for #10331

--- flow-tests/test-embedding/test-embedding-reusable-theme/src/test/java/com/vaadin/flow/webcomponent/ApplicationThemeComponentIT.java
+++ flow-tests/test-embedding/test-embedding-reusable-theme/src/test/java/com/vaadin/flow/webcomponent/ApplicationThemeComponentIT.java
@@ -16,6 +16,7 @@
 package com.vaadin.flow.webcomponent;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -96,6 +97,7 @@ public class ApplicationThemeComponentIT extends ChromeBrowserTest {
                 handElement.getCssValue("color"));
     }
 
+    @Ignore("Flaky test: #10331")
     @Test
     public void componentThemeIsApplied_forPolymerAndLit() {
         open();
