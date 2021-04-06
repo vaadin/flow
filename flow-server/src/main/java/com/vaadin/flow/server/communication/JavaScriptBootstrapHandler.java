cd722be61cbbd31c03b3d7310b28421a0fcd4e02
Artur Signell <artur@vaadin.com>
2021-04-06 19:44:51 +0300

    Remove code that should not be part of this PR

--- flow-server/src/main/java/com/vaadin/flow/server/communication/JavaScriptBootstrapHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/communication/JavaScriptBootstrapHandler.java
@@ -67,14 +67,9 @@ public class JavaScriptBootstrapHandler extends BootstrapHandler {
                 VaadinResponse response, UI ui,
                 Function<VaadinRequest, String> callback) {
             super(request, response, ui.getInternals().getSession(), ui,
-                    callback, JavaScriptBootstrapContext::initRoute);
+                    callback);
         }
 
-        private static Location initRoute(VaadinRequest request) {
-            String pathAndParams = request.getParameter(
-                    ApplicationConstants.REQUEST_LOCATION_PARAMETER);
-            return new Location(pathAndParams);
-        }
     }
 
     /**
