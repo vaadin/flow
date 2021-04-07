2cf92da21c97e6f9924692f93457fe662ca251db
Artur <artur@vaadin.com>
2021-04-07 18:01:04 +0300

    feat: provide a Spring Security base class vaadin/spring#748
    * First step to provide a Spring Security base class
    
    Aims to by default secure the whole application by default and allow overriding configuration to make some views or resources public
    
    * Fix test
    
    * Use base class in tests

--- flow-tests/vaadin-spring-tests/test-spring-security-helper/src/main/java/com/vaadin/flow/spring/test/SecurityConfig.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-helper/src/main/java/com/vaadin/flow/spring/test/SecurityConfig.java
@@ -1,21 +1,20 @@
 package com.vaadin.flow.spring.test;
 
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import com.vaadin.flow.spring.VaadinSpringSecurity;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        VaadinSpringSecurity.configure(http);
+        super.configure(http);
 
         // use the Spring provided login form
         http.csrf().ignoringAntMatchers("/");
@@ -24,11 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // allow non-interactive logout via /logout
         http.logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        VaadinSpringSecurity.configure(web);
     }
 
     @Override protected void configure(AuthenticationManagerBuilder auth)
