4d8bd8908d5f9c2bb58a05cc441db36b1067d41d
Artur <artur@vaadin.com>
2021-04-08 17:44:23 +0300

    feat: provide a default request cache which ignores non-route requests vaadin/spring#744
    Fixes https://github.com/vaadin/flow/issues/10344

--- flow-tests/vaadin-spring-tests/test-spring-security-helper/src/main/java/com/vaadin/flow/spring/test/SecurityConfig.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-helper/src/main/java/com/vaadin/flow/spring/test/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.vaadin.flow.spring.test;
 
+import com.vaadin.flow.spring.security.VaadinDefaultRequestCache;
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
 
+    @Autowired
+    private VaadinDefaultRequestCache vaadinDefaultRequestCache;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
@@ -19,13 +24,15 @@ public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
         // use the Spring provided login form
         http.csrf().ignoringAntMatchers("/");
         http.formLogin();
+        http.requestCache().requestCache(vaadinDefaultRequestCache);
 
         // allow non-interactive logout via /logout
         http.logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
     }
 
-    @Override protected void configure(AuthenticationManagerBuilder auth)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.inMemoryAuthentication().withUser("user").password("{noop}user")
                 .roles("USER");
