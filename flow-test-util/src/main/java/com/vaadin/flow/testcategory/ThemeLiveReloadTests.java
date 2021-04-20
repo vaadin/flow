00e9f3196c1ba2d015000796a5d3fcb88c18b387
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2021-04-20 13:16:13 +0300

    chore: Move theme live reload tests to it's own profile (#10718)

---
+++ flow-test-util/src/main/java/com/vaadin/flow/testcategory/ThemeLiveReloadTests.java
@@ -0,0 +1,9 @@
+package com.vaadin.flow.testcategory;
+
+/**
+ * Theme Live Reload tests should be annotated with
+ * {@code Category(ThemeLiveReloadTests.class} so they can be excluded from
+ * the build and added to a separate profile.
+ */
+public interface ThemeLiveReloadTests extends TestCategory {
+}
