e396475c9e4a0ad8d75e58555292bcd55f58808f
Viktor Lukashov <viktor@vaadin.com>
2021-04-26 23:33:40 +0300

    feat: update snowpack to version 3.2.2
    Do not update to 3.3.4 (latest at the moment) because of an issue snowpack has with the @vaadin/vaadin-login component: https://github.com/snowpackjs/snowpack/issues/3214

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -320,7 +320,7 @@ public abstract class NodeUpdater implements FallibleCommand {
         // polyfill needed for FireFox et.al. at the moment
         defaults.put("construct-style-sheets-polyfill", "2.4.16");
 
-        defaults.put("snowpack", "3.0.13");
+        defaults.put("snowpack", "3.2.2");
 
         // Forcing chokidar version for now until new babel version is available
         // check out https://github.com/babel/babel/issues/11488
--- flow-server/src/main/resources/snowpack.generated.js
+++ flow-server/src/main/resources/snowpack.generated.js
@@ -16,7 +16,7 @@ module.exports = {
     // be accessible through the snowpack dev server
   },
   plugins: [
-    './snowpack-plugin-css-string.js' // replacement for lit-css-loader of webpack
+    './snowpack-plugin-css-string.js' // replacement for lit-css-loader in webpack
   ],
   devOptions: {
     hmr: false,
@@ -24,10 +24,8 @@ module.exports = {
     output: 'stream'
   },
   buildOptions: {
-    // Prepend VAADIN/ to all web modules URLs served by the snowpack dev
-    // server so that Vaadin's DevModeHandler correctly identifies such
-    // requests as 'dev mode' and passes them to the snowpack dev server.
-    // (example: /VAADIN/_snowpack/pkg/lit-element.js)
+    // Prepend with VAADIN/ so that Vaadin Dev Mode Handler correctly detects requests
+    // to node modules as 'dev mode' requests and passes them to the snowpack dev server
     // See com.vaadin.flow.server.DevModeHandler.isDevModeRequest()
     metaUrlPath: 'VAADIN/_snowpack',
   },
@@ -37,11 +35,27 @@ module.exports = {
 
     // quick fix for the missing Vaadin theme plugin
     'themes': './frontend/themes',
+
+    // Fix a build error:
+    // @vaadin/flow-frontend: Unscannable package import found.
+    // Snowpack scans source files for package imports at startup, and on every change.
+    // But, sometimes an import gets added during the build process, invisible to our file scanner.
+    // We'll prepare this package for you now, but should add "@vaadin/flow-frontend" to "knownEntrypoints"
+    // in your config file so that this gets prepared with the rest of your imports during startup.
+    '@vaadin/flow-frontend': './target/flow-frontend',
+    '@vaadin/form': './target/flow-frontend/form'
   },
   // Serve the .js files from node_modules and flow-frontend as they are,
   // even if there are .ts sources along with .js compiled output.
   exclude: [
     '**/node_modules/**/*',
-    '**/target/flow-frontend/**/*',
-  ]
+    '**/target/flow-frontend/**/*.ts', // need to explicitly specify .ts here to workaround a snowpack quirk
+  ],
+
+  // Fix a build warning:
+  // @vaadin/flow-frontend: Locally linked package detected outside of project root.
+  // If you are working in a workspace/monorepo, set your snowpack.config.js "workspaceRoot" to your workspace
+  // directory to take advantage of fast HMR updates for linked packages. Otherwise, this package will be
+  // cached until its package.json "version" changes. To silence this warning, set "workspaceRoot: false".
+  workspaceRoot: false,
 }
