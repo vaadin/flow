2affdcca62da5e30e8429e78e0973c14b9dda690
Anton Platonov <anton@vaadin.com>
2021-04-28 10:11:49 +0300

    Update flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/PopStateHandlerIT.java
    Co-authored-by: Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/PopStateHandlerIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/PopStateHandlerIT.java
@@ -77,7 +77,7 @@ public class PopStateHandlerIT extends ChromeBrowserTest {
     }
 
     @Test
-    @Ignore("Ignored because if the issue https://github.com/vaadin/flow/issues/10825")
+    @Ignore("Ignored because of the issue https://github.com/vaadin/flow/issues/10825")
     public void testSamePathHashChanges_tripleeBack_noServerSideEvent() {
         open();
 
