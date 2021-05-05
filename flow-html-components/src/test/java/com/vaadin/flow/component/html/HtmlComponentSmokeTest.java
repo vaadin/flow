5801c5d4c44b769019ab64acf2a83be0592d5f80
Denis Anisimov <denis@vaadin.com>
2021-05-05 10:24:11 +0300

    yet another change to test to support setter for target object

--- flow-html-components/src/test/java/com/vaadin/flow/component/html/HtmlComponentSmokeTest.java
+++ flow-html-components/src/test/java/com/vaadin/flow/component/html/HtmlComponentSmokeTest.java
@@ -26,7 +26,14 @@ import java.lang.reflect.Type;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
 
 import org.junit.Assert;
@@ -198,11 +205,11 @@ public class HtmlComponentSmokeTest {
             return true;
         }
 
-        // Anchor.setTarget(AnchorTarget) -
+        // Anchor.setTarget(AnchorTargetValue) -
         // https://github.com/vaadin/flow/issues/8346
         if (method.getDeclaringClass() == Anchor.class
                 && method.getName().equals("setTarget")
-                && method.getParameterTypes()[0] == AnchorTarget.class) {
+                && method.getParameterTypes()[0] == AnchorTargetValue.class) {
             return true;
         }
 
