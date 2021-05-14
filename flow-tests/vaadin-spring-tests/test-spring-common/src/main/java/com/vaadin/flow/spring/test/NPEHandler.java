d6a674fdbb6f605ed5614671af3b8fd7f4772ddb
Denis <denis@vaadin.com>
2021-05-14 15:09:33 +0300

    feat: implement RouteScope vaadin/spring#796
    Introduces `@RouteScope` and `@RouteScopeOwner` to be able to scope beans to routing component lifecycle.
    
    Fixes: vaadin/spring#263

--- flow-tests/vaadin-spring-tests/test-spring-common/src/main/java/com/vaadin/flow/spring/test/NPEHandler.java
+++ flow-tests/vaadin-spring-tests/test-spring-common/src/main/java/com/vaadin/flow/spring/test/NPEHandler.java
@@ -17,10 +17,12 @@ package com.vaadin.flow.spring.test;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.ErrorParameter;
 import com.vaadin.flow.router.HasErrorParameter;
-import com.vaadin.flow.router.BeforeEnterEvent;
 
 public class NPEHandler extends Div
         implements HasErrorParameter<NullPointerException> {
@@ -30,6 +32,8 @@ public class NPEHandler extends Div
             ErrorParameter<NullPointerException> parameter) {
         getElement().setText("NPE is thrown");
         setId("npe-handle");
+        LoggerFactory.getLogger(NPEHandler.class).error("NPE is thrown",
+                parameter.getCaughtException());
         return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
     }
 
