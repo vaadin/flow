f1d1016955e9c2708640716637098118fe18c690
caalador <mikael.grankvist@vaadin.com>
2021-05-14 13:46:44 +0300

    fix: update the used Opcode from 7 to 8 (#10979)
    Update used ClassVisitor ASM version
    from 7 to 8 to support Java16 record
    
    Fixes #10870

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/scanner/FrontendAnnotatedClassVisitor.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/scanner/FrontendAnnotatedClassVisitor.java
@@ -55,7 +55,7 @@ final class FrontendAnnotatedClassVisitor extends ClassVisitor {
      *            The annotation class name to visit
      */
     FrontendAnnotatedClassVisitor(ClassFinder finder, String annotationName) {
-        super(Opcodes.ASM7);
+        super(Opcodes.ASM8);
         this.finder = finder;
         this.annotationName = annotationName;
     }
