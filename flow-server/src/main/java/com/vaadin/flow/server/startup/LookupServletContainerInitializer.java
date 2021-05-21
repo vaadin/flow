90435bbce5db932f9fa95873c5735cdbc10f4188
Johannes Eriksson <joheriks@vaadin.com>
2021-05-21 16:17:32 +0300

    Use internal Lookup init instead of Java SPI

--- flow-server/src/main/java/com/vaadin/flow/server/startup/LookupServletContainerInitializer.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/LookupServletContainerInitializer.java
@@ -35,6 +35,8 @@ import com.vaadin.flow.di.Lookup;
 import com.vaadin.flow.di.LookupInitializer;
 import com.vaadin.flow.di.ResourceProvider;
 import com.vaadin.flow.function.VaadinApplicationInitializationBootstrap;
+import com.vaadin.flow.internal.BrowserLiveReloadAccess;
+import com.vaadin.flow.internal.DevModeHandlerAccess;
 import com.vaadin.flow.internal.ReflectTools;
 import com.vaadin.flow.server.StaticFileHandlerFactory;
 import com.vaadin.flow.server.VaadinContext;
@@ -54,7 +56,8 @@ import com.vaadin.flow.server.frontend.EndpointGeneratorTaskFactory;
         DeprecatedPolymerPublishedEventHandler.class,
         EndpointGeneratorTaskFactory.class,
         ApplicationConfigurationFactory.class, AbstractLookupInitializer.class,
-        AppShellPredicate.class, StaticFileHandlerFactory.class })
+        AppShellPredicate.class, StaticFileHandlerFactory.class,
+        DevModeHandlerAccess.class, BrowserLiveReloadAccess.class })
 public class LookupServletContainerInitializer
         implements ClassLoaderAwareServletContainerInitializer {
 
