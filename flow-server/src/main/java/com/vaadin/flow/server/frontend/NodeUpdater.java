86235b4e40276257958cd61d6beb2a7fcfb77863
Serhii Kulykov <iamkulykov@gmail.com>
2021-05-24 12:19:04 +0300

    feat: bump lit-css-loader for Lit 2 (#11062)

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -385,7 +385,7 @@ public abstract class NodeUpdater implements FallibleCommand {
         defaults.put("webpack-merge", "4.2.2");
         defaults.put("css-loader", "4.2.1");
         defaults.put("extract-loader", "5.1.0");
-        defaults.put("lit-css-loader", "0.0.4");
+        defaults.put("lit-css-loader", "0.1.0");
         defaults.put("file-loader", "6.2.0");
         defaults.put("loader-utils", "2.0.0");
         final String WORKBOX_VERSION = "6.1.0";
