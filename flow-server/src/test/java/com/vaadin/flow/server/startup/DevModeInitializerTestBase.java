362b2fdc9131253ca99dc6ff17fef59241b872fa
Manuel Carrasco Moñino <manolo@vaadin.com>
2021-05-25 14:36:14 +0200

    test: fix mockito errors accessing protected API in JDK16 (#11034) (CP: 6.0)
    related to https://github.com/vaadin/flow-components/issues/997
    related to https://github.com/vaadin/flow-components/issues/998

--- flow-server/src/test/java/com/vaadin/flow/server/startup/DevModeInitializerTestBase.java
+++ flow-server/src/test/java/com/vaadin/flow/server/startup/DevModeInitializerTestBase.java
@@ -226,12 +226,10 @@ public class DevModeInitializerTestBase {
 
         Mockito.when(appConfig.getStringProperty(Mockito.anyString(),
                 Mockito.anyString()))
-                .thenAnswer(invocation -> invocation.getArgumentAt(1,
-                        String.class));
+                .thenAnswer(invocation -> invocation.getArgument(1));
         Mockito.when(appConfig.getBooleanProperty(Mockito.anyString(),
                 Mockito.anyBoolean()))
-                .thenAnswer(invocation -> invocation.getArgumentAt(1,
-                        Boolean.class));
+                .thenAnswer(invocation -> invocation.getArgument(1));
 
         Mockito.when(appConfig.getStringProperty(FrontendUtils.PROJECT_BASEDIR,
                 null)).thenReturn(baseDir);
