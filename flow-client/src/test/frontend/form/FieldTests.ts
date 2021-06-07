ddd70951b2aefb9c3541a0685a5f45713b81556d
Kari Söderholm <haprog@vaadin.com>
2021-06-07 18:20:42 +0300

    fix: NumberModel should read empty string as undefined (#11169)
    NumberModel should read empty string as undefined so that the NumberValidator can pass the validation for an option number field when the input value is an empty string.
    
    Fixes https://github.com/vaadin/fusion/issues/13

--- flow-client/src/test/frontend/form/FieldTests.ts
+++ flow-client/src/test/frontend/form/FieldTests.ts
@@ -279,11 +279,11 @@ suite("form/Field", () => {
       });
 
       test('should update binder value on typing', async () => {
-        const cases: Array<[string, number]> = [
+        const cases: Array<[string, number | undefined]> = [
           ['1', 1],
           ['1.', NaN], // not allowed format
           ['1.2', 1.2],
-          ['', NaN],
+          ['', undefined],
           ['not a number', NaN],
           ['.', NaN],
           ['.1', 0.1],
@@ -420,11 +420,11 @@ suite("form/Field", () => {
       });
 
       test('should update binder value on typing', async () => {
-        const cases: Array<[string, number]> = [
+        const cases: Array<[string, number | undefined]> = [
           ['1', 1],
           ['1.', NaN], // not allowed format
           ['1.2', 1.2],
-          ['', NaN],
+          ['', undefined],
           ['not a number', NaN],
           ['.', NaN],
           ['.1', 0.1],
