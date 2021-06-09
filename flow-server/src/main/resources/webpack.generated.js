9f040ea49d7147700c601b68afeeb3fbc36bf5ed
Mikhail Shabarov <mikhail@vaadin.com>
2021-06-09 21:21:08 +0300

    fix: Watch theme folders from Jar dependency

--- flow-server/src/main/resources/webpack.generated.js
+++ flow-server/src/main/resources/webpack.generated.js
@@ -61,7 +61,7 @@ const projectStaticAssetsFolders = [
   frontendFolder
 ];
 
-const projectStaticAssetsOutputFolder = [to-be-generated-by-flow];
+const projectStaticAssetsOutputFolder = '[to-be-generated-by-flow]';
 
 // Folders in the project which can contain application themes
 const themeProjectFolders = projectStaticAssetsFolders.map((folder) =>
@@ -187,8 +187,9 @@ if (devMode) {
   // generated folder
   themeName = extractThemeName(frontendGeneratedFolder);
   const parentThemePaths = findParentThemes(themeName, themeOptions);
-  const currentThemeFolders = projectStaticAssetsFolders
-    .map((folder) => path.resolve(folder, "themes", themeName));
+  const currentThemeFolders = [...projectStaticAssetsFolders
+    .map((folder) => path.resolve(folder, "themes", themeName)),
+    path.resolve(flowFrontendThemesFolder, themeName)];
   // Watch the components folders for component styles update in both
   // current theme and parent themes. Other folders or CSS files except
   // 'styles.css' should be referenced from `styles.css` anyway, so no need
@@ -330,7 +331,7 @@ module.exports = {
 
     ...(devMode && themeName ? [new ExtraWatchWebpackPlugin({
       files: [],
-      dirs: [...themeWatchFolders]
+      dirs: themeWatchFolders
     }), new ThemeLiveReloadPlugin(processThemeResourcesCallback)] : []),
 
     new StatsPlugin({
