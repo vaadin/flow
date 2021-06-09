2723ce9568a29ae3e3637eaeaa0edf82c9fb8f44
Vlad Rindevich <vladrin@vaadin.com>
2021-06-09 10:35:28 +0300

    Move TypeScript client code to separate npm packages  (#11042)
    Creates two npm packages that will be used as dependencies in both Flow and Fusion:
    
    - `@vaadin/client-commons` - this package contains common client code shared by Flow and Fusion.
    - `@vaadin/client-fusion` - this package contains Fusion-specific client code

---
+++ client/web-test-runner.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires,import/no-extraneous-dependencies */
+const { esbuildPlugin } = require('@web/dev-server-esbuild');
+
+const tsExtPattern = /\.ts$/;
+
+module.exports = {
+  rootDir: '.',
+  nodeResolve: true,
+  browserStartTimeout: 60000, // default 30000
+  testsStartTimeout: 60000, // default 10000
+  testsFinishTimeout: 60000, // default 20000
+  plugins: [
+    esbuildPlugin({ ts: true, target: 'auto' }),
+    {
+      name: 'fix-node-resolve-issue',
+      transformImport({ source }) {
+        return source.includes('wds-outside-root') && source.endsWith('.ts')
+          ? source.replace(tsExtPattern, '.js')
+          : source;
+      },
+    },
+  ],
+};
