ec9ac7046f07aec905e88c57251141a0832bf088
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2021-06-11 08:55:20 +0300

    test: add fusion form integration tests vaadin/spring#799
    * test: add fusion form integration tests
    
    * revert unwanted changes
    
    * add package.json and package lock to version control
    
    * update tests
    
    * version updates
    
    * add tests for clearing number field
    
    * reuse the fusion security test module for form

--- flow-tests/vaadin-spring-tests/test-spring-security-flow-contextpath/src/test/java/com/vaadin/flow/spring/flowsecuritycontextpath/AppViewIT.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow-contextpath/src/test/java/com/vaadin/flow/spring/flowsecuritycontextpath/AppViewIT.java
@@ -1,19 +1,5 @@
 package com.vaadin.flow.spring.flowsecuritycontextpath;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import com.vaadin.flow.component.button.testbench.ButtonElement;
-import com.vaadin.flow.component.login.testbench.LoginFormElement;
-import com.vaadin.flow.component.login.testbench.LoginOverlayElement;
-import com.vaadin.flow.testutil.ChromeBrowserTest;
-import com.vaadin.testbench.TestBenchElement;
-
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Test;
-
 public class AppViewIT extends com.vaadin.flow.spring.flowsecurity.AppViewIT {
 
     @Override
