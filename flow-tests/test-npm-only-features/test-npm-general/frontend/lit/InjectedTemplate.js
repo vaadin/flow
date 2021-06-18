444393dc098a2c3f7ee3413d5232722dfa09c291
Denis <denis@vaadin.com>
2021-06-18 10:54:34 +0300

    fix: Use JS equals to check whether property value is changed (#11192)
    Fixes #11175

---
+++ flow-tests/test-npm-only-features/test-npm-general/frontend/lit/InjectedTemplate.js
@@ -0,0 +1,15 @@
+import { LitElement, html } from "lit";
+
+export class InjectedTemplate extends LitElement {
+  static get properties() {
+    return {
+      value: { type: Number}
+    };
+  }
+  render() {
+    return html`
+       <div>Injected Template</div>
+    `;
+  }
+}
+customElements.define("injected-lit-template", InjectedTemplate);
