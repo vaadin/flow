14c496570ec8d057017b3157d1cbb922f841826f
haijian <haijian@vaadin.com>
2021-06-24 13:12:34 +0300

    update imports for packing renaming

--- fusion-endpoint/src/main/java/com/vaadin/fusion/generator/SchemaResolver.java
+++ fusion-endpoint/src/main/java/com/vaadin/fusion/generator/SchemaResolver.java
@@ -34,7 +34,7 @@ import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.StringSchema;
 
-import com.vaadin.flow.server.connect.ExplicitNullableTypeChecker;
+import com.vaadin.fusion.ExplicitNullableTypeChecker;
 
 class SchemaResolver {
 
