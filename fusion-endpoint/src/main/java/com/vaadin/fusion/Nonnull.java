956a228a5513777f23a070b9d91a90346f96d83e
Vlad Rindevich <vladrin@vaadin.com>
2021-06-28 14:07:16 +0300

    docs(fusion-endpoint): add documentation for Nonnull annotation

--- fusion-endpoint/src/main/java/com/vaadin/fusion/Nonnull.java
+++ fusion-endpoint/src/main/java/com/vaadin/fusion/Nonnull.java
@@ -6,6 +6,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark either field, method, parameter or collection item type
+ * as non-nullable. It is used by Typescript Generator as a source of type
+ * nullability information.
+ *
+ * Since Typescript Generator works with any annotation with case-insensitive
+ * `nonnull` name, this annotation exists only for convenience because the most
+ * popular `javax.annotation.Nonnull` annotation is not applicable to collection
+ * item types.
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
