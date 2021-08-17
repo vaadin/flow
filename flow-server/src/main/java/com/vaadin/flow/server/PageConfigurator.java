fe06b1cd9d82c5908a26c30fc86c6e843c2393b1
Pekka Hyvönen <pekka@vaadin.com>
2021-08-17 16:52:15 +0300

    fix: explain deprecation of PageConfigurator [skip-ci]
    The previous deprecation message was totally lacking any details to the
    users.

--- flow-server/src/main/java/com/vaadin/flow/server/PageConfigurator.java
+++ flow-server/src/main/java/com/vaadin/flow/server/PageConfigurator.java
@@ -17,12 +17,22 @@ package com.vaadin.flow.server;
 
 import java.io.Serializable;
 
+import com.vaadin.flow.component.UI;
+
 /**
  * Configures the initial page contents.
  *
  * @since 1.0
- * 
- * @deprecated Use {@link BootstrapListener} instead
+ * @deprecated Deprecated due to multiple issues on feature design, like this
+ * won't work together with the {@link com.vaadin.flow.router.PreserveOnRefresh}
+ * annotation. Will not be removed until <em>after the next long term support
+ * version</em> (targeted Vaadin 23).
+ * <p>
+ * For Vaadin 14, use {@link BootstrapListener} instead, which provides API for
+ * modifying the bootstrap page and access to the {@link UI}, which provides
+ * further replacement API like {@link UI#getLoadingIndicatorConfiguration()}.
+ * <p>
+ * For Vaadin 15+, use {@code AppShellConfigurator} instead.
  */
 @Deprecated
 @FunctionalInterface
