3d10b97d24e6ca6d4f34b4ae2103a63fdc94171e
Mikhail Shabarov <mikhail@vaadin.com>
2021-08-26 16:47:10 +0300

    Add test and explain dev mode session serialization

--- flow-server/src/test/java/com/vaadin/tests/server/SerializationTest.java
+++ flow-server/src/test/java/com/vaadin/tests/server/SerializationTest.java
@@ -6,35 +6,48 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import junit.framework.TestCase;
+import org.mockito.Mockito;
 
 import com.vaadin.flow.server.VaadinContext;
 import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinServletService;
 import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.server.WrappedSession;
 import com.vaadin.flow.server.startup.ApplicationConfiguration;
-
-import org.mockito.Mockito;
-
-import junit.framework.TestCase;
 
 public class SerializationTest extends TestCase {
 
-    public void testVaadinSession() throws Exception {
-        VaadinService service = Mockito.mock(VaadinService.class);
-        VaadinContext context = Mockito.mock(VaadinContext.class);
-        ApplicationConfiguration applicationConfiguration = Mockito
-                .mock(ApplicationConfiguration.class);
-        Mockito.when(service.getContext()).thenReturn(context);
-        Mockito.when(context.getAttribute(Mockito.any(), Mockito.any()))
-                .thenReturn(applicationConfiguration);
-        Mockito.when(applicationConfiguration.isProductionMode())
-                .thenReturn(true);
-        VaadinSession session = new VaadinSession(service);
+    public void testSerializeVaadinSession_accessQueueIsRecreated()
+            throws Exception {
+        VaadinService vaadinService = new MockVaadinService(true);
+        VaadinSession session = new VaadinSession(vaadinService);
 
         session = serializeAndDeserialize(session);
 
         assertNotNull(
                 "Pending access queue was not recreated after deserialization",
                 session.getPendingAccessQueue());
+    }
+
+    public void testSerializeVaadinSession_notProductionMode_disableDevModeSerialization_deserializedSessionHasNoUIs()
+            throws Exception {
+        VaadinService vaadinService = new MockVaadinService(false);
+        VaadinSession session = new VaadinSession(vaadinService);
+
+        session = serializeAndDeserialize(session);
+        // This is done only for test purpose to refresh the session lock,
+        // should be called by Flow internally as soon as the session has
+        // been retrieved from http session.
+        session.refreshTransients(null, vaadinService);
+
+        assertNotNull("UIs should be available after empty deserialization",
+                session.getUIs());
+        assertTrue("UIs should be empty after empty deserialization",
+                session.getUIs().isEmpty());
     }
 
     private static <S extends Serializable> S serializeAndDeserialize(S s)
@@ -89,6 +102,42 @@ public class SerializationTest extends TestCase {
 
         public String getDummyGetter() {
             return dummyGetter;
+        }
+    }
+
+    private static class MockVaadinService extends VaadinServletService {
+
+        private final VaadinContext vaadinContext;
+        private final boolean productionMode;
+        private final Lock lock = new ReentrantLock();
+
+        {
+            lock.lock();
+        }
+
+        public MockVaadinService(boolean productionMode) {
+            super();
+            this.vaadinContext = Mockito.mock(VaadinContext.class);
+            this.productionMode = productionMode;
+        }
+
+        @Override
+        public VaadinContext getContext() {
+            ApplicationConfiguration applicationConfiguration = Mockito
+                    .mock(ApplicationConfiguration.class);
+            Mockito.when(
+                    vaadinContext.getAttribute(Mockito.any(), Mockito.any()))
+                    .thenReturn(applicationConfiguration);
+            Mockito.when(applicationConfiguration.isProductionMode())
+                    .thenReturn(productionMode);
+            Mockito.when(applicationConfiguration
+                    .isDevModeSessionSerializationEnabled()).thenReturn(false);
+            return vaadinContext;
+        }
+
+        @Override
+        protected Lock getSessionLock(WrappedSession wrappedSession) {
+            return lock;
         }
     }
 }
