2a454589d43cbb9606b4e763c6020aae69927437
Sami Ekblad <sami@vaadin.com>
2021-08-26 13:12:34 +0300

    Made proKey class package private and removed unused methods.

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/ProKey.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/ProKey.java
@@ -32,44 +32,42 @@ import org.slf4j.LoggerFactory;
  */
 class ProKey {
 
-    public static final String FIELD_NAME = "username";
-    public static final String FIELD_KEY = "proKey";
+    private static final String FIELD_NAME = "username";
+    private static final String FIELD_KEY = "proKey";
     private final String username;
     private final String key;
 
-    public ProKey(String username, String key) {
+    ProKey(String username, String key) {
         super();
         this.username = username;
         this.key = key;
     }
 
-    public static ProKey fromJson(String jsonData) {
-        ProKey proKey = new ProKey(null, null);
-        try {
-            JsonNode json = JsonHelpers.getJsonMapper().readTree(jsonData);
-            proKey = new ProKey(json.get(FIELD_NAME).asText(),
-                    json.get(FIELD_KEY).asText());
-            return proKey;
-        } catch (JsonProcessingException | NullPointerException e) {
-            getLogger().debug("Failed to parse proKey from json", e);
-        }
-        return proKey;
+    public String getUsername() {
+        return username;
     }
 
-    public static ProKey fromFile(File jsonFile) throws IOException {
-        ProKey proKey = new ProKey(null, null);
-        try {
-            JsonNode json = JsonHelpers.getJsonMapper().readTree(jsonFile);
-            proKey = new ProKey(json.get(FIELD_NAME).asText(),
-                    json.get(FIELD_KEY).asText());
-            return proKey;
-        } catch (JsonProcessingException | NullPointerException e) {
-            getLogger().debug("Failed to parse proKey from json file", e);
-        }
-        return proKey;
+    public String getKey() {
+        return key;
     }
 
-    public static ProKey get() {
+    public String toJson() {
+        ObjectNode json = JsonHelpers.getJsonMapper().createObjectNode();
+        json.put(FIELD_NAME, username);
+        json.put(FIELD_KEY, key);
+        try {
+            return JsonHelpers.getJsonMapper().writeValueAsString(json);
+        } catch (JsonProcessingException e) {
+            getLogger().debug("Unable to read proKey", e);
+        }
+        return null;
+    }
+
+    public void toFile(File fileLocation) throws IOException {
+        JsonHelpers.getJsonMapper().writeValue(fileLocation, this);
+    }
+
+    static ProKey get() {
         ProKey proKey = getSystemProperty();
         if (proKey != null) {
             return proKey;
@@ -80,7 +78,7 @@ class ProKey {
         }
         File proKeyLocation = getFileLocation();
         try {
-            proKey = read(proKeyLocation);
+            proKey = fromFile(proKeyLocation);
             return proKey;
         } catch (IOException e) {
             getLogger().debug("Unable to read proKey", e);
@@ -118,55 +116,30 @@ class ProKey {
         return new ProKey(parts[0], parts[1]);
     }
 
-    public static File getFileLocation() {
+    private static File getFileLocation() {
         File vaadinHome = ProjectHelpers.resolveVaadinHomeDirectory();
         return new File(vaadinHome, StatisticsConstants.PRO_KEY_FILE_NAME);
     }
 
-    private static ProKey read(File proKeyLocation) throws IOException {
-        if (!proKeyLocation.exists()) {
+    static ProKey fromFile(File jsonFile) throws IOException {
+        if (!jsonFile.exists()) {
             return null;
         }
-        return ProKey.fromFile(proKeyLocation);
-    }
 
-    public static void write(ProKey proKey, File proKeyLocation)
-            throws IOException {
-        File proKeyDirectory = proKeyLocation.getParentFile();
-        if (!proKeyDirectory.exists() && !proKeyDirectory.mkdirs()) {
-            throw new IOException("Failed to create directory "
-                    + proKeyDirectory.getAbsolutePath());
+        ProKey proKey = new ProKey(null, null);
+        try {
+            JsonNode json = JsonHelpers.getJsonMapper().readTree(jsonFile);
+            proKey = new ProKey(json.get(FIELD_NAME).asText(),
+                    json.get(FIELD_KEY).asText());
+            return proKey;
+        } catch (JsonProcessingException | NullPointerException e) {
+            getLogger().debug("Failed to parse proKey from json file", e);
         }
-        proKey.toFile(proKeyLocation);
+        return proKey;
     }
 
     private static Logger getLogger() {
         // Use the same logger that DevModeUsageStatistics uses
         return LoggerFactory.getLogger(DevModeUsageStatistics.class.getName());
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void toFile(File proKeyLocation) throws IOException {
-        JsonHelpers.getJsonMapper().writeValue(proKeyLocation, this);
-    }
-
-    public String toJson() {
-        ObjectNode json = JsonHelpers.getJsonMapper().createObjectNode();
-        json.put(FIELD_NAME, username);
-        json.put(FIELD_KEY, key);
-        try {
-            return JsonHelpers.getJsonMapper().writeValueAsString(json);
-        } catch (JsonProcessingException e) {
-            getLogger().debug("Unable to read proKey", e);
-        }
-        return null;
-    }
-
 }
