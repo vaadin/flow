1fb71f50989769d2f8735f116341138a85f6209e
caalador <mikael.grankvist@vaadin.com>
2021-09-09 16:13:16 +0300

    fix: Fixes TypeScript transpilation to ES5 (#11792)
    Fix the transpilation of TypeScript files
    to ES5 for use with IE11
    
    Part of #11713

--- flow-server/src/main/resources/webpack.generated.js
+++ flow-server/src/main/resources/webpack.generated.js
@@ -179,10 +179,15 @@ module.exports = {
 
   module: {
     rules: [
-      {
+      ...(transpile ? [
+        {
+        test: /\.tsx?$/,
+        use: [ BabelMultiTargetPlugin.loader(), 'ts-loader' ],
+      }
+      ] : [{
         test: /\.tsx?$/,
         use: ['ts-loader']
-      },
+      }]),
       ...(transpile ? [{ // Files that Babel has to transpile
         test: /\.js$/,
         use: [BabelMultiTargetPlugin.loader()]
