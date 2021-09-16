508c0c414a610c52df76ab7cd0a2cf37f465b769
Denis <denis@vaadin.com>
2021-09-16 11:38:11 +0300

    fix: add possibility to set header values for StreamResource (#11860)
    fixes #5471

--- flow-server/src/test/java/com/vaadin/flow/server/StreamResourceTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/StreamResourceTest.java
@@ -15,17 +15,17 @@
  */
 package com.vaadin.flow.server;
 
+import javax.servlet.ServletContext;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
-import javax.servlet.ServletContext;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.vaadin.flow.function.ContentTypeResolver;
-import com.vaadin.flow.server.StreamResource;
 
 public class StreamResourceTest {
 
@@ -60,6 +60,20 @@ public class StreamResourceTest {
         Assert.assertNotNull(resource.getContentTypeResolver());
 
         assertContentType(resource, resource.getContentTypeResolver());
+    }
+
+    @Test
+    public void setHeader_headerIsInHeadersListAndGetterReturnsTheValue() {
+        StreamResource resource = new StreamResource("foo",
+                () -> makeEmptyStream());
+
+        resource.setHeader("foo", "bar");
+
+        Assert.assertEquals("bar", resource.getHeader("foo").get());
+
+        Map<String, String> headers = resource.getHeaders();
+        Assert.assertEquals(1, headers.size());
+        Assert.assertEquals("bar", headers.get("foo"));
     }
 
     private void assertContentType(StreamResource resource,
