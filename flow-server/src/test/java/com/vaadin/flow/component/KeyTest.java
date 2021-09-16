e445a9b7804b94403b4e738d757ddf8ea58da854
Denis <denis@vaadin.com>
2021-09-16 13:47:31 +0300

    fix: implement toString, equals, hashCode for Key implementation used in "of" (#11837)
    fixes #8474 and #4626

--- flow-server/src/test/java/com/vaadin/flow/component/KeyTest.java
+++ flow-server/src/test/java/com/vaadin/flow/component/KeyTest.java
@@ -15,6 +15,8 @@
  */
 package com.vaadin.flow.component;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Assert;
@@ -45,6 +47,62 @@ public class KeyTest {
         fired.set(false);
         input.fireEvent(new KeyPressEvent(input, "baz"));
         Assert.assertFalse(fired.get());
+    }
+
+    @Test
+    public void of_toString_returnsKeys() {
+        Key key = Key.of("foo");
+
+        Assert.assertEquals("foo", key.toString());
+
+        key = Key.of("foo", "bar");
+
+        Assert.assertEquals("foo,  additional keys : [bar]", key.toString());
+    }
+
+    @Test
+    public void of_equals_stringRepresentationsEqual_toKeysAreEqual() {
+        Key key1 = Key.of("foo");
+        Key key2 = Key.of("foo");
+
+        Assert.assertEquals(key1, key2);
+    }
+
+    @Test
+    public void of_equals_stringRepresentationsNotEqual_toKeysAreNotEqual() {
+        Key key1 = Key.of("foo");
+        Key key2 = Key.of("bar");
+
+        Assert.assertNotEquals(key1, key2);
+    }
+
+    @Test
+    public void of_equals_secondKeyHasAdditionalKeys_toKeysAreNotEqual() {
+        Key key1 = Key.of("foo");
+        Key key2 = Key.of("foo", "bar");
+
+        Assert.assertNotEquals(key1, key2);
+    }
+
+    @Test
+    public void of_equals_differentClasses_toKeysAreNotEqual() {
+        Key key1 = Key.of("foo");
+        Key key2 = new Key() {
+
+            @Override
+            public List<String> getKeys() {
+                return Collections.singletonList("foo");
+            }
+        };
+
+        Assert.assertNotEquals(key1, key2);
+    }
+
+    @Test
+    public void of_equalKeys_hasSameHashCode() {
+        Key key1 = Key.of("foo", "bar");
+        Key key2 = Key.of("foo", "bar");
+        Assert.assertEquals(key1.hashCode(), key2.hashCode());
     }
 
 }
