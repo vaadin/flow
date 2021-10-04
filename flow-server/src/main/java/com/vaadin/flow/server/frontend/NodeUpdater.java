a6fffabcaeefc7b461eea27d7fb97fae18239328
caalador <mikael.grankvist@gmail.com>
2021-10-04 14:06:00 +0300

    Update to version with fix.

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -297,7 +297,7 @@ public abstract class NodeUpdater implements FallibleCommand {
 
         // Constructable style sheets is only implemented for chrome,
         // polyfill needed for FireFox et.al. at the moment
-        defaults.put("construct-style-sheets-polyfill", "3.0.0");
+        defaults.put("construct-style-sheets-polyfill", "3.0.4");
         return defaults;
     }
 
