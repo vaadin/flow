2cf18d429d8e8c7d80b9c94d80571c546e58e3dc
Artur <artur@vaadin.com>
2021-10-07 12:08:07 +0300

    chore: Format the project vaadin/spring#913

--- flow-tests/vaadin-spring-tests/test-spring-security-flow-contextpath/src/main/java/com/vaadin/flow/spring/flowsecuritycontextpath/Application.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow-contextpath/src/main/java/com/vaadin/flow/spring/flowsecuritycontextpath/Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application extends com.vaadin.flow.spring.flowsecurity.Application {
+public class Application
+        extends com.vaadin.flow.spring.flowsecurity.Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
