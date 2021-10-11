f5191c1d8ddb5138a5de0a681b2cf4113096d9a0
caalador <mikael.grankvist@vaadin.com>
2021-10-11 08:49:00 +0300

    Add more information on goal javaDoc
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

--- flow-plugins/flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/maven/CleanFrontendMojo.java
+++ flow-plugins/flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/maven/CleanFrontendMojo.java
@@ -32,6 +32,13 @@ import elemental.json.impl.JsonUtil;
 
 /**
  * Goal that cleans the frontend files to a clean state.
+ * <p>
+ * Deletes Vaadin dependencies from package.json, the generated frontend folder and the npm/pnpm-related files and folders:
+ * <ul>
+ * <li>node_modules
+ * <li>pnpm-lock.yaml
+ * <li>package-lock.json
+ * </ul>
  *
  * @since 9.0
  */
