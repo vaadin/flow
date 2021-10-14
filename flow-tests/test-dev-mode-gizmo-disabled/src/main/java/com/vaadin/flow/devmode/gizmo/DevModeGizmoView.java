de772f95fec166924e25a822bb540e713ea46364
Mikhail Shabarov <mikhail@vaadin.com>
2021-10-14 19:33:28 +0300

    fix typo

--- flow-tests/test-dev-mode-gizmo-disabled/src/main/java/com/vaadin/flow/devmode/gizmo/DevModeGizmoView.java
+++ flow-tests/test-dev-mode-gizmo-disabled/src/main/java/com/vaadin/flow/devmode/gizmo/DevModeGizmoView.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.flow.devmode.gismo;
+package com.vaadin.flow.devmode.gizmo;
 
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
