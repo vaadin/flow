582545a648f0613ce4e9f91d31582bef162a7a57
Denis Anisimov <denis@vaadin.com>
2021-10-15 10:22:21 +0300

    chore: fix mock

--- flow-server/src/test/java/com/vaadin/flow/server/MockVaadinContext.java
+++ flow-server/src/test/java/com/vaadin/flow/server/MockVaadinContext.java
@@ -32,7 +32,7 @@ public class MockVaadinContext extends VaadinServletContext {
         @Override
         public String getRoutePath(Class<?> navigationTarget) {
             Route route = navigationTarget.getAnnotation(Route.class);
-            return route.value();
+            return route.value()[0];
         }
 
     }
