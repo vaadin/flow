bf36ef73f875939e2cc2d581ead9c34223b5d8a4
Artur Signell <artur@vaadin.com>
2021-10-20 17:00:23 +0300

    Fix old javadoc issue

--- flow-server/src/main/java/com/vaadin/flow/server/PropertyDeploymentConfiguration.java
+++ flow-server/src/main/java/com/vaadin/flow/server/PropertyDeploymentConfiguration.java
@@ -298,7 +298,7 @@ public class PropertyDeploymentConfiguration
     }
 
     /**
-     * Returns parent application configuration;
+     * Returns parent application configuration.
      *
      * @return the parent config
      */
