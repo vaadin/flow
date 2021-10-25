e541ce9040b89f9fd71a6066a6684753dc57dc2a
Denis <denis@vaadin.com>
2021-10-25 21:05:33 +0300

    refactor: Use maven vaadin plugin classloader as a parent for URLClassloader in ClassFinder (#12050)
    Using maven vaadin plugin classloader as a parent for the classloader in ClassFinder allows to avoid extra loading class from flow-server (or its dependencies) since it's already available in the classloader. So it may be referred as is (e.g. Route.class) instead using loadClass(Route.class.getName()). That simplifies the code and allows to avoid unnecessary ClassNotFoundException catching in many places.
    
    fixes #12077

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/scanner/FullScannerPwaTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/scanner/FullScannerPwaTest.java
@@ -6,33 +6,19 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.server.PwaConfiguration;
-import com.vaadin.flow.server.frontend.scanner.samples.pwa.AnotherAppShellWithPwa;
-import com.vaadin.flow.server.frontend.scanner.samples.pwa.AppShellWithPwa;
-import com.vaadin.flow.server.frontend.scanner.samples.pwa.AppShellWithoutPwa;
-import com.vaadin.flow.server.frontend.scanner.samples.pwa.NonAppShellWithPwa;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class FullScannerPwaTest extends AbstractScannerPwaTest {
     private ClassFinder finder = Mockito.mock(ClassFinder.class);
 
+    @Override
     protected PwaConfiguration getPwaConfiguration(Class<?>... classes)
             throws Exception {
-        // use this fake/mock class for the loaded class to check that annotated
-        // classes are requested for the loaded class and not for the
-        // annotationType
-        Class clazz = Object.class;
-
-        Mockito.doReturn(clazz).when(finder).loadClass(PWA.class.getName());
-
         Mockito.doReturn(getPwaAnnotatedClasses(classes)).when(finder)
-                .getAnnotatedClasses(clazz);
+                .getAnnotatedClasses(PWA.class);
 
         FullDependenciesScanner fullDependenciesScanner = new FullDependenciesScanner(
                 finder, (type, annotation) -> findPwaAnnotations(type), false);
