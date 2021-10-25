51837be415104765231ae072e725a024f1ea658d
Denis <denis@vaadin.com>
2021-10-25 21:05:33 +0300

    refactor: Use maven vaadin plugin classloader as a parent for URLClassloader in ClassFinder (#12050)
    Using maven vaadin plugin classloader as a parent for the classloader in ClassFinder allows to avoid extra loading class from flow-server (or its dependencies) since it's already available in the classloader. So it may be referred as is (e.g. Route.class) instead using loadClass(Route.class.getName()). That simplifies the code and allows to avoid unnecessary ClassNotFoundException catching in many places.
    
    fixes #12077

--- flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/utils/LookupImpl.java
+++ flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/utils/LookupImpl.java
@@ -48,32 +48,19 @@ public class LookupImpl implements Lookup {
 
     @Override
     public <T> List<T> lookupAll(Class<T> serviceClass) {
-        Set<?> subTypes = classFinder
-                .getSubTypesOf(loadClassFromClassFindler(serviceClass));
+        // only services declared in flow-server may be used here
+        assert serviceClass.getClassLoader()
+                .equals(LookupImpl.class.getClassLoader());
+        Set<Class<? extends T>> subTypes = classFinder
+                .getSubTypesOf(serviceClass);
         List<T> result = new ArrayList<>(subTypes.size());
-        try {
-            for (Object clazz : subTypes) {
-                if (!ReflectTools.isInstantiableService((Class<?>) clazz)) {
-                    continue;
-                }
-                Class<?> serviceType = serviceClass.getClassLoader()
-                        .loadClass(((Class<?>) clazz).getName());
-                result.add(serviceClass
-                        .cast(ReflectTools.createInstance(serviceType)));
+        for (Class<? extends T> clazz : subTypes) {
+            if (!ReflectTools.isInstantiableService(clazz)) {
+                continue;
             }
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Could not find service class", e);
+            result.add(serviceClass.cast(ReflectTools.createInstance(clazz)));
         }
         return result;
-    }
-
-    private Class<?> loadClassFromClassFindler(Class<?> clz) {
-        try {
-            return classFinder.loadClass(clz.getName());
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException(
-                    "Could not load " + clz.getName() + " class", e);
-        }
     }
 
 }
