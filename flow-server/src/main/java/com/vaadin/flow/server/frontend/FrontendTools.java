ed0b054f0f5071fc687aa5a066b189a84491e756
Artur Signell <artur@vaadin.com>
2021-10-29 12:26:36 +0300

    Fix test

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
@@ -55,6 +55,10 @@ import com.vaadin.flow.server.frontend.installer.ProxyConfig;
 public class FrontendTools {
 
     public static final String DEFAULT_NODE_VERSION = "v16.13.0";
+    /**
+     * This is the version shipped with the default Node version.
+     */
+    public static final String DEFAULT_NPM_VERSION = "8.1.0";
 
     public static final String DEFAULT_PNPM_VERSION = "5.18.10";
 
