a4af1eb5c6dd07b05816925c94b3b858f02795cb
Mikael Grankvist <mikael.grankvist@vaadin.com>
2021-11-01 11:52:44 +0200

    test: Add feature test to validate enabling feature.
    Add a test that enables a feature through
    the gizmo features tab.

---
+++ flow-tests/test-dev-mode/src/test/java/com/vaadin/flow/uitest/ui/FeatureIT.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2000-2021 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import com.vaadin.flow.component.html.testbench.H2Element;
+import com.vaadin.flow.component.html.testbench.NativeButtonElement;
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.flow.testutil.DevModeGizmoElement;
+
+public class FeatureIT extends ChromeBrowserTest {
+
+    @After
+    public void cleanup() {
+
+    }
+
+    @Test
+    public void enableAndDisableFeature() {
+        open();
+        Assert.assertEquals("Feature file missing",
+                $(H2Element.class).id("value").getText());
+        DevModeGizmoElement gizmo = $(DevModeGizmoElement.class).waitForFirst();
+
+        gizmo.expand();
+        gizmo.findElement(By.id("features")).click();
+        gizmo.findElement(By.id("feature-toggle-viteForFrontendBuild")).click();
+
+        try {
+            $(NativeButtonElement.class).id("check").click();
+
+            Assert.assertEquals(
+                    "Feature file exists with properties: com.vaadin.experimental.viteForFrontendBuild",
+                    $(H2Element.class).id("value").getText());
+        } finally {
+            $(NativeButtonElement.class).id("remove").click();
+
+            Assert.assertEquals("Feature file missing",
+                    $(H2Element.class).id("value").getText());
+        }
+    }
+
+}
