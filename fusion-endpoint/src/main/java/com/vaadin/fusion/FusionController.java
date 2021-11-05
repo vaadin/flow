cca08eb5b2f7a3e7f681a9cf275e990a89dd52d2
Artur <artur@vaadin.com>
2021-11-05 22:38:34 +0200

    Update FusionController.java

--- fusion-endpoint/src/main/java/com/vaadin/fusion/FusionController.java
+++ fusion-endpoint/src/main/java/com/vaadin/fusion/FusionController.java
@@ -26,7 +26,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
