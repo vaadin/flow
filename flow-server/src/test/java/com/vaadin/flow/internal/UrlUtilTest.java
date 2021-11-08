0ae02c3b422c3d1a8d590d7d92765b764b6461a5
Artur <artur@vaadin.com>
2021-11-08 09:01:04 +0200

    fix: Make URLUtil.encodeURI behave like encodeURI in JavaScript and add encodeURIComponent (#12291)

--- flow-server/src/test/java/com/vaadin/flow/internal/UrlUtilTest.java
+++ flow-server/src/test/java/com/vaadin/flow/internal/UrlUtilTest.java
@@ -20,7 +20,8 @@ import org.junit.Test;
 
 public class UrlUtilTest {
 
-    private String shouldNotBeEscaped = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/_-.*";
+    private String encodeURIShouldNotBeEscaped = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789;,/?:@&=+$-_.!~*'()#";
+    private String encodeURIComponentShouldNotBeEscaped = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.!~*'()";
 
     @Test
     public void isExternal_URLStartsWithTwoSlashes_returnsTrue() {
@@ -42,24 +43,43 @@ public class UrlUtilTest {
 
     @Test
     public void plusAndSpaceHandledCorrectly() {
-        Assert.assertEquals("Plus%2BSpa%20%2B%20ce",
+        Assert.assertEquals("Plus+Spa%20+%20ce",
                 UrlUtil.encodeURI("Plus+Spa + ce"));
+        Assert.assertEquals("Plus%2BSpa%20%2B%20ce",
+                UrlUtil.encodeURIComponent("Plus+Spa + ce"));
     }
 
     @Test
     public void encodeURI_shouldNotBeEscaped() {
-        Assert.assertEquals(shouldNotBeEscaped,
-                UrlUtil.encodeURI(shouldNotBeEscaped));
+        Assert.assertEquals(encodeURIShouldNotBeEscaped,
+                UrlUtil.encodeURI(encodeURIShouldNotBeEscaped));
     }
 
     @Test
     public void encodeURI_mustBeEscaped() {
         for (char c = 0; c < 255; c++) {
             String s = String.valueOf(c);
-            if (shouldNotBeEscaped.contains(s)) {
+            if (encodeURIShouldNotBeEscaped.contains(s)) {
                 continue;
             }
             Assert.assertNotEquals(UrlUtil.encodeURI(s), s);
+        }
+    }
+
+    @Test
+    public void encodeURIComponent_shouldNotBeEscaped() {
+        Assert.assertEquals(encodeURIComponentShouldNotBeEscaped, UrlUtil
+                .encodeURIComponent(encodeURIComponentShouldNotBeEscaped));
+    }
+
+    @Test
+    public void encodeURIComponent_mustBeEscaped() {
+        for (char c = 0; c < 255; c++) {
+            String s = String.valueOf(c);
+            if (encodeURIComponentShouldNotBeEscaped.contains(s)) {
+                continue;
+            }
+            Assert.assertNotEquals(UrlUtil.encodeURIComponent(s), s);
         }
     }
 }
