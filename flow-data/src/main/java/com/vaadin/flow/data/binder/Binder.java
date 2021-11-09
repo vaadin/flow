0681437e810267c131c83d16ace9de3fcb27b125
Tatu Lund <tatu@vaadin.com>
2021-11-09 10:08:43 +0200

    fix: Ensure that side effects of readBean do not set hasChanges true (#12181)
    JavaDoc of hasChanges says:
    
    "Check whether any of the bound fields' have uncommitted changes
    since last explicit call to readBean(Object), removeBean(), writeBean(Object)}
    or writeBeanIfValid(Object)."
    
    If readBean has converters, they will be run and field values updated accordingly.
    Furthermore if fields have value change listeners that will produce further
    changes in values, this should be considered according to above as part of
    readBean procedure and thus hasChanges still should return false.
    
    Cherry pick from: vaadin/framework#12455
    
    (cherry picked from commit 4e36fd979ec4d592d08e8d1328eeb4149c8fa3ef)

--- flow-data/src/main/java/com/vaadin/flow/data/binder/Binder.java
+++ flow-data/src/main/java/com/vaadin/flow/data/binder/Binder.java
@@ -1875,7 +1875,6 @@ public class Binder<BEAN> implements Serializable {
         if (bean == null) {
             clearFields();
         } else {
-            changedBindings.clear();
             getBindings().forEach(binding -> {
                 /*
                  * Some bindings may have been removed from binder during
@@ -1887,6 +1886,7 @@ public class Binder<BEAN> implements Serializable {
                     binding.initFieldValue(bean, false);
                 }
             });
+            changedBindings.clear();
             getValidationStatusHandler().statusChange(
                     BinderValidationStatus.createUnresolvedStatus(this));
             fireStatusChangeEvent(false);
