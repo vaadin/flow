11237f7a85ca07ef3e9a55545faa7c37695390dc
Artur Signell <artur@vaadin.com>
2021-11-09 14:19:26 +0200

    fix: Log dev server output using standard logger
    Fixes #12281

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/DevServerOutputTracker.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/DevServerOutputTracker.java
@@ -24,8 +24,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
 
-import com.vaadin.flow.server.frontend.FrontendUtils;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -74,7 +72,6 @@ public class DevServerOutputTracker {
             StringBuilder line = new StringBuilder();
             for (int i; (i = reader.read()) >= 0;) {
                 char ch = (char) i;
-                FrontendUtils.console("%c", ch);
                 line.append(ch);
                 if (ch == '\n') {
                     processLine(line.toString());
@@ -88,10 +85,19 @@ public class DevServerOutputTracker {
             if (line.contains("\b")) {
                 return;
             }
-
-            // remove color escape codes for console
+            // remove color escape codes
             String cleanLine = line.replaceAll("(\u001b\\[[;\\d]*m|[\b\r]+)",
                     "");
+
+            // Remove newline and timestamp as logger will add it
+            String logLine = cleanLine;
+            logLine = logLine.replaceAll("\n$", "");
+
+            // Vite preprends a timestamp
+            logLine = logLine.replaceAll("[0-9]+:[0-9]+:[0-9]+ [AP]M ", "");
+            // Webpack often prepends with <i>
+            logLine = logLine.replaceAll("^<i> ", "");
+            getLogger().info(logLine);
 
             boolean succeed = success != null && success.matcher(line).find();
             boolean failed = failure != null && failure.matcher(line).find();
@@ -108,10 +114,6 @@ public class DevServerOutputTracker {
                         new Result(succeed, cumulativeOutput.toString()));
                 cumulativeOutput = new StringBuilder();
             }
-        }
-
-        private static Logger getLogger() {
-            return LoggerFactory.getLogger(Finder.class);
         }
 
     }
@@ -176,7 +178,7 @@ public class DevServerOutputTracker {
     public void find() {
         Thread finderThread = new Thread(finder);
         finderThread.setDaemon(true);
-        finderThread.setName("dev-server-output-reader");
+        finderThread.setName("dev-server-output");
         finderThread.start();
     }
 
@@ -193,6 +195,10 @@ public class DevServerOutputTracker {
     public boolean awaitFirstMatch(int timeoutInSeconds)
             throws InterruptedException {
         return monitor.await(timeoutInSeconds, TimeUnit.SECONDS);
+    }
+
+    private static Logger getLogger() {
+        return LoggerFactory.getLogger(DevServerOutputTracker.class);
     }
 
 }
