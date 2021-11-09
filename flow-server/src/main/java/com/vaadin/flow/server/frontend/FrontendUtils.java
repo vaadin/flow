7c19deec8bd7d10a129f9f210e61ae88fb8d0070
Artur Signell <artur@vaadin.com>
2021-11-09 11:05:50 +0200

    Fix merge issue

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendUtils.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendUtils.java
@@ -30,6 +30,7 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
