51012c93471448ef69816f773c918d5bf69cad5d
Manolo Carrasco <manolo@vaadin.com>
2021-11-11 15:57:50 +0100

    fix compilation

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/FrontendToolsTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/FrontendToolsTest.java
@@ -43,6 +43,7 @@ import org.apache.commons.io.FilenameUtils;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
