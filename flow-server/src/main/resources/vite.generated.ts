81bd546df0ee4eeebd61725694698a7456532f78
Johannes Eriksson <joheriks@vaadin.com>
2021-11-17 10:49:33 +0200

    feat: Add typescript checker plugin (#12344)
    Fixes #12114

--- flow-server/src/main/resources/vite.generated.ts
+++ flow-server/src/main/resources/vite.generated.ts
@@ -12,6 +12,7 @@ import settings from '#settingsImport#';
 import { UserConfigFn, defineConfig, HtmlTagDescriptor, mergeConfig } from 'vite';
 
 import brotli from 'rollup-plugin-brotli';
+import checker from 'vite-plugin-checker'
 
 const frontendFolder = path.resolve(__dirname, settings.frontendFolder);
 const themeFolder = path.resolve(frontendFolder, settings.themeFolder);
@@ -123,7 +124,10 @@ export const vaadinConfig: UserConfigFn = (env) => {
             return [vaadinScript];
           }
         }
-      }
+      },
+      checker({
+        typescript: true
+      })
     ]
   };
 };
