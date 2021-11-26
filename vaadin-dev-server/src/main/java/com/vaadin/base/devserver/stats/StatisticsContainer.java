986bb38553447898490e50476e336dc9428502ee
Artur Signell <artur@vaadin.com>
2021-11-26 14:32:30 +0200

    Fix sonar issues

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsContainer.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsContainer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2021 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.base.devserver.stats;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -13,8 +28,8 @@ public class StatisticsContainer {
     /**
      * Wraps the given json with this API.
      * 
-     * @param the
-     *            json to wrap
+     * @param json
+     *            the json to wrap
      */
     public StatisticsContainer(ObjectNode json) {
         this.json = json;
@@ -113,7 +128,8 @@ public class StatisticsContainer {
      * Returns the given field value as a string.
      * 
      * @param name
-     * @return
+     *            name of the field to get
+     * @return the value of the field as a string
      */
     public String getValue(String name) {
         return json.get(name).asText();
--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsSender.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsSender.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2021 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.base.devserver.stats;
 
 import java.io.IOException;
@@ -18,6 +33,9 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Handles sending of telemetry data.
+ */
 public class StatisticsSender {
 
     private static final String FAILED_TO_READ = "Failed to read ";
