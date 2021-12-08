475ac92fe43094733be82e9354560bce90cd777f
Mikael Grankvist <mikael.grankvist@vaadin.com>
2021-12-08 10:45:21 +0200

    Close in try instead

--- flow-server/src/main/java/com/vaadin/flow/server/communication/StreamReceiverHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/communication/StreamReceiverHandler.java
@@ -380,11 +380,11 @@ public class StreamReceiverHandler implements Serializable {
         } finally {
             session.unlock();
         }
-        try {
+        try (InputStream handledStream = inputStream) {
             // Store ui reference so we can do cleanup even if node is
             // detached in some event handler
             Pair<Boolean, UploadStatus> result = streamToReceiver(session,
-                    inputStream, streamReceiver, filename, mimeType,
+                    handledStream, streamReceiver, filename, mimeType,
                     contentLength);
             if (result.getFirst()) {
                 cleanStreamVariable(session, streamReceiver);
@@ -396,13 +396,6 @@ public class StreamReceiverHandler implements Serializable {
                 session.getErrorHandler().error(new ErrorEvent(e));
             } finally {
                 session.unlock();
-            }
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException ioe) {
-                throw new UncheckedIOException(
-                        "Closing content input stream failed.", ioe);
             }
         }
         return false;
