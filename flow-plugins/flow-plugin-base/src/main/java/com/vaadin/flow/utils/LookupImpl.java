c160b2d5df064ff5b49a3356f2e19b94f9b8469d
Mikael Grankvist <mikael.grankvist@vaadin.com>
2021-12-09 06:56:12 +0200

    Revert "refactor: Use maven vaadin plugin classloader as a parent for URLClassloader in ClassFinder (#12050) (#12151)"
    This reverts commit 86c496df4376a0172226ee9f905a5b402997dc45.

--- flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/utils/LookupImpl.java
+++ flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/utils/LookupImpl.java
@@ -48,19 +48,32 @@ public class LookupImpl implements Lookup {
 
     @Override
     public <T> List<T> lookupAll(Class<T> serviceClass) {
-        // only services declared in flow-server may be used here
-        assert serviceClass.getClassLoader()
-                .equals(LookupImpl.class.getClassLoader());
-        Set<Class<? extends T>> subTypes = classFinder
-                .getSubTypesOf(serviceClass);
+        Set<?> subTypes = classFinder
+                .getSubTypesOf(loadClassFromClassFindler(serviceClass));
         List<T> result = new ArrayList<>(subTypes.size());
-        for (Class<? extends T> clazz : subTypes) {
-            if (!ReflectTools.isInstantiableService(clazz)) {
-                continue;
+        try {
+            for (Object clazz : subTypes) {
+                if (!ReflectTools.isInstantiableService((Class<?>) clazz)) {
+                    continue;
+                }
+                Class<?> serviceType = serviceClass.getClassLoader()
+                        .loadClass(((Class<?>) clazz).getName());
+                result.add(serviceClass
+                        .cast(ReflectTools.createInstance(serviceType)));
             }
-            result.add(serviceClass.cast(ReflectTools.createInstance(clazz)));
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Could not find service class", e);
         }
         return result;
+    }
+
+    private Class<?> loadClassFromClassFindler(Class<?> clz) {
+        try {
+            return classFinder.loadClass(clz.getName());
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException(
+                    "Could not load " + clz.getName() + " class", e);
+        }
     }
 
 }
