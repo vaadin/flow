c03f5b5a8838bf64f2b5f728de5f321a1c0824b3
Artur Signell <artur@vaadin.com>
2021-12-22 15:07:35 +0200

    Make tests faster and more reliable

--- flow-tests/test-frontend/test-npm/src/test/java/com/vaadin/flow/mixedtest/ui/NpmUsedIT.java
+++ flow-tests/test-frontend/test-npm/src/test/java/com/vaadin/flow/mixedtest/ui/NpmUsedIT.java
@@ -2,32 +2,19 @@ package com.vaadin.flow.mixedtest.ui;
 
 import java.io.File;
 
-import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.flow.testutil.FileTestUtil;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-public class NpmUsedIT extends ChromeBrowserTest {
-
-    @Override
-    protected String getTestPath() {
-        return "/context-path/servlet-path/route-path";
-    }
+public class NpmUsedIT {
 
     @Test
     public void npmIsUsed() {
-        open(); // This is to wait for "npm install" to complete in dev mode
-        assertProjectFileExists("package-lock.json",
-                "npm should have been used to install frontend dependencies but no package-lock.json was found");
-        assertProjectFileNotExists("pnpm-lock.yaml",
-                "npm should have been used to install frontend dependencies but a pnpm-lock.yaml was found");
+        File testPackage = new File("node_modules/lit");
+        FileTestUtil.waitForFile(testPackage);
+        FileTestUtil.assertDirectory(testPackage,
+                "npm should have been used to install frontend dependencies but node_modules/lit is a symlink and only pnpm uses symlinks");
+
     }
 
-    private void assertProjectFileExists(String file, String errorMessage) {
-        Assert.assertTrue(errorMessage, new File(file).exists());
-    }
-
-    private void assertProjectFileNotExists(String file, String errorMessage) {
-        Assert.assertFalse(errorMessage, new File(file).exists());
-    }
 }
--- flow-tests/test-frontend/test-pnpm/src/test/java/com/vaadin/flow/mixedtest/ui/PnpmUsedIT.java
+++ flow-tests/test-frontend/test-pnpm/src/test/java/com/vaadin/flow/mixedtest/ui/PnpmUsedIT.java
@@ -2,32 +2,20 @@ package com.vaadin.flow.mixedtest.ui;
 
 import java.io.File;
 
-import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.flow.testutil.FileTestUtil;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-public class PnpmUsedIT extends ChromeBrowserTest {
-
-    @Override
-    protected String getTestPath() {
-        return "/context-path/servlet-path/route-path";
-    }
+public class PnpmUsedIT {
 
     @Test
     public void pnpmIsUsed() {
-        open();
-        assertProjectFileExists("pnpm-lock.yaml",
-                "npm should have been used to install frontend dependencies but no package-lock.json was found");
-        assertProjectFileNotExists("package-lock.json",
-                "npm should have been used to install frontend dependencies but a pnpm-lock.yaml was found");
+        File testPackage = new File("node_modules/lit");
+        FileTestUtil.waitForFile(testPackage);
+        FileTestUtil.assertSymlink(testPackage,
+                "pnpm should have been used to install frontend dependencies but "
+                        + testPackage.getAbsolutePath()
+                        + " is a not a symlink like it should be");
     }
 
-    private void assertProjectFileExists(String file, String errorMessage) {
-        Assert.assertTrue(errorMessage, new File(file).exists());
-    }
-
-    private void assertProjectFileNotExists(String file, String errorMessage) {
-        Assert.assertFalse(errorMessage, new File(file).exists());
-    }
 }
