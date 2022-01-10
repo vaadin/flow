460f1da4de1ac125611b851ca949bd119bfcb60c
Anton Platonov <anton@vaadin.com>
2022-01-10 10:59:10 +0200

    fix: missing import

--- fusion-endpoint/src/main/java/com/vaadin/fusion/generator/SchemaGenerator.java
+++ fusion-endpoint/src/main/java/com/vaadin/fusion/generator/SchemaGenerator.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
