b39a5041ba17f60e2347602d6e5a4ffeaf7a1ed8
Sergey Vinogradov <mr.vursen@gmail.com>
2022-01-21 14:51:07 +0300

    test: add tests

---
+++ flow-tests/test-frontend/vite-context-path-production/src/main/java/com/vaadin/viteapp/AppShell.java
@@ -0,0 +1,8 @@
+package com.vaadin.viteapp;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme(value = "vite-context-path")
+public class AppShell implements AppShellConfigurator {
+}
