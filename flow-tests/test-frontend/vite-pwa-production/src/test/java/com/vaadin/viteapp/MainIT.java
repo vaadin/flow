88efeb2deca5524661742b69cfd36b27d27d7e58
Sergey Vinogradov <mr.vursen@gmail.com>
2022-01-31 16:33:33 +0300

    test: add tests for a basic PWA app

---
+++ flow-tests/test-frontend/vite-pwa-production/src/test/java/com/vaadin/viteapp/MainIT.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2000-2022 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.viteapp;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+
+import com.vaadin.flow.testutil.ChromeDeviceTest;
+
+public class MainIT extends ChromeDeviceTest {
+    @Before
+    public void init() {
+        setCacheDisabled(true);
+    }
+
+    @After
+    public void teardown() {
+        setOfflineEnabled(false);
+    }
+
+    @Test
+    public void openHomePage_pageIsLoaded() {
+        openPage("/");
+
+        WebElement h1 = $("h1").first();
+        Assert.assertEquals(h1.getText(), "Home Page");
+
+        checkLogsForErrors(msg -> !msg.contains("Failed to load"));
+    }
+
+    @Test
+    public void openHomePage_setOfflineAndReload_pageIsLoadedFromCache() {
+        openPage("/");
+        setOfflineAndReload();
+
+        WebElement h1 = $("h1").first();
+        Assert.assertEquals(h1.getText(), "Home Page");
+
+        checkLogsForErrors(msg -> !msg.contains("Failed to load"));
+    }
+
+    @Test
+    public void openAboutPage_pageIsLoaded() {
+        openPage("/about");
+
+        WebElement h1 = $("h1").first();
+        Assert.assertEquals(h1.getText(), "About Page");
+
+        checkLogsForErrors(msg -> !msg.contains("Failed to load"));
+    }
+
+    @Test
+    public void openAboutPage_setOfflineAndReload_pageIsLoadedFromCache() {
+        openPage("/about");
+        setOfflineAndReload();
+
+        WebElement h1 = $("h1").first();
+        Assert.assertEquals(h1.getText(), "About Page");
+
+        checkLogsForErrors(msg -> !msg.contains("Failed to load"));
+    }
+
+    private void setOfflineAndReload() {
+        setOfflineEnabled(true);
+        executeScript("window.location.reload()");
+        waitUntil(webDriver -> ((JavascriptExecutor) driver)
+                .executeScript("return document.readyState")
+                .equals("complete"));
+    }
+
+    private void openPage(String url) {
+        getDriver().get(getRootURL() + url);
+        waitForServiceWorkerReady();
+    }
+}
