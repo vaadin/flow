11584de804e4bbce32669a07e04aae8dc3d9a127
caalador <mikael.grankvist@vaadin.com>
2022-02-09 13:05:25 +0200

    fix: clean default system error messages (#12951)
    Fixes #12939

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/LogoutIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/LogoutIT.java
@@ -32,7 +32,10 @@ public class LogoutIT extends ChromeBrowserTest {
         $(NativeButtonElement.class).first().click();
 
         // There can be "Session Expired" message because of heartbeat
-        checkLogsForErrors(msg -> msg.equals("Session Expired"));
+        // Strings defined in com.vaadin.flow.server.SystemMessages
+        checkLogsForErrors(
+                msg -> msg.contains("Session Expired") || msg.contains(
+                        "Take note of any unsaved data, and click here or press ESC key to continue."));
 
         // There can't be any error dialog
         Assert.assertFalse(isElementPresent(By.className("v-system-error")));
