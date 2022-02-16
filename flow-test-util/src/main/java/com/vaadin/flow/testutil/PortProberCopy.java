5cd96e62b55652bf107c5f6590195dbce70f7498
Artur Signell <artur@vaadin.com>
2022-02-16 10:31:34 +0200

    Try without reuseaddress

--- flow-test-util/src/main/java/com/vaadin/flow/testutil/PortProberCopy.java
+++ flow-test-util/src/main/java/com/vaadin/flow/testutil/PortProberCopy.java
@@ -101,7 +101,7 @@ public class PortProberCopy {
 
   private static int checkPortIsFree(int port) {
     try (ServerSocket socket = new ServerSocket()) {
-      socket.setReuseAddress(true);
+    //   socket.setReuseAddress(true);
       socket.bind(new InetSocketAddress("localhost", port));
       return socket.getLocalPort();
     } catch (IOException e) {
