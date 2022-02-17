1a859755f98683b9de21b462bc3e62e65f72977a
Artur <artur@vaadin.com>
2022-02-17 09:38:08 +0200

    chore: Upgrade to latest lit (#13050)

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -380,7 +380,7 @@ public abstract class NodeUpdater implements FallibleCommand {
 
         defaults.put("@polymer/polymer", POLYMER_VERSION);
 
-        defaults.put("lit", "2.0.0");
+        defaults.put("lit", "2.1.4");
 
         // Constructable style sheets is only implemented for chrome,
         // polyfill needed for FireFox et.al. at the moment
