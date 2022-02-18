4aa214a636164bfe9d423df314f150f572f6e306
Artur Signell <artur@vaadin.com>
2022-02-18 10:54:53 +0200

    ...

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsConstants.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsConstants.java
@@ -31,6 +31,7 @@ public class StatisticsConstants {
     public static final String EVENT_LIVE_RELOAD = "liveReload";
     public static final String EVENT_DEV_SERVER_START_PREFIX = "startDevserver";
     public static final String EVENT_PACKAGEMANAGER_INSTALL_TIME_PREFIX = "packageManagerInstall";
+    public static final String EVENT_PACKAGEMANAGER_CLEANUP_TIME_PREFIX = "packageManagerCleanup";
 
     /*
      * Name of the default JSON file containing all the statistics.
