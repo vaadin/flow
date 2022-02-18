40163228cc1e3e9e4e30f59f72d96a227b3105d0
Johannes Eriksson <joheriks@vaadin.com>
2022-02-18 13:12:12 +0200

    fix: save platform version in node_modules (#13059)
    To decide whether the platform has been updated, compare
    the version of vaadin-core in package.json with the platform
    published version. Was previously using vaadin-shrinkwrap,
    which is removed in V23.

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeTestComponents.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeTestComponents.java
@@ -205,10 +205,6 @@ public class NodeTestComponents extends NodeUpdateTestUtil {
     public static class ExtraImport {
     }
 
-    @NpmPackage(value = "@vaadin/vaadin-shrinkwrap", version = "1.2.3")
-    public static class VaadinShrinkWrap extends Component {
-    }
-
     @JavaScript("javascript/a.js")
     @JavaScript("javascript/b.js")
     @JavaScript("javascript/c.js")
