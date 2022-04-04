f264f260d83fbdd2f4e7e59813601b590b7c3e71
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-04-04 09:55:53 +0300

    Fix formatting

--- flow-server/src/test/java/com/vaadin/flow/server/auth/ViewAccessCheckerTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/auth/ViewAccessCheckerTest.java
@@ -346,7 +346,7 @@ public class ViewAccessCheckerTest {
                         + "@PermitAll, @RolesAllowed.",
                 result.getRerouteErrorMessage());
     }
-    
+
     @Test
     public void openingNoAnnotationViewDoesNotShowAnyReasonAndHintInProductionMode() {
         Result result = checkAccess(NoAnnotationView.class, User.NORMAL_USER,
@@ -354,7 +354,7 @@ public class ViewAccessCheckerTest {
         Assert.assertEquals(NotFoundException.class, result.getRerouteError());
         Assert.assertEquals("", result.getRerouteErrorMessage());
     }
-    
+
     @Test
     public void redirectWhenNoLoginSet() throws Exception {
         resetLoginView();
