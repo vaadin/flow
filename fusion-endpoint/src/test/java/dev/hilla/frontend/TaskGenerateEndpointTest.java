1cb1b87888628c6b11e030f79de2f93c52a88abb
Artur Signell <artur@vaadin.com>
2022-04-05 11:04:34 +0300

    format

--- fusion-endpoint/src/test/java/dev/hilla/frontend/TaskGenerateEndpointTest.java
+++ fusion-endpoint/src/test/java/dev/hilla/frontend/TaskGenerateEndpointTest.java
@@ -86,9 +86,9 @@ public class TaskGenerateEndpointTest {
 
         String outputEndpoinTs1 = FileUtils.readFileToString(ts1, "UTF-8");
         String outputEndpoinTs2 = FileUtils.readFileToString(ts2, "UTF-8");
-        assertThat(outputEndpoinTs1, containsString(
-                "import client from '../connect-client'"));
-        assertThat(outputEndpoinTs2, containsString(
-                "import client from '../connect-client'"));
+        assertThat(outputEndpoinTs1,
+                containsString("import client from '../connect-client'"));
+        assertThat(outputEndpoinTs2,
+                containsString("import client from '../connect-client'"));
     }
 }
