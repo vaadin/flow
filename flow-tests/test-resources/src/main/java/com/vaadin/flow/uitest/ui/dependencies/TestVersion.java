3434ff3f9289d6aea34933032ff2ff13e83d4b94
Artur Signell <artur@vaadin.com>
2022-04-13 09:05:45 +0300

    test: Upgrade test version to 23.0.4

--- flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
+++ flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
@@ -2,7 +2,7 @@ package com.vaadin.flow.uitest.ui.dependencies;
 
 public class TestVersion {
 
-    public static final String VAADIN = "22.0.0-alpha9";
+    public static final String VAADIN = "23.0.4";
     public static final String FONTAWESOME = "5.15.1";
 
 }
