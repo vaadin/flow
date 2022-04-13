09fa08f1736ca6a1322205013e2e53dc15dd40f6
Mikael Grankvist <mikael.grankvist@vaadin.com>
2022-04-13 13:38:23 +0300

    Update to 23.0.5 to get the same versions in lock file

--- flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
+++ flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
@@ -2,7 +2,7 @@ package com.vaadin.flow.uitest.ui.dependencies;
 
 public class TestVersion {
 
-    public static final String VAADIN = "23.0.4";
+    public static final String VAADIN = "23.0.5";
     public static final String FONTAWESOME = "5.15.1";
 
 }
