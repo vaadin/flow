3991fb5b9b71494ba327554122f898627d06aebc
Mikael Grankvist <mikael.grankvist@vaadin.com>
2022-04-14 11:55:43 +0300

    fix: reusable themes with vite
    Fix resolution of reusable themes
    when using vite
    
    Fixes #13538

--- flow-server/src/main/resources/vite.generated.ts
+++ flow-server/src/main/resources/vite.generated.ts
@@ -18,12 +18,13 @@ import brotli from 'rollup-plugin-brotli';
 import replace from '@rollup/plugin-replace';
 import checker from 'vite-plugin-checker';
 
-const appShellUrl = '.';
+const appShellUrl = '.'
 
 const frontendFolder = path.resolve(__dirname, settings.frontendFolder);
 const themeFolder = path.resolve(frontendFolder, settings.themeFolder);
 const frontendBundleFolder = path.resolve(__dirname, settings.frontendBundleOutput);
 const addonFrontendFolder = path.resolve(__dirname, settings.addonFrontendFolder);
+const themeResourceFolder = path.resolve(__dirname, settings.themeResourceFolder);
 
 const projectStaticAssetsFolders = [
   path.resolve(__dirname, 'src', 'main', 'resources', 'META-INF', 'resources'),
@@ -38,7 +39,7 @@ const themeOptions = {
   devMode: false,
   // The following matches folder 'target/flow-frontend/themes/'
   // (not 'frontend/themes') for theme in JAR that is copied there
-  themeResourceFolder: path.resolve(__dirname, settings.themeResourceFolder),
+  themeResourceFolder: path.resolve(themeResourceFolder, settings.themeFolder),
   themeProjectFolders: themeProjectFolders,
   projectStaticAssetsOutputFolder: path.resolve(__dirname, settings.staticOutput),
   frontendGeneratedFolder: path.resolve(frontendFolder, settings.generatedFolder)
@@ -327,10 +328,16 @@ export const vaadinConfig: UserConfigFn = (env) => {
     root: 'frontend',
     base: '',
     resolve: {
-      alias: {
-        themes: themeFolder,
-        Frontend: frontendFolder
-      },
+      alias: [
+        { find: 'themes', replacement: (importee: string) => {
+            if(existsSync(path.resolve(themeResourceFolder, importee))){
+              return path.resolve(themeResourceFolder, settings.themeFolder);
+            }
+            return themeFolder;
+          }
+        },
+        { find: 'Frontend', replacement: frontendFolder }
+      ],
       preserveSymlinks: true
     },
     define: {
