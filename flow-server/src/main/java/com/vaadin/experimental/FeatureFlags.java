f34c5a8fbacaba172d2ac7afba37937867b3e364
Artur <artur@vaadin.com>
2022-04-20 13:59:59 +0300

    feat: Hilla push implementation behind a feature flag (#13350)
    * feat: Hilla push implementation behind a feature flag
    
    This adds support for endpoints methods to return a Flux<T> which will be streamed to the client side to the subscribers that have registered using TheEndpoint.theMethod().onNext(value => ...);
    
    The implementation is based on https://socket.io/ but this is not considered to be part of the public API. We should be free to change the underlying implementation if needed.
    
    Fixes https://github.com/vaadin/hilla/issues/56
    
    * Fix lift
    
    * Fix tests
    
    * Use correct JSON library
    
    * Upgrade engine io to fix long polling send issue
    
    * Move JS code to @hilla/frontend
    
    * Remove unnecessary boolean
    
    * format
    
    * format and remove unnecessary changes
    
    * Unit test for TS generation
    
    * format
    
    * Ensure anonymous user is not interpreted as logged in
    
    * Flux push IT
    
    * TS ignore Subscription import as it is mostly unused
    
    * Clean up engine io server and project reactor threads on shutdown
    
    * Remove commented code
    
    * Upgrade Spring Security to 5.6.3
    
    * Fix cleanup when not using Spring Boot
    
    * fix merge issue

--- flow-server/src/main/java/com/vaadin/experimental/FeatureFlags.java
+++ flow-server/src/main/java/com/vaadin/experimental/FeatureFlags.java
@@ -64,6 +64,9 @@ public class FeatureFlags implements Serializable {
             "Spreadsheet component (Pro)", "spreadsheetComponent",
             "https://github.com/vaadin/platform/issues/2027", true,
             "com.vaadin.flow.component.spreadsheet.Spreadsheet");
+    public static final Feature HILLA_PUSH = new Feature(
+            "Push support in Hilla", "hillaPush",
+            "https://github.com/vaadin/hilla/issues/56", true, null);
     private List<Feature> features = new ArrayList<>();
 
     File propertiesFolder = null;
@@ -84,6 +87,7 @@ public class FeatureFlags implements Serializable {
         features.add(new Feature(VITE));
         features.add(new Feature(MAP_COMPONENT));
         features.add(new Feature(SPREADSHEET_COMPONENT));
+        features.add(new Feature(HILLA_PUSH));
         loadProperties();
     }
 
@@ -324,6 +328,13 @@ public class FeatureFlags implements Serializable {
 
     private boolean isDevelopmentMode() {
         return configuration != null && !configuration.isProductionMode();
+    }
+
+    public String getEnableHelperMessage(Feature feature) {
+        return feature.getTitle()
+                + " is not enabled. Enable it in the debug window or by adding "
+                + getPropertyName(feature.getId())
+                + "=true to src/main/resources/" + PROPERTIES_FILENAME;
     }
 
     private Logger getLogger() {
