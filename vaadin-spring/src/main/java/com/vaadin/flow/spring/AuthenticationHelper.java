f50d1ce2895c9edb96293a904a3886397524df30
Artur Signell <artur@vaadin.com>
2022-05-04 09:15:27 +0300

    feat: Support view access checking in background threads with Spring
    Requires the spring context to be properly set up for the background thread.

---
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/AuthenticationHelper.java
@@ -0,0 +1,42 @@
+package com.vaadin.flow.spring;
+
+import java.util.function.Function;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * Helpers for authentication related tasks.
+ */
+public class AuthenticationHelper {
+
+    /**
+     * Gets the authenticated user from the Spring SecurityContextHolder.
+     *
+     * @return the authenticated user or {@code null}
+     */
+    public static Authentication getSecurityHolderAuthentication() {
+        Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
+
+        return authentication;
+
+    }
+
+    /**
+     * Gets a function for checking if the authenticated user from the Spring
+     * SecurityContextHolder is in a given role.
+     *
+     * @return a function for checking if the given user has the given role
+     */
+    public static Function<String, Boolean> getSecurityHolderRoleChecker() {
+        return role -> getSecurityHolderAuthentication().getAuthorities()
+                .stream().anyMatch(grantedAuthority -> grantedAuthority
+                        .getAuthority().equals("ROLE_" + role));
+    }
+
+}
