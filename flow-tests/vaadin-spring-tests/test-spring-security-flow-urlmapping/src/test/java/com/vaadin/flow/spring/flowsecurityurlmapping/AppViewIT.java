fbb3ff92fc74771687fc8e7dbe01cbd1503ef8ad
Artur Signell <artur@vaadin.com>
2022-05-18 18:24:26 +0300

    test: Use 'urlmapping' instead of 'vaadin' for clarity
    Also reduce log spam from serialization errors

--- flow-tests/vaadin-spring-tests/test-spring-security-flow-urlmapping/src/test/java/com/vaadin/flow/spring/flowsecurityurlmapping/AppViewIT.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow-urlmapping/src/test/java/com/vaadin/flow/spring/flowsecurityurlmapping/AppViewIT.java
@@ -3,6 +3,6 @@ package com.vaadin.flow.spring.flowsecurityurlmapping;
 public class AppViewIT extends com.vaadin.flow.spring.flowsecurity.AppViewIT {
     @Override
     protected String getUrlMappingBasePath() {
-        return "/vaadin";
+        return "/urlmapping";
     }
 }
