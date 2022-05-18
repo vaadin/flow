30d5c6caa1f52e474dc1e7fe1adb2a17bdb1036e
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2022-05-18 16:47:34 +0300

    Change log level from info to debug

--- flow-server/src/main/java/com/vaadin/flow/server/communication/rpc/AbstractRpcInvocationHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/communication/rpc/AbstractRpcInvocationHandler.java
@@ -48,12 +48,12 @@ public abstract class AbstractRpcInvocationHandler
         StateNode node = ui.getInternals().getStateTree()
                 .getNodeById(getNodeId(invocationJson));
         if (node == null) {
-            getLogger().info("Ignoring RPC for non-existent node: {}",
+            getLogger().debug("Ignoring RPC for non-existent node: {}",
                     getNodeId(invocationJson));
             return Optional.empty();
         }
         if (!node.isAttached()) {
-            getLogger().info("Ignoring RPC for detached node: {}",
+            getLogger().debug("Ignoring RPC for detached node: {}",
                     getNodeId(invocationJson));
             return Optional.empty();
         }
