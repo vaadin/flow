f39326db7fdb32b6635080c9eceb205910f5de88
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-05-20 15:35:13 +0300

    Add tests

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/SetTitleAfterNavigationEventView.java
@@ -0,0 +1,33 @@
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.HasDynamicTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+import org.slf4j.LoggerFactory;
+
+@Route(value = "com.vaadin.flow.uitest.ui.SetTitleAfterNavigationEventView", layout = ViewTestLayout.class)
+public class SetTitleAfterNavigationEventView extends AbstractDivView
+        implements HasDynamicTitle, AfterNavigationObserver {
+
+    private String title = "my-initial-title";
+
+    @Override
+    public String getPageTitle() {
+        LoggerFactory
+                .getLogger(SetTitleAfterNavigationEventView.class.getName())
+                .debug("HasDynamicTitle.getPageTitle() called. The current title value is = "
+                        + title);
+        return title;
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
+        LoggerFactory
+                .getLogger(SetTitleAfterNavigationEventView.class.getName())
+                .debug("AfterNavigationEvent listener called. The current title value is = "
+                        + title);
+        title = "my-changed-title-after-AfterNavigationEvent";
+    }
+}
