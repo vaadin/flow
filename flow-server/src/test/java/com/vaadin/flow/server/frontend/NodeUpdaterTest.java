24d0e240b820eaab37375378db85a6a36e3d287c
Mikhail Shabarov <mikhail@vaadin.com>
2022-06-14 10:16:53 +0300

    test: Update webpack version in test

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeUpdaterTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeUpdaterTest.java
@@ -170,7 +170,7 @@ public class NodeUpdaterTest {
         Assert.assertEquals("^2.2.10",
                 packageJson.getObject(NodeUpdater.DEPENDENCIES)
                         .getString("@webcomponents/webcomponentsjs"));
-        Assert.assertEquals("4.42.0", packageJson
+        Assert.assertEquals("4.46.0", packageJson
                 .getObject(NodeUpdater.DEV_DEPENDENCIES).getString("webpack"));
     }
 
