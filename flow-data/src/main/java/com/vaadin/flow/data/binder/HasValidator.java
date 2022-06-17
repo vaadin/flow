7bf397b1fac7b8cae12eafb55a1b1c6d24511c0a
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2022-06-17 09:06:47 +0300

    Apply suggestions from code review

--- flow-data/src/main/java/com/vaadin/flow/data/binder/HasValidator.java
+++ flow-data/src/main/java/com/vaadin/flow/data/binder/HasValidator.java
@@ -121,7 +121,7 @@ public interface HasValidator<V> extends Serializable {
      *
      * @see com.vaadin.flow.data.binder.Binder.BindingBuilderImpl#bind(ValueProvider,
      *      Setter)
-     * @since 23.2
+     * @since 2.7
      *
      * @return Registration of the added listener.
      */
