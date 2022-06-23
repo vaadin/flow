625a6249d7cf49cd3f015e3d01916214a46e0933
Matti Tahvonen <matti@vaadin.com>
2022-06-23 13:12:07 +0300

    Added a test UI

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/ShortcutsWithLazyValueChangeView.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2000-2022 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyModifier;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Input;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+
+@Route(layout = ViewTestLayout.class)
+public class ShortcutsWithLazyValueChangeView extends Div {
+
+
+    public ShortcutsWithLazyValueChangeView() {
+    	
+        Input input = new Input();
+        input.setId("input");
+
+        NativeButton button = new NativeButton("Report value");
+        button.setId("button");
+
+        Paragraph value = new Paragraph();
+        value.setId("value");
+
+        add(input, button, value);
+
+        input.setValueChangeMode(ValueChangeMode.LAZY);
+        // make this really big to make testing easier
+        input.setValueChangeTimeout(3000); 
+
+        // clickShortcutWorks
+        button.setText("Button triggered by CTRL + S");
+        button.addClickShortcut(Key.KEY_S, KeyModifier.CONTROL);
+        button.addClickListener(e -> value.setText(input.getValue()));
+
+    }
+}
