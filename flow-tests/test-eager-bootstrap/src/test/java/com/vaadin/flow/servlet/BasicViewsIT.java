86dd7725535b3a8816a6a23b74966bade8916cdf
Artur Signell <artur@vaadin.com>
2022-06-23 09:25:17 +0300

    Format

--- flow-tests/test-eager-bootstrap/src/test/java/com/vaadin/flow/servlet/BasicViewsIT.java
+++ flow-tests/test-eager-bootstrap/src/test/java/com/vaadin/flow/servlet/BasicViewsIT.java
@@ -26,20 +26,23 @@ public class BasicViewsIT extends ChromeBrowserTest {
     @Test
     public void rootViewShown() throws Exception {
         getDriver().get(getRootURL() + "/");
-        Assert.assertEquals("This is the root view", $("*").id("view").getText());
+        Assert.assertEquals("This is the root view",
+                $("*").id("view").getText());
     }
 
     @Test
     public void helloViewShown() throws Exception {
         getDriver().get(getRootURL() + "/hello");
-        Assert.assertEquals("This is the Hello view", $("*").id("view").getText());
+        Assert.assertEquals("This is the Hello view",
+                $("*").id("view").getText());
     }
 
     @Test
     public void invalidViewShowsNotFound() throws Exception {
         getDriver().get(getRootURL() + "/nonexistant");
 
-        Assert.assertTrue(getDriver().getPageSource().contains("Could not navigate to 'nonexistant'"));
+        Assert.assertTrue(getDriver().getPageSource()
+                .contains("Could not navigate to 'nonexistant'"));
     }
 
 }
