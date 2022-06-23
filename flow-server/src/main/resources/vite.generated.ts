3227c7117bcc0f13e10a38d2549bcac78f0f131a
Artur <artur@vaadin.com>
2022-06-23 15:12:34 +0300

    fix: Set preventAssignment to true to avoid warnings (#14045)
    Fixes #14044

--- flow-server/src/main/resources/vite.generated.ts
+++ flow-server/src/main/resources/vite.generated.ts
@@ -75,8 +75,11 @@ function buildSWPlugin(): PluginOption {
       });
       rollupPlugins.push(
         replace({
-          'process.env.NODE_ENV': JSON.stringify(config.mode),
-          ...config.define
+          values: {
+            'process.env.NODE_ENV': JSON.stringify(config.mode),
+            ...config.define
+          },
+          preventAssignment: true
         })
       );
 
