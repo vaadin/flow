55d58e4722d93b0f89d39a4faff28b3d61153ecc
marcin <106965834+MarcinVaadin@users.noreply.github.com>
2022-08-02 09:11:00 +0200

    chore: removed spring tests from flow

--- flow-tests/vaadin-spring-tests/test-spring-security-flow-urlmapping/src/test/java/com/vaadin/flow/spring/flowsecurityurlmapping/AppViewIT.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.flow.spring.flowsecurityurlmapping;
-
-public class AppViewIT extends com.vaadin.flow.spring.flowsecurity.AppViewIT {
-    @Override
-    protected String getUrlMappingBasePath() {
-        return "/vaadin";
-    }
-}
