7a8ff93a2f7dc5e30580f39b5a9837ac0f10d863
Artur Signell <artur@vaadin.com>
2022-08-12 11:41:09 +0300

    chore: Upgrade to Lit 2.3.0

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -393,7 +393,7 @@ public abstract class NodeUpdater implements FallibleCommand {
 
         defaults.put("@polymer/polymer", POLYMER_VERSION);
 
-        defaults.put("lit", "2.2.7");
+        defaults.put("lit", "2.3.0");
 
         // Constructable style sheets is only implemented for chrome,
         // polyfill needed for FireFox et.al. at the moment
