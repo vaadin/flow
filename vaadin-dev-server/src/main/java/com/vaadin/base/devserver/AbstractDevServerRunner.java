b74914e66e55e9fc9e1871f61a58358a9a4d4247
Artur <artur@vaadin.com>
2022-08-29 13:03:49 +0300

    fix: Never cache the "waiting for frontend build" page (#14436)
    Fixes #14382

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/AbstractDevServerRunner.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/AbstractDevServerRunner.java
@@ -638,6 +638,7 @@ public abstract class AbstractDevServerRunner implements DevModeHandler {
                 // Avoid creating a UI that is thrown away for polling requests
                 response.setContentType("text/html;charset=utf-8");
                 response.getWriter().write("Ready");
+                response.setHeader("Cache-Control", "no-cache");
                 return true;
             }
             return false;
@@ -653,6 +654,7 @@ public abstract class AbstractDevServerRunner implements DevModeHandler {
             }
             response.setContentType("text/html;charset=utf-8");
             response.setHeader("X-DevModePending", "true");
+            response.setHeader("Cache-Control", "no-cache");
             return true;
         }
     }
