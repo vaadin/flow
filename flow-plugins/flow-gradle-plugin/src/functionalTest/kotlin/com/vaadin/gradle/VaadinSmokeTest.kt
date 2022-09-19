6826ad08f77eb9257da5117d9a32fc3c6858ed88
Marco Collovati <marco@vaadin.com>
2022-09-19 13:55:30 +0200

    test: fixed vaadin-themable-mixin version

--- flow-plugins/flow-gradle-plugin/src/functionalTest/kotlin/com/vaadin/gradle/VaadinSmokeTest.kt
+++ flow-plugins/flow-gradle-plugin/src/functionalTest/kotlin/com/vaadin/gradle/VaadinSmokeTest.kt
@@ -319,7 +319,7 @@ class VaadinSmokeTest : AbstractGradleTest() {
 
             @Route("")
             @CssImport("./mystyle.css")
-            @NpmPackage(value = "@vaadin/vaadin-themable-mixin", version = "23.3.0-alpha1")
+            @NpmPackage(value = "@vaadin/vaadin-themable-mixin", version = "23.2.0-alpha5")
             public class MainView extends Div {
 
                 public MainView() {
