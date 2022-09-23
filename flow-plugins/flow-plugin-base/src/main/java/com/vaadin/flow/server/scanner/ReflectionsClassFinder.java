ba47b9b0ae4a80a9d0e6882a99de8f0d2ff6b22a
Marco Collovati <marco@vaadin.com>
2022-09-23 11:26:05 +0200

    fix: sort classes by name in ReflectionsClassFinder (#14556)
    Class scan in production build may differ from development mode because
    DefaultClassFinder and ReflectionsClassFinder may provide classes in different
    order. As a result, when there are multiple NpmPackage annotations for
    the same package wit different version, the final version used in
    package.json may be different in dev mode and production build.
    
    This change makes ReflectionsClassFinder sort classes by name,
    as DefaultClassFinder does in its constructor.
    
    Ref #14381

--- flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/server/scanner/ReflectionsClassFinder.java
+++ flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/server/scanner/ReflectionsClassFinder.java
@@ -21,8 +21,10 @@ import java.lang.reflect.AnnotatedElement;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.reflections.Reflections;
 import org.reflections.util.ConfigurationBuilder;
@@ -64,7 +66,7 @@ public class ReflectionsClassFinder implements ClassFinder {
         Set<Class<?>> classes = new HashSet<>();
         classes.addAll(reflections.getTypesAnnotatedWith(clazz, true));
         classes.addAll(getAnnotatedByRepeatedAnnotation(clazz));
-        return classes;
+        return sortedByClassName(classes);
 
     }
 
@@ -92,11 +94,17 @@ public class ReflectionsClassFinder implements ClassFinder {
 
     @Override
     public <T> Set<Class<? extends T>> getSubTypesOf(Class<T> type) {
-        return reflections.getSubTypesOf(type);
+        return sortedByClassName(reflections.getSubTypesOf(type));
     }
 
     @Override
     public ClassLoader getClassLoader() {
         return classLoader;
+    }
+
+    private <T> Set<Class<? extends T>> sortedByClassName(
+            Set<Class<? extends T>> source) {
+        return source.stream().sorted(Comparator.comparing(Class::getName))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
