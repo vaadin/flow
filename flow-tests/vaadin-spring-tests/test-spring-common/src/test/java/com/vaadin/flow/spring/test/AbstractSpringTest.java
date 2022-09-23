21297275c0e82327751879a7f3b242ad2573f505
Artur Signell <artur@vaadin.com>
2022-09-23 10:59:45 +0300

    test: Test Spring Boot behind a reverse proxy

--- flow-tests/vaadin-spring-tests/test-spring-common/src/test/java/com/vaadin/flow/spring/test/AbstractSpringTest.java
+++ flow-tests/vaadin-spring-tests/test-spring-common/src/test/java/com/vaadin/flow/spring/test/AbstractSpringTest.java
@@ -8,10 +8,22 @@ import com.vaadin.flow.testutil.ChromeBrowserTest;
 
 public abstract class AbstractSpringTest extends ChromeBrowserTest {
 
+    private Properties properties;
+
     @Override
     protected String getTestURL(String... parameters) {
         return getTestURL(getRootURL(), getContextPath() + getTestPath(),
                 parameters);
+    }
+
+    @Override
+    protected int getDeploymentPort() {
+        String proxyPort = getProperty("proxy.port");
+        if (proxyPort != null) {
+            return Integer.parseInt(proxyPort);
+        }
+
+        return super.getDeploymentPort();
     }
 
     protected String getContextRootURL() {
@@ -19,22 +31,35 @@ public abstract class AbstractSpringTest extends ChromeBrowserTest {
     }
 
     protected String getContextPath() {
-        Properties p = new Properties();
-        try {
-            InputStream res = getClass()
-                    .getResourceAsStream("/application.properties");
-            if (res != null) {
-                p.load(res);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        String proxyPublicPath = getProperty("proxy.path");
+        if (proxyPublicPath != null) {
+            return proxyPublicPath;
         }
-        String contextPath = p.getProperty("server.servlet.contextPath");
+
+        String contextPath = getProperty("server.servlet.contextPath");
         if (contextPath != null) {
             return contextPath;
-        } else {
-            return "";
         }
+
+        return "";
+    }
+
+    private String getProperty(String key) {
+        if (properties == null) {
+
+            properties = new Properties();
+            try {
+                InputStream res = getClass()
+                        .getResourceAsStream("/application.properties");
+                if (res != null) {
+                    properties.load(res);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return properties.getProperty(key);
+
     }
 
 }
--- scripts/computeMatrix.js
+++ scripts/computeMatrix.js
@@ -62,6 +62,7 @@ const moduleWeights = {
   'flow-tests/vaadin-spring-tests/test-spring-boot-scan': { pos: 4, weight: 3 },
   'flow-tests/vaadin-spring-tests/test-spring-war': { pos: 4, weight: 3 },
   'flow-tests/vaadin-spring-tests/test-spring-boot-contextpath': { pos: 4, weight: 3 },
+  'flow-tests/vaadin-spring-tests/test-spring-boot-reverseproxy': { pos: 4, weight: 3 },
   'flow-tests/vaadin-spring-tests/test-spring-white-list': { pos: 4, weight: 2 },
   'flow-tests/vaadin-spring-tests/test-spring-security-flow': { pos: 5, weight: 5 },
   'flow-tests/vaadin-spring-tests/test-spring-security-flow-contextpath': { pos: 5, weight: 4 },
