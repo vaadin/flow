1fcaa3ab570ca2e8350c010e7a25b49acdf6159a
marcin <106965834+MarcinVaadin@users.noreply.github.com>
2022-09-27 16:00:01 +0200

    test: added spring test for custom web socket server

---
+++ flow-tests/vaadin-spring-tests/test-spring-common/src/test/java/com/vaadin/flow/spring/test/CustomWebSocketIT.java
@@ -0,0 +1,71 @@
+package com.vaadin.flow.spring.test;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SpringBootOnly.class)
+public class CustomWebSocketIT extends AbstractSpringTest {
+
+    @ClientEndpoint
+    public class CustomWebSocketEndpoint {
+
+        private final CountDownLatch closureLatch = new CountDownLatch(1);
+
+        private String message;
+
+        @OnMessage
+        public void onMessage(Session session, String message)
+                throws IOException {
+            this.message = message;
+            session.close();
+        }
+
+        @OnClose
+        public void onClose(CloseReason reason) {
+            closureLatch.countDown();
+        }
+
+        public String waitForMessage() throws InterruptedException {
+            closureLatch.await();
+            return message;
+        }
+    }
+
+    @Test
+    public void properWebsocketResponseIsReceived() throws Exception {
+        WebSocketContainer container = ContainerProvider
+                .getWebSocketContainer();
+
+        String testUrl = getTestURL().replace("http://", "ws://");
+        URI uri = URI.create(testUrl + CustomWebSocket.WEBSOCKET_URL);
+
+        try {
+            CustomWebSocketEndpoint clientEndpoint = new CustomWebSocketEndpoint();
+            Session session = container.connectToServer(clientEndpoint, uri);
+            session.getBasicRemote().sendText("Hello");
+            Assert.assertEquals(CustomWebSocket.WEBSOCKET_RESPONSE_TEXT,
+                    clientEndpoint.waitForMessage());
+            session.close();
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    @Override
+    protected String getTestPath() {
+        return "";
+    }
+
+}
