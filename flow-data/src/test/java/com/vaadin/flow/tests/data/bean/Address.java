16dae667b0c65d5f493d35ee7aa9596bf36ae4fc
Artur Signell <artur@vaadin.com>
2022-10-13 12:10:15 +0300

    javax.validation > jakarta.validation

--- flow-data/src/test/java/com/vaadin/flow/tests/data/bean/Address.java
+++ flow-data/src/test/java/com/vaadin/flow/tests/data/bean/Address.java
@@ -2,9 +2,9 @@ package com.vaadin.flow.tests.data.bean;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @SuppressWarnings("serial")
 public class Address implements Serializable {
