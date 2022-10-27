f9feb324f34da6bdfdce8862210a1f088241ae7d
Mikael Grankvist <mikael.grankvist@vaadin.com>
2022-10-27 14:15:37 +0300

    Use strip-comments npm module to not need own upkeep.

--- flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
+++ flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
@@ -34,69 +34,6 @@ const stylesCssFile = 'styles.css';
 const headerImport = `import 'construct-style-sheets-polyfill';
 `;
 
-// Strip comments is made according to StringUtil.removeComments that is tested in StringUtilTest
-// No line comments accepted checked as css only supports block comments!
-const stripCommentsMethod = `
-const State = Object.freeze({
-  NORMAL: Symbol("normal"),
-  IN_BLOCK_COMMENT: Symbol("block_comment"),
-  IN_STRING: Symbol("string"),
-  IN_STRING_APOSTROPHE: Symbol("string_apostrophe")
-});
-
-const removeComments = (code) => {
-  var state = State.NORMAL;
-  var result = "";
-  for (var i = 0; i < code.length; i++) {
-    var character = code.charAt(i);
-    switch (state) {
-      case State.NORMAL:
-        if(character == "/" && (i+1) < code.length) {
-          i++;
-          var nextCharacter = code.charAt(i);
-          if(nextCharacter == "*") {
-            state = State.IN_BLOCK_COMMENT;
-          } else {
-            result += character + nextCharacter;
-          }
-        } else {
-          result += character;
-          if(character == "\\"") {
-            state = State.IN_STRING;
-          } else if(character == "'") {
-            state = State.IN_STRING_APOSTROPHE;
-          }
-        }
-        break;
-      case State.IN_STRING:
-        result += character;
-        if(character == "\\"") {
-          state = State.NORMAL;
-        } else if(character == "\\\\" && (i+1) < code.length) {
-          result += code.charAt(i+1);
-          i++;
-        }
-        break;
-      case State.IN_STRING_APOSTROPHE:
-        result += character;
-        if(character == "'") {
-          state = State.NORMAL;
-        } else if(character == "\\\\" && (i+1) < code.length) {
-          result += code.charAt(i+1);
-          i++;
-        }
-        break;
-      case State.IN_BLOCK_COMMENT:
-        if(character == "*" && (i+1) < code.length && code.charAt(i+1) == "/") {
-          i++;
-          state = State.NORMAL;
-        }
-    }
-  }
-  return result;
-};
-`;
-
 const createLinkReferences = `
 const createLinkReferences = (css, target) => {
   // Unresolved urls are written as '@import url(text);' or '@import "text";' to the css
@@ -113,7 +50,7 @@ const createLinkReferences = (css, target) => {
   // Only cleanup if comment exist
   if(/\\/\\*(.|[\\r\\n])*?\\*\\//gm.exec(css) != null) {
     // clean up comments
-    css = removeComments(css);
+    css = strip(css, { line: false });
   }
   
   var match;
@@ -184,10 +121,10 @@ function generateThemeFile(themeFolder, themeName, themeProperties, productionMo
   }
 
   if (themeProperties.parent) {
-    themeFile += `import {applyTheme as applyBaseTheme} from './theme-${themeProperties.parent}.generated.js';`;
+    themeFile += `import {applyTheme as applyBaseTheme} from './theme-${themeProperties.parent}.generated.js';\n`;
   }
+  themeFile += `import strip from 'strip-comments';\n`;
 
-  themeFile += stripCommentsMethod;
   themeFile += createLinkReferences;
   themeFile += injectGlobalCssMethod;
 
