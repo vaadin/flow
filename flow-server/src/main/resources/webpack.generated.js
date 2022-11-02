9283216e831125bc208c06e7659e96ebbb42bc2b
Artur Signell <artur@vaadin.com>
2022-11-02 15:35:32 +0200

    Maybe fix webpack theme resource regexp

--- flow-server/src/main/resources/webpack.generated.js
+++ flow-server/src/main/resources/webpack.generated.js
@@ -305,9 +305,8 @@ module.exports = {
                 if (resourcePath.match(/(\\|\/)node_modules\1/)) {
                   return /(\\|\/)node_modules\1(?!.*node_modules)([\S]+)/.exec(resourcePath)[2].replace(/\\/g, '/');
                 }
-                //FIXME
-                if (resourcePath.match(/(\\|\/)flow-frontend\1/)) {
-                  return /(\\|\/)flow-frontend\1(?!.*flow-frontend)([\S]+)/.exec(resourcePath)[2].replace(/\\/g, '/');
+                if (resourcePath.match(/(\\|\/)generated\1jarResources\1/)) {
+                  return /(\\|\/)generated\1jarResources\1(?!.*jarResources)([\S]+)/.exec(resourcePath)[2].replace(/\\/g, '/');
                 }
                 return '[path][name].[ext]';
               }
