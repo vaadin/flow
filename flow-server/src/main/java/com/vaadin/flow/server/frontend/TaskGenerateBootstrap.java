dccb60048c51591fd41f750cb2a4be4071c93739
Artur Signell <artur@vaadin.com>
2022-11-03 12:23:39 +0200

    format

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskGenerateBootstrap.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskGenerateBootstrap.java
@@ -39,7 +39,9 @@ import static com.vaadin.flow.server.frontend.FrontendUtils.INDEX_TS;
  */
 public class TaskGenerateBootstrap extends AbstractTaskClientGenerator {
 
-    static final String DEV_TOOLS_IMPORT = String.format("import '%svaadin-dev-tools.js';%n", FrontendUtils.JAR_RESOURCES_IMPORT );
+    static final String DEV_TOOLS_IMPORT = String.format(
+            "import '%svaadin-dev-tools.js';%n",
+            FrontendUtils.JAR_RESOURCES_IMPORT);
     private final FrontendDependenciesScanner frontDeps;
     private final File frontendGeneratedDirectory;
     private final File frontendDirectory;
