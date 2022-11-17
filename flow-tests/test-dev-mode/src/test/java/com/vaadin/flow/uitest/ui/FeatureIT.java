dbc9019d107da410569d867625b90d15d1c9b1df
Artur <artur@vaadin.com>
2022-11-17 08:29:48 +0200

    test: Use the first feature flag instead of a specific one (#15131)

--- flow-tests/test-dev-mode/src/test/java/com/vaadin/flow/uitest/ui/FeatureIT.java
+++ flow-tests/test-dev-mode/src/test/java/com/vaadin/flow/uitest/ui/FeatureIT.java
@@ -17,9 +17,12 @@ package com.vaadin.flow.uitest.ui;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import com.vaadin.experimental.FeatureFlags;
 import com.vaadin.flow.component.html.testbench.H2Element;
 import com.vaadin.flow.component.html.testbench.NativeButtonElement;
+import com.vaadin.flow.di.Lookup;
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 import com.vaadin.flow.testutil.DevToolsElement;
 import com.vaadin.testbench.TestBenchElement;
@@ -29,20 +32,24 @@ public class FeatureIT extends ChromeBrowserTest {
     @Test
     public void enableAndDisableFeature() {
         open();
+        Lookup lookup = Mockito.mock(Lookup.class);
+        String featureProperty = new FeatureFlags(lookup).getFeatures().get(1)
+                .getId();
         Assert.assertEquals("Feature file missing",
                 $(H2Element.class).id("value").getText());
 
         // enable the feature
-        toggleWebpackFeature(false);
+        toggleFirstFeature(false);
 
         try {
             $(NativeButtonElement.class).id("check").click();
             Assert.assertEquals(
-                    "Feature file exists with properties: com.vaadin.experimental.webpackForFrontendBuild",
+                    "Feature file exists with properties: com.vaadin.experimental."
+                            + featureProperty,
                     $(H2Element.class).id("value").getText());
 
             // disable the feature
-            toggleWebpackFeature(true);
+            toggleFirstFeature(true);
 
             $(NativeButtonElement.class).id("check").click();
             Assert.assertEquals("Feature file exists with properties:",
@@ -54,15 +61,14 @@ public class FeatureIT extends ChromeBrowserTest {
         }
     }
 
-    private void toggleWebpackFeature(boolean expectedInitialState) {
+    private void toggleFirstFeature(boolean expectedInitialState) {
         DevToolsElement devTools = $(DevToolsElement.class).waitForFirst();
         devTools.expand();
         devTools.$(NativeButtonElement.class).id("features").click();
         TestBenchElement toggleButton = devTools.$(TestBenchElement.class)
-                .id("feature-toggle-webpackForFrontendBuild");
-        String checked = getCommandExecutor()
-                .executeScript("return arguments[0].checked", toggleButton)
-                .toString();
+                .attributeContains("class", "feature-toggle").get(0);
+        String checked = executeScript("return arguments[0].checked",
+                toggleButton).toString();
         Assert.assertEquals(
                 "Toggle button state expected " + expectedInitialState,
                 Boolean.toString(expectedInitialState), checked);
