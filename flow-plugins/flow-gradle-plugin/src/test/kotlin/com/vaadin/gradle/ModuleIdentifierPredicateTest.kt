dc9074755be3dd2c2051975f876f1353950ee7bb
Martin Vysny <martin@vysny.me>
2022-11-18 14:35:04 +0200

    Chore: Add tests for ClasspathFilter (#15195)
    Improves #15186 by adding tests to the added ClasspathFilter class.

---
+++ flow-plugins/flow-gradle-plugin/src/test/kotlin/com/vaadin/gradle/ModuleIdentifierPredicateTest.kt
@@ -0,0 +1,31 @@
+package com.vaadin.gradle
+
+import org.gradle.api.internal.artifacts.DefaultModuleIdentifier
+import org.junit.Test
+import kotlin.test.expect
+
+class ModuleIdentifierPredicateTest {
+    @Test
+    fun testAcceptAnything() {
+        val m = ModuleIdentifierPredicate.fromGroupNameGlob("*:*")
+        expect(true) { m.test(DefaultModuleIdentifier.newId("com.vaadin", "flow-server")) }
+        expect(true) { m.test(DefaultModuleIdentifier.newId("com.vaadin", "checkbox")) }
+        expect(true) { m.test(DefaultModuleIdentifier.newId("org.foo", "bar")) }
+    }
+
+    @Test
+    fun testAcceptAnyModuleFromComVaadin() {
+        val m = ModuleIdentifierPredicate.fromGroupNameGlob("com.vaadin:*")
+        expect(true) { m.test(DefaultModuleIdentifier.newId("com.vaadin", "flow-server")) }
+        expect(true) { m.test(DefaultModuleIdentifier.newId("com.vaadin", "checkbox")) }
+        expect(false) { m.test(DefaultModuleIdentifier.newId("org.foo", "bar")) }
+    }
+
+    @Test
+    fun testAcceptFlowModuleFromComVaadin() {
+        val m = ModuleIdentifierPredicate.fromGroupNameGlob("com.vaadin:flow-*")
+        expect(true) { m.test(DefaultModuleIdentifier.newId("com.vaadin", "flow-server")) }
+        expect(false) { m.test(DefaultModuleIdentifier.newId("com.vaadin", "checkbox")) }
+        expect(false) { m.test(DefaultModuleIdentifier.newId("org.foo", "bar")) }
+    }
+}
