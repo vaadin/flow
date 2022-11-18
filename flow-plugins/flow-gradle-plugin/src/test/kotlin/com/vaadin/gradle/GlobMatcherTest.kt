265c16c8dc5a225eb34547b6fe0b4e2ba8fce9b9
Martin Vysny <martin@vysny.me>
2022-11-18 14:35:04 +0200

    Chore: Add tests for ClasspathFilter (#15195)
    Improves #15186 by adding tests to the added ClasspathFilter class.

---
+++ flow-plugins/flow-gradle-plugin/src/test/kotlin/com/vaadin/gradle/GlobMatcherTest.kt
@@ -0,0 +1,30 @@
+package com.vaadin.gradle
+
+import org.junit.Test
+import kotlin.test.expect
+
+class GlobMatcherTest {
+    @Test
+    fun testMatchAnything() {
+        val m = GlobMatcher("*")
+        expect(true) { m.test("flow-server") }
+        expect(true) { m.test("com.vaadin") }
+    }
+
+    @Test
+    fun testEndsWith() {
+        val m = GlobMatcher("com.*")
+        expect(false) { m.test("flow-server") }
+        expect(true) { m.test("com.vaadin") }
+        expect(false) { m.test("org.foo") }
+        expect(false) { m.test("comma.foo") }
+    }
+
+    @Test
+    fun testString() {
+        val m = GlobMatcher("flow-server")
+        expect(true) { m.test("flow-server") }
+        expect(false) { m.test("com.vaadin") }
+        expect(false) { m.test("org.foo") }
+    }
+}
