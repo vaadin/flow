fb03eac1d78cf2ff4738030a3f5d74a3694e38b3
Marco Collovati <marco@vaadin.com>
2022-12-07 09:07:00 +0100

    Add guard for null UI on redirect

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/security/UidlRedirectStrategy.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/security/UidlRedirectStrategy.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.security.web.DefaultRedirectStrategy;
 
 import com.vaadin.flow.component.UI;
@@ -22,7 +23,15 @@ public class UidlRedirectStrategy extends DefaultRedirectStrategy {
             HttpServletResponse response, String url) throws IOException {
         final var servletMapping = request.getHttpServletMapping().getPattern();
         if (HandlerHelper.isFrameworkInternalRequest(servletMapping, request)) {
-            UI.getCurrent().getPage().setLocation(url);
+            UI ui = UI.getCurrent();
+            if (ui != null) {
+                ui.getPage().setLocation(url);
+            } else {
+                LoggerFactory.getLogger(UidlRedirectStrategy.class).warn(
+                        "A redirect to {} was request during a Vaadin request, "
+                                + "but it was not possible to get the UI instance to perform the action.",
+                        url);
+            }
         } else {
             super.sendRedirect(request, response, url);
         }
