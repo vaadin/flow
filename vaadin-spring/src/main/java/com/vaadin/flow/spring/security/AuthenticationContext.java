2217a206aef82266b0d0b6e6d5a9f44484f54403
Marco Collovati <marco@vaadin.com>
2022-12-15 09:15:12 +0100

    fix: remove Serializable interface from AuthenticationContext
    The component is Serializable because the idea was to potentially inject it
    into Vaadin views, but this may cause several issues:
    
    * a single instance of that class per application is expected, but if
      deserializating views we may end up with many of them
    * logoutSuccessHandler and logoutHandler are transient, but they are not
      Spring beans and we do not provide a simple way to inject them upon
      deserialization
    * for the above reason AuthenticationContext may not work correctly with
      Kubernetes Kit
    
    This change removes the implementation of Serializable interface and
    updates javadocs.

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/security/AuthenticationContext.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/security/AuthenticationContext.java
@@ -40,23 +40,25 @@ import com.vaadin.flow.server.VaadinServletResponse;
 /**
  * The authentication context of the application.
  * <p>
- * An instance of this class is available for injection as bean in view and
- * layout classes.
  *
  * It allows to access authenticated user information and to initiate the logout
  * process.
  *
+ * An instance of this class is available for injection as bean in view and
+ * layout classes. The class is not {@link java.io.Serializable}, so potential
+ * referencing fields in Vaadin view should be defined {@literal transient}.
+ *
  * @author Vaadin Ltd
  * @since 23.3
  */
-public class AuthenticationContext implements Serializable {
+public class AuthenticationContext {
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(AuthenticationContext.class);
 
-    private transient LogoutSuccessHandler logoutSuccessHandler;
+    private LogoutSuccessHandler logoutSuccessHandler;
 
-    private transient CompositeLogoutHandler logoutHandler;
+    private CompositeLogoutHandler logoutHandler;
 
     /**
      * Gets an {@link Optional} with an instance of the current user if it has
@@ -121,9 +123,6 @@ public class AuthenticationContext implements Serializable {
 
     /**
      * Sets component to handle logout process.
-     *
-     * This method should be invoked after deserialization to refresh required
-     * transient fields.
      *
      * @param logoutSuccessHandler
      *            {@link LogoutSuccessHandler} instance, not {@literal null}.
