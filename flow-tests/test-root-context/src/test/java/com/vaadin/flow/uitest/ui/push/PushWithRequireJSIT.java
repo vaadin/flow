8748174ce118cd843732f7596315f09f6db68bd2
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2022-12-20 16:04:02 +0200

    test: Ignore recently added PUSH + WebSockets test for OSGi env (#15522)
    Follow-up for #15509

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/push/PushWithRequireJSIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/push/PushWithRequireJSIT.java
@@ -3,10 +3,11 @@ package com.vaadin.flow.uitest.ui.push;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import com.vaadin.flow.testcategory.IgnoreOSGi;
 import com.vaadin.flow.testcategory.PushTests;
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 
-@Category(PushTests.class)
+@Category({ PushTests.class, IgnoreOSGi.class })
 public class PushWithRequireJSIT extends ChromeBrowserTest {
 
     @Test
