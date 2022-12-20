77748080b66805b583db3a6d6743ee6e0280da03
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2022-12-20 16:04:02 +0200

    test: Ignore recently added PUSH + WebSockets test for OSGi env (#15522)
    Follow-up for #15509

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/push/BasicPushWebsocketXhrIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/push/BasicPushWebsocketXhrIT.java
@@ -2,9 +2,10 @@ package com.vaadin.flow.uitest.ui.push;
 
 import org.junit.experimental.categories.Category;
 
+import com.vaadin.flow.testcategory.IgnoreOSGi;
 import com.vaadin.flow.testcategory.PushTests;
 
-@Category(PushTests.class)
+@Category({ PushTests.class, IgnoreOSGi.class })
 public class BasicPushWebsocketXhrIT extends AbstractClientServerCounterIT {
 
 }
