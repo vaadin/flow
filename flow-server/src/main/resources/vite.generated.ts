00d0f11cf371214910eeea0d0fe58f50a6e4b4db
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2022-12-21 14:04:38 +0200

    feat: Serve theme files separately from dev bundle (#15518)
    In the Express Build mode:
    
    - serves theme files (styles.css and other project's stylesheets imported to styles.css) separately from the dev-bundle directly from custom theme (frontend/themes/my-theme) folder
    - copies the assets referenced in theme.json to the dev bundle within bundle re-compilation
    serves assets/styles from dev-bundle, if can't find them in the frontend folder
    - Does not yet take into account serving styles from default bundle (from classpath).
    
    Related-to #15478

--- flow-server/src/main/resources/vite.generated.ts
+++ flow-server/src/main/resources/vite.generated.ts
@@ -48,11 +48,12 @@ const themeProjectFolders = projectStaticAssetsFolders.map((folder) => path.reso
 
 const themeOptions = {
   devMode: false,
+  useDevBundle: devBundle,
   // The following matches folder 'frontend/generated/themes/'
   // (not 'frontend/themes') for theme in JAR that is copied there
   themeResourceFolder: path.resolve(themeResourceFolder, settings.themeFolder),
   themeProjectFolders: themeProjectFolders,
-  projectStaticAssetsOutputFolder: path.resolve(__dirname, settings.staticOutput),
+  projectStaticAssetsOutputFolder: devBundle ? path.resolve(devBundleFolder, '../assets') : path.resolve(__dirname, settings.staticOutput),
   frontendGeneratedFolder: path.resolve(frontendFolder, settings.generatedFolder)
 };
 
