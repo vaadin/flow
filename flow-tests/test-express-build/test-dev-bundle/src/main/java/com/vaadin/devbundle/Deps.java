707b98aa03d50f4b199bb40908299f4c7e10d7d7
Mikhail Shabarov <mikhail@vaadin.com>
2023-01-03 17:17:07 +0200

    test: Add the integration test

--- flow-tests/test-express-build/test-dev-bundle/src/main/java/com/vaadin/devbundle/Deps.java
+++ flow-tests/test-express-build/test-dev-bundle/src/main/java/com/vaadin/devbundle/Deps.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 @JsModule("@polymer/paper-checkbox/paper-checkbox.js")
 @NpmPackage(value = "@polymer/paper-input", version = "3.0.2")
 @NpmPackage(value = "@polymer/paper-checkbox", version = "3.0.1")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Deps {
 
 }
