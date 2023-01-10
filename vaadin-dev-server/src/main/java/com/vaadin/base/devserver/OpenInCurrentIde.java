0f29a5a58c0588b1722e942d1be90a1810cd988b
Artur Signell <artur@vaadin.com>
2023-01-10 12:00:27 +0200

    Fix review comments

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/OpenInCurrentIde.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/OpenInCurrentIde.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2022 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.base.devserver;
 
 import java.io.File;
@@ -18,10 +33,14 @@ import com.vaadin.open.Open;
  * <p>
  * Supports detecting VS Code, Eclipse and IntelliJ.
  */
-public class OpenInCurrentIde {
+public final class OpenInCurrentIde {
 
     private static final String ECLIPSE_IDENTIFIER = "eclipse";
     private static final String INTELLIJ_IDENTIFIER = "intellij";
+
+    private OpenInCurrentIde() {
+        // Utils only
+    }
 
     /**
      * Opens the given file at the given line number in the IDE used to launch
