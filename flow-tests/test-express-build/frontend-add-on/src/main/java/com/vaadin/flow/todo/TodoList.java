fb0c40fc349e8a921ad96b02fe814aca0d9fde4a
Mikael Grankvist <mikael.grankvist@vaadin.com>
2023-01-10 14:02:39 +0200

    test: Test devBuild with add-ons
    Test that a pure server add-on
    doesn't require a devBundle, but
    an add-on with frontend files does.
    
    Closes #15656

---
+++ flow-tests/test-express-build/frontend-add-on/src/main/java/com/vaadin/flow/todo/TodoList.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.todo;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.template.Id;
+
+/**
+ * The main application view of the todo application.
+ */
+@Tag("todo-template")
+@JsModule("./TodoTemplate.js")
+public class TodoList extends LitTemplate {
+
+    @Id("creator")
+    private TodoCreator creator;
+
+    public TodoList() {
+        setId("template");
+
+        creator.addCreateCallback(todo -> addNewTodoItem(todo));
+    }
+
+    private void addNewTodoItem(Todo todo) {
+        TodoElement todoElement = new TodoElement(todo);
+
+        todoElement.getElement().addEventListener("remove",
+                e -> getElement().removeChild(todoElement.getElement()));
+
+        todoElement.addStateChangeListener(() -> {
+            if (todoElement.isCompleted()) {
+                todoElement.getElement().setAttribute("slot", "done");
+            }
+        });
+
+        getElement().appendChild(todoElement.getElement());
+    }
+
+}
---
+++ flow-tests/test-express-build/test-dev-bundle-frontend-add-on/src/test/java/com/vaadin/flow/testnpmonlyfeatures/nobuildmojo/TodoIT.java
@@ -0,0 +1,74 @@
+package com.vaadin.flow.testnpmonlyfeatures.nobuildmojo;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.testbench.TestBenchElement;
+
+public class TodoIT extends ChromeBrowserTest {
+
+    @Before
+    public void init() {
+        open();
+    }
+
+    @Override
+    protected String getTestPath() {
+        return "/view/com.vaadin.flow.frontend.TodoView";
+    }
+
+    @Test
+    public void testAddOn() {
+        TestBenchElement template = $(TestBenchElement.class).id("template");
+
+        TestBenchElement createTemplate = template.$(TestBenchElement.class)
+                .id("creator");
+
+        TestBenchElement todo = createTemplate.$(TestBenchElement.class)
+                .id("task-input");
+        todo.sendKeys("Important task");
+
+        TestBenchElement user = createTemplate.$(TestBenchElement.class)
+                .id("user-name-input");
+        user.sendKeys("Teuvo testi");
+
+        TestBenchElement createButton = createTemplate.$(TestBenchElement.class)
+                .id("create-button");
+        createButton.click();
+
+        TestBenchElement todoElement = template
+                .findElement(By.tagName("todo-element"));
+        Assert.assertEquals("Important task",
+                todoElement.$(TestBenchElement.class).id("task").getText());
+
+    }
+
+    @Test
+    public void noFrontendFilesCreated() {
+        File baseDir = new File(System.getProperty("user.dir", "."));
+
+        // shouldn't create a dev-bundle
+        Assert.assertTrue("New devBundle should be generated",
+                new File(baseDir, "dev-bundle").exists());
+        Assert.assertTrue("node_modules should be downloaded",
+                new File(baseDir, "node_modules").exists());
+
+        // These should not be generated either, but at the moment they are
+        Assert.assertTrue("package.json should be created",
+                new File(baseDir, "package.json").exists());
+        Assert.assertTrue("vite generated should be created",
+                new File(baseDir, "vite.generated.ts").exists());
+        Assert.assertTrue("vite config should be created",
+                new File(baseDir, "vite.config.ts").exists());
+        Assert.assertTrue("types should be created",
+                new File(baseDir, "types.d.ts").exists());
+        Assert.assertTrue("tsconfig should be created",
+                new File(baseDir, "tsconfig.json").exists());
+    }
+}
