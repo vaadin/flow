df2a5f29b1ada7b5c8d72f3c2fed64903cca37d5
Marco Collovati <marco@vaadin.com>
2023-01-17 18:01:32 +0100

    fixed test component for npm

--- flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/frontend/DirectoryImportView.java
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/frontend/DirectoryImportView.java
@@ -1,6 +1,5 @@
 package com.vaadin.flow.uitest.ui.frontend;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.dependency.JsModule;
@@ -10,14 +9,14 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.uitest.servlet.ViewTestLayout;
 
-
-@JsModule("importdir.js")
 @Route(value = "com.vaadin.flow.uitest.ui.frontend.DirectoryImportView", layout = ViewTestLayout.class)
 public class DirectoryImportView extends Div {
 
     @Tag("a-directory-component")
     @HtmlImport("frontend://com/vaadin/flow/uitest/ui/template/ImportDir.html")
-    public static class DirectoryComponent extends PolymerTemplate<TemplateModel> {
+    @JsModule("importdir.js")
+    public static class DirectoryComponent
+            extends PolymerTemplate<TemplateModel> {
 
     }
 
