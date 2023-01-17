f7bdd27cd149ea404365cf10d83bbcf3f5724d4b
Mikael Grankvist <mikael.grankvist@vaadin.com>
2023-01-17 10:01:43 +0200

    Add logging on why bundle is needed
    Add files to check that they are not generated

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunDevBundleBuild.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunDevBundleBuild.java
@@ -102,12 +102,14 @@ public class TaskRunDevBundleBuild implements FallibleCommand {
 
         if (!FrontendUtils.getDevBundleFolder(npmFolder).exists()
                 && !hasJarBundle()) {
+            getLogger().info("No dev-bundle found.");
             return true;
         }
 
         String statsJsonContent = FrontendUtils.findBundleStatsJson(npmFolder);
         if (statsJsonContent == null) {
             // without stats.json in bundle we can not say if it is up to date
+            getLogger().info("No dev-bundle stats.json found for validation.");
             return true;
         }
 
