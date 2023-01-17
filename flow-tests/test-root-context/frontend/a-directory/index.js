52ea2de259e68b31da367497155ce577fc9b1db9
Marco Collovati <marco@vaadin.com>
2023-01-17 16:38:14 +0100

    fixed test

--- flow-tests/test-root-context/frontend/a-directory/index.js
+++ flow-tests/test-root-context/frontend/a-directory/index.js
@@ -1,8 +1,12 @@
-import { LitElement, html } from 'lit';
+import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {html} from '@polymer/polymer/lib/utils/html-tag.js';
 
-export class ADirectoryComponent extends LitElement {
-  render() {
+export class ADirectoryComponent extends PolymerElement {
+  static get template() {
     return html`Directory import ok`;
   }
+  static get is() {
+    return 'a-directory-component'
+  }
 }
-customElements.define('a-directory-component', ADirectoryComponent);
+customElements.define(ADirectoryComponent.is, ADirectoryComponent);
--- flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/frontend/DirectoryImportView.java
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/frontend/DirectoryImportView.java
@@ -2,17 +2,22 @@ package com.vaadin.flow.uitest.ui.frontend;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.uitest.servlet.ViewTestLayout;
 
-@JsModule("./importdir.js")
+
+@JsModule("importdir.js")
 @Route(value = "com.vaadin.flow.uitest.ui.frontend.DirectoryImportView", layout = ViewTestLayout.class)
 public class DirectoryImportView extends Div {
 
     @Tag("a-directory-component")
-    public static class DirectoryComponent extends Component {
+    @HtmlImport("frontend://com/vaadin/flow/uitest/ui/template/ImportDir.html")
+    public static class DirectoryComponent extends PolymerTemplate<TemplateModel> {
 
     }
 
