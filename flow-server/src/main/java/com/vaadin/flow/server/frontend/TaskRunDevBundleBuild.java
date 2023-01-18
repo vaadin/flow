5d1ad1dceb21f7599948b3634264301099e7ab8b
Mikael Grankvist <mikael.grankvist@vaadin.com>
2023-01-18 13:36:07 +0200

    fix: DevBundle used after clean-frontend goal
    Dev bundle should be used if possible
    even after `clean-frontend` goal has been
    executed.
    
    Fixes #15707

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunDevBundleBuild.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunDevBundleBuild.java
@@ -316,8 +316,16 @@ public class TaskRunDevBundleBuild implements FallibleCommand {
 
         if (packageJsonFile.exists()) {
             try {
-                return Json.parse(FileUtils.readFileToString(packageJsonFile,
-                        StandardCharsets.UTF_8));
+                final JsonObject packageJson = Json
+                        .parse(FileUtils.readFileToString(packageJsonFile,
+                                StandardCharsets.UTF_8));
+                if (!packageJson.hasKey(NodeUpdater.VAADIN_DEP_KEY)
+                        || !packageJson.getObject(NodeUpdater.VAADIN_DEP_KEY)
+                                .hasKey(NodeUpdater.HASH_KEY)) {
+                    updatePackageJsonWithDefaultDependencies(options,
+                            frontendDependencies, finder, packageJson);
+                }
+                return packageJson;
             } catch (IOException e) {
                 getLogger().warn("Failed to read package.json", e);
             }
@@ -329,6 +337,33 @@ public class TaskRunDevBundleBuild implements FallibleCommand {
             }
         }
         return null;
+    }
+
+    private static void updatePackageJsonWithDefaultDependencies(
+            Options options, FrontendDependenciesScanner frontendDependencies,
+            ClassFinder finder, JsonObject packageJson) {
+        // If we have executed flow:clean-frontend we will have an empty
+        // package.json with no hash field.
+        JsonObject defaultPackageJson = getDefaultPackageJson(options,
+                frontendDependencies, finder);
+
+        // Add dependencies as it should be enough to have the expected
+        // dependencies in the package.json
+        final JsonObject defaultDependencies = defaultPackageJson
+                .getObject(NodeUpdater.DEPENDENCIES);
+        for (String key : defaultDependencies.keys()) {
+            packageJson.getObject(NodeUpdater.DEPENDENCIES).put(key,
+                    defaultDependencies.getString(key));
+        }
+
+        // Add hash to package.json so we don't fail just because it is missing
+        final String hash = TaskUpdatePackages
+                .generatePackageJsonHash(packageJson);
+        if (!packageJson.hasKey(NodeUpdater.VAADIN_DEP_KEY)) {
+            packageJson.put(NodeUpdater.VAADIN_DEP_KEY, Json.createObject());
+        }
+        packageJson.getObject(NodeUpdater.VAADIN_DEP_KEY)
+                .put(NodeUpdater.HASH_KEY, hash);
     }
 
     protected static JsonObject getDefaultPackageJson(Options options,
