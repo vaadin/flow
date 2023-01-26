6810db0a92dedfa347d1427054b7d16dcaef2582
Mikhail Shabarov <mikhail@vaadin.com>
2023-01-26 09:36:11 +0200

    Refactor codes for readability

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunDevBundleBuild.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskRunDevBundleBuild.java
@@ -149,7 +149,6 @@ public class TaskRunDevBundleBuild implements FallibleCommand {
 
     private static boolean packagedThemeAddedOrUpdated(Options options,
             JsonObject statsJson) {
-        JarContentsManager jarContentsManager = new JarContentsManager();
         Map<String, String> packagedThemeHashes = new HashMap<>(1);
 
         if (options.jarFiles == null) {
@@ -157,35 +156,9 @@ public class TaskRunDevBundleBuild implements FallibleCommand {
         }
 
         options.jarFiles.stream().filter(File::exists)
-                .filter(file -> !file.isDirectory()).forEach(jarFile -> {
-                    if (jarContentsManager.containsPath(jarFile,
-                            Constants.RESOURCES_THEME_JAR_DEFAULT)) {
-                        List<String> themeJsons = jarContentsManager.findFiles(
-                                jarFile, Constants.RESOURCES_THEME_JAR_DEFAULT,
-                                "theme.json");
-                        themeJsons.forEach(themeJson -> {
-                            byte[] byteContent = jarContentsManager
-                                    .getFileContents(jarFile, themeJson);
-                            String content = IOUtils.toString(byteContent,
-                                    "UTF-8");
-                            content = content.replaceAll("\\r\\n", "\n");
-
-                            JsonObject themeJsonContent = Json.parse(content);
-                            if (themeJsonContent.hasKey(Constants.ASSETS)) {
-                                Matcher matcher = THEME_PATH_PATTERN
-                                        .matcher(themeJson);
-                                if (!matcher.find()) {
-                                    throw new IllegalStateException(
-                                            "Packaged theme folders structure is incorrect, should have META-INF/resources/themes/[theme-name]/");
-                                }
-                                String themeName = matcher.group(1);
-                                String hash = StringUtil.getHash(content,
-                                        StandardCharsets.UTF_8);
-                                packagedThemeHashes.put(themeName, hash);
-                            }
-                        });
-                    }
-                });
+                .filter(file -> !file.isDirectory())
+                .forEach(jarFile -> calculateHashesForPackagedThemes(jarFile,
+                        packagedThemeHashes));
 
         JsonObject hashesInStats = statsJson.getObject("themeJsonHashes");
         if (hashesInStats == null && !packagedThemeHashes.isEmpty()) {
@@ -194,16 +167,14 @@ public class TaskRunDevBundleBuild implements FallibleCommand {
         }
         for (Map.Entry<String, String> themeHash : packagedThemeHashes
                 .entrySet()) {
-            if (hashesInStats.hasKey(themeHash.getKey())) {
-                if (!hashesInStats.getString(themeHash.getKey())
-                        .equals(themeHash.getValue())) {
-                    getLogger().info("Found updated package custom theme '{}'.",
-                            themeHash.getKey());
-                    return true;
-                }
-            } else {
+            if (!hashesInStats.hasKey(themeHash.getKey())) {
                 getLogger().info(
                         "Found newly added packaged custom theme '{}'.",
+                        themeHash.getKey());
+                return true;
+            } else if (!hashesInStats.getString(themeHash.getKey())
+                    .equals(themeHash.getValue())) {
+                getLogger().info("Found updated package custom theme '{}'.",
                         themeHash.getKey());
                 return true;
             }
@@ -502,6 +473,36 @@ public class TaskRunDevBundleBuild implements FallibleCommand {
             return false;
         }
         return true;
+    }
+
+    private static void calculateHashesForPackagedThemes(File jarFileToLookup,
+            Map<String, String> packagedThemeHashes) {
+        JarContentsManager jarContentsManager = new JarContentsManager();
+        if (jarContentsManager.containsPath(jarFileToLookup,
+                Constants.RESOURCES_THEME_JAR_DEFAULT)) {
+            List<String> themeJsons = jarContentsManager.findFiles(
+                    jarFileToLookup, Constants.RESOURCES_THEME_JAR_DEFAULT,
+                    "theme.json");
+            for (String themeJson : themeJsons) {
+                byte[] byteContent = jarContentsManager
+                        .getFileContents(jarFileToLookup, themeJson);
+                String content = IOUtils.toString(byteContent, "UTF-8");
+                content = content.replaceAll("\\r\\n", "\n");
+
+                JsonObject themeJsonContent = Json.parse(content);
+                if (themeJsonContent.hasKey(Constants.ASSETS)) {
+                    Matcher matcher = THEME_PATH_PATTERN.matcher(themeJson);
+                    if (!matcher.find()) {
+                        throw new IllegalStateException(
+                                "Packaged theme folders structure is incorrect, should have META-INF/resources/themes/[theme-name]/");
+                    }
+                    String themeName = matcher.group(1);
+                    String hash = StringUtil.getHash(content,
+                            StandardCharsets.UTF_8);
+                    packagedThemeHashes.put(themeName, hash);
+                }
+            }
+        }
     }
 
     private static Logger getLogger() {
