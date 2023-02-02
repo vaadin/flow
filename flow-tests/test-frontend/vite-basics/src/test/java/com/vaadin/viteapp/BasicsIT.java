cf481a1ac233b0d72c8287e99434101c44a06256
Artur Signell <artur@vaadin.com>
2023-02-02 16:06:01 +0200

    add import

--- flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/BasicsIT.java
+++ flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/BasicsIT.java
@@ -3,6 +3,7 @@ package com.vaadin.viteapp;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.vaadin.flow.component.html.testbench.ParagraphElement;
 import com.vaadin.flow.testutil.DevToolsElement;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.viteapp.views.empty.MainView;
