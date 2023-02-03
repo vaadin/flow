7b9dbf6e229d9a869a79ae202316c8e77dc202fa
Marco Collovati <marco@vaadin.com>
2023-02-03 19:26:59 +0100

    fix: preserve push messages in cache until they are seen by client (#15764)
    Atmospehere caches messages when the client is disconnected, but unfortunately it may happen that a message does not reach the client because of network disconnection during async response write operation. In this case the message is not cached and will be completely lost, causing a UI resynchronization request.
    This change preserves messages in broadcaster cache until the client confirms that they have been processed, by sending the last seen server sync identifier on reconnection.
    This should prevent the need for a UI resynchronization. It may happen in some cases, e.g. back to online after being offline, that messages already seen will be sent to the client, but Flow will discard them.
    
    Part of #15281
    Fixes #15205
    
    Co-authored-by: Teppo Kurki <teppo.kurki@vaadin.com>

---
+++ flow-server/src/main/java/com/vaadin/flow/server/communication/LongPollingCacheFilter.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.server.communication;
+
+import java.io.Serializable;
+
+import org.atmosphere.cache.BroadcastMessage;
+import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.BroadcasterCache;
+import org.atmosphere.cpr.PerRequestBroadcastFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A {@link PerRequestBroadcastFilter} implementation that handles
+ * {@link com.vaadin.flow.server.communication.AtmospherePushConnection.PushMessage}s
+ * to ensure that a message is preserved in the {@link BroadcasterCache} until
+ * the client has received it.
+ *
+ * The filter acts only on {@literal LONG POLLING} transport and expects that
+ * the client sends the {@literal X-Vaadin-LastSeenServerSyncId} header with the
+ * identifier of the last message seen, every time the connection is
+ * established.
+ *
+ * Messages already seen are discarded, whereas messages not yet sent to the
+ * client are added again to the cache to preserve them until client confirms
+ * reception by sending the last seen message identifier.
+ */
+public class LongPollingCacheFilter
+        implements PerRequestBroadcastFilter, Serializable {
+    public static final String SEEN_SERVER_SYNC_ID = "X-Vaadin-LastSeenServerSyncId";
+
+    private static Logger getLogger() {
+        return LoggerFactory.getLogger(LongPollingCacheFilter.class.getName());
+    }
+
+    @Override
+    public BroadcastAction filter(String broadcasterId, AtmosphereResource r,
+            Object originalMessage, Object message) {
+        if (originalMessage instanceof AtmospherePushConnection.PushMessage
+                && r.transport() == AtmosphereResource.TRANSPORT.LONG_POLLING
+                && r.getRequest().getHeader(SEEN_SERVER_SYNC_ID) != null) {
+            AtmospherePushConnection.PushMessage pushMessage = (AtmospherePushConnection.PushMessage) originalMessage;
+            String uuid = r.uuid();
+            int lastSeenOnClient = Integer
+                    .parseInt(r.getRequest().getHeader(SEEN_SERVER_SYNC_ID));
+            if (pushMessage.alreadySeen(lastSeenOnClient)) {
+                getLogger().trace(
+                        "Discarding message {} for resource {} as client already seen {}. {}",
+                        pushMessage.serverSyncId, uuid, lastSeenOnClient,
+                        pushMessage.message);
+                // Client has already seen this message, discard it
+                return new BroadcastAction(BroadcastAction.ACTION.ABORT,
+                        message);
+            } else {
+                // In rare cases with long polling, message may be lost during
+                // write operation and the client may never receive it.
+                // To prevent this kind of issues we move the message back to
+                // the cache until we get confirmation that the message has been
+                // seen
+                getLogger().trace(
+                        "Put message {} for resource {} back to the cache because it may not have reached the client, as the last seen message is {}. {}",
+                        pushMessage.serverSyncId, uuid, lastSeenOnClient,
+                        pushMessage.message);
+                BroadcasterCache cache = r.getBroadcaster()
+                        .getBroadcasterConfig().getBroadcasterCache();
+                cache.addToCache(broadcasterId, uuid,
+                        new BroadcastMessage(originalMessage));
+            }
+        }
+        return new BroadcastAction(message);
+    }
+
+    @Override
+    public BroadcastAction filter(String broadcasterId, Object originalMessage,
+            Object message) {
+        return new BroadcastAction(message);
+    }
+
+}
