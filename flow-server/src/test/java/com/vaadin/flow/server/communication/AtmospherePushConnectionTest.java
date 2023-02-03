42262f374fbca6f3260288fcd33f698cdcf92243
Marco Collovati <marco@vaadin.com>
2023-02-03 19:40:28 +0100

    fix: prevent concurrent disconnect and push operations (#15767)
    Disconnecting an AtmospherePushConnection while it is sending a message
    may result in a NullPointerException if AtmosphereResource is nullified
    before the message is sent.
    This change synchronizes operations, so that disconnect will wait until
    current push finishes, or push waits for disconnect to complete so that
    the isConnected() method reflects correctly current state.
    
    Fixes #15571

--- flow-server/src/test/java/com/vaadin/flow/server/communication/AtmospherePushConnectionTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/communication/AtmospherePushConnectionTest.java
@@ -19,14 +19,19 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.atmosphere.cpr.AtmosphereResource;
-import org.easymock.EasyMock;
+import org.atmosphere.cpr.Broadcaster;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.server.communication.AtmospherePushConnection;
+import com.vaadin.flow.server.MockVaadinSession;
 import com.vaadin.flow.server.communication.AtmospherePushConnection.State;
 
 /**
@@ -37,9 +42,8 @@ public class AtmospherePushConnectionTest {
     @Test
     public void testSerialization() throws Exception {
 
-        UI ui = EasyMock.createNiceMock(UI.class);
-        AtmosphereResource resource = EasyMock
-                .createNiceMock(AtmosphereResource.class);
+        UI ui = Mockito.mock(UI.class);
+        AtmosphereResource resource = Mockito.mock(AtmosphereResource.class);
 
         AtmospherePushConnection connection = new AtmospherePushConnection(ui);
         connection.connect(resource);
@@ -55,4 +59,79 @@ public class AtmospherePushConnectionTest {
 
         Assert.assertEquals(State.DISCONNECTED, connection.getState());
     }
+
+    @Test
+    public void pushWhileDisconnect_disconnectedWithoutSendingMessage()
+            throws Exception {
+
+        UI ui = Mockito.spy(new UI());
+        MockVaadinSession vaadinSession = new MockVaadinSession();
+        vaadinSession.getService().init();
+        Mockito.when(ui.getSession()).thenReturn(vaadinSession);
+        Broadcaster broadcaster = Mockito.mock(Broadcaster.class);
+        AtmosphereResource resource = Mockito.mock(AtmosphereResource.class);
+        Mockito.when(resource.getBroadcaster()).thenReturn(broadcaster);
+
+        AtmospherePushConnection connection = new AtmospherePushConnection(ui);
+        connection.connect(resource);
+
+        CountDownLatch latch = new CountDownLatch(1);
+        CompletableFuture.runAsync(() -> {
+            try {
+                vaadinSession.runWithLock(() -> {
+                    connection.push();
+                    return null;
+                });
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                latch.countDown();
+            }
+        });
+        connection.disconnect();
+        Assert.assertTrue("AtmospherePushConnection not disconnected",
+                latch.await(2, TimeUnit.SECONDS));
+        Assert.assertEquals(State.PUSH_PENDING, connection.getState());
+        Mockito.verifyNoInteractions(broadcaster);
+    }
+
+    @Test
+    public void disconnectWhilePush_messageSentAndThenDisconnected()
+            throws Exception {
+
+        UI ui = Mockito.spy(new UI());
+        MockVaadinSession vaadinSession = new MockVaadinSession();
+        vaadinSession.getService().init();
+        Mockito.when(ui.getSession()).thenReturn(vaadinSession);
+        Broadcaster broadcaster = Mockito.mock(Broadcaster.class);
+        AtmosphereResource resource = Mockito.mock(AtmosphereResource.class);
+        Mockito.when(resource.getBroadcaster()).thenReturn(broadcaster);
+
+        AtmospherePushConnection connection = new AtmospherePushConnection(ui);
+        connection.connect(resource);
+
+        CountDownLatch latch = new CountDownLatch(1);
+        CompletableFuture.runAsync(() -> {
+            try {
+                vaadinSession.runWithLock(() -> {
+                    CompletableFuture.runAsync(connection::disconnect);
+                    connection.push();
+                    return null;
+                });
+            } catch (Throwable ex) {
+                throw new RuntimeException(ex);
+            } finally {
+                latch.countDown();
+            }
+        }).whenComplete((x, err) -> {
+            if (err != null) {
+                err.printStackTrace();
+            }
+        });
+        Assert.assertTrue("Push not completed",
+                latch.await(2, TimeUnit.SECONDS));
+        Mockito.verify(broadcaster).broadcast(ArgumentMatchers.any(),
+                ArgumentMatchers.eq(resource));
+    }
+
 }
