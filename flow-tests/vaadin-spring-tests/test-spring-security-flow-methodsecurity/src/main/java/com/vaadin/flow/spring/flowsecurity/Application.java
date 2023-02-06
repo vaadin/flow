4723f77bffb3a31804ef8c40b89d5dc5043442e0
marcin <marcin@vaadin.com>
2023-02-06 09:20:29 +0100

    fix: fixed support for Spring EnableMethodSecurity (#15059) (#15786)
    Vaadin clone of #15671 plus tests
    
    Fixes #15059
    
    Co-authored-by @aspan
    
    Co-authored-by: Marco Collovati <marco@vaadin.com>

---
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow-methodsecurity/src/main/java/com/vaadin/flow/spring/flowsecurity/Application.java
@@ -0,0 +1,20 @@
+package com.vaadin.flow.spring.flowsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
