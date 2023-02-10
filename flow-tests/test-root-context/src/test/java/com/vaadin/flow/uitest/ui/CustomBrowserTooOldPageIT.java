dba962cf30d00c3476906992e64147bb65185185
Artur Signell <artur@vaadin.com>
2023-02-10 16:39:21 +0200

    Test for custom 'too old' page

---
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/CustomBrowserTooOldPageIT.java
@@ -0,0 +1,17 @@
+package com.vaadin.flow.uitest.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
+public class CustomBrowserTooOldPageIT extends ChromeBrowserTest {
+
+    @Test
+    public void customPageUsed() {
+        getDriver().get("/view/?v-r=browser-too-old");
+        Assert.assertTrue(getDriver().getPageSource()
+                .contains("You so old you cannot view this page"));
+    }
+
+}
