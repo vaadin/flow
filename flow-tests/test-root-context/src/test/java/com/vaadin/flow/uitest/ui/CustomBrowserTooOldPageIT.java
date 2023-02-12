a465ed59683083b4815213d9a93dff27da4de023
Artur Signell <artur@vaadin.com>
2023-02-12 18:13:41 +0200

    Use constant in test

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/CustomBrowserTooOldPageIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/CustomBrowserTooOldPageIT.java
@@ -3,13 +3,15 @@ package com.vaadin.flow.uitest.ui;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.vaadin.flow.server.HandlerHelper.RequestType;
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 
 public class CustomBrowserTooOldPageIT extends ChromeBrowserTest {
 
     @Test
     public void customPageUsed() {
-        getDriver().get(getRootURL() + "/view/?v-r=browser-too-old");
+        getDriver().get(getRootURL() + "/view/?v-r="
+                + RequestType.BROWSER_TOO_OLD.getIdentifier());
         Assert.assertTrue(getDriver().getPageSource()
                 .contains("You so old you cannot view this page"));
     }
