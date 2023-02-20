225e99f89a7e473d2bb811b5393d2f6897502d71
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-02-20 15:52:54 +0100

    fix patch script to only replace dev tools import

--- vaadin-dev-server/patch-application.js
+++ vaadin-dev-server/patch-application.js
@@ -6,16 +6,17 @@ const path = require('path');
 
 // Verify that the script was passed a path to a Flow application
 const appPath = process.argv[2];
-
 if (!appPath) {
-  throw new Error("Missing application path argument. " +
-    "The patch-app script should be run as: npm run patch-app /path/to/flow-app");
+  throw new Error(
+    'Missing application path argument. The patch-app script should be run as: npm run patch-app /path/to/flow-app'
+  );
 }
 
-const generatedFrontendPath = path.join(appPath, 'frontend', 'generated');
-
-if (!fs.existsSync(generatedFrontendPath)) {
-  throw new Error(`Application path does not contain a ./frontend/generated directory: ${generatedFrontendPath}. Make sure to start the app first.`);
+const vaadinFilePath = path.join(appPath, 'frontend', 'generated', 'vaadin.ts');
+if (!fs.existsSync(vaadinFilePath)) {
+  throw new Error(
+    `Application path does not contain a ./frontend/generated directory: ${vaadinFilePath}. Make sure to start the app first.`
+  );
 }
 
 // Patch content of vaadin.ts to:
@@ -23,14 +24,16 @@ if (!fs.existsSync(generatedFrontendPath)) {
 // - add HMR script from @web/dev-server
 // - add the vaadin-dev-tools hosted by web-dev-server with HMR support
 const webDevServerUrl = 'http://localhost:8000';
-const vaadinFilePath = path.join(generatedFrontendPath, 'vaadin.ts');
-const vaadinFileContent = `
-import './vaadin-featureflags.ts';
-import './index';
-import { applyTheme } from './theme.js';
+const devToolsImport = "import 'Frontend/generated/jar-resources/vaadin-dev-tools.js';";
+const vaadinFileContent = fs.readFileSync(vaadinFilePath, 'utf8');
 
-applyTheme(document);
+const isPatched = vaadinFileContent.includes('__web-dev-server__web-socket.js');
+const hasDevToolsImport = vaadinFileContent.includes(devToolsImport);
 
+if (isPatched) {
+  console.log(`✅ ${vaadinFilePath} is already patched`);
+} else if (hasDevToolsImport) {
+  const patchContent = `
 const hmrScript = document.createElement("script");
 hmrScript.setAttribute("type", "module");
 hmrScript.setAttribute("src", "${webDevServerUrl}/__web-dev-server__web-socket.js");
@@ -42,6 +45,9 @@ devToolsScript.setAttribute("src", "${webDevServerUrl}/src/main/resources/META-I
 document.body.append(devToolsScript);
 `;
 
-fs.writeFileSync(vaadinFilePath, vaadinFileContent, 'utf8');
-
-console.log(`✅ Patched ${vaadinFilePath}`);
+  const patchedVaadinFileContent = vaadinFileContent.replace(devToolsImport, patchContent);
+  fs.writeFileSync(vaadinFilePath, patchedVaadinFileContent, 'utf8');
+  console.log(`✅ Patched ${vaadinFilePath}`);
+} else {
+  console.log(`❌ Failed to patch ${vaadinFilePath}. The file does not seem to contain an import for the dev tools.`);
+}
