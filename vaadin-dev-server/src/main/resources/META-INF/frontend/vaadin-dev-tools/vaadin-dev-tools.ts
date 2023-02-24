c3f33ac00f2e79c7108fa0aa8ef52612c8c10e35
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-02-24 08:39:00 +0100

    refactor: show notice for missing theme in theme editor (#16014)

--- vaadin-dev-server/src/main/resources/META-INF/frontend/vaadin-dev-tools/vaadin-dev-tools.ts
+++ vaadin-dev-server/src/main/resources/META-INF/frontend/vaadin-dev-tools/vaadin-dev-tools.ts
@@ -1634,6 +1634,7 @@ export class VaadinDevTools extends LitElement {
 
   renderThemeEditor() {
     return html` <vaadin-dev-tools-theme-editor
+      .themeEditorState=${this.themeEditorState}
       .pickerProvider=${() => this.componentPicker}
     ></vaadin-dev-tools-theme-editor>`;
   }
