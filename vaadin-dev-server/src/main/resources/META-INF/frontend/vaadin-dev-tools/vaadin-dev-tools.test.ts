a20e7fde992714faae2a65593db63c0a56b2f4f1
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-03-02 14:03:53 +0100

    refactor: save theme editor changes immediately

--- vaadin-dev-server/src/main/resources/META-INF/frontend/vaadin-dev-tools/vaadin-dev-tools.test.ts
+++ vaadin-dev-server/src/main/resources/META-INF/frontend/vaadin-dev-tools/vaadin-dev-tools.test.ts
@@ -80,8 +80,7 @@ describe('vaadin-dev-tools', function () {
       });
 
       it('should temporarily disable live reload on theme editor before save event', async () => {
-        expect(VaadinDevTools.isActive).to.be.true;
-
+        // enable and open theme editor tab
         sendThemeEditorStateMessage(ThemeEditorState.enabled);
         await elementUpdated(devTools);
 
@@ -92,11 +91,40 @@ describe('vaadin-dev-tools', function () {
         const editor = devTools.shadowRoot!.querySelector('vaadin-dev-tools-theme-editor')!;
         expect(editor).to.exist;
 
+        // simulate saving
+        expect(VaadinDevTools.isActive).to.be.true;
         editor.dispatchEvent(new CustomEvent('before-save'));
         expect(VaadinDevTools.isActive).to.be.false;
 
-        clock.tick(5000);
+        clock.tick(2500);
+        expect(VaadinDevTools.isActive).to.be.true;
+      });
 
+      it('should extend disabling live reload when theme editor saves again', async () => {
+        // enable and open theme editor tab
+        sendThemeEditorStateMessage(ThemeEditorState.enabled);
+        await elementUpdated(devTools);
+
+        const themeEditorTab = getTab('theme-editor');
+        themeEditorTab.click();
+        await elementUpdated(devTools);
+
+        const editor = devTools.shadowRoot!.querySelector('vaadin-dev-tools-theme-editor')!;
+        expect(editor).to.exist;
+
+        // simulate saving
+        expect(VaadinDevTools.isActive).to.be.true;
+        editor.dispatchEvent(new CustomEvent('before-save'));
+        expect(VaadinDevTools.isActive).to.be.false;
+
+        clock.tick(2000);
+        editor.dispatchEvent(new CustomEvent('before-save'));
+        expect(VaadinDevTools.isActive).to.be.false;
+
+        clock.tick(1000);
+        expect(VaadinDevTools.isActive).to.be.false;
+
+        clock.tick(1500);
         expect(VaadinDevTools.isActive).to.be.true;
       });
     });
