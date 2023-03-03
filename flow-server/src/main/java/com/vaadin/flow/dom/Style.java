0026425bcc95de520d3d6eaf4539213943923627
Matti Tahvonen <matti@vaadin.com>
2023-03-03 17:03:58 +0200

    Adds helper methods for commonly used CSS properties
    The current Style API is basically a dummy string-string map. These helper methods
    makes finding the right property value easier for Java users and prevents making typos
    in the property names.

--- flow-server/src/main/java/com/vaadin/flow/dom/Style.java
+++ flow-server/src/main/java/com/vaadin/flow/dom/Style.java
@@ -25,6 +25,7 @@ import java.util.stream.Stream;
  * @since 1.0
  */
 public interface Style extends Serializable {
+
     /**
      * Gets the value of the given style property.
      * <p>
@@ -96,5 +97,425 @@ public interface Style extends Serializable {
      * @return a stream of defined style property names
      */
     Stream<String> getNames();
+
+    /**
+     * Sets the <code>background</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-background
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setBackground(String value) {
+        set("background", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>border</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-border
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setBorder(String value) {
+        set("border", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>box-sizing</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-box-sizing
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setBoxSizing(String value) {
+        set("box-sizing", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>box-shadow</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-box-shadow
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setBoxShadow(String value) {
+        set("box-shadow", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>clear</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setClear(String value) {
+        set("clear", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>cursor</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-cursor
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setCursor(String value) {
+        set("cursor", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>color</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setColor(String value) {
+        set("color", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>display</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-display
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setDisplay(String value) {
+        set("display", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>float</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setFloat(String value) {
+        set("float", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>font</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setFont(String value) {
+        set("font", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>height</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setHeight(String value) {
+        set("height", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>margin</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setMargin(String value) {
+        set("margin", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>outline</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-outline
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setOutline(String value) {
+        set("outline", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>opacity</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setOpacity(String value) {
+        set("opacity", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>overflow</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setOverflow(String value) {
+        set("overflow", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>padding</code> property. * @param value the style property
+     * value (if <code>null</code>, the property will be removed)
+     *
+     * @return this style instance
+     */
+    default Style setPadding(String value) {
+        set("padding", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>position</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setPosition(String value) {
+        set("positio ", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>scale</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-scale
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setScale(String value) {
+        set("scael", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>text-align</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-text-align
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setTextAlign(String value) {
+        set("text-align", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>text-decoration</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-text-decoration
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setTextDecoration(String value) {
+        set("text-decoration", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>transform</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-transform
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setTransform(String value) {
+        set("transform", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>transition</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-transition
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setTransition(String value) {
+        set("transition", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>visibility</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-visibility
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setVisibility(String value) {
+        set("visibility", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>width</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setWidth(String value) {
+        set("width", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>white-space</code> property.
+     *
+     * @see https://drafts.csswg.org/css-backgrounds-3/#propdef-white-space
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setWhiteSpace(String value) {
+        set("white-space", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>left</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setLeft(String value) {
+        set("left", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>right</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setRight(String value) {
+        set("right", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>top</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setTop(String value) {
+        set("top", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>bottom</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setBottom(String value) {
+        set("bottom", value);
+        return this;
+    }
+
+    /**
+     * Sets the <code>z-index</code> property.
+     *
+     * @param value
+     *            the style property value (if <code>null</code>, the property
+     *            will be removed)
+     * @return this style instance
+     */
+    default Style setZIndex(Integer value) {
+        if (value == null) {
+            remove("z-index");
+        } else {
+            set("z-index", "" + value);
+        }
+        return this;
+    }
 
 }
