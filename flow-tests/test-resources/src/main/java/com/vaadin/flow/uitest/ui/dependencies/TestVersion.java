6296a7ec0940e4375bb2b9b34ca4740618f09583
Mikhail Shabarov <mikhail@vaadin.com>
2023-03-06 14:42:29 +0200

    chore: Add package and package lock files in tests to VCS

--- flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
+++ flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
@@ -2,7 +2,7 @@ package com.vaadin.flow.uitest.ui.dependencies;
 
 public class TestVersion {
 
-    public static final String VAADIN = "24.0.0-alpha13";
+    public static final String VAADIN = "24.0.0-rc2";
     public static final String FONTAWESOME = "5.15.1";
 
 }
