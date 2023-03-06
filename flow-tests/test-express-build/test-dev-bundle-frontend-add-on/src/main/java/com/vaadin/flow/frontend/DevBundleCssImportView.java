7a5d192c88b93cc13a109038fd778cb8726aaf13
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2023-03-06 14:22:01 +0200

    fix: Not throw for not yet copied CSS when checking for re-bundling (CP: 24.0) (#16142)
    * fix: Not throw for not yet copied CSS when checking for re-bundling
    
    * Add tests for checking no exception and styles are applied
    
    * fix formatting
    
    * fix expected hash

--- flow-tests/test-express-build/test-dev-bundle-frontend-add-on/src/main/java/com/vaadin/flow/frontend/DevBundleCssImportView.java
+++ flow-tests/test-express-build/test-dev-bundle-frontend-add-on/src/main/java/com/vaadin/flow/frontend/DevBundleCssImportView.java
@@ -24,11 +24,17 @@ import com.vaadin.flow.router.Route;
 @CssImport("./styles/my-styles.css")
 public class DevBundleCssImportView extends Div {
 
+    static final String MY_COMPONENT_ID = "test-css-import-meta-inf-resources-span";
+
     static final String SPAN_ID = "test-css-import-frontend-span";
 
     public DevBundleCssImportView() {
         Span span = new Span("Test CssImport with dev bundle");
         span.setId(SPAN_ID);
         add(span);
+
+        MyComponent myComponent = new MyComponent();
+        myComponent.setId(MY_COMPONENT_ID);
+        add(myComponent);
     }
 }
--- flow-tests/test-express-build/test-dev-bundle-frontend-add-on/src/test/java/com/vaadin/flow/frontend/DevBundleCssImportIT.java
+++ flow-tests/test-express-build/test-dev-bundle-frontend-add-on/src/test/java/com/vaadin/flow/frontend/DevBundleCssImportIT.java
@@ -35,7 +35,7 @@ import elemental.json.JsonObject;
 public class DevBundleCssImportIT extends ChromeBrowserTest {
 
     @Test
-    public void cssImportedStyles_hashCalculatedWithNotQuestionMark()
+    public void cssImportedStyles_hashCalculatedWithNoQuestionMark()
             throws IOException {
         open();
         waitForElementPresent(By.id(DevBundleCssImportView.SPAN_ID));
@@ -60,6 +60,37 @@ public class DevBundleCssImportIT extends ChromeBrowserTest {
             }
         }
         Assert.assertTrue("My-styles.css import is expected", found);
+    }
+
+    @Test
+    public void cssImportedStyles_stylesInMetaInfResources_stylesApplied_hashCalculated()
+            throws IOException {
+        open();
+
+        waitForElementPresent(By.id(DevBundleCssImportView.MY_COMPONENT_ID));
+        WebElement myComponent = findElement(
+                By.id(DevBundleCssImportView.MY_COMPONENT_ID));
+
+        Assert.assertEquals("1px solid rgb(0, 128, 0)",
+                myComponent.getCssValue("border"));
+
+        JsonObject frontendHashes = getFrontendHashes();
+
+        Assert.assertTrue("Add-on styles content hash is expected",
+                frontendHashes.hasKey("addons-styles/add-on-styles.css"));
+        Assert.assertEquals("Unexpected addon styles content hash",
+                "f6062ef78e2712e881faa15252bf001d737ab4f12b12e91f0d9f8030100643b6",
+                frontendHashes.getString("addons-styles/add-on-styles.css"));
+
+        JsonArray bundleImports = getBundleImports();
+        boolean found = false;
+        for (int i = 0; i < bundleImports.length(); i++) {
+            if (bundleImports.get(i).asString().equals(
+                    "Frontend/generated/jar-resources/addons-styles/add-on-styles.css")) {
+                found = true;
+            }
+        }
+        Assert.assertTrue("Addon import is expected", found);
     }
 
     private static JsonObject getFrontendHashes() throws IOException {
