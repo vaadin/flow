6bbb6f7797d3440b691002602aa2dc54d8bfd203
Artur Signell <artur@vaadin.com>
2023-03-19 18:10:49 +0200

    refactor: Extract theme utils from the generated theme

--- flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
+++ flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
@@ -36,69 +36,6 @@ const stylesCssFile = 'styles.css';
 const headerImport = `import 'construct-style-sheets-polyfill';
 `;
 
-const createLinkReferences = `
-const createLinkReferences = (css, target) => {
-  // Unresolved urls are written as '@import url(text);' or '@import "text";' to the css
-  // media query can be present on @media tag or on @import directive after url
-  // Note that with Vite production build there is no space between @import and "text"
-  // [0] is the full match
-  // [1] matches the media query
-  // [2] matches the url
-  // [3] matches the quote char surrounding in '@import "..."'
-  // [4] matches the url in '@import "..."'
-  // [5] matches media query on @import statement
-  const importMatcher = /(?:@media\\s(.+?))?(?:\\s{)?\\@import\\s*(?:url\\(\\s*['"]?(.+?)['"]?\\s*\\)|(["'])((?:\\\\.|[^\\\\])*?)\\3)([^;]*);(?:})?/g
-  
-  // Only cleanup if comment exist
-  if(/\\/\\*(.|[\\r\\n])*?\\*\\//gm.exec(css) != null) {
-    // clean up comments
-    css = stripCssComments(css);
-  }
-  
-  var match;
-  var styleCss = css;
-  
-  // For each external url import add a link reference
-  while((match = importMatcher.exec(css)) !== null) {
-    styleCss = styleCss.replace(match[0], "");
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = match[2] || match[4];
-    const media = match[1] || match[5];
-    if (media) {
-      link.media = media;
-    }
-    // For target document append to head else append to target
-    if (target === document) {
-      document.head.appendChild(link);
-    } else {
-      target.appendChild(link);
-    }
-  };
-  return styleCss;
-};
-`;
-
-const injectGlobalCssMethod = `
-// target: Document | ShadowRoot
-export const injectGlobalCss = (css, target, first) => {
-  if(target === document) {
-    const hash = getHash(css);
-    if (window.Vaadin.theme.injectedGlobalCss.indexOf(hash) !== -1) {
-      return;
-    }
-    window.Vaadin.theme.injectedGlobalCss.push(hash);
-  }
-  const sheet = new CSSStyleSheet();
-  sheet.replaceSync(createLinkReferences(css,target));
-  if (first) {
-    target.adoptedStyleSheets = [sheet, ...target.adoptedStyleSheets];
-  } else {
-    target.adoptedStyleSheets = [...target.adoptedStyleSheets, sheet];
-  }
-};
-`;
-
 /**
  * Generate the [themeName].js file for themeFolder which collects all required information from the folder.
  *
@@ -131,10 +68,8 @@ function generateThemeFile(themeFolder, themeName, themeProperties, options) {
   if (themeProperties.parent) {
     themeFile += `import {applyTheme as applyBaseTheme} from './theme-${themeProperties.parent}.generated.js';\n`;
   }
-  themeFile += `import stripCssComments from 'strip-css-comments';\n`;
 
-  themeFile += createLinkReferences;
-  themeFile += injectGlobalCssMethod;
+  themeFile += `import { injectGlobalCss } from 'Frontend/generated/theme-util.js';\n`;
 
   const imports = [];
   const globalCssCode = [];
@@ -251,44 +186,6 @@ function generateThemeFile(themeFolder, themeName, themeProperties, options) {
   }
 
   themeFile += imports.join('');
-  themeFile += `
-window.Vaadin = window.Vaadin || {};
-window.Vaadin.theme = window.Vaadin.theme || {};
-window.Vaadin.theme.injectedGlobalCss = [];
-
-/**
- * Calculate a 32 bit FNV-1a hash
- * Found here: https://gist.github.com/vaiorabbit/5657561
- * Ref.: http://isthe.com/chongo/tech/comp/fnv/
- *
- * @param {string} str the input value
- * @returns {string} 32 bit (as 8 byte hex string)
- */
-function hashFnv32a(str) {
-  /*jshint bitwise:false */
-  let i, l, hval = 0x811c9dc5;
-
-  for (i = 0, l = str.length; i < l; i++) {
-    hval ^= str.charCodeAt(i);
-    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
-  }
-
-  // Convert to 8 digit hex string
-  return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
-}
-
-/**
- * Calculate a 64 bit hash for the given input.
- * Double hash is used to significantly lower the collision probability.
- *
- * @param {string} input value to get hash for
- * @returns {string} 64 bit (as 16 byte hex string)
- */
-function getHash(input) {
-  let h1 = hashFnv32a(input); // returns 32 bit (as 8 byte hex string)
-  return h1 + hashFnv32a(h1 + input); 
-}
-`;
 
   // Don't format as the generated file formatting will get wonky!
   // If targets check that we only register the style parts once, checks exist for global css and component css
