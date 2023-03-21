bcccb332cbd4850d853bd454a13eb3c95bdcaafc
Artur Signell <artur@vaadin.com>
2023-03-21 15:56:32 +0200

    format

--- flow-tests/test-express-build/test-dev-bundle-no-plugin/src/main/java/com/vaadin/flow/testnpmonlyfeatures/nobuildmojo/NoAppBundleView.java
+++ flow-tests/test-express-build/test-dev-bundle-no-plugin/src/main/java/com/vaadin/flow/testnpmonlyfeatures/nobuildmojo/NoAppBundleView.java
@@ -5,8 +5,8 @@ import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
 
 @Route("com.vaadin.flow.testnpmonlyfeatures.nobuildmojo.NoAppBundleView")
-public class NoAppBundleView extends Div{
-    
+public class NoAppBundleView extends Div {
+
     public NoAppBundleView() {
         add(new Span("Hello"));
     }
