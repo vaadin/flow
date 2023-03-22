986de03d547c8b467806b76ab2b376379c4e4af6
Artur Signell <artur@vaadin.com>
2023-03-22 09:55:45 +0200

    Fix tests

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeUpdaterTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeUpdaterTest.java
@@ -348,7 +348,7 @@ public class NodeUpdaterTest {
                 + "\"vaadin\": {"
                   + "\"dependencies\": {"
                     + "\"lit\": \"2.0.0\","
-                    + "\"@vaadin/router\": \"1.7.4\","
+                    + "\"@vaadin/router\": \"1.7.5\","
                     + "\"@polymer/polymer\": \"3.4.1\","
                   + "},"
                   + "\"devDependencies\": {"
@@ -358,7 +358,7 @@ public class NodeUpdaterTest {
                 + "},"
                 + "\"dependencies\": {"
                   + "\"lit\": \"2.0.0\","
-                  + "\"@vaadin/router\": \"1.7.4\","
+                  + "\"@vaadin/router\": \"1.7.5\","
                   + "\"@polymer/polymer\": \"3.4.1\","
                 + "},"
                 + "\"devDependencies\": {"
@@ -377,7 +377,7 @@ public class NodeUpdaterTest {
         final JsonObject versionsJson = Json.parse(FileUtils.readFileToString(
                 generatedVersionsFile, StandardCharsets.UTF_8));
         Assert.assertEquals(
-                "{" + "\"lit\":\"2.0.0\"," + "\"@vaadin/router\":\"1.7.4\","
+                "{" + "\"lit\":\"2.0.0\"," + "\"@vaadin/router\":\"1.7.5\","
                         + "\"@polymer/polymer\":\"3.4.1\","
                         + "\"css-loader\":\"4.2.1\","
                         + "\"file-loader\":\"6.1.0\"" + "}",
