953d0aadf078116e15d612ef098fd6eb087edcec
Peter Czuczor <czuczor@gmail.com>
2023-03-22 22:35:22 +0100

    Locale.US is the first element of this list: List.of(Locale.US, Locale.CANADA_FRENCH);  Not CANADA_FRENCH (night facepalm for myself)  Should fix this error:  "java.lang.AssertionError: First provided locale should be returned expected:<fr_CA> but was:<en_US>""

--- flow-server/src/test/java/com/vaadin/flow/component/ComponentTest.java
+++ flow-server/src/test/java/com/vaadin/flow/component/ComponentTest.java
@@ -356,7 +356,7 @@ public class ComponentTest {
         Component test = new TestButton();
         final Locale locale = test.getLocale();
         Assert.assertEquals("First provided locale should be returned",
-                Locale.CANADA_FRENCH, locale);
+                Locale.US, locale);
     }
 
     @Test
