5408e223ffffbe03e91e53d1349c5834808d2d5f
Artur Signell <artur@vaadin.com>
2023-04-06 10:04:33 +0300

    CSS rewrite tests and fixes...

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/CssBundler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/CssBundler.java
@@ -3,6 +3,8 @@ package com.vaadin.flow.server.frontend;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,16 +14,50 @@ import org.slf4j.LoggerFactory;
 
 public class CssBundler {
 
-    private static Pattern urlMatcher = Pattern
-            .compile("@import\\s*(url\\(\\s*)(\\'|\\\")?(\\S*)(\\2\\s*\\));?");
+    private static final String WHITE_SPACE = "\\s*";
+
+    private static final String STRING = "(.*)";
+
+    private static final String CSS_STRING = "('" + STRING + "'|\"" + STRING
+            + "\"|" + STRING + ")";
+    private static final String QUOTED_CSS_STRING = "('" + STRING + "'|\""
+            + STRING + "\")";
+
+    private static final String URL = "url" + WHITE_SPACE + "\\(" + WHITE_SPACE
+            + CSS_STRING + WHITE_SPACE + "\\)";
+
+    private static final String URL_OR_STRING = "(" + URL + "|"
+            + QUOTED_CSS_STRING + ")";
+
+    private static final String LAYER = "(layer|layer" + WHITE_SPACE
+            + "\\((.*?)\\))";
+    private static final String MEDIA_QUERY = "(\\s+[a-zA-Z].*)?";
+    private static final String MAYBE_LAYER_OR_MEDIA_QUERY = "(" + LAYER + "|"
+            + MEDIA_QUERY + ")";
+
+    private static Pattern urlMatcher = Pattern.compile("@import" + WHITE_SPACE
+            + URL_OR_STRING + MAYBE_LAYER_OR_MEDIA_QUERY + WHITE_SPACE + ";");
 
     public static String inlineImports(File cssFile) throws IOException {
         String content = FileUtils.readFileToString(cssFile,
                 StandardCharsets.UTF_8);
         Matcher matcher = urlMatcher.matcher(content);
         return matcher.replaceAll(result -> {
-            String url = result.group(3);
-            File potentialFile = new File(cssFile.getParentFile(), url);
+            // Oh the horror
+            // Group 3,4,5 are url() urls with different quotes
+            // Group 7,8 are strings with different quotes
+            // Group 9 is layer info
+            String layerOrMediaQueryInfo = result.group(9);
+            if (layerOrMediaQueryInfo != null
+                    && !layerOrMediaQueryInfo.trim().isEmpty()) {
+                return result.group();
+            }
+            String url = getNonNullGroup(result, 3, 4, 5, 7, 8);
+            if (url == null) {
+                return result.group();
+            }
+
+            File potentialFile = new File(cssFile.getParentFile(), url.trim());
             if (potentialFile.exists()) {
                 try {
                     return inlineImports(potentialFile);
@@ -33,6 +69,16 @@ public class CssBundler {
             return result.group();
         });
 
+    }
+
+    private static String getNonNullGroup(MatchResult result, int... groupId) {
+        for (int i : groupId) {
+            String res = result.group(i);
+            if (res != null) {
+                return res;
+            }
+        }
+        return null;
     }
 
     private static Logger getLogger() {
---
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/CssBundlerTest.java
@@ -0,0 +1,80 @@
+package com.vaadin.flow.server.frontend;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CssBundlerTest {
+
+    private static final String TEST_CSS = "body {background: blue};";
+
+    @Test
+    public void differentImportSyntaxesSupported() throws Exception {
+        String[] validImports = new String[] { //
+                // The typical you actually use
+                "@import url(foo.css);", //
+                "@import url('foo.css');", //
+                "@import url(\"foo.css\");", //
+                "@import 'foo.css';", //
+                "@import \"foo.css\";", //
+
+                // Invalid according to spec, works in browser
+                "@import url(foo.css);", //
+
+                // Extra whitespace
+                "  @import    url  (  '  foo.css  '  )  ;   ", //
+                "   @import   '  foo.css   '   ;", //
+        };
+
+        for (String valid : validImports) {
+            assertImportWorks(valid);
+        }
+    }
+
+    @Test
+    public void layerImportsNotHandled() throws IOException {
+        assertImportNotHandled("@import url('foo.css') layer(foo);");
+        assertImportNotHandled("@import url('foo.css') layer(foo) ;");
+        assertImportNotHandled("@import 'theme.css' layer(utilities);");
+        assertImportNotHandled("@import \"theme.css\" layer();");
+        assertImportNotHandled("@import \"style.css\" layer;");
+        assertImportNotHandled("@import \"style.css\" layer print;");
+    }
+
+    @Test
+    public void conditionalImportsNotHandled() throws IOException {
+        assertImportNotHandled("@import url('foo.css') print;");
+        assertImportNotHandled("@import url('bluish.css') print, screen;");
+        assertImportNotHandled("@import \"common.css\" screen;");
+        assertImportNotHandled(
+                "@import url('landscape.css') screen and (orientation: landscape);");
+    }
+
+    private void assertImportWorks(String importCss) throws IOException {
+        File f = writeFileWithImport(importCss, "foo.css");
+        Assert.assertEquals(importCss, TEST_CSS.trim(),
+                CssBundler.inlineImports(f).trim());
+
+    }
+
+    private void assertImportNotHandled(String importCss) throws IOException {
+        File f = writeFileWithImport(importCss, "foo.css");
+        Assert.assertEquals(importCss, CssBundler.inlineImports(f));
+
+    }
+
+    private File writeFileWithImport(String css, String otherFilename)
+            throws IOException {
+        File dir = Files.createTempDirectory("cssbundlertest").toFile();
+        File stylesCss = new File(dir, "styles.css");
+        FileUtils.writeStringToFile(stylesCss, css, StandardCharsets.UTF_8);
+        FileUtils.writeStringToFile(new File(dir, otherFilename), TEST_CSS,
+                StandardCharsets.UTF_8);
+        return stylesCss;
+    }
+}
