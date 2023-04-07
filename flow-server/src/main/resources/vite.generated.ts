39fe186de276f540bdc3b78bc67cb56c09fce925
Marco Collovati <marco@vaadin.com>
2023-04-07 09:56:24 +0200

    vite seems not to be affected by the issue

--- flow-server/src/main/resources/vite.generated.ts
+++ flow-server/src/main/resources/vite.generated.ts
@@ -171,11 +171,12 @@ function statsExtracterPlugin(): PluginOption {
     enforce: 'post',
     async writeBundle(options: OutputOptions, bundle: { [fileName: string]: AssetInfo | ChunkInfo }) {
       const modules = Object.values(bundle).flatMap((b) => (b.modules ? Object.keys(b.modules) : []));
-      const nodeModulesFolders = modules.filter((id) => id.includes('node_modules'));
+      const nodeModulesFolders = modules.
+          filter((id) => id.includes('node_modules'));
       const npmModules = nodeModulesFolders
         .map((id) => id.replace(/.*node_modules./, ''))
         .map((id) => {
-          const parts = id.split(path.sep);
+          const parts = id.split('/');
           if (id.startsWith('@')) {
             return parts[0] + '/' + parts[1];
           } else {
