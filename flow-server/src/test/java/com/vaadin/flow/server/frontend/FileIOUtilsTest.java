b86612736ed3a84fb1b4e246c97c873282864b2f
Artur Signell <artur@vaadin.com>
2023-04-18 11:38:46 +0300

    fix: Form a correct path for the project folder when on Windows
    Fixes #16605

---
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/FileIOUtilsTest.java
@@ -0,0 +1,35 @@
+package com.vaadin.flow.server.frontend;
+
+import java.io.File;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
+
+import com.vaadin.open.OSUtils;
+
+public class FileIOUtilsTest {
+
+    @Test
+    public void projectFolderOnWindows() throws Exception {
+        Assume.assumeTrue(OSUtils.isWindows());
+
+        URL url = new URL(
+                "file:/C:/Users/John%20Doe/Downloads/my-app%20(21)/my-app/target/classes/");
+        Assert.assertEquals(new File(
+                "C:\\Users\\John Doe\\Downloads\\my-app (21)\\my-app\\target\\classes"),
+                FileIOUtils.getProjectFolderFromClasspath(url));
+    }
+
+    @Test
+    public void projectFolderOnMacOrLinux() throws Exception {
+        Assume.assumeFalse(OSUtils.isWindows());
+
+        URL url = new URL(
+                "file:/Users/John%20Doe/Downloads/my-app%20(21)/my-app/target/classes/");
+        Assert.assertEquals(
+                new File("/Users/John Doe/Downloads/my-app (21)/my-app"),
+                FileIOUtils.getProjectFolderFromClasspath(url));
+    }
+}
