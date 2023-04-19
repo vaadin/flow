1e93145dd580f323fc7f12b528513fbed86b08a5
Mikael Grankvist <mikael.grankvist@vaadin.com>
2023-04-19 11:08:21 +0300

    format

--- flow-tests/test-express-build/test-prod-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
+++ flow-tests/test-express-build/test-prod-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
@@ -20,10 +20,11 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+
 @Theme("vaadin-prod-bundle")
 @PWA(name = "vaadin-prod-bundle", shortName = "vaadin-prod-bundle")
 @JsModule("@vaadin-component-factory/vcf-nav")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
-public class FakeAppConf implements AppShellConfigurator{
+public class FakeAppConf implements AppShellConfigurator {
 
 }
