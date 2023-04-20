b1847deac09aeebd9c8631870e7b1ea3a95ad0dc
Tatu Lund <tatu@vaadin.com>
2023-04-20 14:42:05 +0300

    Refactor ClickEvent to extend AbstractClickEvent

--- flow-server/src/main/java/com/vaadin/flow/component/ClickEvent.java
+++ flow-server/src/main/java/com/vaadin/flow/component/ClickEvent.java
@@ -25,20 +25,9 @@ package com.vaadin.flow.component;
  *            The source component type
  */
 @DomEvent("click")
-public class ClickEvent<C extends Component> extends ComponentEvent<C> {
-
-    private final int screenX;
-    private final int screenY;
-
-    private final int clientX;
-    private final int clientY;
+public class ClickEvent<C extends Component> extends AbstractClickEvent<C> {
 
     private final int clickCount;
-    private final int button;
-    private final boolean ctrlKey;
-    private final boolean shiftKey;
-    private final boolean altKey;
-    private final boolean metaKey;
 
     /**
      * Creates a new click event.
@@ -90,17 +79,9 @@ public class ClickEvent<C extends Component> extends ComponentEvent<C> {
             @EventData("event.shiftKey") boolean shiftKey,
             @EventData("event.altKey") boolean altKey,
             @EventData("event.metaKey") boolean metaKey) {
-        super((C) source, fromClient);
-        this.screenX = screenX;
-        this.screenY = screenY;
-        this.clientX = clientX;
-        this.clientY = clientY;
+        super(source, fromClient, screenX, screenY, clientX, clientY, button,
+                ctrlKey, shiftKey, altKey, metaKey);
         this.clickCount = clickCount;
-        this.button = button;
-        this.ctrlKey = ctrlKey;
-        this.shiftKey = shiftKey;
-        this.altKey = altKey;
-        this.metaKey = metaKey;
     }
 
     /**
@@ -115,110 +96,12 @@ public class ClickEvent<C extends Component> extends ComponentEvent<C> {
     }
 
     /**
-     * Gets the x coordinate of the click event, relative to the upper left
-     * corner of the browser viewport.
-     *
-     * @return the x coordinate, -1 if unknown
-     */
-    public int getClientX() {
-        return clientX;
-    }
-
-    /**
-     * Gets the y coordinate of the click event, relative to the upper left
-     * corner of the browser viewport.
-     *
-     * @return the y coordinate, -1 if unknown
-     */
-    public int getClientY() {
-        return clientY;
-    }
-
-    /**
-     * Gets the x coordinate of the click event, relative to the upper left
-     * corner of the screen.
-     *
-     * @return the x coordinate, -1 if unknown
-     */
-    public int getScreenX() {
-        return screenX;
-    }
-
-    /**
-     * Gets the y coordinate of the click event, relative to the upper left
-     * corner of the screen.
-     *
-     * @return the y coordinate, -1 if unknown
-     */
-    public int getScreenY() {
-        return screenY;
-    }
-
-    /**
      * Gets the number of consecutive clicks recently recorded.
      *
      * @return the click count
      */
     public int getClickCount() {
         return clickCount;
-    }
-
-    /**
-     * Gets the id of the pressed mouse button.
-     * <ul>
-     * <li>-1: No button
-     * <li>0: The primary button, typically the left mouse button
-     * <li>1: The middle button,
-     * <li>2: The secondary button, typically the right mouse button
-     * <li>3: The first additional button, typically the back button
-     * <li>4: The second additional button, typically the forward button
-     * <li>5+: More additional buttons without any typical meanings
-     * </ul>
-     *
-     * @return the button id, or -1 if no button was pressed
-     */
-    public int getButton() {
-        return button;
-    }
-
-    /**
-     * Checks whether the ctrl key was was down when the event was fired.
-     *
-     * @return <code>true</code> if the ctrl key was down when the event was
-     *         fired, <code>false</code> otherwise
-     */
-    public boolean isCtrlKey() {
-        return ctrlKey;
-    }
-
-    /**
-     * Checks whether the alt key was was down when the event was fired.
-     *
-     * @return <code>true</code> if the alt key was down when the event was
-     *         fired, <code>false</code> otherwise
-     */
-    public boolean isAltKey() {
-        return altKey;
-    }
-
-    /**
-     * Checks whether the meta key was was down when the event was fired.
-     *
-     * @return <code>true</code> if the meta key was down when the event was
-     *         fired, <code>false</code> otherwise
-     */
-    public boolean isMetaKey() {
-        return metaKey;
-    }
-
-    /**
-     * Checks whether the shift key was was down when the event was fired.
-     *
-     * @return <code>true</code> if the shift key was down when the event was
-     *         fired, <code>false</code> otherwise
-     */
-    public boolean isShiftKey() {
-        return shiftKey;
     }
 
 }
