4023b085afd313c850938c83e7225a36f4e9796b
Tatu Lund <tatu@vaadin.com>
2023-04-20 12:02:24 +0300

    Update test and add new test

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/ImageClickIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/ImageClickIT.java
@@ -12,6 +12,7 @@ public class ImageClickIT extends ChromeBrowserTest {
         open();
 
         WebElement message = findElement(By.id("message"));
+        WebElement message2 = findElement(By.id("message2"));
         WebElement image = findElement(By.id("image"));
 
         Assert.assertEquals("Before click", message.getText());
@@ -19,6 +20,26 @@ public class ImageClickIT extends ChromeBrowserTest {
         image.click();
 
         Assert.assertEquals("After click", message.getText());
+        Assert.assertEquals("Single click", message2.getText());
+    }
+
+    @Test
+    public void testDoubleClickOnImage() {
+        open();
+
+        Actions act = new Actions(getDriver());
+
+        WebElement message = findElement(By.id("message"));
+        WebElement message2 = findElement(By.id("message2"));
+        WebElement image = findElement(By.id("image"));
+
+        Assert.assertEquals("Before click", message.getText());
+
+        act.doubleClick(image).perform();
+
+        Assert.assertEquals("After click", message.getText());
+        Assert.assertEquals("Double click", message2.getText());
 
     }
+
 }
