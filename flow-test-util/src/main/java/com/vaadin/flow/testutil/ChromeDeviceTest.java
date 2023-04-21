8b28294f95db32aad4b620255a502b8239b82a93
Peter Czuczor <czuczor@gmail.com>
2023-04-21 20:21:36 +0200

    Selenium most up-to-date update is not letting anymore to setup networkConnectionEnabled. I have checked and we do not use it anymore it in our tests (We used back then for AirPlane mode and so, but not anymore). So we can delete the whole code now I think.

--- flow-test-util/src/main/java/com/vaadin/flow/testutil/ChromeDeviceTest.java
+++ flow-test-util/src/main/java/com/vaadin/flow/testutil/ChromeDeviceTest.java
@@ -101,19 +101,11 @@ public class ChromeDeviceTest extends ViewOrUITest {
      */
     protected ChromeOptions customizeChromeOptions(
             ChromeOptions chromeOptions) {
-        // Unfortunately using offline emulation ("setNetworkConnection"
-        // session command) in Chrome requires the "networkConnectionEnabled"
-        // capability, which is:
-        // - Not W3C WebDriver API compliant, so we disable W3C protocol
-        // - device mode: mobileEmulation option with some device settings
-
         final Map<String, Object> mobileEmulationParams = new HashMap<>();
         mobileEmulationParams.put("deviceName", "Laptop with touch");
 
-        // chromeOptions.setExperimentalOption("w3c", false);
         chromeOptions.setExperimentalOption("mobileEmulation",
                 mobileEmulationParams);
-        chromeOptions.setCapability("networkConnectionEnabled", true);
 
         if (getDeploymentHostname().equals("localhost")) {
             // Use headless Chrome for running locally
@@ -140,30 +132,6 @@ public class ChromeDeviceTest extends ViewOrUITest {
     @Override
     protected List<DesiredCapabilities> getHubBrowsersToTest() {
         return getBrowserCapabilities(Browser.CHROME);
-    }
-
-    /**
-     * Change network connection type in the browser.
-     *
-     * @param connectionType
-     *            the new connection type
-     * @throws IOException
-     */
-    protected void setConnectionType(
-            NetworkConnection.ConnectionType connectionType)
-            throws IOException {
-        RemoteWebDriver driver = (RemoteWebDriver) ((TestBenchDriverProxy) getDriver())
-                .getWrappedDriver();
-        final Map<String, Integer> parameters = new HashMap<>();
-        parameters.put("type", connectionType.hashCode());
-        final Map<String, Object> connectionParams = new HashMap<>();
-        connectionParams.put("parameters", parameters);
-        Response response = driver.getCommandExecutor()
-                .execute(new Command(driver.getSessionId(),
-                        "setNetworkConnection", connectionParams));
-        if (response.getStatus() != 0) {
-            throw new RuntimeException("Unable to set connection type");
-        }
     }
 
     public void waitForServiceWorkerReady() {
