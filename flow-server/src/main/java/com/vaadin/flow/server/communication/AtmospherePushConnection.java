35b9046448b00f11790f70259b47e414878504a9
Matti Tahvonen <matti@vaadin.com>
2023-04-27 10:05:49 +0300

    Update flow-server/src/main/java/com/vaadin/flow/server/communication/AtmospherePushConnection.java
    Co-authored-by: Marco Collovati <marco@vaadin.com>

--- flow-server/src/main/java/com/vaadin/flow/server/communication/AtmospherePushConnection.java
+++ flow-server/src/main/java/com/vaadin/flow/server/communication/AtmospherePushConnection.java
@@ -351,7 +351,7 @@ public class AtmospherePushConnection implements PushConnection {
                 } catch (TimeoutException e) {
                     if (ui.isClosing()) {
                         getLogger().debug(
-                                "Something was not sent to client on an UI that was already closed by beacon reqeuest or similar. This seems to happen with Safari occassionally when navigating away from a UI.");
+                                "Something was not sent to client on an UI that was already closed by beacon request or similar. This seems to happen with Safari occassionally when navigating away from a UI.");
                     } else {
                         getLogger().info(
                                 "Timeout waiting for messages to be sent to client before disconnect",
