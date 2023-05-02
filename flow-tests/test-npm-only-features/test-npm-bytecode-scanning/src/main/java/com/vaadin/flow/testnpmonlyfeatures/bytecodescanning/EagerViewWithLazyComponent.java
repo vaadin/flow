28879f8b63cf8c79041604d2237c6f977db0574c
Artur Signell <artur@vaadin.com>
2023-05-02 12:24:55 +0300

    fix test

--- flow-tests/test-npm-only-features/test-npm-bytecode-scanning/src/main/java/com/vaadin/flow/testnpmonlyfeatures/bytecodescanning/EagerViewWithLazyComponent.java
+++ flow-tests/test-npm-only-features/test-npm-bytecode-scanning/src/main/java/com/vaadin/flow/testnpmonlyfeatures/bytecodescanning/EagerViewWithLazyComponent.java
@@ -3,11 +3,10 @@ package com.vaadin.flow.testnpmonlyfeatures.bytecodescanning;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.dom.Element;
-import com.vaadin.flow.router.Load;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.uitest.servlet.ViewTestLayout;
 
-@Route(value = "com.vaadin.flow.testnpmonlyfeatures.bytecodescanning.EagerViewWithLazyComponent", layout = ViewTestLayout.class, dependencies = Load.ON_DEMAND)
+@Route(value = "com.vaadin.flow.testnpmonlyfeatures.bytecodescanning.EagerViewWithLazyComponent", layout = ViewTestLayout.class)
 public class EagerViewWithLazyComponent extends Div {
 
     public EagerViewWithLazyComponent() {
--- flow-tests/test-npm-only-features/test-npm-bytecode-scanning/src/main/java/com/vaadin/flow/testnpmonlyfeatures/bytecodescanning/LazyView.java
+++ flow-tests/test-npm-only-features/test-npm-bytecode-scanning/src/main/java/com/vaadin/flow/testnpmonlyfeatures/bytecodescanning/LazyView.java
@@ -5,11 +5,10 @@ import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.router.Load;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.uitest.servlet.ViewTestLayout;
 
-@Route(value = "com.vaadin.flow.testnpmonlyfeatures.bytecodescanning.LazyView", layout = ViewTestLayout.class, dependencies = Load.ON_DEMAND)
+@Route(value = "com.vaadin.flow.testnpmonlyfeatures.bytecodescanning.LazyView", layout = ViewTestLayout.class)
 public class LazyView extends Div {
 
     @Tag("lazy-component")
