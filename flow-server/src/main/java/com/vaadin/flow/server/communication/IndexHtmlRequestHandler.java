1b9614bc471ff1fcbe9ca814c932d18c8b155e7f
Artur <artur@vaadin.com>
2023-05-12 17:24:39 +0300

    fix: Run the IndexHtmlRequestListener after the whole page is complete (#16786)
    This ensures you can overwrite all parts of the index page in listeners

--- flow-server/src/main/java/com/vaadin/flow/server/communication/IndexHtmlRequestHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/server/communication/IndexHtmlRequestHandler.java
@@ -140,9 +140,6 @@ public class IndexHtmlRequestHandler extends JavaScriptBootstrapHandler {
 
         redirectToOldBrowserPageWhenNeeded(indexDocument);
 
-        // modify the page based on registered IndexHtmlRequestListener:
-        service.modifyIndexHtmlResponse(indexHtmlResponse);
-
         if (!config.isProductionMode()) {
             // Ensure no older tools incorrectly detect a bundle as production
             // mode
@@ -158,6 +155,10 @@ public class IndexHtmlRequestHandler extends JavaScriptBootstrapHandler {
 
             addLicenseChecker(indexDocument);
         }
+
+        // this invokes any custom listeners and should be run when the whole
+        // page is constructed
+        service.modifyIndexHtmlResponse(indexHtmlResponse);
 
         try {
             response.getOutputStream()
