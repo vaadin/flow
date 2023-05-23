3edb446c578d357274915cd62863713f36980037
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2023-05-23 14:21:02 +0300

    fix: Nullify a registration when listener removed (#16855)

--- flow-server/src/main/java/com/vaadin/flow/component/internal/UIInternals.java
+++ flow-server/src/main/java/com/vaadin/flow/component/internal/UIInternals.java
@@ -644,6 +644,7 @@ public class UIInternals implements Serializable {
                     pendingInvocation -> pendingInvocation.equals(invocation));
             if (invocationList.isEmpty() && registration != null) {
                 registration.remove();
+                registration = null;
             }
         }
 
