d79ae4f23667693d16e2d05d56e656a7a5fd8227
Matti Tahvonen <matti@vaadin.com>
2023-05-26 18:28:07 +0300

    replaced apache http-client with jdk http client

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsSender.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/stats/StatisticsSender.java
@@ -22,14 +22,12 @@ import java.util.concurrent.atomic.AtomicReference;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.logging.Level;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,8 +42,7 @@ public class StatisticsSender {
     /**
      * Creates a new instance connected to the given storage.
      *
-     * @param storage
-     *            the storage to use
+     * @param storage the storage to use
      */
     public StatisticsSender(StatisticsStorage storage) {
         this.storage = storage;
@@ -63,10 +60,9 @@ public class StatisticsSender {
     /**
      * Get the last server message.
      *
-     * @param json
-     *            The json returned by {@link StatisticsStorage#read()}
+     * @param json The json returned by {@link StatisticsStorage#read()}
      * @return The message string returned from server in last successful
-     *         requests.
+     * requests.
      */
     String getLastServerMessage(ObjectNode json) {
         return json.has(StatisticsConstants.FIELD_SERVER_MESSAGE)
@@ -79,8 +75,7 @@ public class StatisticsSender {
      * <p>
      * Uses <code>System.currentTimeMillis</code> as time source.
      *
-     * @param json
-     *            The json returned by {@link StatisticsStorage#read()}
+     * @param json The json returned by {@link StatisticsStorage#read()}
      * @return true if enough time has passed since the last send attempt.
      * @see #getLastSendTime()
      * @see #getInterval()
@@ -95,11 +90,10 @@ public class StatisticsSender {
     /**
      * Reads the statistics update interval.
      *
-     * @param json
-     *            The json returned by {@link StatisticsStorage#read()}
+     * @param json The json returned by {@link StatisticsStorage#read()}
      * @return Time interval in seconds.
-     *         {@link StatisticsConstants#TIME_SEC_24H} in minumun and
-     *         {@link StatisticsConstants#TIME_SEC_30D} as maximum.
+     * {@link StatisticsConstants#TIME_SEC_24H} in minumun and
+     * {@link StatisticsConstants#TIME_SEC_30D} as maximum.
      * @see StatisticsConstants#FIELD_SEND_INTERVAL
      */
     long getInterval(ObjectNode json) {
@@ -120,8 +114,7 @@ public class StatisticsSender {
      * Get interval that is between {@link StatisticsConstants#TIME_SEC_12H} and
      * {@link StatisticsConstants#TIME_SEC_30D}
      *
-     * @param intervalSec
-     *            Interval to normalize
+     * @param intervalSec Interval to normalize
      * @return <code>interval</code> if inside valid range.
      */
     private static long normalizeInterval(long intervalSec) {
@@ -135,8 +128,7 @@ public class StatisticsSender {
      * Gets the last time the data was collected according to the statistics
      * file.
      *
-     * @param json
-     *            The json returned by {@link StatisticsStorage#read()}
+     * @param json The json returned by {@link StatisticsStorage#read()}
      * @return Unix timestamp or -1 if not present
      * @see StatisticsConstants#FIELD_LAST_SENT
      */
@@ -155,8 +147,7 @@ public class StatisticsSender {
      * Gets the last time the data was collected according to the statistics
      * file.
      *
-     * @param json
-     *            The json returned by {@link StatisticsStorage#read()}
+     * @param json The json returned by {@link StatisticsStorage#read()}
      * @return Unix timestamp or -1 if not present
      * @see StatisticsConstants#FIELD_LAST_STATUS
      */
@@ -174,8 +165,7 @@ public class StatisticsSender {
     /**
      * Send data in the background if needed.
      *
-     * @param json
-     *            The json returned by {@link StatisticsStorage#read()}
+     * @param json The json returned by {@link StatisticsStorage#read()}
      */
     public void triggerSendIfNeeded(ObjectNode json) {
         // Send usage statistics asynchronously, if enough time has
@@ -198,8 +188,7 @@ public class StatisticsSender {
      * Reads the current data and posts it to given URL. Updates or replaces the
      * local data according to the response.
      *
-     * @param json
-     *            The json returned by {@link StatisticsStorage#read()}
+     * @param json The json returned by {@link StatisticsStorage#read()}
      *
      * @see #postData(String, JsonNode)
      */
@@ -271,30 +260,29 @@ public class StatisticsSender {
      * <p>
      * Updates <code>FIELD_LAST_STATUS</code>.
      *
-     * @param postUrl
-     *            URL to post data to.
-     * @param data
-     *            Json data to send
+     * @param postUrl URL to post data to.
+     * @param data Json data to send
      * @return Response or <code>data</code> if the data was not successfully
-     *         sent.
+     * sent.
      */
     private static ObjectNode postData(String postUrl, String data) {
         ObjectNode result;
         try {
-            HttpPost post = new HttpPost(postUrl);
-            post.addHeader("Content-Type", "application/json");
-            post.setEntity(new StringEntity(data));
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(postUrl))
+                    .POST(HttpRequest.BodyPublishers.ofString(data))
+                    .header("Content-Type", "application/json")
+                    .build();
 
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpResponse response = client.execute(post);
-            String responseStatus = response.getStatusLine().getStatusCode()
-                    + ": " + response.getStatusLine().getReasonPhrase();
+            HttpResponse<String> r = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            int statusCode = r.statusCode();
+
             JsonNode jsonResponse = null;
-            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                String responseString = EntityUtils
-                        .toString(response.getEntity());
+            if (statusCode == 200) {
                 jsonResponse = JsonHelpers.getJsonMapper()
-                        .readTree(responseString);
+                        .readTree(r.body());
             }
 
             if (jsonResponse != null && jsonResponse.isObject()) {
@@ -304,11 +292,11 @@ public class StatisticsSender {
                 result = JsonHelpers.getJsonMapper().createObjectNode();
             }
             // Update the status and return the results
-            result.put(StatisticsConstants.FIELD_LAST_STATUS, responseStatus);
+            result.put(StatisticsConstants.FIELD_LAST_STATUS, statusCode + ": "); // TODO is reason phrase needed here
             return result;
 
-        } catch (IOException e) {
-            getLogger().debug("Failed to send statistics.", e);
+        } catch (IOException | InterruptedException ex) {
+            getLogger().debug("Failed to send statistics.", ex);
         }
 
         // Fallback
