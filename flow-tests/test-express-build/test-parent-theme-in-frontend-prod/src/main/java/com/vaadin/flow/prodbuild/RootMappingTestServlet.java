e97bcb4cce6b28e02f8a496cde4621f32ff6632b
Mikhail Shabarov <mikhail@vaadin.com>
2023-05-26 16:38:55 +0300

    Extend servlet

--- flow-tests/test-express-build/test-parent-theme-in-frontend-prod/src/main/java/com/vaadin/flow/prodbuild/RootMappingTestServlet.java
+++ flow-tests/test-express-build/test-parent-theme-in-frontend-prod/src/main/java/com/vaadin/flow/prodbuild/RootMappingTestServlet.java
@@ -2,6 +2,8 @@ package com.vaadin.flow.prodbuild;
 
 import jakarta.servlet.annotation.WebServlet;
 
+import com.vaadin.flow.server.VaadinServlet;
+
 @WebServlet(asyncSupported = true, urlPatterns = { "/*" })
-public class RootMappingTestServlet {
+public class RootMappingTestServlet extends VaadinServlet {
 }
