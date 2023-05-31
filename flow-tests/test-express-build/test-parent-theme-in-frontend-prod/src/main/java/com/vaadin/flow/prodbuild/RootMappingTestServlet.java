e4b8417ea0f90025cb63743521f1d361a21118e2
Mikhail Shabarov <mikhail@vaadin.com>
2023-05-31 17:42:30 +0300

    Revert "Use root servlet mapping for test"
    This reverts commit 94eb7e6c70e94b19e95af175b2f6b9537e2c779f.

--- flow-tests/test-express-build/test-parent-theme-in-frontend-prod/src/main/java/com/vaadin/flow/prodbuild/RootMappingTestServlet.java
+++
@@ -1,7 +0,0 @@
-package com.vaadin.flow.prodbuild;
-
-import jakarta.servlet.annotation.WebServlet;
-
-@WebServlet(asyncSupported = true, urlPatterns = { "/*" })
-public class RootMappingTestServlet {
-}
