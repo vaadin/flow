e4cbd2f2111418b62f22ba36dd8fc0839d2104d0
Mikhail Shabarov <mikhail@vaadin.com>
2023-05-31 17:42:30 +0300

    Revert "Extend servlet"
    This reverts commit e97bcb4cce6b28e02f8a496cde4621f32ff6632b.

--- flow-tests/test-express-build/test-parent-theme-in-frontend-prod/src/main/java/com/vaadin/flow/prodbuild/RootMappingTestServlet.java
+++ flow-tests/test-express-build/test-parent-theme-in-frontend-prod/src/main/java/com/vaadin/flow/prodbuild/RootMappingTestServlet.java
@@ -2,8 +2,6 @@ package com.vaadin.flow.prodbuild;
 
 import jakarta.servlet.annotation.WebServlet;
 
-import com.vaadin.flow.server.VaadinServlet;
-
 @WebServlet(asyncSupported = true, urlPatterns = { "/*" })
-public class RootMappingTestServlet extends VaadinServlet {
+public class RootMappingTestServlet {
 }
