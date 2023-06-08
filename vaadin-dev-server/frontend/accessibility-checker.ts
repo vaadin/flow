c8355ea4990d118e14af1f52184df14f575be6cb
Matthew Wilson <matthew_w@vaadin.com>
2023-06-08 15:59:46 +0300

    Format list of messages

--- vaadin-dev-server/frontend/accessibility-checker.ts
+++ vaadin-dev-server/frontend/accessibility-checker.ts
@@ -4,10 +4,26 @@ import {customElement, property} from 'lit/decorators.js';
 import {RuleDetails} from "accessibility-checker/lib/api/IEngine";
 // @ts-ignore
 import {runAccessibilityCheck} from "./accessibility-checker-lib.js";
+import {getComponents} from "./component-util";
 
 @customElement('accessibility-checker')
 export class AccessibilityChecker extends LitElement {
-    static styles = css`p { color: pink }`;
+    static styles = css`
+        p { color: pink; }
+        
+        .button {
+            all: initial;
+            font-family: inherit;
+            font-size: var(--dev-tools-font-size-small);
+            line-height: 1;
+            white-space: nowrap;
+            background-color: rgba(255, 255, 255, 0.12);
+            color: var(--dev-tools-text-color);
+            font-weight: 600;
+            padding: 0.25rem 0.375rem;
+            border-radius: 0.25rem;
+        }
+    `;
 
     @property()
     report?: RuleDetails[];
@@ -27,33 +43,37 @@ export class AccessibilityChecker extends LitElement {
 
     render() {
         console.error("render()");
+        debugger;
         return html`
-            <button @click=${this.runTests}>Run accessibility check</button>
             ${this.report
             ? html`
-
-                        <ul>
-                            ${this.report.map(
-                (item) => this.renderItem(item))}
-                        </ul>
-          `
+                  <div>
+                      ${this.report.length} issues
+                      ${this.report.filter((issues: any) => issues.value[0] == "VIOLATION").length} violations
+                      ${this.report.filter((issues: any) => issues.value[0] == "RECOMMENDATION").length} need review
+                      ${this.report.filter((issues: any) => issues.value[0] == "INFORMATION").length} improvements
+                      <button class="button" @click=${this.runTests}>Re-run Check</button>
+                  </div>
+                  <ul>
+                      ${this.report.map((item) => this.renderItem(item))}
+                  </ul>
+            `
             : html`
-            <p>empty</p>
-          `}
+                  Click "Run check" to start the accessibility assessment.
+                  <button class="button" @click=${this.runTests}>Run Check</button>
+            `}
         `;
     }
 
 
     renderItem(issue:RuleDetails) {
         console.error("renderItem()");
-        let minIssue = {
-            message: issue.message,
-            snippet: issue.snippet,
-            value: issue.value,
-            reasonId: issue.reasonId,
-            ruleId: issue.ruleId
-        };
-        return html`<li>ISSUE: ${minIssue.ruleId}, ${minIssue.reasonId}, ${minIssue.message}, </li>
+        const componentList = getComponents(issue.node.parentElement!);
+        const component = componentList[componentList.length - 1];
+        return html`<li>
+            ${component.element?.tagName} ${issue.message}
+            <button @click="">&gt;</button>
+        </li>
         `;
     }
 }
