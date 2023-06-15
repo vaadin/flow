97b15fcade9ce2176d125ec6c9bbdc37ec80c59d
Martin Vysny <mavi@vaadin.com>
2023-06-15 13:19:10 +0300

    fix: Load Vaadin version from pom.properties instead of from vaadin-core-versions.json
    vaadin-core-versions.json file is present both in vaadin-core.jar and hilla.jar.
    For hybrid projects, the outcome of Platform.getVaadinVersion() is undeterministic.
    This causes the function to parse pom.properties file instead.
    
    Fixes #17017

--- flow-server/src/main/java/com/vaadin/flow/server/Platform.java
+++ flow-server/src/main/java/com/vaadin/flow/server/Platform.java
@@ -18,9 +18,8 @@ package com.vaadin.flow.server;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Optional;
+import java.util.Properties;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -38,25 +37,21 @@ public class Platform implements Serializable {
      * @return the platform version or {@link Optional#empty()} if unavailable.
      */
     public static Optional<String> getVaadinVersion() {
-        try (InputStream vaadinVersionsStream = Platform.class.getClassLoader()
-                .getResourceAsStream(Constants.VAADIN_CORE_VERSIONS_JSON)) {
-            if (vaadinVersionsStream != null) {
-                ObjectMapper m = new ObjectMapper();
-                JsonNode vaadinVersions = m.readTree(vaadinVersionsStream);
-                return Optional.of(vaadinVersions.get("platform").asText());
-            } else {
-                if (!versionErrorLogged) {
-                    versionErrorLogged = true;
-                    LoggerFactory.getLogger(Platform.class)
-                            .info("Unable to determine version information. "
-                                    + "No vaadin-core-versions.json found");
-                }
+        try (final InputStream vaadinPomProperties = Thread.currentThread()
+                .getContextClassLoader().getResourceAsStream(
+                        "META-INF/maven/com.vaadin/vaadin-core/pom.properties")) {
+            if (vaadinPomProperties != null) {
+                final Properties properties = new Properties();
+                properties.load(vaadinPomProperties);
+                return Optional.of(properties.getProperty("version"));
             }
         } catch (Exception e) {
-            LoggerFactory.getLogger(Platform.class)
-                    .error("Unable to determine version information", e);
+            if (!versionErrorLogged) {
+                versionErrorLogged = true;
+                LoggerFactory.getLogger(Platform.class)
+                        .info("Unable to determine Vaadin version", e);
+            }
         }
-
         return Optional.empty();
     }
 }
