f49b2e3b05272a00181fbd2222e56d59c410fbaa
Martin Vysny <mavi@vaadin.com>
2023-06-18 14:43:19 +0300

    Add tests for Platform.getVaadinVersion()

--- flow-server/src/main/java/com/vaadin/flow/server/Platform.java
+++ flow-server/src/main/java/com/vaadin/flow/server/Platform.java
@@ -20,6 +20,8 @@ import java.io.Serializable;
 import java.util.Optional;
 import java.util.Properties;
 
+import com.helger.commons.annotation.VisibleForTesting;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -28,9 +30,7 @@ import org.slf4j.LoggerFactory;
  * @since 23.0
  */
 public class Platform implements Serializable {
-
-    private static boolean versionErrorLogged = false;
-
+    private static final Logger log = LoggerFactory.getLogger(Platform.class);
     /**
      * Memoized hilla version.
      */
@@ -38,32 +38,41 @@ public class Platform implements Serializable {
     static String hillaVersion = null;
 
     /**
+     * Memoized Vaadin version.
+     */
+    @VisibleForTesting
+    static String vaadinVersion = null;
+
+    /**
      * Returns the platform version string, e.g., {@code "23.0.0"}.
      *
      * @return the platform version or {@link Optional#empty()} if unavailable.
      */
     public static Optional<String> getVaadinVersion() {
-        try (final InputStream vaadinPomProperties = Thread.currentThread()
-                .getContextClassLoader().getResourceAsStream(
-                        "META-INF/maven/com.vaadin/vaadin-core/pom.properties")) {
-            if (vaadinPomProperties != null) {
-                final Properties properties = new Properties();
-                properties.load(vaadinPomProperties);
-                return Optional.of(properties.getProperty("version"));
-            } else {
-                if (!versionErrorLogged) {
-                    versionErrorLogged = true;
-                    LoggerFactory.getLogger(Platform.class)
-                            .info("Unable to determine Vaadin version. "
-                                    + "No META-INF/maven/com.vaadin/vaadin-core/pom.properties found");
+        // thread-safe: in the worst case hillaVersion may be computed multiple
+        // times by concurrent threads.
+        if (vaadinVersion == null) {
+            try (final InputStream vaadinPomProperties = Thread.currentThread()
+                    .getContextClassLoader().getResourceAsStream(
+                            "META-INF/maven/com.vaadin/vaadin-core/pom.properties")) {
+                if (vaadinPomProperties != null) {
+                    final Properties properties = new Properties();
+                    properties.load(vaadinPomProperties);
+                    vaadinVersion = properties.getProperty("version", "");
+                } else {
+                    log.info("Unable to determine Vaadin version. "
+                            + "No META-INF/maven/com.vaadin/vaadin-core/pom.properties found");
+                    vaadinVersion = "";
                 }
+            } catch (Exception e) {
+                LoggerFactory.getLogger(Platform.class)
+                        .error("Unable to determine Vaadin version", e);
+                vaadinVersion = "";
             }
-        } catch (Exception e) {
-            LoggerFactory.getLogger(Platform.class)
-                    .error("Unable to determine Vaadin version", e);
         }
 
-        return Optional.empty();
+        return vaadinVersion.isEmpty() ? Optional.empty()
+                : Optional.of(vaadinVersion);
     }
 
     /**
@@ -84,6 +93,8 @@ public class Platform implements Serializable {
                     properties.load(hillaPomProperties);
                     hillaVersion = properties.getProperty("version", "");
                 } else {
+                    log.info("Unable to determine Vaadin version. "
+                            + "No META-INF/maven/dev.hilla/hilla/pom.properties found");
                     hillaVersion = "";
                 }
             } catch (Exception e) {
