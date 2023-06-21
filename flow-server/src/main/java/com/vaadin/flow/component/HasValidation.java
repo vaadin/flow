7d30574139e498f1270b1c0a4120211761e39519
Sergey Vinogradov <mr.vursen@gmail.com>
2023-06-21 16:23:51 +0300

    avoid using you

--- flow-server/src/main/java/com/vaadin/flow/component/HasValidation.java
+++ flow-server/src/main/java/com/vaadin/flow/component/HasValidation.java
@@ -29,8 +29,8 @@ public interface HasValidation extends Serializable {
      * Sets whether manual validation mode is enabled for the component.
      * <p>
      * When enabled, the component doesn't perform its built-in constraint
-     * validation on value change, blur, and other events. This allows you to
-     * manually control the invalid state and error messages using the
+     * validation on value change, blur, and other events. This allows
+     * manually controlling the invalid state and error messages using the
      * {@link #setInvalid(boolean)} and {@link #setErrorMessage(String)}
      * methods. Manual mode is helpful when there is a need for a totally
      * custom validation logic that cannot be achieved with Binder.
