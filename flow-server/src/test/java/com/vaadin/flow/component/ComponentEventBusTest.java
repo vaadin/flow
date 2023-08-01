afbae57ff3bdb10666c46cc0e95712186d5f2aba
Marco Collovati <marco@vaadin.com>
2023-08-01 10:41:52 +0200

    fix: fail when adding null component event listener (#17276)
    Currently, it is possible to add component event listeners pointing to a null
    reference. At runtime an NPE is thrown, but for an internal wrapper class,
    making difficult to spot where the null reference comes from.
    This change will throw immediately if null is given as a component event
    listener.

--- flow-server/src/test/java/com/vaadin/flow/component/ComponentEventBusTest.java
+++ flow-server/src/test/java/com/vaadin/flow/component/ComponentEventBusTest.java
@@ -21,7 +21,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
@@ -40,6 +42,9 @@ import elemental.json.Json;
 import elemental.json.JsonObject;
 
 public class ComponentEventBusTest {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
     private static class EventTracker<T extends ComponentEvent<?>>
             implements ComponentEventListener<T> {
@@ -739,5 +744,14 @@ public class ComponentEventBusTest {
             });
             util.verifyNoInteractions();
         }
+    }
+
+    @Test
+    public void addListener_nullListener_failFast() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("component event listener cannot be null");
+
+        final TestButton button = new TestButton();
+        button.addListener(ServerEvent.class, null);
     }
 }
