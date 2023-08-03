920f767fd7a6c4a34e70faeb69dfe13d48388419
marcin <marcin@vaadin.com>
2023-08-03 08:31:53 +0200

    chore: support for dialog, confirm-dialog and notification metadata, resolvers simplification (#17351)

--- vaadin-dev-server/frontend/theme-editor/components/component-resolver.ts
+++ vaadin-dev-server/frontend/theme-editor/components/component-resolver.ts
@@ -3,47 +3,86 @@
  *
  * Used with overlays that have different HTMLElements visible than present in node tree.
  *
- * MatcherResolvers cannot be added to component metadata as they are dynamically imported after being picked.
+ * Resolvers cannot be added to component metadata as component metadata is dynamically imported after being picked.
  */
 
-const _cookieConsentMatcherResolver = <MatcherResolver>{
-  matches: (element: HTMLElement) => {
-    return element.classList.contains('cc-banner');
-  },
-  resolves: (_element: HTMLElement) => <HTMLElement>document.querySelector('vaadin-cookie-consent')
+type Resolver = {
+  // returns source Vaadin element for given picked element
+  resolve: (element: HTMLElement) => HTMLElement | undefined;
 };
 
-const _loginFormOverlayMatcherResolver = <MatcherResolver>{
-  matches: (element: HTMLElement) => {
-    return element.localName === 'vaadin-login-overlay-wrapper';
-  },
-  resolves: (_element: HTMLElement) => <HTMLElement>document.querySelector('vaadin-login-overlay')
+const _cookieConsentResolver: Resolver = {
+  resolve: (element: HTMLElement) => {
+    const matcher = (element: HTMLElement) => element.classList.contains('cc-banner');
+    const matched = _isMatchingRecursive(matcher, element);
+    return matched ? <HTMLElement>document.querySelector('vaadin-cookie-consent') : undefined;
+  }
 };
 
-const _matcherResolvers = <MatcherResolver[]>[_cookieConsentMatcherResolver, _loginFormOverlayMatcherResolver];
+const _loginFormOverlayResolver: Resolver = {
+  resolve: (element: HTMLElement) => {
+    const matcher = (element: HTMLElement) => element.localName === 'vaadin-login-overlay-wrapper';
+    const matched = _isMatchingRecursive(matcher, element);
+    return matched ? <HTMLElement>document.querySelector('vaadin-login-overlay') : undefined;
+  }
+};
 
-type MatcherResolver = {
-  matches: (element: HTMLElement) => boolean;
-  resolves: (element: HTMLElement) => HTMLElement;
+const _dialogOverlayResolver: Resolver = {
+  resolve: (element: HTMLElement) => {
+    // @ts-ignore explicit usage of Polymer property
+    return element.localName === 'vaadin-dialog-overlay' ? <HTMLElement>_element['__dataHost'] : undefined;
+  }
+};
+
+const _confirmDialogOverlayResolver: Resolver = {
+  resolve: (element: HTMLElement) => {
+    const matcher = (element: HTMLElement) => element.localName === 'vaadin-confirm-dialog-overlay';
+    const matched = _isMatchingRecursive(matcher, element);
+    // @ts-ignore explicit usage of Polymer property
+    return matched ? <HTMLElement>matched['__dataHost'] : undefined;
+  }
+};
+
+const _notificationOverlayResolver: Resolver = {
+  resolve: (element: HTMLElement) => {
+    const matcher = (element: HTMLElement) => element.localName === 'vaadin-notification-card';
+    const matched = _isMatchingRecursive(matcher, element);
+    // @ts-ignore explicit usage of Polymer property
+    return matched ? <HTMLElement>matched['__dataHost'] : undefined;
+  }
+};
+
+const _resolvers = <Resolver[]>[
+  _cookieConsentResolver,
+  _loginFormOverlayResolver,
+  _dialogOverlayResolver,
+  _confirmDialogOverlayResolver,
+  _notificationOverlayResolver
+];
+
+// finds matching element or its parent
+const _isMatchingRecursive = function (
+  matcher: (element: HTMLElement) => boolean,
+  element: HTMLElement
+): HTMLElement | undefined {
+  if (matcher(element)) {
+    return element;
+  } else if (element.parentNode && element.parentNode instanceof HTMLElement) {
+    return _isMatchingRecursive(matcher, element.parentNode);
+  } else {
+    return undefined;
+  }
 };
 
 class ComponentResolver {
   resolveElement(element: HTMLElement) {
-    const resolved = _matcherResolvers
-      .filter((mr) => this._isMatchingRecursive(mr.matches, element))
-      .map((mr) => mr.resolves(element))
-      .pop();
-    return resolved ?? element;
-  }
-
-  _isMatchingRecursive(matcher: (element: HTMLElement) => boolean, element: HTMLElement): HTMLElement | undefined {
-    if (matcher(element)) {
-      return element;
-    } else if (element.parentNode && element.parentNode instanceof HTMLElement) {
-      return this._isMatchingRecursive(matcher, element.parentNode);
-    } else {
-      return undefined;
+    for (const i in _resolvers) {
+      let resolved: HTMLElement | undefined = element;
+      if ((resolved = _resolvers[i].resolve(element)) !== undefined) {
+        return resolved;
+      }
     }
+    return element;
   }
 }
 
