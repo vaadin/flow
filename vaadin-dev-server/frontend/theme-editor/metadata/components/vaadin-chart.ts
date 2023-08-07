24ff2b9a9a9660b81f9836e11e0ca9cb406b5598
Daniel Sarkwa <sarkwa@vaadin.com>
2023-08-07 14:01:35 +0300

    Add meta data for Chart

---
+++ vaadin-dev-server/frontend/theme-editor/metadata/components/vaadin-chart.ts
@@ -0,0 +1,64 @@
+import { ComponentMetadata, EditorType } from '../model';
+
+export default {
+  tagName: 'vaadin-chart',
+  displayName: 'Chart',
+  elements: [
+    {
+      selector: 'vaadin-chart',
+      displayName: 'Chart',
+      properties: [
+        {
+          propertyName: '--vaadin-charts-color-0',
+          displayName: 'Color 0',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-1',
+          displayName: 'Color 1',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-2',
+          displayName: 'Color 2',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-3',
+          displayName: 'Color 3',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-4',
+          displayName: 'Color 4',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-5',
+          displayName: 'Color 5',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-6',
+          displayName: 'Color 6',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-7',
+          displayName: 'Color 7',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-8',
+          displayName: 'Color 8',
+          editorType: EditorType.color
+        },
+        {
+          propertyName: '--vaadin-charts-color-9',
+          displayName: 'Color 9',
+          editorType: EditorType.color
+        }
+      ]
+    }
+  ]
+} as ComponentMetadata;
