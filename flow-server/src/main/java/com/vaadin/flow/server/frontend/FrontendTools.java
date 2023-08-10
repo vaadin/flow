07f7a07f2a97e715fcfda18d8c9ca09e3642c4e8
Artur Signell <artur@vaadin.com>
2023-08-10 10:07:27 +0300

    chore: Upgrade Node to 18.17.1
    This is a node security release

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
@@ -66,11 +66,11 @@ public class FrontendTools {
      * the installed version is older than {@link #SUPPORTED_NODE_VERSION}, i.e.
      * {@value #SUPPORTED_NODE_MAJOR_VERSION}.{@value #SUPPORTED_NODE_MINOR_VERSION}.
      */
-    public static final String DEFAULT_NODE_VERSION = "v18.16.1";
+    public static final String DEFAULT_NODE_VERSION = "v18.17.1";
     /**
      * This is the version shipped with the default Node version.
      */
-    public static final String DEFAULT_NPM_VERSION = "9.5.1";
+    public static final String DEFAULT_NPM_VERSION = "9.6.7";
 
     public static final String DEFAULT_PNPM_VERSION = "8.6.11";
 
