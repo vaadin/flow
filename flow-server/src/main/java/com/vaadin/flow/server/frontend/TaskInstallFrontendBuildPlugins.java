256ae9051f1b2041fe43bbede51e2dbae20526dc
Artur Signell <artur@vaadin.com>
2023-08-14 09:51:32 +0300

    fix: Only write Vite config files when they have changed
    Prevents Vite from restarting when not needed
    
    Fixes https://github.com/vaadin/hilla/issues/1134

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskInstallFrontendBuildPlugins.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskInstallFrontendBuildPlugins.java
@@ -21,10 +21,10 @@ import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import elemental.json.Json;
 import elemental.json.JsonArray;
 import elemental.json.JsonObject;
 
@@ -73,7 +73,7 @@ public class TaskInstallFrontendBuildPlugins implements FallibleCommand {
 
     private void generatePluginFiles(String pluginName) throws IOException {
         // Get the target folder where the plugin should be installed to
-        File pluginTargetFile = new File(targetFolder, pluginName);
+        File pluginTargetFolder = new File(targetFolder, pluginName);
 
         final String pluginFolderName = PLUGIN_TARGET + "/" + pluginName + "/";
         final JsonObject packageJson = FrontendPluginsUtil
@@ -85,35 +85,27 @@ public class TaskInstallFrontendBuildPlugins implements FallibleCommand {
             return;
         }
 
-        // Validate installed version and don't override if same
-        if (pluginTargetFile.exists()
-                && new File(pluginTargetFile, PACKAGE_JSON).exists()) {
-            String packageFile = FileUtils.readFileToString(
-                    new File(pluginTargetFile, PACKAGE_JSON),
-                    StandardCharsets.UTF_8);
-            final JsonObject targetJson = Json.parse(packageFile);
-            if (targetJson.hasKey("update")
-                    && !targetJson.getBoolean("update")) {
-                return;
-            }
-        }
-
         // Create target folder if necessary
-        FileUtils.forceMkdir(pluginTargetFile);
+        FileUtils.forceMkdir(pluginTargetFolder);
 
         // copy only files named in package.json { files }
         final JsonArray files = packageJson.getArray("files");
         for (int i = 0; i < files.length(); i++) {
             final String file = files.getString(i);
-            FileUtils.copyURLToFile(
-                    FrontendPluginsUtil.getResourceUrl(pluginFolderName + file),
-                    new File(pluginTargetFile, file));
+            copyIfNeeded(new File(pluginTargetFolder, file),
+                    pluginFolderName + file);
         }
         // copy package.json to plugin directory
-        FileUtils.copyURLToFile(
-                FrontendPluginsUtil
-                        .getResourceUrl(pluginFolderName + PACKAGE_JSON),
-                new File(pluginTargetFile, PACKAGE_JSON));
+        copyIfNeeded(new File(pluginTargetFolder, PACKAGE_JSON),
+                pluginFolderName + PACKAGE_JSON);
+    }
+
+    private void copyIfNeeded(File targetFile, String sourceResource)
+            throws IOException {
+        String content = IOUtils.toString(
+                FrontendPluginsUtil.getResourceUrl(sourceResource),
+                StandardCharsets.UTF_8);
+        FileIOUtils.writeIfChanged(targetFile, content);
     }
 
     private Logger log() {
