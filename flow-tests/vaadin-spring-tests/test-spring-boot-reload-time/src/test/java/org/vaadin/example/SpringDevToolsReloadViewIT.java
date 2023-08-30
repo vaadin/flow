271b27a8e9868675f1722b5f141c8cd1946bd359
Tomi Virtanen <tltv@vaadin.com>
2023-08-30 10:44:15 +0300

    test: Add new Spring boot reload time test module (#17464)
    * test: Add new Spring boot reload time test module
    
    New 'flow-tests/vaadin-spring-tests/test-spring-boot-reload-time' measures reload time of a tiny Spring boot application with Spring dev tools enabled and reports result via TeamCity Service Messages.
    
    * test: added another test with horizontal layout
    
    And moved classes to correct packages and removed invalid package.
    
    * refactor: refactored test class
    
    * refactor: moved dependencies to correct module
    
    * test: added flow version to teamcity messages
    
    * test: exclude test-spring-boot-reload-time module
    
    * test: add new test module for large Spring app
    
    New module 'test-spring-boot-multimodule-reload-time' under 'vaadin-spring-tests' adds multimodule Spring boot application to measure reload times with partly generated large application.
    
    * test: exclude reload time test submodules
    
    * test: include vaadin-spring-module with benchmark profile
    
    * decs: fixed path in readme
    
    * test: removed SideNav and replaced with RouterLink
    
    SideNav component is too fresh adn will not work older versions.
    
    * test: removed setAriaLabel
    
    Removed setAriaLabel call which breaks with 24.0 and probably with earlier FLow versions. It's not needed for the test.
    
    * docs: updated README.MD
    
    * docs: updated copyright texts and formatted xml
    
    * refactor: code formatting
    
    * test: Add more measurements in reload time tests (#17498)
    
    * test: add option to generate CssImport and JsModule
    
    In 'test-spring-boot-multimodule-reload-time' module, code generator supports two new properties: 'vaadin.test.codegen.maven.plugin.cssimports.per.route' and 'vaadin.test.codegen.maven.plugin.jsmodules.per.route'.
    
    * test: add option to load hierarchical routes
    
    Added product catalog view with route like 'catalog/product/0' or 'catalog/prod/0' with alias. Route is loaded by default. Added system property 'route.hierarchy.enabled' to make it optional.
    
    * test: reload time tests calculate average reload time
    
    Each reload time test runs five times and calculates and reports average.
    
    * test: increased waitUntil timeout to 20 seconds
    
    * test: clean generated js folder
    
    * test: added more logging
    
    * test: fixed generated service class name
    
    * refactor: code formatting updated copuright texts
    
    * refactor: fixed formatting
    
    ---------
    
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

---
+++ flow-tests/vaadin-spring-tests/test-spring-boot-reload-time/src/test/java/org/vaadin/example/SpringDevToolsReloadViewIT.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.vaadin.example;
+
+import com.vaadin.flow.server.Version;
+import com.vaadin.flow.spring.test.SpringDevToolsReloadUtils;
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+/**
+ * Class for testing reload time of tiny Vaadin app triggered by spring-boot Dev
+ * Tool.
+ */
+public class SpringDevToolsReloadViewIT extends ChromeBrowserTest {
+
+    @Test
+    public void testSpringBootReloadTime_withNativeButton() {
+        String result = SpringDevToolsReloadUtils
+                .runAndCalculateAverageResult(5, () -> {
+                    open("/reload-nativebutton-test");
+
+                    waitForElementPresent(By.id("start-button"));
+                    triggerReload();
+                    waitForElementVisible(By.id("result"));
+
+                    return assertAndGetReloadTimeResult();
+                });
+
+        System.out.printf(
+                "##teamcity[buildStatisticValue key='%s,nativebutton,spring-boot-devtools-reload-time' value='%s']%n",
+                getVaadinMajorMinorVersion(), result);
+    }
+
+    @Test
+    public void testSpringBootReloadTime_withHorizontalLayout() {
+        String result = SpringDevToolsReloadUtils
+                .runAndCalculateAverageResult(5, () -> {
+                    open("/reload-layout-test");
+
+                    waitForElementPresent(By.id("start-button"));
+                    triggerReload();
+                    waitForElementVisible(By.id("result"));
+
+                    return assertAndGetReloadTimeResult();
+                });
+
+        System.out.printf(
+                "##teamcity[buildStatisticValue key='%s,orderedlayout,spring-boot-devtools-reload-time' value='%s']%n",
+                getVaadinMajorMinorVersion(), result);
+    }
+
+    private void triggerReload() {
+        findElement(By.id("start-button")).click(); // trigger for reload
+    }
+
+    private void open(String testPath) {
+        getDriver().get(getTestURL(getRootURL(), testPath, null));
+        waitForDevServer();
+    }
+
+    private String assertAndGetReloadTimeResult() {
+        String reloadTimeInMsText = findElement(By.id("result")).getText();
+        Assert.assertNotNull(reloadTimeInMsText);
+
+        return reloadTimeInMsText.substring(reloadTimeInMsText.indexOf("[") + 1,
+                reloadTimeInMsText.indexOf("]"));
+    }
+
+    private String getVaadinMajorMinorVersion() {
+        return Version.getMajorVersion() + "." + Version.getMinorVersion();
+    }
+}
