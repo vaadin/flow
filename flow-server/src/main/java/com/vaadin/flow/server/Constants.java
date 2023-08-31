60c42970c3f73f9f8ae250515fa0bd652a3a4361
caalador <mikael.grankvist@vaadin.com>
2023-08-31 08:41:54 +0300

    fix: update atmosphere version (#17517)
    Update atmosphere version
    to next release.
    
    Closes #17326
    
    * Update testbench
    
    * Fix test that doesn't wait for dev server
    # Conflicts:
    #	flow-server/src/main/java/com/vaadin/flow/server/Constants.java
    #	flow-tests/vaadin-spring-tests/test-spring-common/src/test/java/com/vaadin/flow/spring/test/NPEHandlerIT.java
    #	pom.xml
    
    # Conflicts:
    #	flow-server/src/main/java/com/vaadin/flow/server/Constants.java
    #	pom.xml

--- flow-server/src/main/java/com/vaadin/flow/server/Constants.java
+++ flow-server/src/main/java/com/vaadin/flow/server/Constants.java
@@ -30,7 +30,7 @@ import com.vaadin.flow.shared.ApplicationConstants;
 public final class Constants implements Serializable {
 
     // Keep the version number in sync with flow-push/pom.xml
-    public static final String REQUIRED_ATMOSPHERE_RUNTIME_VERSION = "2.7.3.slf4jvaadin4";
+    public static final String REQUIRED_ATMOSPHERE_RUNTIME_VERSION = "2.7.3.slf4jvaadin5";
 
     /**
      * The prefix used for System property parameters.
