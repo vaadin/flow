e9e6a5f0b56edfc44269308c5e5c89ce8d97ddde
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2023-09-01 14:19:06 +0300

    move disclaimer to the exception message in form filler

--- flow-server/src/main/java/com/vaadin/experimental/FeatureFlags.java
+++ flow-server/src/main/java/com/vaadin/experimental/FeatureFlags.java
@@ -65,7 +65,7 @@ public class FeatureFlags implements Serializable {
             "com.vaadin.flow.server.webpush.WebPush");
 
     public static final Feature FORM_FILLER_ADDON = new Feature(
-            "Form Filler Add-on (This is an experimental feature. It may be removed, altered, or limited to commercial subscribers in future releases.\n)",
+            "Form Filler Add-on",
             "formFillerAddon", "https://github.com/vaadin/form-filler-addon",
             true, "com.vaadin.flow.ai.formfiller.FormFiller");
 
