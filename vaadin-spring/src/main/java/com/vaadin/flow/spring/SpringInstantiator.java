d44f8e05ae63f025818726f907a588206549e29d
Mikhail Shabarov <mikhail@vaadin.com>
2023-09-05 16:21:06 +0300

    chore: Collect usage statistics for Spring-Boot

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/SpringInstantiator.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/SpringInstantiator.java
@@ -21,6 +21,7 @@ import java.util.stream.Stream;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanInstantiationException;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.SpringVersion;
 
@@ -56,8 +57,13 @@ public class SpringInstantiator extends DefaultInstantiator {
         this.context = context;
 
         UsageStatistics.markAsUsed("flow/SpringInstantiator", null);
-        UsageStatistics.markAsUsed("SpringFramework", Optional
-                .ofNullable(SpringVersion.getVersion()).orElse("unknown"));
+        if (SpringUtil.isSpringBoot()) {
+            UsageStatistics.markAsUsed("SpringBoot",
+                    SpringBootVersion.getVersion());
+        } else {
+            UsageStatistics.markAsUsed("SpringFramework", Optional
+                    .ofNullable(SpringVersion.getVersion()).orElse("unknown"));
+        }
     }
 
     @Override
