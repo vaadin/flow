4756a182050be50eafed913745d458b1acc57c32
caalador <mikael.grankvist@vaadin.com>
2023-09-20 11:36:41 +0300

    fix: router-link postpone navigation (#17647)
    Fixes issue with update timing
    when postponing a router-link
    as the navigation goes through
    the vaadin-router.
    
    Fixes #17528

--- flow-server/src/main/java/com/vaadin/flow/component/UI.java
+++ flow-server/src/main/java/com/vaadin/flow/component/UI.java
@@ -54,6 +54,7 @@ import com.vaadin.flow.internal.nodefeature.PollConfigurationMap;
 import com.vaadin.flow.internal.nodefeature.ReconnectDialogConfigurationMap;
 import com.vaadin.flow.router.AfterNavigationListener;
 import com.vaadin.flow.router.BeforeEnterListener;
+import com.vaadin.flow.router.BeforeLeaveEvent;
 import com.vaadin.flow.router.BeforeLeaveListener;
 import com.vaadin.flow.router.ErrorNavigationEvent;
 import com.vaadin.flow.router.ErrorParameter;
@@ -1721,7 +1722,7 @@ public class UI extends Component
             navigateToClient(getForwardToClientUrl());
             acknowledgeClient();
         } else if (isPostponed()) {
-            cancelClient();
+            serverPaused();
         } else {
             acknowledgeClient();
         }
@@ -1761,6 +1762,10 @@ public class UI extends Component
 
     private void cancelClient() {
         serverConnected(true);
+    }
+
+    private void serverPaused() {
+        wrapperElement.executeJs("this.serverPaused()");
     }
 
     private void serverConnected(boolean cancel) {
