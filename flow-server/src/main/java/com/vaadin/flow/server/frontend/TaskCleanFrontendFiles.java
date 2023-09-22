129101c44e1a58c4173541f630519b151a573a3c
Artur Signell <artur@vaadin.com>
2023-09-22 12:48:51 +0300

    Cleanup pnpmfile

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskCleanFrontendFiles.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/TaskCleanFrontendFiles.java
@@ -50,7 +50,7 @@ public class TaskCleanFrontendFiles implements FallibleCommand {
     private List<String> generatedFiles = List.of(NODE_MODULES,
             Constants.PACKAGE_JSON, Constants.PACKAGE_LOCK_JSON,
             Constants.PACKAGE_LOCK_YAML, TaskGenerateTsConfig.TSCONFIG_JSON,
-            TaskGenerateTsDefinitions.TS_DEFINITIONS, ".npmrc",
+            TaskGenerateTsDefinitions.TS_DEFINITIONS, ".pnpmfile.cjs", ".npmrc",
             FrontendUtils.VITE_GENERATED_CONFIG, FrontendUtils.VITE_CONFIG);
     private Set<File> existingFiles = new HashSet<>();
 
