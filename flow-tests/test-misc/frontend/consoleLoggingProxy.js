3bf9f41175a8c6fc6fa5a7130469482bd4846dd9
Teppo Kurki <teppo.kurki@vaadin.com>
2023-10-12 14:34:01 +0300

    Add test to ensure exceptions are logged in prod mode

---
+++ flow-tests/test-misc/frontend/consoleLoggingProxy.js
@@ -0,0 +1,37 @@
+window.allLogMessages = [];
+
+const console = (function(oldConsole) {
+  return {
+    log: function(message){
+      oldConsole.log(message);
+      window.allLogMessages.push(message);
+    },
+    info: function (message) {
+      oldConsole.info(message);
+      window.allLogMessages.push(message);
+    },
+    debug: function (message) {
+      oldConsole.debug(message);
+      window.allLogMessages.push(message);
+    },
+    trace: function (message) {
+      oldConsole.trace(message);
+      window.allLogMessages.push(message);
+    },
+    warn: function (message) {
+      oldConsole.warn(message);
+      window.allLogMessages.push(message);
+    },
+    error: function (message) {
+      oldConsole.error(message);
+      window.allLogMessages.push(message);
+    }
+  };
+}(window.console));
+
+window.console = console;
+
+window.addEventListener("error", function (e) {
+  window.console.trace(e);
+  return false;
+});
