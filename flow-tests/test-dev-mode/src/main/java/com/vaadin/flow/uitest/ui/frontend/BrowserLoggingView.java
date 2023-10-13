db4b4395ee4e0721ba7549b750203c1e32a2a1c5
Teppo Kurki <teppo.kurki@vaadin.com>
2023-10-13 18:23:46 +0300

    test: Add tests for dev mode client-side logging; Clarify prod mode test (#17833)
    Add tests for dev mode client-side logging; Clarify prod mode test

--- flow-tests/test-dev-mode/src/main/java/com/vaadin/flow/uitest/ui/frontend/BrowserLoggingView.java
+++ flow-tests/test-dev-mode/src/main/java/com/vaadin/flow/uitest/ui/frontend/BrowserLoggingView.java
@@ -19,6 +19,7 @@ package com.vaadin.flow.uitest.ui.frontend;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.shared.ui.LoadMode;
@@ -32,5 +33,12 @@ public class BrowserLoggingView extends Div {
         NativeLabel label = new NativeLabel("Just a label");
         label.setId("elementId");
         add(label);
+
+        NativeButton causeException = new NativeButton(
+                "Cause client side exception", e -> {
+                    getUI().get().getPage().executeJs("null.foo");
+                });
+        causeException.setId("exception");
+        add(causeException);
     }
 }
