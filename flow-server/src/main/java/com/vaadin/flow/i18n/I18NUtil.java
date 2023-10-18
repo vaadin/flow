3ecb95efbd3dec962bbdc260be10e41047854dcd
caalador <mikael.grankvist@vaadin.com>
2023-10-18 08:27:21 +0300

    feat: Default I18N provider (#17823)
    Add implementation for a I18N
    provider that is loaded if there
    are translation properties files
    in the defined classpath folder `vaadin-i18n`.
    
    If a custom I18N is set default will
    never be loaded.
    
    Fixes #5917

---
+++ flow-server/src/main/java/com/vaadin/flow/i18n/I18NUtil.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.i18n;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Locale;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.vaadin.flow.i18n.DefaultI18NProvider.BUNDLE_FILENAME;
+import static com.vaadin.flow.i18n.DefaultI18NProvider.BUNDLE_FOLDER;
+
+/**
+ * Utility class for use with determining default i18n property files and
+ * locales.
+ * <p>
+ * For internal use only. May be renamed or removed in a future release.
+ */
+public final class I18NUtil {
+
+    public static final String PROPERTIES_SUFFIX = ".properties";
+
+    /**
+     * Check if we have a default translation properties file
+     * {@link DefaultI18NProvider#BUNDLE_FILENAME} in the folder
+     * {@link DefaultI18NProvider#BUNDLE_FOLDER}
+     * <p>
+     * For internal use only. May be renamed or removed in a future release.
+     *
+     * @return {@code true} if default property file found
+     */
+    public static boolean containsDefaultTranslation() {
+        URL resource = getClassLoader()
+                .getResource(DefaultI18NProvider.BUNDLE_FOLDER + "/"
+                        + DefaultI18NProvider.BUNDLE_FILENAME
+                        + PROPERTIES_SUFFIX);
+        if (resource == null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check that we have the translation folder
+     * {@link DefaultI18NProvider#BUNDLE_FOLDER} and collect all translation
+     * properties files. Parse names to get locales.
+     * <p>
+     * For internal use only. May be renamed or removed in a future release.
+     *
+     * @return List of locales parsed from property files.
+     */
+    public static List<Locale> getDefaultTranslationLocales() {
+        List<Locale> locales = new ArrayList<>();
+
+        URL resource = getClassLoader()
+                .getResource(DefaultI18NProvider.BUNDLE_FOLDER);
+        if (resource == null) {
+            return locales;
+        }
+
+        List<File> listedFiles = getTranslationFiles(resource).stream()
+                .filter(file -> file.getName()
+                        .startsWith(DefaultI18NProvider.BUNDLE_FILENAME)
+                        && file.getName().endsWith(PROPERTIES_SUFFIX))
+                .collect(Collectors.toList());
+        for (File file : listedFiles) {
+            String name = file.getName();
+
+            if (!name.contains("_")) {
+                // This is the default bundle and that doesn't have a
+                // locale
+                continue;
+            }
+            String langCode = name.substring(BUNDLE_FILENAME.length() + 1,
+                    name.lastIndexOf('.'));
+            String[] langParts = langCode.split("_");
+            if (langParts.length == 1) {
+                locales.add(new Locale(langParts[0]));
+            } else if (langParts.length == 2) {
+                locales.add(new Locale(langParts[0], langParts[1]));
+            } else if (langParts.length == 3) {
+                locales.add(
+                        new Locale(langParts[0], langParts[1], langParts[2]));
+            }
+        }
+        return locales;
+    }
+
+    protected static List<File> getTranslationFiles(URL resource) {
+        List<File> files = new ArrayList<>();
+
+        File bundleFolder = new File(resource.getFile());
+
+        if ("jar".equals(resource.getProtocol())) {
+            String file = resource.getFile().substring("file:".length(),
+                    resource.getFile().indexOf('!'));
+            try {
+                Enumeration<JarEntry> entries = new JarFile(file).entries();
+                entries.asIterator().forEachRemaining(entry -> {
+                    String fileName = entry.getName();
+                    if (fileName.contains(BUNDLE_FOLDER)
+                            && fileName.endsWith(PROPERTIES_SUFFIX)) {
+                        files.add(new File(fileName));
+                    }
+                });
+            } catch (IOException ioe) {
+                getLogger().debug(
+                        "failed to read jar file '" + file + "' contents", ioe);
+            }
+        } else if (bundleFolder.exists() && bundleFolder.isDirectory()) {
+            Arrays.stream(bundleFolder.listFiles()).filter(File::isFile)
+                    .forEach(files::add);
+        }
+
+        return files;
+    }
+
+    protected static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    protected static Logger getLogger() {
+        return LoggerFactory.getLogger(I18NUtil.class);
+    }
+}
---
+++ flow-server/src/test/java/com/vaadin/flow/i18n/DefaultInstantiatorI18NTest.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2000-2023 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.i18n;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import com.vaadin.flow.di.DefaultInstantiator;
+import com.vaadin.flow.di.Lookup;
+import com.vaadin.flow.server.VaadinContext;
+import com.vaadin.flow.server.VaadinService;
+
+public class DefaultInstantiatorI18NTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private File translations;
+    private ClassLoader urlClassLoader;
+    private DefaultInstantiator instantiator;
+
+    @Before
+    public void init() throws IOException {
+        File resources = temporaryFolder.newFolder();
+
+        translations = new File(resources, DefaultI18NProvider.BUNDLE_FOLDER);
+        translations.mkdirs();
+
+        urlClassLoader = new URLClassLoader(
+                new URL[] { resources.toURI().toURL() });
+    }
+
+    @After
+    public void cleanup() throws NoSuchFieldException, IllegalAccessException {
+        ResourceBundle.clearCache(urlClassLoader);
+        if (instantiator != null) {
+            Field field = DefaultInstantiator.class
+                    .getDeclaredField("i18nProvider");
+            field.setAccessible(true);
+            ((AtomicReference<I18NProvider>) field.get(instantiator)).set(null);
+            field.setAccessible(false);
+        }
+    }
+
+    @Test
+    public void translationFileOnClasspath_instantiateDefaultI18N()
+            throws IOException {
+
+        createTranslationFiles(translations);
+
+        try (MockedStatic<I18NUtil> util = Mockito.mockStatic(I18NUtil.class,
+                Mockito.CALLS_REAL_METHODS)) {
+            util.when(() -> I18NUtil.getClassLoader())
+                    .thenReturn(urlClassLoader);
+
+            VaadinService service = Mockito.mock(VaadinService.class);
+            mockLookup(service);
+
+            instantiator = new DefaultInstantiator(service);
+
+            I18NProvider i18NProvider = instantiator.getI18NProvider();
+            Assert.assertNotNull(i18NProvider);
+            Assert.assertTrue(i18NProvider instanceof DefaultI18NProvider);
+
+            Assert.assertEquals("Suomi", i18NProvider.getTranslation("title",
+                    new Locale("fi", "FI")));
+
+            Assert.assertEquals("deutsch",
+                    i18NProvider.getTranslation("title", new Locale("de")));
+
+            Assert.assertEquals(
+                    "non existing country should select language bundle",
+                    "deutsch", i18NProvider.getTranslation("title",
+                            new Locale("de", "AT")));
+
+            Assert.assertEquals("Korean", i18NProvider.getTranslation("title",
+                    new Locale("ko", "KR")));
+
+            // Note!
+            // default translations.properties will be used if
+            // the locale AND system default locale is not found
+            Assert.assertEquals("Default lang", i18NProvider
+                    .getTranslation("title", new Locale("en", "GB")));
+        }
+    }
+
+    @Test
+    public void onlyDefaultTranslation_instantiateDefaultI18N()
+            throws IOException {
+        File file = new File(translations,
+                DefaultI18NProvider.BUNDLE_FILENAME + ".properties");
+        Files.writeString(file.toPath(), "title=Default lang",
+                StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+
+        try (MockedStatic<I18NUtil> util = Mockito.mockStatic(I18NUtil.class,
+                Mockito.CALLS_REAL_METHODS)) {
+            util.when(() -> I18NUtil.getClassLoader())
+                    .thenReturn(urlClassLoader);
+
+            VaadinService service = Mockito.mock(VaadinService.class);
+            mockLookup(service);
+
+            instantiator = new DefaultInstantiator(service);
+
+            I18NProvider i18NProvider = instantiator.getI18NProvider();
+            Assert.assertNotNull(i18NProvider);
+            Assert.assertTrue(i18NProvider instanceof DefaultI18NProvider);
+
+            Assert.assertEquals("Default lang", i18NProvider
+                    .getTranslation("title", new Locale("fi", "FI")));
+
+            Assert.assertEquals("Default lang",
+                    i18NProvider.getTranslation("title", new Locale("de")));
+
+            Assert.assertEquals("Default lang", i18NProvider
+                    .getTranslation("title", new Locale("ko", "KR")));
+
+            // Note!
+            // default translations.properties will be used if
+            // the locale AND system default locale is not found
+            Assert.assertEquals("Default lang", i18NProvider
+                    .getTranslation("title", new Locale("en", "GB")));
+        }
+    }
+
+    @Test
+    public void onlyLangTransalation_nonExistingLangReturnsKey()
+            throws IOException {
+        File file = new File(translations,
+                DefaultI18NProvider.BUNDLE_FILENAME + "_ja.properties");
+        Files.writeString(file.toPath(), "title=No Default",
+                StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+
+        try (MockedStatic<I18NUtil> util = Mockito.mockStatic(I18NUtil.class,
+                Mockito.CALLS_REAL_METHODS)) {
+            util.when(() -> I18NUtil.getClassLoader())
+                    .thenReturn(urlClassLoader);
+
+            VaadinService service = Mockito.mock(VaadinService.class);
+            mockLookup(service);
+
+            instantiator = new DefaultInstantiator(service);
+
+            I18NProvider i18NProvider = instantiator.getI18NProvider();
+            Assert.assertNotNull(i18NProvider);
+            Assert.assertTrue(i18NProvider instanceof DefaultI18NProvider);
+
+            Assert.assertEquals("No Default",
+                    i18NProvider.getTranslation("title", new Locale("ja")));
+
+            Assert.assertEquals("title", i18NProvider.getTranslation("title",
+                    new Locale("en", "GB")));
+        }
+    }
+
+    private static void createTranslationFiles(File translationsFolder)
+            throws IOException {
+        File file = new File(translationsFolder,
+                DefaultI18NProvider.BUNDLE_FILENAME + ".properties");
+        Files.writeString(file.toPath(), "title=Default lang",
+                StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+
+        file = new File(translationsFolder,
+                DefaultI18NProvider.BUNDLE_FILENAME + "_ko_KR.properties");
+        Files.writeString(file.toPath(), "title=Korean", StandardCharsets.UTF_8,
+                StandardOpenOption.CREATE);
+
+        file = new File(translationsFolder,
+                DefaultI18NProvider.BUNDLE_FILENAME + "_fi_FI.properties");
+        Files.writeString(file.toPath(), "title=Suomi", StandardCharsets.UTF_8,
+                StandardOpenOption.CREATE);
+
+        file = new File(translationsFolder,
+                DefaultI18NProvider.BUNDLE_FILENAME + "_de.properties");
+        Files.writeString(file.toPath(), "title=deutsch",
+                StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+    }
+
+    private Lookup mockLookup(VaadinService service) {
+        VaadinContext context = Mockito.mock(VaadinContext.class);
+        Mockito.when(service.getContext()).thenReturn(context);
+
+        Lookup lookup = Mockito.mock(Lookup.class);
+        Mockito.when(context.getAttribute(Lookup.class)).thenReturn(lookup);
+        return lookup;
+    }
+}
