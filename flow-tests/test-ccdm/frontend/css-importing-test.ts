fed7984ccfcc5ef2d75d9e0b5b6068e8390c764d
Artur Signell <artur@vaadin.com>
2023-10-24 14:31:03 +0300

    test: Use correct styles import

--- flow-tests/test-ccdm/frontend/css-importing-test.ts
+++ flow-tests/test-ccdm/frontend/css-importing-test.ts
@@ -1,7 +1,8 @@
 import { LitElement } from 'lit';
 
 import '@vaadin/vaadin-login/vaadin-login-overlay';
-import styles from './test-styles.css';
+// @ts-ignore
+import styles from './test-styles.css?inline';
 
 // Regression test for flow#9167 (`styles` assignment will cause a type
 // error if `styles` imported from `./test-styles.css` is not a
--- flow-tests/test-custom-frontend-directory/test-themes-custom-frontend-directory/side-src/main/frontend/typescript/hello-world-view.ts
+++ flow-tests/test-custom-frontend-directory/test-themes-custom-frontend-directory/side-src/main/frontend/typescript/hello-world-view.ts
@@ -1,7 +1,8 @@
 import '@vaadin/text-field';
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import styles from './hello-world-view.css';
+// @ts-ignore
+import styles from './hello-world-view.css?inline';
 
 
 @customElement('hello-world-view')
