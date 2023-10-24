8f1771d22b6c2f3028b51085bf522bf74677d8fc
Artur Signell <artur@vaadin.com>
2023-10-24 14:37:44 +0300

    Make it serializable

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/ServerInfo.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/ServerInfo.java
@@ -28,7 +28,8 @@ import com.vaadin.flow.server.Version;
  */
 public class ServerInfo implements Serializable {
 
-    public record NameAndVersion(String name, String version) {
+    public record NameAndVersion(String name, String version)
+            implements Serializable {
     };
 
     private List<NameAndVersion> versions = new ArrayList<>();
