4392119c5de5bf710bb4b368ab104ca5cb267be0
Artur Signell <artur@vaadin.com>
2023-11-08 14:20:00 +0200

    fix: Make TypeScriptBootstrapModifier work when building with Maven/Gradle

--- flow-tests/test-frontend/vite-basics/src/main/java/com/vaadin/viteapp/BootstrapModifier.java
+++ flow-tests/test-frontend/vite-basics/src/main/java/com/vaadin/viteapp/BootstrapModifier.java
@@ -7,7 +7,7 @@ import com.vaadin.flow.server.frontend.TypeScriptBootstrapModifier;
 public class BootstrapModifier implements TypeScriptBootstrapModifier {
 
     @Override
-    public void modify(List<String> bootstrapTypeScript) {
+    public void modify(List<String> bootstrapTypeScript, boolean productionMode) {
         bootstrapTypeScript.add("(window as any).bootstrapMod=1;");
     }
 
