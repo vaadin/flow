4f58e914b948e9e718d440bf60def61012085859
Marco Collovati <marco@vaadin.com>
2023-11-15 17:49:10 +0100

    update TestVersion

--- flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
+++ flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
@@ -2,7 +2,7 @@ package com.vaadin.flow.uitest.ui.dependencies;
 
 public class TestVersion {
 
-    public static final String VAADIN = "24.2.1";
+    public static final String VAADIN = "24.2.3";
     public static final String FONTAWESOME = "5.15.1";
 
 }
