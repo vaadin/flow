87bdaa62b86389c4dd866813559db9ede04d75e6
Tomi Virtanen <tltv@vaadin.com>
2023-12-15 14:28:22 +0200

    chore: update javadoc

--- flow-data/src/main/java/com/vaadin/flow/data/provider/ItemIndexProvider.java
+++ flow-data/src/main/java/com/vaadin/flow/data/provider/ItemIndexProvider.java
@@ -39,5 +39,20 @@ import com.vaadin.flow.function.SerializableBiFunction;
 @FunctionalInterface
 public interface ItemIndexProvider<T, F>
         extends SerializableBiFunction<T, Query<T, F>, Integer> {
+    /**
+     * Gets the index of the item in the filtered and sorted data set.
+     * <p>
+     * There will be inconsistent index if the data set for the returned index
+     * is different from the component's data set. Changing the data set of
+     * either side during this call may cause inconsistent index as a result.
+     *
+     * @param item
+     *            Target item to get the index for
+     * @param query
+     *            Query prepared for fetching all items including filter and
+     *            sorting.
+     * @return the index of the item in the filtered and sorted data set, or
+     *         null if not found
+     */
     Integer apply(T item, Query<T, F> query);
 }
