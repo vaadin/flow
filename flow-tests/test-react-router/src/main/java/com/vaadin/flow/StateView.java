64bbd62c3a1b289330aee8c97d43181a8004a6ab
Mikael Grankvist <mgrankvi@vaadin.com>
2023-12-22 10:04:42 +0200

    Fix production test
    Add parameter to build-frontend to
    not clear frontend file for
    a production build as there are
    tests that need it.

--- flow-tests/test-react-router/src/main/java/com/vaadin/flow/StateView.java
+++ flow-tests/test-react-router/src/main/java/com/vaadin/flow/StateView.java
@@ -3,6 +3,8 @@ package com.vaadin.flow;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -12,6 +14,8 @@ import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.frontend.FileIOUtils;
+import com.vaadin.flow.server.frontend.FrontendUtils;
 
 @Route("com.vaadin.flow.StateView")
 public class StateView extends Div {
@@ -30,6 +34,9 @@ public class StateView extends Div {
 
         File baseDir = attachEvent.getSession().getConfiguration()
                 .getProjectFolder();
+        if (baseDir == null) {
+            baseDir = getBaseDir(attachEvent);
+        }
         String packageJson = null;
         try {
             packageJson = FileUtils.readFileToString(
@@ -46,4 +53,39 @@ public class StateView extends Div {
         add(enabled, new Div(), react);
     }
 
+    private File getBaseDir(AttachEvent attachEvent) {
+
+        String folder = attachEvent.getSession().getConfiguration()
+                .getStringProperty(FrontendUtils.PROJECT_BASEDIR, null);
+        if (folder != null) {
+            return new File(folder);
+        }
+
+        File projectFolder = FileIOUtils.getProjectFolderFromClasspath();
+        if (projectFolder != null) {
+            return projectFolder;
+        }
+
+        /*
+         * Accept user.dir or cwd as a fallback only if the directory seems to
+         * be a Maven or Gradle project. Check to avoid cluttering server
+         * directories (see tickets #8249, #8403).
+         */
+        String baseDirCandidate = System.getProperty("user.dir", ".");
+        Path path = Paths.get(baseDirCandidate);
+        if (path.toFile().isDirectory()
+                && (path.resolve("pom.xml").toFile().exists()
+                        || path.resolve("build.gradle").toFile().exists())) {
+            return path.toAbsolutePath().toFile();
+        } else {
+            throw new IllegalStateException(String.format(
+                    "Failed to determine project directory for dev mode. "
+                            + "Directory '%s' does not look like a Maven or "
+                            + "Gradle project. Ensure that you have run the "
+                            + "prepare-frontend Maven goal, which generates "
+                            + "'flow-build-info.json', prior to deploying your "
+                            + "application",
+                    path.toString()));
+        }
+    }
 }
