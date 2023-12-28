e27cb16ee5e1330f51559edf5d367bfd0bce39eb
Marco Collovati <marco@vaadin.com>
2023-12-28 10:53:19 +0100

    fix: fix css module definition in typescript declaration file (#18342)
    * fix: fix css module definition in typescript declaration file
    
    When importing styles in typescript sources, the import should contain
    the inline parameter (e.g. import styles from './some.css?inline').
    The current module definition in types.d.ts does not handle the parameter,
    so TypeScript errors happen during build.
    This change updates the definition in Flow default types.d.ts file and
    handles upgrade of existing files.
    
    Fixes #18330
    
    * Apply review suggestions
    
    * compare ignoring EOL

--- flow-tests/test-ccdm/frontend/css-importing-test.ts
+++ flow-tests/test-ccdm/frontend/css-importing-test.ts
@@ -1,7 +1,6 @@
 import { LitElement } from 'lit';
 
 import '@vaadin/vaadin-login/vaadin-login-overlay';
-// @ts-ignore
 import styles from './test-styles.css?inline';
 
 // Regression test for flow#9167 (`styles` assignment will cause a type
