98be0fa781561a6aeb1b5725c44b6701a9c454ce
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2024-01-03 15:40:03 +0200

    chore:update license header to 2024 (#18391)

--- flow-tests/test-scalability/src/main/java/com/vaadin/flow/test/scalability/HelloWorldUI.java
+++ flow-tests/test-scalability/src/main/java/com/vaadin/flow/test/scalability/HelloWorldUI.java
@@ -6,7 +6,7 @@ import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.router.Route;
 
 /*
- * Copyright 2000-2023 Vaadin Ltd.
+ * Copyright 2000-2024 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
