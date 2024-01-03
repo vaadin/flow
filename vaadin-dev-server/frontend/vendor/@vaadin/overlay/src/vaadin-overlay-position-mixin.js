8f7b578d401d73e481fc62e14d4cee99a2918528
Zhe Sun <zhe@vaadin.com>
2024-01-03 09:00:01 +0200

    chore:update license header to 2024

--- vaadin-dev-server/frontend/vendor/@vaadin/overlay/src/vaadin-overlay-position-mixin.js
+++ vaadin-dev-server/frontend/vendor/@vaadin/overlay/src/vaadin-overlay-position-mixin.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2017 - 2023 Vaadin Ltd.
+ * Copyright (c) 2017 - 2024 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 
