a2febe80810b3343caec286c7ac37a88c01ca916
David Sosa <sosa@vaadin.com>
2024-01-03 09:51:59 +0200

    chore: add license headers

--- flow-plugins/flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/maven/InvocationRequestBuilder.java
+++ flow-plugins/flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/maven/InvocationRequestBuilder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2024 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.flow.plugin.maven;
 
 import java.io.File;
@@ -9,37 +24,37 @@ import org.apache.maven.shared.invoker.InvocationRequest;
 /**
  * Builder class for {@link InvocationRequest} objects.
  */
-public class InvocationRequestBuilder {
+class InvocationRequestBuilder {
 
     private String groupId;
     private String artifactId;
     private String version;
     private String goal;
 
-    public InvocationRequestBuilder() {
+    InvocationRequestBuilder() {
     }
 
-    public InvocationRequestBuilder groupId(String groupId) {
+    InvocationRequestBuilder groupId(String groupId) {
         this.groupId = groupId;
         return this;
     }
 
-    public InvocationRequestBuilder artifactId(String artifactId) {
+    InvocationRequestBuilder artifactId(String artifactId) {
         this.artifactId = artifactId;
         return this;
     }
 
-    public InvocationRequestBuilder version(String version) {
+    InvocationRequestBuilder version(String version) {
         this.version = version;
         return this;
     }
 
-    public InvocationRequestBuilder goal(String goal) {
+    InvocationRequestBuilder goal(String goal) {
         this.goal = goal;
         return this;
     }
 
-    public InvocationRequest createInvocationRequest() {
+    InvocationRequest createInvocationRequest() {
         if (groupId == null) {
             throw new IllegalStateException(
                     "Cannot create invocation request without a groupId");
