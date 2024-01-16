c50a1f9f680042d198e0c99d7473eed05b33d884
Marco Collovati <marco@vaadin.com>
2024-01-16 14:29:10 +0200

    cleanup

--- flow-server/src/main/java/com/vaadin/flow/server/DevToolsToken.java
+++ flow-server/src/main/java/com/vaadin/flow/server/DevToolsToken.java
@@ -16,23 +16,15 @@
 
 package com.vaadin.flow.server;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.HexFormat;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
 import org.slf4j.LoggerFactory;
-
-import com.vaadin.pro.licensechecker.MachineId;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Representation of the security token exchanged with the Dev Tools client to
@@ -69,7 +61,9 @@ public class DevToolsToken implements Serializable {
                             .toString();
                 } catch (Exception e) {
                     LoggerFactory.getLogger(DevToolsToken.class).debug(
-                            "Cannot read dev-tools token file, using a random new token. Browser page might need a reload to make dev-tools websocket establish a connection.");
+                            "Cannot read dev-tools token file, using a random new token. "
+                                    + "Browser page might need a reload to make dev-tools websocket establish a connection.",
+                            e);
                 }
             } else {
                 randomDevToolsToken = UUID.randomUUID().toString();
@@ -77,7 +71,8 @@ public class DevToolsToken implements Serializable {
                     Files.writeString(tokenFile.toPath(), token);
                 } catch (IOException e) {
                     LoggerFactory.getLogger(DevToolsToken.class).debug(
-                            "Cannot write dev-tools token file. A new token will be generated on server restart.");
+                            "Cannot write dev-tools token file. A new token will be generated on server restart.",
+                            e);
                 }
             }
         }
@@ -91,75 +86,7 @@ public class DevToolsToken implements Serializable {
      * @return signed token, never {@literal null}.
      */
     public static String token() {
-        // return SIGNED_DEV_TOOLS_TOKEN;
         return randomDevToolsToken;
-    }
-
-    private static String singValue(String value) {
-        try {
-            Mac hmac = Mac.getInstance("HmacSHA256");
-            String machineId = MachineId.get();
-            if (machineId.isEmpty()) {
-                machineId = randomDevToolsToken;
-            }
-            SecretKeySpec secretKeySpec = new SecretKeySpec(
-                    machineId.getBytes(UTF_8), "HmacSHA256");
-            hmac.init(secretKeySpec);
-            byte[] hash = hmac.doFinal(value.getBytes(UTF_8));
-            return HexFormat.of().formatHex(hash);
-        } catch (Exception ex) {
-            LoggerFactory.getLogger(DevToolsToken.class)
-                    .debug("Cannot sign dev-tools token", ex);
-        }
-        return value;
-    }
-
-    /**
-     * The result of dev-tools token validation.
-     */
-    public enum TokenValidation {
-        /**
-         * The client token is correctly signed and matches the server token.
-         */
-        OK,
-        /**
-         * The client token has a valid signature, but it does not match the
-         * server token.
-         */
-        EXPIRED,
-        /**
-         * The token is not signed correctly.
-         */
-        INVALID
-    }
-
-    /**
-     * Verifies the validity of the given token.
-     *
-     * The token may be valid ({@link TokenValidation#OK}, correctly signed but
-     * expired because of a server restart ({@link TokenValidation#EXPIRED} or
-     * invalid ({@link TokenValidation#INVALID}.
-     *
-     * @param token
-     *            the token to validate
-     * @return the validation result, never {@literal null}.
-     */
-    public static TokenValidation validateToken(String token) {
-        if (token == null || token.isBlank()) {
-            return TokenValidation.INVALID;
-        }
-        String[] parts = TOKEN_DELIMITER.split(token);
-        if (parts.length == 2) {
-            String clientToken = parts[0];
-            String clientSignedValue = parts[1];
-            String serverSignedValue = singValue(clientToken);
-            if (clientSignedValue.equals(serverSignedValue)) {
-                return randomDevToolsToken.equals(clientToken)
-                        ? TokenValidation.OK
-                        : TokenValidation.EXPIRED;
-            }
-        }
-        return TokenValidation.INVALID;
     }
 
 }
