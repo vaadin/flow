539b63a2ddebb3aab7d133e31ab9e71e31c9ab0c
Marco Collovati <marco@vaadin.com>
2024-01-16 08:33:50 +0200

    Apply suggestions from code review
    Co-authored-by: Peter Czuczor <61667986+czp13@users.noreply.github.com>

--- vaadin-spring/src/test/java/com/vaadin/flow/spring/SpringSecurityAutoConfigurationTest.java
+++ vaadin-spring/src/test/java/com/vaadin/flow/spring/SpringSecurityAutoConfigurationTest.java
@@ -18,7 +18,6 @@ package com.vaadin.flow.spring;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Method;
