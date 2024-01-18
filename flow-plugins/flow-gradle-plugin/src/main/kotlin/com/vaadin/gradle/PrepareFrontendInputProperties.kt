3f10ef1cf610f8cb0fdf68f6dd2affaba0ef9006
Mikael Grankvist <mgrankvi@vaadin.com>
2024-01-18 13:43:06 +0200

    fix typo

--- flow-plugins/flow-gradle-plugin/src/main/kotlin/com/vaadin/gradle/PrepareFrontendInputProperties.kt
+++ flow-plugins/flow-gradle-plugin/src/main/kotlin/com/vaadin/gradle/PrepareFrontendInputProperties.kt
@@ -137,7 +137,7 @@ internal class PrepareFrontendInputProperties(private val config: PluginEffectiv
     public fun getForceProductionBuild(): Provider<Boolean> = config.forceProductionBuild
 
     @Input
-    public fun geReactRouterEnabled(): Provider<Boolean> = config.reactRouterEnabled
+    public fun getReactRouterEnabled(): Provider<Boolean> = config.reactRouterEnabled
 
     @Input
     @Optional
