3242252d9accdca932f8ff265e26c880b6b26bc7
Artur Signell <artur@vaadin.com>
2024-01-22 19:48:38 +0200

    format

--- flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/ReactComponentsIT.java
+++ flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/ReactComponentsIT.java
@@ -12,7 +12,6 @@ import com.vaadin.testbench.TestBenchElement;
 
 public class ReactComponentsIT extends ChromeBrowserTest {
 
-
     @Before
     public void openView() {
         getDriver().get(getTestURL());
@@ -23,9 +22,11 @@ public class ReactComponentsIT extends ChromeBrowserTest {
     protected String getTestPath() {
         return "/react-components";
     }
+
     @Test
     public void functionLocationsAvailable() {
-        List<TestBenchElement> elements = $("*").hasAttribute("data-expected").all();
+        List<TestBenchElement> elements = $("*").hasAttribute("data-expected")
+                .all();
         Assert.assertTrue(elements.size() > 5);
         for (TestBenchElement element : elements) {
             String expected = element.getAttribute("data-expected");
@@ -33,7 +34,7 @@ public class ReactComponentsIT extends ChromeBrowserTest {
             Long column = Long.parseLong(expected.split("_")[1]);
             String filenameEnd = "vite-basics/frontend/ReactComponents.tsx";
 
-            Map<String,Object> result =  (Map<String, Object>) executeScript(
+            Map<String, Object> result = (Map<String, Object>) executeScript(
                     """
                             const key = Object.keys(arguments[0]).filter(a => a.startsWith("__reactFiber"))[0];
                             const fiber = arguments[0][key];
@@ -42,11 +43,12 @@ public class ReactComponentsIT extends ChromeBrowserTest {
                     element);
 
             Assert.assertTrue(
-                result.get("fileName").toString().endsWith(filenameEnd));
+                    result.get("fileName").toString().endsWith(filenameEnd));
             Assert.assertSame(line, result.get("lineNumber"));
             Assert.assertSame(column, result.get("columnNumber"));
 
-        };
+        }
+        ;
     }
 
 }
