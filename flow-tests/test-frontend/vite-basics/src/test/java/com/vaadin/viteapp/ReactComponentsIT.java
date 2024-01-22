97b23593eb8be6d3ec61728ec747783cadba629d
Artur Signell <artur@vaadin.com>
2024-01-22 19:24:29 +0200

    Test

---
+++ flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/ReactComponentsIT.java
@@ -0,0 +1,52 @@
+package com.vaadin.viteapp;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.testbench.TestBenchElement;
+
+public class ReactComponentsIT extends ChromeBrowserTest {
+
+
+    @Before
+    public void openView() {
+        getDriver().get(getTestURL());
+        waitForDevServer();
+    }
+
+    @Override
+    protected String getTestPath() {
+        return "/react-components";
+    }
+    @Test
+    public void functionLocationsAvailable() {
+        List<TestBenchElement> elements = $("*").hasAttribute("data-expected").all();
+        Assert.assertTrue(elements.size() > 5);
+        for (TestBenchElement element : elements) {
+            String expected = element.getAttribute("data-expected");
+            Long line = Long.parseLong(expected.split("_")[0]);
+            Long column = Long.parseLong(expected.split("_")[1]);
+            String filenameEnd = "vite-basics/frontend/ReactComponents.tsx";
+
+            Map<String,Object> result =  (Map<String, Object>) executeScript(
+                    """
+                            const key = Object.keys(arguments[0]).filter(a => a.startsWith("__reactFiber"))[0];
+                            const fiber = arguments[0][key];
+                            return fiber.return.type.__debugSourceDefine;
+                            """,
+                    element);
+
+            Assert.assertTrue(
+                result.get("fileName").toString().endsWith(filenameEnd));
+            Assert.assertSame(line, result.get("lineNumber"));
+            Assert.assertSame(column, result.get("columnNumber"));
+
+        };
+    }
+
+}
