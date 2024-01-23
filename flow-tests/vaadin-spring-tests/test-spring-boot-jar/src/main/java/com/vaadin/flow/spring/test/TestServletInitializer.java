54a4865c7d039bbf97653f779ac557a91a1cd554
Tomi Virtanen <tltv@vaadin.com>
2024-01-23 10:22:40 +0200

    chore: fixed formatting

--- flow-tests/vaadin-spring-tests/test-spring-boot-jar/src/main/java/com/vaadin/flow/spring/test/TestServletInitializer.java
+++ flow-tests/vaadin-spring-tests/test-spring-boot-jar/src/main/java/com/vaadin/flow/spring/test/TestServletInitializer.java
@@ -23,7 +23,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @SpringBootApplication
 @Configuration
