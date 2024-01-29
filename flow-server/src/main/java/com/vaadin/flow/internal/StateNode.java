1eb2a498d8caa2bdaad4a180051c44ae9e82f57c
Teppo Kurki <teppo.kurki@vaadin.com>
2024-01-29 09:02:24 +0200

    Call reset(false) instead of clearing fields directly

--- flow-server/src/main/java/com/vaadin/flow/internal/StateNode.java
+++ flow-server/src/main/java/com/vaadin/flow/internal/StateNode.java
@@ -795,9 +795,9 @@ public class StateNode implements Serializable {
             }
         } else if (id > -1 && getOwner() == NullOwner.get()) {
             // When id is set but owner is NullOwner, removeFromTree has been
-            // called, so we should allow moving the node to another StateTree
-            id = -1;
-            wasAttached = false;
+            // called, so we should clear node id and attached state, thus allow
+            // moving the node to another StateTree
+            reset(false);
         }
         owner = tree;
     }
