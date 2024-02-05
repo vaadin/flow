ed8306844248d2af046a660e4e3de71af36fb32f
Teppo Kurki <teppo.kurki@vaadin.com>
2024-02-05 13:24:17 +0200

    Fix regression NPE in binder

--- flow-data/src/main/java/com/vaadin/flow/data/binder/Binder.java
+++ flow-data/src/main/java/com/vaadin/flow/data/binder/Binder.java
@@ -1024,6 +1024,12 @@ public class Binder<BEAN> implements Serializable {
 
             BindingImpl<BEAN, FIELDVALUE, TARGET> binding = new BindingImpl<>(
                     this, getter, setter);
+            // Setting the binding field value has been moved up here to fix a
+            // regression NPE https://github.com/vaadin/flow/issues/18608 which
+            // breaks tests of Grid component: UpdateEditorComponentIT,
+            // DynamicEditorKBNavigationIT and
+            // GridViewEditorIT.dynamicNotBufferedEditor*
+            this.binding = binding;
 
             // Remove existing binding for same field to avoid potential
             // multiple application of converter
@@ -1042,7 +1048,6 @@ public class Binder<BEAN> implements Serializable {
             if (getBinder().incompleteBindings != null) {
                 getBinder().incompleteBindings.remove(getField());
             }
-            this.binding = binding;
 
             return binding;
         }
