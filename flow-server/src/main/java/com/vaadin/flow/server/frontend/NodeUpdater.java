ec2311a94f150e0af098ea4f35d8139737b5b43b
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2024-02-06 09:50:52 +0200

    feat: Use React/Lit auto-detection to add Hilla components (#18597)
    Adds Hilla components to package.json automatically in build time, so that developers don't need to swap hilla-react and hilla-lit artifacts in the pom dependencies.
    
    Related-to #18564, vaadin/hilla#2015

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -37,8 +37,6 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.vaadin.experimental.FeatureFlags;
-import com.vaadin.flow.internal.hilla.EndpointRequestUtil;
 import com.vaadin.flow.server.Constants;
 import com.vaadin.flow.server.frontend.scanner.ClassFinder;
 import com.vaadin.flow.server.frontend.scanner.FrontendDependencies;
@@ -306,6 +304,7 @@ public abstract class NodeUpdater implements FallibleCommand {
             dependencies
                     .putAll(readDependencies("vaadin-router", "dependencies"));
         }
+        putHillaComponentsDependencies(options, dependencies, "dependencies");
         return dependencies;
     }
 
@@ -343,6 +342,7 @@ public abstract class NodeUpdater implements FallibleCommand {
         Map<String, String> defaults = new HashMap<>();
         defaults.putAll(readDependencies("default", "devDependencies"));
         defaults.putAll(readDependencies("vite", "devDependencies"));
+        putHillaComponentsDependencies(options, defaults, "devDependencies");
 
         return defaults;
     }
@@ -572,5 +572,31 @@ public abstract class NodeUpdater implements FallibleCommand {
         }
 
         return versionsJson;
+    }
+
+    /**
+     * Adds Hilla components to package.json if Hilla is used in the project.
+     *
+     * @param options
+     *            build options
+     * @param dependencies
+     *            to be added into package.json
+     * @param packageJsonKey
+     *            the key inside package.json containing the sub-list of
+     *            dependencies to read and add
+     * @see <a href=
+     *      "https://github.com/vaadin/hilla/tree/main/packages/java/hilla/src/main/resources/com/vaadin/flow/server/frontend/dependencies/hilla/components</a>
+     */
+    private static void putHillaComponentsDependencies(Options options,
+            Map<String, String> dependencies, String packageJsonKey) {
+        if (FrontendUtils.isHillaUsed(options.getFrontendDirectory())) {
+            if (options.isReactRouterEnabled()) {
+                dependencies.putAll(readDependencies("hilla/components/react",
+                        packageJsonKey));
+            } else {
+                dependencies.putAll(readDependencies("hilla/components/lit",
+                        packageJsonKey));
+            }
+        }
     }
 }
