99d87defa8c1165d932c8d2db41dfec80f7713f3
David Sosa <sosa@vaadin.com>
2024-02-10 20:42:45 +0200

    feat: add caption to the table

--- flow-html-components/src/main/java/com/vaadin/flow/component/html/NativeTable.java
+++ flow-html-components/src/main/java/com/vaadin/flow/component/html/NativeTable.java
@@ -15,6 +15,9 @@
  */
 package com.vaadin.flow.component.html;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.vaadin.flow.component.ClickNotifier;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasComponents;
@@ -29,6 +32,11 @@ import com.vaadin.flow.component.Tag;
 @Tag(Tag.TABLE)
 public class NativeTable extends HtmlComponent
         implements HasComponents, ClickNotifier<NativeTable> {
+
+    /**
+     * The table's caption.
+     */
+    private TableCaption caption;
 
     /**
      * Creates a new empty table.
@@ -46,5 +54,37 @@ public class NativeTable extends HtmlComponent
     public NativeTable(Component... components) {
         super();
         add(components);
+    }
+
+    /**
+     * Return the table's caption component.
+     * Creates a new instance if no caption is present.
+     *
+     * @return the table's caption.
+     */
+    public TableCaption getCaption() {
+        if (caption == null) {
+            caption = new TableCaption();
+            this.addComponentAsFirst(caption);
+        }
+        return caption;
+    }
+
+    /**
+     * Sets the caption for this table.
+     *
+     * @param text the caption's text.
+     */
+    public void setCaption(String text) {
+        getCaption().setText(text);
+    }
+
+    /**
+     * Remove the caption from this table.
+     */
+    public void removeCaption() {
+        if (caption != null) {
+            remove(caption);
+        }
     }
 }
