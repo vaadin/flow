5a2d5e384936477fd82d7fd9f898e82150a981b2
David Sosa <sosa@vaadin.com>
2024-02-10 20:27:24 +0200

    feat: add TableCaption

---
+++ flow-html-components/src/main/java/com/vaadin/flow/component/html/TableCaption.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2000-2024 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.html;
+
+import com.vaadin.flow.component.HasSize;
+import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.HtmlContainer;
+import com.vaadin.flow.component.Tag;
+
+/**
+ * Represents the table caption element ({@code <caption>}).
+ *
+ * @author Stefan Uebe
+ * @since 24.4
+ */
+@Tag("caption")
+public class TableCaption extends HtmlContainer implements HasStyle, HasSize {
+}
