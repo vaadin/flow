5a0f4e15f59d3453aaedbd639027a91f711e00b5
David Sosa <sosa@vaadin.com>
2024-02-10 22:45:18 +0200

    feat: add TestBench class for caption element

---
+++ flow-html-components-testbench/src/main/java/com/vaadin/flow/component/html/testbench/TableCaptionElement.java
@@ -0,0 +1,13 @@
+package com.vaadin.flow.component.html.testbench;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+/**
+ * A TestBench element representing a <code>&lt;caption&gt;</code> element.
+ *
+ * @since 24.4
+ */
+@Element("caption")
+public class TableCaptionElement extends TestBenchElement {
+}
