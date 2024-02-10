cdc4225f80f45746615bf66d035250c5e768c4bd
David Sosa <sosa@vaadin.com>
2024-02-10 22:41:52 +0200

    feat: add methods to manage cells in a row

--- flow-html-components/src/main/java/com/vaadin/flow/component/html/TableRow.java
+++ flow-html-components/src/main/java/com/vaadin/flow/component/html/TableRow.java
@@ -15,6 +15,10 @@
  */
 package com.vaadin.flow.component.html;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import com.vaadin.flow.component.ClickNotifier;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasOrderedComponents;
@@ -47,4 +51,137 @@ public class TableRow extends HtmlComponent
         super();
         add(components);
     }
+
+    /**
+     * Add a header cell to this row.
+     *
+     * @return the new {@code <th>} element.
+     */
+    public TableHeaderCell addHeaderCell() {
+        TableHeaderCell cell = new TableHeaderCell();
+        add(cell);
+        return cell;
+    }
+
+    /**
+     * Insert a new header cell into a given position.
+     *
+     * @param position the position into which the header cell must be added.
+     * @return the new header cell.
+     */
+    public TableHeaderCell insertHeaderCell(int position) {
+        if (position == 0) {
+            return addHeaderCell();
+        }
+        TableHeaderCell headerCell = new TableHeaderCell();
+        addComponentAtIndex(position, headerCell);
+        return headerCell;
+    }
+
+    /**
+     * Add a data cell to this row.
+     *
+     * @return the new {@code <td>} element.
+     */
+    public TableCell addDataCell() {
+        TableCell cell = new TableCell();
+        add(cell);
+        return cell;
+    }
+
+    /**
+     * Insert a new data cell into a given position.
+     *
+     * @param position the position into which the data cell must be added.
+     * @return the new data cell.
+     */
+    public TableCell insertDataCell(int position) {
+        if (position == 0) {
+            return addDataCell();
+        }
+        TableCell tableCell = new TableCell();
+        addComponentAtIndex(position, tableCell);
+        return tableCell;
+    }
+
+    /**
+     * Returns a list of all header cells in this row.
+     *
+     * @return A list of all header cells in this row.
+     */
+    public List<TableHeaderCell> getHeaderCells() {
+        return getChildren()
+                .filter(c -> c instanceof TableHeaderCell)
+                .map(c -> (TableHeaderCell) c)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns a list of all data cells in this row.
+     *
+     * @return A list of all data cells in this row.
+     */
+    public List<TableCell> getDataCells() {
+        return getChildren()
+                .filter(c -> c instanceof TableCell)
+                .map(c -> (TableCell) c)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns a list of all cells in this row.
+     *
+     * @return a list of all cells in this row.
+     */
+    public List<Component> getAllCells() {
+        return getChildren()
+                .filter(c -> c instanceof TableCell
+                        || c instanceof TableHeaderCell)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<TableHeaderCell> getHeaderCell(int index) {
+        return getChildren()
+                .filter(c -> c instanceof TableHeaderCell)
+                .map(c -> (TableHeaderCell) c)
+                .skip(index)
+                .findFirst();
+    }
+
+    public Optional<TableCell> getDataCell(int index) {
+        return getChildren()
+                .filter(c -> c instanceof TableCell)
+                .map(c -> (TableCell) c)
+                .skip(index)
+                .findFirst();
+    }
+
+    public Optional<Component> getCell(int index) {
+        return getChildren()
+                .filter(c -> c instanceof TableCell
+                        || c instanceof TableHeaderCell)
+                .skip(index)
+                .findFirst();
+    }
+
+    public void removeCell(int index) {
+        getCell(index).ifPresent(this::remove);
+    }
+
+    public void removeHeaderCell(int index) {
+        getHeaderCell(index).ifPresent(this::remove);
+    }
+
+    public void removeHeaderCell(TableHeaderCell headerCell) {
+        remove(headerCell);
+    }
+
+    public void removeDataCell(int index) {
+        getDataCell(index).ifPresent(this::remove);
+    }
+
+    public void removeDataCell(TableCell dataCell) {
+        remove(dataCell);
+    }
+
 }
