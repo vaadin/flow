246a9f5f6eb61670d6ca42b2d25f520faf3662d6
Marco Collovati <marco@vaadin.com>
2024-02-19 07:49:20 +0100

    chore: Upgrade to npm 10.2.4 (24.2)

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
@@ -70,7 +70,7 @@ public class FrontendTools {
     /**
      * This is the version shipped with the default Node version.
      */
-    public static final String DEFAULT_NPM_VERSION = "10.1.0";
+    public static final String DEFAULT_NPM_VERSION = "10.2.4";
 
     public static final String DEFAULT_PNPM_VERSION = "8.6.11";
 
