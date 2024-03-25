1fc450df417324cd45f72af98704a3e83d4b6387
Mikhail Shabarov <mikhail@vaadin.com>
2024-03-25 08:38:54 +0200

    update routes.tsx in vite-basics

--- flow-tests/test-frontend/vite-basics/src/main/frontend/routes.tsx
+++ flow-tests/test-frontend/vite-basics/src/main/frontend/routes.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter, RouteObject } from 'react-router-dom';
-import { serverSideRoutes } from 'Frontend/generated/flow/Flow';
+import { serverRoute } from 'Frontend/generated/flow/server-route';
 import ReactComponents from './ReactComponents';
 
 export const routes = [
   { path: 'react-components',  element: <ReactComponents/> },
-  ...serverSideRoutes
+  ...serverRoute
 ] as RouteObject[];
 
 export default createBrowserRouter(routes);
