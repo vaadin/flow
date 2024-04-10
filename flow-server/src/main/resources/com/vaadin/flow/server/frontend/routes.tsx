90cd304ed1fbd7718f10a42443787e160acd37e4
Mikhail Shabarov <mikhail@vaadin.com>
2024-04-10 09:48:35 +0300

    fix: Imports file routes as ts file

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/routes.tsx
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/routes.tsx
@@ -34,7 +34,7 @@
  ******************************************************************************/
 import { RouterConfigurationBuilder } from '@vaadin/hilla-file-router/runtime.js';
 import Flow from 'Frontend/generated/flow/Flow';
-import fileRoutes from 'Frontend/generated/file-routes.js';
+import fileRoutes from 'Frontend/generated/file-routes';
 
 export const { router, routes } = new RouterConfigurationBuilder()
     .withFileRoutes(fileRoutes) // (1)
