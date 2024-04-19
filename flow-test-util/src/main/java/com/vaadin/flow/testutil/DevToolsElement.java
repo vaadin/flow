4fca3e6f592877a01a8cd9bdd3e3e37f176c7240
Artur Signell <artur@vaadin.com>
2024-04-19 09:46:07 +0300

    format

--- flow-test-util/src/main/java/com/vaadin/flow/testutil/DevToolsElement.java
+++ flow-test-util/src/main/java/com/vaadin/flow/testutil/DevToolsElement.java
@@ -6,9 +6,8 @@ import com.vaadin.testbench.elementsbase.Element;
 @Element("vaadin-dev-tools")
 public class DevToolsElement extends TestBenchElement {
 
-
     public void setLiveReload(boolean enabled) {
-        callFunction("setActive",enabled);
+        callFunction("setActive", enabled);
     }
 
 }
