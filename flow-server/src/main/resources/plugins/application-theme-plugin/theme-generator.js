31e2cb50d817901d5b81ace741f92ac653c9935a
Marco Collovati <marco@vaadin.com>
2024-04-24 09:21:43 +0200

    fix: prevent embedded styles to leak main document (#19221)
    * fix: prevent embedded styles to leak main document
    
    When using an exported a themed Flow web-component, Lumo style may leak
    the embedding document, causing invalid CSS rules to be applied.
    This change prevents applying Lumo global imports when the theme is
    applied to a web-component.
    
    Fixes #12704
    
    * apply review suggestions

--- flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
+++ flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
@@ -81,6 +81,7 @@ function writeThemeFiles(themeFolder, themeName, themeProperties, options) {
 
   themeFileContent += `let needsReloadOnChanges = false;\n`;
   const imports = [];
+  const deferredImports = [];
   const componentCssImports = [];
   const globalFileContent = [];
   const globalCssCode = [];
@@ -118,7 +119,9 @@ function writeThemeFiles(themeFolder, themeName, themeProperties, options) {
       imports.push(`import { ${lumoImport} } from '@vaadin/vaadin-lumo-styles/${lumoImport}.js';\n`);
       if (lumoImport === 'utility' || lumoImport === 'badge' || lumoImport === 'typography' || lumoImport === 'color') {
         // Inject into main document the same way as other Lumo styles are injected
-        imports.push(`import '@vaadin/vaadin-lumo-styles/${lumoImport}-global.js';\n`);
+        // Defer import at runtime to prevent leaking document when theme is applied
+        // to an embedded component
+        deferredImports.push(`import('@vaadin/vaadin-lumo-styles/${lumoImport}-global.js');\n`);
       }
     });
 
@@ -220,6 +223,8 @@ function writeThemeFiles(themeFolder, themeName, themeProperties, options) {
     const removers = [];
     if (target !== document) {
       ${shadowOnlyCss.join('')}
+    } else {
+      ${deferredImports.join('\n')}
     }
     ${parentTheme}
     ${globalCssCode.join('')}
