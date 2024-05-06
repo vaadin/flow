e02e7aa1343f74fde3699dcc9593fcdec3487ed6
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2024-05-06 14:34:53 +0300

    Update TestVersion.java

--- flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
+++ flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
@@ -3,6 +3,6 @@ package com.vaadin.flow.uitest.ui.dependencies;
 public class TestVersion {
 
     public static final String VAADIN = "24.3.11";
-    public static final String FONTAWESOME = "6.5.2";
+    public static final String FONTAWESOME = "5.15.1";
 
 }
