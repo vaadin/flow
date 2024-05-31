f8ef28b8edb65395ccf86e221a7eb1513377c0bd
Mikhail Shabarov <mikhail@vaadin.com>
2024-05-31 16:53:39 +0300

    Revert "Use a base path for the router"
    This reverts commit bca440d7350804cf87d6ba7fcf8201080e0e5e2e.

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/routes-flow.tsx
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/routes-flow.tsx
@@ -12,7 +12,7 @@ import { serverSideRoutes } from 'Frontend/generated/flow/Flow';
 function build() {
     const routes = [...serverSideRoutes] as RouteObject[];
     return {
-        router: createBrowserRouter(routes, { basename: new URL(document.baseURI).pathname }),
+        router: createBrowserRouter(routes),
         routes
     };
 }
