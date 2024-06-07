d1621fa6fdaf83631160d8c051c3274399115b6e
Tomi Virtanen <tltv@vaadin.com>
2024-06-07 12:30:08 +0300

    chore: preserve navigated.current value

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/Flow.tsx
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/Flow.tsx
@@ -165,7 +165,7 @@ function Flow() {
     const ref = useRef<HTMLOutputElement>(null);
     const navigate = useNavigate();
     const blocker = useBlocker(({ currentLocation, nextLocation }) => {
-        navigated.current = nextLocation.pathname === currentLocation.pathname && nextLocation.search === currentLocation.search && nextLocation.hash === currentLocation.hash;
+        navigated.current = navigated.current || (nextLocation.pathname === currentLocation.pathname && nextLocation.search === currentLocation.search && nextLocation.hash === currentLocation.hash);
         return true;
     });
     const {pathname, search, hash} = useLocation();
