ac4bb64e2f266ec867cfc93a9632d76cb411dfcd
Artur <artur@vaadin.com>
2024-06-10 12:00:13 +0300

    fix: preserve navigated.current value (#19539) (CP: 24.4) (#19546)
    pushState does not trigger history change event.

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/HistoryIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/HistoryIT.java
@@ -58,7 +58,7 @@ public class HistoryIT extends ChromeBrowserTest {
         Assert.assertEquals(baseUrl, getCurrentUrl());
         // idx value in history state is added by react-router
         Assert.assertEquals(
-                Arrays.asList("New location: asdf",
+                Arrays.asList(
                         "New location: com.vaadin.flow.uitest.ui.HistoryView"),
                 getStatusMessages());
         clearButton.click();
