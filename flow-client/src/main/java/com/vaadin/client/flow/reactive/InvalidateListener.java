cc36154b3735d38c2feb112208efc40645fca50a
Zhe Sun <zhe@vaadin.com>
2024-06-26 14:45:57 +0300

    chore: fix javadoc failure

--- flow-client/src/main/java/com/vaadin/client/flow/reactive/InvalidateListener.java
+++ flow-client/src/main/java/com/vaadin/client/flow/reactive/InvalidateListener.java
@@ -3,7 +3,7 @@
  *
  * This program is available under Vaadin Commercial License and Service Terms.
  *
- * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * See {@literal <https://vaadin.com/commercial-license-and-service-terms>}  for the full
  * license.
  */
 package com.vaadin.client.flow.reactive;
--- flow-tests/test-scalability/src/main/java/com/vaadin/flow/test/scalability/HelloWorldUI.java
+++ flow-tests/test-scalability/src/main/java/com/vaadin/flow/test/scalability/HelloWorldUI.java
@@ -14,7 +14,7 @@ import com.vaadin.flow.server.VaadinServletConfiguration;
  *
  * This program is available under Vaadin Commercial License and Service Terms.
  *
- * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * See {@literal <https://vaadin.com/commercial-license-and-service-terms>}  for the full
  * license.
  */
 
