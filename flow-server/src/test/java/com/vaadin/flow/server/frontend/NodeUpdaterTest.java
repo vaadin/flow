8d406939f99d75c084137268d59cf77bf21e8b20
Artur Signell <artur@vaadin.com>
2024-07-02 11:51:55 +0300

    chore: Bump frontend dependencies

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeUpdaterTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/NodeUpdaterTest.java
@@ -214,7 +214,7 @@ public class NodeUpdaterTest {
                 "7.0.0");
         nodeUpdater.updateDefaultDependencies(packageJson);
 
-        Assert.assertEquals("10.4.1", packageJson
+        Assert.assertEquals("10.4.2", packageJson
                 .getObject(NodeUpdater.DEV_DEPENDENCIES).getString("glob"));
     }
 
