0cf0e86e362ff6b735955097cab97702da3d3286
Teppo Kurki <teppo.kurki@vaadin.com>
2024-07-12 14:32:18 +0300

    Throw if comp/bower mode enabled

--- flow-server/src/main/java/com/vaadin/flow/server/DefaultDeploymentConfiguration.java
+++ flow-server/src/main/java/com/vaadin/flow/server/DefaultDeploymentConfiguration.java
@@ -298,9 +298,10 @@ public class DefaultDeploymentConfiguration
     }
 
     /**
-     * Log a warning if Vaadin is running in compatibility mode. Throw
+     * Check for compatibility mode / Bower mode setting. Throw
      * {@link IllegalStateException} if the mode could not be determined from
-     * parameters.
+     * parameters. Throw {@link UnsupportedOperationException} compatibility
+     * mode / Bower mode is enabled.
      */
     private void checkCompatibilityMode(boolean logWarning) {
         boolean explicitlySet = false;
@@ -328,14 +329,18 @@ public class DefaultDeploymentConfiguration
             }
         }
 
-        if (compatibilityMode && logWarning) {
-            warnings.add(WARNING_COMPATIBILITY_MODE);
+        if (compatibilityMode) {
+            throw new UnsupportedOperationException(
+                    "Bower mode / compatibility mode is deprecated and no longer supported. "
+                            + "Please don't use bower / compatibility mode "
+                            + "(set to false or delete bowerMode and compatibilityMode configuration parameters), "
+                            + "instead use npm or pnpm package manager (used by default)");
         }
     }
 
     /**
-     * Log a warning if new license checker is enabled in compatibility mode or
-     * while the live reload is off.
+     * Log a warning if new license checker is enabled while the live reload is
+     * off.
      */
     private void checkNewLicenseChecker(boolean logWarning) {
         boolean enableNewLicenseChecker = !getBooleanProperty(
