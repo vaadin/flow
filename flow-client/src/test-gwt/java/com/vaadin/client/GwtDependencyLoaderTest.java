bc10e0d3a3888d43ee17fe3b6706bc28fc416497
Teppo Kurki <teppo.kurki@vaadin.com>
2024-07-12 12:25:51 +0300

    Fix test

--- flow-client/src/test-gwt/java/com/vaadin/client/GwtDependencyLoaderTest.java
+++ flow-client/src/test-gwt/java/com/vaadin/client/GwtDependencyLoaderTest.java
@@ -111,6 +111,7 @@ public class GwtDependencyLoaderTest extends ClientEngineTestBase {
                 set(URIResolver.class, new URIResolver(this));
                 ApplicationConfiguration appConf = new ApplicationConfiguration();
                 appConf.setContextRootUrl("../");
+                appConf.setFrontendRootUrl("/frontend/");
                 set(ApplicationConfiguration.class, appConf);
             }
         };
