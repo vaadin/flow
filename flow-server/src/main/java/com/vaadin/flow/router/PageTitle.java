e1a1bb252a67d3b259309366a5207257d6bf69f7
Teppo Kurki <teppo.kurki@vaadin.com>
2024-07-19 16:06:06 +0300

    Remove import as well

--- flow-server/src/main/java/com/vaadin/flow/router/PageTitle.java
+++ flow-server/src/main/java/com/vaadin/flow/router/PageTitle.java
@@ -17,7 +17,6 @@
 package com.vaadin.flow.router;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
