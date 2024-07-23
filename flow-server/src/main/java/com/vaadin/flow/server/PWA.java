7ce729ca3b9235b7d78c273e1ac572dd84823199
Teppo Kurki <teppo.kurki@vaadin.com>
2024-07-23 10:50:56 +0300

    cp: ignore abstract AppShellConfigurators

--- flow-server/src/main/java/com/vaadin/flow/server/PWA.java
+++ flow-server/src/main/java/com/vaadin/flow/server/PWA.java
@@ -9,6 +9,7 @@
 package com.vaadin.flow.server;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -52,6 +53,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface PWA {
 
     /**
