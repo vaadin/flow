vaadin/flow

commit 9cb610428305a83b96ea6cf5ac4fd75a36d98ada

feat: add refresh flag to navigation event (#15324) (#15331)
Add the isRefreshEvent to BeforeEnterEvent
and AfterNavigationEvent to be make it possible
to distinguish if the event is for a refresh of
a preserve on refresh view.

Fixes #14999

Co-authored-by: caalador <mikael.grankvist@vaadin.com>

flow-server/src/main/java/com/vaadin/flow/router/internal/AbstractNavigationStateRenderer.java
@@ -189,6 +189,13 @@ public abstract class AbstractNavigationStateRenderer
             clearAllPreservedChains(ui);
         }
 
+        // Set navigationTrigger to RELOAD if this is a refresh of a preserve
+        // view.
+        if (preserveOnRefreshTarget && !chain.isEmpty()) {
+            event = new NavigationEvent(event.getSource(), event.getLocation(),
+                    event.getUI(), NavigationTrigger.REFRESH);
+        }
+
         // If the navigation is postponed, using BeforeLeaveEvent#postpone,
         // pushing history state shouldn't be done. So, it's done here to make
         // sure that when history state is pushed the navigation is not

flow-server/src/main/java/com/vaadin/flow/router/AfterNavigationEvent.java
@@ -73,4 +73,13 @@ public class AfterNavigationEvent extends EventObject {
     public Router getSource() {
         return (Router) super.getSource();
     }
+
+    /**
+     * Check if event is for a refresh of a preserveOnRefresh view.
+     *
+     * @return true if refresh of a preserve on refresh view
+     */
+    public boolean isRefreshEvent() {
+        return event.getTrigger().equals(NavigationTrigger.REFRESH);
+    }
 }

flow-server/src/main/java/com/vaadin/flow/router/BeforeEnterEvent.java
@@ -111,4 +111,13 @@ public class BeforeEnterEvent extends BeforeEvent {
         super(router, trigger, location, navigationTarget, parameters, ui,
                 layouts);
     }
+
+    /**
+     * Check if event is for a refresh of a preserveOnRefresh view.
+     *
+     * @return true if refresh of a preserve on refresh view
+     */
+    public boolean isRefreshEvent() {
+        return getTrigger().equals(NavigationTrigger.REFRESH);
+    }
 }

flow-server/src/main/java/com/vaadin/flow/router/NavigationTrigger.java
@@ -63,5 +63,10 @@ public enum NavigationTrigger {
      *
      * @see com.vaadin.flow.component.internal.JavaScriptBootstrapUI
      */
-    CLIENT_SIDE
+    CLIENT_SIDE,
+
+    /**
+     * Navigation is for a reload event on a preserveOnRefresh route.
+     */
+    REFRESH
 }

flow-server/src/test/java/com/vaadin/flow/router/internal/NavigationStateRendererTest.java
@@ -43,6 +43,11 @@ import com.vaadin.flow.component.page.History;
 import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.page.PendingJavaScriptResult;
 import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.BeforeLeaveEvent;
 import com.vaadin.flow.router.Location;
 import com.vaadin.flow.router.NavigationEvent;
 import com.vaadin.flow.router.NavigationState;
@@ -79,6 +84,29 @@ public class NavigationStateRendererTest {
     private static class PreservedView extends Text {
         PreservedView() {
             super("");
+        }
+    }
+
+    @Route(value = "preserved")
+    @PreserveOnRefresh
+    private static class PreservedEventView extends Text
+            implements BeforeEnterObserver, AfterNavigationObserver {
+        static boolean refreshBeforeEnter;
+        static boolean refreshAfterNavigation;
+
+        PreservedEventView() {
+            super("");
+        }
+
+        @Override
+        public void beforeEnter(BeforeEnterEvent event) {
+            refreshBeforeEnter = event.isRefreshEvent();
+        }
+
+        @Override
+        public void afterNavigation(AfterNavigationEvent event) {
+            refreshAfterNavigation = event.isRefreshEvent();
+
         }
     }
 
@@ -322,6 +350,59 @@ public class NavigationStateRendererTest {
         Assert.assertFalse("Session expected to not have cached view",
                 AbstractNavigationStateRenderer.hasPreservedChainOfLocation(
                         session, new Location("preserved")));
+    }
+
+    @Test
+    public void handle_preserveOnRefresh_refreshIsFlaggedInEvent() {
+        // given a service with instantiator
+        MockVaadinServletService service = createMockServiceWithInstantiator();
+
+        // given a locked session
+        MockVaadinSession session = new AlwaysLockedVaadinSession(service);
+        session.setConfiguration(new MockDeploymentConfiguration());
+
+        // given a UI that contain a window name ROOT.123
+        MockUI ui = new MockUI(session);
+        ExtendedClientDetails details = Mockito
+                .mock(ExtendedClientDetails.class);
+        Mockito.when(details.getWindowName()).thenReturn("ROOT.123");
+        ui.getInternals().setExtendedClientDetails(details);
+
+        // given a NavigationStateRenderer mapping to PreservedEventView
+        NavigationStateRenderer renderer = new NavigationStateRenderer(
+                navigationStateFromTarget(PreservedEventView.class));
+
+        // when a navigation event reaches the renderer
+        renderer.handle(new NavigationEvent(new Router(new TestRouteRegistry()),
+                new Location("preserved"), ui, NavigationTrigger.PAGE_LOAD));
+
+        // then the session has a cached record of the view
+        Assert.assertTrue("Session expected to have cached view",
+                AbstractNavigationStateRenderer.getPreservedChain(session,
+                        "ROOT.123", new Location("preserved")).isPresent());
+
+        // given the recently instantiated view
+        final PreservedEventView view = (PreservedEventView) ui.getInternals()
+                .getActiveRouterTargetsChain().get(0);
+        Assert.assertFalse(
+                "Initial view load should not be a refresh for before",
+                view.refreshBeforeEnter);
+        Assert.assertFalse(
+                "Initial view load should not be a refresh for after",
+                view.refreshAfterNavigation);
+
+        // when another navigation targets the same location
+        renderer.handle(new NavigationEvent(new Router(new TestRouteRegistry()),
+                new Location("preserved"), ui, NavigationTrigger.PAGE_LOAD));
+
+        // then the same view is routed to
+        Assert.assertEquals("Expected same view", view,
+                ui.getInternals().getActiveRouterTargetsChain().get(0));
+
+        Assert.assertTrue("Reload should be flagged for before",
+                view.refreshBeforeEnter);
+        Assert.assertTrue("Reload should be flagged for after",
+                view.refreshAfterNavigation);
     }
 
     @Test

commit 408a853aff08fc15c7e87857d4459fd1f9985a59

fix: update browser URL on preserve on refresh navigation
Programmatic self navigation with query parameters on view with preserve on
refresh do not update the URL in the browser.
This change fixes the regression introduced by #15331, updating push
history also for navigation events with trigger REFRESH.

Fixes #19701

flow-server/src/main/java/com/vaadin/flow/router/internal/AbstractNavigationStateRenderer.java
@@ -38,7 +38,6 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.page.ExtendedClientDetails;
 import com.vaadin.flow.di.Instantiator;
 import com.vaadin.flow.dom.Element;
-import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.internal.Pair;
 import com.vaadin.flow.internal.StateNode;
 import com.vaadin.flow.internal.UsageStatistics;
@@ -68,7 +67,6 @@ import com.vaadin.flow.router.Router;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.Constants;
 import com.vaadin.flow.server.HttpStatusCode;
-import com.vaadin.flow.server.Mode;
 import com.vaadin.flow.server.VaadinSession;
 
 /**
@@ -300,7 +298,8 @@ public abstract class AbstractNavigationStateRenderer
     }
 
     protected boolean shouldPushHistoryState(NavigationEvent event) {
-        return NavigationTrigger.UI_NAVIGATE.equals(event.getTrigger());
+        return NavigationTrigger.UI_NAVIGATE.equals(event.getTrigger())
+                || NavigationTrigger.REFRESH.equals(event.getTrigger());
     }
 
     private boolean isRouterLinkNotFoundNavigationError(

flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/PreserveOnRefreshNavigationView.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright 2000-2024 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.dom.ElementFactory;
+import com.vaadin.flow.router.PreserveOnRefresh;
+import com.vaadin.flow.router.QueryParameters;
+import com.vaadin.flow.router.Route;
+
+@Route(value = PreserveOnRefreshNavigationView.VIEW_PATH)
+@PreserveOnRefresh
+public class PreserveOnRefreshNavigationView extends Div {
+
+    static final String VIEW_PATH = "com.vaadin.flow.uitest.ui.PreserveOnRefreshNavigationView";
+
+    public PreserveOnRefreshNavigationView() {
+        add(createNavigationButton("one"));
+        add(createNavigationButton("two"));
+        add(createNavigationButton("three"));
+
+        getElement().appendChild(createRouterLink("one"),
+                createRouterLink("two"), createRouterLink("three"));
+    }
+
+    private NativeButton createNavigationButton(String param) {
+        NativeButton button = new NativeButton("navigate to " + param,
+                ev -> selfNavigate(param));
+        button.setId("button-" + param);
+        return button;
+    }
+
+    private void selfNavigate(String param) {
+        UI.getCurrent().navigate(PreserveOnRefreshNavigationView.class,
+                QueryParameters.of("param", param));
+    }
+
+    private Element createRouterLink(String param) {
+        Element routerLink = ElementFactory.createRouterLink(
+                VIEW_PATH + "?param=" + param, "link to " + param);
+        routerLink.setAttribute("id", "link-" + param);
+        return routerLink;
+    }
+}

flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/PreserveOnRefreshNavigationIT.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2000-2024 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.flow.uitest.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.testbench.TestBenchElement;
+
+public class PreserveOnRefreshNavigationIT extends ChromeBrowserTest {
+
+    @Test
+    public void routerLink_selfNavigationWithQueryParams_urlChanges() {
+        open();
+        $(TestBenchElement.class).id("link-one").click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("?param=one"));
+        $(TestBenchElement.class).id("link-two").click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("?param=two"));
+        $(TestBenchElement.class).id("link-three").click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("?param=three"));
+    }
+
+    @Test
+    public void programmaticNavigation_selfNavigationWithQueryParams_urlChanges() {
+        open();
+        $(TestBenchElement.class).id("button-one").click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("?param=one"));
+        $(TestBenchElement.class).id("button-two").click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("?param=two"));
+        $(TestBenchElement.class).id("button-three").click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("?param=three"));
+    }
+
+}