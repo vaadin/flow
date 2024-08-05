87dd3b50a4e209910bd2c1da3672971f172d21b1
Tomi Virtanen <tltv@vaadin.com>
2024-08-05 12:13:52 +0300

    chore: add autoInjectGlobalCssImports theme property
    Default value is false and global CssImport (with just a value) is not injected by default to exported web components.

--- flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
+++ flow-server/src/main/resources/plugins/application-theme-plugin/theme-generator.js
@@ -51,6 +51,7 @@ function writeThemeFiles(themeFolder, themeName, themeProperties, options) {
   const styles = resolve(themeFolder, stylesCssFilename);
   const documentCssFile = resolve(themeFolder, documentCssFilename);
   const autoInjectComponents = themeProperties.autoInjectComponents ?? true;
+  const autoInjectGlobalCssImports = themeProperties.autoInjectGlobalCssImports ?? false;
   const globalFilename = 'theme-' + themeName + '.global.generated.js';
   const componentsFilename = 'theme-' + themeName + '.components.generated.js';
   const themeFilename = 'theme-' + themeName + '.generated.js';
@@ -223,9 +224,11 @@ function writeThemeFiles(themeFolder, themeName, themeProperties, options) {
     const removers = [];
     if (target !== document) {
       ${shadowOnlyCss.join('')}
-      webcomponentGlobalCssInjector((css) => {
-        removers.push(injectGlobalCss(css, '', target));
-      });
+      ${autoInjectGlobalCssImports ? `
+        webcomponentGlobalCssInjector((css) => {
+          removers.push(injectGlobalCss(css, '', target));
+        });
+        ` : ''}
     }
     ${parentTheme}
     ${globalCssCode.join('')}
