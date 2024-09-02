0d27e3361bb1f30b173432ae9db923d2e55c03e1
Tomi Virtanen <tltv@vaadin.com>
2024-09-02 09:24:08 +0300

    chore: add premiumFeatures.enable token to build info (#19846)
    * chore: add premiumFeatures.enable token to build info
    
    * dropped _TOKEN from constant name
    
    * formatting
    
    * Update flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/plugin/base/BuildFrontendUtil.java
    
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
    
    * Update flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/plugin/base/BuildFrontendUtil.java
    
    ---------
    
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

--- flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/plugin/base/BuildFrontendUtil.java
+++ flow-plugins/flow-plugin-base/src/main/java/com/vaadin/flow/plugin/base/BuildFrontendUtil.java
@@ -755,9 +755,16 @@ public class BuildFrontendUtil {
             buildInfo.put(SERVLET_PARAMETER_PRODUCTION_MODE, true);
             buildInfo.put(APPLICATION_IDENTIFIER,
                     adapter.applicationIdentifier());
-            if (licenseRequired && LocalSubscriptionKey.get() != null) {
-                adapter.logInfo("Daily Active User tracking enabled");
-                buildInfo.put(DAU_TOKEN, true);
+            if (licenseRequired) {
+                if (LocalSubscriptionKey.get() != null) {
+                    adapter.logInfo("Daily Active User tracking enabled");
+                    buildInfo.put(DAU_TOKEN, true);
+                }
+                if (LicenseChecker.isValidLicense("vaadin-commercial-cc-client",
+                        null, BuildType.PRODUCTION)) {
+                    adapter.logInfo("Premium Features are enabled");
+                    buildInfo.put(Constants.PREMIUM_FEATURES, true);
+                }
             }
 
             FileUtils.write(tokenFile, JsonUtil.stringify(buildInfo, 2) + "\n",
