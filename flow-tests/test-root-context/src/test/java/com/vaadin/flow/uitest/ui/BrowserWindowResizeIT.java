0df562f6b102f474f0745a5677ef2f831c256afb
Marco Collovati <marco@vaadin.com>
2024-09-04 12:33:44 +0200

    test: assert after browser resize event listener completes (#19886)
    Waits for the browser resize event listener to update the UI
    before asserting on the window width.

--- flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/BrowserWindowResizeIT.java
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/BrowserWindowResizeIT.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 
@@ -22,19 +23,24 @@ public class BrowserWindowResizeIT extends ChromeBrowserTest {
     public void listenResizeEvent() {
         open();
         Dimension currentSize = getDriver().manage().window().getSize();
-
         int newWidth = currentSize.getWidth() - 10;
         getDriver().manage().window()
                 .setSize(new Dimension(newWidth, currentSize.getHeight()));
 
         WebElement info = findElement(By.id("size-info"));
+        waitForWidthChange(info, currentSize.getWidth());
 
         Assert.assertEquals(String.valueOf(newWidth), info.getText());
 
+        currentSize = getDriver().manage().window().getSize();
         newWidth -= 30;
         getDriver().manage().window()
                 .setSize(new Dimension(newWidth, currentSize.getHeight()));
-
+        waitForWidthChange(info, currentSize.getWidth());
         Assert.assertEquals(String.valueOf(newWidth), info.getText());
+    }
+
+    private void waitForWidthChange(WebElement info, int currentWidth) {
+        waitUntil(d -> !String.valueOf(currentWidth).equals(info.getText()));
     }
 }
