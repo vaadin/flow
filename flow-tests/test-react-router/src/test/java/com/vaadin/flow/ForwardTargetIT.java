f89e4d1fe971364e0b80898e2154854182b30783
Teppo Kurki <teppo.kurki@vaadin.com>
2024-09-18 13:06:04 +0300

    Tests

--- flow-tests/test-react-router/src/test/java/com/vaadin/flow/ForwardTargetIT.java
+++ flow-tests/test-react-router/src/test/java/com/vaadin/flow/ForwardTargetIT.java
@@ -3,22 +3,41 @@ package com.vaadin.flow;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.vaadin.flow.component.html.testbench.SpanElement;
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 
 public class ForwardTargetIT extends ChromeBrowserTest {
 
+    public static final String FORWARD_TARGET_VIEW = "/view/com.vaadin.flow.ForwardTargetView";
+
     // Test for https://github.com/vaadin/flow/issues/19794
     @Test
     public void testUrlIsCorrectAfterForward() {
-        open();
+        getDriver().get(getTestURL(getRootURL(), FORWARD_TARGET_VIEW, null));
 
-        waitUntil(arg -> driver.getCurrentUrl().endsWith("ForwardTargetView"));
+        waitUntil(arg -> driver.getCurrentUrl().endsWith(FORWARD_TARGET_VIEW));
 
-        openUrl("/view/com.vaadin.flow.ForwardingView");
+        getDriver().get(getTestURL(getRootURL(),
+                "/view/com.vaadin.flow.ForwardingView", null));
 
-        waitUntil(arg -> driver.getCurrentUrl().endsWith("ForwardTargetView"));
+        waitUntil(arg -> driver.getCurrentUrl().endsWith(FORWARD_TARGET_VIEW));
 
-        Assert.assertTrue(driver.getCurrentUrl()
-                .endsWith("com.vaadin.flow.ForwardTargetView"));
+        Assert.assertTrue("URL was not the expected one after forward call",
+                driver.getCurrentUrl().endsWith(FORWARD_TARGET_VIEW));
+    }
+
+    // Test for https://github.com/vaadin/flow/issues/19822
+    @Test
+    public void testSetParameterCalledOnlyOnceAfterForward() {
+        getDriver().get(getTestURL(getRootURL(),
+                "/view/com.vaadin.flow.ForwardingToParametersView", null));
+
+        waitUntil(arg -> driver.getCurrentUrl().endsWith(
+                "/view/com.vaadin.flow.ForwardTargetWithParametersView"));
+
+        Assert.assertEquals("setParameter was called more than once", 1,
+                $(SpanElement.class).all().stream()
+                        .filter(span -> span.getText().equals("setParameter"))
+                        .count());
     }
 }
