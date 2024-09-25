ea3631d957c5cfcabee5b3a16287ff2ff3baf989
Tomi Virtanen <tltv@vaadin.com>
2024-09-25 08:13:11 +0300

    fix: hide root output element (#20040)
    Ensures that root output element added for React router in Flow.tsx doesn't accidentally change the main layout with CSS rules targeting it.
    
    Fixes: #19871

--- flow-server/src/main/resources/com/vaadin/flow/server/frontend/Flow.tsx
+++ flow-server/src/main/resources/com/vaadin/flow/server/frontend/Flow.tsx
@@ -367,7 +367,7 @@ function Flow() {
     }, [pathname, search, hash]);
 
     return <>
-        <output ref={ref} />
+        <output ref={ref} style={{display: "none"}}/>
         {portals.map(({children, domNode}) => createPortal(children, domNode))}
     </>;
 }
