2cbdae4f576864a90210f000e723d3a4381ac7f1
Artur Signell <artur@vaadin.com>
2024-10-07 14:50:45 +0300

    chore!: Wrap each enum on a separate line when there are many of them

--- flow-data/src/test/java/com/vaadin/flow/tests/data/bean/Country.java
+++ flow-data/src/test/java/com/vaadin/flow/tests/data/bean/Country.java
@@ -2,8 +2,12 @@ package com.vaadin.flow.tests.data.bean;
 
 public enum Country {
 
-    FINLAND("Finland"), SWEDEN("Sweden"), USA("USA"), RUSSIA(
-            "Russia"), NETHERLANDS("Netherlands"), SOUTH_AFRICA("South Africa");
+    FINLAND("Finland"),
+    SWEDEN("Sweden"),
+    USA("USA"),
+    RUSSIA("Russia"),
+    NETHERLANDS("Netherlands"),
+    SOUTH_AFRICA("South Africa");
 
     private final String name;
 
