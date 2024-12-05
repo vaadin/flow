9cbca2f147519aea862d8e265927ebbd6d69e839
caalador <mikael.grankvist@gmail.com>
2024-12-05 09:50:23 +0200

    revert

--- flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
+++ flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
@@ -2,7 +2,7 @@ package com.vaadin.flow.uitest.ui.dependencies;
 
 public class TestVersion {
 
-    public static final String VAADIN = "24.5.7";
+    public static final String VAADIN = "24.5.4";
     public static final String FONTAWESOME = "5.15.1";
 
 }
