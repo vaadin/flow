5f86ae3b68a1860c4b487ce57edd6d6d47edff54
Mikael Grankvist <mgrankvi@vaadin.com>
2024-12-13 17:43:53 +0200

    unused import
    revert stopWait change

--- flow-server/src/main/java/com/vaadin/flow/server/startup/DevModeInitializer.java
+++ flow-server/src/main/java/com/vaadin/flow/server/startup/DevModeInitializer.java
@@ -30,7 +30,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
