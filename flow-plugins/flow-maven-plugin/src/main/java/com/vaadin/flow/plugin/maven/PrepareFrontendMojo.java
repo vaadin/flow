6aa43a57153b62425e487643e833f04acbd515ce
AB <a.bierler@xdev-software.de>
2024-12-13 10:45:39 +0100

    Remove ``productionMode`` check
    This check is only needed once during migration and never again.

--- flow-plugins/flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/maven/PrepareFrontendMojo.java
+++ flow-plugins/flow-maven-plugin/src/main/java/com/vaadin/flow/plugin/maven/PrepareFrontendMojo.java
@@ -52,11 +52,6 @@ public class PrepareFrontendMojo extends FlowModeAbstractMojo {
     @Override
     protected void executeInternal()
             throws MojoExecutionException, MojoFailureException {
-        if (productionMode != null) {
-            logWarn("The <productionMode>" + productionMode
-                    + "</productionMode> Maven parameter no longer has any effect and can be removed. Production mode is automatically enabled when you run the build-frontend target.");
-        }
-
         // propagate info via System properties and token file
         File tokenFile = BuildFrontendUtil.propagateBuildInfo(this);
 
