c272a4355a7d16ce36068afdffea43a40e05a8bf
Artur Signell <artur@vaadin.com>
2025-01-13 11:00:14 +0200

    Add convenience constructors

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/data/filter/AndFilter.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/data/filter/AndFilter.java
@@ -9,7 +9,25 @@ import java.util.List;
  * filters and verifying that all of them pass.
  */
 public class AndFilter extends Filter {
+
     private List<Filter> children;
+
+    /**
+     * Create an empty filter.
+     */
+    public AndFilter() {
+        // Empty constructor is needed for serialization
+    }
+
+    /**
+     * Create a filter with the given children.
+     * 
+     * @param children
+     *            the children of the filter
+     */
+    public AndFilter(Filter... children) {
+        setChildren(List.of(children));
+    }
 
     public List<Filter> getChildren() {
         return children;
@@ -21,7 +39,7 @@ public class AndFilter extends Filter {
 
     @Override
     public String toString() {
-        return "AndFilter [children=" + children + "]";
+        return getClass().getSimpleName()+" [children=" + children + "]";
     }
 
 }
