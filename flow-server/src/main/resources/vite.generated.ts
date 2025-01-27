4a88b1340e2c02062d450873ffe8730a18a640b8
Teppo Kurki <teppo.kurki@vaadin.com>
2025-01-27 11:31:20 +0200

    Update vite.generated.ts

--- flow-server/src/main/resources/vite.generated.ts
+++ flow-server/src/main/resources/vite.generated.ts
@@ -266,7 +266,7 @@ function statsExtracterPlugin(): PluginOption {
       const frontendFiles: Record<string, string> = {};
       frontendFiles['index.html'] = createHash('sha256').update(customIndexData.replace(/\r\n/g, '\n'), 'utf8').digest('hex');
 
-      const projectFileExtensions = ['.js', '.js.map', '.ts', '.ts.map', '.tsx', '.tsx.map', '.css', '.css.map'#frontendExtraFileExtensions#];
+      const projectFileExtensions = ['.js', '.js.map', '.ts', '.ts.map', '.tsx', '.tsx.map', '.css', '.css.map'];
 
       const isThemeComponentsResource = (id: string) =>
           id.startsWith(themeOptions.frontendGeneratedFolder.replace(/\\/g, '/'))
