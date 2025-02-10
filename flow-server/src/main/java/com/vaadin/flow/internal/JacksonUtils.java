8dfbe537c4283a1d4f7fd605b402f61dd0af1622
Mikael Grankvist <mgrankvi@vaadin.com>
2025-02-10 14:09:22 +0200

    feat: update fronted to use Jackson
    part of #20741

--- flow-server/src/main/java/com/vaadin/flow/internal/JacksonUtils.java
+++ flow-server/src/main/java/com/vaadin/flow/internal/JacksonUtils.java
@@ -41,6 +41,7 @@ import com.fasterxml.jackson.databind.node.DoubleNode;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.LoggerFactory;
 
 /**
  * Helpers for using <code>jackson</code>.
