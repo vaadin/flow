975ede155d2b6e325d32877912a748ba9999c1a8
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2025-02-12 11:06:37 +0200

    Revert "feat: Add interfaces for CRUD services (#20743)"
    This reverts commit c96213d5

--- vaadin-spring/src/test/java/com/vaadin/flow/spring/data/TestApplication.java
+++
@@ -1,7 +0,0 @@
-package com.vaadin.flow.spring.data;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TestApplication {
-}
--- vaadin-spring/src/test/java/com/vaadin/flow/spring/data/jpa/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vaadin.flow.spring.data.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface TestRepository extends JpaRepository<TestObject, Integer>,
-        JpaSpecificationExecutor<TestObject> {
-
-}
