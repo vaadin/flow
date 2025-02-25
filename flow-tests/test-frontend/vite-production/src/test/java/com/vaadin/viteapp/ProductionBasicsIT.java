fe1db226217de7bb69dd53c02a4c84a21f94bc69
Artur <artur@vaadin.com>
2025-02-25 15:08:43 +0200

    chore: Upgrade target to es2022 (#21039)
    The combination of TS target es2022 and Vite build target including safari15 keeps the output compatible with Safari 15. It also enables using all features added in es2021 and es2022.

--- flow-tests/test-frontend/vite-production/src/test/java/com/vaadin/viteapp/ProductionBasicsIT.java
+++ flow-tests/test-frontend/vite-production/src/test/java/com/vaadin/viteapp/ProductionBasicsIT.java
@@ -57,4 +57,15 @@ public class ProductionBasicsIT extends ChromeBrowserTest {
         Assert.assertEquals(200, size.getWidth());
         Assert.assertEquals(200, size.getHeight());
     }
+
+    @Test
+    public void toplevelAwaitWorks() {
+        getDriver().get(getRootURL());
+        waitForDevServer();
+        String value = waitUntil(driver -> (String) executeScript(
+                "return window.topLevelAwaitValue"));
+
+        Assert.assertEquals("This is the value set in other.js", value);
+    }
+
 }
