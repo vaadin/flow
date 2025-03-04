f11e94d775501d4be486dd9663e43c8bf5170ce9
Mikael Grankvist <mgrankvi@vaadin.com>
2025-03-04 06:45:16 +0200

    Have deprecated broadcast

--- vaadin-dev-server/src/main/java/com/vaadin/base/devserver/DebugWindowConnection.java
+++ vaadin-dev-server/src/main/java/com/vaadin/base/devserver/DebugWindowConnection.java
@@ -46,6 +46,8 @@ import com.vaadin.pro.licensechecker.BuildType;
 import com.vaadin.pro.licensechecker.LicenseChecker;
 import com.vaadin.pro.licensechecker.Product;
 
+import elemental.json.JsonObject;
+
 /**
  * {@link BrowserLiveReload} implementation class.
  * <p>
@@ -247,6 +249,18 @@ public class DebugWindowConnection implements BrowserLiveReload {
     @Override
     public boolean isLiveReload(AtmosphereResource resource) {
         return getRef(resource) != null;
+    }
+
+    /**
+     * Broadcasts the given message to all connected clients.
+     *
+     * @param msg
+     *            the message to broadcast
+     * @deprecated Use {@link #broadcast(ObjectNode)} instead.
+     */
+    @Deprecated
+    public void broadcast(JsonObject msg) {
+        this.broadcast(JacksonUtils.readTree(msg.toJson()));
     }
 
     /**
