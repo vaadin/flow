c278fde22fc79b50f8c04cf9ce9b0dd587800758
Teppo Kurki <teppo.kurki@vaadin.com>
2025-03-13 13:27:56 +0200

    fix: watch active project resources (#21099)
    * fix: watch active project resources
    
    * add tests for external file watcher
    
    * fix typo, increase sleep time
    
    * fix non-matching case

--- vaadin-dev-server/src/test/java/com/vaadin/base/devserver/FileWatcherTest.java
+++ vaadin-dev-server/src/test/java/com/vaadin/base/devserver/FileWatcherTest.java
@@ -1,11 +1,18 @@
 package com.vaadin.base.devserver;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import com.vaadin.flow.server.InitParameters;
+import com.vaadin.flow.server.VaadinContext;
+import com.vaadin.flow.server.startup.ApplicationConfiguration;
 
 public class FileWatcherTest {
 
@@ -25,5 +32,116 @@ public class FileWatcherTest {
 
         Thread.sleep(50); // The watcher is supposed to be triggered immediately
         Assert.assertEquals(newFile, changed.get());
+    }
+
+    @Test
+    public void externalDependencyWatcher_setViaParameter_TriggeredForModification()
+            throws Exception {
+        File projectFolder = Files.createTempDirectory("projectFolder")
+                .toFile();
+        projectFolder.deleteOnExit();
+
+        String metaInf = "/src/main/resources/META-INF/";
+        String rootPorjectResourceFrontend = projectFolder.getAbsolutePath()
+                + metaInf + "resources/frontend";
+        String subProjectLegacyFrontend = projectFolder.getAbsolutePath()
+                + "/fakeproject" + metaInf + "frontend";
+
+        new File(rootPorjectResourceFrontend).mkdirs();
+        new File(subProjectLegacyFrontend).mkdirs();
+
+        File jarFrontendResources = Files
+                .createTempDirectory("jarFrontendResources").toFile();
+        jarFrontendResources.deleteOnExit();
+
+        VaadinContext vaadinContext = Mockito.mock(VaadinContext.class);
+        ApplicationConfiguration config = Mockito
+                .mock(ApplicationConfiguration.class);
+        Mockito.when(config.getStringProperty(
+                InitParameters.FRONTEND_HOTDEPLOY_DEPENDENCIES, null))
+                .thenReturn("./,./fakeproject");
+        Mockito.when(config.getProjectFolder()).thenReturn(projectFolder);
+
+        try (MockedStatic<ApplicationConfiguration> appConfig = Mockito
+                .mockStatic(ApplicationConfiguration.class)) {
+            appConfig.when(() -> ApplicationConfiguration.get(Mockito.any()))
+                    .thenReturn(config);
+            new ExternalDependencyWatcher(vaadinContext, jarFrontendResources);
+
+            assertFileCountFound(jarFrontendResources, 0);
+
+            createFile(rootPorjectResourceFrontend + "/somestyles.css");
+            assertFileCountFound(jarFrontendResources, 1);
+
+            createFile(subProjectLegacyFrontend + "/somejs.js");
+            assertFileCountFound(jarFrontendResources, 2);
+
+            Assert.assertEquals("somestyles.css",
+                    jarFrontendResources.listFiles()[0].getName());
+            Assert.assertEquals("somejs.js",
+                    jarFrontendResources.listFiles()[1].getName());
+        }
+    }
+
+    @Test
+    public void externalDependencyWatcher_setAsDefaultForRunnerProjectButNotSubProject_TriggeredForModification()
+            throws Exception {
+        File projectFolder = Files.createTempDirectory("projectFolder")
+                .toFile();
+        projectFolder.deleteOnExit();
+
+        String metaInf = "/src/main/resources/META-INF/";
+        String rootPorjectResourceFrontend = projectFolder.getAbsolutePath()
+                + metaInf + "resources/frontend";
+        String subProjectLegacyFrontend = projectFolder.getAbsolutePath()
+                + "/fakeproject" + metaInf + "frontend";
+
+        new File(rootPorjectResourceFrontend).mkdirs();
+        new File(subProjectLegacyFrontend).mkdirs();
+
+        File jarFrontendResources = Files
+                .createTempDirectory("jarFrontendResources").toFile();
+        jarFrontendResources.deleteOnExit();
+
+        VaadinContext vaadinContext = Mockito.mock(VaadinContext.class);
+        ApplicationConfiguration config = Mockito
+                .mock(ApplicationConfiguration.class);
+        Mockito.when(config.getStringProperty(
+                InitParameters.FRONTEND_HOTDEPLOY_DEPENDENCIES, null))
+                .thenReturn(null);
+        Mockito.when(config.getProjectFolder()).thenReturn(projectFolder);
+
+        try (MockedStatic<ApplicationConfiguration> appConfig = Mockito
+                .mockStatic(ApplicationConfiguration.class)) {
+            appConfig.when(() -> ApplicationConfiguration.get(Mockito.any()))
+                    .thenReturn(config);
+            new ExternalDependencyWatcher(vaadinContext, jarFrontendResources);
+
+            assertFileCountFound(jarFrontendResources, 0);
+
+            createFile(rootPorjectResourceFrontend + "/somestyles.css");
+            assertFileCountFound(jarFrontendResources, 1);
+
+            createFile(subProjectLegacyFrontend + "/somejs.js");
+            assertFileCountFound(jarFrontendResources, 1);
+
+            Assert.assertEquals("somestyles.css",
+                    jarFrontendResources.listFiles()[0].getName());
+        }
+    }
+
+    private void assertFileCountFound(File directory, int count)
+            throws InterruptedException {
+        Thread.sleep(100);
+        Assert.assertEquals(
+                "Wrong amount of copied files found when there should be "
+                        + count + ".",
+                count, directory.listFiles().length);
+
+    }
+
+    private void createFile(String path) throws IOException {
+        File newFile = new File(path);
+        newFile.createNewFile();
     }
 }
