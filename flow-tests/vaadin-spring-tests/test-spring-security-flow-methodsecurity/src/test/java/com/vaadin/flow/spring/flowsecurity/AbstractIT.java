11ed648bea80dc2e6cfd290c44039c1695408064
Marco Collovati <marco@vaadin.com>
2025-04-18 10:22:39 +0200

    fix test

--- flow-tests/vaadin-spring-tests/test-spring-security-flow-methodsecurity/src/test/java/com/vaadin/flow/spring/flowsecurity/AbstractIT.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow-methodsecurity/src/test/java/com/vaadin/flow/spring/flowsecurity/AbstractIT.java
@@ -86,6 +86,8 @@ public abstract class AbstractIT extends AbstractSpringTest {
         form.getUsernameField().setValue(username);
         form.getPasswordField().setValue(password);
         form.submit();
+        waitUntilNot(
+                driver -> driver.getCurrentUrl().contains("my/login/page"));
         waitUntilNot(driver -> $(LoginOverlayElement.class).exists());
     }
 
