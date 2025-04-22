48013a548001adcfdd6b27599c9c333570e075ed
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2025-04-22 14:07:55 +0300

    upgrade pnpm to 8

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
@@ -57,7 +57,7 @@ public class FrontendTools {
      */
     public static final String DEFAULT_NODE_VERSION = "v22.14.0";
 
-    public static final String DEFAULT_PNPM_VERSION = "10.9.2";
+    public static final String DEFAULT_PNPM_VERSION = "8.6.11";
 
     public static final String INSTALL_NODE_LOCALLY = "%n  $ mvn com.github.eirslett:frontend-maven-plugin:1.10.0:install-node-and-npm "
             + "-DnodeVersion=\"" + DEFAULT_NODE_VERSION + "\" ";
