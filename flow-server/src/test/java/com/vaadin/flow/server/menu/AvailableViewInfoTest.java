57bfdcd5c4ac8fa81fdc59f21220cdf142151e29
Luciano Vernaschi <luciano@vaadin.com>
2025-04-23 13:36:47 +0200

    Add some tests

---
+++ flow-server/src/test/java/com/vaadin/flow/server/menu/AvailableViewInfoTest.java
@@ -0,0 +1,48 @@
+package com.vaadin.flow.server.menu;
+
+import com.vaadin.flow.router.MenuData;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Map;
+
+public class AvailableViewInfoTest {
+
+    MenuData menuData = new MenuData("title", 1.0, false, "icon", null);
+
+    @Test
+    public void testEquality() {
+        Assert.assertEquals("Two instance created the same way are not equal",
+                createInfo(false), createInfo(false));
+    }
+
+    @Test
+    public void testSerialization() throws IOException, ClassNotFoundException {
+        var info = createInfo(true);
+
+        var baos = new ByteArrayOutputStream();
+        var oos = new ObjectOutputStream(baos);
+        oos.writeObject(info);
+        oos.close();
+
+        var ois = new ObjectInputStream(
+                new ByteArrayInputStream(baos.toByteArray()));
+        var deserializedInfo = (AvailableViewInfo) ois.readObject();
+
+        Assert.assertEquals("Serialized instance is not equal to origin", info,
+                deserializedInfo);
+    }
+
+    private AvailableViewInfo createInfo(boolean withChild) {
+        return new AvailableViewInfo("Title", new String[] { "role1" }, false,
+                "route", false, true, menuData,
+                withChild ? List.of(createInfo(false)) : List.of(),
+                Map.of("param", RouteParamType.REQUIRED), false, "detail");
+    }
+}
