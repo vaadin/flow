385ea9d35c45eae5cfda32a42754e2814dfa7110
Luciano Vernaschi <luciano@vaadin.com>
2025-04-23 16:12:50 +0200

    Enrich test

--- flow-server/src/test/java/com/vaadin/flow/server/menu/AvailableViewInfoTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/menu/AvailableViewInfoTest.java
@@ -1,7 +1,10 @@
 package com.vaadin.flow.server.menu;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.router.MenuData;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -14,7 +17,26 @@ import java.util.Map;
 
 public class AvailableViewInfoTest {
 
-    MenuData menuData = new MenuData("title", 1.0, false, "icon", null);
+    private record Badge(String text, String color) {
+    }
+
+    private record Detail(String description, int importance, Badge badge) {
+    }
+
+    ObjectMapper mapper;
+    MenuData menuData;
+    Badge badge;
+    Detail detail;
+    String detailAsString;
+
+    @Before
+    public void setUp() throws Exception {
+        mapper = new ObjectMapper();
+        menuData = new MenuData("title", 1.0, false, "icon", null);
+        badge = new Badge("New!", "green");
+        detail = new Detail("A nice page", 123, badge);
+        detailAsString = mapper.writeValueAsString(detail);
+    }
 
     @Test
     public void testEquality() {
@@ -39,10 +61,19 @@ public class AvailableViewInfoTest {
                 deserializedInfo);
     }
 
+    @Test
+    public void testJsonSerialization() throws JsonProcessingException {
+        var info = createInfo(true);
+        var json = mapper.writeValueAsString(info);
+        Assert.assertEquals("JSON conversion doesn't give the same object",
+                info, mapper.readValue(json, AvailableViewInfo.class));
+    }
+
     private AvailableViewInfo createInfo(boolean withChild) {
         return new AvailableViewInfo("Title", new String[] { "role1" }, false,
                 "route", false, true, menuData,
                 withChild ? List.of(createInfo(false)) : List.of(),
-                Map.of("param", RouteParamType.REQUIRED), false, "detail");
+                Map.of("param", RouteParamType.REQUIRED), false,
+                detailAsString);
     }
 }
