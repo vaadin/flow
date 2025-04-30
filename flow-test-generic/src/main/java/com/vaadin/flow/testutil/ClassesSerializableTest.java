642d98648dc05304612e7d239d0b232dc16d88ea
Marco Collovati <marco@vaadin.com>
2025-04-30 12:31:20 +0200

    fix: prevent file watcher threads from slowing down JVM shutdown (#21365) (#21372)
    FileWatcher was using an executor that creates non-daemon threads to watch
    for file changes. This caused a delay in JVM shutdown, as it would
    wait at least 60 seconds for all threads in the executor pool to be
    evicted.
    
    This change provides a custom thread factory that creates daemon threads
    for the executor to prevent JVM shutdown delays.

--- flow-test-generic/src/main/java/com/vaadin/flow/testutil/ClassesSerializableTest.java
+++ flow-test-generic/src/main/java/com/vaadin/flow/testutil/ClassesSerializableTest.java
@@ -85,6 +85,7 @@ public abstract class ClassesSerializableTest extends ClassFinder {
                 "com\\.vaadin\\.base\\.devserver\\.DevToolsMessageHandler",
                 "com\\.vaadin\\.base\\.devserver\\.ExternalDependencyWatcher",
                 "com\\.vaadin\\.base\\.devserver\\.FileWatcher",
+                "com\\.vaadin\\.base\\.devserver\\.NamedDaemonThreadFactory",
                 "com\\.vaadin\\.base\\.devserver\\.IdeIntegration",
                 "com\\.vaadin\\.base\\.devserver\\.OpenInCurrentIde.*",
                 "com\\.vaadin\\.base\\.devserver\\.RestartMonitor",
