65400dfa7881ec55af5075f469b688abd7da67f6
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2025-05-06 09:26:07 +0300

    fix serializable test

--- flow-test-generic/src/main/java/com/vaadin/flow/testutil/ClassesSerializableTest.java
+++ flow-test-generic/src/main/java/com/vaadin/flow/testutil/ClassesSerializableTest.java
@@ -220,7 +220,7 @@ public abstract class ClassesSerializableTest extends ClassFinder {
                 "com\\.vaadin\\.flow\\.server\\.webcomponent\\.WebComponentGenerator",
                 "com\\.vaadin\\.flow\\.server\\.communication\\.WebComponentBootstrapHandler(\\$.*)?",
                 "com\\.vaadin\\.flow\\.server\\.streams\\.TransferContext",
-                "com\\.vaadin\\.flow\\.server\\.DownloadRequest",
+                "com\\.vaadin\\.flow\\.server\\.DownloadEvent",
 
                 "com\\.vaadin\\.flow\\.server\\.DevModeHandler(\\$.*)?",
                 // Frontend tasks classes which are not stored anywhere but used
