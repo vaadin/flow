fa013158878bb69dcdc4f62d5e5b5dd1fe178846
Teppo Kurki <teppo.kurki@vaadin.com>
2025-05-09 15:40:14 +0300

    remove webpack stuff

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/TaskInstallFrontendBuildPluginsTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/TaskInstallFrontendBuildPluginsTest.java
@@ -81,13 +81,6 @@ public class TaskInstallFrontendBuildPluginsTest {
     }
 
     @Test
-    public void webpackPluginsAreCopied() throws IOException {
-        task.execute();
-
-        assertPlugins();
-    }
-
-    @Test
     public void pluginsDefineAllScriptFiles() throws IOException {
         for (String plugin : FrontendPluginsUtil.getPlugins()) {
             verifyPluginScriptFilesAreDefined(plugin);
