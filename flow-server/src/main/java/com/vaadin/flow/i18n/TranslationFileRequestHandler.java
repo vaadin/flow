447ed446bc68de311fb596c9a8adfe97cd4e3d53
Luciano Vernaschi <luciano@vaadin.com>
2025-05-27 12:58:54 +0200

    feat(i18n): add chunking support (#21480)
    Co-authored-by: Anton Platonov <anton@vaadin.com>

--- flow-server/src/main/java/com/vaadin/flow/i18n/TranslationFileRequestHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/i18n/TranslationFileRequestHandler.java
@@ -29,12 +29,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Handles translation file requests. Translation file requests are internal
@@ -62,6 +66,8 @@ public class TranslationFileRequestHandler extends SynchronizedRequestHandler {
 
     static final String LANGUAGE_TAG_PARAMETER_NAME = "langtag";
 
+    static final String CHUNK_PARAMETER_NAME = "chunks";
+
     static final String RETRIEVED_LOCALE_HEADER_NAME = "X-Vaadin-Retrieved-Locale";
 
     private static final Locale FALLBACK_LOCALE = Locale.ROOT;
@@ -69,6 +75,8 @@ public class TranslationFileRequestHandler extends SynchronizedRequestHandler {
     private final DefaultI18NProvider i18NProvider;
 
     private final boolean hasFallbackBundle;
+
+    private Map<String, String[]> chunkData;
 
     public TranslationFileRequestHandler(I18NProvider i18NProvider) {
         boolean hasDefaultI18NProvider = i18NProvider != null
@@ -92,7 +100,7 @@ public class TranslationFileRequestHandler extends SynchronizedRequestHandler {
         if (translationPropertyFile == null) {
             handleNotFound(response);
         } else {
-            handleFound(response, translationPropertyFile);
+            handleFound(request, response, translationPropertyFile);
         }
         return true;
     }
@@ -103,13 +111,13 @@ public class TranslationFileRequestHandler extends SynchronizedRequestHandler {
                 HandlerHelper.RequestType.TRANSLATION_FILE);
     }
 
-    private void handleFound(VaadinResponse response,
+    private void handleFound(VaadinRequest request, VaadinResponse response,
             ResourceBundle translationPropertyFile) throws IOException {
         response.setStatus(HttpStatusCode.OK.getCode());
         response.setHeader(RETRIEVED_LOCALE_HEADER_NAME,
                 translationPropertyFile.getLocale().toLanguageTag());
         response.setHeader("Content-Type", JsonConstants.JSON_CONTENT_TYPE);
-        writeFileToResponse(response, translationPropertyFile);
+        writeFileToResponse(request, response, translationPropertyFile);
     }
 
     private void handleNotFound(VaadinResponse response) {
@@ -129,10 +137,26 @@ public class TranslationFileRequestHandler extends SynchronizedRequestHandler {
         getLogger().debug(errorMessage);
     }
 
-    private void writeFileToResponse(VaadinResponse response,
-            ResourceBundle translationPropertyFile) throws IOException {
+    private void writeFileToResponse(VaadinRequest request,
+            VaadinResponse response, ResourceBundle translationPropertyFile)
+            throws IOException {
         ObjectNode json = JacksonUtils.createObjectNode();
-        translationPropertyFile.keySet().forEach(
+        var stream = translationPropertyFile.keySet().stream();
+        var chunks = request.getParameterMap().get(CHUNK_PARAMETER_NAME);
+
+        if (chunks != null && chunks.length > 0) {
+            // Filter the keys based on the requested chunks.
+            var chunkData = getChunkData();
+            var keys = Arrays.stream(chunks).map(chunkData::get)
+                    .filter(Objects::nonNull).flatMap(Arrays::stream)
+                    .collect(Collectors.toSet());
+
+            if (!keys.isEmpty()) {
+                stream = stream.filter(keys::contains);
+            }
+        }
+
+        stream.forEach(
                 key -> json.put(key, translationPropertyFile.getString(key)));
         response.getWriter().write(json.toString());
     }
@@ -224,6 +248,46 @@ public class TranslationFileRequestHandler extends SynchronizedRequestHandler {
             }
         }
         return false;
+    }
+
+    /**
+     * Retrieves the chunk data from the JSON file.
+     *
+     * @return a map containing chunk names and their corresponding keys
+     * @throws IOException
+     *             if thrown when reading the chunk resource
+     */
+    private Map<String, String[]> getChunkData() throws IOException {
+        if (chunkData == null) {
+            chunkData = new HashMap<>();
+            var chunkResource = i18NProvider.getChunkResource();
+
+            if (chunkResource != null) {
+                var json = JacksonUtils.getMapper().readTree(chunkResource);
+                var chunksNode = json.get("chunks");
+
+                if (chunksNode != null && chunksNode.isObject()) {
+                    var fieldNames = chunksNode.fieldNames();
+
+                    while (fieldNames.hasNext()) {
+                        var chunkName = fieldNames.next();
+                        var keysNode = chunksNode.get(chunkName).get("keys");
+
+                        if (keysNode != null && keysNode.isArray()) {
+                            var keys = new String[keysNode.size()];
+
+                            for (int i = 0; i < keysNode.size(); i++) {
+                                keys[i] = keysNode.get(i).asText();
+                            }
+
+                            chunkData.put(chunkName, keys);
+                        }
+                    }
+                }
+            }
+        }
+
+        return chunkData;
     }
 
     private Logger getLogger() {
