98ece08ca2b56ba9675c92a65c6a3adaafc06b4d
Marco Collovati <marco@vaadin.com>
2025-06-05 11:00:07 +0200

    chore: bump components version (#21644)

--- flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
+++ flow-tests/test-resources/src/main/java/com/vaadin/flow/uitest/ui/dependencies/TestVersion.java
@@ -10,7 +10,7 @@ package com.vaadin.flow.uitest.ui.dependencies;
 
 public class TestVersion {
 
-    public static final String VAADIN = "23.3.0-alpha2";
+    public static final String VAADIN = "23.5.13";
     public static final String FONTAWESOME = "5.15.1";
 
 }
