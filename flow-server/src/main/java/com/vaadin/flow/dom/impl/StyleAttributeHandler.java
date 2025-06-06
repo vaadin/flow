0da09c1bfa5e24901d8ef653044b7fc1cc2ec184
Matti Tahvonen <matti@vaadin.com>
2025-06-06 16:57:07 +0300

    Get rid of transitive dependency with little value
    Closes #18719

--- flow-server/src/main/java/com/vaadin/flow/dom/impl/StyleAttributeHandler.java
+++ flow-server/src/main/java/com/vaadin/flow/dom/impl/StyleAttributeHandler.java
@@ -18,13 +18,6 @@ package com.vaadin.flow.dom.impl;
 import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 
-import com.helger.css.ECSSVersion;
-import com.helger.css.decl.CSSDeclaration;
-import com.helger.css.decl.CSSDeclarationList;
-import com.helger.css.reader.CSSReaderDeclarationList;
-import com.helger.css.reader.errorhandler.CollectingCSSParseErrorHandler;
-import com.helger.css.writer.CSSWriterSettings;
-
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.dom.StyleUtil;
@@ -38,6 +31,7 @@ import com.vaadin.flow.dom.StyleUtil;
  * @since 1.0
  */
 public class StyleAttributeHandler extends CustomAttribute {
+
     private static final String ERROR_PARSING_STYLE = "Error parsing style '%s': %s";
 
     @Override
@@ -75,30 +69,26 @@ public class StyleAttributeHandler extends CustomAttribute {
      */
     public static LinkedHashMap<String, String> parseStyles(
             String styleString) {
-        CollectingCSSParseErrorHandler errorCollector = new CollectingCSSParseErrorHandler();
-        CSSDeclarationList parsed = CSSReaderDeclarationList.readFromString(
-                styleString, ECSSVersion.LATEST, errorCollector);
-        if (errorCollector.hasParseErrors()) {
-            throw new IllegalArgumentException(String
-                    .format(ERROR_PARSING_STYLE, styleString, errorCollector
-                            .getAllParseErrors().get(0).getErrorMessage()));
+        try {
+            LinkedHashMap<String, String> parsedStyles = new LinkedHashMap<>();
+            String[] rules = styleString.split(";");
+            for (String rule : rules) {
+                String name = rule.substring(0, rule.indexOf(":")).trim();
+                String value = rule.substring(rule.indexOf(":") + 1);
+                if (name.isEmpty() || value.isEmpty()) {
+                    throw new IllegalArgumentException(
+                            "Style rule must contain name and value");
+                }
+                // TODO figure out if any better sanitatization is
+                // needed/feasible
+                // Probably not: users are on the "pro API" already and modern
+                // browsers doing enough sanitatision later in the browser
+                parsedStyles.put(name, value);
+            }
+            return parsedStyles;
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex);
         }
-        if (parsed == null) {
-            // Did not find any styles
-            throw new IllegalArgumentException(String.format(
-                    ERROR_PARSING_STYLE, styleString, "No styles found"));
-        }
-
-        LinkedHashMap<String, String> parsedStyles = new LinkedHashMap<>();
-        for (CSSDeclaration declaration : parsed.getAllDeclarations()) {
-            String key = declaration.getProperty();
-            String value = declaration.getExpression()
-                    .getAsCSSString(new CSSWriterSettings(ECSSVersion.LATEST)
-                            .setOptimizedOutput(true), 0);
-            parsedStyles.put(StyleUtil.styleAttributeToProperty(key), value);
-        }
-
-        return parsedStyles;
     }
 
     @Override
