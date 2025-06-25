3755f45dc2e83f44a63c6bcce343afc8eb576161
Artur Signell <artur@vaadin.com>
2025-06-25 16:45:29 +0300

    fix test

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/FrontendTools.java
@@ -68,7 +68,7 @@ public class FrontendTools {
     /**
      * This is the version shipped with the default Node version.
      */
-    public static final String DEFAULT_NPM_VERSION = "11.3.0";
+    public static final String DEFAULT_NPM_VERSION = "11.4.2";
 
     public static final String DEFAULT_PNPM_VERSION = "8.6.11";
 
