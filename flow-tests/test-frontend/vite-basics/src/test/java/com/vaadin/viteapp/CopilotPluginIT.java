dfacd2e4b90635e4e96228c09a40c077df2ef95a
abdullahtellioglu <abdullah@vaadin.com>
2025-07-22 15:21:19 +0300

    Formatter

--- flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/CopilotPluginIT.java
+++ flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/CopilotPluginIT.java
@@ -29,8 +29,10 @@ public class CopilotPluginIT extends ChromeBrowserTest {
 
     @Test
     public void expectAvailableFeaturesInjectedIntoWindow() {
-        final Set<String> expectedFeaturesSet = new HashSet<>(Set.of("COMPONENT_ANALYZER"));
-        List<String> features = (List<String>) executeScript("return window.Vaadin.copilot.VITE_COPILOT_PLUGIN.availableFeatures");
+        final Set<String> expectedFeaturesSet = new HashSet<>(
+                Set.of("COMPONENT_ANALYZER"));
+        List<String> features = (List<String>) executeScript(
+                "return window.Vaadin.copilot.VITE_COPILOT_PLUGIN.availableFeatures");
         for (String feature : features) {
             Assert.assertTrue(expectedFeaturesSet.contains(feature));
             expectedFeaturesSet.remove(feature);
@@ -40,9 +42,11 @@ public class CopilotPluginIT extends ChromeBrowserTest {
 
     @Test
     public void expectUnExpectedRequestsHandled() {
-        executeScript("window.META_HOT.send('vaadin-copilot:analyze-component', { });");
+        executeScript(
+                "window.META_HOT.send('vaadin-copilot:analyze-component', { });");
         WebElement element = findElement(By.cssSelector("div#response"));
-        waitUntil(driver -> StringUtils.isNotEmpty(findElement(By.cssSelector("div#response")).getText()));
+        waitUntil(driver -> StringUtils.isNotEmpty(
+                findElement(By.cssSelector("div#response")).getText()));
         String text = element.getText();
         ObjectNode responseBody = JacksonUtils.readTree(text);
         Assert.assertTrue(responseBody.hasNonNull("error"));
@@ -52,9 +56,13 @@ public class CopilotPluginIT extends ChromeBrowserTest {
 
     @Test
     public void expectAllNodesAreAnalyzedInGivenFilesWhenRequested() {
-        String buttonContainingFilePath = getFilePathOfNode(findElement(By.cssSelector("button")));
-        executeScript("window.META_HOT.send('vaadin-copilot:analyze-component', { filePaths: [ \"" + buttonContainingFilePath + "\"] });");
-        waitUntil(driver -> StringUtils.isNotEmpty(findElement(By.cssSelector("div#response")).getText()));
+        String buttonContainingFilePath = getFilePathOfNode(
+                findElement(By.cssSelector("button")));
+        executeScript(
+                "window.META_HOT.send('vaadin-copilot:analyze-component', { filePaths: [ \""
+                        + buttonContainingFilePath + "\"] });");
+        waitUntil(driver -> StringUtils.isNotEmpty(
+                findElement(By.cssSelector("div#response")).getText()));
         String text = findElement(By.cssSelector("div#response")).getText();
         ObjectNode responseBody = JacksonUtils.readTree(text);
         Assert.assertFalse(responseBody.has("error"));
@@ -62,15 +70,16 @@ public class CopilotPluginIT extends ChromeBrowserTest {
         Assert.assertTrue(responseBody.get("body").isArray());
     }
 
-
     private String getFilePathOfNode(WebElement element) {
-        return (String) executeScript("""
-                const fiberKey = Object.keys(arguments[0]).find(a => a.startsWith('__reactFiber'));
-                const fiber = arguments[0][fiberKey];
-                const debugSource = fiber._debugSource;
-                const fileName = debugSource.fileName;
-                return fileName;
-                """, element);
+        return (String) executeScript(
+                """
+                        const fiberKey = Object.keys(arguments[0]).find(a => a.startsWith('__reactFiber'));
+                        const fiber = arguments[0][fiberKey];
+                        const debugSource = fiber._debugSource;
+                        const fileName = debugSource.fileName;
+                        return fileName;
+                        """,
+                element);
     }
 
 }
