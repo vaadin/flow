64391d883e1bae6beef6d8caaeb501b661939938
Sergey Vinogradov <mr.vursen@gmail.com>
2025-08-12 12:10:32 +0400

    refactor!: centralize hierarchy management server-side (#21966)
    The PR refactors HierarchicalDataCommunicator so that hierarchy management (expanded items, hierarchical cache) is centralized on the server side instead of being shared between client and server. This makes the client a simple consumer of flattened data lists, which significantly reduces the client-side complexity and opens the door for introducing flat data providers to support fetching flattened data directly from the data source.
    
    Key changes
    HierarchyMapper and HierarchicalCommunicationController have been replaced with the new concept, Cache. This new class provides a system for storing data in a hierarchical structure while enabling access in a flattened format for client-side consumption.
    setRequestedRange and setParentRequestedRange have been replaced with a single setViewportRange which spans all hierarchy levels.
    Caveats
    setViewportRange does not currently remove items from Cache and KeyMapper when they move out of the viewport. However, the previous version also only removed items from KeyMapper while keeping them in HierarchyMapper, so item instances actually continued to remain in memory. I feel this could be optimized separately if needed.
    setViewportRange currently always sends the full viewport to the client even when some items are already present on the client-side. I plan to address this separately, see #21989
    refresh(T item) currently also sends the full viewport on every item refresh. I plan to address this separately, see #21989
    Test coverage
    The PR aims at providing test coverage for all core functionality and public APIs. Individual tests for internal APIs such as Cache will be added separately.
    The refactoring successfully passes all TreeGrid ITs, see #7676
    Depends on
    
    refactor: remove deprecated HierarchicalDataCommunicator APIs #21965
    Fixes #21876, #21877

---
+++ flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorDataPreloadingTest.java
@@ -0,0 +1,158 @@
+package com.vaadin.flow.data.provider.hierarchy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vaadin.flow.data.provider.CompositeDataGenerator;
+
+public class HierarchicalDataCommunicatorDataPreloadingTest
+        extends AbstractHierarchicalDataCommunicatorTest {
+    private HierarchicalDataCommunicator<Item> dataCommunicator;
+
+    @Before
+    public void init() {
+        super.init();
+
+        var treeData = new TreeData<Item>();
+        populateTreeData(treeData, 20, 2, 2);
+
+        dataCommunicator = new HierarchicalDataCommunicator<>(
+                new CompositeDataGenerator<Item>(), arrayUpdater, (items) -> {
+                }, ui.getElement().getNode(), () -> null);
+        dataCommunicator.setDataProvider(new TreeDataProvider<>(treeData),
+                null);
+    }
+
+    @Test
+    public void preloadFlatRangeForward_nearStart_requestedRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 2"), new Item("Item 2-0"), new Item("Item 1-0"),
+                new Item("Item 9")));
+
+        var items = dataCommunicator.preloadFlatRangeForward(0, 10);
+        assertPreloadedRange(
+                "Item 0, Item 0-0, Item 0-1, Item 1, Item 2, Item 2-0, Item 2-0-0, Item 2-0-1, Item 2-1, Item 3",
+                items);
+
+        Assert.assertEquals(26, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeForward_nearMiddle_requestedRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 9"), new Item("Item 19")));
+
+        var items = dataCommunicator.preloadFlatRangeForward(9, 6);
+        assertPreloadedRange(
+                "Item 9, Item 9-0, Item 9-1, Item 10, Item 11, Item 12", items);
+
+        Assert.assertEquals(22, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeForward_nearEnd_requestedRangeReturned() {
+        dataCommunicator
+                .expand(Arrays.asList(new Item("Item 0"), new Item("Item 9"),
+                        new Item("Item 19"), new Item("Item 19-1")));
+
+        var items = dataCommunicator.preloadFlatRangeForward(18, 6);
+        assertPreloadedRange(
+                "Item 18, Item 19, Item 19-0, Item 19-1, Item 19-1-0, Item 19-1-1",
+                items);
+
+        Assert.assertEquals(24, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeForward_rangeEndOutOfBounds_availableRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 9"), new Item("Item 19")));
+
+        var items = dataCommunicator.preloadFlatRangeForward(19, 10);
+        assertPreloadedRange("Item 19, Item 19-0, Item 19-1", items);
+
+        Assert.assertEquals(22, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeForward_rangeStartOutOfBounds_emptyRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 9"), new Item("Item 19")));
+
+        var items = dataCommunicator.preloadFlatRangeForward(100, 10);
+        Assert.assertEquals(0, items.size());
+
+        Assert.assertEquals(20, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeBackward_nearEnd_requestedRangeReturned() {
+        dataCommunicator
+                .expand(Arrays.asList(new Item("Item 19"), new Item("Item 18"),
+                        new Item("Item 18-1"), new Item("Item 17-1"),
+                        new Item("Item 16"), new Item("Item 9")));
+
+        var items = dataCommunicator.preloadFlatRangeBackward(19, 10);
+        assertPreloadedRange(
+                "Item 16, Item 16-0, Item 16-1, Item 17, Item 18, Item 18-0, Item 18-1, Item 18-1-0, Item 18-1-1, Item 19",
+                items);
+
+        Assert.assertEquals(26, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeBackward_nearMiddle_requestedRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 9"), new Item("Item 12"), new Item("Item 19")));
+
+        var items = dataCommunicator.preloadFlatRangeBackward(12, 6);
+        assertPreloadedRange(
+                "Item 9, Item 9-0, Item 9-1, Item 10, Item 11, Item 12", items);
+
+        Assert.assertEquals(22, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeBackward_nearStart_requestedRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 0-1"), new Item("Item 9"), new Item("Item 19")));
+
+        var items = dataCommunicator.preloadFlatRangeBackward(1, 6);
+        assertPreloadedRange(
+                "Item 0, Item 0-0, Item 0-1, Item 0-1-0, Item 0-1-1, Item 1",
+                items);
+
+        Assert.assertEquals(24, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeBackward_rangeEndOutOfBounds_availableRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 9"), new Item("Item 19")));
+
+        var items = dataCommunicator.preloadFlatRangeBackward(0, 10);
+        assertPreloadedRange("Item 0", items);
+
+        Assert.assertEquals(20, dataCommunicator.rootCache.getFlatSize());
+    }
+
+    @Test
+    public void preloadFlatRangeBackward_rangeStartOutOfBounds_emptyRangeReturned() {
+        dataCommunicator.expand(Arrays.asList(new Item("Item 0"),
+                new Item("Item 9"), new Item("Item 19")));
+        var items = dataCommunicator.preloadFlatRangeBackward(-100, 10);
+        Assert.assertEquals(0, items.size());
+
+        Assert.assertEquals(20, dataCommunicator.rootCache.getFlatSize());
+    };
+
+    private void assertPreloadedRange(String expectedItems, List<Item> items) {
+        Assert.assertEquals(expectedItems, items.stream().map(Item::getName)
+                .collect(Collectors.joining(", ")));
+    }
+}
