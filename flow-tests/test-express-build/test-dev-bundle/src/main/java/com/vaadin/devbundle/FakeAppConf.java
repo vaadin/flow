a17acd86cb064ba4b4acb3af80542e59aa82e434
Mikael Grankvist <mgrankvi@vaadin.com>
2025-08-25 14:33:03 +0300

    Add utility if imports used

--- flow-tests/test-express-build/test-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
+++ flow-tests/test-express-build/test-dev-bundle/src/main/java/com/vaadin/devbundle/FakeAppConf.java
@@ -15,13 +15,11 @@
  */
 package com.vaadin.devbundle;
 
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
 @Theme("vaadin-dev-bundle")
-@CssImport("@vaadin/vaadin-lumo-styles/utility.css")
 @PWA(name = "vaadin-dev-bundle", shortName = "vaadin-dev-bundle")
 public class FakeAppConf implements AppShellConfigurator {
 
--- flow-tests/test-themes/src/main/java/com/vaadin/flow/uitest/ui/theme/AppShell.java
+++ flow-tests/test-themes/src/main/java/com/vaadin/flow/uitest/ui/theme/AppShell.java
@@ -16,7 +16,6 @@
 
 package com.vaadin.flow.uitest.ui.theme;
 
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.LoadDependenciesOnStartup;
@@ -28,7 +27,6 @@ import com.vaadin.flow.uitest.ui.dependencies.TestVersion;
 @NpmPackage(value = "@vaadin/vaadin-themable-mixin", version = TestVersion.VAADIN)
 @NpmPackage(value = "@fortawesome/fontawesome-free", version = TestVersion.FONTAWESOME, assets = {
         "svgs/regular/**:npm/icons" })
-@CssImport("@vaadin/vaadin-lumo-styles/utility.css")
 @LoadDependenciesOnStartup
 public class AppShell implements AppShellConfigurator {
 }
