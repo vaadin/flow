617c596666c6aab1862b67503ec4818bd741035a
Artur <artur@vaadin.com>
2025-08-25 12:33:33 +0300

    feat!: Upgrade to React 19 (#21523)
    Removes the React 19 feature flag and always uses React 19.

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/NodeUpdater.java
@@ -307,10 +307,6 @@ public abstract class NodeUpdater implements FallibleCommand {
         if (options.isReactEnabled()) {
             dependencies
                     .putAll(readDependencies("react-router", "dependencies"));
-            if (options.getFeatureFlags().isEnabled(FeatureFlags.REACT19)) {
-                dependencies
-                        .putAll(readDependencies("react19", "dependencies"));
-            }
         } else {
             dependencies
                     .putAll(readDependencies("vaadin-router", "dependencies"));
@@ -377,9 +373,6 @@ public abstract class NodeUpdater implements FallibleCommand {
         if (options.isReactEnabled()) {
             defaults.putAll(
                     readDependencies("react-router", "devDependencies"));
-            if (options.getFeatureFlags().isEnabled(FeatureFlags.REACT19)) {
-                defaults.putAll(readDependencies("react19", "devDependencies"));
-            }
         }
 
         return defaults;
