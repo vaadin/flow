a04b0bc35d23bbacec0b5fb5386dfe7750369c23
abdullahtellioglu <abdullah@vaadin.com>
2025-09-01 14:56:42 +0300

    Formatter

--- flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/ReactComponentPropertiesIT.java
+++ flow-tests/test-frontend/vite-basics/src/test/java/com/vaadin/viteapp/ReactComponentPropertiesIT.java
@@ -26,39 +26,54 @@ public class ReactComponentPropertiesIT extends ChromeBrowserTest {
 
     @Test
     public void inlinePropsCanFound() {
-        var componentWithInlinePropsElement = $("*").withAttribute(DATA_TEST_ID, "component-with-inline-props").waitForFirst();
-        Map<String, Object> debugPropertiesFromFiberNode = getDebugPropertiesFromFiberNode(componentWithInlinePropsElement);
+        var componentWithInlinePropsElement = $("*")
+                .withAttribute(DATA_TEST_ID, "component-with-inline-props")
+                .waitForFirst();
+        Map<String, Object> debugPropertiesFromFiberNode = getDebugPropertiesFromFiberNode(
+                componentWithInlinePropsElement);
         var error = (Boolean) debugPropertiesFromFiberNode.get("error");
         Assert.assertFalse(error);
         debugPropertiesFromFiberNode.get("properties");
-        List<?> properties = (List<?>) debugPropertiesFromFiberNode.get("properties");
+        List<?> properties = (List<?>) debugPropertiesFromFiberNode
+                .get("properties");
         Assert.assertFalse(properties.isEmpty());
     }
 
     @Test
     public void intrinsicPropsCanFoundInWindow() {
-        var h1 = $("*").withAttribute(DATA_TEST_ID, "simple-header").waitForFirst();
-        Map<String, Object> debugPropertiesFromFiberNode = getDebugPropertiesFromFiberNode(h1);
+        var h1 = $("*").withAttribute(DATA_TEST_ID, "simple-header")
+                .waitForFirst();
+        Map<String, Object> debugPropertiesFromFiberNode = getDebugPropertiesFromFiberNode(
+                h1);
         Assert.assertNull(debugPropertiesFromFiberNode);
-        Map<String, Object> debugPropertiesFromWindow = getDebugPropertiesFromWindow(h1);
+        Map<String, Object> debugPropertiesFromWindow = getDebugPropertiesFromWindow(
+                h1);
         Assert.assertNotNull(debugPropertiesFromWindow);
         Assert.assertFalse((Boolean) debugPropertiesFromWindow.get("error"));
-        Assert.assertFalse(((List<?>) debugPropertiesFromWindow.get("properties")).isEmpty());
+        Assert.assertFalse(
+                ((List<?>) debugPropertiesFromWindow.get("properties"))
+                        .isEmpty());
     }
 
     @Test
     public void interfacePropsCanFound() {
-        var compWithInterfaceProps = $("*").withAttribute(DATA_TEST_ID, "component-with-interface-props").waitForFirst();
-        Map<String, Object> debugPropertiesFromFiberNode = getDebugPropertiesFromFiberNode(compWithInterfaceProps);
+        var compWithInterfaceProps = $("*")
+                .withAttribute(DATA_TEST_ID, "component-with-interface-props")
+                .waitForFirst();
+        Map<String, Object> debugPropertiesFromFiberNode = getDebugPropertiesFromFiberNode(
+                compWithInterfaceProps);
         Assert.assertNotNull(debugPropertiesFromFiberNode);
         assertError(debugPropertiesFromFiberNode, false);
     }
 
     @Test
     public void componentWithoutPropsShouldHaveAnError() {
-        var element = $("*").withAttribute(DATA_TEST_ID, "component-with-any-props").waitForFirst();
+        var element = $("*")
+                .withAttribute(DATA_TEST_ID, "component-with-any-props")
+                .waitForFirst();
         Assert.assertNull(getDebugPropertiesFromFiberNode(element));
-        Map<String, Object> debugPropertiesFromWindow = getDebugPropertiesFromWindow(element);
+        Map<String, Object> debugPropertiesFromWindow = getDebugPropertiesFromWindow(
+                element);
         assertError(debugPropertiesFromWindow, true);
     }
 
@@ -69,26 +84,29 @@ public class ReactComponentPropertiesIT extends ChromeBrowserTest {
         assertError(spanProps, true);
 
         var anyDiv = $("div").waitForFirst();
-        Map<String, Object> divProperties = getDebugPropertiesFromWindow(anyDiv);
+        Map<String, Object> divProperties = getDebugPropertiesFromWindow(
+                anyDiv);
         assertError(divProperties, false);
 
     }
-
 
     private void assertError(Map<String, Object> propObj, boolean expected) {
         var error = (Boolean) propObj.get("error");
         Assert.assertEquals(expected, error);
     }
 
-
-    private Map<String, Object> getDebugPropertiesFromWindow(WebElement element) {
+    private Map<String, Object> getDebugPropertiesFromWindow(
+            WebElement element) {
         String tagName = element.getTagName();
-        return (Map<String, Object>) executeScript("""
-                return window.Vaadin.copilot.ReactProperties.properties[arguments[0]];
-                """, tagName);
+        return (Map<String, Object>) executeScript(
+                """
+                        return window.Vaadin.copilot.ReactProperties.properties[arguments[0]];
+                        """,
+                tagName);
     }
 
-    private Map<String, Object> getDebugPropertiesFromFiberNode(WebElement element) {
+    private Map<String, Object> getDebugPropertiesFromFiberNode(
+            WebElement element) {
         return (Map<String, Object>) executeScript(
                 """
                         const key = Object.keys(arguments[0]).filter(a => a.startsWith("__reactFiber"))[0];
