d578f6860c8788c34c2e27d2ebb22166a0505a6d
Sergey Vinogradov <mr.vursen@gmail.com>
2025-09-02 10:19:01 +0400

    fix rebase errors

--- flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorFlatHierarchyTest.java
+++ flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorFlatHierarchyTest.java
@@ -1,10 +1,10 @@
 package com.vaadin.flow.data.provider.hierarchy;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Before;
