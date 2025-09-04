5994ddec8cfe195ac2dace5d0763a7734931db96
Tomi Virtanen <tltv@vaadin.com>
2025-09-04 12:26:28 +0300

    chore!: remove VaadinWebSecurity
    Fixes: #22129

--- flow-tests/vaadin-spring-tests/test-spring-security-flow-routepathaccesschecker/src/main/java/com/vaadin/flow/spring/flowsecurity/SecurityConfig.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow-routepathaccesschecker/src/main/java/com/vaadin/flow/spring/flowsecurity/SecurityConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.vaadin.flow.spring.flowsecurity;
 
 import com.vaadin.flow.component.UI;
@@ -28,6 +44,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import java.util.stream.Collectors;
 
 import static com.vaadin.flow.spring.flowsecurity.service.UserInfoService.ROLE_ADMIN;
+import static com.vaadin.flow.spring.security.VaadinSecurityConfigurer.vaadin;
 
 @EnableWebSecurity
 @Configuration
@@ -84,16 +101,23 @@ public class SecurityConfig {
             throws Exception {
         http.authorizeHttpRequests(cfg -> cfg
                 .requestMatchers("/admin-only/**", "/admin")
-                .hasAnyRole(ROLE_ADMIN).requestMatchers("/private")
+                .hasAnyRole(ROLE_ADMIN)
+                .requestMatchers("/private")
                 .authenticated()
                 .requestMatchers("/", "/public/**", "/another", "/menu-list")
-                .permitAll().requestMatchers("/error").permitAll()
+                .permitAll()
+                .requestMatchers("/error")
+                .permitAll()
                 // routes aliases
-                .requestMatchers("/alias-for-admin").hasAnyRole(ROLE_ADMIN)
-                .requestMatchers("/home", "/hey/**").permitAll()
-                .requestMatchers("/all-logged-in/**").authenticated());
+                .requestMatchers("/alias-for-admin")
+                .hasAnyRole(ROLE_ADMIN)
+                .requestMatchers("/home", "/hey/**")
+                .permitAll()
+                .requestMatchers("/all-logged-in/**")
+                .authenticated()
+        );
         // @formatter:on
-        http.with(VaadinSecurityConfigurer.vaadin(), vaadin -> {
+        http.with(vaadin(), vaadin -> {
             if (getLogoutSuccessUrl().equals("/")) {
                 // Test the default url with empty context path
                 vaadin.loginView(LoginView.class);
