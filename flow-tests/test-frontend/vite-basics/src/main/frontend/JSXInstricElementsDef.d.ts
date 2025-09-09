9acfbcafdfc9d5676644c777b4c235201dec6db1
abdullahtellioglu <abdullah@vaadin.com>
2025-09-09 20:27:41 +0300

    Broken as JSX cannot be resolved

--- flow-tests/test-frontend/vite-basics/src/main/frontend/JSXInstricElementsDef.d.ts
+++
@@ -1,9 +0,0 @@
-// Sample of IntrinsicElements to mimic the react-types loaded
-// via node modules. Used by ReactPropertiesPlugin
-
-declare namespace JSX {
-    interface IntrinsicElements {
-        h1: { text: string };
-        div: { [key: string]: any, "aria-label": string };
-    }
-}
