c92f6d1a42a64dae2c906ffbfac6d715c087e970
Leif Åstrand <leif@vaadin.com>
2025-09-15 12:50:08 +0300

    fix: Move map from AbstractSignal to Signal (#22250)
    Fixes #22249

--- signals/src/main/java/com/vaadin/signals/AbstractSignal.java
+++ signals/src/main/java/com/vaadin/signals/AbstractSignal.java
@@ -532,25 +532,4 @@ public abstract class AbstractSignal<T> implements Signal<T> {
 
         return fromJson(((Data) node).value(), valueType);
     }
-
-    /*
-     * These are a bunch of public access points to API that is originally
-     * implemented in classes in the .impl package. These methods are unit
-     * tested from the same class that tests the implementation class.
-     */
-
-    /**
-     * Creates a computed signal based on a mapper function that is passed the
-     * value of this signal. If the mapper function accesses other signal
-     * values, then the computed signal will also depend on those signals.
-     *
-     * @param <C>
-     *            the computed signal type
-     * @param mapper
-     *            the mapper function to use, not <code>null</code>
-     * @return the computed signal, not <code>null</code>
-     */
-    public <C> Signal<C> map(Function<T, C> mapper) {
-        return Signal.computed(() -> mapper.apply(value()));
-    }
 }
