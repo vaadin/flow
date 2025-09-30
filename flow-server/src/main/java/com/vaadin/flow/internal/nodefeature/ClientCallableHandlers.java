08002c4efc7be19a775562224571f909c6b0c6b3
Artur Signell <artur@vaadin.com>
2025-09-30 16:37:00 +0300

    format

--- flow-server/src/main/java/com/vaadin/flow/internal/nodefeature/ClientCallableHandlers.java
+++ flow-server/src/main/java/com/vaadin/flow/internal/nodefeature/ClientCallableHandlers.java
@@ -57,7 +57,6 @@ public class ClientCallableHandlers extends AbstractServerHandlers<Component> {
         // All parameter types are supported through JSON deserialization
     }
 
-
     @Override
     protected DisabledUpdateMode getUpdateMode(Method method) {
         return method.getAnnotation(ClientCallable.class).value();
