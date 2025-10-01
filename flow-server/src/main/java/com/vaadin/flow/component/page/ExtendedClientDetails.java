726700f39aa725ea2f448d2c1e74bce95e76a8f1
Artur <artur@vaadin.com>
2025-10-01 10:09:21 +0300

    feat: Add bean serialization support (#22378)
    Implements bean-only serialization using Jackson.
    Beans are serialized on server and handled as standard JS objects on client.

--- flow-server/src/main/java/com/vaadin/flow/component/page/ExtendedClientDetails.java
+++ flow-server/src/main/java/com/vaadin/flow/component/page/ExtendedClientDetails.java
@@ -27,8 +27,7 @@ import com.vaadin.flow.server.VaadinSession;
  * <p>
  * Please note that all information is fetched only once, and <em>not updated
  * automatically</em>. To retrieve updated values, you can execute JS with
- * {@link Page#executeJs(String, Serializable...)} and get the current value
- * back.
+ * {@link Page#executeJs(String, Object...)} and get the current value back.
  *
  * @author Vaadin Ltd
  * @since 2.0
