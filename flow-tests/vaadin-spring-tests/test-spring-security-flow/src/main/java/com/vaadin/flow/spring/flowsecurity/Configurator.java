8b7cddbbcc163523d7b413b45f8de7f5388bbe07
mikhail <mikhail@vaadin.com>
2025-10-07 14:51:09 +0300

    remove stylesheet from main module

--- flow-tests/vaadin-spring-tests/test-spring-security-flow/src/main/java/com/vaadin/flow/spring/flowsecurity/Configurator.java
+++ flow-tests/vaadin-spring-tests/test-spring-security-flow/src/main/java/com/vaadin/flow/spring/flowsecurity/Configurator.java
@@ -1,6 +1,5 @@
 package com.vaadin.flow.spring.flowsecurity;
 
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
@@ -9,7 +8,6 @@ import com.vaadin.flow.theme.Theme;
 @PWA(name = "Spring Security Helper Test Project", shortName = "SSH Test")
 @Theme("spring-security-test-app")
 @Push
-@StyleSheet("@vaadin/aura/fake-aura.css")
 public class Configurator implements AppShellConfigurator {
 
 }
