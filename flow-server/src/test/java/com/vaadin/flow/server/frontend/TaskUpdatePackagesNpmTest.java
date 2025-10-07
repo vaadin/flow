5330547281cd8014e2b81edee2a9d4df24294901
Artur <artur@vaadin.com>
2025-10-07 17:11:28 +0300

    chore: Check file license headers using Spotless (#22449)

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/TaskUpdatePackagesNpmTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/TaskUpdatePackagesNpmTest.java
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package com.vaadin.flow.server.frontend;
 
 import static com.vaadin.flow.server.Constants.PACKAGE_JSON;
--- flow-tests/vaadin-spring-tests/test-spring-boot-multimodule-reload-time/generator/src/main/java/com/vaadin/flow/spring/test/JavaSpringServiceContext.java
+++ flow-tests/vaadin-spring-tests/test-spring-boot-multimodule-reload-time/generator/src/main/java/com/vaadin/flow/spring/test/JavaSpringServiceContext.java
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package com.vaadin.flow.spring.test;
 
 public class JavaSpringServiceContext extends JavaClassContext {
