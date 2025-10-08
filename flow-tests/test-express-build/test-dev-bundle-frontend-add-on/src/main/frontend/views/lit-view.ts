8e0d9426402d936cff87b95a090c6f5c44685a26
Artur Signell <artur@vaadin.com>
2025-10-08 09:13:21 +0300

    Do not format files which are checksummed in tests

--- flow-tests/test-express-build/test-dev-bundle-frontend-add-on/src/main/frontend/views/lit-view.ts
+++ flow-tests/test-express-build/test-dev-bundle-frontend-add-on/src/main/frontend/views/lit-view.ts
@@ -1,13 +1,16 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { Greetings } from 'Frontend/views/another.js';
+import { Greetings } from 'Frontend/views/another.js'
 
 @customElement('lit-view')
 export class LitView extends LitElement {
-  render() {
-    const greetings = new Greetings();
-    const hello = greetings.sayHello('John Doe');
 
-    return html` <div><p>Greetings from test web component: ${hello}</p></div> `;
+  render() {
+      const greetings = new Greetings();
+      const hello = greetings.sayHello("John Doe");
+
+      return html`
+          <div><p>Greetings from test web component: ${hello}</p></div>
+      `;
   }
 }
