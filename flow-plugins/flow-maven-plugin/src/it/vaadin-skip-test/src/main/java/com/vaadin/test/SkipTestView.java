87c2e655789f63b3f8fa7b58dac1f7c1c47a9d14
Jorg Heymans <jorg.heymans@gmail.com>
2025-10-15 14:58:54 +0200

    fix test compilation error

--- flow-plugins/flow-maven-plugin/src/it/vaadin-skip-test/src/main/java/com/vaadin/test/SkipTestView.java
+++ flow-plugins/flow-maven-plugin/src/it/vaadin-skip-test/src/main/java/com/vaadin/test/SkipTestView.java
@@ -1,11 +1,9 @@
 package com.vaadin.test;
 
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.router.Route;
 
-@Route("")
-public class SkipTestView extends Div {
-    public SkipTestView() {
-        setText("Skip test view");
-    }
+@Route
+public class SkipTestView extends Component { 
+
 }
