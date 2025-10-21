e216fcb9d9da7528e6769fe8d880169330c9d6fb
Marco Collovati <marco@vaadin.com>
2025-10-21 08:55:05 +0200

    fix: fix glob pattern in runtime hints registration (#22531)
    Fixes the pattern used to register runtime hints for native build
    in order to match resources in subfolders.
    Also adds some basic test to ensure main resources are accepted
    
    Fixes #22526

--- vaadin-spring/src/main/java/com/vaadin/flow/spring/springnative/VaadinHintsRegistrar.java
+++ vaadin-spring/src/main/java/com/vaadin/flow/spring/springnative/VaadinHintsRegistrar.java
@@ -34,7 +34,7 @@ public class VaadinHintsRegistrar implements RuntimeHintsRegistrar {
                 .forEach(cls -> ref.registerType(cls, MemberCategory.values()));
 
         // Bundles, build info etc
-        hints.resources().registerPattern("META-INF/VAADIN/*");
+        hints.resources().registerPattern("META-INF/VAADIN/**");
         hints.resources().registerPattern("vaadin-i18n/*");
         hints.resources().registerPattern("vaadin-featureflags.properties");
 
@@ -55,8 +55,8 @@ public class VaadinHintsRegistrar implements RuntimeHintsRegistrar {
 
         // Flow server resources like BootstrapHandler.js and
         // RouteNotFoundError_prod.html
-        hints.resources().registerPattern("com/vaadin/flow/server/*");
-        hints.resources().registerPattern("com/vaadin/flow/router/*");
+        hints.resources().registerPattern("com/vaadin/flow/server/**");
+        hints.resources().registerPattern("com/vaadin/flow/router/**");
     }
 
     private void registerResourceIfPresent(RuntimeHints hints, String path) {
