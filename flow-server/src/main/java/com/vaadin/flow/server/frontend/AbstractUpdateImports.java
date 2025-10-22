beff2378e8d64ceeaf0e51bff4daf9e22ad3a14f
Anton Platonov <platosha@gmail.com>
2025-10-22 16:07:39 +0300

    chore: formatting

--- flow-server/src/main/java/com/vaadin/flow/server/frontend/AbstractUpdateImports.java
+++ flow-server/src/main/java/com/vaadin/flow/server/frontend/AbstractUpdateImports.java
@@ -261,8 +261,10 @@ abstract class AbstractUpdateImports implements Runnable {
     private List<String> mergeWebComponentOutputLines(
             Map<File, List<String>> outputFiles) {
         var lines = new ArrayList<String>();
-        lines.addAll(outputFiles.getOrDefault(appShellImports, Collections.emptyList()));
-        lines.addAll(outputFiles.getOrDefault(generatedFlowImports, Collections.emptyList()));
+        lines.addAll(outputFiles.getOrDefault(appShellImports,
+                Collections.emptyList()));
+        lines.addAll(outputFiles.getOrDefault(generatedFlowImports,
+                Collections.emptyList()));
         return lines;
     }
 
