bf22ccf03402b1d79666eda25396b74b33f89975
Tomi Virtanen <tltv@vaadin.com>
2025-11-06 15:51:15 +0200

    feat: add SignalPropertySupport helper
    Provides a SignalPropertySupport helper class that encapsulates the state management needed for making getXyz, setXyz, and bindXyz behave in the same way as Signal for Element properties.
    
    Fixes: #22672

---
+++ flow-server/src/main/java/com/vaadin/flow/component/SignalPropertySupport.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.vaadin.flow.function.SerializableConsumer;
+import com.vaadin.flow.shared.Registration;
+import com.vaadin.signals.BindingActiveException;
+import com.vaadin.signals.Signal;
+
+/**
+ * Helper class for binding a {@link Signal} to a property of a
+ * {@link Component}. Not all component features delegate directly to the state
+ * in {@link com.vaadin.flow.dom.Element}. For those features, this helper class
+ * ensures state management behaves consistently with Element properties.
+ * <p>
+ * Example of usage:
+ *
+ * <pre>
+ * ValueSignal&lt;String&gt; signal = new ValueSignal&lt;&gt;("");
+ * MyComponent component = new MyComponent();
+ * add(component);
+ * component.bindTextContent(signal);
+ * signal.set("Hello"); // component content showing now "Content: Hello" text
+ * </pre>
+ * 
+ * <pre>
+ * &#064;Tag(div)
+ * public class MyComponent extends Component {
+ *     private final SignalPropertySupport&lt;String&gt; textProperty = SignalPropertySupport
+ *             .create(this, value -> {
+ *                 getElement().executeJs("this.textContent = 'Content: ' + $0",
+ *                         value);
+ *             });
+ *
+ *     public String getText() {
+ *         return textProperty.get();
+ *     }
+ *
+ *     public void setText(String text) {
+ *         textProperty.set(text);
+ *     }
+ *
+ *     public void bindTextContent(Signal&lt;String&gt; textSignal) {
+ *         textProperty.bind(textSignal);
+ *     }
+ * }
+ * </pre>
+ *
+ * @param <T>
+ *            the type of the property
+ */
+public class SignalPropertySupport<T> implements Serializable {
+
+    private final SerializableConsumer<T> valueChangeConsumer;
+
+    private final Component owner;
+
+    private Registration registration;
+
+    private Signal<T> signal;
+
+    private T value;
+
+    private SignalPropertySupport(Component owner,
+            SerializableConsumer<T> valueChangeConsumer) {
+        this.owner = Objects.requireNonNull(owner,
+                "Owner component cannot be null");
+        this.valueChangeConsumer = Objects.requireNonNull(valueChangeConsumer,
+                "Value change consumer cannot be null");
+    }
+
+    /**
+     * Creates a new instance of SignalPropertySupport for the given owner
+     * component and a value change consumer to be called when property value
+     * changes. The property value changes when set manually or when bound to a
+     * {@link Signal} with {@link #bind(Signal)}, and the signal value changes
+     * when the owner component is in the attached state.
+     *
+     * @param owner
+     *            the owner component for which the value change consumer is
+     *            applied, must not be null
+     * @param valueChangeConsumer
+     *            the consumer to be called when the value changes, must not be
+     *            null
+     * @param <T>
+     *            the type of the property
+     * @return a new instance of SignalPropertySupport
+     * @see #bind(Signal)
+     */
+    public static <T> SignalPropertySupport<T> create(Component owner,
+            SerializableConsumer<T> valueChangeConsumer) {
+        return new SignalPropertySupport<>(owner, valueChangeConsumer);
+    }
+
+    /**
+     * Binds a {@link Signal}'s value to this property support and keeps the
+     * value synchronized with the signal value while the component is in
+     * attached state. When the component is in detached state, signal value
+     * changes have no effect. <code>null</code> signal unbinds existing
+     * binding.
+     * <p>
+     * While a Signal is bound to a property support, any attempt to set value
+     * manually throws {@link com.vaadin.signals.BindingActiveException}. Same
+     * happens when trying to bind a new Signal while one is already bound.
+     *
+     * @param signal
+     *            the signal to bind or <code>null</code> to unbind any existing
+     *            binding
+     * @throws com.vaadin.signals.BindingActiveException
+     *             thrown when there is already an existing binding
+     */
+    public void bind(Signal<T> signal) {
+        if (signal != null && this.signal != null) {
+            throw new BindingActiveException();
+        }
+        this.signal = signal;
+        if (signal == null && registration != null) {
+            registration.remove();
+            registration = null;
+        }
+        if (signal != null) {
+            registration = ComponentEffect.effect(owner, () -> {
+                value = signal.value();
+                valueChangeConsumer.accept(value);
+            });
+        }
+    }
+
+    /**
+     * Gets the current value of this property support.
+     *
+     * @return the current value
+     */
+    public T get() {
+        return value;
+    }
+
+    /**
+     * Sets the value of this property support.
+     *
+     * @param value
+     *            the value to set
+     * @throws com.vaadin.signals.BindingActiveException
+     *             thrown when there is an existing binding
+     */
+    public void set(T value) {
+        if (signal != null) {
+            throw new BindingActiveException();
+        }
+        this.value = value;
+        valueChangeConsumer.accept(value);
+    }
+}
---
+++ flow-server/src/test/java/com/vaadin/flow/component/SignalPropertySupportTest.java
@@ -0,0 +1,313 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component;
+
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+
+import com.vaadin.experimental.FeatureFlags;
+import com.vaadin.flow.internal.CurrentInstance;
+import com.vaadin.flow.server.ErrorEvent;
+import com.vaadin.flow.server.MockVaadinServletService;
+import com.vaadin.flow.server.MockVaadinSession;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.signals.BindingActiveException;
+import com.vaadin.signals.Signal;
+import com.vaadin.signals.ValueSignal;
+import com.vaadin.tests.util.MockUI;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+public class SignalPropertySupportTest {
+
+    private static MockVaadinServletService service;
+
+    private MockedStatic<FeatureFlags> featureFlagStaticMock;
+
+    private LinkedList<ErrorEvent> events;
+
+    private AtomicInteger callCount;
+    private AtomicReference<Object> lastValue;
+
+    @BeforeClass
+    public static void init() {
+        var featureFlagStaticMock = mockStatic(FeatureFlags.class);
+        featureFlagEnabled(featureFlagStaticMock);
+        service = new MockVaadinServletService();
+        close(featureFlagStaticMock);
+    }
+
+    @AfterClass
+    public static void clean() {
+        CurrentInstance.clearAll();
+        service.destroy();
+    }
+
+    @Before
+    public void before() {
+        featureFlagStaticMock = mockStatic(FeatureFlags.class);
+        featureFlagEnabled(featureFlagStaticMock);
+        events = mockLockedSessionWithErrorHandler();
+        callCount = new AtomicInteger(0);
+        lastValue = new AtomicReference<>();
+    }
+
+    @After
+    public void after() {
+        close(featureFlagStaticMock);
+        events = null;
+    }
+
+    private static void featureFlagEnabled(
+            MockedStatic<FeatureFlags> featureFlagStaticMock) {
+        FeatureFlags flags = mock(FeatureFlags.class);
+        when(flags.isEnabled(FeatureFlags.FLOW_FULLSTACK_SIGNALS.getId()))
+                .thenReturn(true);
+        featureFlagStaticMock.when(() -> FeatureFlags.get(any()))
+                .thenReturn(flags);
+    }
+
+    private static void close(
+            MockedStatic<FeatureFlags> featureFlagStaticMock) {
+        CurrentInstance.clearAll();
+        featureFlagStaticMock.close();
+    }
+
+    private LinkedList<ErrorEvent> mockLockedSessionWithErrorHandler() {
+        VaadinService.setCurrent(service);
+
+        var session = new MockVaadinSession(service);
+        session.lock();
+
+        var ui = new MockUI(session);
+        var events = new LinkedList<ErrorEvent>();
+        session.setErrorHandler(events::add);
+
+        return events;
+    }
+
+    @Test
+    public void create_nullArguments_throwException() {
+        assertThrows(NullPointerException.class,
+                () -> SignalPropertySupport.create(null, value -> {
+                }));
+        assertThrows(NullPointerException.class,
+                () -> SignalPropertySupport.create(new TestComponent(), null));
+    }
+
+    @Test
+    public void get_notBound_nullValue() {
+        var component = new TestComponent();
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                });
+        assertNull(signalPropertySupport.get());
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void get_boundButNotAttached_valueNotSetInitially() {
+        var component = new TestComponent();
+
+        ValueSignal<String> signal = new ValueSignal<>("foo");
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                    callCount.incrementAndGet();
+                });
+        signalPropertySupport.bind(signal);
+
+        assertNull(signalPropertySupport.get());
+        assertEquals(0, callCount.get());
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void get_boundAndAttached_valueSet() {
+        var component = new TestComponent();
+        UI.getCurrent().add(component);
+
+        ValueSignal<String> signal = new ValueSignal<>("foo");
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                    callCount.incrementAndGet();
+                    lastValue.set(value);
+                });
+        signalPropertySupport.bind(signal);
+
+        assertEquals("foo", signalPropertySupport.get());
+        assertEquals("foo", lastValue.get());
+
+        signal.value("bar");
+
+        assertEquals("bar", signalPropertySupport.get());
+        assertEquals("bar", lastValue.get());
+        assertEquals(2, callCount.get());
+        Assert.assertTrue(events.isEmpty());
+
+    }
+
+    @Test
+    public void set_notBound_valueSet() {
+        var component = new TestComponent();
+
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                    callCount.incrementAndGet();
+                    lastValue.set(value);
+                });
+        signalPropertySupport.set("foo");
+
+        assertEquals("foo", signalPropertySupport.get());
+        assertEquals("foo", lastValue.get());
+
+        signalPropertySupport.set("bar");
+
+        assertEquals("bar", signalPropertySupport.get());
+        assertEquals("bar", lastValue.get());
+        assertEquals(2, callCount.get());
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void set_alreadyBound_throwException() {
+        var component = new TestComponent();
+        ValueSignal<String> signal = new ValueSignal<>("foo");
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                    callCount.incrementAndGet();
+                });
+        signalPropertySupport.bind(signal);
+
+        assertThrows(BindingActiveException.class,
+                () -> signalPropertySupport.set("bar"));
+        assertEquals(0, callCount.get());
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void set_computedSignal_valueSet() {
+        var component = new TestComponent();
+        UI.getCurrent().add(component);
+
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, lastValue::set);
+        ValueSignal<String> signal = new ValueSignal<>("foo");
+        signalPropertySupport
+                .bind(Signal.computed(() -> "computed-" + signal.value()));
+        assertEquals("computed-foo", signalPropertySupport.get());
+        assertEquals("computed-foo", lastValue.get());
+
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void set_mappedSignal_valueSet() {
+        var component = new TestComponent();
+        UI.getCurrent().add(component);
+
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, lastValue::set);
+        ValueSignal<String> signal = new ValueSignal<>("foo");
+        signalPropertySupport.bind(signal.map(value -> "mapped-" + value));
+        assertEquals("mapped-foo", signalPropertySupport.get());
+        assertEquals("mapped-foo", lastValue.get());
+
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void bind_nullWithNotYetBound_noEffect() {
+        var component = new TestComponent();
+        UI.getCurrent().add(component);
+
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                });
+        signalPropertySupport.bind(null);
+
+        assertNull(signalPropertySupport.get());
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void bind_nullWithAlreadyBound_removeBinding() {
+        var component = new TestComponent();
+        UI.getCurrent().add(component);
+
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                });
+        ValueSignal<String> signal = new ValueSignal<>("foo");
+        signalPropertySupport.bind(new ValueSignal<>("foo"));
+        assertEquals("foo", signalPropertySupport.get());
+        signalPropertySupport.bind(null);
+        signal.value("bar");
+        assertEquals("foo", signalPropertySupport.get());
+    }
+
+    @Test
+    public void bind_alreadyBound_throw() {
+        var component = new TestComponent();
+        UI.getCurrent().add(component);
+
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                });
+        signalPropertySupport.bind(new ValueSignal<>("foo"));
+
+        assertThrows(BindingActiveException.class,
+                () -> signalPropertySupport.bind(new ValueSignal<>("foo")));
+        Assert.assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void bind_boundDetachedAttached_bindingRemovedAndAddedBack() {
+        var component = new TestComponent();
+        UI.getCurrent().add(component);
+
+        SignalPropertySupport<String> signalPropertySupport = SignalPropertySupport
+                .create(component, value -> {
+                });
+        ValueSignal<String> signal = new ValueSignal<>("foo");
+        signalPropertySupport.bind(signal);
+        assertEquals("foo", signalPropertySupport.get());
+        component.removeFromParent();
+        signal.value("bar");
+        assertEquals("foo", signalPropertySupport.get());
+        UI.getCurrent().add(component);
+        assertEquals("bar", signalPropertySupport.get());
+    }
+
+    @Tag("div")
+    private static class TestComponent extends Component {
+
+    }
+}
