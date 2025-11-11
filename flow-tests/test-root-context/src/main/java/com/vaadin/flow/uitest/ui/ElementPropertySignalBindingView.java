7b684045348ab39966161a64d28ead48c0168042
Teppo Kurki <teppo.kurki@vaadin.com>
2025-11-11 14:34:56 +0200

    add IT

---
+++ flow-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/ElementPropertySignalBindingView.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.uitest.servlet.ViewTestLayout;
+import com.vaadin.signals.Signal;
+import com.vaadin.signals.ValueSignal;
+
+@Route(value = "com.vaadin.flow.uitest.ui.ElementPropertySignalBindingView", layout = ViewTestLayout.class)
+public class ElementPropertySignalBindingView extends AbstractDivView {
+
+    @Override
+    protected void onShow() {
+        Div target = new Div();
+        target.setId("target-div");
+        add(target);
+
+        Div result = new Div();
+        result.setId("result-div");
+        add(result);
+
+        Signal<String> signal = new ValueSignal<>("foo");
+        target.getElement().bindProperty("testproperty", signal);
+
+        target.getElement().addPropertyChangeListener("testproperty", "change",
+                event -> {
+                    String newValue = (String) event.getValue();
+                    result.setText("testproperty changed to: " + newValue);
+                });
+    }
+
+}
---
+++ flow-tests/test-root-context/src/test/java/com/vaadin/flow/uitest/ui/ElementPropertySignalBindingIT.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2025 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.uitest.ui;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
+public class ElementPropertySignalBindingIT extends ChromeBrowserTest {
+
+    @Before
+    public void setUp() {
+        open();
+    }
+
+    @Test
+    public void checkInitialPropertyValue_modifyPropertyValue_checkModifiedValue() {
+        WebElement resultElement = findElement(By.id("result-div"));
+        Assert.assertEquals("testproperty changed to: foo",
+                resultElement.getText());
+
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript(
+                "document.getElementById('target-div').testproperty = 'changed-value';");
+
+        Assert.assertEquals("testproperty changed to: changed-value",
+                resultElement.getText());
+    }
+}
