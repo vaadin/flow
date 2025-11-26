0c96edc25dc52c91430cef9b529bc32158c1527d
Tomi Virtanen <tltv@vaadin.com>
2025-11-26 14:02:05 +0200

    chore: improve warning about missing vaadin-dev

--- flow-server/src/main/java/com/vaadin/flow/server/VaadinServletService.java
+++ flow-server/src/main/java/com/vaadin/flow/server/VaadinServletService.java
@@ -103,8 +103,23 @@ public class VaadinServletService extends VaadinService {
                         }, () -> handlers.add(devModeHandler));
             } else if (mode == Mode.DEVELOPMENT_FRONTEND_LIVERELOAD) {
                 getLogger().warn(
-                        "DevModeHandlerManager not found, but dev server is enabled. "
-                                + "Add 'com.vaadin.vaadin-dev-server' dependency or include it transitively via 'com.vaadin.vaadin-dev'.");
+                        """
+                        DevModeHandlerManager not found, but dev server is enabled. Add 'com.vaadin.vaadin-dev' dependency
+                        or 'com.vaadin.vaadin-dev-server' for minimal working configuration.
+                        
+                        Maven:
+                            <dependency>
+                                <groupId>com.vaadin</groupId>
+                                <artifactId>vaadin-dev</artifactId>
+                                <optional>true</optional>
+                            </dependency>
+                        
+                        Gradle:
+                            dependencies {
+                                implementation('com.vaadin:vaadin-dev')
+                            }
+                        
+                        """);
             }
         }
 
