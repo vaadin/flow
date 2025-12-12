50091263cba2a840af3afbb8353f756953bb7b01
Artur Signell <artur@vaadin.com>
2025-12-12 14:46:24 +0200

    refactor: Move ThemeUtils and CSSBundler to internal package
    They are not related the the frontend build

--- flow-server/src/main/java/com/vaadin/flow/internal/CssBundler.java
+++ flow-server/src/main/java/com/vaadin/flow/internal/CssBundler.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.flow.server.frontend;
+package com.vaadin.flow.internal;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,9 +30,6 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tools.jackson.databind.JsonNode;
-
-import com.vaadin.flow.internal.JacksonUtils;
-import com.vaadin.flow.internal.StringUtil;
 
 /**
  * Utility methods to handle application theme CSS content.
