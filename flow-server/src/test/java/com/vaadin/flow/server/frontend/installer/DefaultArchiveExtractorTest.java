178ef04077914ea091e21c3d875f867b88afb246
Artur Signell <artur@vaadin.com>
2025-12-12 13:10:59 +0200

    refactor: complete test migration to flow-build-tools
    Update DefaultArchiveExtractorTest package declaration to match its new
    location in flow-build-tools module. Add necessary test dependencies
    including flow-test-generic for CurrentInstanceCleaner support.

--- flow-server/src/test/java/com/vaadin/flow/server/frontend/installer/DefaultArchiveExtractorTest.java
+++ flow-server/src/test/java/com/vaadin/flow/server/frontend/installer/DefaultArchiveExtractorTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.flow.server.frontend.installer;
+package com.vaadin.frontendtools.installer;
 
 import java.io.File;
 import java.io.IOException;
