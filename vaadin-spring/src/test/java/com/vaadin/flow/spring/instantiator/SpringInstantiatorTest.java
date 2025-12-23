54a1f93cf97849cae3ce2735c8c59556b0e4af89
Absurdon <peter@brodkorb-web.de>
2025-12-23 20:19:28 +0100

    feat: Improve SpringInstantiator
    Do not rely on counting the beans of a type oneselves but instead let spring do its own resolving logic so ambiguity might actually not be an issue. If it still is fall back to the previous behavior.
    
    This fixes an issue where one might have multiple beans of an abstract or interface type in the context but has already taken care of ambiguity by hinting spring on which one to use (e.g. by using @Primary, @Fallback or other spring provided mechanisms). With the old implementation this would lead to an exception as it would try to instantiate a bean of an abstract type or interface, when spring would have been able to find a unique bean.
    
    This especially fixes cases where one has multiple beans of I18NProvider or MenuAccessControl which themselves are a perfect example where the old implementation was lacking.
    
    The unit tests had to be changed slightly as they use mocking and the used method on the context was changed.
    
    Fixes #18918

--- vaadin-spring/src/test/java/com/vaadin/flow/spring/instantiator/SpringInstantiatorTest.java
+++ vaadin-spring/src/test/java/com/vaadin/flow/spring/instantiator/SpringInstantiatorTest.java
@@ -32,6 +32,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.BeanInstantiationException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
@@ -277,8 +279,8 @@ public class SpringInstantiatorTest {
     public void getOrCreateBean_noBeansGivenCannotInstantiate_throwsExceptionWithoutHint() {
         ApplicationContext context = Mockito.mock(ApplicationContext.class,
                 Mockito.RETURNS_DEEP_STUBS);
-        Mockito.when(context.getBeanNamesForType(Number.class))
-                .thenReturn(new String[] {});
+        Mockito.when(context.getBean(Number.class))
+                .thenThrow(NoSuchBeanDefinitionException.class);
         Mockito.when(context.getAutowireCapableBeanFactory()
                 .createBean(Number.class))
                 .thenThrow(new BeanInstantiationException(Number.class,
@@ -299,8 +301,8 @@ public class SpringInstantiatorTest {
     public void getOrCreateBean_multipleBeansGivenCannotInstantiate_throwsExceptionWithHint() {
         ApplicationContext context = Mockito.mock(ApplicationContext.class,
                 Mockito.RETURNS_DEEP_STUBS);
-        Mockito.when(context.getBeanNamesForType(Number.class))
-                .thenReturn(new String[] { "one", "two" });
+        Mockito.when(context.getBean(Number.class))
+                .thenThrow(NoUniqueBeanDefinitionException.class);
         Mockito.when(context.getAutowireCapableBeanFactory()
                 .createBean(Number.class))
                 .thenThrow(new BeanInstantiationException(Number.class,
@@ -334,8 +336,8 @@ public class SpringInstantiatorTest {
     public void getOrCreateBean_multipleBeansGivenButCanInstantiate_noException() {
         ApplicationContext context = Mockito.mock(ApplicationContext.class,
                 Mockito.RETURNS_DEEP_STUBS);
-        Mockito.when(context.getBeanNamesForType(String.class))
-                .thenReturn(new String[] { "one", "two" });
+        Mockito.when(context.getBean(String.class))
+                .thenThrow(NoUniqueBeanDefinitionException.class);
         Mockito.when(context.getAutowireCapableBeanFactory()
                 .createBean(String.class)).thenReturn("string");
         SpringInstantiator instantiator = new SpringInstantiator(null, context);
