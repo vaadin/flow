e9b263e8f4e593f70250ed33852ef7566300c699
Tomi Virtanen <tltv@vaadin.com>
2026-01-05 10:34:32 +0200

    Update vaadin-spring/src/test/java/com/vaadin/flow/spring/i18n/PrimaryI18NProviderInstantiationTest.java

--- vaadin-spring/src/test/java/com/vaadin/flow/spring/i18n/PrimaryI18NProviderInstantiationTest.java
+++ vaadin-spring/src/test/java/com/vaadin/flow/spring/i18n/PrimaryI18NProviderInstantiationTest.java
@@ -28,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
