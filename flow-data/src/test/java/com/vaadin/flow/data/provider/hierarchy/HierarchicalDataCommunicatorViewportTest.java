vaadin/flow

commit 4baa131825e8e30d35797580b5408bad27746b36

fix: Remove client keys only after viewport update is confirmed (#22328)
Updates HierarchicalDataCommunicator to clean up no longer visible items, their client keys and generated data in confirmUpdate instead of flush. Removing client keys from keyMapper in flush is too early, since there may be new pending client requests (for example, item selection) that were made while the current request was in progress and they might still rely on those keys.

Fixes vaadin/flow-components#8040

flow-data/src/main/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicator.java
@@ -105,7 +105,7 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
 
     private FlushRequest<T> flushRequest = null;
     private Range viewportRange = Range.withLength(0, 0);
-    private int nextUpdateId = 0;
+    private int lastUpdateId = -1;
 
     // package private for testing purposes
     RootCache<T> rootCache;
@@ -314,11 +314,6 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
 
         throw new IllegalArgumentException(
                 "Only HierarchicalDataProvider and its subtypes are supported");
-    }
-
-    @Override
-    public void confirmUpdate(int updateId) {
-        // NO-OP
     }
 
     /**
@@ -674,8 +669,6 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
         var end = viewportRange.getEnd();
 
         var viewportItems = preloadFlatRangeForward(start, length);
-        var viewportItemIds = viewportItems.stream()
-                .map(getDataProvider()::getId).collect(Collectors.toSet());
 
         var flatSize = rootCache.getFlatSize();
 
@@ -699,14 +692,48 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
                 update.set(index, List.of(generateItemJson(item)));
             }
         }
-        update.commit(nextUpdateId++);
+        update.commit(++lastUpdateId);
+    }
 
-        // Clear items that are no longer visible from the cache to free memory.
-        // This also clears them from keyMapper and disposes of their generated
-        // data in dataGenerator to avoid memory leaks on both server and
-        // client side.
-        rootCache.removeDescendantItemIf((item) -> getKeyMapper().has(item)
-                && !viewportItemIds.contains(getDataProvider().getId(item)));
+    /**
+     * Removes no longer visible items from the cache, {@code keyMapper}, and
+     * {@code dataGenerator} so that any associated client-side resources (for
+     * example, DOM elements created by {@code ComponentRenderer}) can also be
+     * released.
+     */
+    @Override
+    public void confirmUpdate(int updateId) {
+        if (updateId != lastUpdateId) {
+            // Postpone cleanup until the most recent update is confirmed
+            return;
+        }
+
+        if (rootCache == null) {
+            // Nothing to clean up
+            return;
+        }
+
+        HashSet<Object> viewportItemIds = new HashSet<>();
+        for (int i = viewportRange.getStart(); i < viewportRange
+                .getEnd(); i++) {
+            var context = rootCache.getContextByFlatIndex(i);
+            if (context == null) {
+                continue;
+            }
+            var cache = context.cache();
+            var index = context.index();
+            if (!cache.hasItem(index)) {
+                continue;
+            }
+
+            var item = cache.getItem(index);
+            viewportItemIds.add(getDataProvider().getId(item));
+        }
+
+        // Remove items from the cache, keyMapper, and dataGenerator
+        rootCache.removeDescendantItemIf((item) -> {
+            return !viewportItemIds.contains(getDataProvider().getId(item));
+        });
     }
 
     private HierarchyFormat getHierarchyFormat() {

flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorDataGenerationTest.java
@@ -69,19 +69,24 @@ public class HierarchicalDataCommunicatorDataGenerationTest
     }
 
     @Test
-    @SuppressWarnings("unchecked")
-    public void changeViewportRangeBackAndForth_destroyDataCalledForNoLongerVisibleItems() {
+    public void changeViewportRangeBackAndForth_destroyDataCalledForNoLongerVisibleItemsAfterConfirmation() {
         populateTreeData(treeData, 100, 2);
         dataCommunicator.expand(new Item("Item 0"));
+
         dataCommunicator.setViewportRange(0, 4);
         fakeClientCommunication();
-
+        dataCommunicator.confirmUpdate(captureArrayUpdateId());
         Mockito.verify(dataGenerator, Mockito.never())
                 .destroyData(Mockito.any());
 
+        Mockito.clearInvocations(dataGenerator, arrayUpdater, arrayUpdate);
+
         dataCommunicator.setViewportRange(2, 4);
         fakeClientCommunication();
+        Mockito.verify(dataGenerator, Mockito.never())
+                .destroyData(Mockito.any()); // not confirmed yet
 
+        dataCommunicator.confirmUpdate(captureArrayUpdateId());
         Mockito.verify(dataGenerator) //
                 .destroyData(new Item("Item 0"));
         Mockito.verify(dataGenerator) //
@@ -91,11 +96,14 @@ public class HierarchicalDataCommunicatorDataGenerationTest
         Mockito.verify(dataGenerator, Mockito.never())
                 .destroyData(new Item("Item 1"));
 
-        Mockito.clearInvocations(dataGenerator);
+        Mockito.clearInvocations(dataGenerator, arrayUpdater, arrayUpdate);
 
         dataCommunicator.setViewportRange(0, 4);
         fakeClientCommunication();
+        Mockito.verify(dataGenerator, Mockito.never())
+                .destroyData(Mockito.any()); // not confirmed yet
 
+        dataCommunicator.confirmUpdate(captureArrayUpdateId());
         Mockito.verify(dataGenerator, Mockito.never())
                 .destroyData(new Item("Item 0"));
         Mockito.verify(dataGenerator, Mockito.never())

flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/AbstractHierarchicalDataCommunicatorTest.java
@@ -152,6 +152,12 @@ abstract public class AbstractHierarchicalDataCommunicatorTest {
         return result;
     }
 
+    protected int captureArrayUpdateId() {
+        var argumentCaptor = ArgumentCaptor.forClass(Integer.class);
+        Mockito.verify(arrayUpdate).commit(argumentCaptor.capture());
+        return argumentCaptor.getValue();
+    }
+
     protected int captureArrayUpdateSize() {
         var argumentCaptor = ArgumentCaptor.forClass(Integer.class);
         Mockito.verify(arrayUpdater).startUpdate(argumentCaptor.capture());

flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorBasicTest.java
@@ -188,8 +188,6 @@ public class HierarchicalDataCommunicatorBasicTest
         Assert.assertEquals(-1, dataCommunicator.getDepth(new Item("Item 1")));
 
         fakeClientCommunication();
-
-        // Loaded
         Assert.assertEquals(0, dataCommunicator.getDepth(new Item("Item 0")));
         Assert.assertEquals(1, dataCommunicator.getDepth(new Item("Item 0-0")));
         Assert.assertEquals(2,
@@ -198,12 +196,6 @@ public class HierarchicalDataCommunicatorBasicTest
 
         dataCommunicator.setViewportRange(4, 4);
         fakeClientCommunication();
-
-        // Out of new viewport
-        Assert.assertEquals(-1,
-                dataCommunicator.getDepth(new Item("Item 0-0")));
-
-        // Within new viewport
         Assert.assertEquals(0, dataCommunicator.getDepth(new Item("Item 5")));
     }
 

flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorKeyGenerationTest.java
@@ -89,21 +89,27 @@ public class HierarchicalDataCommunicatorKeyGenerationTest
     }
 
     @Test
-    public void changeViewportRangeBackAndForth_noLongerVisibleKeysRemoved() {
+    public void changeViewportRangeBackAndForth_noLongerVisibleKeysRemovedAfterConfirmation() {
         populateTreeData(treeData, 100, 2);
         dataCommunicator.expand(new Item("Item 0"));
+
         dataCommunicator.setViewportRange(0, 4);
         fakeClientCommunication();
-
+        dataCommunicator.confirmUpdate(captureArrayUpdateId());
         Assert.assertTrue(keyMapper.has(new Item("Item 0")));
         Assert.assertTrue(keyMapper.has(new Item("Item 0-0")));
         Assert.assertTrue(keyMapper.has(new Item("Item 0-1")));
         Assert.assertTrue(keyMapper.has(new Item("Item 1")));
         Assert.assertFalse(keyMapper.has(new Item("Item 2")));
 
-        dataCommunicator.setViewportRange(2, 4);
-        fakeClientCommunication();
+        Mockito.clearInvocations(arrayUpdater, arrayUpdate);
 
+        dataCommunicator.setViewportRange(2, 4);
+        fakeClientCommunication(); // not confirmed yet
+        Assert.assertTrue(keyMapper.has(new Item("Item 0")));
+        Assert.assertTrue(keyMapper.has(new Item("Item 0-0")));
+
+        dataCommunicator.confirmUpdate(captureArrayUpdateId()); // confirmed
         Assert.assertFalse(keyMapper.has(new Item("Item 0")));
         Assert.assertFalse(keyMapper.has(new Item("Item 0-0")));
         Assert.assertTrue(keyMapper.has(new Item("Item 0-1")));
@@ -111,9 +117,14 @@ public class HierarchicalDataCommunicatorKeyGenerationTest
         Assert.assertTrue(keyMapper.has(new Item("Item 2")));
         Assert.assertTrue(keyMapper.has(new Item("Item 3")));
 
-        dataCommunicator.setViewportRange(0, 4);
-        fakeClientCommunication();
+        Mockito.clearInvocations(arrayUpdater, arrayUpdate);
 
+        dataCommunicator.setViewportRange(0, 4);
+        fakeClientCommunication(); // not confirmed yet
+        Assert.assertTrue(keyMapper.has(new Item("Item 2")));
+        Assert.assertTrue(keyMapper.has(new Item("Item 3")));
+
+        dataCommunicator.confirmUpdate(captureArrayUpdateId()); // confirmed
         Assert.assertTrue(keyMapper.has(new Item("Item 0")));
         Assert.assertTrue(keyMapper.has(new Item("Item 0-0")));
         Assert.assertTrue(keyMapper.has(new Item("Item 0-1")));

commit 13427a00553963acda13fdc2e996e1e1ae715258

fix: Avoid removing out-of-viewport items from cache (#23141)
Fixes a regression that was introduced in #22328. Removing items from the cache turned out to cause issues with the collapse(T item) method, which became unable to find such "unloaded" items and clean up their sub-caches if they were expanded.

Simplifies the implementation and restores the behavior to what it was in Vaadin 24, where items were removed from the key mapper but remained in the hierarchy mapper.

Note, as part of the fix, a new objects() method was added to KeyMapper to allow HierarchicalDataCommunicator to get all items that have keys and clean up those that are no longer in the viewport. Since KeyMapper always contains fewer items than the Cache, this API helps reduce the number of iterations needed during cleanup.

Fixes: vaadin/flow-components#8427

flow-data/src/main/java/com/vaadin/flow/data/provider/KeyMapper.java
@@ -15,6 +15,8 @@
  */
 package com.vaadin.flow.data.provider;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -159,6 +161,13 @@ public class KeyMapper<V> implements DataKeyMapper<V> {
         if (key != null) {
             keyObjectMap.put(key, dataObject);
         }
+    }
+
+    /**
+     * Gets all mapped objects.
+     */
+    public Collection<V> objects() {
+        return Collections.unmodifiableCollection(keyObjectMap.values());
     }
 
     @Override

flow-data/src/main/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicator.java
@@ -98,6 +98,7 @@ import com.vaadin.flow.internal.StateNode;
 public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
     private final Set<Object> expandedItemIds = new HashSet<>();
     private final StateNode stateNode;
+    private final KeyMapper<T> keyMapper;
     private final ArrayUpdater arrayUpdater;
     private final DataGenerator<T> dataGenerator;
     private final SerializableSupplier<ValueProvider<T, String>> uniqueKeyProviderSupplier;
@@ -136,9 +137,8 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
         this.arrayUpdater = arrayUpdater;
         this.dataGenerator = dataGenerator;
         this.uniqueKeyProviderSupplier = uniqueKeyProviderSupplier;
-
-        KeyMapperWrapper<T> keyMapperWrapper = new KeyMapperWrapper<>();
-        setKeyMapper(keyMapperWrapper);
+        this.keyMapper = new KeyMapperWrapper<>();
+        setKeyMapper(keyMapper);
 
         setDataProvider(new TreeDataProvider<>(new TreeData<>()), null);
 
@@ -174,7 +174,7 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
     public void reset() {
         if (rootCache != null) {
             rootCache = null;
-            getKeyMapper().removeAll();
+            keyMapper.removeAll();
             dataGenerator.destroyAllData();
         }
 
@@ -230,7 +230,7 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
                             """);
         }
 
-        getKeyMapper().refresh(item);
+        keyMapper.refresh(item);
         dataGenerator.refreshData(item);
 
         if (rootCache == null) {
@@ -470,7 +470,7 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
 
     private JsonNode generateItemJson(T item) {
         ObjectNode json = JacksonUtils.createObjectNode();
-        json.put("key", getKeyMapper().key(item));
+        json.put("key", keyMapper.key(item));
         dataGenerator.generateData(item, json);
         return json;
     }
@@ -717,6 +717,7 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
             return;
         }
 
+        // Collect IDs of items that are currently in the viewport
         HashSet<Object> viewportItemIds = new HashSet<>();
         for (int i = viewportRange.getStart(); i < viewportRange
                 .getEnd(); i++) {
@@ -734,9 +735,18 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
             viewportItemIds.add(getDataProvider().getId(item));
         }
 
-        // Remove items from the cache, keyMapper, and dataGenerator
-        rootCache.removeDescendantItemIf((item) -> {
-            return !viewportItemIds.contains(getDataProvider().getId(item));
+        // Collect items that have an active key in the keyMapper but
+        // are no longer in the viewport
+        var itemsToRemove = keyMapper.objects().stream().filter((item) -> {
+            var itemId = getDataProvider().getId(item);
+            return !viewportItemIds.contains(itemId);
+        }).toList();
+
+        // Remove those items from the keyMapper and dataGenerator to
+        // release any associated client-side resources.
+        itemsToRemove.forEach((item) -> {
+            dataGenerator.destroyData(item);
+            keyMapper.remove(item);
         });
     }
 
@@ -833,9 +843,9 @@ public class HierarchicalDataCommunicator<T> extends DataCommunicator<T> {
                 void onItemRemoved(T item) {
                     super.onItemRemoved(item);
 
-                    if (getKeyMapper().has(item)) {
+                    if (keyMapper.has(item)) {
                         dataGenerator.destroyData(item);
-                        getKeyMapper().remove(item);
+                        keyMapper.remove(item);
                     }
                 }
             };

flow-data/src/main/java/com/vaadin/flow/data/provider/hierarchy/RootCache.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import com.vaadin.flow.function.SerializablePredicate;
 import com.vaadin.flow.function.ValueProvider;
 
 /**
@@ -189,25 +188,6 @@ class RootCache<T> extends Cache<T> {
     public ItemContext<T> getContextByItem(T item) {
         Object itemId = getItemId(item);
         return itemIdToContext.get(itemId);
-    }
-
-    /**
-     * Removes all descendant items that match the given predicate.
-     *
-     * @param predicate
-     *            the predicate to match items against
-     */
-    public void removeDescendantItemIf(SerializablePredicate<T> predicate) {
-        itemIdToContext.values().stream().filter((itemContext) -> {
-            var cache = itemContext.cache();
-            var index = itemContext.index();
-            var item = cache.getItem(index);
-            return predicate.test(item);
-        }).toList().forEach((itemContext) -> {
-            var cache = itemContext.cache();
-            var index = itemContext.index();
-            cache.removeItem(index);
-        });
     }
 
     void onItemAdded(T item, Cache<T> cache, int index) {

flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorDataGenerationTest.java
@@ -17,6 +17,7 @@ package com.vaadin.flow.data.provider.hierarchy;
 
 import java.util.Arrays;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -48,6 +49,16 @@ public class HierarchicalDataCommunicatorDataGenerationTest
         dataCommunicator.setDataProvider(treeDataProvider, null);
 
         compositeDataGenerator.addDataGenerator(dataGenerator);
+
+        Mockito.doAnswer((invocation) -> {
+            Item item = invocation.getArgument(0);
+
+            Assert.assertTrue(
+                    "Item should be in keyMapper when generateData is called",
+                    dataCommunicator.getKeyMapper().has(item));
+
+            return null;
+        }).when(dataGenerator).destroyData(Mockito.any());
     }
 
     @Test
@@ -152,25 +163,5 @@ public class HierarchicalDataCommunicatorDataGenerationTest
                 .destroyData(new Item("Item 0-1"));
         Mockito.verify(dataGenerator, Mockito.never())
                 .destroyData(new Item("Item 1"));
-    }
-
-    @Test
-    public void collapseItems_destroyDataCalledBeforeItemRemovedFromKeyMapper() {
-        populateTreeData(treeData, 4, 2, 2);
-        dataCommunicator.expand(
-                Arrays.asList(new Item("Item 0"), new Item("Item 0-0")));
-        dataCommunicator.setViewportRange(0, 4);
-        fakeClientCommunication();
-
-        Mockito.doAnswer((invocation) -> {
-            Item item = invocation.getArgument(0);
-            if (!dataCommunicator.getKeyMapper().has(item)) {
-                throw new AssertionError(
-                        "Item should still be in keyMapper when destroyData is called");
-            }
-            return null;
-        }).when(dataGenerator).destroyData(Mockito.any());
-
-        dataCommunicator.collapse(new Item("Item 0"));
     }
 }

flow-data/src/test/java/com/vaadin/flow/data/provider/hierarchy/HierarchicalDataCommunicatorViewportTest.java
@@ -256,7 +256,7 @@ public class HierarchicalDataCommunicatorViewportTest
     }
 
     @Test
-    public void setViewportRange_toggleItemOutsideRange_flatSizeNotUpdated() {
+    public void setViewportRange_toggleItemOutsideViewport_flatSizeNotUpdated() {
         populateTreeData(treeData, 100, 2, 2);
         dataCommunicator.setViewportRange(0, 10);
         fakeClientCommunication();
@@ -275,7 +275,7 @@ public class HierarchicalDataCommunicatorViewportTest
     }
 
     @Test
-    public void setViewportRange_expandItemOutsideRange_adjustRangeToIncludeItem_flatSizeUpdated() {
+    public void setViewportRange_expandItemOutsideViewport_moveItemIntoViewport_flatSizeUpdated() {
         populateTreeData(treeData, 100, 2, 2);
         dataCommunicator.setViewportRange(0, 6);
         fakeClientCommunication();
@@ -291,6 +291,28 @@ public class HierarchicalDataCommunicatorViewportTest
         dataCommunicator.setViewportRange(94, 6);
         fakeClientCommunication();
         assertArrayUpdateSize(102);
+    }
+
+    @Test
+    public void setViewportRange_expandItem_moveItemOutOfViewport_collapseItem_flatSizeUpdated() {
+        populateTreeData(treeData, 100, 2, 2);
+        dataCommunicator.expand(new Item("Item 0"));
+        dataCommunicator.setViewportRange(0, 6);
+        fakeClientCommunication();
+        assertArrayUpdateSize(102);
+
+        Mockito.clearInvocations(arrayUpdater, arrayUpdate);
+
+        dataCommunicator.setViewportRange(101, 1);
+        fakeClientCommunication();
+        dataCommunicator.confirmUpdate(captureArrayUpdateId());
+        fakeClientCommunication();
+
+        Mockito.clearInvocations(arrayUpdater, arrayUpdate);
+
+        dataCommunicator.collapse(new Item("Item 0"));
+        fakeClientCommunication();
+        assertArrayUpdateSize(100);
     }
 
     @Test