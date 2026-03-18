029856fe58ea4b6606e62c69384872351604cae7
Tatu Lund <tatu@vaadin.com>
2026-03-18 12:06:05 +0200

    feat: Implement production mode license verification in VaadinServlet (#23864)
    Add runtime license verification for production mode.
    License check failure is only logged, it does not prevent the application from running.

--- flow-data/src/test/java/com/vaadin/flow/data/provider/DataCommunicatorAsyncTest.java
+++ flow-data/src/test/java/com/vaadin/flow/data/provider/DataCommunicatorAsyncTest.java
@@ -25,10 +25,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.internal.Range;
 import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinService;
@@ -261,8 +263,9 @@ public class DataCommunicatorAsyncTest {
         private static VaadinSession findOrcreateSession() {
             VaadinSession session = VaadinSession.getCurrent();
             if (session == null) {
+                DeploymentConfiguration conf = Mockito.mock(DeploymentConfiguration.class);
                 session = new AlwaysLockedVaadinSession(
-                        new VaadinServletService(new VaadinServlet(), null));
+                        new VaadinServletService(new VaadinServlet(), conf));
                 VaadinSession.setCurrent(session);
             }
             return session;
--- flow-server/src/main/java/com/vaadin/flow/server/VaadinServlet.java
+++ flow-server/src/main/java/com/vaadin/flow/server/VaadinServlet.java
@@ -16,8 +16,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.vaadin.pro.licensechecker.BuildType;
-import com.vaadin.pro.licensechecker.LicenseChecker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,6 +38,9 @@ import com.vaadin.flow.internal.VaadinContextInitializer;
 import com.vaadin.flow.server.HandlerHelper.RequestType;
 import com.vaadin.flow.server.startup.ApplicationConfiguration;
 import com.vaadin.flow.shared.JsonConstants;
+import com.vaadin.pro.licensechecker.BuildType;
+import com.vaadin.pro.licensechecker.LicenseChecker;
+import com.vaadin.pro.licensechecker.LicenseException;
 
 /**
  * The main servlet, which handles all incoming requests to the application.
@@ -58,6 +59,8 @@ import com.vaadin.flow.shared.JsonConstants;
 public class VaadinServlet extends HttpServlet {
 
     public static final String INTERNAL_VAADIN_SERVLET_VITE_DEV_MODE_FRONTEND_PATH = "VAADIN_SERVLET_VITE_DEV_MODE_FRONTEND_PATH";
+
+    private static final String PROJECT_NAME = "flow";
 
     private VaadinServletService servletService;
     private StaticFileHandler staticFileHandler;
@@ -123,7 +126,8 @@ public class VaadinServlet extends HttpServlet {
 
             try {
                 servletService = createServletService();
-                validateLicense(servletService.getDeploymentConfiguration());
+                verifyLicense(servletService.getDeploymentConfiguration()
+                        .isProductionMode());
             } catch (ServiceException e) {
                 throw new ServletException("Could not initialize VaadinServlet",
                         e);
@@ -139,6 +143,25 @@ public class VaadinServlet extends HttpServlet {
             servletInitialized();
         } finally {
             CurrentInstance.clearAll();
+        }
+    }
+
+    private void verifyLicense(boolean productionMode) {
+        String frameworkVersion = Version.getFullVersion();
+        if (productionMode) {
+            try {
+                LicenseChecker.checkLicense(PROJECT_NAME, frameworkVersion,
+                        BuildType.PRODUCTION, null);
+            } catch (LicenseException e) {
+                getLogger().error(
+                        "This Vaadin version requires an extended maintenance subscription."
+                                + "Provide either a server key or an online license checking key,"
+                                + "which you can get from: https://vaadin.com/myaccount/licenses#latest.",
+                        e);
+            }
+        } else {
+            LicenseChecker.checkLicense(PROJECT_NAME, frameworkVersion,
+                    BuildType.DEVELOPMENT);
         }
     }
 
@@ -621,14 +644,5 @@ public class VaadinServlet extends HttpServlet {
      */
     public static String getFrontendMapping() {
         return frontendMapping;
-    }
-
-    private void validateLicense(
-            DeploymentConfiguration deploymentConfiguration) {
-        // Check the license at runtime if in development mode
-        if (!deploymentConfiguration.isProductionMode()) {
-            LicenseChecker.checkLicense("flow", Version.getFullVersion(),
-                    BuildType.DEVELOPMENT);
-        }
     }
 }
