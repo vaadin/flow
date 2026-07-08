6eae250b7dbc3c05e517518470cebbc52dfa30e4
Anton Platonov <anton@vaadin.com>
2026-07-08 12:59:00 +0300

    feat: validate URL schemes in Anchor, IFrame and Page#open (#24539, #24897) (CP: 25.1) (#24924)
    This PR cherry-picks changes from the original PRs #24539 and #24897 to
    branch 25.1, and also disables the validation by default.
    
    The defaults when "safeUrlSchemes" configuration is missing is adjusted,
    so that URL validation is disabled by default, and all URLs are
    considered safe. This avoids unexpected behavior changes for existing
    users of Flow 25.1 and below.
    
    The URL scheme validation feature could be enabled with configuration
    when necessary.
    
    (cherry picked from commit 28e55a3ac19028663efa98a596e05fc1d48a1398)
    (cherry picked from commit 51048d1c52c1ec55f79f526bb5172377ed9aaa96)

--- flow-html-components/src/test/java/com/vaadin/flow/component/html/ComponentTest.java
+++ flow-html-components/src/test/java/com/vaadin/flow/component/html/ComponentTest.java
@@ -22,15 +22,21 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasAriaLabel;
 import com.vaadin.flow.component.HasOrderedComponents;
 import com.vaadin.flow.component.HtmlComponent;
 import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.function.DeploymentConfiguration;
+import com.vaadin.flow.server.VaadinService;
 
 public abstract class ComponentTest {
 
@@ -38,6 +44,28 @@ public abstract class ComponentTest {
     private List<ComponentProperty> properties = new ArrayList<>();
 
     private Set<String> WHITE_LIST = new HashSet<>();
+
+    private static MockedStatic<VaadinService> vaadinServiceMockedStatic;
+
+    @BeforeClass
+    public static void init() {
+        // Mock strict Flow 25.2+ safe URL scheme validation configuration
+        // for feature validation in component tests (AnchorTest, IFrameTest)
+        DeploymentConfiguration config = Mockito
+                .mock(DeploymentConfiguration.class);
+        Mockito.when(config.getUrlSafeSchemes())
+                .thenReturn(Set.of("http", "https", "mailto", "tel", "ftp"));
+        VaadinService service = Mockito.mock(VaadinService.class);
+        Mockito.when(service.getDeploymentConfiguration()).thenReturn(config);
+        vaadinServiceMockedStatic = Mockito.mockStatic(VaadinService.class);
+        vaadinServiceMockedStatic.when(VaadinService::getCurrent)
+                .thenReturn(service);
+    }
+
+    @AfterClass
+    public static void clean() {
+        vaadinServiceMockedStatic.close();
+    }
 
     @Before
     public void setup() throws IntrospectionException, InstantiationException,
